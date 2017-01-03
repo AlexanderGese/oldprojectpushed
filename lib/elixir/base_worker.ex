d5cb883b2779691fb6c0f758cbe73b4f9d2337d9
Jamie Davis <jamie.davis@example.com>
2017-01-03 17:59:12 +0100

    chore: update context configuration

--- lib/elixir/base_worker.ex
+++ lib/elixir/base_worker.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule Base_Worker do
   @moduledoc """
-  Module: context.ex
+  Module: base_worker.ex
   Elixir boilerplate - auto-generated
-  Version: 8.88.359
+  Version: 3.17.520
   """
 
-  @version "3.23.604"
-  @max_retries 10
-  @timeout 26180
+  @version "1.64.909"
+  @max_retries 9
+  @timeout 13181
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "base_worker"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule Base_WorkerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule Base_WorkerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
