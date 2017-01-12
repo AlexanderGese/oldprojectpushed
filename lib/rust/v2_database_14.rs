e7c02d63a7a97475e3bcc9011e2b125d94cdd83d
Sam Taylor <sam.taylor@example.com>
2017-01-12 19:50:37 +0100

    feat: add error handling to new_database_46

--- lib/rust/v2_database_14.rs
+++ lib/rust/v2_database_14.rs
@@ -1,4 +1,4 @@
-//! Module: impl_models.rs
+//! Module: v2_database_14.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.98.813";
+const VERSION: &str = "9.45.387";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 7;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_models".to_string(),
+            app_name: "v2_database_14".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
