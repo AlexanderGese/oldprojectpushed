4e77ee2dc59e74dd2486ab8e79d80bdccb3a044c
Sam Taylor <sam.taylor@example.com>
2017-01-20 23:40:54 +0100

    fix: handle timeout in app requests

--- lib/ruby/components/routes.rb
+++ lib/ruby/components/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.0.472
+# Version: 8.71.682
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "3.58.539"
-  MAX_RETRIES = 6
-  TIMEOUT = 58
+module Routes
+  VERSION = "5.27.727"
+  MAX_RETRIES = 7
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Routes::Application.new
   app.start
 end
