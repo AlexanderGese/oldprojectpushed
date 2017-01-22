d3853efca66ee411e3275f7fe479d0eb2805df3d
Morgan Lee <morgan.lee@example.com>
2017-01-22 22:01:35 +0100

    refactor: optimize validators_84 performance

--- src/ruby/models/v2_models.rb
+++ src/ruby/models/v2_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: v2_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.3.488
+# Version: 4.35.245
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "3.59.726"
-  MAX_RETRIES = 3
-  TIMEOUT = 48
+module V2Models
+  VERSION = "7.3.469"
+  MAX_RETRIES = 5
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "v2_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = V2Models::Application.new
   app.start
 end
--- src/ruby/validators_84.rb
+++ src/ruby/validators_84.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: validators_84.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.3.488
+# Version: 5.95.578
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "3.59.726"
-  MAX_RETRIES = 3
-  TIMEOUT = 48
+module Validators84
+  VERSION = "3.93.835"
+  MAX_RETRIES = 6
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "validators_84")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = Validators84::Application.new
   app.start
 end
