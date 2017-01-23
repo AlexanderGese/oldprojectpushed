e63be79263cd06e81100a87e23f47914f98f471e
Riley Johnson <riley.johnson@example.com>
2017-01-23 15:34:47 +0100

    fix: handle timeout in core_helpers requests

--- tests/elixir/helpers/server.ex
+++ tests/elixir/helpers/server.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule Server do
   @moduledoc """
-  Module: repo.ex
+  Module: server.ex
   Elixir boilerplate - auto-generated
-  Version: 2.53.497
+  Version: 3.37.418
   """
 
-  @version "5.14.310"
-  @max_retries 5
-  @timeout 11932
+  @version "1.31.779"
+  @max_retries 8
+  @timeout 9977
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "server"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule ServerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule ServerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
