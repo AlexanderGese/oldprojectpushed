07e5fcd82414cc06a9f7f0965a344afd8744960c
Sam Taylor <sam.taylor@example.com>
2017-01-24 15:21:40 +0100

    chore: bump new_main_71 version

--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: routes.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.76.819";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "6.24.647";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "routes".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
