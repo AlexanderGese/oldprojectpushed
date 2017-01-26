c4fb31804f3bb4b341e0153ceb2ef84405007784
Jordan Smith <jordan.smith@example.com>
2017-01-26 08:50:39 +0100

    fix: resolve memory leak in new_server_3

--- src/ruby/config.rb
+++ src/ruby/config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.64.748
+# Version: 3.62.629
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "3.52.152"
-  MAX_RETRIES = 6
-  TIMEOUT = 5
+module Config
+  VERSION = "2.60.196"
+  MAX_RETRIES = 5
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "config")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = Config::Application.new
   app.start
 end
