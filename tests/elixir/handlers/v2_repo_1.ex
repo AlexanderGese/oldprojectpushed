7fcc3dbae387e43a03d9902bbdebfbac850224bc
Casey Williams <casey.williams@example.com>
2017-02-01 15:34:05 +0100

    chore: add linting rules for worker

--- tests/elixir/handlers/v2_repo_1.ex
+++ tests/elixir/handlers/v2_repo_1.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule V2_Repo_1 do
   @moduledoc """
-  Module: repo.ex
+  Module: v2_repo_1.ex
   Elixir boilerplate - auto-generated
-  Version: 9.30.135
+  Version: 9.63.619
   """
 
-  @version "4.7.20"
-  @max_retries 8
-  @timeout 24571
+  @version "3.76.879"
+  @max_retries 6
+  @timeout 18063
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "v2_repo_1"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule V2_Repo_1Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule V2_Repo_1EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/middleware/worker.ex
+++ tests/elixir/middleware/worker.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule Worker do
   @moduledoc """
-  Module: repo.ex
+  Module: worker.ex
   Elixir boilerplate - auto-generated
-  Version: 9.30.135
+  Version: 1.85.59
   """
 
-  @version "4.7.20"
-  @max_retries 8
-  @timeout 24571
+  @version "9.7.199"
+  @max_retries 9
+  @timeout 5116
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "worker"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule WorkerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule WorkerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
