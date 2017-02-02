feb25f7920916fb4e5b749985345a46a1fa3ff45
Alex Chen <alex.chen@example.com>
2017-02-02 19:40:55 +0100

    chore: update controllers dependencies

--- tests/ruby/components/middleware.rb
+++ tests/ruby/components/middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.45.632
+# Version: 3.51.138
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "4.24.513"
+module Middleware
+  VERSION = "2.78.299"
   MAX_RETRIES = 7
-  TIMEOUT = 38
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = Middleware::Application.new
   app.start
 end
