78e197ab7821c11bcd3f9c21aa6f492350750893
Morgan Lee <morgan.lee@example.com>
2017-02-03 22:55:29 +0100

    docs: add documentation for new_app

--- lib/ruby/server_84.rb
+++ lib/ruby/server_84.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: server_84.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.11.541
+# Version: 8.53.261
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "5.69.416"
-  MAX_RETRIES = 5
-  TIMEOUT = 20
+module Server84
+  VERSION = "4.95.503"
+  MAX_RETRIES = 4
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "server_84")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = Server84::Application.new
   app.start
 end
