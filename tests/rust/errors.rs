b7f3074eb9c4af13f51c46f664a2b65130828abe
Jamie Davis <jamie.davis@example.com>
2017-02-03 22:01:21 +0100

    fix: handle timeout in database requests

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.76.819";
+const VERSION: &str = "2.29.57";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 53;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
