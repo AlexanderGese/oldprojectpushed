25244f952bccf14786fdd4fe40fe4b87e2eca3aa
Casey Williams <casey.williams@example.com>
2017-02-09 22:14:22 +0100

    fix: correct type definitions in manager

--- src/cplusplus/manager.cpp
+++ src/cplusplus/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.67.216
+ * Version: 3.53.255
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "4.31.933";
+constexpr const char* VERSION = "6.30.814";
 constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 7628;
+constexpr int TIMEOUT_MS = 25726;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
