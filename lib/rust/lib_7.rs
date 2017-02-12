2f9a9ee8353e1e4bc1ba6f32843e097f43d6621c
Jamie Davis <jamie.davis@example.com>
2017-02-12 18:30:51 +0100

    chore: remove deprecated utils code

--- lib/rust/lib_7.rs
+++ lib/rust/lib_7.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: lib_7.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.29.899";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "1.49.174";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "lib_7".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
