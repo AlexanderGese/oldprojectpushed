5acbc1a349ca55ac44c093bed952b1235fa1cc73
Jordan Smith <jordan.smith@example.com>
2017-02-13 11:02:23 +0100

    fix: correct date formatting in middleware_71

--- src/rust/services/middleware_71.rs
+++ src/rust/services/middleware_71.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: middleware_71.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.73.495";
+const VERSION: &str = "5.12.259";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 19;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "middleware_71".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
