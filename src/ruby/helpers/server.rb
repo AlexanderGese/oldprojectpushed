43cc4be914e40e2352a527b3b3ac1ef0bbfbbca9
Morgan Lee <morgan.lee@example.com>
2017-02-14 23:40:20 +0100

    fix: correct type definitions in server

--- src/ruby/helpers/server.rb
+++ src/ruby/helpers/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.3.821
+# Version: 5.98.632
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "6.38.928"
-  MAX_RETRIES = 10
+  VERSION = "2.44.830"
+  MAX_RETRIES = 8
   TIMEOUT = 14
 
   class Config
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
