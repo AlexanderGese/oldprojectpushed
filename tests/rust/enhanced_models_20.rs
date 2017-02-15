0b3b9810143544aa6dee8075042c126abd6a0e16
Morgan Lee <morgan.lee@example.com>
2017-02-15 16:40:25 +0100

    test: add unit tests for middleware

--- tests/rust/enhanced_models_20.rs
+++ tests/rust/enhanced_models_20.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: enhanced_models_20.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.54.300";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "9.96.809";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "enhanced_models_20".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
