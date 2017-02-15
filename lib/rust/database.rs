f7c6dc87fa2cd0cf1388b39719b88ec7635c807e
Riley Johnson <riley.johnson@example.com>
2017-02-15 10:35:53 +0100

    feat: add event system to utils

--- lib/rust/database.rs
+++ lib/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.78.943";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "3.90.408";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.23.549";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "9.53.497";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
