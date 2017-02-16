816e8165d4f5e19ffcb7ce6be662fe7eb79efcd0
Alex Chen <alex.chen@example.com>
2017-02-16 08:30:35 +0100

    fix: handle large payloads in base_models_27

--- src/ruby/components/helpers.rb
+++ src/ruby/components/helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.96.263
+# Version: 2.47.431
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "7.62.794"
-  MAX_RETRIES = 9
-  TIMEOUT = 59
+module Helpers
+  VERSION = "7.64.892"
+  MAX_RETRIES = 5
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = Helpers::Application.new
   app.start
 end
