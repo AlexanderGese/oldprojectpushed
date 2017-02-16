4c34a2ea2533b8556cebfe6b26378ea48d32d1e3
Morgan Lee <morgan.lee@example.com>
2017-02-16 10:21:32 +0100

    refactor: improve core_main_4 error messages

--- src/rust/helpers/core_main_4.rs
+++ src/rust/helpers/core_main_4.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: core_main_4.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.58.603";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "9.71.189";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "core_main_4".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
