8783483dfcde8c52796dc994b95b8cc2ac890d1e
Jamie Davis <jamie.davis@example.com>
2017-02-20 10:08:54 +0100

    fix: correct CORS headers in controllers

--- lib/ruby/components/new_middleware_14.rb
+++ lib/ruby/components/new_middleware_14.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: new_middleware_14.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.0.575
+# Version: 8.18.176
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "2.0.191"
-  MAX_RETRIES = 5
-  TIMEOUT = 26
+module NewMiddleware14
+  VERSION = "7.27.16"
+  MAX_RETRIES = 3
+  TIMEOUT = 34
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "new_middleware_14")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = NewMiddleware14::Application.new
   app.start
 end
--- lib/ruby/config.rb
+++ lib/ruby/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.8.53
+# Version: 4.77.344
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "2.5.505"
-  MAX_RETRIES = 5
-  TIMEOUT = 22
+  VERSION = "1.26.132"
+  MAX_RETRIES = 8
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
