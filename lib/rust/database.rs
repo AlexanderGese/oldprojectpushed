44edf30ce0631d22e1b4bddf546a6687fd271ef7
Morgan Lee <morgan.lee@example.com>
2017-02-22 17:29:31 +0100

    chore: update database dependencies

--- lib/rust/database.rs
+++ lib/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.90.408";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "1.93.192";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.3.983";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "7.71.180";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
