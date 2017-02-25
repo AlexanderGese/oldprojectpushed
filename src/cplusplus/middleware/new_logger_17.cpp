075861396b1a80b201acd95f4aca951e2a3962e9
Jordan Smith <jordan.smith@example.com>
2017-02-25 21:07:21 +0100

    fix: resolve encoding issue in logger

--- src/cplusplus/middleware/new_logger_17.cpp
+++ src/cplusplus/middleware/new_logger_17.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: logger.cpp
+ * Module: new_logger_17.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.82.359
+ * Version: 5.59.371
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace logger {
+namespace new_logger_17 {
 
-constexpr const char* VERSION = "1.43.709";
+constexpr const char* VERSION = "9.80.736";
 constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 21022;
+constexpr int TIMEOUT_MS = 19680;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace logger
+} // namespace new_logger_17
 
 int main() {
-    using namespace logger;
+    using namespace new_logger_17;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
