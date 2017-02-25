3391ce2138c71331b8118318dc75afbb2dccf8da
Sam Taylor <sam.taylor@example.com>
2017-02-25 16:27:44 +0100

    refactor: extract app utility functions

--- src/ruby/base_validators.rb
+++ src/ruby/base_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: base_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.67.80
+# Version: 1.96.448
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseValidators
-  VERSION = "5.10.653"
+  VERSION = "2.10.393"
   MAX_RETRIES = 10
-  TIMEOUT = 6
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- src/ruby/handlers/app.rb
+++ src/ruby/handlers/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.16.305
+# Version: 1.96.506
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "9.73.440"
+  VERSION = "4.70.838"
   MAX_RETRIES = 10
-  TIMEOUT = 31
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
