bde7906a72ea4813175b4a6691d42afa07588d8c
Morgan Lee <morgan.lee@example.com>
2017-02-27 16:22:39 +0100

    feat: add logging to errors

--- src/rust/middleware/middleware.rs
+++ src/rust/middleware/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.9.11";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "8.40.647";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/new_errors.rs
+++ src/rust/new_errors.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: new_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.19.967";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "1.30.580";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "new_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
