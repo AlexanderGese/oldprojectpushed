e0ee20b35f46d65f71dca47ab07f44897a115496
Riley Johnson <riley.johnson@example.com>
2017-03-05 19:13:03 +0100

    test: add unit tests for live

--- tests/elixir/new_controller.ex
+++ tests/elixir/new_controller.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule New_Controller do
   @moduledoc """
-  Module: live.ex
+  Module: new_controller.ex
   Elixir boilerplate - auto-generated
-  Version: 9.43.4
+  Version: 3.94.836
   """
 
-  @version "5.53.136"
-  @max_retries 9
-  @timeout 22223
+  @version "3.17.243"
+  @max_retries 3
+  @timeout 25286
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "new_controller"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule New_ControllerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule New_ControllerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
