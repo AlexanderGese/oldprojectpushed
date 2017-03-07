d641767dc8f9e940d8fec037d4e0fb35e1c0cf37
Alex Chen <alex.chen@example.com>
2017-03-07 13:14:00 +0100

    chore: clean up factory imports

--- src/cplusplus/factory.cpp
+++ src/cplusplus/factory.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.69.543
+ * Version: 4.21.637
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace factory {
 
-constexpr const char* VERSION = "1.58.287";
+constexpr const char* VERSION = "5.81.188";
 constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 19436;
+constexpr int TIMEOUT_MS = 22314;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace factory
 
 int main() {
-    using namespace utils;
+    using namespace factory;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
