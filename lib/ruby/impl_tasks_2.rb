1a8bf37cb3a55c7a745f54d01614e9b5e05de2f7
Jordan Smith <jordan.smith@example.com>
2017-03-10 18:32:02 +0100

    refactor: decouple v2_app dependencies

--- lib/ruby/impl_tasks_2.rb
+++ lib/ruby/impl_tasks_2.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: impl_tasks_2.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.9.954
+# Version: 1.68.415
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "4.98.305"
-  MAX_RETRIES = 6
-  TIMEOUT = 59
+module ImplTasks2
+  VERSION = "5.25.903"
+  MAX_RETRIES = 8
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "impl_tasks_2")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = ImplTasks2::Application.new
   app.start
 end
--- lib/ruby/middleware/v2_app.rb
+++ lib/ruby/middleware/v2_app.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: v2_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.9.954
+# Version: 5.41.740
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "4.98.305"
-  MAX_RETRIES = 6
-  TIMEOUT = 59
+module V2App
+  VERSION = "6.94.49"
+  MAX_RETRIES = 9
+  TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "v2_app")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = V2App::Application.new
   app.start
 end
