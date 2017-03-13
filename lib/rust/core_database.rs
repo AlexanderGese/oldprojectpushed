7ef4037e604afcd4965ee2ad0e2df67f0bccd83a
Casey Williams <casey.williams@example.com>
2017-03-13 14:42:57 +0100

    feat: add pagination to utils

--- lib/rust/core_database.rs
+++ lib/rust/core_database.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: core_database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.51.549";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "6.16.193";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "core_database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
