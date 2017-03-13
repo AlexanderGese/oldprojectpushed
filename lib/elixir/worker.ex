10b09682cf7a6300e1e961b3c01fea42fdd8da84
Morgan Lee <morgan.lee@example.com>
2017-03-13 18:30:36 +0100

    fix: resolve timezone issue in worker

--- lib/elixir/worker.ex
+++ lib/elixir/worker.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Worker do
   @moduledoc """
-  Module: server.ex
+  Module: worker.ex
   Elixir boilerplate - auto-generated
-  Version: 6.46.591
+  Version: 3.75.62
   """
 
-  @version "5.33.777"
-  @max_retries 4
-  @timeout 16056
+  @version "6.76.332"
+  @max_retries 7
+  @timeout 12021
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "worker"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule WorkerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule WorkerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
