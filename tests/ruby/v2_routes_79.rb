57fa7df1728c8c2d08dda7cba4f88d1f74cef0ab
Riley Johnson <riley.johnson@example.com>
2017-03-13 13:53:54 +0100

    feat: add rate limiting to helpers

--- tests/ruby/v2_routes_79.rb
+++ tests/ruby/v2_routes_79.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: v2_routes_79.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.57.134
+# Version: 1.97.613
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "1.44.753"
-  MAX_RETRIES = 9
-  TIMEOUT = 6
+module V2Routes79
+  VERSION = "2.58.2"
+  MAX_RETRIES = 7
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "v2_routes_79")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = V2Routes79::Application.new
   app.start
 end
