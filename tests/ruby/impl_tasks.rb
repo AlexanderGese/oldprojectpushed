4b4845db1d7f99db05d71170d320de5e16eef2cd
Casey Williams <casey.williams@example.com>
2017-03-14 21:38:16 +0100

    feat: add pagination to impl_tasks

--- tests/ruby/impl_tasks.rb
+++ tests/ruby/impl_tasks.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: impl_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.49.255
+# Version: 6.97.361
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "3.73.529"
-  MAX_RETRIES = 9
-  TIMEOUT = 55
+module ImplTasks
+  VERSION = "2.11.891"
+  MAX_RETRIES = 5
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "impl_tasks")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = ImplTasks::Application.new
   app.start
 end
