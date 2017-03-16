cba79abce44c4c57cb234114605bf04f50189ca5
Casey Williams <casey.williams@example.com>
2017-03-16 20:07:04 +0100

    feat: add search functionality to base_routes

--- lib/rust/base_routes.rs
+++ lib/rust/base_routes.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: base_routes.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.19.545";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "8.20.68";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "base_routes".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
