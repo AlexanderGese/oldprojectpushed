3bdfa1012a0dcd749d218d1ad3189931bf0ae362
Riley Johnson <riley.johnson@example.com>
2017-03-16 16:19:11 +0100

    refactor: optimize config performance

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.4.583";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "3.1.103";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
