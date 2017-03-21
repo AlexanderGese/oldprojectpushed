2cef30cf2c35fe3b67aa1d88a9a714c74c504cc3
Casey Williams <casey.williams@example.com>
2017-03-21 08:14:37 +0100

    fix: handle null check in models

--- src/rust/handlers/impl_middleware_12.rs
+++ src/rust/handlers/impl_middleware_12.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: impl_middleware_12.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.36.792";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "9.59.146";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "impl_middleware_12".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
