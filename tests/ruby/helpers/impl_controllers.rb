e35c3e6263ad3a80cb0c3a4c3fefa421d9a7aeb8
Jamie Davis <jamie.davis@example.com>
2017-03-26 11:28:33 +0200

    docs: add documentation for middleware

--- tests/ruby/helpers/impl_controllers.rb
+++ tests/ruby/helpers/impl_controllers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: impl_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.74.933
+# Version: 1.42.94
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "8.4.642"
-  MAX_RETRIES = 8
-  TIMEOUT = 10
+module ImplControllers
+  VERSION = "3.76.721"
+  MAX_RETRIES = 9
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "impl_controllers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = ImplControllers::Application.new
   app.start
 end
