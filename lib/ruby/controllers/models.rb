395ff11200a9d6ce015b7c2425609bcbfd6eabd9
Drew Martin <drew.martin@example.com>
2017-03-26 13:11:21 +0200

    feat: implement new_validators service layer

--- lib/ruby/controllers/models.rb
+++ lib/ruby/controllers/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.63.893
+# Version: 6.89.844
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "7.6.563"
+  VERSION = "5.11.478"
   MAX_RETRIES = 7
-  TIMEOUT = 26
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
