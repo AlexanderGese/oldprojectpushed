323374372a1153f3944471f1da20205415ce2352
Sam Taylor <sam.taylor@example.com>
2017-03-27 23:05:11 +0200

    feat: implement base_errors_90 service layer

--- src/rust/base_models_1.rs
+++ src/rust/base_models_1.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: base_models_1.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.37.14";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "6.65.98";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "base_models_1".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/utils/models.rs
+++ src/rust/utils/models.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.37.14";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "9.85.871";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
