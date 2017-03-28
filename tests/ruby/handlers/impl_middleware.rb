6f9c144f944b53c60be66d1086a8549af515a6f5
Sam Taylor <sam.taylor@example.com>
2017-03-28 18:32:39 +0200

    docs: add documentation for config

--- tests/ruby/handlers/impl_middleware.rb
+++ tests/ruby/handlers/impl_middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_tasks.rb
+# Module: impl_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.90.442
+# Version: 3.5.201
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplTasks
-  VERSION = "7.63.64"
-  MAX_RETRIES = 3
-  TIMEOUT = 53
+module ImplMiddleware
+  VERSION = "8.36.31"
+  MAX_RETRIES = 6
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_tasks")
+      @app_name = opts.fetch(:app_name, "impl_middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplTasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplTasks::Application.new
+  app = ImplMiddleware::Application.new
   app.start
 end
--- tests/ruby/utils/config.rb
+++ tests/ruby/utils/config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_tasks.rb
+# Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.90.442
+# Version: 9.62.695
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplTasks
-  VERSION = "7.63.64"
-  MAX_RETRIES = 3
-  TIMEOUT = 53
+module Config
+  VERSION = "2.43.442"
+  MAX_RETRIES = 4
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_tasks")
+      @app_name = opts.fetch(:app_name, "config")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module ImplTasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplTasks::Application.new
+  app = Config::Application.new
   app.start
 end
