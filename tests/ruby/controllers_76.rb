724e3bc8654a88f3f0f8fc4b04f884c90bb24ef5
Casey Williams <casey.williams@example.com>
2017-03-29 20:49:39 +0200

    fix: resolve memory leak in new_middleware

--- tests/ruby/controllers_76.rb
+++ tests/ruby/controllers_76.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_middleware.rb
+# Module: controllers_76.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.94.393
+# Version: 3.90.29
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewMiddleware
-  VERSION = "2.32.375"
-  MAX_RETRIES = 9
-  TIMEOUT = 24
+module Controllers76
+  VERSION = "4.16.730"
+  MAX_RETRIES = 5
+  TIMEOUT = 54
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_middleware")
+      @app_name = opts.fetch(:app_name, "controllers_76")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module NewMiddleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewMiddleware::Application.new
+  app = Controllers76::Application.new
   app.start
 end
