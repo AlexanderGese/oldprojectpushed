194383de9bb5eacd0c5500dd97330a00342498c3
Casey Williams <casey.williams@example.com>
2017-03-29 15:11:57 +0200

    feat: implement SSE streaming in new_handlers

--- tests/rust/utils/database.rs
+++ tests/rust/utils/database.rs
@@ -1,4 +1,4 @@
-//! Module: impl_routes.rs
+//! Module: database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.75.840";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "7.76.293";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_routes".to_string(),
+            app_name: "database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
