7583f2b280e027f69281cb29ae726caa7051c37d
Jordan Smith <jordan.smith@example.com>
2017-03-30 12:42:00 +0200

    chore: configure CI/CD for server

--- src/elixir/middleware/helpers.ex
+++ src/elixir/middleware/helpers.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Helpers do
   @moduledoc """
-  Module: server.ex
+  Module: helpers.ex
   Elixir boilerplate - auto-generated
-  Version: 7.38.646
+  Version: 7.15.418
   """
 
-  @version "5.67.506"
-  @max_retries 3
-  @timeout 8044
+  @version "5.64.913"
+  @max_retries 8
+  @timeout 12447
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "helpers"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule HelpersStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule HelpersEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
