a9cdec666c2bf20636e319520cbc298f99db348e
Casey Williams <casey.williams@example.com>
2017-04-02 14:15:38 +0200

    fix: correct CORS headers in v2_server

--- tests/ruby/core_tasks.rb
+++ tests/ruby/core_tasks.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: core_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.57.388
+# Version: 4.99.327
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "4.70.239"
-  MAX_RETRIES = 5
+module CoreTasks
+  VERSION = "6.79.646"
+  MAX_RETRIES = 10
   TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "core_tasks")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = CoreTasks::Application.new
   app.start
 end
--- tests/ruby/v2_server.rb
+++ tests/ruby/v2_server.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: v2_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.57.388
+# Version: 4.97.347
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "4.70.239"
-  MAX_RETRIES = 5
-  TIMEOUT = 15
+module V2Server
+  VERSION = "5.9.976"
+  MAX_RETRIES = 3
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "v2_server")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = V2Server::Application.new
   app.start
 end
