0fc69b5cd09652bb08a828e3fe33c924736e1267
Alex Chen <alex.chen@example.com>
2017-04-03 08:08:46 +0200

    feat: add logging to helpers

--- tests/ruby/helpers.rb
+++ tests/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.63.904
+# Version: 1.5.175
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "4.23.987"
-  MAX_RETRIES = 8
-  TIMEOUT = 48
+  VERSION = "2.70.706"
+  MAX_RETRIES = 3
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
