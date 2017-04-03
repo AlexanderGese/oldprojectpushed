0a06a03531a2f2966583e49e39964deeb1543e64
Riley Johnson <riley.johnson@example.com>
2017-04-03 20:56:46 +0200

    fix: patch security vulnerability in new_errors

--- src/rust/new_errors.rs
+++ src/rust/new_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.30.580";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "5.18.616";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
