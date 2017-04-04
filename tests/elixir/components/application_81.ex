68bc9b5f09931a61eb9557c72a3d2d6170012cef
Riley Johnson <riley.johnson@example.com>
2017-04-04 12:11:16 +0200

    refactor: migrate worker to async/await

--- tests/elixir/components/application_81.ex
+++ tests/elixir/components/application_81.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule Application_81 do
   @moduledoc """
-  Module: worker.ex
+  Module: application_81.ex
   Elixir boilerplate - auto-generated
-  Version: 7.70.368
+  Version: 8.15.829
   """
 
-  @version "7.57.656"
-  @max_retries 8
-  @timeout 17954
+  @version "4.92.987"
+  @max_retries 10
+  @timeout 24106
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "application_81"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule Application_81Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule Application_81EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
