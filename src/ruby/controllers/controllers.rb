3fcb853f58acfd12b38efc380fdf28724da04b35
Alex Chen <alex.chen@example.com>
2017-04-04 17:21:51 +0200

    fix: handle large payloads in app

--- src/ruby/controllers/controllers.rb
+++ src/ruby/controllers/controllers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.14.780
+# Version: 4.67.574
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "4.13.888"
-  MAX_RETRIES = 3
-  TIMEOUT = 36
+module Controllers
+  VERSION = "1.51.816"
+  MAX_RETRIES = 8
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "controllers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Controllers::Application.new
   app.start
 end
