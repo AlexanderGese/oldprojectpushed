a9507b11da857dc7aa2d1881f98f860bc4a2baa5
Sam Taylor <sam.taylor@example.com>
2017-04-04 08:34:16 +0200

    fix: correct date formatting in worker

--- lib/elixir/middleware/worker.ex
+++ lib/elixir/middleware/worker.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule Worker do
   @moduledoc """
-  Module: repo.ex
+  Module: worker.ex
   Elixir boilerplate - auto-generated
-  Version: 1.28.966
+  Version: 2.60.570
   """
 
-  @version "9.89.831"
-  @max_retries 7
-  @timeout 5597
+  @version "1.92.880"
+  @max_retries 3
+  @timeout 27810
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "worker"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule WorkerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule WorkerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
