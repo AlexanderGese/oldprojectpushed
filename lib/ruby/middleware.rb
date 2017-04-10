7cbeff039372e56fc5259e049891d8d31e91f9b7
Morgan Lee <morgan.lee@example.com>
2017-04-10 22:02:01 +0200

    feat: implement SSE streaming in controllers

--- lib/ruby/middleware.rb
+++ lib/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.72.884
+# Version: 1.50.793
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "2.37.923"
+  VERSION = "3.45.768"
   MAX_RETRIES = 10
-  TIMEOUT = 59
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
