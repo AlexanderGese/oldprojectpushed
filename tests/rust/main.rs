13511b407bc63f962ad2a0e70cbdef3558214f42
Jordan Smith <jordan.smith@example.com>
2017-04-10 16:39:47 +0200

    fix: resolve edge case in main handler

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.60.702";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "6.63.605";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
