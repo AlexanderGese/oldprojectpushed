dff197b3b0115e2aead9a50e9f0c9a90a37dd27f
Sam Taylor <sam.taylor@example.com>
2017-04-10 20:12:46 +0200

    fix: correct error propagation in tasks

--- src/ruby/components/tasks.rb
+++ src/ruby/components/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.5.880
+# Version: 3.94.402
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "2.25.331"
-  MAX_RETRIES = 10
-  TIMEOUT = 37
+  VERSION = "2.28.434"
+  MAX_RETRIES = 9
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
