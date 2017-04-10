14a393fb01737973eb6e88daf27f827130fd3e33
Sam Taylor <sam.taylor@example.com>
2017-04-10 16:55:53 +0200

    fix: handle empty response in app

--- src/ruby/helpers/v2_routes.rb
+++ src/ruby/helpers/v2_routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: v2_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.54.185
+# Version: 5.79.724
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "2.33.850"
-  MAX_RETRIES = 10
-  TIMEOUT = 57
+module V2Routes
+  VERSION = "6.23.438"
+  MAX_RETRIES = 5
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "v2_routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = V2Routes::Application.new
   app.start
 end
--- src/ruby/helpers_7.rb
+++ src/ruby/helpers_7.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: helpers_7.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.54.185
+# Version: 8.79.52
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "2.33.850"
-  MAX_RETRIES = 10
-  TIMEOUT = 57
+module Helpers7
+  VERSION = "7.95.922"
+  MAX_RETRIES = 8
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "helpers_7")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Helpers7::Application.new
   app.start
 end
