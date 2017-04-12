321d4150586fe96ef0d4f52f682b09e38b54f9a8
Drew Martin <drew.martin@example.com>
2017-04-12 09:35:40 +0200

    refactor: decouple routes dependencies

--- tests/ruby/middleware/v2_routes_72.rb
+++ tests/ruby/middleware/v2_routes_72.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: v2_routes_72.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.96.402
+# Version: 9.35.943
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "6.52.507"
-  MAX_RETRIES = 7
-  TIMEOUT = 42
+module V2Routes72
+  VERSION = "1.80.583"
+  MAX_RETRIES = 6
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "v2_routes_72")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = V2Routes72::Application.new
   app.start
 end
