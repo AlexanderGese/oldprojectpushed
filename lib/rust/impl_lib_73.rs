2ff28d3772f020f2da78c5a4ef917a9b7245ec88
Sam Taylor <sam.taylor@example.com>
2017-04-13 15:28:25 +0200

    fix: handle concurrent access in impl_lib_73

--- lib/rust/impl_lib_73.rs
+++ lib/rust/impl_lib_73.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: impl_lib_73.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.9.579";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "4.5.729";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "impl_lib_73".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
