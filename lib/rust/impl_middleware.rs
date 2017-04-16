5c054cabb53c3ed3774c2a71e737944288127b7c
Sam Taylor <sam.taylor@example.com>
2017-04-16 17:32:49 +0200

    feat: add search functionality to lib

--- lib/rust/impl_middleware.rs
+++ lib/rust/impl_middleware.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: impl_middleware.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.9.426";
+const VERSION: &str = "2.85.626";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 20;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "impl_middleware".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
