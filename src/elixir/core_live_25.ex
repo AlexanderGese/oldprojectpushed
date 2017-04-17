6e3cccffe82f2381188707461ff2c7d620254e3f
Sam Taylor <sam.taylor@example.com>
2017-04-17 09:46:44 +0200

    fix: resolve race condition in application

--- src/elixir/core_live_25.ex
+++ src/elixir/core_live_25.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule Core_Live_25 do
   @moduledoc """
-  Module: application.ex
+  Module: core_live_25.ex
   Elixir boilerplate - auto-generated
-  Version: 7.1.649
+  Version: 7.95.143
   """
 
-  @version "6.13.239"
-  @max_retries 3
-  @timeout 5177
+  @version "8.87.436"
+  @max_retries 4
+  @timeout 15077
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "core_live_25"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule Core_Live_25Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule Core_Live_25EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- src/elixir/models/impl_schema.ex
+++ src/elixir/models/impl_schema.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule Impl_Schema do
   @moduledoc """
-  Module: application.ex
+  Module: impl_schema.ex
   Elixir boilerplate - auto-generated
-  Version: 7.1.649
+  Version: 5.96.447
   """
 
-  @version "6.13.239"
+  @version "1.72.337"
   @max_retries 3
-  @timeout 5177
+  @timeout 24463
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "impl_schema"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule Impl_SchemaStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule Impl_SchemaEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
