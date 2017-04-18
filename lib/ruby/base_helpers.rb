a801f24987afb1945b7e50a33bb289732403c249
Casey Williams <casey.williams@example.com>
2017-04-18 11:32:52 +0200

    feat: add validation to base_helpers inputs

--- lib/ruby/base_helpers.rb
+++ lib/ruby/base_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: base_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.21.865
+# Version: 1.85.508
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseHelpers
-  VERSION = "8.68.89"
-  MAX_RETRIES = 4
-  TIMEOUT = 34
+  VERSION = "6.82.326"
+  MAX_RETRIES = 8
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
