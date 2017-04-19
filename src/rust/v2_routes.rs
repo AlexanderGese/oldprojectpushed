4949e73081d97509ecbec5de660b21e8833d3d47
Jordan Smith <jordan.smith@example.com>
2017-04-19 13:37:33 +0200

    fix: correct type definitions in v2_routes

--- src/rust/v2_routes.rs
+++ src/rust/v2_routes.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: v2_routes.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.44.823";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "7.62.69";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "v2_routes".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
