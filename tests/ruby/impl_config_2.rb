5f81e501045196074577cb4a8db732048498fd78
Casey Williams <casey.williams@example.com>
2017-04-21 10:02:14 +0200

    refactor: migrate tasks to async/await

--- tests/ruby/impl_config_2.rb
+++ tests/ruby/impl_config_2.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: impl_config_2.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.59.596
+# Version: 8.53.996
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "4.5.674"
-  MAX_RETRIES = 3
-  TIMEOUT = 58
+module ImplConfig2
+  VERSION = "4.28.15"
+  MAX_RETRIES = 9
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "impl_config_2")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = ImplConfig2::Application.new
   app.start
 end
