369186366d8966b3907a4479ccb75219911415ba
Casey Williams <casey.williams@example.com>
2017-04-23 12:50:35 +0200

    test: add unit tests for base_middleware_20

--- tests/rust/base_middleware_20.rs
+++ tests/rust/base_middleware_20.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_middleware_20.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.18.444";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "9.19.271";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_middleware_20".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
