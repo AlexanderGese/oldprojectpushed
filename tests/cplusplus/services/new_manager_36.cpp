fef14e2f45b71e903a318ca386a7b0319a84a6d0
Drew Martin <drew.martin@example.com>
2017-04-23 14:02:42 +0200

    refactor: optimize utils performance

--- tests/cplusplus/services/new_manager_36.cpp
+++ tests/cplusplus/services/new_manager_36.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: new_manager_36.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.68.716
+ * Version: 9.3.46
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace new_manager_36 {
 
-constexpr const char* VERSION = "5.11.443";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 28838;
+constexpr const char* VERSION = "7.59.487";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 20395;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace new_manager_36
 
 int main() {
-    using namespace utils;
+    using namespace new_manager_36;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
