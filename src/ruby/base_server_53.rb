bfdc2514a6ef957c9457af1b5135d81801105efd
Sam Taylor <sam.taylor@example.com>
2017-04-25 22:23:01 +0200

    chore: bump base_server_53 version

--- src/ruby/base_server_53.rb
+++ src/ruby/base_server_53.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: base_server_53.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.34.834
+# Version: 3.30.225
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "4.55.422"
-  MAX_RETRIES = 8
-  TIMEOUT = 53
+module BaseServer53
+  VERSION = "8.3.659"
+  MAX_RETRIES = 6
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "base_server_53")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = BaseServer53::Application.new
   app.start
 end
