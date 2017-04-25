e9bd532f947638f72a0c536b6caf873cc01c899a
Sam Taylor <sam.taylor@example.com>
2017-04-25 21:12:17 +0200

    feat: implement authentication in core_controllers

--- lib/ruby/middleware/v2_validators.rb
+++ lib/ruby/middleware/v2_validators.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_controllers.rb
+# Module: v2_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.19.622
+# Version: 7.91.548
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreControllers
-  VERSION = "8.38.428"
-  MAX_RETRIES = 4
-  TIMEOUT = 54
+module V2Validators
+  VERSION = "6.87.766"
+  MAX_RETRIES = 7
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_controllers")
+      @app_name = opts.fetch(:app_name, "v2_validators")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module CoreControllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreControllers::Application.new
+  app = V2Validators::Application.new
   app.start
 end
--- lib/ruby/new_helpers.rb
+++ lib/ruby/new_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_controllers.rb
+# Module: new_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.19.622
+# Version: 4.6.490
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreControllers
-  VERSION = "8.38.428"
-  MAX_RETRIES = 4
-  TIMEOUT = 54
+module NewHelpers
+  VERSION = "7.82.254"
+  MAX_RETRIES = 10
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_controllers")
+      @app_name = opts.fetch(:app_name, "new_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module CoreControllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreControllers::Application.new
+  app = NewHelpers::Application.new
   app.start
 end
