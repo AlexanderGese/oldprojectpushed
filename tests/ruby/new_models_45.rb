0ed2a89c6588cdcf7e3e549c058cbc7da1cb519e
Casey Williams <casey.williams@example.com>
2017-04-27 23:44:06 +0200

    fix: handle null check in validators

--- tests/ruby/new_models_45.rb
+++ tests/ruby/new_models_45.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: new_models_45.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.67.944
+# Version: 9.16.887
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "8.24.803"
+module NewModels45
+  VERSION = "8.20.130"
   MAX_RETRIES = 9
-  TIMEOUT = 49
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "new_models_45")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = NewModels45::Application.new
   app.start
 end
