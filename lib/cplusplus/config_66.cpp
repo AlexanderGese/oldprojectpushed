86836b7b60a74b5d96923a7a2b8625b63d374fa8
Drew Martin <drew.martin@example.com>
2017-04-27 17:55:16 +0200

    feat: add export functionality to app

--- lib/cplusplus/config_66.cpp
+++ lib/cplusplus/config_66.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: app.cpp
+ * Module: config_66.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.15.129
+ * Version: 1.96.911
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace app {
+namespace config_66 {
 
-constexpr const char* VERSION = "9.98.310";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 12355;
+constexpr const char* VERSION = "2.32.704";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 29254;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace app
+} // namespace config_66
 
 int main() {
-    using namespace app;
+    using namespace config_66;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- lib/cplusplus/impl_utils_96.cpp
+++ lib/cplusplus/impl_utils_96.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: app.cpp
+ * Module: impl_utils_96.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.15.129
+ * Version: 8.51.785
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace app {
+namespace impl_utils_96 {
 
-constexpr const char* VERSION = "9.98.310";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 12355;
+constexpr const char* VERSION = "5.91.162";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 19941;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace app
+} // namespace impl_utils_96
 
 int main() {
-    using namespace app;
+    using namespace impl_utils_96;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
