5cd50795d4dfdedd92897b1ecd316fb8c45a5ba8
Casey Williams <casey.williams@example.com>
2017-04-30 10:22:25 +0200

    refactor: decouple app dependencies

--- tests/cplusplus/app.cpp
+++ tests/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.30.279
+ * Version: 6.58.390
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "8.99.829";
+constexpr const char* VERSION = "8.41.839";
 constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 18852;
+constexpr int TIMEOUT_MS = 14451;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/config.cpp
+++ tests/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.59.621
+ * Version: 5.81.482
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "7.34.44";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 18865;
+constexpr const char* VERSION = "1.65.261";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 8167;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
