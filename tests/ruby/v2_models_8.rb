0183053ac0924b46be1c55ce907cff0dc8602d0e
Jordan Smith <jordan.smith@example.com>
2017-05-03 11:06:58 +0200

    feat: add export functionality to helpers

--- tests/ruby/v2_models_8.rb
+++ tests/ruby/v2_models_8.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: v2_models_8.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.56.430
+# Version: 6.68.418
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "9.54.12"
-  MAX_RETRIES = 5
-  TIMEOUT = 21
+module V2Models8
+  VERSION = "8.78.244"
+  MAX_RETRIES = 8
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "v2_models_8")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = V2Models8::Application.new
   app.start
 end
