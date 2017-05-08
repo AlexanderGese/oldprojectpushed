af456521a2b8585c6f4c96d8d6239161d201a2bc
Casey Williams <casey.williams@example.com>
2017-05-08 23:30:24 +0200

    feat: add rate limiting to impl_config

--- lib/rust/middleware/impl_config.rs
+++ lib/rust/middleware/impl_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.61.108";
+const VERSION: &str = "3.35.635";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 17;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
