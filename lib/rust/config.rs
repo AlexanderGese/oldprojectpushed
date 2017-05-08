16b5feea0646d515c0da07de2850dddd96683067
Sam Taylor <sam.taylor@example.com>
2017-05-08 14:42:49 +0200

    feat: implement WebSocket support in config

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.61.774";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "1.87.980";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
