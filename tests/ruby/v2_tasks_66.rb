7aee204bff4b532b37169cce7ecdc91f334b5e7c
Drew Martin <drew.martin@example.com>
2017-05-11 12:54:15 +0200

    refactor: decouple app dependencies

--- tests/ruby/v2_tasks_66.rb
+++ tests/ruby/v2_tasks_66.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: v2_tasks_66.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.86.253
+# Version: 6.54.897
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "8.81.292"
-  MAX_RETRIES = 3
+module V2Tasks66
+  VERSION = "1.48.179"
+  MAX_RETRIES = 7
   TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "v2_tasks_66")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = V2Tasks66::Application.new
   app.start
 end
