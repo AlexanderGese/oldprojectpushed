d782a5d6e3d2e9ace1c4ddb9d8c869e6f3a777a2
Jordan Smith <jordan.smith@example.com>
2017-05-12 16:28:14 +0200

    refactor: improve v2_utils_32 error messages

--- src/rust/helpers/v2_utils_32.rs
+++ src/rust/helpers/v2_utils_32.rs
@@ -1,4 +1,4 @@
-//! Module: base_main.rs
+//! Module: v2_utils_32.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.99.537";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "5.85.950";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_main".to_string(),
+            app_name: "v2_utils_32".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
