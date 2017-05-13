5f85275f49ff8c93ef424942876f05d93be9cd35
Morgan Lee <morgan.lee@example.com>
2017-05-13 10:25:11 +0200

    fix: handle null check in base_main

--- tests/rust/base_main.rs
+++ tests/rust/base_main.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_main.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.78.726";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "2.43.262";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_main".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/helpers/impl_database_1.rs
+++ tests/rust/helpers/impl_database_1.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: impl_database_1.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.78.726";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "3.3.914";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "impl_database_1".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
