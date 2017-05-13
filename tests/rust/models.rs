07b29c7eec043de9df19b550b846b725e37f11a5
Morgan Lee <morgan.lee@example.com>
2017-05-13 22:02:20 +0200

    feat: add pagination to models

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.28.859";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "2.66.66";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
