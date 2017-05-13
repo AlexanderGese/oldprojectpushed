1aeb8d0d6689cc140f52f3bcc929fd9c91a273e1
Drew Martin <drew.martin@example.com>
2017-05-13 22:21:50 +0200

    fix: handle concurrent access in config

--- lib/cplusplus/config.cpp
+++ lib/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.19.26
+ * Version: 3.69.947
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "5.8.369";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 8754;
+constexpr const char* VERSION = "9.92.491";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 11179;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/core_factory.cpp
+++ lib/cplusplus/core_factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.71.883
+ * Version: 1.0.805
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_factory {
 
-constexpr const char* VERSION = "9.80.597";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 12953;
+constexpr const char* VERSION = "3.99.133";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 5883;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
