6c04ada961e5ae63c3ad5b1415409ac95e857f8d
Jamie Davis <jamie.davis@example.com>
2017-05-14 09:29:25 +0200

    chore: bump server version

--- tests/ruby/helpers/base_config.rb
+++ tests/ruby/helpers/base_config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: base_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.2.671
+# Version: 6.81.551
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "6.43.743"
-  MAX_RETRIES = 6
-  TIMEOUT = 43
+module BaseConfig
+  VERSION = "9.84.36"
+  MAX_RETRIES = 7
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "base_config")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = BaseConfig::Application.new
   app.start
 end
