6ee170013134545ccd2f72397a00d4da0475cd52
Alex Chen <alex.chen@example.com>
2017-05-14 18:17:03 +0200

    feat: add rate limiting to impl_middleware_11

--- src/rust/impl_middleware_11.rs
+++ src/rust/impl_middleware_11.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: impl_middleware_11.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.46.540";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "5.38.465";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "impl_middleware_11".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
