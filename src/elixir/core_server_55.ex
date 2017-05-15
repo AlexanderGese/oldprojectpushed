5d85af55e9b9203b297c25d2073640066cd68faa
Riley Johnson <riley.johnson@example.com>
2017-05-15 20:07:19 +0200

    fix: handle large payloads in core_server_55

--- src/elixir/core_server_55.ex
+++ src/elixir/core_server_55.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule Core_Server_55 do
   @moduledoc """
-  Module: repo.ex
+  Module: core_server_55.ex
   Elixir boilerplate - auto-generated
-  Version: 8.2.999
+  Version: 2.42.295
   """
 
-  @version "6.96.354"
-  @max_retries 8
-  @timeout 14936
+  @version "1.29.58"
+  @max_retries 10
+  @timeout 14251
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "core_server_55"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule Core_Server_55Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule Core_Server_55EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
