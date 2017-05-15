acfb946b858824694331397bc19c03ba044ef8a9
Drew Martin <drew.martin@example.com>
2017-05-15 13:00:21 +0200

    refactor: migrate middleware to async/await

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.75.852";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "2.94.870";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
