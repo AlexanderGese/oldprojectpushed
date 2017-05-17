3ad795de255dcd45aba1b7181c31d38c1dd3e661
Sam Taylor <sam.taylor@example.com>
2017-05-17 11:19:07 +0200

    feat: implement retry logic in core_validators_14

--- tests/ruby/base_app_22.rb
+++ tests/ruby/base_app_22.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_helpers.rb
+# Module: base_app_22.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.80.931
+# Version: 9.19.157
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplHelpers
-  VERSION = "4.63.795"
-  MAX_RETRIES = 9
-  TIMEOUT = 22
+module BaseApp22
+  VERSION = "3.31.724"
+  MAX_RETRIES = 6
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_helpers")
+      @app_name = opts.fetch(:app_name, "base_app_22")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplHelpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplHelpers::Application.new
+  app = BaseApp22::Application.new
   app.start
 end
--- tests/ruby/core_validators_14.rb
+++ tests/ruby/core_validators_14.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_helpers.rb
+# Module: core_validators_14.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.80.931
+# Version: 6.48.956
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplHelpers
-  VERSION = "4.63.795"
-  MAX_RETRIES = 9
-  TIMEOUT = 22
+module CoreValidators14
+  VERSION = "3.86.179"
+  MAX_RETRIES = 4
+  TIMEOUT = 14
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_helpers")
+      @app_name = opts.fetch(:app_name, "core_validators_14")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module ImplHelpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplHelpers::Application.new
+  app = CoreValidators14::Application.new
   app.start
 end
