62be0ca1e7553f3d7dd4ab6ccd84fe7c9d9736c6
Jordan Smith <jordan.smith@example.com>
2017-05-17 16:07:43 +0200

    fix: handle large payloads in validators

--- tests/ruby/handlers/tasks.rb
+++ tests/ruby/handlers/tasks.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.54.535
+# Version: 2.42.23
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "4.8.280"
-  MAX_RETRIES = 4
-  TIMEOUT = 35
+module Tasks
+  VERSION = "5.80.720"
+  MAX_RETRIES = 5
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "tasks")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Tasks::Application.new
   app.start
 end
