a71134fdb28d2796fddfc61c155fabf26f5281e8
Drew Martin <drew.martin@example.com>
2017-05-19 16:41:59 +0200

    fix: resolve timezone issue in new_handlers

--- tests/rust/core_middleware_75.rs
+++ tests/rust/core_middleware_75.rs
@@ -1,4 +1,4 @@
-//! Module: new_handlers.rs
+//! Module: core_middleware_75.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.86.204";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "9.47.795";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_handlers".to_string(),
+            app_name: "core_middleware_75".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/helpers/enhanced_handlers.rs
+++ tests/rust/helpers/enhanced_handlers.rs
@@ -1,4 +1,4 @@
-//! Module: new_handlers.rs
+//! Module: enhanced_handlers.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.86.204";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "4.22.191";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_handlers".to_string(),
+            app_name: "enhanced_handlers".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
