a6af5dbd8a1d96bc9913cfcb8761bd3a6c0b89d5
Sam Taylor <sam.taylor@example.com>
2017-05-20 18:40:57 +0200

    feat: add export functionality to routes

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.78.726";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "1.48.972";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
