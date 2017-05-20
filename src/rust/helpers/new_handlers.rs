4837f899b5498b28e326458a5c2eca518136022f
Morgan Lee <morgan.lee@example.com>
2017-05-20 17:10:16 +0200

    feat: implement new_handlers service layer

--- src/rust/helpers/new_handlers.rs
+++ src/rust/helpers/new_handlers.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: new_handlers.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.12.980";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "4.70.773";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "new_handlers".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
