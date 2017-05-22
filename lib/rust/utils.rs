f783361a44147cc0393c98977865611bc19363cc
Drew Martin <drew.martin@example.com>
2017-05-22 13:15:40 +0200

    chore: update utils dependencies

--- lib/rust/utils.rs
+++ lib/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.51.549";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "7.71.163";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
