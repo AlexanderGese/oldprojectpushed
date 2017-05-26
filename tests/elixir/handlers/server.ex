953dc161565f80b983023a0fb6676cb72801e822
Riley Johnson <riley.johnson@example.com>
2017-05-26 12:36:53 +0200

    feat: add middleware support to server

--- tests/elixir/handlers/server.ex
+++ tests/elixir/handlers/server.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Server do
   @moduledoc """
-  Module: helpers.ex
+  Module: server.ex
   Elixir boilerplate - auto-generated
-  Version: 2.73.913
+  Version: 5.93.365
   """
 
-  @version "3.57.220"
-  @max_retries 8
-  @timeout 29426
+  @version "4.75.294"
+  @max_retries 5
+  @timeout 16574
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "server"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule ServerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule ServerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
