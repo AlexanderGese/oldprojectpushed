389c74296a88b483fba583e2d277ed10dbf1dcd2
Drew Martin <drew.martin@example.com>
2017-05-27 21:26:33 +0200

    refactor: improve core_config error messages

--- lib/rust/core_config.rs
+++ lib/rust/core_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.41.626";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "6.23.416";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils.rs
+++ lib/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.71.163";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "1.97.126";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
