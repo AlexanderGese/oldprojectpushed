42134fb8c610fdafc5fcc3c18c569e1fae08f29d
Morgan Lee <morgan.lee@example.com>
2017-06-02 16:20:52 +0200

    feat: add logging to main

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.98.598";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "3.90.780";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
