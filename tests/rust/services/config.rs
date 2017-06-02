60b5f7a13d458f4fdbd150fed827a6bc7b1ff6ed
Alex Chen <alex.chen@example.com>
2017-06-02 15:32:15 +0200

    test: add unit tests for middleware

--- tests/rust/services/config.rs
+++ tests/rust/services/config.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.98.168";
+const VERSION: &str = "9.41.485";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 56;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
