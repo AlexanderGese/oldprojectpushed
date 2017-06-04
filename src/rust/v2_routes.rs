da2871857234b48b4c23ebb8d98f201ccdaeb461
Morgan Lee <morgan.lee@example.com>
2017-06-04 19:40:15 +0200

    feat: implement v2_routes service layer

--- src/rust/v2_routes.rs
+++ src/rust/v2_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.62.69";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "6.83.856";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
