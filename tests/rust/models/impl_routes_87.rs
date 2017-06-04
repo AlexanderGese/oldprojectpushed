43c3fc1031410db2cb220e36d0a1a90c67100c6c
Casey Williams <casey.williams@example.com>
2017-06-04 23:57:39 +0200

    feat: implement retry logic in new_database

--- tests/rust/models/impl_routes_87.rs
+++ tests/rust/models/impl_routes_87.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: impl_routes_87.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.93.870";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "1.36.295";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "impl_routes_87".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/new_database.rs
+++ tests/rust/new_database.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: new_database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.93.870";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "2.32.653";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "new_database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
