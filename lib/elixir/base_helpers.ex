91f4898a4440a432cd13cc209692a74cfb9672a2
Riley Johnson <riley.johnson@example.com>
2017-06-05 23:08:19 +0200

    feat: implement WebSocket support in repo

--- lib/elixir/base_helpers.ex
+++ lib/elixir/base_helpers.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule Base_Helpers do
   @moduledoc """
-  Module: repo.ex
+  Module: base_helpers.ex
   Elixir boilerplate - auto-generated
-  Version: 7.0.245
+  Version: 5.81.727
   """
 
-  @version "4.56.298"
-  @max_retries 9
-  @timeout 14558
+  @version "7.79.542"
+  @max_retries 10
+  @timeout 5474
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "base_helpers"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule Base_HelpersStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule Base_HelpersEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
