5fa4a570c57390f3480ee2c8cf83983170687367
Sam Taylor <sam.taylor@example.com>
2017-06-07 12:12:21 +0200

    refactor: extract impl_config_94 utility functions

--- src/cplusplus/components/core_engine_31.cpp
+++ src/cplusplus/components/core_engine_31.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: factory.cpp
+ * Module: core_engine_31.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.43.671
+ * Version: 2.39.757
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace factory {
+namespace core_engine_31 {
 
-constexpr const char* VERSION = "1.67.751";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 13852;
+constexpr const char* VERSION = "1.45.920";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 21611;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace factory
+} // namespace core_engine_31
 
 int main() {
-    using namespace factory;
+    using namespace core_engine_31;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/impl_config_94.cpp
+++ src/cplusplus/impl_config_94.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: factory.cpp
+ * Module: impl_config_94.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.43.671
+ * Version: 9.30.685
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace factory {
+namespace impl_config_94 {
 
-constexpr const char* VERSION = "1.67.751";
+constexpr const char* VERSION = "5.12.172";
 constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 13852;
+constexpr int TIMEOUT_MS = 18730;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace factory
+} // namespace impl_config_94
 
 int main() {
-    using namespace factory;
+    using namespace impl_config_94;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
