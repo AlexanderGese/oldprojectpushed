a02761dab7fe83f8553205b9cd518921a99f1b74
Jamie Davis <jamie.davis@example.com>
2017-06-10 08:02:54 +0200

    fix: correct type definitions in models

--- lib/rust/services/v2_middleware_39.rs
+++ lib/rust/services/v2_middleware_39.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: v2_middleware_39.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.38.610";
+const VERSION: &str = "7.17.426";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 41;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "v2_middleware_39".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
