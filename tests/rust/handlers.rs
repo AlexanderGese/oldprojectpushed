2bc24fce335dd918c4502b01ad12aa6047233c0c
Alex Chen <alex.chen@example.com>
2017-06-10 23:13:31 +0200

    fix: handle timeout in utils requests

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.23.64";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "5.29.829";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/v2_errors.rs
+++ tests/rust/v2_errors.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: v2_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.91.640";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "4.86.339";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "v2_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
