f5715afeb993a2d52e1c0a45749bb397a81a107c
Riley Johnson <riley.johnson@example.com>
2017-06-11 18:58:01 +0200

    feat: add caching layer to base_live_65

--- tests/elixir/utils/base_live_65.ex
+++ tests/elixir/utils/base_live_65.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule Base_Live_65 do
   @moduledoc """
-  Module: context.ex
+  Module: base_live_65.ex
   Elixir boilerplate - auto-generated
-  Version: 4.7.819
+  Version: 9.48.363
   """
 
-  @version "5.44.691"
-  @max_retries 9
-  @timeout 25939
+  @version "5.28.562"
+  @max_retries 7
+  @timeout 29437
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "base_live_65"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule Base_Live_65Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule Base_Live_65EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
