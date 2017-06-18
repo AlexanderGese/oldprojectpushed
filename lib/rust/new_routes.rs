340a97b5302facd5af81821588cdcde9f17fdeac
Sam Taylor <sam.taylor@example.com>
2017-06-18 18:18:45 +0200

    feat: add pagination to new_routes

--- lib/rust/new_routes.rs
+++ lib/rust/new_routes.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: new_routes.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.92.136";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "9.96.722";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "new_routes".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
