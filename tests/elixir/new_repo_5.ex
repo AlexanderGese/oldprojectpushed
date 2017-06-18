0789cdb117c5ca81a0f5760ad89956989aad5617
Riley Johnson <riley.johnson@example.com>
2017-06-18 09:09:24 +0200

    chore: bump worker version

--- tests/elixir/new_repo_5.ex
+++ tests/elixir/new_repo_5.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule New_Repo_5 do
   @moduledoc """
-  Module: worker.ex
+  Module: new_repo_5.ex
   Elixir boilerplate - auto-generated
-  Version: 9.62.221
+  Version: 4.55.933
   """
 
-  @version "9.10.514"
-  @max_retries 8
-  @timeout 8121
+  @version "7.13.36"
+  @max_retries 5
+  @timeout 18743
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "new_repo_5"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule New_Repo_5Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule New_Repo_5EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
