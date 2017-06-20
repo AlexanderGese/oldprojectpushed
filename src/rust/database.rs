80e174560b6049c35cdedb88ae29e3751aaae021
Sam Taylor <sam.taylor@example.com>
2017-06-20 23:50:51 +0200

    chore: clean up core_main_53 imports

--- src/rust/database.rs
+++ src/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.28.101";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "7.5.233";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
