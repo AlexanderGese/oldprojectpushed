e640614f00a3285f4bdb712bc42b88b347744a20
Sam Taylor <sam.taylor@example.com>
2017-06-20 13:36:34 +0200

    fix: handle null check in handlers

--- src/rust/models.rs
+++ src/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.81.512";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "2.77.949";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils/errors.rs
+++ src/rust/utils/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.95.216";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "3.1.606";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
