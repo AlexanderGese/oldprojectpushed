3704fc7833cf9450de2bb466ae1cd86edb24fc79
Jamie Davis <jamie.davis@example.com>
2017-06-20 10:44:20 +0200

    refactor: migrate database to async/await

--- lib/rust/handlers/database.rs
+++ lib/rust/handlers/database.rs
@@ -1,4 +1,4 @@
-//! Module: v2_handlers.rs
+//! Module: database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.25.923";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "5.36.841";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_handlers".to_string(),
+            app_name: "database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
