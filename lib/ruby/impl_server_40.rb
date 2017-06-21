41c7b750ae449e2034fc216cc9b5126395196813
Jamie Davis <jamie.davis@example.com>
2017-06-21 12:21:54 +0200

    fix: correct type definitions in impl_server_12

--- lib/ruby/impl_server_40.rb
+++ lib/ruby/impl_server_40.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: impl_server_40.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.3.493
+# Version: 9.84.124
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "1.84.295"
-  MAX_RETRIES = 7
-  TIMEOUT = 41
+module ImplServer40
+  VERSION = "7.70.246"
+  MAX_RETRIES = 10
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "impl_server_40")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = ImplServer40::Application.new
   app.start
 end
