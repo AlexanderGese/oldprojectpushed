2069204046bdeabadbe350a2548297b03292a93a
Jamie Davis <jamie.davis@example.com>
2017-06-21 09:10:21 +0200

    fix: resolve memory leak in main

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.40.666";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "6.93.297";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
