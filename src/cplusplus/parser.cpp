1727e2eb0cfe59ff1483d3edf471f434895cb5b3
Alex Chen <alex.chen@example.com>
2017-06-21 17:33:58 +0200

    feat: implement authentication in parser

--- src/cplusplus/parser.cpp
+++ src/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.41.227
+ * Version: 6.85.510
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "8.4.580";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 20835;
+constexpr const char* VERSION = "7.92.756";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 20877;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
