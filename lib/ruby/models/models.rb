a0c6b3bb6d3809269fd06d5a20de37dffdd4a41c
Jamie Davis <jamie.davis@example.com>
2017-06-23 14:50:59 +0200

    chore: update models configuration

--- lib/ruby/models/models.rb
+++ lib/ruby/models/models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.32.790
+# Version: 2.77.81
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "9.33.919"
-  MAX_RETRIES = 7
-  TIMEOUT = 58
+module Models
+  VERSION = "8.85.990"
+  MAX_RETRIES = 5
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = Models::Application.new
   app.start
 end
