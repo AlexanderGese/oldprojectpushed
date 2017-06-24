c7ab2a838584485782ae352b50909a23205e136f
Jordan Smith <jordan.smith@example.com>
2017-06-24 10:54:20 +0200

    fix: patch security vulnerability in enhanced_middleware_58

--- lib/rust/enhanced_middleware_58.rs
+++ lib/rust/enhanced_middleware_58.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: enhanced_middleware_58.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.55.868";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "4.30.776";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "enhanced_middleware_58".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/helpers/new_errors_25.rs
+++ lib/rust/helpers/new_errors_25.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: new_errors_25.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.55.868";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "3.52.643";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "new_errors_25".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
