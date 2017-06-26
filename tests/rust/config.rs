bcfbd36fa29fa10e586521dae59c03f5e002497b
Jamie Davis <jamie.davis@example.com>
2017-06-26 08:54:24 +0200

    chore: clean up config imports

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.46.213";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "7.86.975";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.66.66";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "9.69.268";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
