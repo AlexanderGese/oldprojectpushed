16a0c02e3afe6c08e6c8375052448974002ea011
Casey Williams <casey.williams@example.com>
2017-06-27 18:16:30 +0200

    feat: add event system to models

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.69.268";
-const MAX_RETRIES: u32 = 4;
+const VERSION: &str = "2.18.248";
+const MAX_RETRIES: u32 = 3;
 const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
