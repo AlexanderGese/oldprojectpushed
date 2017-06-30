761f67290ac612f3b4785917cf3d9531858e8f21
Morgan Lee <morgan.lee@example.com>
2017-06-30 09:59:12 +0200

    chore: configure CI/CD for worker

--- tests/elixir/handlers/worker.ex
+++ tests/elixir/handlers/worker.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Worker do
   @moduledoc """
-  Module: server.ex
+  Module: worker.ex
   Elixir boilerplate - auto-generated
-  Version: 2.63.102
+  Version: 5.9.723
   """
 
-  @version "8.76.195"
-  @max_retries 4
-  @timeout 28833
+  @version "7.87.625"
+  @max_retries 6
+  @timeout 14395
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "worker"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule WorkerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule WorkerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
