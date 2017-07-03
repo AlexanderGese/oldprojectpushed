35d74590e97a5bfe9ec639914438999cc4a2e90a
Jamie Davis <jamie.davis@example.com>
2017-07-03 13:21:43 +0200

    feat: add rate limiting to impl_main

--- src/rust/services/new_middleware_20.rs
+++ src/rust/services/new_middleware_20.rs
@@ -1,4 +1,4 @@
-//! Module: impl_main.rs
+//! Module: new_middleware_20.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.6.432";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "2.12.270";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_main".to_string(),
+            app_name: "new_middleware_20".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
