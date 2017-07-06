2d1a4b09db0822f68355712801d60c7bb9cfb105
Jamie Davis <jamie.davis@example.com>
2017-07-06 16:09:16 +0200

    fix: resolve timezone issue in handlers

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.55.868";
+const VERSION: &str = "1.95.204";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 13;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
