bf7f9d937f3639729e78e8422a14bf30cbef3b3e
Riley Johnson <riley.johnson@example.com>
2017-07-06 14:31:48 +0200

    feat: add error handling to new_routes

--- tests/rust/core_errors.rs
+++ tests/rust/core_errors.rs
@@ -1,4 +1,4 @@
-//! Module: new_routes.rs
+//! Module: core_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.68.382";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "7.94.62";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_routes".to_string(),
+            app_name: "core_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
