033dbad5664996b39cd780a35e8447aab0069690
Alex Chen <alex.chen@example.com>
2017-07-08 18:51:17 +0200

    feat: add error handling to config

--- tests/rust/handlers/middleware.rs
+++ tests/rust/handlers/middleware.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: middleware.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.19.771";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "3.74.333";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "middleware".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
