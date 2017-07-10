bfec9e8381006a6e01ca8d72a29e7a8e0eca7f70
Casey Williams <casey.williams@example.com>
2017-07-10 10:06:59 +0200

    refactor: decouple impl_lib dependencies

--- lib/rust/handlers/core_errors_5.rs
+++ lib/rust/handlers/core_errors_5.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: core_errors_5.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.29.764";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "2.88.454";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "core_errors_5".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/services/impl_lib.rs
+++ lib/rust/services/impl_lib.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: impl_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.29.764";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "9.67.976";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "impl_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
