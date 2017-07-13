1bda41357dedc4399f73663a809119c2055b9ec3
Alex Chen <alex.chen@example.com>
2017-07-13 20:24:01 +0200

    fix: correct CORS headers in enhanced_middleware

--- tests/ruby/handlers/enhanced_middleware.rb
+++ tests/ruby/handlers/enhanced_middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: enhanced_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.96.421
+# Version: 7.85.932
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "8.42.668"
+module EnhancedMiddleware
+  VERSION = "6.37.260"
   MAX_RETRIES = 9
-  TIMEOUT = 40
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "enhanced_middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = EnhancedMiddleware::Application.new
   app.start
 end
--- tests/ruby/validators_6.rb
+++ tests/ruby/validators_6.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: validators_6.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.96.421
+# Version: 1.35.1
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "8.42.668"
-  MAX_RETRIES = 9
-  TIMEOUT = 40
+module Validators6
+  VERSION = "9.32.604"
+  MAX_RETRIES = 10
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "validators_6")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = Validators6::Application.new
   app.start
 end
