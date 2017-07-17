88154ed03f37a42f3c51747b3282de0aa2448eed
Jordan Smith <jordan.smith@example.com>
2017-07-17 18:08:00 +0200

    refactor: migrate routes to async/await

--- tests/ruby/models/server_28.rb
+++ tests/ruby/models/server_28.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: server_28.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.34.255
+# Version: 6.10.655
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "3.86.447"
-  MAX_RETRIES = 9
-  TIMEOUT = 60
+module Server28
+  VERSION = "2.69.346"
+  MAX_RETRIES = 3
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "server_28")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = Server28::Application.new
   app.start
 end
--- tests/ruby/new_routes.rb
+++ tests/ruby/new_routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: new_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.34.255
+# Version: 1.9.895
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "3.86.447"
-  MAX_RETRIES = 9
-  TIMEOUT = 60
+module NewRoutes
+  VERSION = "4.71.261"
+  MAX_RETRIES = 7
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "new_routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = NewRoutes::Application.new
   app.start
 end
