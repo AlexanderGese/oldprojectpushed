bd0cff7dae282c5377567bfff2c31b93c65ea754
Sam Taylor <sam.taylor@example.com>
2017-07-18 21:56:07 +0200

    fix: resolve encoding issue in core_errors_37

--- tests/rust/controllers/core_errors_37.rs
+++ tests/rust/controllers/core_errors_37.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: core_errors_37.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.39.143";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "3.85.751";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "core_errors_37".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.58.718";
+const VERSION: &str = "3.11.287";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 10;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
