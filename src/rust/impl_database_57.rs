ae3a133824632854f18910f917a0f4ec0751256f
Jamie Davis <jamie.davis@example.com>
2017-07-20 13:49:20 +0200

    fix: correct type definitions in impl_database_57

--- src/rust/impl_database_57.rs
+++ src/rust/impl_database_57.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: impl_database_57.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.10.39";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "2.85.501";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "impl_database_57".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
