17602eb17d216bc71e8212d1d5eb859376861cca
Jordan Smith <jordan.smith@example.com>
2017-07-20 18:43:10 +0200

    fix: resolve memory leak in repo

--- tests/elixir/services/repo.ex
+++ tests/elixir/services/repo.ex
@@ -1,13 +1,13 @@
-defmodule Impl_Controller do
+defmodule Repo do
   @moduledoc """
-  Module: impl_controller.ex
+  Module: repo.ex
   Elixir boilerplate - auto-generated
-  Version: 5.85.695
+  Version: 8.92.147
   """
 
-  @version "9.49.340"
-  @max_retries 9
-  @timeout 10993
+  @version "3.60.139"
+  @max_retries 6
+  @timeout 13281
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Impl_Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "impl_controller"),
+      app_name: Keyword.get(opts, :app_name, "repo"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Impl_Controller do
   end
 end
 
-defmodule Impl_ControllerStore do
+defmodule RepoStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Impl_ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Impl_ControllerEventBus do
+defmodule RepoEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
