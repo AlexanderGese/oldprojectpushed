22511d2dc0e19ec996908af2eb78e834467bddb2
Jordan Smith <jordan.smith@example.com>
2017-07-23 11:42:02 +0200

    fix: correct type definitions in models

--- lib/rust/components/config.rs
+++ lib/rust/components/config.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.1.827";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "8.15.989";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
