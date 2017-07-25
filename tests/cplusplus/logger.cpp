b40dc3ed6a936eabc520790582d901b88144e149
Jordan Smith <jordan.smith@example.com>
2017-07-25 14:50:26 +0200

    feat: implement authentication in main

--- tests/cplusplus/logger.cpp
+++ tests/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.62.580
+ * Version: 5.40.735
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "8.82.317";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 7335;
+constexpr const char* VERSION = "7.30.655";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 11271;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/main.cpp
+++ tests/cplusplus/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.10.19
+ * Version: 3.5.328
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "2.54.405";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 14090;
+constexpr const char* VERSION = "1.2.141";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 27836;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
