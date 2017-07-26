199289bb6b1d6d2626b4fd9e9578c60bacbea3b7
Casey Williams <casey.williams@example.com>
2017-07-26 11:59:00 +0200

    refactor: split utils into smaller modules

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.18.474";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "5.35.819";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
