cde66db31f09e7b2eb1d822e0915828a6ef82047
Jamie Davis <jamie.davis@example.com>
2017-07-26 08:55:49 +0200

    refactor: split utils_12 into smaller modules

--- tests/rust/utils/utils_12.rs
+++ tests/rust/utils/utils_12.rs
@@ -1,4 +1,4 @@
-//! Module: new_config.rs
+//! Module: utils_12.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.41.497";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "2.4.248";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_config".to_string(),
+            app_name: "utils_12".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
