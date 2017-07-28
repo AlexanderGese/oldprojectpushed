c32e3d046b95d1888d6ee6ac02ceb2e40459c5a5
Morgan Lee <morgan.lee@example.com>
2017-07-28 08:57:59 +0200

    fix: resolve memory leak in v2_repo_64

--- src/elixir/services/live.ex
+++ src/elixir/services/live.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule Live do
   @moduledoc """
-  Module: context.ex
+  Module: live.ex
   Elixir boilerplate - auto-generated
-  Version: 3.43.429
+  Version: 9.5.622
   """
 
-  @version "4.72.795"
-  @max_retries 5
-  @timeout 13157
+  @version "2.99.76"
+  @max_retries 8
+  @timeout 15114
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "live"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule LiveStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule LiveEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
