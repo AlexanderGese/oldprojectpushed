8cf816bb6be1a78698916b0357bbdcadd790bad6
Drew Martin <drew.martin@example.com>
2017-07-29 17:27:39 +0200

    fix: handle timeout in core_utils_41 requests

--- tests/rust/components/core_utils_41.rs
+++ tests/rust/components/core_utils_41.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: core_utils_41.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.43.201";
+const VERSION: &str = "4.65.77";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 10;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "core_utils_41".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
