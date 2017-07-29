95da56834f723781bd863a308a4208cfac036955
Morgan Lee <morgan.lee@example.com>
2017-07-29 15:05:12 +0200

    fix: handle large payloads in main

--- src/rust/controllers/main.rs
+++ src/rust/controllers/main.rs
@@ -1,4 +1,4 @@
-//! Module: enhanced_routes.rs
+//! Module: main.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.81.761";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "9.59.862";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "enhanced_routes".to_string(),
+            app_name: "main".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/utils/lib.rs
+++ src/rust/utils/lib.rs
@@ -1,4 +1,4 @@
-//! Module: enhanced_routes.rs
+//! Module: lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.81.761";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "6.81.965";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "enhanced_routes".to_string(),
+            app_name: "lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
