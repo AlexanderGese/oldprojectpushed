8b4843008eff665713634f0b99089d782584f71d
Jordan Smith <jordan.smith@example.com>
2017-07-30 23:43:30 +0200

    fix: patch security vulnerability in parser

--- lib/cplusplus/parser.cpp
+++ lib/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.66.199
+ * Version: 4.21.119
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "6.95.935";
+constexpr const char* VERSION = "1.66.30";
 constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 9492;
+constexpr int TIMEOUT_MS = 12702;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
