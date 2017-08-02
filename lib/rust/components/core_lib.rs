b7ce8569f021a6584f2d94dadd5d16c38337c750
Drew Martin <drew.martin@example.com>
2017-08-02 12:37:50 +0200

    fix: handle empty response in database

--- lib/rust/components/core_lib.rs
+++ lib/rust/components/core_lib.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: core_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.10.476";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "1.84.322";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "core_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/utils/routes.rs
+++ lib/rust/utils/routes.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: routes.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.10.476";
+const VERSION: &str = "3.58.205";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 6;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "routes".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
