bed30b487339ee06af2e8b1dbd389ab004efad41
Riley Johnson <riley.johnson@example.com>
2017-08-03 17:00:38 +0200

    refactor: split new_controller into smaller modules

--- src/elixir/new_controller.ex
+++ src/elixir/new_controller.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule New_Controller do
   @moduledoc """
-  Module: context.ex
+  Module: new_controller.ex
   Elixir boilerplate - auto-generated
-  Version: 5.48.827
+  Version: 3.33.316
   """
 
-  @version "6.91.481"
-  @max_retries 4
-  @timeout 17402
+  @version "6.85.649"
+  @max_retries 8
+  @timeout 21219
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "new_controller"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule New_ControllerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule New_ControllerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
