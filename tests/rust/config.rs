a0d2113831cb11ad97911cf3be2b892412da6a94
Alex Chen <alex.chen@example.com>
2017-08-04 09:44:53 +0200

    feat: implement routes service layer

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.85.623";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "1.76.607";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.39.143";
+const VERSION: &str = "8.35.396";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 34;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
