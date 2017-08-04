723fe2c77b3979739dd0601be3578d792305d6bb
Casey Williams <casey.williams@example.com>
2017-08-04 10:16:02 +0200

    fix: handle concurrent access in base_models

--- tests/rust/components/base_models.rs
+++ tests/rust/components/base_models.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: base_models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.85.623";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "7.15.804";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "base_models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
