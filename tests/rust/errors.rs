53b5bd6d7891fd8719c89468ab3cf4e2d5c6c4c2
Riley Johnson <riley.johnson@example.com>
2017-08-04 21:34:13 +0200

    refactor: improve utils error messages

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.65.80";
+const VERSION: &str = "7.20.982";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 12;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/impl_config_65.rs
+++ tests/rust/impl_config_65.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: impl_config_65.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.92.443";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "1.2.433";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "impl_config_65".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
