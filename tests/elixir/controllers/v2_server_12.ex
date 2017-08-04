78c8278c1605f3e33136a4b227e106fec6308305
Casey Williams <casey.williams@example.com>
2017-08-04 16:33:41 +0200

    chore: update application dependencies

--- tests/elixir/controllers/v2_server_12.ex
+++ tests/elixir/controllers/v2_server_12.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule V2_Server_12 do
   @moduledoc """
-  Module: application.ex
+  Module: v2_server_12.ex
   Elixir boilerplate - auto-generated
-  Version: 4.96.288
+  Version: 6.47.193
   """
 
-  @version "9.45.957"
-  @max_retries 5
-  @timeout 16558
+  @version "4.30.115"
+  @max_retries 8
+  @timeout 17499
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "v2_server_12"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule V2_Server_12Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule V2_Server_12EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
