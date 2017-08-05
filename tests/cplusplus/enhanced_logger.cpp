613237b0deab7210d80435b73b025b09d3685a01
Jordan Smith <jordan.smith@example.com>
2017-08-05 10:45:19 +0200

    refactor: migrate utils to async/await

--- tests/cplusplus/enhanced_logger.cpp
+++ tests/cplusplus/enhanced_logger.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: enhanced_logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.8.207
+ * Version: 9.93.995
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace enhanced_logger {
 
-constexpr const char* VERSION = "4.46.222";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 17903;
+constexpr const char* VERSION = "4.1.997";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 21142;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace enhanced_logger
 
 int main() {
-    using namespace utils;
+    using namespace enhanced_logger;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
