cc34b8d59f73632320d5d70bd9611ff58cdeae5b
Drew Martin <drew.martin@example.com>
2017-08-10 20:26:18 +0200

    fix: resolve timezone issue in new_routes_18

--- tests/rust/new_routes_18.rs
+++ tests/rust/new_routes_18.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.13.199";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "9.50.148";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
