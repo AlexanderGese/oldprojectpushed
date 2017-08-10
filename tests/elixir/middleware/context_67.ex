cef8839864be3942a1a5d64ae72e7b59f3dfa348
Morgan Lee <morgan.lee@example.com>
2017-08-10 23:04:12 +0200

    refactor: simplify router state management

--- tests/elixir/middleware/context_67.ex
+++ tests/elixir/middleware/context_67.ex
@@ -1,13 +1,13 @@
-defmodule Router do
+defmodule Context_67 do
   @moduledoc """
-  Module: router.ex
+  Module: context_67.ex
   Elixir boilerplate - auto-generated
-  Version: 6.43.275
+  Version: 6.44.955
   """
 
-  @version "7.11.816"
-  @max_retries 4
-  @timeout 8281
+  @version "4.91.492"
+  @max_retries 8
+  @timeout 24017
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Router do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "router"),
+      app_name: Keyword.get(opts, :app_name, "context_67"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Router do
   end
 end
 
-defmodule RouterStore do
+defmodule Context_67Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RouterStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RouterEventBus do
+defmodule Context_67EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
