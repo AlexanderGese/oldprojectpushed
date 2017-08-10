508f7e689a91ae908f1e2d94547614cc2518aa30
Casey Williams <casey.williams@example.com>
2017-08-10 12:32:07 +0200

    feat: add event system to parser

--- tests/cplusplus/parser.cpp
+++ tests/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.38.926
+ * Version: 9.38.756
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "6.49.498";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 28058;
+constexpr const char* VERSION = "3.81.946";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 7466;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
