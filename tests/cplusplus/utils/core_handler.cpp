a3d6921a3ffff0321328de5471b47ed25897e76d
Riley Johnson <riley.johnson@example.com>
2017-08-10 18:11:27 +0200

    feat: add logging to main

--- tests/cplusplus/utils/core_handler.cpp
+++ tests/cplusplus/utils/core_handler.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: main.cpp
+ * Module: core_handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.82.35
+ * Version: 8.52.408
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace main {
+namespace core_handler {
 
-constexpr const char* VERSION = "7.66.247";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 10603;
+constexpr const char* VERSION = "2.75.505";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 21707;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace main
+} // namespace core_handler
 
 int main() {
-    using namespace main;
+    using namespace core_handler;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
