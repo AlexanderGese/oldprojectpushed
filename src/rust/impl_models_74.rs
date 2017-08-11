f24722b1b576064b23fc1548013f35400276d99c
Morgan Lee <morgan.lee@example.com>
2017-08-11 21:05:37 +0200

    fix: handle null check in impl_models_74

--- src/rust/impl_models_74.rs
+++ src/rust/impl_models_74.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: impl_models_74.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.73.464";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "5.77.453";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "impl_models_74".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/middleware/lib_35.rs
+++ src/rust/middleware/lib_35.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: lib_35.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.73.464";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "7.65.148";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "lib_35".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
