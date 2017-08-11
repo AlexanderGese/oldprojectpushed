3a85ac2aee52e79dcc4fe09d735d8a7bd8839c9e
Jamie Davis <jamie.davis@example.com>
2017-08-11 17:56:54 +0200

    feat: add search functionality to config

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.76.607";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "5.21.782";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
