f862b9902faf58cd9f480646d736622cb2ba1f37
Drew Martin <drew.martin@example.com>
2017-08-11 17:44:07 +0200

    refactor: extract base_middleware utility functions

--- src/ruby/base_middleware.rb
+++ src/ruby/base_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: base_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.22.782
+# Version: 3.39.70
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseMiddleware
-  VERSION = "3.85.491"
-  MAX_RETRIES = 6
-  TIMEOUT = 36
+  VERSION = "1.73.346"
+  MAX_RETRIES = 9
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
--- src/ruby/base_routes.rb
+++ src/ruby/base_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: base_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.75.0
+# Version: 1.52.50
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseRoutes
-  VERSION = "1.3.395"
-  MAX_RETRIES = 9
-  TIMEOUT = 41
+  VERSION = "4.97.546"
+  MAX_RETRIES = 5
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
