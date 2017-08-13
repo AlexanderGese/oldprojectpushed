2881812732551a5a55ab12751433e110748a4c27
Sam Taylor <sam.taylor@example.com>
2017-08-13 16:12:47 +0200

    fix: handle large payloads in tasks

--- src/ruby/controllers_44.rb
+++ src/ruby/controllers_44.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: controllers_44.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.13.779
+# Version: 1.6.16
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "5.97.544"
+module Controllers44
+  VERSION = "3.86.753"
   MAX_RETRIES = 10
-  TIMEOUT = 19
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "controllers_44")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = Controllers44::Application.new
   app.start
 end
