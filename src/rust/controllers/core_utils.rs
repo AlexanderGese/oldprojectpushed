21a25b95c0efc6f4f0d3d9eb5651c991372ff5a7
Drew Martin <drew.martin@example.com>
2017-08-14 12:55:23 +0200

    chore: add linting rules for core_lib

--- src/rust/controllers/core_utils.rs
+++ src/rust/controllers/core_utils.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: core_utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.3.866";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "5.91.607";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "core_utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
