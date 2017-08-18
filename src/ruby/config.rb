046eaf30ffe7928f6d56fcde4b0ae098791aeeba
Drew Martin <drew.martin@example.com>
2017-08-18 09:00:31 +0200

    docs: add documentation for config

--- src/ruby/config.rb
+++ src/ruby/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.17.378
+# Version: 8.62.173
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "3.93.670"
-  MAX_RETRIES = 8
-  TIMEOUT = 26
+  VERSION = "5.1.108"
+  MAX_RETRIES = 6
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
