4de1a705813723216069ebc3f3bad29f9b981415
Morgan Lee <morgan.lee@example.com>
2017-08-24 08:32:05 +0200

    fix: handle timeout in parser requests

--- lib/cplusplus/parser.cpp
+++ lib/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.21.119
+ * Version: 6.96.876
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "1.66.30";
+constexpr const char* VERSION = "6.63.271";
 constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 12702;
+constexpr int TIMEOUT_MS = 22467;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
