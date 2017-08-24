28f335a7cb94a0f6658ff6213e360ca7928508f8
Alex Chen <alex.chen@example.com>
2017-08-24 20:12:34 +0200

    test: add unit tests for base_lib

--- lib/rust/base_database.rs
+++ lib/rust/base_database.rs
@@ -1,4 +1,4 @@
-//! Module: base_errors.rs
+//! Module: base_database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.44.49";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "7.95.191";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_errors".to_string(),
+            app_name: "base_database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
