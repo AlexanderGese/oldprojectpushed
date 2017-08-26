29d46e0e993ff9142b7dd4d0f844a147163a31d4
Jordan Smith <jordan.smith@example.com>
2017-08-26 17:30:30 +0200

    fix: resolve edge case in app handler

--- src/ruby/config.rb
+++ src/ruby/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.28.164
+# Version: 3.44.652
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "5.28.690"
-  MAX_RETRIES = 8
-  TIMEOUT = 21
+  VERSION = "3.67.154"
+  MAX_RETRIES = 4
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
