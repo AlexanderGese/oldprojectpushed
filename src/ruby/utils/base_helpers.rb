a14496df2b2f062d08c7d5f158fe87cc0930815e
Alex Chen <alex.chen@example.com>
2017-08-26 13:47:37 +0200

    refactor: simplify base_models state management

--- src/ruby/utils/base_helpers.rb
+++ src/ruby/utils/base_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: base_models.rb
+# Module: base_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.44.929
+# Version: 9.47.97
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module BaseModels
-  VERSION = "7.24.922"
-  MAX_RETRIES = 10
-  TIMEOUT = 60
+module BaseHelpers
+  VERSION = "3.38.501"
+  MAX_RETRIES = 9
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "base_models")
+      @app_name = opts.fetch(:app_name, "base_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module BaseModels
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = BaseModels::Application.new
+  app = BaseHelpers::Application.new
   app.start
 end
