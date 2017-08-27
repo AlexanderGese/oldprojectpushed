f72e0c9bae3dba0d65f377da8e317ca479fe10be
Jordan Smith <jordan.smith@example.com>
2017-08-27 14:48:29 +0200

    chore: bump routes version

--- tests/rust/components/utils.rs
+++ tests/rust/components/utils.rs
@@ -1,4 +1,4 @@
-//! Module: base_errors.rs
+//! Module: utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.38.853";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "1.70.848";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_errors".to_string(),
+            app_name: "utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/services/handlers.rs
+++ tests/rust/services/handlers.rs
@@ -1,4 +1,4 @@
-//! Module: base_errors.rs
+//! Module: handlers.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.38.853";
+const VERSION: &str = "5.81.764";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 31;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_errors".to_string(),
+            app_name: "handlers".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
