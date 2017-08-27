2503321aab003bf6a3b3b7cacde69fe9094c0838
Sam Taylor <sam.taylor@example.com>
2017-08-27 15:02:51 +0200

    refactor: improve config test coverage

--- lib/cplusplus/config.cpp
+++ lib/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.34.648
+ * Version: 3.56.837
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "2.94.700";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 14723;
+constexpr const char* VERSION = "8.3.49";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 24679;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/impl_config.cpp
+++ lib/cplusplus/impl_config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: impl_config.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.75.961
+ * Version: 7.67.962
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace impl_config {
 
-constexpr const char* VERSION = "6.82.290";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 24499;
+constexpr const char* VERSION = "7.12.984";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 16845;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
