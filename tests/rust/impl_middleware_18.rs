270565915ef458fcb35c21406d16597bfed89659
Jamie Davis <jamie.davis@example.com>
2017-08-29 20:09:55 +0200

    fix: correct CORS headers in models

--- tests/rust/impl_middleware_18.rs
+++ tests/rust/impl_middleware_18.rs
@@ -1,4 +1,4 @@
-//! Module: v2_routes.rs
+//! Module: impl_middleware_18.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.83.552";
+const VERSION: &str = "7.83.281";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 46;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_routes".to_string(),
+            app_name: "impl_middleware_18".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/utils/models.rs
+++ tests/rust/utils/models.rs
@@ -1,4 +1,4 @@
-//! Module: v2_routes.rs
+//! Module: models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.83.552";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "5.0.606";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_routes".to_string(),
+            app_name: "models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
