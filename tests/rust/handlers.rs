a348dc8352420f8184a80f5bfab9ab8779b772a1
Jamie Davis <jamie.davis@example.com>
2017-09-01 20:23:54 +0200

    refactor: migrate v2_utils to async/await

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.98.211";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "4.61.282";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
