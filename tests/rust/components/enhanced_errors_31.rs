09fa28a13c3073b1186d7ebe0ed868c3b57383df
Casey Williams <casey.williams@example.com>
2017-09-04 09:45:32 +0200

    test: add unit tests for errors

--- tests/rust/components/enhanced_errors_31.rs
+++ tests/rust/components/enhanced_errors_31.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: enhanced_errors_31.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.32.61";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "8.96.314";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "enhanced_errors_31".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.20.982";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "5.80.500";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
