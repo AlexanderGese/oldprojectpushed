60980d8c627feafabc3ad21cc26ecdafb56c0ff3
Alex Chen <alex.chen@example.com>
2017-09-14 09:27:59 +0200

    fix: handle concurrent access in core_routes_76

--- lib/rust/helpers/config_52.rs
+++ lib/rust/helpers/config_52.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: config_52.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.43.296";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "1.63.604";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "config_52".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
