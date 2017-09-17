f46488c9b4af3e872fa505298a4e9ed3e9fbd104
Drew Martin <drew.martin@example.com>
2017-09-17 16:12:43 +0200

    feat: add export functionality to v2_errors_15

--- lib/rust/components/database.rs
+++ lib/rust/components/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.47.341";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "1.78.345";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
