1db5526b543d30d7307c98621e07e506d2b32913
Morgan Lee <morgan.lee@example.com>
2017-09-25 22:39:59 +0200

    refactor: optimize routes performance

--- src/rust/controllers/routes.rs
+++ src/rust/controllers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.61.789";
+const VERSION: &str = "9.90.322";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 45;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
