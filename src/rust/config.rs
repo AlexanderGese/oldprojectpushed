4e3f12b30aa925b9d018f9d611322ff4136af1da
Drew Martin <drew.martin@example.com>
2017-09-26 16:10:23 +0200

    chore: update lib dependencies

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.38.916";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "7.75.226";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
