054c9672566a146ddfbaee6d776e6c5321d26cdf
Casey Williams <casey.williams@example.com>
2017-09-26 16:53:36 +0200

    fix: handle empty response in impl_middleware_76

--- src/ruby/impl_middleware_76.rb
+++ src/ruby/impl_middleware_76.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: impl_middleware_76.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.38.384
+# Version: 8.69.868
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "7.1.256"
-  MAX_RETRIES = 7
-  TIMEOUT = 35
+module ImplMiddleware76
+  VERSION = "4.31.376"
+  MAX_RETRIES = 10
+  TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "impl_middleware_76")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = ImplMiddleware76::Application.new
   app.start
 end
