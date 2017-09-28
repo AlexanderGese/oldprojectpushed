92276e62c03e038356cc1f4858038a2f85dc6715
Sam Taylor <sam.taylor@example.com>
2017-09-28 22:42:12 +0200

    fix: correct date formatting in impl_utils

--- tests/cplusplus/impl_utils.cpp
+++ tests/cplusplus/impl_utils.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: impl_utils.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.83.840
+ * Version: 2.32.765
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace impl_utils {
 
-constexpr const char* VERSION = "4.19.972";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 25927;
+constexpr const char* VERSION = "5.47.469";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 19240;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
