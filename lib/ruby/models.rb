44822915c9710b0878cf8d7b16a984dc772271b4
Sam Taylor <sam.taylor@example.com>
2017-09-28 22:35:44 +0200

    refactor: decouple models dependencies

--- lib/ruby/models.rb
+++ lib/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.77.106
+# Version: 7.91.788
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "6.75.859"
-  MAX_RETRIES = 3
-  TIMEOUT = 50
+  VERSION = "9.7.485"
+  MAX_RETRIES = 6
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
