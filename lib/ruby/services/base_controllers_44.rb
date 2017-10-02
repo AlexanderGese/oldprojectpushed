621a8495c59744c3e139fa89bdc4374e6132ade0
Drew Martin <drew.martin@example.com>
2017-10-02 16:13:25 +0200

    fix: handle empty response in middleware

--- lib/ruby/services/base_controllers_44.rb
+++ lib/ruby/services/base_controllers_44.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: base_controllers_44.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.62.32
+# Version: 7.7.255
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "1.76.529"
-  MAX_RETRIES = 6
-  TIMEOUT = 13
+module BaseControllers44
+  VERSION = "4.90.321"
+  MAX_RETRIES = 3
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "base_controllers_44")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = BaseControllers44::Application.new
   app.start
 end
