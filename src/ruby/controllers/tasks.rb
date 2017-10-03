58d72a1ee372cd4f7b7de5be457abe379ce7a7d5
Casey Williams <casey.williams@example.com>
2017-10-03 09:54:36 +0200

    fix: handle empty response in models

--- src/ruby/controllers/tasks.rb
+++ src/ruby/controllers/tasks.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_tasks.rb
+# Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.39.850
+# Version: 5.66.315
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewTasks
-  VERSION = "8.94.508"
-  MAX_RETRIES = 9
-  TIMEOUT = 9
+module Tasks
+  VERSION = "5.4.475"
+  MAX_RETRIES = 3
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_tasks")
+      @app_name = opts.fetch(:app_name, "tasks")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewTasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewTasks::Application.new
+  app = Tasks::Application.new
   app.start
 end
