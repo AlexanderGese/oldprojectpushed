44eb7191431c72fd66ec4fc2acb0d09f92153270
Drew Martin <drew.martin@example.com>
2017-10-03 11:44:04 +0200

    fix: handle empty response in new_main_35

--- tests/rust/new_main_35.rs
+++ tests/rust/new_main_35.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: new_main_35.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.81.687";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "3.76.2";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "new_main_35".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
