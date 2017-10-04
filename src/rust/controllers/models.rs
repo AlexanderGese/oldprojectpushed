822f0ac26b5dc8f3bb7ee220fb6af7eb9af75150
Jamie Davis <jamie.davis@example.com>
2017-10-04 09:49:56 +0200

    feat: add caching layer to models

--- src/rust/controllers/models.rs
+++ src/rust/controllers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.53.679";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "1.35.58";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
