17b928b411d079b7dcae4e5a9adbbaec660b7c1a
Alex Chen <alex.chen@example.com>
2017-10-05 17:47:20 +0200

    fix: resolve memory leak in new_schema_92

--- tests/elixir/new_schema_92.ex
+++ tests/elixir/new_schema_92.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule New_Schema_92 do
   @moduledoc """
-  Module: application.ex
+  Module: new_schema_92.ex
   Elixir boilerplate - auto-generated
-  Version: 6.5.201
+  Version: 9.3.296
   """
 
-  @version "9.47.791"
-  @max_retries 7
-  @timeout 15250
+  @version "8.3.354"
+  @max_retries 8
+  @timeout 18752
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "new_schema_92"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule New_Schema_92Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule New_Schema_92EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/schema_24.ex
+++ tests/elixir/schema_24.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule Schema_24 do
   @moduledoc """
-  Module: application.ex
+  Module: schema_24.ex
   Elixir boilerplate - auto-generated
-  Version: 6.5.201
+  Version: 9.35.759
   """
 
-  @version "9.47.791"
-  @max_retries 7
-  @timeout 15250
+  @version "3.62.459"
+  @max_retries 3
+  @timeout 15847
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "schema_24"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule Schema_24Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule Schema_24EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
