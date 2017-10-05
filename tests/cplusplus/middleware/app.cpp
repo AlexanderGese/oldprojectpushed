faa63e4890a89859ac1e5f2457ae73a52fa31547
Drew Martin <drew.martin@example.com>
2017-10-05 10:35:26 +0200

    feat: implement authentication in new_parser_25

--- tests/cplusplus/middleware/app.cpp
+++ tests/cplusplus/middleware/app.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: config.cpp
+ * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.74.935
+ * Version: 2.72.591
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace config {
+namespace app {
 
-constexpr const char* VERSION = "4.84.209";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 9592;
+constexpr const char* VERSION = "7.12.181";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 13596;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace config
+} // namespace app
 
 int main() {
-    using namespace config;
+    using namespace app;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
