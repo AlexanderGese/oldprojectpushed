b45ebbe8bfa7ba9f0e3713b32b2ab7088670c18b
Riley Johnson <riley.johnson@example.com>
2017-10-07 10:13:20 +0200

    fix: handle timeout in handler requests

--- tests/cplusplus/handler.cpp
+++ tests/cplusplus/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.44.218
+ * Version: 3.79.439
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "2.88.710";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 12129;
+constexpr const char* VERSION = "7.94.18";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 5771;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/utils/parser.cpp
+++ tests/cplusplus/utils/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.47.782
+ * Version: 7.28.167
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "3.48.760";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 14077;
+constexpr const char* VERSION = "8.23.705";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 22111;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
