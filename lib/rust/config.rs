1da9050c4f763d0510dd572e77bef26e505f7484
Jordan Smith <jordan.smith@example.com>
2017-10-09 10:55:47 +0200

    fix: resolve encoding issue in errors

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.82.443";
+const VERSION: &str = "4.10.305";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 51;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
