f5db2d88955446e9ab6ef1d23b9f1361408fb904
Jordan Smith <jordan.smith@example.com>
2017-10-10 15:11:41 +0200

    fix: handle large payloads in factory

--- src/cplusplus/factory.cpp
+++ src/cplusplus/factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.83.868
+ * Version: 1.97.397
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace factory {
 
-constexpr const char* VERSION = "1.0.971";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 22717;
+constexpr const char* VERSION = "5.52.528";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 13544;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
