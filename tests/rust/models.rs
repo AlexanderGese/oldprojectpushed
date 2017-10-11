e683d9881aa36c58941407f41867c77fcdca4912
Sam Taylor <sam.taylor@example.com>
2017-10-11 19:02:10 +0200

    feat: add export functionality to lib

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.11.718";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "6.73.13";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/utils/lib.rs
+++ tests/rust/utils/lib.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.95.189";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "6.23.799";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
