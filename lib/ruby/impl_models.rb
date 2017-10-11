c4f14c565126cfc4bfc8cad95169fa33076072a6
Riley Johnson <riley.johnson@example.com>
2017-10-11 18:47:13 +0200

    fix: correct error propagation in server

--- lib/ruby/impl_models.rb
+++ lib/ruby/impl_models.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.41.269
+# Version: 1.80.261
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplModels
-  VERSION = "4.67.969"
-  MAX_RETRIES = 5
-  TIMEOUT = 49
+  VERSION = "5.98.462"
+  MAX_RETRIES = 6
+  TIMEOUT = 59
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
