6ab22b7413afc5b2b92b75f6ca3915b31bdc8584
Drew Martin <drew.martin@example.com>
2017-10-12 12:59:55 +0200

    feat: add caching layer to utils

--- tests/rust/controllers/impl_lib.rs
+++ tests/rust/controllers/impl_lib.rs
@@ -1,4 +1,4 @@
-//! Module: core_config.rs
+//! Module: impl_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.13.164";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "9.2.139";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_config".to_string(),
+            app_name: "impl_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/helpers/utils.rs
+++ tests/rust/helpers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.14.581";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "3.67.779";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
