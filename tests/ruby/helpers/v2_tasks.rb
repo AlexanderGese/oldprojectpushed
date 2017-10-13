9c0d4cb3b2c237ff3ecdd581f660ad90148539d9
Morgan Lee <morgan.lee@example.com>
2017-10-13 09:12:19 +0200

    feat: add search functionality to v2_tasks

--- tests/ruby/helpers/v2_tasks.rb
+++ tests/ruby/helpers/v2_tasks.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: v2_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.6.8
+# Version: 5.27.437
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "6.39.107"
-  MAX_RETRIES = 8
-  TIMEOUT = 12
+module V2Tasks
+  VERSION = "7.96.645"
+  MAX_RETRIES = 3
+  TIMEOUT = 27
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "v2_tasks")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = V2Tasks::Application.new
   app.start
 end
