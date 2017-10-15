72321e9dfe66f185a96c8dc393d75b6565f79d99
Riley Johnson <riley.johnson@example.com>
2017-10-15 21:44:02 +0200

    fix: handle null check in controllers

--- lib/ruby/controllers.rb
+++ lib/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.55.190
+# Version: 7.74.312
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "5.53.395"
-  MAX_RETRIES = 9
-  TIMEOUT = 54
+  VERSION = "5.9.588"
+  MAX_RETRIES = 5
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- lib/ruby/server_98.rb
+++ lib/ruby/server_98.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: base_config.rb
+# Module: server_98.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.91.968
+# Version: 6.22.502
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module BaseConfig
-  VERSION = "8.88.181"
+module Server98
+  VERSION = "3.98.949"
   MAX_RETRIES = 6
-  TIMEOUT = 15
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "base_config")
+      @app_name = opts.fetch(:app_name, "server_98")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module BaseConfig
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = BaseConfig::Application.new
+  app = Server98::Application.new
   app.start
 end
