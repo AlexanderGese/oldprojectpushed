d27136a3d285b45c5a1afe0326e734f7c8fe7f3d
Casey Williams <casey.williams@example.com>
2017-10-15 13:07:35 +0200

    feat: add pagination to v2_middleware

--- lib/ruby/v2_middleware.rb
+++ lib/ruby/v2_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.2.101
+# Version: 7.73.940
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Middleware
-  VERSION = "9.6.738"
-  MAX_RETRIES = 8
-  TIMEOUT = 44
+  VERSION = "9.86.97"
+  MAX_RETRIES = 5
+  TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
