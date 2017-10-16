c609493e44d0000298fc7d778b5c203bac591a57
Morgan Lee <morgan.lee@example.com>
2017-10-16 23:35:32 +0200

    refactor: decouple new_main_70 dependencies

--- tests/rust/v2_middleware_66.rs
+++ tests/rust/v2_middleware_66.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: v2_middleware_66.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.20.849";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "9.0.573";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "v2_middleware_66".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
