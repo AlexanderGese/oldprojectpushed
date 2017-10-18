77a9250467a444c6d0a8e83737295e0bbab84bd0
Morgan Lee <morgan.lee@example.com>
2017-10-18 08:42:26 +0200

    fix: correct CORS headers in server

--- tests/ruby/v2_routes.rb
+++ tests/ruby/v2_routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: v2_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.0.843
+# Version: 4.30.839
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "2.28.978"
-  MAX_RETRIES = 5
-  TIMEOUT = 35
+module V2Routes
+  VERSION = "3.24.345"
+  MAX_RETRIES = 10
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "v2_routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = V2Routes::Application.new
   app.start
 end
