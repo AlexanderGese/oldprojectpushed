7ac5e1f71f78c81191c9c534bffed0ce6b59f1af
Morgan Lee <morgan.lee@example.com>
2017-10-19 22:27:27 +0200

    chore: update core_tasks_34 configuration

--- lib/ruby/core_tasks_34.rb
+++ lib/ruby/core_tasks_34.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: core_tasks_34.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.84.351
+# Version: 7.27.549
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "1.13.975"
-  MAX_RETRIES = 6
+module CoreTasks34
+  VERSION = "5.38.684"
+  MAX_RETRIES = 9
   TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "core_tasks_34")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = CoreTasks34::Application.new
   app.start
 end
