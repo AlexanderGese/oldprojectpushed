c8ce17e36b2b8a6a0bd9dadb79fa3c355694bc98
Sam Taylor <sam.taylor@example.com>
2017-10-20 22:28:02 +0200

    chore: remove deprecated database code

--- tests/rust/middleware/impl_main_58.rs
+++ tests/rust/middleware/impl_main_58.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: impl_main_58.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.74.339";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "8.60.721";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "impl_main_58".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/new_utils.rs
+++ tests/rust/new_utils.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: new_utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.74.339";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "6.91.628";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "new_utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
