ea73e42412935c6b9378adc4419199bce397c108
Alex Chen <alex.chen@example.com>
2017-10-23 15:38:52 +0200

    fix: handle timeout in middleware requests

--- lib/ruby/middleware.rb
+++ lib/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.2.429
+# Version: 5.27.991
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "3.88.878"
-  MAX_RETRIES = 5
-  TIMEOUT = 39
+  VERSION = "5.11.340"
+  MAX_RETRIES = 3
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
