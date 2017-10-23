31e96b4c63f1a4acd5be1b45beea2d11618254a6
Jordan Smith <jordan.smith@example.com>
2017-10-23 09:46:47 +0200

    fix: correct CORS headers in lib

--- src/rust/database.rs
+++ src/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.70.373";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "6.12.506";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/routes.rs
+++ src/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.96.645";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "2.33.19";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
