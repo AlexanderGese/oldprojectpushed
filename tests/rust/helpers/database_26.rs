e39cbdbc250ae0d09e451e8f67c8ab0bce941207
Casey Williams <casey.williams@example.com>
2017-10-23 08:09:28 +0200

    fix: handle null check in database_26

--- tests/rust/helpers/database_26.rs
+++ tests/rust/helpers/database_26.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: database_26.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.41.811";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "4.36.895";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "database_26".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
