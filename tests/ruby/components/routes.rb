48c8424f0bfcd60bb0e1c9a948755a4eaa3d4936
Riley Johnson <riley.johnson@example.com>
2017-10-27 20:13:22 +0200

    fix: handle concurrent access in validators_75

--- tests/ruby/components/routes.rb
+++ tests/ruby/components/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.49.359
+# Version: 7.36.948
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "3.6.221"
-  MAX_RETRIES = 6
-  TIMEOUT = 22
+module Routes
+  VERSION = "6.44.614"
+  MAX_RETRIES = 9
+  TIMEOUT = 42
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = Routes::Application.new
   app.start
 end
--- tests/ruby/validators_75.rb
+++ tests/ruby/validators_75.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: validators_75.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.49.359
+# Version: 8.8.436
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "3.6.221"
-  MAX_RETRIES = 6
-  TIMEOUT = 22
+module Validators75
+  VERSION = "8.68.180"
+  MAX_RETRIES = 5
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "validators_75")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = Validators75::Application.new
   app.start
 end
