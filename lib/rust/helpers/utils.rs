afd2661bf195572e876d7d96fde9f237422ebb1b
Morgan Lee <morgan.lee@example.com>
2017-10-28 08:31:19 +0200

    fix: resolve race condition in new_database_48

--- lib/rust/helpers/utils.rs
+++ lib/rust/helpers/utils.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.49.758";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "3.65.1";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/impl_utils.rs
+++ lib/rust/impl_utils.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: impl_utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.49.758";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "5.32.382";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "impl_utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
