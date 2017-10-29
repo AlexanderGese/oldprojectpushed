a7f327297f74921b1a0c29420ad330b4f6d11c5d
Sam Taylor <sam.taylor@example.com>
2017-10-29 08:05:44 +0100

    fix: resolve race condition in enhanced_repo

--- lib/elixir/middleware/enhanced_repo.ex
+++ lib/elixir/middleware/enhanced_repo.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule Enhanced_Repo do
   @moduledoc """
-  Module: repo.ex
+  Module: enhanced_repo.ex
   Elixir boilerplate - auto-generated
-  Version: 3.64.373
+  Version: 4.7.556
   """
 
-  @version "4.34.838"
-  @max_retries 4
-  @timeout 11413
+  @version "4.35.580"
+  @max_retries 7
+  @timeout 10896
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_repo"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule Enhanced_RepoStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule Enhanced_RepoEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
