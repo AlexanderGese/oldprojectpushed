41417a5a84f68167c5dbe4be7b9bc8e1c6dc0f61
Morgan Lee <morgan.lee@example.com>
2017-10-29 23:13:17 +0100

    refactor: split middleware into smaller modules

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.93.879";
+const VERSION: &str = "8.9.468";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 18;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/handlers/core_utils.rs
+++ lib/rust/handlers/core_utils.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: core_utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.41.403";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "1.1.859";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "core_utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
