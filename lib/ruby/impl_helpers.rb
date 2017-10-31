662a398e0117d8e69dfb63634e5c832f15066de6
Alex Chen <alex.chen@example.com>
2017-10-31 09:35:04 +0100

    chore: bump impl_helpers version

--- lib/ruby/impl_helpers.rb
+++ lib/ruby/impl_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.77.534
+# Version: 6.61.480
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplHelpers
-  VERSION = "8.62.527"
-  MAX_RETRIES = 5
-  TIMEOUT = 36
+  VERSION = "4.10.611"
+  MAX_RETRIES = 3
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplHelpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
