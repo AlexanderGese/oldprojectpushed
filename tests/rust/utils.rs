65f210026368e3ebac788bf9d3a58b767b818747
Morgan Lee <morgan.lee@example.com>
2017-11-04 17:49:02 +0100

    feat: add error handling to utils

--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.49.632";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "7.1.749";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
