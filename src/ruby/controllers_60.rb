e0cf967ff5e28cd63ab9306742b5738275879398
Sam Taylor <sam.taylor@example.com>
2017-11-08 18:50:34 +0100

    feat: add caching layer to validators

--- src/ruby/controllers_60.rb
+++ src/ruby/controllers_60.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: controllers_60.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.17.996
+# Version: 4.18.313
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "9.9.437"
-  MAX_RETRIES = 7
-  TIMEOUT = 12
+module Controllers60
+  VERSION = "1.90.719"
+  MAX_RETRIES = 5
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "controllers_60")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = Controllers60::Application.new
   app.start
 end
--- src/ruby/validators.rb
+++ src/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.97.421
+# Version: 4.0.801
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "1.13.199"
-  MAX_RETRIES = 8
-  TIMEOUT = 29
+  VERSION = "8.70.715"
+  MAX_RETRIES = 7
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
