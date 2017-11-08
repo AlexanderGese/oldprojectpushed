680ef41845e0218383b115f7d06cc043eab52dd7
Jamie Davis <jamie.davis@example.com>
2017-11-08 17:27:04 +0100

    feat: add error handling to schema_46

--- lib/elixir/base_application_74.ex
+++ lib/elixir/base_application_74.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Base_Application_74 do
   @moduledoc """
-  Module: controller.ex
+  Module: base_application_74.ex
   Elixir boilerplate - auto-generated
-  Version: 9.99.32
+  Version: 7.2.522
   """
 
-  @version "5.31.893"
-  @max_retries 10
-  @timeout 16926
+  @version "6.12.233"
+  @max_retries 9
+  @timeout 12912
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "base_application_74"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Base_Application_74Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Base_Application_74EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
