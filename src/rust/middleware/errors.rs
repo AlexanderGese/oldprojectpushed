b280a3d688cc7b40806238efca39b7bb3dc8075a
Riley Johnson <riley.johnson@example.com>
2017-11-13 22:31:59 +0100

    refactor: improve utils error messages

--- src/rust/middleware/errors.rs
+++ src/rust/middleware/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.9.918";
+const VERSION: &str = "5.48.120";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 50;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
