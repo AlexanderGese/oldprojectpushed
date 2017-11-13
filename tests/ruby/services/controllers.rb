ae7d92d95af4d8657f0ff23ea916c736c90a21a7
Riley Johnson <riley.johnson@example.com>
2017-11-13 09:29:30 +0100

    chore: update helpers configuration

--- tests/ruby/services/controllers.rb
+++ tests/ruby/services/controllers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.59.452
+# Version: 6.40.847
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "2.35.741"
-  MAX_RETRIES = 3
-  TIMEOUT = 14
+module Controllers
+  VERSION = "4.47.740"
+  MAX_RETRIES = 9
+  TIMEOUT = 27
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "controllers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = Controllers::Application.new
   app.start
 end
