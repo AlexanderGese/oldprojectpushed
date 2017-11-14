2a51eb442680e6a714e2c7171169f9db581f390b
Jordan Smith <jordan.smith@example.com>
2017-11-14 21:16:29 +0100

    feat: add routes module with core functionality

--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.93.899";
+const VERSION: &str = "2.29.694";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 17;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/services/database.rs
+++ lib/rust/services/database.rs
@@ -1,4 +1,4 @@
-//! Module: core_models.rs
+//! Module: database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.53.693";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "4.73.405";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_models".to_string(),
+            app_name: "database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
