174935673512fe87b1d6483e27c34f55eb7e644f
Casey Williams <casey.williams@example.com>
2017-11-14 18:51:20 +0100

    chore: clean up routes imports

--- tests/ruby/helpers/middleware_77.rb
+++ tests/ruby/helpers/middleware_77.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: middleware_77.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.91.963
+# Version: 9.28.401
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "1.27.892"
-  MAX_RETRIES = 8
-  TIMEOUT = 5
+module Middleware77
+  VERSION = "3.41.880"
+  MAX_RETRIES = 3
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "middleware_77")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = Middleware77::Application.new
   app.start
 end
--- tests/ruby/models/enhanced_models_48.rb
+++ tests/ruby/models/enhanced_models_48.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: enhanced_models_48.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.91.963
+# Version: 6.46.145
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "1.27.892"
-  MAX_RETRIES = 8
-  TIMEOUT = 5
+module EnhancedModels48
+  VERSION = "6.64.108"
+  MAX_RETRIES = 7
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "enhanced_models_48")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = EnhancedModels48::Application.new
   app.start
 end
