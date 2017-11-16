a61d3cfb29e4dd0cc3ff37ab1164dc309953e7df
Casey Williams <casey.williams@example.com>
2017-11-16 14:22:43 +0100

    chore: remove deprecated v2_errors_23 code

--- src/rust/handlers/main.rs
+++ src/rust/handlers/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.36.728";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "7.58.39";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/v2_errors_23.rs
+++ src/rust/v2_errors_23.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: v2_errors_23.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.64.237";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "6.3.645";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "v2_errors_23".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
