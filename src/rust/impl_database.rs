f12c9648aa17d676a2d8b5b32752ff4c932b7df0
Jordan Smith <jordan.smith@example.com>
2017-11-24 09:00:24 +0100

    feat: add error handling to new_middleware

--- src/rust/impl_database.rs
+++ src/rust/impl_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.67.0";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "2.99.507";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
