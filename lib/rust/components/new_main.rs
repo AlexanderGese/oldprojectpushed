34d6a6499acca59b5cb2eb867e45248a58ee0469
Alex Chen <alex.chen@example.com>
2017-11-24 09:58:55 +0100

    chore: bump handlers version

--- lib/rust/components/new_main.rs
+++ lib/rust/components/new_main.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: new_main.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.0.223";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "6.47.404";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "new_main".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
