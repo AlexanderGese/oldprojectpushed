b4beb4e19675693819a7cbc10c27b5a9748f4f10
Drew Martin <drew.martin@example.com>
2017-11-24 16:56:56 +0100

    chore: update routes configuration

--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.30.477";
-const MAX_RETRIES: u32 = 10;
+const VERSION: &str = "9.51.744";
+const MAX_RETRIES: u32 = 3;
 const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
