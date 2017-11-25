a88a1927ff1f1396400b27ce7372f9dbe7ab767b
Morgan Lee <morgan.lee@example.com>
2017-11-25 13:16:03 +0100

    feat: add validation to v2_validators inputs

--- lib/ruby/v2_validators.rb
+++ lib/ruby/v2_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.81.501
+# Version: 1.72.826
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Validators
-  VERSION = "8.72.751"
-  MAX_RETRIES = 9
-  TIMEOUT = 15
+  VERSION = "6.5.660"
+  MAX_RETRIES = 5
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Validators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
