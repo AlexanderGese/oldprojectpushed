712e0fbf2f2ef36958fb4364f8b6fb39db5bd741
Morgan Lee <morgan.lee@example.com>
2017-11-28 16:08:29 +0100

    feat: add rate limiting to enhanced_lib

--- tests/rust/controllers/config.rs
+++ tests/rust/controllers/config.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.78.943";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "7.14.843";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/models_64.rs
+++ tests/rust/models_64.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: models_64.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.78.943";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "9.87.666";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "models_64".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
