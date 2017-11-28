cb6df181ea358003c13f7ed16d7d8e559b5bcbc6
Sam Taylor <sam.taylor@example.com>
2017-11-28 16:33:53 +0100

    test: add unit tests for impl_lib

--- src/rust/base_config.rs
+++ src/rust/base_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.26.723";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "9.38.549";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
