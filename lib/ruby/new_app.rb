3d2528fb240f9c69a0edc09929380c1b4fd23806
Sam Taylor <sam.taylor@example.com>
2017-11-29 19:53:33 +0100

    feat: add caching layer to tasks

--- lib/ruby/new_app.rb
+++ lib/ruby/new_app.rb
@@ -2,7 +2,7 @@
 
 # Module: new_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.36.689
+# Version: 2.35.4
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module NewApp
-  VERSION = "6.76.294"
-  MAX_RETRIES = 6
+  VERSION = "7.68.401"
+  MAX_RETRIES = 3
   TIMEOUT = 43
 
   class Config
@@ -25,7 +25,7 @@ module NewApp
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
