5ff591c78c6cad8610860ad12b84a7f4eea1c405
Sam Taylor <sam.taylor@example.com>
2017-12-01 08:52:14 +0100

    feat: implement retry logic in core_validators

--- src/ruby/core_validators.rb
+++ src/ruby/core_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: core_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.47.901
+# Version: 5.25.353
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreValidators
-  VERSION = "1.78.854"
-  MAX_RETRIES = 8
-  TIMEOUT = 47
+  VERSION = "1.20.89"
+  MAX_RETRIES = 7
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
