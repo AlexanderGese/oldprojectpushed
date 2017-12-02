2235baec1c0a63c77803bcd754f38264ef7136f4
Alex Chen <alex.chen@example.com>
2017-12-02 22:08:00 +0100

    feat: add batch processing to lib

--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.51.744";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "4.99.378";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
