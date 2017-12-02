726ca60f5b9509ad38d6f04a2cd5381e088e2a38
Drew Martin <drew.martin@example.com>
2017-12-02 16:32:27 +0100

    chore: remove deprecated core_utils code

--- src/rust/core_utils.rs
+++ src/rust/core_utils.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: core_utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.23.432";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "8.99.677";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "core_utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
