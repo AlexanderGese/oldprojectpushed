c6c629e665a16964a7b794dd36d78acd0c83567a
Sam Taylor <sam.taylor@example.com>
2017-12-03 13:08:49 +0100

    feat: add event system to utils

--- tests/rust/utils/handlers.rs
+++ tests/rust/utils/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.51.883";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "4.86.353";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
