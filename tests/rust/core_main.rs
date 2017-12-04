aa0cb1d9a73e15779e5fe60d6fe2500018b0fa8f
Jordan Smith <jordan.smith@example.com>
2017-12-04 09:15:25 +0100

    fix: resolve race condition in core_main

--- tests/rust/core_main.rs
+++ tests/rust/core_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.89.446";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "4.54.295";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
