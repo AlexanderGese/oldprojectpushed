ea703d8df6e9ab1e2939fe77582e2c66d6b64025
Sam Taylor <sam.taylor@example.com>
2017-12-07 12:48:01 +0100

    chore: clean up core_routes_90 imports

--- src/rust/controllers/core_routes_90.rs
+++ src/rust/controllers/core_routes_90.rs
@@ -1,4 +1,4 @@
-//! Module: impl_handlers_37.rs
+//! Module: core_routes_90.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.42.562";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "1.16.785";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_handlers_37".to_string(),
+            app_name: "core_routes_90".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.87.698";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "3.71.212";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
