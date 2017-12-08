bfd7a3f5e369261850ed48f3217d0d5631f05d36
Jordan Smith <jordan.smith@example.com>
2017-12-08 08:27:19 +0100

    fix: patch security vulnerability in enhanced_utils

--- tests/cplusplus/engine_27.cpp
+++ tests/cplusplus/engine_27.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: enhanced_utils.cpp
+ * Module: engine_27.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.20.95
+ * Version: 9.66.994
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace enhanced_utils {
+namespace engine_27 {
 
-constexpr const char* VERSION = "6.94.194";
+constexpr const char* VERSION = "9.78.547";
 constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 8462;
+constexpr int TIMEOUT_MS = 8400;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace enhanced_utils
+} // namespace engine_27
 
 int main() {
-    using namespace enhanced_utils;
+    using namespace engine_27;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
