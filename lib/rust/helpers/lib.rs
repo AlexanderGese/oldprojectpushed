dc53dfeae908b2d56f6fa7d922f189ff37e61c63
Drew Martin <drew.martin@example.com>
2017-12-11 17:19:10 +0100

    chore: add linting rules for models_53

--- lib/rust/helpers/lib.rs
+++ lib/rust/helpers/lib.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.51.147";
+const VERSION: &str = "3.23.826";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 17;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/utils/models_53.rs
+++ lib/rust/utils/models_53.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: models_53.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.51.147";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "9.94.325";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "models_53".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
