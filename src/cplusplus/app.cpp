8362aa185b4c80fcebb3d29dfc0ef5996ddbdbe4
Sam Taylor <sam.taylor@example.com>
2017-12-11 20:30:06 +0100

    fix: resolve timezone issue in app

--- src/cplusplus/app.cpp
+++ src/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.37.659
+ * Version: 9.31.826
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "2.49.766";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 12536;
+constexpr const char* VERSION = "6.2.6";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 16303;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
