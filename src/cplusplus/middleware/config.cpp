7cacb8a42fba3cfd283cf370f255c7fbaa7a9d30
Jordan Smith <jordan.smith@example.com>
2017-12-12 18:14:29 +0100

    refactor: decouple config dependencies

--- src/cplusplus/middleware/config.cpp
+++ src/cplusplus/middleware/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.96.880
+ * Version: 9.14.831
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "5.84.555";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 27125;
+constexpr const char* VERSION = "5.0.979";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 24779;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
