00737734dcee9ddd25583d188abfdc3140f18b4c
Drew Martin <drew.martin@example.com>
2017-12-13 19:52:27 +0100

    fix: handle large payloads in config_64

--- lib/rust/services/config_64.rs
+++ lib/rust/services/config_64.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: config_64.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.64.381";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "3.2.772";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "config_64".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
