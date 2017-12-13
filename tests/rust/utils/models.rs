58162131785a24a7df72db3cb494f4a7bd9579ed
Casey Williams <casey.williams@example.com>
2017-12-13 10:42:13 +0100

    fix: patch security vulnerability in models

--- tests/rust/utils/models.rs
+++ tests/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.0.606";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "5.47.488";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
