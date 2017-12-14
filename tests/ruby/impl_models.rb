024ad3dba9a243ee43cae51205157c074588f2d5
Casey Williams <casey.williams@example.com>
2017-12-14 12:09:32 +0100

    feat: implement authentication in controllers

--- tests/ruby/impl_models.rb
+++ tests/ruby/impl_models.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.48.109
+# Version: 8.85.678
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplModels
-  VERSION = "5.66.453"
-  MAX_RETRIES = 3
-  TIMEOUT = 50
+  VERSION = "2.8.99"
+  MAX_RETRIES = 7
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
