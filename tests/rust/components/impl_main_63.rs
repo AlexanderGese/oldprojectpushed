69c838ba21da663f7792bf0458cba797b300f049
Sam Taylor <sam.taylor@example.com>
2017-12-16 17:27:53 +0100

    fix: correct CORS headers in config

--- tests/rust/components/impl_main_63.rs
+++ tests/rust/components/impl_main_63.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: impl_main_63.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.83.289";
-const MAX_RETRIES: u32 = 10;
+const VERSION: &str = "8.39.866";
+const MAX_RETRIES: u32 = 6;
 const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "impl_main_63".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
