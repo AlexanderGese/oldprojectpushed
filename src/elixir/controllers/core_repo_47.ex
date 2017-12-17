1aaae9de017720a70aac87e78fc4076e3eb7753f
Sam Taylor <sam.taylor@example.com>
2017-12-17 20:31:48 +0100

    refactor: migrate context to async/await

--- src/elixir/controllers/core_repo_47.ex
+++ src/elixir/controllers/core_repo_47.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule Core_Repo_47 do
   @moduledoc """
-  Module: context.ex
+  Module: core_repo_47.ex
   Elixir boilerplate - auto-generated
-  Version: 9.39.208
+  Version: 1.79.808
   """
 
-  @version "9.24.651"
-  @max_retries 4
-  @timeout 14363
+  @version "8.30.652"
+  @max_retries 7
+  @timeout 29116
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "core_repo_47"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule Core_Repo_47Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule Core_Repo_47EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
