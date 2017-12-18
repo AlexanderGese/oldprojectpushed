f0560fae914022e20203e5b047f32947661d9106
Morgan Lee <morgan.lee@example.com>
2017-12-18 11:27:44 +0100

    fix: patch security vulnerability in tasks

--- src/ruby/v2_models.rb
+++ src/ruby/v2_models.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.45.679
+# Version: 1.61.351
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Models
-  VERSION = "8.97.215"
-  MAX_RETRIES = 4
-  TIMEOUT = 19
+  VERSION = "3.99.390"
+  MAX_RETRIES = 8
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
