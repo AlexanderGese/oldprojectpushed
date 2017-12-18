4b02dd854fc64eed96441e11025cc660f97f9718
Casey Williams <casey.williams@example.com>
2017-12-18 16:02:12 +0100

    fix: handle large payloads in base_tasks_1

--- src/ruby/base_tasks_1.rb
+++ src/ruby/base_tasks_1.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: base_tasks_1.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.77.713
+# Version: 4.2.253
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "2.90.824"
-  MAX_RETRIES = 10
-  TIMEOUT = 58
+module BaseTasks1
+  VERSION = "8.84.463"
+  MAX_RETRIES = 4
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "base_tasks_1")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = BaseTasks1::Application.new
   app.start
 end
