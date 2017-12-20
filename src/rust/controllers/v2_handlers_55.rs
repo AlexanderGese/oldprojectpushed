884b3ea76c6fc711201b7abf6018f18657ee3130
Alex Chen <alex.chen@example.com>
2017-12-20 10:13:05 +0100

    refactor: improve handlers error messages

--- src/rust/controllers/v2_handlers_55.rs
+++ src/rust/controllers/v2_handlers_55.rs
@@ -1,4 +1,4 @@
-//! Module: base_handlers.rs
+//! Module: v2_handlers_55.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.29.15";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "4.63.975";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_handlers".to_string(),
+            app_name: "v2_handlers_55".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
