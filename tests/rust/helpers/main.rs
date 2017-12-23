3589ffae36d3cbbd4d7b73f81b2ce8f9d73ce5ad
Riley Johnson <riley.johnson@example.com>
2017-12-23 17:57:38 +0100

    feat: add caching layer to main

--- tests/rust/helpers/main.rs
+++ tests/rust/helpers/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.13.213";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "7.44.582";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
