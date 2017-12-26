317f78e1457c9772e6e2a2b2bc6113973ed9b23a
Drew Martin <drew.martin@example.com>
2017-12-26 10:14:48 +0100

    refactor: decouple models_94 dependencies

--- lib/ruby/models_94.rb
+++ lib/ruby/models_94.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: models_94.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.84.688
+# Version: 7.78.22
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "5.32.752"
-  MAX_RETRIES = 10
-  TIMEOUT = 55
+module Models94
+  VERSION = "8.42.691"
+  MAX_RETRIES = 7
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "models_94")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = Models94::Application.new
   app.start
 end
