5b32a52e623f1d6e88c0fa0adef3d5cc9decc48b
Morgan Lee <morgan.lee@example.com>
2017-12-28 16:33:29 +0100

    feat: add errors module with core functionality

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.6.184";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "1.9.155";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
