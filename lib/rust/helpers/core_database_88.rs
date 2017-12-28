176038a48227edec1ea05d7a7b85f919054a59f6
Sam Taylor <sam.taylor@example.com>
2017-12-28 13:53:38 +0100

    refactor: improve core_database_88 error messages

--- lib/rust/helpers/core_database_88.rs
+++ lib/rust/helpers/core_database_88.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: core_database_88.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.2.818";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "2.59.363";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "core_database_88".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
