dc0d0159cd76bc6a5db801362e66cb8e3480dbc5
Riley Johnson <riley.johnson@example.com>
2017-12-31 11:58:59 +0100

    feat: add middleware support to config

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.83.289";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "1.84.31";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
