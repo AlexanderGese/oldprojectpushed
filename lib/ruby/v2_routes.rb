cba4a2b4f2a257c3298356e92e665b7ba1de27fc
Morgan Lee <morgan.lee@example.com>
2017-12-31 14:23:53 +0100

    feat: implement authentication in v2_routes

--- lib/ruby/v2_routes.rb
+++ lib/ruby/v2_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.6.242
+# Version: 4.68.259
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Routes
-  VERSION = "1.40.854"
-  MAX_RETRIES = 7
-  TIMEOUT = 50
+  VERSION = "6.47.61"
+  MAX_RETRIES = 4
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
