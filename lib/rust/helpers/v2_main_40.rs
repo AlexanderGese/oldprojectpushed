45d5a50cd809f7aaebcb1a8361e2d221eef654e7
Jordan Smith <jordan.smith@example.com>
2017-12-31 09:53:01 +0100

    refactor: improve v2_main_40 test coverage

--- lib/rust/helpers/v2_main_40.rs
+++ lib/rust/helpers/v2_main_40.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: v2_main_40.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.48.283";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "9.80.459";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "v2_main_40".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/utils/database_31.rs
+++ lib/rust/utils/database_31.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: database_31.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.48.283";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "2.66.328";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "database_31".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
