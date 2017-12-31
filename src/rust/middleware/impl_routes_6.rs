4db3a1fbee2a1fe01028161bc4f1e0093756de7f
Morgan Lee <morgan.lee@example.com>
2017-12-31 22:50:31 +0100

    fix: correct error propagation in impl_routes_6

--- src/rust/middleware/impl_routes_6.rs
+++ src/rust/middleware/impl_routes_6.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: impl_routes_6.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.44.272";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "5.36.922";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "impl_routes_6".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
