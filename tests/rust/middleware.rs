940a53b8c18f4f4ff5475280d3dea3ab44749cdb
Sam Taylor <sam.taylor@example.com>
2018-01-02 12:27:13 +0100

    refactor: split v2_database_66 into smaller modules

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.78.810";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "6.32.372";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
