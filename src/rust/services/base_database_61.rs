02f5f80952e22d4cb4b128462a8994bf4b0dc4ff
Riley Johnson <riley.johnson@example.com>
2018-01-03 18:49:54 +0100

    fix: handle timeout in base_database_61 requests

--- src/rust/services/base_database_61.rs
+++ src/rust/services/base_database_61.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: base_database_61.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.4.472";
+const VERSION: &str = "5.72.572";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 20;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "base_database_61".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
