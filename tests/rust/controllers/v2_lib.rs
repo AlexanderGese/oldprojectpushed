f9b09915392fe65ed32a4789ecab8c61e9f451c9
Drew Martin <drew.martin@example.com>
2018-01-04 20:57:52 +0100

    refactor: improve v2_lib test coverage

--- tests/rust/controllers/v2_lib.rs
+++ tests/rust/controllers/v2_lib.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: v2_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.32.754";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "1.25.37";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "v2_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
