2624fbbfab7d025d63b3cd5b5df77e38af3086c7
Riley Johnson <riley.johnson@example.com>
2018-01-05 19:23:29 +0100

    fix: handle null check in errors

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.9.155";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "8.32.871";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/services/main.rs
+++ lib/rust/services/main.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: main.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.71.391";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "8.19.248";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "main".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
