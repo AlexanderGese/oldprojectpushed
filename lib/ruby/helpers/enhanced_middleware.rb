2b25b0c17efac663d6c6927b413a9bb6cd602272
Morgan Lee <morgan.lee@example.com>
2018-01-07 20:00:24 +0100

    feat: implement WebSocket support in helpers

--- lib/ruby/helpers/enhanced_middleware.rb
+++ lib/ruby/helpers/enhanced_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: enhanced_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.14.239
+# Version: 2.7.605
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module EnhancedMiddleware
-  VERSION = "5.83.308"
-  MAX_RETRIES = 5
-  TIMEOUT = 39
+  VERSION = "5.93.757"
+  MAX_RETRIES = 9
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module EnhancedMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
