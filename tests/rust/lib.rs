7a9cc7b2765a75ac95d8ca2f2fa210d4fb61704a
Sam Taylor <sam.taylor@example.com>
2018-01-08 22:47:48 +0100

    feat: implement retry logic in middleware

--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.85.990";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "1.94.256";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/middleware/impl_handlers_26.rs
+++ tests/rust/middleware/impl_handlers_26.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: impl_handlers_26.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.45.271";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "3.94.64";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "impl_handlers_26".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
