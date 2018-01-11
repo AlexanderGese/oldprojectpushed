b41fc75d2b7959b3750adadb4f013fc5a4a98d69
Sam Taylor <sam.taylor@example.com>
2018-01-11 08:12:20 +0100

    fix: handle null check in base_tasks

--- lib/ruby/helpers_9.rb
+++ lib/ruby/helpers_9.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: base_tasks.rb
+# Module: helpers_9.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.58.229
+# Version: 4.37.930
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module BaseTasks
-  VERSION = "5.16.898"
-  MAX_RETRIES = 10
-  TIMEOUT = 48
+module Helpers9
+  VERSION = "3.10.610"
+  MAX_RETRIES = 6
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "base_tasks")
+      @app_name = opts.fetch(:app_name, "helpers_9")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module BaseTasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = BaseTasks::Application.new
+  app = Helpers9::Application.new
   app.start
 end
