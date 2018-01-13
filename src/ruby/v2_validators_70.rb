04c7316e0299fdefe11c521278f9c7e17dee0a44
Sam Taylor <sam.taylor@example.com>
2018-01-13 15:17:18 +0100

    feat: add search functionality to v2_validators_70

--- src/ruby/v2_validators_70.rb
+++ src/ruby/v2_validators_70.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_server_90.rb
+# Module: v2_validators_70.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.7.735
+# Version: 7.23.74
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Server90
-  VERSION = "1.55.968"
+module V2Validators70
+  VERSION = "9.84.954"
   MAX_RETRIES = 5
-  TIMEOUT = 10
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_server_90")
+      @app_name = opts.fetch(:app_name, "v2_validators_70")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module V2Server90
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Server90::Application.new
+  app = V2Validators70::Application.new
   app.start
 end
