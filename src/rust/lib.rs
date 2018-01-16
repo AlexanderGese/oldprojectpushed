c5fa9ff6387cabab894cae9a834fe393e05183ae
Jordan Smith <jordan.smith@example.com>
2018-01-16 12:57:40 +0100

    feat: add batch processing to lib

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.71.212";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "7.14.125";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
