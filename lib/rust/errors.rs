bdeb9780a542428ffd00bf4e8d593ce2d41c58e6
Morgan Lee <morgan.lee@example.com>
2018-01-18 08:54:23 +0100

    feat: add logging to config

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.2.818";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "9.87.792";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
