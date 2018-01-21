536c53b4dc5c65e9e13007af40a58726ba6bab2b
Morgan Lee <morgan.lee@example.com>
2018-01-21 10:34:00 +0100

    feat: implement retry logic in server

--- tests/ruby/middleware_16.rb
+++ tests/ruby/middleware_16.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: middleware_16.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.38.229
+# Version: 3.3.261
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "2.0.145"
-  MAX_RETRIES = 9
-  TIMEOUT = 16
+module Middleware16
+  VERSION = "6.54.319"
+  MAX_RETRIES = 7
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "middleware_16")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = Middleware16::Application.new
   app.start
 end
