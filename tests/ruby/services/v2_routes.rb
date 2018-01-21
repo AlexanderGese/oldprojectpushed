5dd31409d78c61ad12424dcda7128c3d2f400f38
Riley Johnson <riley.johnson@example.com>
2018-01-21 10:37:12 +0100

    chore: clean up v2_routes imports

--- tests/ruby/services/v2_routes.rb
+++ tests/ruby/services/v2_routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: v2_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.78.348
+# Version: 5.8.370
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "3.57.132"
-  MAX_RETRIES = 4
-  TIMEOUT = 48
+module V2Routes
+  VERSION = "3.98.981"
+  MAX_RETRIES = 5
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "v2_routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = V2Routes::Application.new
   app.start
 end
