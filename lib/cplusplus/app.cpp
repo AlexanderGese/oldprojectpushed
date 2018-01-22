dcbf775448531ee1767a6ed0959a077cbbb49c26
Drew Martin <drew.martin@example.com>
2018-01-22 09:32:34 +0100

    fix: handle large payloads in app

--- lib/cplusplus/app.cpp
+++ lib/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.20.312
+ * Version: 7.41.276
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "1.65.711";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 17269;
+constexpr const char* VERSION = "6.0.920";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 9736;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
