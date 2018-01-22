1795a7222e9d33f709671933c36273642e4b81f8
Riley Johnson <riley.johnson@example.com>
2018-01-22 11:34:26 +0100

    feat: implement SSE streaming in lib

--- src/rust/core_errors.rs
+++ src/rust/core_errors.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: core_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.64.200";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "3.35.86";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "core_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
