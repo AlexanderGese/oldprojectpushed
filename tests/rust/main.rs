852cb98875be8c0b9a16a08539f6b0ad7562bfb3
Morgan Lee <morgan.lee@example.com>
2018-01-23 14:06:38 +0100

    fix: resolve race condition in main

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.21.910";
+const VERSION: &str = "8.89.21";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 43;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.97.812";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "5.81.59";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
