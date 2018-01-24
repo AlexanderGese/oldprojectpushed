e72d48dbee761982751cc15235ff27d8079f8b80
Casey Williams <casey.williams@example.com>
2018-01-24 21:22:09 +0100

    chore: update config configuration

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.32.754";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "8.50.195";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/controllers/config.rs
+++ tests/rust/controllers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.84.562";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "3.79.381";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
