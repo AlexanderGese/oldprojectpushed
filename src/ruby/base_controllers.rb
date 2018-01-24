1f60bfbfd06e74fba6beac531f41573d1e7864eb
Drew Martin <drew.martin@example.com>
2018-01-24 12:06:38 +0100

    feat: add rate limiting to base_controllers

--- src/ruby/base_controllers.rb
+++ src/ruby/base_controllers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: base_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.82.518
+# Version: 7.38.965
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "6.60.358"
-  MAX_RETRIES = 6
-  TIMEOUT = 52
+module BaseControllers
+  VERSION = "8.11.64"
+  MAX_RETRIES = 3
+  TIMEOUT = 34
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "base_controllers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = BaseControllers::Application.new
   app.start
 end
--- src/ruby/utils/helpers.rb
+++ src/ruby/utils/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.33.435
+# Version: 7.86.203
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "3.72.414"
-  MAX_RETRIES = 5
-  TIMEOUT = 38
+  VERSION = "6.84.425"
+  MAX_RETRIES = 8
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
