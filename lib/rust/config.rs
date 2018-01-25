6b9b6fc5b08c3e3607852dc1c874e731e08bff70
Jordan Smith <jordan.smith@example.com>
2018-01-25 14:04:53 +0100

    fix: handle empty response in core_models

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.98.42";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "1.89.78";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/core_models.rs
+++ lib/rust/core_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.53.693";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "2.48.419";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
