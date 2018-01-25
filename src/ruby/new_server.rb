72cb32b9448e51a02ff4cdc54574f6c97c3d79bc
Jamie Davis <jamie.davis@example.com>
2018-01-25 17:59:58 +0100

    chore: clean up helpers imports

--- src/ruby/new_server.rb
+++ src/ruby/new_server.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: new_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.93.941
+# Version: 1.50.206
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "6.86.509"
-  MAX_RETRIES = 10
-  TIMEOUT = 46
+module NewServer
+  VERSION = "9.40.503"
+  MAX_RETRIES = 4
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "new_server")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = NewServer::Application.new
   app.start
 end
