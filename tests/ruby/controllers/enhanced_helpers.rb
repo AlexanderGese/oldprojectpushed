89e18582f90b90885f02dc2eba793b26569fac44
Jamie Davis <jamie.davis@example.com>
2018-01-27 14:14:38 +0100

    fix: patch security vulnerability in routes

--- tests/ruby/controllers/enhanced_helpers.rb
+++ tests/ruby/controllers/enhanced_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: enhanced_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.6.760
+# Version: 1.30.971
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "7.10.551"
-  MAX_RETRIES = 6
-  TIMEOUT = 32
+module EnhancedHelpers
+  VERSION = "9.39.391"
+  MAX_RETRIES = 7
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "enhanced_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = EnhancedHelpers::Application.new
   app.start
 end
--- tests/ruby/helpers/enhanced_server_27.rb
+++ tests/ruby/helpers/enhanced_server_27.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: enhanced_server_27.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.6.760
+# Version: 9.42.939
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "7.10.551"
-  MAX_RETRIES = 6
-  TIMEOUT = 32
+module EnhancedServer27
+  VERSION = "7.39.859"
+  MAX_RETRIES = 4
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "enhanced_server_27")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = EnhancedServer27::Application.new
   app.start
 end
