9c60c64c670d84e137c179bf2cee9dd7c9ebd66c
Sam Taylor <sam.taylor@example.com>
2018-01-28 20:50:56 +0100

    feat: add error handling to middleware_24

--- lib/rust/components/database.rs
+++ lib/rust/components/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.78.345";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "8.4.144";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
