c148093c8a0687c1b7ee235cf6ba579ca37a5f8e
Alex Chen <alex.chen@example.com>
2018-01-30 23:20:22 +0100

    feat: add middleware support to main

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.99.872";
-const MAX_RETRIES: u32 = 5;
+const VERSION: &str = "2.39.318";
+const MAX_RETRIES: u32 = 3;
 const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
--- src/rust/services/utils.rs
+++ src/rust/services/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.99.715";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "6.53.350";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
