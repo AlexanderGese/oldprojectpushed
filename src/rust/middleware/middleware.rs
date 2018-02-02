158dcd566b3d8da971f4e00fcac168b57dfa3f2b
Riley Johnson <riley.johnson@example.com>
2018-02-02 14:21:50 +0100

    refactor: decouple middleware dependencies

--- src/rust/middleware/middleware.rs
+++ src/rust/middleware/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.40.647";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "7.68.77";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
