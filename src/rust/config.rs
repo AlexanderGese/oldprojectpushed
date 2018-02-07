fcd35015154da0a14234dfb90e128e26655e418d
Sam Taylor <sam.taylor@example.com>
2018-02-07 17:09:48 +0100

    chore: add linting rules for config

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.57.240";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "4.6.341";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/enhanced_config.rs
+++ src/rust/enhanced_config.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.78.818";
-const MAX_RETRIES: u32 = 5;
+const VERSION: &str = "9.25.947";
+const MAX_RETRIES: u32 = 10;
 const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
