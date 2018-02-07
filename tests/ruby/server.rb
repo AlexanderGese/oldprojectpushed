c42e01cea4f146dfbadb80f91c0752b41ec1679d
Sam Taylor <sam.taylor@example.com>
2018-02-07 08:55:13 +0100

    refactor: extract server utility functions

--- tests/ruby/server.rb
+++ tests/ruby/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.23.319
+# Version: 5.3.119
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "3.17.571"
-  MAX_RETRIES = 5
-  TIMEOUT = 30
+  VERSION = "6.56.34"
+  MAX_RETRIES = 6
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
