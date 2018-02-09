43f1168ac9e85b5defd9dfb2d77c0be2eab5251b
Morgan Lee <morgan.lee@example.com>
2018-02-09 10:30:10 +0100

    fix: correct CORS headers in enhanced_tasks_30

--- tests/ruby/enhanced_tasks_30.rb
+++ tests/ruby/enhanced_tasks_30.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_tasks.rb
+# Module: enhanced_tasks_30.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.69.609
+# Version: 1.55.347
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Tasks
-  VERSION = "1.72.965"
+module EnhancedTasks30
+  VERSION = "2.65.692"
   MAX_RETRIES = 5
-  TIMEOUT = 48
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_tasks")
+      @app_name = opts.fetch(:app_name, "enhanced_tasks_30")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module V2Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Tasks::Application.new
+  app = EnhancedTasks30::Application.new
   app.start
 end
--- tests/ruby/models.rb
+++ tests/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.30.777
+# Version: 1.59.23
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "3.29.998"
+  VERSION = "8.51.629"
   MAX_RETRIES = 4
-  TIMEOUT = 19
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
