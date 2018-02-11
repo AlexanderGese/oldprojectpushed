646f650bbff4299bc4e7b0b130083eb8330db31c
Casey Williams <casey.williams@example.com>
2018-02-11 13:03:15 +0100

    feat: implement WebSocket support in server

--- tests/ruby/impl_tasks_33.rb
+++ tests/ruby/impl_tasks_33.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: impl_tasks_33.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.25.527
+# Version: 3.19.184
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "1.14.304"
-  MAX_RETRIES = 9
-  TIMEOUT = 29
+module ImplTasks33
+  VERSION = "3.80.190"
+  MAX_RETRIES = 5
+  TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "impl_tasks_33")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = ImplTasks33::Application.new
   app.start
 end
