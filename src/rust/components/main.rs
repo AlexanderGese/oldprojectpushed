92daf35b0c77876066059afffff84dcfef16079a
Alex Chen <alex.chen@example.com>
2018-02-12 12:52:08 +0100

    chore: bump main version

--- src/rust/components/main.rs
+++ src/rust/components/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.1.960";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "2.79.233";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/components/utils.rs
+++ src/rust/components/utils.rs
@@ -6,7 +6,7 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.32.25";
+const VERSION: &str = "4.33.136";
 const MAX_RETRIES: u32 = 6;
 const TIMEOUT_SECS: u64 = 16;
 
