003fe920b0c8d6cb5a9b5196c0fce82c87af6601
Jordan Smith <jordan.smith@example.com>
2018-02-12 11:21:00 +0100

    refactor: migrate routes to async/await

--- lib/rust/utils/routes.rs
+++ lib/rust/utils/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.58.205";
+const VERSION: &str = "1.69.615";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 26;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
