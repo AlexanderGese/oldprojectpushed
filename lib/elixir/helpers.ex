63f30db917853167c9f987e820790609d566926a
Jordan Smith <jordan.smith@example.com>
2018-02-12 18:18:33 +0100

    docs: add documentation for helpers

--- lib/elixir/helpers.ex
+++ lib/elixir/helpers.ex
@@ -2,12 +2,12 @@ defmodule Helpers do
   @moduledoc """
   Module: helpers.ex
   Elixir boilerplate - auto-generated
-  Version: 3.8.456
+  Version: 8.99.173
   """
 
-  @version "4.13.855"
-  @max_retries 8
-  @timeout 16416
+  @version "9.71.109"
+  @max_retries 10
+  @timeout 12082
 
   defstruct [
     :app_name,
