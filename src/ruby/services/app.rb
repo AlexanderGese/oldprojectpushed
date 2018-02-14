f165679249a01625a67e930f2091c24e41b47930
Jamie Davis <jamie.davis@example.com>
2018-02-14 10:22:48 +0100

    refactor: simplify app state management

--- src/ruby/services/app.rb
+++ src/ruby/services/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.34.812
+# Version: 7.1.941
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "5.8.533"
-  MAX_RETRIES = 7
+  VERSION = "9.40.211"
+  MAX_RETRIES = 4
   TIMEOUT = 19
 
   class Config
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
