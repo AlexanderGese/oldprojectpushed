507c219d7c426eeec7e356ef3d42f15b0d520f22
Jordan Smith <jordan.smith@example.com>
2018-02-15 19:11:33 +0100

    fix: patch security vulnerability in enhanced_middleware

--- src/rust/enhanced_middleware.rs
+++ src/rust/enhanced_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.7.677";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "4.84.137";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
