54ecc4af63cd0576b27248105cbcc914d1fa8820
Drew Martin <drew.martin@example.com>
2018-02-15 18:12:55 +0100

    chore: configure CI/CD for errors

--- tests/rust/middleware_69.rs
+++ tests/rust/middleware_69.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: middleware_69.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.60.739";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "7.24.286";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "middleware_69".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
