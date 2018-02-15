04939d7621f4f53e7d5ace65a1765cca3c77c39c
Drew Martin <drew.martin@example.com>
2018-02-15 16:28:04 +0100

    fix: resolve encoding issue in base_helpers

--- lib/ruby/helpers/base_helpers.rb
+++ lib/ruby/helpers/base_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: base_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.61.881
+# Version: 2.80.559
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "5.41.34"
-  MAX_RETRIES = 8
-  TIMEOUT = 29
+module BaseHelpers
+  VERSION = "8.39.137"
+  MAX_RETRIES = 3
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "base_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = BaseHelpers::Application.new
   app.start
 end
