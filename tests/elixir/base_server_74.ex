1bd01a6c4a59e6f2f0ae217b8dfb0a284bb6abb7
Alex Chen <alex.chen@example.com>
2018-02-16 23:48:09 +0100

    feat: add middleware support to new_worker

--- tests/elixir/base_server_74.ex
+++ tests/elixir/base_server_74.ex
@@ -1,13 +1,13 @@
-defmodule Router do
+defmodule Base_Server_74 do
   @moduledoc """
-  Module: router.ex
+  Module: base_server_74.ex
   Elixir boilerplate - auto-generated
-  Version: 1.54.664
+  Version: 9.20.670
   """
 
-  @version "3.60.460"
-  @max_retries 8
-  @timeout 6453
+  @version "8.3.665"
+  @max_retries 6
+  @timeout 12771
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Router do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "router"),
+      app_name: Keyword.get(opts, :app_name, "base_server_74"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Router do
   end
 end
 
-defmodule RouterStore do
+defmodule Base_Server_74Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RouterStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RouterEventBus do
+defmodule Base_Server_74EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/handlers/new_worker.ex
+++ tests/elixir/handlers/new_worker.ex
@@ -1,13 +1,13 @@
-defmodule Router do
+defmodule New_Worker do
   @moduledoc """
-  Module: router.ex
+  Module: new_worker.ex
   Elixir boilerplate - auto-generated
-  Version: 1.54.664
+  Version: 3.87.43
   """
 
-  @version "3.60.460"
-  @max_retries 8
-  @timeout 6453
+  @version "4.92.180"
+  @max_retries 5
+  @timeout 10786
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Router do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "router"),
+      app_name: Keyword.get(opts, :app_name, "new_worker"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Router do
   end
 end
 
-defmodule RouterStore do
+defmodule New_WorkerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RouterStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RouterEventBus do
+defmodule New_WorkerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
