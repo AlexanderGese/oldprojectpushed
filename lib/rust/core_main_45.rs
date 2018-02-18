02bb4706a98c13db2a11f5a7c49b533d4c2688c0
Sam Taylor <sam.taylor@example.com>
2018-02-18 15:38:04 +0100

    feat: add logging to core_main_45

--- lib/rust/core_main_45.rs
+++ lib/rust/core_main_45.rs
@@ -1,4 +1,4 @@
-//! Module: core_models.rs
+//! Module: core_main_45.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.75.892";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "9.16.495";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_models".to_string(),
+            app_name: "core_main_45".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
