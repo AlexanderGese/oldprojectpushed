8414b00c69f573976553a255ffde1f7516c9c60d
Morgan Lee <morgan.lee@example.com>
2018-02-18 14:03:48 +0100

    refactor: migrate utils to async/await

--- tests/cplusplus/v2_app_2.cpp
+++ tests/cplusplus/v2_app_2.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: v2_app_2.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.37.876
+ * Version: 6.58.266
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace v2_app_2 {
 
-constexpr const char* VERSION = "8.32.479";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 5989;
+constexpr const char* VERSION = "6.52.167";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 25776;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace v2_app_2
 
 int main() {
-    using namespace utils;
+    using namespace v2_app_2;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
