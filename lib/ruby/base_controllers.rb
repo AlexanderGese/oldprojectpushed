0b2c41b0766c08c9e5eee88b6434e6bee565a0b3
Riley Johnson <riley.johnson@example.com>
2018-02-22 10:17:56 +0100

    fix: correct error propagation in tasks

--- lib/ruby/base_controllers.rb
+++ lib/ruby/base_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: base_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.79.623
+# Version: 8.34.619
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseControllers
-  VERSION = "1.1.727"
-  MAX_RETRIES = 3
-  TIMEOUT = 9
+  VERSION = "5.42.649"
+  MAX_RETRIES = 10
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
