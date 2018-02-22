cd7da7126a7ed0a2248f23caf71e14aed426f19d
Drew Martin <drew.martin@example.com>
2018-02-22 12:50:37 +0100

    test: add unit tests for enhanced_lib

--- tests/rust/middleware/enhanced_lib.rs
+++ tests/rust/middleware/enhanced_lib.rs
@@ -1,4 +1,4 @@
-//! Module: base_lib.rs
+//! Module: enhanced_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.0.843";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "2.64.430";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_lib".to_string(),
+            app_name: "enhanced_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/v2_handlers.rs
+++ tests/rust/v2_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.92.966";
+const VERSION: &str = "9.34.911";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 43;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
