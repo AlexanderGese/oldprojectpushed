e08b0b140987db26d8e360f2d96fe60a2bb2052d
Jordan Smith <jordan.smith@example.com>
2018-02-22 14:45:20 +0100

    feat: add logging to validators

--- src/ruby/new_app_66.rb
+++ src/ruby/new_app_66.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: new_app_66.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.26.342
+# Version: 5.63.140
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "1.47.627"
-  MAX_RETRIES = 10
-  TIMEOUT = 53
+module NewApp66
+  VERSION = "3.24.559"
+  MAX_RETRIES = 3
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "new_app_66")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = NewApp66::Application.new
   app.start
 end
