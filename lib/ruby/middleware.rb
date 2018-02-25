11f7607e6b47e780310f73a3d5f8a9e933baff60
Sam Taylor <sam.taylor@example.com>
2018-02-25 12:29:08 +0100

    fix: resolve edge case in middleware handler

--- lib/ruby/middleware.rb
+++ lib/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.15.858
+# Version: 6.13.265
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "3.13.430"
+  VERSION = "5.64.251"
   MAX_RETRIES = 10
-  TIMEOUT = 54
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
