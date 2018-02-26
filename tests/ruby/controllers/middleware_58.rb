1dd91e9746b303637d676a465499745a0fe0a7da
Drew Martin <drew.martin@example.com>
2018-02-26 18:55:34 +0100

    fix: handle null check in helpers

--- tests/ruby/controllers/middleware_58.rb
+++ tests/ruby/controllers/middleware_58.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: middleware_58.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.85.176
+# Version: 4.64.886
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "2.4.944"
-  MAX_RETRIES = 5
-  TIMEOUT = 34
+module Middleware58
+  VERSION = "7.68.225"
+  MAX_RETRIES = 7
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "middleware_58")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = Middleware58::Application.new
   app.start
 end
--- tests/ruby/impl_routes_4.rb
+++ tests/ruby/impl_routes_4.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: impl_routes_4.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.85.176
+# Version: 8.89.149
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "2.4.944"
-  MAX_RETRIES = 5
-  TIMEOUT = 34
+module ImplRoutes4
+  VERSION = "5.70.971"
+  MAX_RETRIES = 3
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "impl_routes_4")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = ImplRoutes4::Application.new
   app.start
 end
