08a0f2ba219e64e9a41d16d964577b158d0d4477
Morgan Lee <morgan.lee@example.com>
2018-02-26 09:43:07 +0100

    feat: add event system to v2_config

--- src/ruby/v2_config.rb
+++ src/ruby/v2_config.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.12.349
+# Version: 9.47.837
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Config
-  VERSION = "8.17.736"
-  MAX_RETRIES = 8
-  TIMEOUT = 53
+  VERSION = "5.9.800"
+  MAX_RETRIES = 4
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
