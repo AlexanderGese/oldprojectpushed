bc683327913bae05eb0fa69e7be94204b6cd5640
Drew Martin <drew.martin@example.com>
2018-02-27 20:49:06 +0100

    feat: add logging to base_routes

--- tests/ruby/utils/server.rb
+++ tests/ruby/utils/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.91.211
+# Version: 4.3.871
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "4.85.217"
-  MAX_RETRIES = 9
-  TIMEOUT = 53
+  VERSION = "8.79.604"
+  MAX_RETRIES = 3
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
