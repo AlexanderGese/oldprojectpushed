dbd63bfef674e15a359b046cda0fd0c9a893b053
Morgan Lee <morgan.lee@example.com>
2018-03-01 16:55:39 +0100

    fix: resolve edge case in base_app handler

--- src/ruby/base_app.rb
+++ src/ruby/base_app.rb
@@ -2,7 +2,7 @@
 
 # Module: base_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.55.312
+# Version: 8.37.844
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseApp
-  VERSION = "4.71.250"
+  VERSION = "1.71.558"
   MAX_RETRIES = 4
-  TIMEOUT = 54
+  TIMEOUT = 14
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseApp
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
--- src/ruby/middleware_47.rb
+++ src/ruby/middleware_47.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware_47.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.62.461
+# Version: 4.0.653
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware47
-  VERSION = "2.81.35"
-  MAX_RETRIES = 4
-  TIMEOUT = 52
+  VERSION = "4.71.679"
+  MAX_RETRIES = 5
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
