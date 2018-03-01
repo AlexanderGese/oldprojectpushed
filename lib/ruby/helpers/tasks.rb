9fd08e1a09df68aabe10f111dfd7710b2a0f5835
Sam Taylor <sam.taylor@example.com>
2018-03-01 15:23:50 +0100

    refactor: decouple models dependencies

--- lib/ruby/helpers/tasks.rb
+++ lib/ruby/helpers/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.97.907
+# Version: 6.36.352
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "2.73.767"
-  MAX_RETRIES = 10
-  TIMEOUT = 47
+  VERSION = "3.45.84"
+  MAX_RETRIES = 8
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
