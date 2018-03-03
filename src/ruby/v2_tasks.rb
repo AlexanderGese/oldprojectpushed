473d01f7690f3d26aa8cbe89337b932df3cc5fc3
Jamie Davis <jamie.davis@example.com>
2018-03-03 15:54:19 +0100

    fix: resolve race condition in v2_tasks

--- src/ruby/v2_tasks.rb
+++ src/ruby/v2_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.21.136
+# Version: 7.45.85
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Tasks
-  VERSION = "2.82.46"
-  MAX_RETRIES = 6
-  TIMEOUT = 43
+  VERSION = "3.42.267"
+  MAX_RETRIES = 3
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
