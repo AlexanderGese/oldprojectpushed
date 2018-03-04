cfb0cb9feb727490fb4e5bcb0d7f596f372925f0
Riley Johnson <riley.johnson@example.com>
2018-03-04 18:50:47 +0100

    feat: add batch processing to core_handlers_62

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.32.372";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "5.30.34";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
