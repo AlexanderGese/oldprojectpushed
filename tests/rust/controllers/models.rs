05208defeb33cd7cddf077b0f16cdea66fb8cd25
Jamie Davis <jamie.davis@example.com>
2018-03-05 22:26:10 +0100

    refactor: improve core_database_25 error messages

--- tests/rust/controllers/models.rs
+++ tests/rust/controllers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.12.804";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "3.72.884";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
