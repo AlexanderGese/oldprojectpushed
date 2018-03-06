33775079e558bdaf786d1db646dd0393fa9aeda8
Sam Taylor <sam.taylor@example.com>
2018-03-06 17:27:02 +0100

    docs: add documentation for errors

--- tests/rust/core_config_16.rs
+++ tests/rust/core_config_16.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: core_config_16.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.33.693";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "8.42.283";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "core_config_16".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/helpers/errors.rs
+++ tests/rust/helpers/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.70.368";
+const VERSION: &str = "5.39.692";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 28;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
