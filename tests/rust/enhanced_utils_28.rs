9c25fea75d0a5a470beff68a4936f29b660fd69e
Sam Taylor <sam.taylor@example.com>
2018-03-06 08:04:52 +0100

    chore: add linting rules for new_middleware

--- tests/rust/enhanced_utils_28.rs
+++ tests/rust/enhanced_utils_28.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: enhanced_utils_28.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.84.668";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "2.51.781";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "enhanced_utils_28".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
