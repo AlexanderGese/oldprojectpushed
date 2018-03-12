0f2311d6e27addace2b35521031547190a084803
Alex Chen <alex.chen@example.com>
2018-03-12 23:38:01 +0100

    fix: resolve memory leak in base_config

--- lib/ruby/utils/base_config.rb
+++ lib/ruby/utils/base_config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: base_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.98.970
+# Version: 7.37.160
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "4.4.518"
-  MAX_RETRIES = 4
-  TIMEOUT = 24
+module BaseConfig
+  VERSION = "9.78.262"
+  MAX_RETRIES = 8
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "base_config")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = BaseConfig::Application.new
   app.start
 end
