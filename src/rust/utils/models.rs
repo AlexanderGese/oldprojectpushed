553e19626e959509d2c1e4885442299bbf433ef7
Alex Chen <alex.chen@example.com>
2018-03-13 19:39:08 +0100

    refactor: improve models test coverage

--- src/rust/utils/models.rs
+++ src/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.87.373";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "6.84.389";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
