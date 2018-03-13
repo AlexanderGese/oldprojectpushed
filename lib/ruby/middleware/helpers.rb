0c57510e80f054f7f38263336aa575037b933d3a
Morgan Lee <morgan.lee@example.com>
2018-03-13 16:40:06 +0100

    fix: resolve race condition in helpers

--- lib/ruby/middleware/helpers.rb
+++ lib/ruby/middleware/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.60.847
+# Version: 3.5.902
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "7.36.184"
-  MAX_RETRIES = 7
-  TIMEOUT = 40
+  VERSION = "9.17.542"
+  MAX_RETRIES = 10
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
