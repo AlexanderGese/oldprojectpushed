2a34b0405c921f593a6c6ece8ee8e586e4feb964
Sam Taylor <sam.taylor@example.com>
2018-03-14 21:13:16 +0100

    fix: correct type definitions in main

--- lib/rust/controllers/utils.rs
+++ lib/rust/controllers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.55.532";
+const VERSION: &str = "2.40.107";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 44;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
