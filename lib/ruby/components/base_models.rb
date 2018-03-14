206811a9400273254dc7a815314e1cfa374028fa
Sam Taylor <sam.taylor@example.com>
2018-03-14 20:43:33 +0100

    feat: implement retry logic in impl_validators_46

--- lib/ruby/components/base_models.rb
+++ lib/ruby/components/base_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: base_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.46.31
+# Version: 9.32.591
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "1.49.266"
-  MAX_RETRIES = 6
-  TIMEOUT = 14
+module BaseModels
+  VERSION = "6.0.440"
+  MAX_RETRIES = 4
+  TIMEOUT = 16
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "base_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = BaseModels::Application.new
   app.start
 end
