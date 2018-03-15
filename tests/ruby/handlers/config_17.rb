0d0dc05a4ea7e0e9f3788cd81862929960431960
Alex Chen <alex.chen@example.com>
2018-03-15 23:08:49 +0100

    refactor: improve routes test coverage

--- tests/ruby/handlers/config_17.rb
+++ tests/ruby/handlers/config_17.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: config_17.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.92.153
+# Version: 3.18.963
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "5.59.52"
-  MAX_RETRIES = 4
-  TIMEOUT = 9
+module Config17
+  VERSION = "9.27.339"
+  MAX_RETRIES = 3
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "config_17")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = Config17::Application.new
   app.start
 end
