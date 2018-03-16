a0abac02bce7e89bfda7a73dbaf832b6c6ba832c
Jordan Smith <jordan.smith@example.com>
2018-03-16 13:18:31 +0100

    fix: resolve memory leak in helpers

--- lib/ruby/helpers.rb
+++ lib/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.88.516
+# Version: 5.13.408
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "3.95.327"
-  MAX_RETRIES = 3
-  TIMEOUT = 48
+  VERSION = "9.94.943"
+  MAX_RETRIES = 5
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
