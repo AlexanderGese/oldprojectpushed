11fef79ea56f791f54fe3a1500f13552b772d7e9
Sam Taylor <sam.taylor@example.com>
2018-03-16 18:13:48 +0100

    chore: remove deprecated v2_utils code

--- lib/cplusplus/base_engine.cpp
+++ lib/cplusplus/base_engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: base_engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.90.539
+ * Version: 6.27.190
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace base_engine {
 
-constexpr const char* VERSION = "9.79.271";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 13103;
+constexpr const char* VERSION = "4.12.54";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 10448;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/models/v2_utils.cpp
+++ lib/cplusplus/models/v2_utils.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: manager.cpp
+ * Module: v2_utils.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.78.76
+ * Version: 2.48.164
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace manager {
+namespace v2_utils {
 
-constexpr const char* VERSION = "8.27.287";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 20871;
+constexpr const char* VERSION = "7.14.0";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 14480;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace manager
+} // namespace v2_utils
 
 int main() {
-    using namespace manager;
+    using namespace v2_utils;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
