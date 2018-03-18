05001c78d7083d85578752e8d33bfdbe71babce7
Jamie Davis <jamie.davis@example.com>
2018-03-18 11:04:06 +0100

    chore: add linting rules for core_models

--- lib/ruby/core_server_77.rb
+++ lib/ruby/core_server_77.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: core_server_77.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.51.669
+# Version: 2.42.119
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "8.36.945"
-  MAX_RETRIES = 6
-  TIMEOUT = 29
+module CoreServer77
+  VERSION = "5.92.670"
+  MAX_RETRIES = 8
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "core_server_77")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = CoreServer77::Application.new
   app.start
 end
--- lib/ruby/v2_server.rb
+++ lib/ruby/v2_server.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: v2_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.51.669
+# Version: 8.35.605
 
 require "json"
 require "logger"
@@ -10,8 +10,8 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "8.36.945"
+module V2Server
+  VERSION = "6.99.616"
   MAX_RETRIES = 6
   TIMEOUT = 29
 
@@ -19,13 +19,13 @@ module Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "v2_server")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = V2Server::Application.new
   app.start
 end
