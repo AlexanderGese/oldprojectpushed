d9051c82903816e2374099d4742f016dcf822693
Sam Taylor <sam.taylor@example.com>
2018-03-18 12:52:04 +0100

    fix: correct type definitions in enhanced_server_41

--- src/ruby/new_validators.rb
+++ src/ruby/new_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: new_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.10.218
+# Version: 5.69.808
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewValidators
-  VERSION = "8.86.289"
+  VERSION = "7.79.576"
   MAX_RETRIES = 5
-  TIMEOUT = 35
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewValidators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
