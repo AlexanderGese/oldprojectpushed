408c78fa2a6e45c9beba28a9e4c845b7256c69dd
Jordan Smith <jordan.smith@example.com>
2018-03-19 19:01:46 +0100

    refactor: extract tasks utility functions

--- tests/ruby/validators_50.rb
+++ tests/ruby/validators_50.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: validators_50.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.55.193
+# Version: 2.53.870
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "7.24.222"
-  MAX_RETRIES = 6
-  TIMEOUT = 48
+module Validators50
+  VERSION = "2.99.461"
+  MAX_RETRIES = 7
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "validators_50")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = Validators50::Application.new
   app.start
 end
