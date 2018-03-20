29e780fe1293922b3a58fb7db64ba612f58cad93
Sam Taylor <sam.taylor@example.com>
2018-03-20 22:16:53 +0100

    fix: handle concurrent access in database

--- tests/rust/components/enhanced_errors.rs
+++ tests/rust/components/enhanced_errors.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: enhanced_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.14.832";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "9.27.540";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "enhanced_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
