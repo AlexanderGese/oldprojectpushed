c8cd5d2c12f5d982da07b32eec8322d1236cdfc2
Morgan Lee <morgan.lee@example.com>
2018-03-23 19:20:47 +0100

    fix: resolve edge case in v2_routes_90 handler

--- lib/rust/helpers/v2_models_10.rs
+++ lib/rust/helpers/v2_models_10.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: v2_models_10.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.45.296";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "6.81.475";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "v2_models_10".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
