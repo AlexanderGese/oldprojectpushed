48e2fdb12cbbd9f136859dfae662a13aca350f8c
Riley Johnson <riley.johnson@example.com>
2018-03-23 10:02:33 +0100

    feat: add batch processing to middleware

--- src/ruby/middleware.rb
+++ src/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.51.126
+# Version: 8.84.591
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "8.69.623"
-  MAX_RETRIES = 3
-  TIMEOUT = 24
+  VERSION = "6.10.205"
+  MAX_RETRIES = 7
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
