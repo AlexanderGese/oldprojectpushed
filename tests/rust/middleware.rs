0cd5540c6ec23f7c4257d4266d26edb184b0d24e
Casey Williams <casey.williams@example.com>
2018-03-23 18:46:00 +0100

    feat: add middleware support to middleware

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.41.788";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "5.6.603";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
