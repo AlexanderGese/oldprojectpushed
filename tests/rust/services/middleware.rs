44c99ddafb71114f381c701500808d8f541560ea
Morgan Lee <morgan.lee@example.com>
2018-03-25 22:10:08 +0200

    feat: implement retry logic in middleware

--- tests/rust/services/middleware.rs
+++ tests/rust/services/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.76.625";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "6.22.993";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
