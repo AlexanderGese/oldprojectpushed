bb789dd2011a7a390cb24e88055f5071c7379a6c
Riley Johnson <riley.johnson@example.com>
2018-03-27 16:45:33 +0200

    fix: handle empty response in base_validators

--- tests/ruby/handlers/base_validators.rb
+++ tests/ruby/handlers/base_validators.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: base_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.93.343
+# Version: 1.29.625
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "9.82.86"
+module BaseValidators
+  VERSION = "9.49.338"
   MAX_RETRIES = 5
-  TIMEOUT = 44
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "base_validators")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = BaseValidators::Application.new
   app.start
 end
