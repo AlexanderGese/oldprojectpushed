87016b10d93006fb4ceca1582e311e5049a0efa3
Morgan Lee <morgan.lee@example.com>
2018-03-28 16:13:36 +0200

    feat: add batch processing to models

--- src/rust/handlers/core_lib.rs
+++ src/rust/handlers/core_lib.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: core_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.38.615";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "5.11.458";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "core_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/services/utils.rs
+++ src/rust/services/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.53.350";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "7.60.883";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
