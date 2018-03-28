f58e833cccb37b1242d6e9d856dcf4f851803861
Sam Taylor <sam.taylor@example.com>
2018-03-28 17:43:58 +0200

    feat: add validation to new_middleware inputs

--- lib/ruby/new_middleware.rb
+++ lib/ruby/new_middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: new_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.39.940
+# Version: 9.53.375
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "7.38.532"
+module NewMiddleware
+  VERSION = "5.75.261"
   MAX_RETRIES = 4
-  TIMEOUT = 31
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "new_middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = NewMiddleware::Application.new
   app.start
 end
