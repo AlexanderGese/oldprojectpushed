da6bc73c2782f37d9c5bf306dd13ae7d594bcfdd
Riley Johnson <riley.johnson@example.com>
2018-03-29 19:55:50 +0200

    fix: resolve race condition in errors

--- lib/rust/services/errors.rs
+++ lib/rust/services/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.89.333";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "1.15.743";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
