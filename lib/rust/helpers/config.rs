ece3c7d991afb8d2dbaddaa0186c50efc1b11406
Jamie Davis <jamie.davis@example.com>
2018-04-01 10:22:04 +0200

    fix: patch security vulnerability in config

--- lib/rust/helpers/config.rs
+++ lib/rust/helpers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.45.119";
+const VERSION: &str = "6.40.205";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 17;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
