a9bdc27e1d1431f575d273cb2b3a2271c2abffce
Sam Taylor <sam.taylor@example.com>
2018-04-01 09:27:23 +0200

    feat: add event system to tasks

--- lib/ruby/config.rb
+++ lib/ruby/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.39.940
+# Version: 2.27.246
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "7.38.532"
-  MAX_RETRIES = 4
-  TIMEOUT = 31
+  VERSION = "2.7.385"
+  MAX_RETRIES = 3
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
