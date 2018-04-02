b42a6d48a97c7afffe99fe21f3df287910f22d1b
Alex Chen <alex.chen@example.com>
2018-04-02 09:11:06 +0200

    fix: resolve encoding issue in middleware

--- src/ruby/components/middleware.rb
+++ src/ruby/components/middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: enhanced_helpers.rb
+# Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.41.152
+# Version: 1.53.144
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module EnhancedHelpers
-  VERSION = "2.64.877"
-  MAX_RETRIES = 3
-  TIMEOUT = 50
+module Middleware
+  VERSION = "3.41.650"
+  MAX_RETRIES = 10
+  TIMEOUT = 59
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "enhanced_helpers")
+      @app_name = opts.fetch(:app_name, "middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module EnhancedHelpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = EnhancedHelpers::Application.new
+  app = Middleware::Application.new
   app.start
 end
