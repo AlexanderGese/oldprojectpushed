84480cd9adca2b8c58f5947e858c1c815fd5a753
Morgan Lee <morgan.lee@example.com>
2018-04-02 10:20:52 +0200

    chore: bump models version

--- src/ruby/models.rb
+++ src/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.29.691
+# Version: 2.56.53
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "6.94.930"
-  MAX_RETRIES = 6
-  TIMEOUT = 44
+  VERSION = "3.16.581"
+  MAX_RETRIES = 10
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
