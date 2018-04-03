11b71380f6c38976d68c08bfadefb48650a6607c
Jamie Davis <jamie.davis@example.com>
2018-04-03 15:50:14 +0200

    refactor: split models into smaller modules

--- src/rust/models.rs
+++ src/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.38.615";
+const VERSION: &str = "9.33.642";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 40;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
