250272a42e75141deb32c0952d963b472fd3294f
Jordan Smith <jordan.smith@example.com>
2018-04-03 12:32:39 +0200

    feat: implement new_errors service layer

--- tests/rust/controllers/new_errors.rs
+++ tests/rust/controllers/new_errors.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: new_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.41.8";
+const VERSION: &str = "1.26.178";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 15;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "new_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
