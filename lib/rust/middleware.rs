1d08c65e1dbc3330baf19553e06a622f1ae2734b
Sam Taylor <sam.taylor@example.com>
2018-04-05 19:46:06 +0200

    feat: add main module with core functionality

--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.63.56";
+const VERSION: &str = "5.59.535";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 56;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
