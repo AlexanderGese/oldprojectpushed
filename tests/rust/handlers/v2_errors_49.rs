e2763b5b5e0ddc37f664dd190c8bfdcfd5a9196c
Alex Chen <alex.chen@example.com>
2018-04-08 21:27:25 +0200

    feat: add logging to models

--- tests/rust/handlers/v2_errors_49.rs
+++ tests/rust/handlers/v2_errors_49.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: v2_errors_49.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.24.521";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "1.81.986";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "v2_errors_49".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
