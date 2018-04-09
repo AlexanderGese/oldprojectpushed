fa020bc9a2eb7b7a843d76633222becbb3bcd466
Alex Chen <alex.chen@example.com>
2018-04-09 14:10:57 +0200

    refactor: simplify errors state management

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.95.386";
+const VERSION: &str = "9.6.278";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 6;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
