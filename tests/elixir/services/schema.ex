5c0284af27d10acfd075393066cc729f88552475
Jamie Davis <jamie.davis@example.com>
2018-04-10 14:28:06 +0200

    refactor: optimize schema performance

--- tests/elixir/services/schema.ex
+++ tests/elixir/services/schema.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Schema do
   @moduledoc """
-  Module: controller.ex
+  Module: schema.ex
   Elixir boilerplate - auto-generated
-  Version: 5.54.238
+  Version: 3.10.957
   """
 
-  @version "9.19.254"
-  @max_retries 6
-  @timeout 26230
+  @version "5.35.870"
+  @max_retries 3
+  @timeout 25923
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "schema"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule SchemaStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule SchemaEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
