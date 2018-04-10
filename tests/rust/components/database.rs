d5cb5712b0b0495de87310d73ec1d16088298a68
Sam Taylor <sam.taylor@example.com>
2018-04-10 11:15:23 +0200

    fix: handle concurrent access in database

--- tests/rust/components/database.rs
+++ tests/rust/components/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.60.542";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "5.58.428";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/handlers/core_routes_44.rs
+++ tests/rust/handlers/core_routes_44.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: core_routes_44.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.3.586";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "2.64.967";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "core_routes_44".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
