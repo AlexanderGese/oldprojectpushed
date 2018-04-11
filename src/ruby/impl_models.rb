5880339fb4aba2bb14b56cd65f422ba6da4725cb
Sam Taylor <sam.taylor@example.com>
2018-04-11 18:02:12 +0200

    fix: correct date formatting in impl_models

--- src/ruby/impl_models.rb
+++ src/ruby/impl_models.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.66.594
+# Version: 1.5.647
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplModels
-  VERSION = "2.89.757"
-  MAX_RETRIES = 4
-  TIMEOUT = 10
+  VERSION = "4.28.895"
+  MAX_RETRIES = 8
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
