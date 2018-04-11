56813bc76adb062e29e95efba5944d81c734a668
Alex Chen <alex.chen@example.com>
2018-04-11 20:29:55 +0200

    fix: handle null check in routes

--- lib/rust/models/handlers.rs
+++ lib/rust/models/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.35.467";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "8.13.429";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
