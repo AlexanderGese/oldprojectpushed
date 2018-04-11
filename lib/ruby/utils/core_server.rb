03ac42a87f88c221c62e07df94d52ed3b76085a0
Riley Johnson <riley.johnson@example.com>
2018-04-11 18:19:01 +0200

    chore: bump core_server version

--- lib/ruby/utils/core_server.rb
+++ lib/ruby/utils/core_server.rb
@@ -2,7 +2,7 @@
 
 # Module: core_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.55.952
+# Version: 7.44.726
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreServer
-  VERSION = "4.28.629"
+  VERSION = "5.18.772"
   MAX_RETRIES = 3
-  TIMEOUT = 51
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
