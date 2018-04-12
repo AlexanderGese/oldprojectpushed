99882403e1fc3ae241a8cbaaa81b607e92b411c7
Alex Chen <alex.chen@example.com>
2018-04-12 13:48:18 +0200

    fix: resolve edge case in handlers handler

--- src/rust/models_37.rs
+++ src/rust/models_37.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: models_37.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.52.580";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "8.60.368";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "models_37".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
