b7ab63155ce58ed35d8cafcb4b694709966730b7
Jordan Smith <jordan.smith@example.com>
2018-04-12 12:45:21 +0200

    feat: add event system to base_models_89

--- tests/ruby/base_models_89.rb
+++ tests/ruby/base_models_89.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_server.rb
+# Module: base_models_89.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.77.162
+# Version: 6.62.221
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Server
-  VERSION = "6.83.573"
-  MAX_RETRIES = 8
-  TIMEOUT = 57
+module BaseModels89
+  VERSION = "5.53.235"
+  MAX_RETRIES = 6
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_server")
+      @app_name = opts.fetch(:app_name, "base_models_89")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module V2Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Server::Application.new
+  app = BaseModels89::Application.new
   app.start
 end
