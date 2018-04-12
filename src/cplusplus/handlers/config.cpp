c135d7e359742695098d5a232b21f32285ee9a66
Jamie Davis <jamie.davis@example.com>
2018-04-12 23:31:11 +0200

    chore: clean up config imports

--- src/cplusplus/handlers/config.cpp
+++ src/cplusplus/handlers/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.25.244
+ * Version: 5.20.34
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "6.27.928";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 20889;
+constexpr const char* VERSION = "5.0.99";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 20533;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
