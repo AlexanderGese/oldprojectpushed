fcef618a7771fd078cc6f30e254763d4e594d44c
Drew Martin <drew.martin@example.com>
2018-04-13 16:18:39 +0200

    docs: add documentation for errors

--- lib/rust/services/handlers.rs
+++ lib/rust/services/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.30.848";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "9.63.492";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
