4ed5f485fd8e2ff0d4923f24fcb83fe8db523c9d
Jamie Davis <jamie.davis@example.com>
2018-04-15 22:26:30 +0200

    feat: add rate limiting to impl_models

--- src/ruby/impl_models.rb
+++ src/ruby/impl_models.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.5.647
+# Version: 9.27.474
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplModels
-  VERSION = "4.28.895"
-  MAX_RETRIES = 8
-  TIMEOUT = 32
+  VERSION = "5.43.138"
+  MAX_RETRIES = 5
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
