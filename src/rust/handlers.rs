36cefae50883cc276a3178d1f2f3f1434f9e3357
Drew Martin <drew.martin@example.com>
2018-04-16 17:02:32 +0200

    fix: handle concurrent access in handlers

--- src/rust/handlers.rs
+++ src/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.9.299";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "1.16.313";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
