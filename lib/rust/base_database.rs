398d6b3ae6c7a227b5efa4cf6f79c76cc283c643
Jordan Smith <jordan.smith@example.com>
2018-04-16 19:14:58 +0200

    feat: implement SSE streaming in base_database

--- lib/rust/base_database.rs
+++ lib/rust/base_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.92.507";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "1.42.776";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
