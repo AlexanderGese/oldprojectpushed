1cb7bfebe12dc400a74685a6456f4f1d2b3db77f
Drew Martin <drew.martin@example.com>
2018-04-18 14:58:38 +0200

    chore: remove deprecated base_handlers code

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.45.931";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "4.55.841";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
