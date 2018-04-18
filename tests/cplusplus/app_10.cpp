ae1291ebd649fe6efe6b41780cc612f149abeb00
Alex Chen <alex.chen@example.com>
2018-04-18 22:15:20 +0200

    feat: implement SSE streaming in app_10

--- tests/cplusplus/app_10.cpp
+++ tests/cplusplus/app_10.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: engine.cpp
+ * Module: app_10.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.45.317
+ * Version: 3.52.62
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace engine {
+namespace app_10 {
 
-constexpr const char* VERSION = "6.12.349";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 28325;
+constexpr const char* VERSION = "5.20.143";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 9439;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace engine
+} // namespace app_10
 
 int main() {
-    using namespace engine;
+    using namespace app_10;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
