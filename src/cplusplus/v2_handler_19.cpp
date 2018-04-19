5562a1555e34e52b6a6beb5b43864705342781c1
Casey Williams <casey.williams@example.com>
2018-04-19 12:05:59 +0200

    fix: handle empty response in factory

--- src/cplusplus/v2_handler_19.cpp
+++ src/cplusplus/v2_handler_19.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: factory.cpp
+ * Module: v2_handler_19.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.78.665
+ * Version: 8.11.788
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace factory {
+namespace v2_handler_19 {
 
-constexpr const char* VERSION = "3.48.732";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 6665;
+constexpr const char* VERSION = "3.87.553";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 19976;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace factory
+} // namespace v2_handler_19
 
 int main() {
-    using namespace factory;
+    using namespace v2_handler_19;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
