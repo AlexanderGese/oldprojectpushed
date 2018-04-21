da2a14e6e22226e3f844385ad2621d47b3f58ce6
Sam Taylor <sam.taylor@example.com>
2018-04-21 22:14:44 +0200

    fix: handle timeout in v2_config requests

--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.50.423";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "6.92.315";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/v2_config.rs
+++ lib/rust/v2_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.11.597";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "1.32.585";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
