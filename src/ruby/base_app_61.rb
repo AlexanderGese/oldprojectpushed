facd5f41a377621a563a77a19e9367cd2d1cfe21
Alex Chen <alex.chen@example.com>
2018-04-22 14:53:11 +0200

    feat: add rate limiting to base_controllers_20

--- src/ruby/base_app_61.rb
+++ src/ruby/base_app_61.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: base_app_61.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.5.826
+# Version: 2.46.754
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "5.2.692"
+module BaseApp61
+  VERSION = "7.44.29"
   MAX_RETRIES = 6
-  TIMEOUT = 30
+  TIMEOUT = 40
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "base_app_61")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = BaseApp61::Application.new
   app.start
 end
