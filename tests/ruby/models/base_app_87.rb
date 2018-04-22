83bdd5e9a75bfed44a790fdd90fc42ad903ad408
Drew Martin <drew.martin@example.com>
2018-04-22 13:17:22 +0200

    refactor: simplify models state management

--- tests/ruby/models/base_app_87.rb
+++ tests/ruby/models/base_app_87.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: base_app_87.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.19.946
+# Version: 7.78.940
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "9.64.272"
-  MAX_RETRIES = 8
-  TIMEOUT = 32
+module BaseApp87
+  VERSION = "8.1.117"
+  MAX_RETRIES = 6
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "base_app_87")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = BaseApp87::Application.new
   app.start
 end
