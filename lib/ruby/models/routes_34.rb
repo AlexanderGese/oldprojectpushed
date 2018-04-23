eec1e1125854be8fd2ef73676426ad6499bca8b0
Jordan Smith <jordan.smith@example.com>
2018-04-23 10:39:16 +0200

    chore: bump tasks version

--- lib/ruby/models/routes_34.rb
+++ lib/ruby/models/routes_34.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: routes_34.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.29.560
+# Version: 2.65.29
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "2.72.410"
+module Routes34
+  VERSION = "7.0.782"
   MAX_RETRIES = 4
-  TIMEOUT = 45
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "routes_34")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = Routes34::Application.new
   app.start
 end
