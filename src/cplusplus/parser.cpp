419b74fc314bf9f366c078c203cdf28ef5f122a2
Jamie Davis <jamie.davis@example.com>
2018-04-27 09:11:45 +0200

    feat: add export functionality to factory

--- src/cplusplus/parser.cpp
+++ src/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.13.447
+ * Version: 5.29.490
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "3.74.354";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 24161;
+constexpr const char* VERSION = "4.82.184";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 21601;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
