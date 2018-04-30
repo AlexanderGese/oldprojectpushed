93dbc35606ad8f1a6dbf37470b6a257230b1eefd
Morgan Lee <morgan.lee@example.com>
2018-04-30 19:47:53 +0200

    feat: add middleware support to utils

--- src/cplusplus/models/handler_64.cpp
+++ src/cplusplus/models/handler_64.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: handler_64.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.40.219
+ * Version: 1.72.344
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace handler_64 {
 
-constexpr const char* VERSION = "6.29.170";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 17571;
+constexpr const char* VERSION = "8.88.283";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 11222;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace handler_64
 
 int main() {
-    using namespace utils;
+    using namespace handler_64;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
