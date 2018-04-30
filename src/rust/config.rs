1cf0fd902160ea2b5e5b1cc73785919f01e7fb41
Morgan Lee <morgan.lee@example.com>
2018-04-30 23:18:39 +0200

    refactor: improve enhanced_errors test coverage

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.7.61";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "7.58.89";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/enhanced_errors.rs
+++ src/rust/enhanced_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.89.951";
+const VERSION: &str = "2.57.952";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 46;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
