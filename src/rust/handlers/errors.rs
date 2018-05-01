b6401486a05f514aade3f600a499aa8225fab46e
Sam Taylor <sam.taylor@example.com>
2018-05-01 14:37:28 +0200

    docs: add documentation for errors

--- src/rust/handlers/errors.rs
+++ src/rust/handlers/errors.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.89.862";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "6.48.390";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/services/errors.rs
+++ src/rust/services/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.72.822";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "4.95.584";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
