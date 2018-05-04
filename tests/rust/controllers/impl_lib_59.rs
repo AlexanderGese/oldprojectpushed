89ea9b40599bb040d5d5fc73e1d11b5fc726eeee
Drew Martin <drew.martin@example.com>
2018-05-04 15:54:47 +0200

    fix: handle empty response in impl_lib_59

--- tests/rust/controllers/impl_lib_59.rs
+++ tests/rust/controllers/impl_lib_59.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: impl_lib_59.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.32.132";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "1.37.804";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "impl_lib_59".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.91.326";
+const VERSION: &str = "5.5.860";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 28;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
