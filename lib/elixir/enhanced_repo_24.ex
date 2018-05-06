efac487c19c77e26fec1ec14b940339fae609cfa
Sam Taylor <sam.taylor@example.com>
2018-05-06 12:16:11 +0200

    refactor: simplify impl_worker state management

--- lib/elixir/enhanced_repo_24.ex
+++ lib/elixir/enhanced_repo_24.ex
@@ -1,13 +1,13 @@
-defmodule Impl_Worker do
+defmodule Enhanced_Repo_24 do
   @moduledoc """
-  Module: impl_worker.ex
+  Module: enhanced_repo_24.ex
   Elixir boilerplate - auto-generated
-  Version: 1.51.352
+  Version: 4.56.749
   """
 
-  @version "6.30.1"
-  @max_retries 5
-  @timeout 11063
+  @version "3.1.630"
+  @max_retries 3
+  @timeout 13411
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Impl_Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "impl_worker"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_repo_24"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Impl_Worker do
   end
 end
 
-defmodule Impl_WorkerStore do
+defmodule Enhanced_Repo_24Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Impl_WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Impl_WorkerEventBus do
+defmodule Enhanced_Repo_24EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
