cf35d450784857c0fce9be9f36dd799905cf4842
Sam Taylor <sam.taylor@example.com>
2018-05-06 12:10:35 +0200

    refactor: improve controllers error messages

--- lib/ruby/v2_server.rb
+++ lib/ruby/v2_server.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.35.605
+# Version: 5.67.203
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Server
-  VERSION = "6.99.616"
-  MAX_RETRIES = 6
-  TIMEOUT = 29
+  VERSION = "7.64.784"
+  MAX_RETRIES = 3
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
