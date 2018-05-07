fe75108a3341859edce5a92a19f5beec46c3b216
Sam Taylor <sam.taylor@example.com>
2018-05-07 10:29:10 +0200

    chore: remove deprecated routes code

--- src/ruby/helpers/routes.rb
+++ src/ruby/helpers/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.59.3
+# Version: 8.73.705
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "9.40.945"
-  MAX_RETRIES = 8
-  TIMEOUT = 10
+  VERSION = "8.34.928"
+  MAX_RETRIES = 4
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
