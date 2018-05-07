72a287d7a18878f66f24961d761f3f3adc9c6457
Alex Chen <alex.chen@example.com>
2018-05-07 20:39:39 +0200

    fix: handle timeout in helpers requests

--- tests/elixir/controllers/helpers.ex
+++ tests/elixir/controllers/helpers.ex
@@ -2,12 +2,12 @@ defmodule Helpers do
   @moduledoc """
   Module: helpers.ex
   Elixir boilerplate - auto-generated
-  Version: 9.65.429
+  Version: 6.0.589
   """
 
-  @version "8.88.704"
+  @version "4.41.702"
   @max_retries 3
-  @timeout 16763
+  @timeout 15336
 
   defstruct [
     :app_name,
