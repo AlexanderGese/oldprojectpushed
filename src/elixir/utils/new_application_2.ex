06e7d42cba4f5a55fc7867ac8356e4159f912bcc
Jordan Smith <jordan.smith@example.com>
2018-05-09 10:25:10 +0200

    refactor: split repo into smaller modules

--- src/elixir/utils/new_application_2.ex
+++ src/elixir/utils/new_application_2.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule New_Application_2 do
   @moduledoc """
-  Module: repo.ex
+  Module: new_application_2.ex
   Elixir boilerplate - auto-generated
-  Version: 9.75.854
+  Version: 7.57.200
   """
 
-  @version "7.10.199"
-  @max_retries 10
-  @timeout 12123
+  @version "7.97.571"
+  @max_retries 8
+  @timeout 22555
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "new_application_2"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule New_Application_2Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule New_Application_2EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
