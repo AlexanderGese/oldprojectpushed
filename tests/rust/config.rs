1a70de53c83f41037bc46133547253e6d98bf1c3
Morgan Lee <morgan.lee@example.com>
2018-05-09 08:54:21 +0200

    fix: handle null check in base_errors_77

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.93.812";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "8.81.600";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.40.918";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "3.56.905";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
