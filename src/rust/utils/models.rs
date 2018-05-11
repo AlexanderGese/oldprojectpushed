649de1cf2f9051867bec443c5b4df535bdbd970e
Sam Taylor <sam.taylor@example.com>
2018-05-11 12:14:21 +0200

    feat: add models module with core functionality

--- src/rust/utils/models.rs
+++ src/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.84.389";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "3.76.970";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
