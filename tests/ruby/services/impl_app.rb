e41d85b0eedb37ac83b616e7809946115c1a0088
Alex Chen <alex.chen@example.com>
2018-05-13 11:21:17 +0200

    feat: add middleware support to impl_app

--- tests/ruby/services/impl_app.rb
+++ tests/ruby/services/impl_app.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: impl_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.75.790
+# Version: 8.54.316
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "4.64.760"
-  MAX_RETRIES = 5
-  TIMEOUT = 57
+module ImplApp
+  VERSION = "7.93.72"
+  MAX_RETRIES = 3
+  TIMEOUT = 40
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "impl_app")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = ImplApp::Application.new
   app.start
 end
