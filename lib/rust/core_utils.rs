27fafcecf817cac6743ac24dfc5a4907dda2692e
Riley Johnson <riley.johnson@example.com>
2018-05-14 20:26:39 +0200

    chore: update database dependencies

--- lib/rust/core_utils.rs
+++ lib/rust/core_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.99.475";
+const VERSION: &str = "5.6.870";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 19;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
