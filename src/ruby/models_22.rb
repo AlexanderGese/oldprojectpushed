61f2e61625d85c7600f440a165c3da04a6a93cf8
Alex Chen <alex.chen@example.com>
2018-05-14 14:02:42 +0200

    feat: implement WebSocket support in models_22

--- src/ruby/models_22.rb
+++ src/ruby/models_22.rb
@@ -2,7 +2,7 @@
 
 # Module: models_22.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.71.3
+# Version: 9.91.426
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models22
-  VERSION = "2.71.669"
+  VERSION = "8.87.269"
   MAX_RETRIES = 6
-  TIMEOUT = 23
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models22
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
