900725d2066ea41b042675a37d462053dfdca12c
Drew Martin <drew.martin@example.com>
2018-05-16 11:06:43 +0200

    docs: add documentation for database

--- lib/rust/database.rs
+++ lib/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.75.752";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "1.66.999";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
