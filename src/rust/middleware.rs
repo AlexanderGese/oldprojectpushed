db010eac145105c5cf1393d62cbc59dd149646e5
Riley Johnson <riley.johnson@example.com>
2018-05-16 18:32:27 +0200

    test: add unit tests for middleware

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.89.862";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "3.29.897";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
