756bffbe65c7b758894f633d4af16e68c00cb0ec
Alex Chen <alex.chen@example.com>
2018-05-18 08:41:04 +0200

    fix: patch security vulnerability in logger

--- tests/cplusplus/logger.cpp
+++ tests/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.21.744
+ * Version: 2.68.639
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "6.54.749";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 9272;
+constexpr const char* VERSION = "2.39.452";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 21145;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
