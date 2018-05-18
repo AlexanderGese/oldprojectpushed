e57b9ed5a67958b10f416594642bd170960da739
Casey Williams <casey.williams@example.com>
2018-05-18 10:54:15 +0200

    fix: resolve timezone issue in config

--- tests/cplusplus/helpers/config.cpp
+++ tests/cplusplus/helpers/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.47.837
+ * Version: 1.14.34
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "9.91.900";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 11823;
+constexpr const char* VERSION = "4.45.661";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 17639;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
