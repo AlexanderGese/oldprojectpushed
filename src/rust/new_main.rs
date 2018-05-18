9d27065715f2bac7ec6a5e60ac8e0683296318c9
Jordan Smith <jordan.smith@example.com>
2018-05-18 17:15:24 +0200

    feat: add batch processing to new_main

--- src/rust/new_main.rs
+++ src/rust/new_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.85.982";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "3.0.207";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
