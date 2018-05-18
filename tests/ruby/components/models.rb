28a55f1e90a886289771be57f43e8073db34a638
Drew Martin <drew.martin@example.com>
2018-05-18 11:02:42 +0200

    refactor: migrate models to async/await

--- tests/ruby/components/models.rb
+++ tests/ruby/components/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.63.409
+# Version: 5.28.704
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "4.94.575"
+  VERSION = "1.97.371"
   MAX_RETRIES = 10
-  TIMEOUT = 22
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
