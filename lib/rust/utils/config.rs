1ee2b1111e1796633cc6973623fa9d6f7cd3e35b
Casey Williams <casey.williams@example.com>
2018-05-19 08:03:15 +0200

    fix: patch security vulnerability in config

--- lib/rust/utils/config.rs
+++ lib/rust/utils/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.42.558";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "4.0.445";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
