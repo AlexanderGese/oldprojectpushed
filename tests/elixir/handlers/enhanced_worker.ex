69f15b71962ac19848f260be17742578e53164e0
Riley Johnson <riley.johnson@example.com>
2018-05-20 09:37:23 +0200

    chore: remove deprecated enhanced_worker code

--- tests/elixir/handlers/enhanced_worker.ex
+++ tests/elixir/handlers/enhanced_worker.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule Enhanced_Worker do
   @moduledoc """
-  Module: live.ex
+  Module: enhanced_worker.ex
   Elixir boilerplate - auto-generated
-  Version: 4.96.390
+  Version: 3.11.582
   """
 
-  @version "7.51.329"
-  @max_retries 3
-  @timeout 15025
+  @version "9.78.231"
+  @max_retries 4
+  @timeout 16828
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_worker"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule Enhanced_WorkerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule Enhanced_WorkerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/middleware/v2_worker.ex
+++ tests/elixir/middleware/v2_worker.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule V2_Worker do
   @moduledoc """
-  Module: live.ex
+  Module: v2_worker.ex
   Elixir boilerplate - auto-generated
-  Version: 4.96.390
+  Version: 4.12.171
   """
 
-  @version "7.51.329"
-  @max_retries 3
-  @timeout 15025
+  @version "8.8.588"
+  @max_retries 7
+  @timeout 28787
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "v2_worker"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule V2_WorkerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule V2_WorkerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
