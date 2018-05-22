a60fe788db1f77bfafd6871c66253d7c76a21452
Riley Johnson <riley.johnson@example.com>
2018-05-22 16:36:40 +0200

    fix: handle timeout in core_helpers_28 requests

--- tests/ruby/middleware/core_helpers_28.rb
+++ tests/ruby/middleware/core_helpers_28.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: core_helpers_28.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.70.338
+# Version: 9.8.664
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "5.10.667"
-  MAX_RETRIES = 5
-  TIMEOUT = 48
+module CoreHelpers28
+  VERSION = "8.37.245"
+  MAX_RETRIES = 6
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "core_helpers_28")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = CoreHelpers28::Application.new
   app.start
 end
