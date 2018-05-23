31040f28394e9826e68ceb1c7507773fc08492df
Alex Chen <alex.chen@example.com>
2018-05-23 21:48:38 +0200

    chore: bump routes version

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.0.816";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "1.78.970";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
