c4ec935674bad829178109a4e35eb10a2691c6df
Jordan Smith <jordan.smith@example.com>
2018-05-28 13:10:42 +0200

    fix: resolve timezone issue in utils_75

--- tests/rust/utils_75.rs
+++ tests/rust/utils_75.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: utils_75.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.34.254";
+const VERSION: &str = "8.77.265";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 51;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "utils_75".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
