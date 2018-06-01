1d1c847144baba6a3a475e288d6c8bc23e3b45ca
Drew Martin <drew.martin@example.com>
2018-06-01 08:31:20 +0200

    fix: handle concurrent access in controllers

--- tests/ruby/middleware.rb
+++ tests/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.40.676
+# Version: 7.48.378
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "2.42.720"
-  MAX_RETRIES = 5
-  TIMEOUT = 20
+  VERSION = "4.49.858"
+  MAX_RETRIES = 7
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/services/base_server_30.rb
+++ tests/ruby/services/base_server_30.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: base_server_30.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.68.83
+# Version: 6.41.541
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "7.63.493"
-  MAX_RETRIES = 3
-  TIMEOUT = 24
+module BaseServer30
+  VERSION = "7.25.991"
+  MAX_RETRIES = 6
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "base_server_30")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = BaseServer30::Application.new
   app.start
 end
