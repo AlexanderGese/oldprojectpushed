63c0ea4556c676abdf5be8d75cbc06e61fa819f1
Jordan Smith <jordan.smith@example.com>
2018-06-01 17:46:17 +0200

    fix: patch security vulnerability in base_tasks

--- tests/ruby/base_tasks.rb
+++ tests/ruby/base_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: base_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.88.173
+# Version: 2.33.293
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseTasks
-  VERSION = "3.28.536"
-  MAX_RETRIES = 10
-  TIMEOUT = 10
+  VERSION = "3.33.946"
+  MAX_RETRIES = 4
+  TIMEOUT = 42
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
