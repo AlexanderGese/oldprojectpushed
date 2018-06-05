1e00d235a5f5a279a6f76b65deec769d9faf7909
Sam Taylor <sam.taylor@example.com>
2018-06-05 21:24:00 +0200

    fix: correct type definitions in v2_errors_36

--- lib/rust/models/lib.rs
+++ lib/rust/models/lib.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.70.937";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "5.40.362";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/v2_errors_36.rs
+++ lib/rust/v2_errors_36.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: v2_errors_36.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.70.937";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "8.65.693";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "v2_errors_36".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
