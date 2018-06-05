bf7dbd2fd501aaf5896bad6099f9b4c3e7820b9b
Sam Taylor <sam.taylor@example.com>
2018-06-05 20:06:38 +0200

    feat: add rate limiting to utils_28

--- tests/cplusplus/utils_28.cpp
+++ tests/cplusplus/utils_28.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: factory.cpp
+ * Module: utils_28.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.34.251
+ * Version: 7.22.696
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace factory {
+namespace utils_28 {
 
-constexpr const char* VERSION = "2.67.31";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 18985;
+constexpr const char* VERSION = "8.98.339";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 26878;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace factory
+} // namespace utils_28
 
 int main() {
-    using namespace factory;
+    using namespace utils_28;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
