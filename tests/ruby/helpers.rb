b44d795250143363af5a7cdac5623b7711b4cd3d
Alex Chen <alex.chen@example.com>
2018-06-07 18:47:25 +0200

    test: add unit tests for helpers

--- tests/ruby/helpers.rb
+++ tests/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.79.526
+# Version: 6.9.942
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "4.97.515"
-  MAX_RETRIES = 3
-  TIMEOUT = 18
+  VERSION = "3.85.838"
+  MAX_RETRIES = 6
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
--- tests/ruby/tasks_3.rb
+++ tests/ruby/tasks_3.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_config.rb
+# Module: tasks_3.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.19.198
+# Version: 8.48.777
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewConfig
-  VERSION = "2.8.120"
-  MAX_RETRIES = 7
-  TIMEOUT = 24
+module Tasks3
+  VERSION = "8.10.948"
+  MAX_RETRIES = 10
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_config")
+      @app_name = opts.fetch(:app_name, "tasks_3")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewConfig
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewConfig::Application.new
+  app = Tasks3::Application.new
   app.start
 end
