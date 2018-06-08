31be17bc78cb66e680815c20b3638771ec0ad609
Riley Johnson <riley.johnson@example.com>
2018-06-08 15:16:04 +0200

    refactor: improve helpers test coverage

--- tests/elixir/impl_router_55.ex
+++ tests/elixir/impl_router_55.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Impl_Router_55 do
   @moduledoc """
-  Module: helpers.ex
+  Module: impl_router_55.ex
   Elixir boilerplate - auto-generated
-  Version: 4.16.137
+  Version: 1.80.713
   """
 
-  @version "1.33.157"
-  @max_retries 9
-  @timeout 18345
+  @version "5.99.861"
+  @max_retries 7
+  @timeout 9790
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "impl_router_55"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule Impl_Router_55Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule Impl_Router_55EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
