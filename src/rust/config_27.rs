44422228881104b85a6229703271b38adcc3981e
Sam Taylor <sam.taylor@example.com>
2018-06-08 09:10:41 +0200

    feat: add validation to handlers inputs

--- src/rust/config_27.rs
+++ src/rust/config_27.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: config_27.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.62.980";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "1.1.160";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "config_27".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
