50a8f915f3813b5a779afd61c0d5e6e473242482
Jordan Smith <jordan.smith@example.com>
2018-06-09 14:58:55 +0200

    feat: add rate limiting to handlers

--- src/rust/utils/handlers.rs
+++ src/rust/utils/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.91.651";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "2.0.100";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
