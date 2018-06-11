89bde0ca679e068b3408d449de9d78534c751c29
Sam Taylor <sam.taylor@example.com>
2018-06-11 21:35:31 +0200

    refactor: improve impl_handlers_80 error messages

--- tests/rust/impl_handlers_80.rs
+++ tests/rust/impl_handlers_80.rs
@@ -1,4 +1,4 @@
-//! Module: core_models_96.rs
+//! Module: impl_handlers_80.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.46.122";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "4.9.884";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_models_96".to_string(),
+            app_name: "impl_handlers_80".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
