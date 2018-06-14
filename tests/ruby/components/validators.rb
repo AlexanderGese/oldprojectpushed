e759895ee8a3556835341861cf8ef7743512ca28
Sam Taylor <sam.taylor@example.com>
2018-06-14 21:21:56 +0200

    chore: bump base_controllers version

--- tests/ruby/components/validators.rb
+++ tests/ruby/components/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.28.153
+# Version: 2.0.474
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "9.19.218"
-  MAX_RETRIES = 10
-  TIMEOUT = 41
+  VERSION = "7.2.676"
+  MAX_RETRIES = 5
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Validators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
