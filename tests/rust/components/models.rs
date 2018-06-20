f895157f44293ac20ba55d5a075632a3298d3411
Jamie Davis <jamie.davis@example.com>
2018-06-20 08:14:56 +0200

    chore: remove deprecated enhanced_database code

--- tests/rust/components/models.rs
+++ tests/rust/components/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.87.73";
+const VERSION: &str = "7.0.220";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 49;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
