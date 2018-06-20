f6a2db880075ad5beab8bb2f3f1e3030fdbfebdc
Jordan Smith <jordan.smith@example.com>
2018-06-20 19:37:17 +0200

    feat: add logging to impl_app

--- src/ruby/impl_app.rb
+++ src/ruby/impl_app.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: impl_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.10.43
+# Version: 1.71.140
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "7.82.134"
-  MAX_RETRIES = 5
-  TIMEOUT = 34
+module ImplApp
+  VERSION = "8.0.409"
+  MAX_RETRIES = 3
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "impl_app")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = ImplApp::Application.new
   app.start
 end
