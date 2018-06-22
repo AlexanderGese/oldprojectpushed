dbb49c5ddaf2d1b979c04e5ec1218558e576f75c
Jamie Davis <jamie.davis@example.com>
2018-06-22 23:17:00 +0200

    refactor: improve core_app error messages

--- tests/ruby/core_app.rb
+++ tests/ruby/core_app.rb
@@ -2,7 +2,7 @@
 
 # Module: core_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.31.482
+# Version: 2.86.504
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreApp
-  VERSION = "4.32.948"
-  MAX_RETRIES = 6
-  TIMEOUT = 48
+  VERSION = "3.31.340"
+  MAX_RETRIES = 5
+  TIMEOUT = 27
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreApp
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
