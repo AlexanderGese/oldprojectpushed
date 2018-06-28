0c097af4e40f6864a6bd6c3d0ffbc5ac74bdf10b
Jordan Smith <jordan.smith@example.com>
2018-06-28 21:29:19 +0200

    chore: add linting rules for config

--- tests/rust/components/models.rs
+++ tests/rust/components/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.0.220";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "4.45.189";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
