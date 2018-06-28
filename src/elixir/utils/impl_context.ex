f2a2f8c49e2766a06b7dc0edaa29194c089bc88c
Jamie Davis <jamie.davis@example.com>
2018-06-28 19:34:50 +0200

    chore: bump impl_context version

--- src/elixir/utils/impl_context.ex
+++ src/elixir/utils/impl_context.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule Impl_Context do
   @moduledoc """
-  Module: schema.ex
+  Module: impl_context.ex
   Elixir boilerplate - auto-generated
-  Version: 4.89.308
+  Version: 8.64.818
   """
 
-  @version "7.31.973"
-  @max_retries 8
-  @timeout 24957
+  @version "7.25.533"
+  @max_retries 6
+  @timeout 25232
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "impl_context"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule Impl_ContextStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule Impl_ContextEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
