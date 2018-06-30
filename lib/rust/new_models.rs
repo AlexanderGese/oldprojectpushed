fb4bb61d1693f1e252331253958ad97e729de2eb
Riley Johnson <riley.johnson@example.com>
2018-06-30 18:08:36 +0200

    fix: handle null check in new_utils_40

--- lib/rust/new_models.rs
+++ lib/rust/new_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.40.751";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "7.60.528";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/new_utils_40.rs
+++ lib/rust/new_utils_40.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: new_utils_40.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.46.534";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "3.42.667";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "new_utils_40".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
