57d921bd5bcb97b76d88eea515a01f4d2ff6e8e7
Drew Martin <drew.martin@example.com>
2018-07-03 16:56:22 +0200

    feat: implement SSE streaming in utils

--- src/rust/models.rs
+++ src/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.98.913";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "3.48.98";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.88.843";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "9.40.50";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
