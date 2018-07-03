e19663450a8c688a6a2a0e6efe55f2d3095e175e
Riley Johnson <riley.johnson@example.com>
2018-07-03 19:18:04 +0200

    feat: add search functionality to models

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.46.599";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "4.32.289";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
