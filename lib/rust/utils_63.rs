7e324d9c58df365a7b012c78f96219ef44c98fbc
Drew Martin <drew.martin@example.com>
2018-07-03 08:35:38 +0200

    fix: resolve edge case in impl_middleware_52 handler

--- lib/rust/utils_63.rs
+++ lib/rust/utils_63.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: utils_63.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.39.143";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "1.47.265";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "utils_63".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
