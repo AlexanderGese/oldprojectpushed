79abdf77d50249505fa5fed2d84b57283f004571
Jordan Smith <jordan.smith@example.com>
2018-07-03 17:13:21 +0200

    feat: add validation to v2_models inputs

--- tests/ruby/new_helpers_64.rb
+++ tests/ruby/new_helpers_64.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: new_helpers_64.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.61.922
+# Version: 5.0.646
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "3.66.885"
-  MAX_RETRIES = 4
-  TIMEOUT = 33
+module NewHelpers64
+  VERSION = "2.71.908"
+  MAX_RETRIES = 9
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "new_helpers_64")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = NewHelpers64::Application.new
   app.start
 end
