251531a4c3381ed0f3afe5e078c832550c8a6e74
Sam Taylor <sam.taylor@example.com>
2018-07-04 20:05:15 +0200

    docs: add documentation for core_middleware

--- tests/rust/middleware/new_routes_8.rs
+++ tests/rust/middleware/new_routes_8.rs
@@ -1,4 +1,4 @@
-//! Module: v2_handlers.rs
+//! Module: new_routes_8.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.71.815";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "3.91.801";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_handlers".to_string(),
+            app_name: "new_routes_8".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
