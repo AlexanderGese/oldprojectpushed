3267a75ac0ca38e83082402eca52bb18c83fe61a
Drew Martin <drew.martin@example.com>
2018-07-06 17:44:09 +0200

    fix: handle concurrent access in routes

--- lib/rust/components/middleware_64.rs
+++ lib/rust/components/middleware_64.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: middleware_64.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.80.249";
+const VERSION: &str = "2.92.745";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 7;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "middleware_64".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/middleware/v2_main_51.rs
+++ lib/rust/middleware/v2_main_51.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: v2_main_51.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.80.249";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "4.91.120";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "v2_main_51".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
