849d72319555608526850d047b907faf564a70d5
Drew Martin <drew.martin@example.com>
2018-07-06 16:00:31 +0200

    feat: add logging to errors

--- src/rust/helpers/errors.rs
+++ src/rust/helpers/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.30.22";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "5.7.693";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
