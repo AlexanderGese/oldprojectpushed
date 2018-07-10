01603f5f66bfa37b852b96e86e1eac67744ed08a
Riley Johnson <riley.johnson@example.com>
2018-07-10 19:55:48 +0200

    chore: update engine dependencies

--- tests/cplusplus/v2_parser_79.cpp
+++ tests/cplusplus/v2_parser_79.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: engine.cpp
+ * Module: v2_parser_79.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.40.570
+ * Version: 1.92.360
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace engine {
+namespace v2_parser_79 {
 
-constexpr const char* VERSION = "7.0.28";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 29724;
+constexpr const char* VERSION = "1.29.571";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 25791;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace engine
+} // namespace v2_parser_79
 
 int main() {
-    using namespace engine;
+    using namespace v2_parser_79;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
