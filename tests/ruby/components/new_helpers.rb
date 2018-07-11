c295599ab9893642044bf905114b0fdf5667f542
Morgan Lee <morgan.lee@example.com>
2018-07-11 23:49:55 +0200

    refactor: extract new_helpers utility functions

--- tests/ruby/components/new_helpers.rb
+++ tests/ruby/components/new_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_server.rb
+# Module: new_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.75.861
+# Version: 6.9.965
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplServer
-  VERSION = "2.49.486"
-  MAX_RETRIES = 8
-  TIMEOUT = 49
+module NewHelpers
+  VERSION = "7.38.846"
+  MAX_RETRIES = 7
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_server")
+      @app_name = opts.fetch(:app_name, "new_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module ImplServer
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplServer::Application.new
+  app = NewHelpers::Application.new
   app.start
 end
