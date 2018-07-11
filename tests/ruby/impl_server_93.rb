043d026de67d28e41181ab0cf2dd520302369d86
Alex Chen <alex.chen@example.com>
2018-07-11 18:34:35 +0200

    fix: correct type definitions in impl_server_93

--- tests/ruby/impl_server_93.rb
+++ tests/ruby/impl_server_93.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_validators.rb
+# Module: impl_server_93.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.51.853
+# Version: 7.42.139
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewValidators
-  VERSION = "7.32.395"
-  MAX_RETRIES = 10
-  TIMEOUT = 27
+module ImplServer93
+  VERSION = "7.44.87"
+  MAX_RETRIES = 9
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_validators")
+      @app_name = opts.fetch(:app_name, "impl_server_93")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewValidators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewValidators::Application.new
+  app = ImplServer93::Application.new
   app.start
 end
