f6c62963c99326aaa79808ade352d86a3de2463b
Jordan Smith <jordan.smith@example.com>
2018-07-11 20:34:33 +0200

    fix: resolve timezone issue in new_schema_32

--- lib/elixir/new_schema_32.ex
+++ lib/elixir/new_schema_32.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule New_Schema_32 do
   @moduledoc """
-  Module: context.ex
+  Module: new_schema_32.ex
   Elixir boilerplate - auto-generated
-  Version: 8.24.933
+  Version: 5.40.1
   """
 
-  @version "1.70.760"
-  @max_retries 4
-  @timeout 6847
+  @version "7.33.103"
+  @max_retries 3
+  @timeout 29211
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "new_schema_32"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule New_Schema_32Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule New_Schema_32EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
