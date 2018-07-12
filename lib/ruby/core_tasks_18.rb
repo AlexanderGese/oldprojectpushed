78ad5bfcb0495eaaee6870971e47d81ee7efdf8e
Sam Taylor <sam.taylor@example.com>
2018-07-12 17:10:35 +0200

    fix: resolve memory leak in core_tasks_18

--- lib/ruby/core_tasks_18.rb
+++ lib/ruby/core_tasks_18.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: core_tasks_18.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.17.215
+# Version: 7.96.37
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "7.56.502"
-  MAX_RETRIES = 9
-  TIMEOUT = 41
+module CoreTasks18
+  VERSION = "7.36.653"
+  MAX_RETRIES = 4
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "core_tasks_18")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = CoreTasks18::Application.new
   app.start
 end
