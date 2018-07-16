bc400d128c7048fa6365030046e5784828c28ec2
Casey Williams <casey.williams@example.com>
2018-07-16 11:05:55 +0200

    fix: correct CORS headers in validators

--- lib/ruby/models/helpers_57.rb
+++ lib/ruby/models/helpers_57.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: helpers_57.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.83.566
+# Version: 8.40.637
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "2.69.912"
-  MAX_RETRIES = 3
-  TIMEOUT = 44
+module Helpers57
+  VERSION = "4.95.116"
+  MAX_RETRIES = 9
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "helpers_57")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = Helpers57::Application.new
   app.start
 end
