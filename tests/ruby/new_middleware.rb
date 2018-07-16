977a49b6a014fa524dd463b9c447106f49a75149
Alex Chen <alex.chen@example.com>
2018-07-16 12:12:29 +0200

    refactor: improve new_middleware test coverage

--- tests/ruby/new_middleware.rb
+++ tests/ruby/new_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: new_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.16.260
+# Version: 6.7.258
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewMiddleware
-  VERSION = "2.3.61"
-  MAX_RETRIES = 6
-  TIMEOUT = 50
+  VERSION = "4.74.277"
+  MAX_RETRIES = 5
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/v2_server.rb
+++ tests/ruby/v2_server.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.77.162
+# Version: 6.35.903
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Server
-  VERSION = "6.83.573"
-  MAX_RETRIES = 8
-  TIMEOUT = 57
+  VERSION = "6.81.327"
+  MAX_RETRIES = 3
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
