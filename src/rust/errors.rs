dd19de4f396d640a5a2c08b9273cc50a2d775a01
Sam Taylor <sam.taylor@example.com>
2018-07-18 19:27:31 +0200

    chore: configure CI/CD for errors

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.85.571";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "9.19.568";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/main.rs
+++ src/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.55.841";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "4.28.4";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
