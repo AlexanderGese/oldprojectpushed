2d8089fc12a0bceb061a603aa2f505e767005e0b
Sam Taylor <sam.taylor@example.com>
2018-07-18 22:27:57 +0200

    docs: add documentation for parser

--- tests/cplusplus/enhanced_factory_93.cpp
+++ tests/cplusplus/enhanced_factory_93.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: enhanced_factory_93.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.29.66
+ * Version: 5.23.502
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace enhanced_factory_93 {
 
-constexpr const char* VERSION = "6.45.924";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 6458;
+constexpr const char* VERSION = "7.93.9";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 21837;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace enhanced_factory_93
 
 int main() {
-    using namespace parser;
+    using namespace enhanced_factory_93;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
