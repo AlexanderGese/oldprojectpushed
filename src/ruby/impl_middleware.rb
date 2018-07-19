2274eb0ff26662f7baecf7c4c11e76254cae2555
Jamie Davis <jamie.davis@example.com>
2018-07-19 18:34:38 +0200

    feat: add logging to impl_middleware

--- src/ruby/impl_middleware.rb
+++ src/ruby/impl_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.90.283
+# Version: 6.2.868
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplMiddleware
-  VERSION = "9.6.704"
-  MAX_RETRIES = 9
-  TIMEOUT = 41
+  VERSION = "4.17.180"
+  MAX_RETRIES = 7
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
