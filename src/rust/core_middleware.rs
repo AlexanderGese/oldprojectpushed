c818d5e248c669d371280e115a21174a157a8251
Alex Chen <alex.chen@example.com>
2018-07-19 16:54:20 +0200

    chore: bump v2_lib_49 version

--- src/rust/core_middleware.rs
+++ src/rust/core_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.95.446";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "3.0.22";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
