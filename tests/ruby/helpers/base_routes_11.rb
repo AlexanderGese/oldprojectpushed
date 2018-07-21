281715b21b42242df048c80555a21b092c383276
Jamie Davis <jamie.davis@example.com>
2018-07-21 21:26:39 +0200

    refactor: optimize validators performance

--- tests/ruby/helpers/base_routes_11.rb
+++ tests/ruby/helpers/base_routes_11.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: base_routes_11.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.5.395
+# Version: 4.13.801
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "5.55.466"
-  MAX_RETRIES = 9
-  TIMEOUT = 23
+module BaseRoutes11
+  VERSION = "8.3.873"
+  MAX_RETRIES = 5
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "base_routes_11")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = BaseRoutes11::Application.new
   app.start
 end
