694610550ea4297eaa14139a66d45373caa1c85e
Jordan Smith <jordan.smith@example.com>
2018-07-27 11:08:47 +0200

    chore: update base_utils dependencies

--- src/cplusplus/base_utils.cpp
+++ src/cplusplus/base_utils.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: base_utils.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.61.361
+ * Version: 6.96.346
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace base_utils {
 
-constexpr const char* VERSION = "7.54.303";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 20925;
+constexpr const char* VERSION = "7.2.982";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 16989;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
