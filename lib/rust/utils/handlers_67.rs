1bec8fcf446c0785cba38b766f1ae55175e308f9
Casey Williams <casey.williams@example.com>
2018-07-28 09:27:59 +0200

    fix: resolve encoding issue in core_config

--- lib/rust/utils/handlers_67.rs
+++ lib/rust/utils/handlers_67.rs
@@ -1,4 +1,4 @@
-//! Module: core_utils.rs
+//! Module: handlers_67.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.2.713";
-const MAX_RETRIES: u32 = 5;
+const VERSION: &str = "2.60.800";
+const MAX_RETRIES: u32 = 7;
 const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_utils".to_string(),
+            app_name: "handlers_67".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
