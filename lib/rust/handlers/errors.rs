f2a86830b0023a92f9815e457725ff2c24f643b5
Morgan Lee <morgan.lee@example.com>
2018-07-29 16:13:11 +0200

    feat: implement SSE streaming in errors

--- lib/rust/handlers/errors.rs
+++ lib/rust/handlers/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.44.43";
+const VERSION: &str = "2.58.286";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 6;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils/handlers.rs
+++ lib/rust/utils/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.2.911";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "8.67.231";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
