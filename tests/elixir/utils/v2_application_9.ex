f7246aad4c47f745431ad3c66427a3a796671770
Drew Martin <drew.martin@example.com>
2018-07-29 15:47:50 +0200

    fix: patch security vulnerability in enhanced_context

--- tests/elixir/utils/v2_application_9.ex
+++ tests/elixir/utils/v2_application_9.ex
@@ -1,13 +1,13 @@
-defmodule Enhanced_Context do
+defmodule V2_Application_9 do
   @moduledoc """
-  Module: enhanced_context.ex
+  Module: v2_application_9.ex
   Elixir boilerplate - auto-generated
-  Version: 5.75.990
+  Version: 8.51.753
   """
 
-  @version "2.21.249"
-  @max_retries 4
-  @timeout 7747
+  @version "3.32.699"
+  @max_retries 7
+  @timeout 19297
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Enhanced_Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "enhanced_context"),
+      app_name: Keyword.get(opts, :app_name, "v2_application_9"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Enhanced_Context do
   end
 end
 
-defmodule Enhanced_ContextStore do
+defmodule V2_Application_9Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Enhanced_ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Enhanced_ContextEventBus do
+defmodule V2_Application_9EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
