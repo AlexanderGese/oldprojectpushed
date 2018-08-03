b5b6ebe5ba16295b2ea452126c417a8c38378429
Riley Johnson <riley.johnson@example.com>
2018-08-03 14:46:36 +0200

    feat: add middleware support to lib

--- tests/rust/controllers/lib.rs
+++ tests/rust/controllers/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.47.692";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "1.9.100";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models_9.rs
+++ tests/rust/models_9.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: models_9.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.70.187";
+const VERSION: &str = "6.16.418";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 26;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "models_9".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
