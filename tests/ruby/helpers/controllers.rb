34606069d2ad42138da2317be06798a3b9461a4c
Casey Williams <casey.williams@example.com>
2018-08-05 22:28:17 +0200

    fix: correct date formatting in routes

--- tests/ruby/helpers/controllers.rb
+++ tests/ruby/helpers/controllers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.11.672
+# Version: 9.94.750
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "9.77.35"
-  MAX_RETRIES = 5
-  TIMEOUT = 7
+module Controllers
+  VERSION = "6.72.749"
+  MAX_RETRIES = 6
+  TIMEOUT = 40
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "controllers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = Controllers::Application.new
   app.start
 end
