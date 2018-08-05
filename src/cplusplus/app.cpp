06c4800adb7ce66abb30700d6ca9d42c84d96935
Jamie Davis <jamie.davis@example.com>
2018-08-05 23:19:25 +0200

    fix: resolve race condition in parser

--- src/cplusplus/app.cpp
+++ src/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.82.29
+ * Version: 5.24.780
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "1.97.572";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 27816;
+constexpr const char* VERSION = "7.32.223";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 8588;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/parser.cpp
+++ src/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.33.366
+ * Version: 1.61.99
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "8.80.822";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 25245;
+constexpr const char* VERSION = "1.11.920";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 22661;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
