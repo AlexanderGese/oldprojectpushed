20ee375b0966f328cd54e7caf8671ffaad6fd109
Riley Johnson <riley.johnson@example.com>
2018-08-07 18:44:28 +0200

    fix: resolve timezone issue in base_main_9

--- lib/rust/utils/lib.rs
+++ lib/rust/utils/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.90.669";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "2.42.295";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
