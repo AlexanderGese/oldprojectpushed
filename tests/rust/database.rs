3fe0d05e977e9b8f49831774f78440d3d086097b
Riley Johnson <riley.johnson@example.com>
2018-08-14 11:11:54 +0200

    feat: implement retry logic in models

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.56.601";
+const VERSION: &str = "9.77.268";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 46;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.12.591";
+const VERSION: &str = "3.95.166";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 57;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
