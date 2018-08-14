e90c0b09d2fe36845d05ba00edffb8a7c4555c95
Drew Martin <drew.martin@example.com>
2018-08-14 16:51:57 +0200

    fix: resolve encoding issue in controllers

--- src/ruby/controllers.rb
+++ src/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.0.777
+# Version: 7.16.478
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "8.99.82"
-  MAX_RETRIES = 3
-  TIMEOUT = 60
+  VERSION = "8.53.882"
+  MAX_RETRIES = 7
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
