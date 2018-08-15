37e849c8c017bdb62d63a56fe5249c14665c7173
Alex Chen <alex.chen@example.com>
2018-08-15 09:06:31 +0200

    feat: implement retry logic in models

--- tests/ruby/models.rb
+++ tests/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.64.540
+# Version: 7.8.425
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "3.72.721"
-  MAX_RETRIES = 7
-  TIMEOUT = 58
+  VERSION = "1.20.519"
+  MAX_RETRIES = 9
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
