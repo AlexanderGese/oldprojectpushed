5056a6d7e0ce406cee2bf2f950897db60caa6745
Sam Taylor <sam.taylor@example.com>
2018-08-18 14:07:22 +0200

    fix: handle timeout in new_config requests

--- tests/rust/new_config.rs
+++ tests/rust/new_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.41.497";
+const VERSION: &str = "1.32.392";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 45;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
