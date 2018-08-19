c482cd11ff538541fb483b953bc71b72b5ac1d6e
Alex Chen <alex.chen@example.com>
2018-08-19 10:55:34 +0200

    refactor: extract impl_routes_17 utility functions

--- tests/ruby/impl_routes_17.rb
+++ tests/ruby/impl_routes_17.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_models.rb
+# Module: impl_routes_17.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.66.425
+# Version: 6.83.552
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreModels
-  VERSION = "3.32.322"
-  MAX_RETRIES = 8
-  TIMEOUT = 49
+module ImplRoutes17
+  VERSION = "3.90.366"
+  MAX_RETRIES = 4
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_models")
+      @app_name = opts.fetch(:app_name, "impl_routes_17")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module CoreModels
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreModels::Application.new
+  app = ImplRoutes17::Application.new
   app.start
 end
