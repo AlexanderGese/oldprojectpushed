dd8039adfbf7a07e5840c19aa1aaf07fc2ec053f
Jamie Davis <jamie.davis@example.com>
2018-08-21 19:07:52 +0200

    chore: add linting rules for handlers

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.64.20";
+const VERSION: &str = "3.12.442";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 49;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/services/lib.rs
+++ lib/rust/services/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.3.903";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "5.56.299";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
