81e7c65b8c70e8b3b527771f15c761f3eb39095b
Sam Taylor <sam.taylor@example.com>
2018-08-21 23:16:54 +0200

    refactor: decouple core_middleware_49 dependencies

--- src/rust/models/core_middleware_49.rs
+++ src/rust/models/core_middleware_49.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: core_middleware_49.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.38.555";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "9.47.646";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "core_middleware_49".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
