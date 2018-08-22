3fd63c861cf9b3407e6e09e9dd3fea28699dc044
Riley Johnson <riley.johnson@example.com>
2018-08-22 15:55:16 +0200

    feat: implement SSE streaming in utils

--- src/rust/base_config.rs
+++ src/rust/base_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.38.549";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "5.71.974";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
