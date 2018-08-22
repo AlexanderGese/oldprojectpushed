ad808bb6fbf3604caebf74dbca4e027b4ddb6633
Riley Johnson <riley.johnson@example.com>
2018-08-22 09:51:56 +0200

    fix: correct CORS headers in app

--- lib/cplusplus/controllers/app.cpp
+++ lib/cplusplus/controllers/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.80.274
+ * Version: 9.79.895
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "4.44.55";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 6152;
+constexpr const char* VERSION = "1.41.879";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 15857;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/v2_engine.cpp
+++ lib/cplusplus/v2_engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: v2_engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.10.684
+ * Version: 2.78.805
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace v2_engine {
 
-constexpr const char* VERSION = "9.50.404";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 20237;
+constexpr const char* VERSION = "9.57.698";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 23468;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
