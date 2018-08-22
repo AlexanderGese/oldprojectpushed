7b274c6c29d192ac8bea49c89a3dc5af2c3d4f62
Jamie Davis <jamie.davis@example.com>
2018-08-22 22:00:27 +0200

    refactor: migrate base_utils to async/await

--- src/rust/components/base_utils.rs
+++ src/rust/components/base_utils.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: base_utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.54.818";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "8.51.883";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "base_utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
