48fc5b93dcddda3db47c48285ec7efcbcd1aac3e
Drew Martin <drew.martin@example.com>
2018-08-23 14:40:13 +0200

    feat: add validation to config inputs

--- tests/rust/handlers/config.rs
+++ tests/rust/handlers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.50.200";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "9.57.249";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
