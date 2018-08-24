b50c6133dc7dc756016668397eb07974c73060f6
Alex Chen <alex.chen@example.com>
2018-08-24 08:33:02 +0200

    feat: add export functionality to config

--- lib/ruby/config.rb
+++ lib/ruby/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.94.809
+# Version: 2.96.186
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "2.5.859"
+  VERSION = "8.77.647"
   MAX_RETRIES = 10
-  TIMEOUT = 35
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
