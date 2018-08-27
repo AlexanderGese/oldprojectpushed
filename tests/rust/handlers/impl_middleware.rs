09a70382005e13e934039700b1247465bc93f622
Jamie Davis <jamie.davis@example.com>
2018-08-27 13:46:15 +0200

    feat: add export functionality to impl_middleware

--- tests/rust/handlers/impl_middleware.rs
+++ tests/rust/handlers/impl_middleware.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: impl_middleware.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.74.96";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "3.38.262";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "impl_middleware".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
