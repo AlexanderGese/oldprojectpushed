28e3d2f0213cd4ef5499d83587bd127f7667b442
Sam Taylor <sam.taylor@example.com>
2018-08-29 09:59:22 +0200

    chore: update impl_helpers_35 configuration

--- src/ruby/middleware/impl_helpers_35.rb
+++ src/ruby/middleware/impl_helpers_35.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: impl_helpers_35.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.3.189
+# Version: 7.86.676
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "5.75.279"
-  MAX_RETRIES = 7
-  TIMEOUT = 36
+module ImplHelpers35
+  VERSION = "1.88.271"
+  MAX_RETRIES = 3
+  TIMEOUT = 27
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "impl_helpers_35")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = ImplHelpers35::Application.new
   app.start
 end
--- src/ruby/v2_routes.rb
+++ src/ruby/v2_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.29.622
+# Version: 6.26.543
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Routes
-  VERSION = "9.31.458"
-  MAX_RETRIES = 6
-  TIMEOUT = 31
+  VERSION = "9.62.141"
+  MAX_RETRIES = 9
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
