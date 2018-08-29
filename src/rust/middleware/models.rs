2e0f911065eeef72633fb3078747e508692f4a4d
Jamie Davis <jamie.davis@example.com>
2018-08-29 15:57:12 +0200

    feat: implement SSE streaming in utils_1

--- src/rust/middleware/models.rs
+++ src/rust/middleware/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.0.771";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "8.34.2";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
