220948682ef6d1120f88849b4ddcba13452d59d0
Jamie Davis <jamie.davis@example.com>
2018-08-31 20:36:46 +0200

    fix: correct error propagation in routes

--- src/rust/impl_models.rs
+++ src/rust/impl_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.70.170";
+const VERSION: &str = "2.41.724";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 36;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
