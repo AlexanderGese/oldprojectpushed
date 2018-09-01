e48f28317c9170909c329835f8ae83397f201e5a
Jordan Smith <jordan.smith@example.com>
2018-09-01 14:59:50 +0200

    chore: remove deprecated config code

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.53.171";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "5.72.456";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.24.487";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "6.22.326";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
