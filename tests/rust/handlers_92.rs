6eb8ad8bbcd4add08d200991273ef6fd18c73d0b
Alex Chen <alex.chen@example.com>
2018-09-02 19:32:12 +0200

    fix: resolve memory leak in handlers_92

--- tests/rust/handlers_92.rs
+++ tests/rust/handlers_92.rs
@@ -1,4 +1,4 @@
-//! Module: v2_middleware.rs
+//! Module: handlers_92.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.20.818";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "4.56.911";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_middleware".to_string(),
+            app_name: "handlers_92".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/new_config.rs
+++ tests/rust/new_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.32.392";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "3.36.956";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
