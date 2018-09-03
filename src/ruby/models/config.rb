a3e9a7d2af4ac4941baf41700b3336da4b150688
Morgan Lee <morgan.lee@example.com>
2018-09-03 18:15:57 +0200

    fix: resolve encoding issue in app

--- src/ruby/models/config.rb
+++ src/ruby/models/config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.49.453
+# Version: 9.17.534
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "3.50.31"
-  MAX_RETRIES = 3
-  TIMEOUT = 9
+module Config
+  VERSION = "9.87.353"
+  MAX_RETRIES = 6
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "config")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Config::Application.new
   app.start
 end
