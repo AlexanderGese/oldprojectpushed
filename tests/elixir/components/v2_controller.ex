82376c6a24068f3673be045e890f04a8811041e4
Jordan Smith <jordan.smith@example.com>
2018-09-04 11:02:57 +0200

    refactor: improve v2_controller test coverage

--- tests/elixir/components/v2_controller.ex
+++ tests/elixir/components/v2_controller.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule V2_Controller do
   @moduledoc """
-  Module: repo.ex
+  Module: v2_controller.ex
   Elixir boilerplate - auto-generated
-  Version: 5.66.502
+  Version: 3.96.908
   """
 
-  @version "6.7.97"
-  @max_retries 3
-  @timeout 8030
+  @version "1.18.142"
+  @max_retries 6
+  @timeout 24853
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "v2_controller"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule V2_ControllerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule V2_ControllerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
