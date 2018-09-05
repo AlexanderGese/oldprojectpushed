b379aaaac9b51f270b06cf9b69f6d83e9f1eea33
Riley Johnson <riley.johnson@example.com>
2018-09-05 09:32:20 +0200

    refactor: extract routes utility functions

--- lib/rust/controllers/routes.rs
+++ lib/rust/controllers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.84.945";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "2.98.316";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
