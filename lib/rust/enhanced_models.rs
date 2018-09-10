4ca720cc4967e9cb76ac4166a8dca6cd5a1b39a0
Riley Johnson <riley.johnson@example.com>
2018-09-10 14:12:44 +0200

    fix: patch security vulnerability in enhanced_utils_97

--- lib/rust/enhanced_models.rs
+++ lib/rust/enhanced_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.68.88";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "7.44.31";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/services/errors.rs
+++ lib/rust/services/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.15.743";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "2.91.839";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
