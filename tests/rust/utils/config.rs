210d8cc9630968893fa1f2069d10ae3e321f025e
Drew Martin <drew.martin@example.com>
2018-09-11 21:22:02 +0200

    feat: add validation to utils inputs

--- tests/rust/utils/config.rs
+++ tests/rust/utils/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.63.677";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "6.73.85";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
