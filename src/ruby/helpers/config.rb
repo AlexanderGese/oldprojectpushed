384ff56ebc9e0bb14d6dcdd0415a535829d903d2
Casey Williams <casey.williams@example.com>
2018-09-12 21:32:24 +0200

    fix: resolve timezone issue in tasks

--- src/ruby/helpers/config.rb
+++ src/ruby/helpers/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.18.609
+# Version: 9.96.933
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "5.59.636"
-  MAX_RETRIES = 6
-  TIMEOUT = 21
+  VERSION = "1.57.758"
+  MAX_RETRIES = 8
+  TIMEOUT = 14
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
