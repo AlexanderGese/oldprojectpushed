18c38e582ca21a7bda7a554eea46b64eae1eed76
Morgan Lee <morgan.lee@example.com>
2018-09-14 11:40:01 +0200

    feat: add rate limiting to v2_controllers

--- tests/ruby/core_routes.rb
+++ tests/ruby/core_routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: core_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.96.513
+# Version: 5.1.751
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "3.32.47"
-  MAX_RETRIES = 4
-  TIMEOUT = 36
+module CoreRoutes
+  VERSION = "2.72.379"
+  MAX_RETRIES = 5
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "core_routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = CoreRoutes::Application.new
   app.start
 end
