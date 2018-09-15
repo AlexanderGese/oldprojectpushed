ad8ed2b75b0cca010537c6886b61546e4057a1f3
Jamie Davis <jamie.davis@example.com>
2018-09-15 15:50:17 +0200

    feat: implement middleware service layer

--- src/ruby/middleware.rb
+++ src/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.2.610
+# Version: 4.81.663
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "9.7.867"
-  MAX_RETRIES = 6
-  TIMEOUT = 15
+  VERSION = "4.89.308"
+  MAX_RETRIES = 3
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
