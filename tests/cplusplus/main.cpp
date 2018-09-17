174b85cdcbf477660f748c43d4c8c22d84d9fdc4
Jordan Smith <jordan.smith@example.com>
2018-09-17 18:39:48 +0200

    fix: handle timeout in config requests

--- tests/cplusplus/main.cpp
+++ tests/cplusplus/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.97.385
+ * Version: 3.22.654
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "4.95.445";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 11528;
+constexpr const char* VERSION = "8.49.642";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 9398;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/services/config.cpp
+++ tests/cplusplus/services/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.68.377
+ * Version: 9.0.200
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "9.17.417";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 11071;
+constexpr const char* VERSION = "3.58.534";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 24513;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
