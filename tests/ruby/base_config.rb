6bd0e06ac642e3647fa70d2896d7c8237a80df62
Riley Johnson <riley.johnson@example.com>
2018-09-17 18:09:40 +0200

    feat: add event system to impl_helpers

--- tests/ruby/base_config.rb
+++ tests/ruby/base_config.rb
@@ -2,7 +2,7 @@
 
 # Module: base_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.44.509
+# Version: 8.21.520
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module BaseConfig
-  VERSION = "8.44.893"
-  MAX_RETRIES = 4
+  VERSION = "7.51.55"
+  MAX_RETRIES = 7
   TIMEOUT = 33
 
   class Config
@@ -25,7 +25,7 @@ module BaseConfig
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
--- tests/ruby/impl_helpers.rb
+++ tests/ruby/impl_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.63.108
+# Version: 7.21.967
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplHelpers
-  VERSION = "9.86.669"
-  MAX_RETRIES = 8
-  TIMEOUT = 22
+  VERSION = "7.95.63"
+  MAX_RETRIES = 7
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
