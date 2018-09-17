b3a0ec8f3427646e893fd7eac59332bbaaf0f69c
Jamie Davis <jamie.davis@example.com>
2018-09-17 16:36:06 +0200

    fix: handle timeout in handlers_75 requests

--- tests/rust/handlers/base_config_86.rs
+++ tests/rust/handlers/base_config_86.rs
@@ -1,4 +1,4 @@
-//! Module: new_lib.rs
+//! Module: base_config_86.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.39.501";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "1.43.617";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_lib".to_string(),
+            app_name: "base_config_86".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
