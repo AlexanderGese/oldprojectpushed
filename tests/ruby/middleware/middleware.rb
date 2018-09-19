f7c00637dae345652d8a912708a54ad49fdcd47c
Riley Johnson <riley.johnson@example.com>
2018-09-19 13:37:26 +0200

    fix: resolve edge case in tasks handler

--- tests/ruby/middleware/middleware.rb
+++ tests/ruby/middleware/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.60.281
+# Version: 9.3.54
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "7.58.977"
-  MAX_RETRIES = 4
-  TIMEOUT = 27
+  VERSION = "8.83.552"
+  MAX_RETRIES = 9
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
