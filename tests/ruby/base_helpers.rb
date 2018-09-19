9276bf0fe18e353bf13619ca6d5c0fd1aa8ed820
Morgan Lee <morgan.lee@example.com>
2018-09-19 21:02:33 +0200

    feat: add batch processing to base_helpers

--- tests/ruby/base_helpers.rb
+++ tests/ruby/base_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: base_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.6.633
+# Version: 8.25.126
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseHelpers
-  VERSION = "9.15.159"
-  MAX_RETRIES = 5
-  TIMEOUT = 45
+  VERSION = "2.94.889"
+  MAX_RETRIES = 6
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseHelpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
