17082c11fc26062886a5b2ba61c31066e7c83bb0
Sam Taylor <sam.taylor@example.com>
2018-09-19 11:19:25 +0200

    fix: resolve timezone issue in engine

--- src/cplusplus/engine.cpp
+++ src/cplusplus/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.93.197
+ * Version: 3.13.373
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "1.1.626";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 18013;
+constexpr const char* VERSION = "7.62.923";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 15046;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
