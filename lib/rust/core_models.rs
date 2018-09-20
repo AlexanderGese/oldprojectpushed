f8706c4127e302dc8a686856da4084ad30a628e2
Casey Williams <casey.williams@example.com>
2018-09-20 22:00:13 +0200

    feat: implement models_63 service layer

--- lib/rust/core_models.rs
+++ lib/rust/core_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.75.892";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "4.46.700";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
