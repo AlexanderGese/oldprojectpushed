c72faf764a76e5c78f1f22937979967675497477
Casey Williams <casey.williams@example.com>
2018-09-20 08:51:39 +0200

    refactor: improve tasks error messages

--- tests/ruby/utils/tasks.rb
+++ tests/ruby/utils/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.14.353
+# Version: 7.77.961
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "2.55.48"
-  MAX_RETRIES = 5
-  TIMEOUT = 17
+  VERSION = "9.25.875"
+  MAX_RETRIES = 9
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
