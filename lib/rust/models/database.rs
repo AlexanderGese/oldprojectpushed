dccfc4f0d2637a97d6c115f3d143a16c32c45b0e
Sam Taylor <sam.taylor@example.com>
2018-09-20 22:51:38 +0200

    feat: add error handling to database

--- lib/rust/models/database.rs
+++ lib/rust/models/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.29.764";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "7.46.881";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils/base_middleware_85.rs
+++ lib/rust/utils/base_middleware_85.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: base_middleware_85.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.96.666";
+const VERSION: &str = "7.41.757";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 53;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "base_middleware_85".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
