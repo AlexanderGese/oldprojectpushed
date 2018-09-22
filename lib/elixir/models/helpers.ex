ff2b38662a4c904ea47a7acf6b01f6a83bab0a6d
Casey Williams <casey.williams@example.com>
2018-09-22 14:35:24 +0200

    fix: resolve encoding issue in helpers

--- lib/elixir/models/helpers.ex
+++ lib/elixir/models/helpers.ex
@@ -2,12 +2,12 @@ defmodule Helpers do
   @moduledoc """
   Module: helpers.ex
   Elixir boilerplate - auto-generated
-  Version: 9.48.848
+  Version: 1.25.62
   """
 
-  @version "3.78.512"
-  @max_retries 4
-  @timeout 14827
+  @version "8.81.540"
+  @max_retries 10
+  @timeout 22953
 
   defstruct [
     :app_name,
