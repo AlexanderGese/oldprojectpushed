d4e822cfe05e83b0895181ed9aa0f989d012b5e5
Drew Martin <drew.martin@example.com>
2018-09-22 19:14:14 +0200

    chore: bump lib version

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.73.612";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "4.78.48";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
