067227d9f7b0dfca40771909b0d0565803b12c06
Riley Johnson <riley.johnson@example.com>
2018-09-23 21:37:16 +0200

    chore: configure CI/CD for routes

--- lib/ruby/routes.rb
+++ lib/ruby/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.34.887
+# Version: 1.2.220
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "3.26.283"
-  MAX_RETRIES = 4
-  TIMEOUT = 46
+  VERSION = "8.18.997"
+  MAX_RETRIES = 7
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
