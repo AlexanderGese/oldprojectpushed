36bb4dffd39e9983d59513b2c0f5f4de4228e2e6
Jordan Smith <jordan.smith@example.com>
2018-09-23 20:12:12 +0200

    refactor: improve impl_database test coverage

--- tests/rust/utils_62.rs
+++ tests/rust/utils_62.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: utils_62.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.44.253";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "5.21.550";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "utils_62".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
