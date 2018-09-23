f5d809392a1000e54e68e94f745868dc658147aa
Sam Taylor <sam.taylor@example.com>
2018-09-23 17:13:28 +0200

    fix: correct error propagation in v2_config

--- lib/rust/components/impl_main_64.rs
+++ lib/rust/components/impl_main_64.rs
@@ -1,4 +1,4 @@
-//! Module: v2_config.rs
+//! Module: impl_main_64.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.82.260";
+const VERSION: &str = "5.70.740";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 31;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_config".to_string(),
+            app_name: "impl_main_64".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.97.190";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "5.14.491";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
