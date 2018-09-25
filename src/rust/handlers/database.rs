11d06694f8b051c526c8bd1a160c7407fac36fc5
Sam Taylor <sam.taylor@example.com>
2018-09-25 15:08:26 +0200

    feat: add pagination to database

--- src/rust/handlers/database.rs
+++ src/rust/handlers/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.1.763";
+const VERSION: &str = "6.11.925";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 53;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
