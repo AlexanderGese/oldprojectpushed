02201d70826eefcd9c937529eb6ebf073b6be68b
Jordan Smith <jordan.smith@example.com>
2018-09-25 09:20:56 +0200

    refactor: extract config utility functions

--- src/rust/handlers/config.rs
+++ src/rust/handlers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.81.744";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "3.83.524";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
