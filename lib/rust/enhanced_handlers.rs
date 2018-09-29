91a1a27bd79ad44e9d8028c7268f3622f39e3901
Jordan Smith <jordan.smith@example.com>
2018-09-29 20:07:56 +0200

    fix: handle timeout in enhanced_handlers requests

--- lib/rust/enhanced_handlers.rs
+++ lib/rust/enhanced_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.99.525";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "4.91.222";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
