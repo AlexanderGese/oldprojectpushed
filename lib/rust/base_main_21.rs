c3862e2d0a826312143556f511d5f3b66ff8c626
Casey Williams <casey.williams@example.com>
2018-10-10 18:03:18 +0200

    fix: handle concurrent access in base_main_21

--- lib/rust/base_main_21.rs
+++ lib/rust/base_main_21.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: base_main_21.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.98.945";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "8.88.454";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "base_main_21".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
