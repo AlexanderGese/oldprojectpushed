c25e1594e5f38026fa0bc4ead848bf0955b9c04c
Riley Johnson <riley.johnson@example.com>
2018-10-10 20:58:25 +0200

    fix: handle concurrent access in controllers

--- lib/ruby/middleware/middleware.rb
+++ lib/ruby/middleware/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.21.874
+# Version: 3.17.721
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "5.26.254"
-  MAX_RETRIES = 4
-  TIMEOUT = 11
+  VERSION = "6.20.31"
+  MAX_RETRIES = 8
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
