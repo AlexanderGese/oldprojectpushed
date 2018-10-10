6074bd02308c9e572a94e7acc1072fafc7c1cbfc
Morgan Lee <morgan.lee@example.com>
2018-10-10 10:16:39 +0200

    test: add unit tests for main

--- tests/cplusplus/main.cpp
+++ tests/cplusplus/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.22.654
+ * Version: 4.29.911
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "8.49.642";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 9398;
+constexpr const char* VERSION = "3.92.535";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 25622;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
