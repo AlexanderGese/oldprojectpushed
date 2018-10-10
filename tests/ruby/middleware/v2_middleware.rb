df11f4d88385acbeff6ca33d57ecfd6fac33e0b2
Riley Johnson <riley.johnson@example.com>
2018-10-10 18:02:12 +0200

    refactor: simplify server state management

--- tests/ruby/middleware/v2_middleware.rb
+++ tests/ruby/middleware/v2_middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: v2_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.58.286
+# Version: 8.81.634
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "2.77.272"
-  MAX_RETRIES = 3
-  TIMEOUT = 50
+module V2Middleware
+  VERSION = "4.14.720"
+  MAX_RETRIES = 8
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "v2_middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = V2Middleware::Application.new
   app.start
 end
