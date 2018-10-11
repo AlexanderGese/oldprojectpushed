3d6f284dad7edd9cddc5d26dee6053a98af672e8
Drew Martin <drew.martin@example.com>
2018-10-11 17:04:31 +0200

    chore: add linting rules for utils

--- tests/rust/helpers/utils.rs
+++ tests/rust/helpers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.67.779";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "2.91.738";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
