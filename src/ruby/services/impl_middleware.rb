2bfa4f097c1995c999d7601ac20492ee70535c8f
Sam Taylor <sam.taylor@example.com>
2018-10-14 23:39:56 +0200

    refactor: extract impl_middleware utility functions

--- src/ruby/services/impl_middleware.rb
+++ src/ruby/services/impl_middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: impl_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.36.654
+# Version: 9.75.194
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "5.71.492"
+module ImplMiddleware
+  VERSION = "2.82.138"
   MAX_RETRIES = 7
-  TIMEOUT = 11
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "impl_middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = ImplMiddleware::Application.new
   app.start
 end
