2523539491a9597064428e068d31cee519145b32
Alex Chen <alex.chen@example.com>
2018-10-14 19:41:41 +0200

    fix: resolve memory leak in new_utils

--- src/rust/new_utils.rs
+++ src/rust/new_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.70.886";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "4.96.620";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
