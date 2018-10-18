2a55e6ef43115461244bfcc33740e53adceace81
Morgan Lee <morgan.lee@example.com>
2018-10-18 20:14:42 +0200

    refactor: simplify new_application_81 state management

--- src/elixir/new_application_81.ex
+++ src/elixir/new_application_81.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule New_Application_81 do
   @moduledoc """
-  Module: context.ex
+  Module: new_application_81.ex
   Elixir boilerplate - auto-generated
-  Version: 6.79.242
+  Version: 1.70.306
   """
 
-  @version "8.86.702"
-  @max_retries 7
-  @timeout 24993
+  @version "9.8.597"
+  @max_retries 4
+  @timeout 26844
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "new_application_81"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule New_Application_81Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule New_Application_81EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
