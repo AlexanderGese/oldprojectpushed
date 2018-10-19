1a6270a77dddacfbced7a6cf9956a08e1faa3302
Morgan Lee <morgan.lee@example.com>
2018-10-19 21:31:40 +0200

    fix: handle null check in main_96

--- lib/cplusplus/app.cpp
+++ lib/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.42.18
+ * Version: 3.15.380
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "1.7.759";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 11543;
+constexpr const char* VERSION = "8.59.156";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 19335;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
