527a167f9d1877dfe42cd0d8a48171959353a8e6
Morgan Lee <morgan.lee@example.com>
2018-10-22 22:22:49 +0200

    fix: handle timeout in impl_models_70 requests

--- tests/ruby/v2_models_19.rb
+++ tests/ruby/v2_models_19.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: v2_models_19.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.89.346
+# Version: 8.30.4
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "7.40.881"
-  MAX_RETRIES = 5
-  TIMEOUT = 8
+module V2Models19
+  VERSION = "4.83.30"
+  MAX_RETRIES = 4
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "v2_models_19")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = V2Models19::Application.new
   app.start
 end
