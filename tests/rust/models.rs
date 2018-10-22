d001a6af7fec93e35080e6c2ca01359ee49edbdb
Alex Chen <alex.chen@example.com>
2018-10-22 21:36:47 +0200

    feat: add logging to models

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.97.466";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "8.25.744";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
