f57be08e231433ed7e5296e94d01178b8a44ebba
Jordan Smith <jordan.smith@example.com>
2018-10-22 11:53:35 +0200

    fix: handle concurrent access in app

--- lib/ruby/app.rb
+++ lib/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.56.344
+# Version: 2.34.785
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "1.52.469"
-  MAX_RETRIES = 8
-  TIMEOUT = 24
+  VERSION = "5.17.743"
+  MAX_RETRIES = 4
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
