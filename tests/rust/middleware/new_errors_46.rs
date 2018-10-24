dfc55846942fb518c0878804f53f29f72cde7ffa
Drew Martin <drew.martin@example.com>
2018-10-24 23:24:52 +0200

    feat: add pagination to main

--- tests/rust/middleware/new_errors_46.rs
+++ tests/rust/middleware/new_errors_46.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: new_errors_46.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.49.709";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "5.67.869";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "new_errors_46".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
