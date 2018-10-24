dfa2437fd6faeff4768cca66fac3506651fc11d0
Sam Taylor <sam.taylor@example.com>
2018-10-24 08:14:16 +0200

    feat: implement SSE streaming in new_tasks

--- tests/ruby/new_tasks.rb
+++ tests/ruby/new_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: new_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.11.480
+# Version: 7.6.809
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewTasks
-  VERSION = "4.32.447"
-  MAX_RETRIES = 4
-  TIMEOUT = 29
+  VERSION = "8.62.807"
+  MAX_RETRIES = 3
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
--- tests/ruby/utils/middleware.rb
+++ tests/ruby/utils/middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_tasks.rb
+# Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.62.53
+# Version: 1.14.697
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplTasks
-  VERSION = "4.99.311"
-  MAX_RETRIES = 10
-  TIMEOUT = 8
+module Middleware
+  VERSION = "4.57.636"
+  MAX_RETRIES = 4
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_tasks")
+      @app_name = opts.fetch(:app_name, "middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplTasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplTasks::Application.new
+  app = Middleware::Application.new
   app.start
 end
