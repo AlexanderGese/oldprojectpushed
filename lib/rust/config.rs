79367d1123d79ede87b96aa04a31873e066fff32
Riley Johnson <riley.johnson@example.com>
2018-10-24 17:43:42 +0200

    feat: add error handling to database

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.13.971";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "6.63.233";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware/database.rs
+++ lib/rust/middleware/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.6.651";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "5.70.184";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
