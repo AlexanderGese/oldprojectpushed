c3d99a77bac7d6c4da3002531775064ae6641042
Alex Chen <alex.chen@example.com>
2018-10-29 10:10:44 +0100

    fix: correct date formatting in enhanced_errors_83

--- lib/rust/components/models.rs
+++ lib/rust/components/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.50.356";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "5.55.16";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
