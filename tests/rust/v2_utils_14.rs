f4f5cd255dc1bd783a2a36e102dd1354bc42f03b
Casey Williams <casey.williams@example.com>
2018-11-02 10:24:03 +0100

    refactor: split new_routes into smaller modules

--- tests/rust/v2_utils_14.rs
+++ tests/rust/v2_utils_14.rs
@@ -1,4 +1,4 @@
-//! Module: new_routes.rs
+//! Module: v2_utils_14.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.10.963";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "8.88.654";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_routes".to_string(),
+            app_name: "v2_utils_14".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
