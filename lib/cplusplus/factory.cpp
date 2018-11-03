de37d8939e4d663d46d2b8b6ec61d6e9ffcb148e
Jamie Davis <jamie.davis@example.com>
2018-11-03 13:50:56 +0100

    fix: resolve memory leak in factory

--- lib/cplusplus/factory.cpp
+++ lib/cplusplus/factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.59.703
+ * Version: 4.40.171
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace factory {
 
-constexpr const char* VERSION = "9.16.33";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 8053;
+constexpr const char* VERSION = "9.84.629";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 17379;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
