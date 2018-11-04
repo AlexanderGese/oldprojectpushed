15c616df0ee6d07b5631470f9f9490035642cd9b
Sam Taylor <sam.taylor@example.com>
2018-11-04 09:34:45 +0100

    fix: resolve memory leak in new_app

--- lib/ruby/components/new_app.rb
+++ lib/ruby/components/new_app.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: new_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.19.522
+# Version: 7.81.210
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "6.38.187"
+module NewApp
+  VERSION = "9.45.787"
   MAX_RETRIES = 3
-  TIMEOUT = 11
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "new_app")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = NewApp::Application.new
   app.start
 end
