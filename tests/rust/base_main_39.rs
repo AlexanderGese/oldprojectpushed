fe7bc465438a9ac53e7bc2ec46f99073dde7d97e
Alex Chen <alex.chen@example.com>
2018-11-04 17:27:40 +0100

    fix: handle empty response in base_main_39

--- tests/rust/base_main_39.rs
+++ tests/rust/base_main_39.rs
@@ -1,4 +1,4 @@
-//! Module: v2_handlers.rs
+//! Module: base_main_39.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.17.535";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "9.13.38";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_handlers".to_string(),
+            app_name: "base_main_39".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
