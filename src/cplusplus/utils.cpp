adb4bbc1e2b68dfa86aa2aaf434a282a9a65f924
Sam Taylor <sam.taylor@example.com>
2018-11-06 12:37:38 +0100

    feat: implement SSE streaming in utils

--- src/cplusplus/utils.cpp
+++ src/cplusplus/utils.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: utils.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.29.216
+ * Version: 4.7.326
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace utils {
 
-constexpr const char* VERSION = "9.13.521";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 27313;
+constexpr const char* VERSION = "2.50.235";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 7629;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
