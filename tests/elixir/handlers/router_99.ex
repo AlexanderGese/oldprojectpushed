57a0e4f317806242531892b52a024d3ecb78cf48
Drew Martin <drew.martin@example.com>
2018-11-09 13:48:04 +0100

    fix: resolve timezone issue in router_99

--- tests/elixir/handlers/router_99.ex
+++ tests/elixir/handlers/router_99.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule Router_99 do
   @moduledoc """
-  Module: repo.ex
+  Module: router_99.ex
   Elixir boilerplate - auto-generated
-  Version: 8.18.476
+  Version: 5.93.104
   """
 
-  @version "7.30.765"
-  @max_retries 9
-  @timeout 11352
+  @version "2.3.303"
+  @max_retries 5
+  @timeout 26423
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "router_99"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule Router_99Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule Router_99EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
