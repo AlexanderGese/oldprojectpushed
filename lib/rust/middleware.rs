336838f67eeb2b6f2d14913218664cae2c48c2f8
Jordan Smith <jordan.smith@example.com>
2018-11-12 20:50:57 +0100

    fix: handle large payloads in routes

--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.56.445";
+const VERSION: &str = "1.42.868";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 41;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils/routes.rs
+++ lib/rust/utils/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.69.615";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "4.8.273";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
