892ec29db90acdb76968ea0e634bbd14ef6059a2
Casey Williams <casey.williams@example.com>
2018-11-15 10:25:52 +0100

    feat: add base_database_79 module with core functionality

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.25.744";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "6.59.909";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
