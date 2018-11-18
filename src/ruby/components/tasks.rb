1d9e81e2d1fccf3a38e170af1495a45fc57f9ccd
Drew Martin <drew.martin@example.com>
2018-11-18 08:40:02 +0100

    fix: resolve edge case in helpers handler

--- src/ruby/components/tasks.rb
+++ src/ruby/components/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.49.255
+# Version: 9.21.334
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "7.23.642"
-  MAX_RETRIES = 10
-  TIMEOUT = 23
+  VERSION = "3.65.820"
+  MAX_RETRIES = 6
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
