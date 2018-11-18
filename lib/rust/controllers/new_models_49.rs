3c27bc6386d6eb45fb92b99ef7437c31f4742f28
Alex Chen <alex.chen@example.com>
2018-11-18 20:37:16 +0100

    chore: remove deprecated new_models_49 code

--- lib/rust/controllers/new_models_49.rs
+++ lib/rust/controllers/new_models_49.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: new_models_49.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.2.903";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "7.65.176";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "new_models_49".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
