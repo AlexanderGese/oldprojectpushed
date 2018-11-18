54b07f83c833080d48c8185335ef1afda56d81d1
Alex Chen <alex.chen@example.com>
2018-11-18 08:59:44 +0100

    feat: implement WebSocket support in lib

--- src/rust/handlers/lib.rs
+++ src/rust/handlers/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.83.119";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "3.58.264";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
