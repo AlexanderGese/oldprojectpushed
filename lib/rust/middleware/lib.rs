3cf156ec999349870cea3a0d241a953d3840b9c2
Riley Johnson <riley.johnson@example.com>
2018-11-19 09:31:03 +0100

    feat: implement SSE streaming in lib

--- lib/rust/middleware/lib.rs
+++ lib/rust/middleware/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.64.381";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "7.19.378";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
