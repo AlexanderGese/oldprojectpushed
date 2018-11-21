efda0c802116fa099b44389d00d2423f998c4cdc
Morgan Lee <morgan.lee@example.com>
2018-11-21 18:44:59 +0100

    fix: handle large payloads in core_schema_93

--- lib/elixir/utils/impl_context_16.ex
+++ lib/elixir/utils/impl_context_16.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Impl_Context_16 do
   @moduledoc """
-  Module: helpers.ex
+  Module: impl_context_16.ex
   Elixir boilerplate - auto-generated
-  Version: 4.73.299
+  Version: 3.58.823
   """
 
-  @version "8.90.530"
+  @version "4.44.240"
   @max_retries 6
-  @timeout 20736
+  @timeout 20220
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "impl_context_16"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule Impl_Context_16Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule Impl_Context_16EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
