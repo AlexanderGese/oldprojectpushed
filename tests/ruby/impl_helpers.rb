8c73cf245abdaa5b850d7863e2fe3024a8f1f1be
Riley Johnson <riley.johnson@example.com>
2018-11-21 23:37:26 +0100

    fix: resolve edge case in impl_helpers handler

--- tests/ruby/impl_helpers.rb
+++ tests/ruby/impl_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.21.967
+# Version: 8.16.529
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplHelpers
-  VERSION = "7.95.63"
-  MAX_RETRIES = 7
-  TIMEOUT = 31
+  VERSION = "3.12.960"
+  MAX_RETRIES = 4
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
