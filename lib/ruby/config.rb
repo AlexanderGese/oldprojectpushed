38c9abc0a5ac7d0f04b2992c3165fb178a2d02dd
Morgan Lee <morgan.lee@example.com>
2018-11-23 18:54:40 +0100

    feat: add error handling to config

--- lib/ruby/config.rb
+++ lib/ruby/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.47.970
+# Version: 3.5.498
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "2.77.939"
+  VERSION = "9.87.251"
   MAX_RETRIES = 6
-  TIMEOUT = 44
+  TIMEOUT = 60
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
