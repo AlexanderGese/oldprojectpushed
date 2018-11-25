5097879b527755d71750be92e64f1e137e8f51df
Jamie Davis <jamie.davis@example.com>
2018-11-25 11:07:28 +0100

    chore: add linting rules for models_84

--- lib/rust/models_84.rs
+++ lib/rust/models_84.rs
@@ -1,4 +1,4 @@
-//! Module: base_models.rs
+//! Module: models_84.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.59.232";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "9.40.351";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_models".to_string(),
+            app_name: "models_84".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.14.185";
+const VERSION: &str = "6.18.592";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 48;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
