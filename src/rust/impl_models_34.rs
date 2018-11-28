34123863dbbfa0106098a00d732e3971e18262cf
Morgan Lee <morgan.lee@example.com>
2018-11-28 13:00:58 +0100

    docs: add documentation for routes

--- src/rust/impl_models_34.rs
+++ src/rust/impl_models_34.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: impl_models_34.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.52.837";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "6.91.290";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "impl_models_34".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
