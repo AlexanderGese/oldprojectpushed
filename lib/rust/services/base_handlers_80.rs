12c972f4958c8cd2f104c810b4d80c296cadecf7
Drew Martin <drew.martin@example.com>
2018-12-01 19:35:27 +0100

    chore: update v2_utils configuration

--- lib/rust/services/base_handlers_80.rs
+++ lib/rust/services/base_handlers_80.rs
@@ -1,4 +1,4 @@
-//! Module: v2_utils.rs
+//! Module: base_handlers_80.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.24.603";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "5.94.376";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_utils".to_string(),
+            app_name: "base_handlers_80".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
