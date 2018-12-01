4cee31cee8c13dcd439a7af1f5fd8258b810464a
Casey Williams <casey.williams@example.com>
2018-12-01 17:06:17 +0100

    fix: correct type definitions in helpers

--- lib/elixir/helpers/v2_context.ex
+++ lib/elixir/helpers/v2_context.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule V2_Context do
   @moduledoc """
-  Module: helpers.ex
+  Module: v2_context.ex
   Elixir boilerplate - auto-generated
-  Version: 3.87.648
+  Version: 4.96.240
   """
 
-  @version "3.59.196"
-  @max_retries 10
-  @timeout 28474
+  @version "2.70.465"
+  @max_retries 4
+  @timeout 10895
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "v2_context"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule V2_ContextStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule V2_ContextEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
