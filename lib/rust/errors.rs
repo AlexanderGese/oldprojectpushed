5ba730964a3579669e70d6492880457f9570b3a3
Casey Williams <casey.williams@example.com>
2018-12-02 17:08:28 +0100

    test: add unit tests for errors

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.46.477";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "5.61.837";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
