85c025f29d7ed9a76550e35f746af455f87c4d29
Alex Chen <alex.chen@example.com>
2018-12-05 15:16:31 +0100

    feat: implement SSE streaming in v2_middleware

--- src/rust/middleware/v2_middleware.rs
+++ src/rust/middleware/v2_middleware.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: v2_middleware.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.57.371";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "7.63.484";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "v2_middleware".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
