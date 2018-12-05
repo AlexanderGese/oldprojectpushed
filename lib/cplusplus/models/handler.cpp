4271ea21fa092a8f117e3cecdb43350e2f439c74
Jamie Davis <jamie.davis@example.com>
2018-12-05 17:23:17 +0100

    fix: handle empty response in handler

--- lib/cplusplus/models/handler.cpp
+++ lib/cplusplus/models/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.12.37
+ * Version: 3.92.610
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "6.22.55";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 11457;
+constexpr const char* VERSION = "7.7.649";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 27568;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
