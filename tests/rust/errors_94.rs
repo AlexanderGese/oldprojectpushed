8898ae5d255afbe9c05133673da0a62d26a360b3
Jamie Davis <jamie.davis@example.com>
2018-12-08 21:02:42 +0100

    fix: resolve timezone issue in errors

--- tests/rust/errors_94.rs
+++ tests/rust/errors_94.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: errors_94.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.88.459";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "4.52.774";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "errors_94".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
