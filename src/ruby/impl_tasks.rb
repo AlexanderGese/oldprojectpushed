3a0c12be2d56cee4079c5096c3bcf7ec5328161b
Morgan Lee <morgan.lee@example.com>
2018-12-08 12:15:50 +0100

    refactor: simplify impl_tasks state management

--- src/ruby/impl_tasks.rb
+++ src/ruby/impl_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.82.233
+# Version: 7.96.272
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplTasks
-  VERSION = "8.26.221"
-  MAX_RETRIES = 6
-  TIMEOUT = 30
+  VERSION = "7.84.636"
+  MAX_RETRIES = 3
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
