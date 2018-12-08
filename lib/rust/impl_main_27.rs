7d57edaf793b694828643b0b0386c0c73abae27b
Jordan Smith <jordan.smith@example.com>
2018-12-08 21:34:04 +0100

    feat: add middleware support to impl_main_27

--- lib/rust/impl_main_27.rs
+++ lib/rust/impl_main_27.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: impl_main_27.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.1.196";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "1.56.298";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "impl_main_27".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
