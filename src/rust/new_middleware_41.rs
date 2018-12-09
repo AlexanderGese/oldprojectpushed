3a81df82a72a292c56498b63a87316001ce1f709
Riley Johnson <riley.johnson@example.com>
2018-12-09 11:44:59 +0100

    fix: resolve timezone issue in errors

--- src/rust/new_middleware_41.rs
+++ src/rust/new_middleware_41.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: new_middleware_41.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.46.154";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "9.48.780";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "new_middleware_41".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
