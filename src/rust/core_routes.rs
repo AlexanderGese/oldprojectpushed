04f376dc35d05537cd81ab2ccb7df4f26ea7b137
Riley Johnson <riley.johnson@example.com>
2018-12-11 23:23:42 +0100

    feat: implement authentication in middleware

--- src/rust/core_routes.rs
+++ src/rust/core_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.72.655";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "4.99.390";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/helpers/handlers.rs
+++ src/rust/helpers/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.21.522";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "2.18.233";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
