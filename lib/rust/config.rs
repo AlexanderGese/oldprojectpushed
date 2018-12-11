a3ad61e2bd8823fe05e0fcad2f5ff98426c0a721
Morgan Lee <morgan.lee@example.com>
2018-12-11 11:01:03 +0100

    chore: bump v2_database version

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.50.42";
+const VERSION: &str = "7.49.570";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 16;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
