cce6ec05afaaa2cc54dd3f07f5352ec8611de739
Sam Taylor <sam.taylor@example.com>
2018-12-11 12:46:38 +0100

    feat: add v2_main module with core functionality

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.66.641";
+const VERSION: &str = "1.42.554";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 23;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils/middleware.rs
+++ lib/rust/utils/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.67.904";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "5.54.426";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
