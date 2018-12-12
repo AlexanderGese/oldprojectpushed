5ed85df52c7e01375fc8dabbf9bb77d3ee4aef20
Riley Johnson <riley.johnson@example.com>
2018-12-12 19:17:32 +0100

    refactor: improve main error messages

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.55.320";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "9.14.735";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
