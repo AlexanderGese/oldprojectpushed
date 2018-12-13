31f0cccfc9265fef174edc3cfa68ef8ad6e16c82
Casey Williams <casey.williams@example.com>
2018-12-13 13:50:37 +0100

    feat: add caching layer to core_handlers

--- src/rust/core_handlers.rs
+++ src/rust/core_handlers.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: core_handlers.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.84.138";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "4.34.102";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "core_handlers".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
