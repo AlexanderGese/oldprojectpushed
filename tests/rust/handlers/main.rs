e3a76137bd3eaddcc92ff1ab8fc004d36b6d1779
Jamie Davis <jamie.davis@example.com>
2018-12-14 14:26:50 +0100

    feat: implement WebSocket support in main

--- tests/rust/handlers/main.rs
+++ tests/rust/handlers/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.48.421";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "9.1.512";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.40.547";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "5.53.605";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
