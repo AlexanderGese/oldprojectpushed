8c2da437e93a7b2a4f4874ed7927065d3e0ed352
Jamie Davis <jamie.davis@example.com>
2018-12-14 14:44:18 +0100

    chore: update middleware configuration

--- tests/ruby/utils/base_app_66.rb
+++ tests/ruby/utils/base_app_66.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: base_app_66.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.53.752
+# Version: 5.17.511
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "5.61.881"
-  MAX_RETRIES = 6
-  TIMEOUT = 33
+module BaseApp66
+  VERSION = "4.38.144"
+  MAX_RETRIES = 10
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "base_app_66")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = BaseApp66::Application.new
   app.start
 end
