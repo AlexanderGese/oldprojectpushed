c5d911dd967477ec7735ef8d30a9a2ba7bab9873
Sam Taylor <sam.taylor@example.com>
2018-12-17 09:43:07 +0100

    refactor: migrate context to async/await

--- src/elixir/components/new_repo.ex
+++ src/elixir/components/new_repo.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule New_Repo do
   @moduledoc """
-  Module: context.ex
+  Module: new_repo.ex
   Elixir boilerplate - auto-generated
-  Version: 5.71.371
+  Version: 8.31.646
   """
 
-  @version "3.60.443"
-  @max_retries 7
-  @timeout 17135
+  @version "9.27.576"
+  @max_retries 3
+  @timeout 16539
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "new_repo"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule New_RepoStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule New_RepoEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
