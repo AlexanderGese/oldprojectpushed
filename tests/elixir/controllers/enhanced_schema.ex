571d7b92ed561bafed512f6a37b4cfaa4df06c4a
Drew Martin <drew.martin@example.com>
2018-12-23 08:36:48 +0100

    fix: correct error propagation in enhanced_schema

--- tests/elixir/controllers/enhanced_schema.ex
+++ tests/elixir/controllers/enhanced_schema.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Enhanced_Schema do
   @moduledoc """
-  Module: helpers.ex
+  Module: enhanced_schema.ex
   Elixir boilerplate - auto-generated
-  Version: 8.76.574
+  Version: 3.4.459
   """
 
-  @version "1.19.698"
-  @max_retries 3
-  @timeout 10112
+  @version "1.38.130"
+  @max_retries 9
+  @timeout 24589
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_schema"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule Enhanced_SchemaStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule Enhanced_SchemaEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
