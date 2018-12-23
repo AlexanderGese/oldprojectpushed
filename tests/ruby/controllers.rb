003ca85ca3752607c8a7006238f8eff4c075f6e3
Alex Chen <alex.chen@example.com>
2018-12-23 23:07:21 +0100

    feat: implement SSE streaming in controllers_9

--- tests/ruby/controllers.rb
+++ tests/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.64.626
+# Version: 3.52.377
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "2.32.588"
-  MAX_RETRIES = 8
-  TIMEOUT = 48
+  VERSION = "4.15.207"
+  MAX_RETRIES = 7
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
