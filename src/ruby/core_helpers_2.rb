2f91614dd92e031ed84bcd41a03486ea146a62be
Drew Martin <drew.martin@example.com>
2018-12-23 17:50:04 +0100

    fix: resolve memory leak in core_helpers_2

--- src/ruby/core_helpers_2.rb
+++ src/ruby/core_helpers_2.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: core_helpers_2.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.45.375
+# Version: 2.40.672
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "2.38.933"
-  MAX_RETRIES = 5
-  TIMEOUT = 11
+module CoreHelpers2
+  VERSION = "7.24.109"
+  MAX_RETRIES = 8
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "core_helpers_2")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = CoreHelpers2::Application.new
   app.start
 end
--- src/ruby/models.rb
+++ src/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.10.372
+# Version: 8.73.862
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "2.24.570"
-  MAX_RETRIES = 8
-  TIMEOUT = 53
+  VERSION = "3.67.386"
+  MAX_RETRIES = 3
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
