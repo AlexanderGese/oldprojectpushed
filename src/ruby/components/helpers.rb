63d83ee7200b7b50d96fd80492e05f642899c01a
Riley Johnson <riley.johnson@example.com>
2018-12-26 12:14:32 +0100

    feat: implement authentication in helpers

--- src/ruby/components/helpers.rb
+++ src/ruby/components/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.84.933
+# Version: 5.22.186
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "7.84.525"
-  MAX_RETRIES = 10
-  TIMEOUT = 40
+  VERSION = "5.87.187"
+  MAX_RETRIES = 5
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
