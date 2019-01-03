8c3669bcd0ef6e1e7df882921aff888297f9c223
Jordan Smith <jordan.smith@example.com>
2019-01-03 16:09:34 +0100

    chore: update utils configuration

--- tests/rust/base_config.rs
+++ tests/rust/base_config.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: base_config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.60.158";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "4.5.754";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "base_config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
