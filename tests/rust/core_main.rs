3134e1af10b00afecf0b321bd89f6753544d1f13
Casey Williams <casey.williams@example.com>
2019-01-04 22:57:10 +0100

    refactor: migrate errors to async/await

--- tests/rust/core_main.rs
+++ tests/rust/core_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.49.236";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "7.32.497";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/handlers/handlers.rs
+++ tests/rust/handlers/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.6.134";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "5.9.816";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
