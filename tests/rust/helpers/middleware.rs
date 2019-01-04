4ffb8d6b4940f58ef626e4622e72400dbc03803e
Jamie Davis <jamie.davis@example.com>
2019-01-04 18:30:55 +0100

    fix: correct CORS headers in middleware

--- tests/rust/helpers/middleware.rs
+++ tests/rust/helpers/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.62.553";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "6.49.528";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
