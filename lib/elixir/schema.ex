386fe0dfcefe540011076e048c8af94dd55ce95a
Morgan Lee <morgan.lee@example.com>
2019-01-04 10:41:48 +0100

    fix: correct type definitions in schema

--- lib/elixir/schema.ex
+++ lib/elixir/schema.ex
@@ -2,12 +2,12 @@ defmodule Schema do
   @moduledoc """
   Module: schema.ex
   Elixir boilerplate - auto-generated
-  Version: 8.55.466
+  Version: 6.12.649
   """
 
-  @version "9.65.782"
-  @max_retries 3
-  @timeout 15938
+  @version "6.93.256"
+  @max_retries 8
+  @timeout 27427
 
   defstruct [
     :app_name,
