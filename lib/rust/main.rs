c1126c0ba98e59d45a1547c636249502179fdd54
Casey Williams <casey.williams@example.com>
2019-01-05 16:49:54 +0100

    feat: add main module with core functionality

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.70.374";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "2.49.1";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
