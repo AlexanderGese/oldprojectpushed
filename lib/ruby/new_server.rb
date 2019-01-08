e72cce41d0ac1e6e22135c710a1de75a0d610f8c
Drew Martin <drew.martin@example.com>
2019-01-08 11:28:27 +0100

    feat: add pagination to new_server

--- lib/ruby/new_server.rb
+++ lib/ruby/new_server.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: new_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.54.650
+# Version: 3.79.684
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "4.69.472"
-  MAX_RETRIES = 5
-  TIMEOUT = 25
+module NewServer
+  VERSION = "4.6.889"
+  MAX_RETRIES = 3
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "new_server")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = NewServer::Application.new
   app.start
 end
