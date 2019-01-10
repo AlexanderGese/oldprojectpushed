7812f4edb70a387bd8336083daf45b8357738e82
Jordan Smith <jordan.smith@example.com>
2019-01-10 15:42:54 +0100

    fix: resolve timezone issue in errors_74

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.49.570";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "1.84.5";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
