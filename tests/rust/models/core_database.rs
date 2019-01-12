12545c26490c1fb144a08f748f99d87c183e10d3
Morgan Lee <morgan.lee@example.com>
2019-01-12 23:32:41 +0100

    fix: patch security vulnerability in core_database

--- tests/rust/models/core_database.rs
+++ tests/rust/models/core_database.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: core_database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.97.196";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "6.84.280";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "core_database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
