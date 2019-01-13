d41dea8f3316e7911816c386c56349e700b4dfd3
Jamie Davis <jamie.davis@example.com>
2019-01-13 15:22:20 +0100

    feat: implement retry logic in v2_models

--- lib/ruby/helpers/controllers.rb
+++ lib/ruby/helpers/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.89.84
+# Version: 5.93.306
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "2.90.844"
-  MAX_RETRIES = 7
-  TIMEOUT = 22
+  VERSION = "5.59.686"
+  MAX_RETRIES = 10
+  TIMEOUT = 40
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
