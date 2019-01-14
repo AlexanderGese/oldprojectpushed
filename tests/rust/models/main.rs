70f4f8053f81b35b4b70ad70d534a137c8e5c9f3
Jordan Smith <jordan.smith@example.com>
2019-01-14 23:58:41 +0100

    fix: correct date formatting in main

--- tests/rust/models/main.rs
+++ tests/rust/models/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.92.844";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "1.76.277";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
