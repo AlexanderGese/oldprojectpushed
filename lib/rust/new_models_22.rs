db9e5a747fcb7910154b55ac6f6208cf29cad33f
Jamie Davis <jamie.davis@example.com>
2019-01-14 14:00:08 +0100

    fix: correct CORS headers in routes

--- lib/rust/new_models_22.rs
+++ lib/rust/new_models_22.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: new_models_22.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.83.948";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "9.33.300";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "new_models_22".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
