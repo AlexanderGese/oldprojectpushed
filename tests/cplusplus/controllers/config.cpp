829dfdb1ca12683121308a78f65efa4940f619cc
Casey Williams <casey.williams@example.com>
2019-01-14 20:08:55 +0100

    feat: add validation to config inputs

--- tests/cplusplus/controllers/config.cpp
+++ tests/cplusplus/controllers/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.37.536
+ * Version: 9.49.520
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "3.24.760";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 23386;
+constexpr const char* VERSION = "7.37.179";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 26040;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
