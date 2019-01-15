f5e518a9d9c2102bf9b2223a1bb35f67874a695a
Jordan Smith <jordan.smith@example.com>
2019-01-15 22:45:55 +0100

    fix: handle concurrent access in middleware

--- src/rust/handlers.rs
+++ src/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.38.620";
+const VERSION: &str = "2.60.345";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 37;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
