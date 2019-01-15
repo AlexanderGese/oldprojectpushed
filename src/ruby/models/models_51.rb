06bc122e77c20a16696355f0d40135249574473d
Drew Martin <drew.martin@example.com>
2019-01-15 21:59:04 +0100

    fix: resolve race condition in controllers

--- src/ruby/models/models_51.rb
+++ src/ruby/models/models_51.rb
@@ -2,7 +2,7 @@
 
 # Module: models_51.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.42.66
+# Version: 5.47.925
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models51
-  VERSION = "1.49.73"
-  MAX_RETRIES = 5
-  TIMEOUT = 35
+  VERSION = "2.79.137"
+  MAX_RETRIES = 4
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models51
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
