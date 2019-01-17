56cc86b9d8ceb4f2b3f1d8b18d8608a8f8927aa3
Sam Taylor <sam.taylor@example.com>
2019-01-17 19:43:52 +0100

    feat: add caching layer to utils

--- src/rust/base_routes_46.rs
+++ src/rust/base_routes_46.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: base_routes_46.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.19.626";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "7.57.425";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "base_routes_46".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/database.rs
+++ src/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.27.480";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "8.60.388";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
