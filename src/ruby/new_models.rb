82c7f11ade7f3715f3ff6e021d1500c7191114a6
Jamie Davis <jamie.davis@example.com>
2019-01-18 13:56:00 +0100

    feat: add batch processing to new_models

--- src/ruby/new_models.rb
+++ src/ruby/new_models.rb
@@ -2,7 +2,7 @@
 
 # Module: new_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.33.236
+# Version: 7.22.835
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewModels
-  VERSION = "4.5.698"
-  MAX_RETRIES = 9
-  TIMEOUT = 49
+  VERSION = "3.64.963"
+  MAX_RETRIES = 8
+  TIMEOUT = 27
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
