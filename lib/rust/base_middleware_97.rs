587a6cb160ae6109d1bfc22a9e25a4a9fa4c2e48
Casey Williams <casey.williams@example.com>
2019-01-18 09:58:37 +0100

    fix: resolve timezone issue in base_middleware_97

--- lib/rust/base_middleware_97.rs
+++ lib/rust/base_middleware_97.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: base_middleware_97.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.47.47";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "9.4.508";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "base_middleware_97".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
