2bc7339f25affcf34186dbfe2feb518820694970
Alex Chen <alex.chen@example.com>
2019-01-19 12:57:44 +0100

    chore: update errors dependencies

--- lib/rust/base_errors_16.rs
+++ lib/rust/base_errors_16.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: base_errors_16.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.68.678";
+const VERSION: &str = "8.21.852";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 53;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "base_errors_16".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/impl_database_92.rs
+++ lib/rust/impl_database_92.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: impl_database_92.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.68.678";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "4.88.504";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "impl_database_92".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
