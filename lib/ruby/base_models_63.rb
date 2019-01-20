8d534c54aeccaa4fc07f06b55ec94012db1819b0
Jamie Davis <jamie.davis@example.com>
2019-01-20 16:43:17 +0100

    feat: add rate limiting to base_models_63

--- lib/ruby/base_models_63.rb
+++ lib/ruby/base_models_63.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: base_models_63.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.14.629
+# Version: 9.2.285
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "8.92.228"
-  MAX_RETRIES = 6
-  TIMEOUT = 46
+module BaseModels63
+  VERSION = "5.2.321"
+  MAX_RETRIES = 8
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "base_models_63")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = BaseModels63::Application.new
   app.start
 end
