d3f126e5a15d5ab7015281310905c7d3226366f8
Casey Williams <casey.williams@example.com>
2019-01-21 13:34:56 +0100

    fix: resolve memory leak in lib

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.47.47";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "1.22.798";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
