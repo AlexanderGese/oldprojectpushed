2820570511841f429f858cb90d10a757b96f3001
Riley Johnson <riley.johnson@example.com>
2019-01-22 10:06:21 +0100

    refactor: decouple app dependencies

--- lib/ruby/utils/validators.rb
+++ lib/ruby/utils/validators.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.83.296
+# Version: 6.12.622
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "8.97.724"
-  MAX_RETRIES = 3
-  TIMEOUT = 34
+module Validators
+  VERSION = "4.62.804"
+  MAX_RETRIES = 8
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "validators")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Validators::Application.new
   app.start
 end
