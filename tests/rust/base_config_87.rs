2aae0affc08af3843659adf83fc9c69b9c9bef02
Riley Johnson <riley.johnson@example.com>
2019-01-23 17:33:23 +0100

    chore: update v2_middleware dependencies

--- tests/rust/base_config_87.rs
+++ tests/rust/base_config_87.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_config_87.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.34.881";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "8.2.907";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_config_87".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/v2_middleware.rs
+++ tests/rust/v2_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.16.746";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "9.44.424";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
