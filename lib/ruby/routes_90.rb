94271376606b1be5fe85ad2bc8fbe564854f6128
Casey Williams <casey.williams@example.com>
2019-01-24 11:45:06 +0100

    refactor: extract routes utility functions

--- lib/ruby/routes_90.rb
+++ lib/ruby/routes_90.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: routes_90.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.65.236
+# Version: 2.37.641
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "4.22.713"
-  MAX_RETRIES = 8
-  TIMEOUT = 26
+module Routes90
+  VERSION = "3.19.54"
+  MAX_RETRIES = 9
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "routes_90")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Routes90::Application.new
   app.start
 end
