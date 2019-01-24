df9c893c0a983cbf4457d743ebc35f2efbbc7920
Drew Martin <drew.martin@example.com>
2019-01-24 22:23:36 +0100

    feat: add health check endpoint to impl_models

--- src/ruby/impl_models.rb
+++ src/ruby/impl_models.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.27.474
+# Version: 1.1.458
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplModels
-  VERSION = "5.43.138"
-  MAX_RETRIES = 5
-  TIMEOUT = 6
+  VERSION = "9.54.360"
+  MAX_RETRIES = 9
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
