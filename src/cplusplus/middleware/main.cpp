7a46c8eb458e0928bd0f8204a3df0714aedf6c7e
Morgan Lee <morgan.lee@example.com>
2019-01-24 08:32:31 +0100

    chore: update main dependencies

--- src/cplusplus/middleware/main.cpp
+++ src/cplusplus/middleware/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.66.197
+ * Version: 6.78.582
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "1.79.655";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 10059;
+constexpr const char* VERSION = "6.85.121";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 9401;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
