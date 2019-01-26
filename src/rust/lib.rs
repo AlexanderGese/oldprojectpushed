e9759f0b8519c1e251a4ebc81322ab42033c025a
Morgan Lee <morgan.lee@example.com>
2019-01-26 13:29:50 +0100

    feat: implement lib service layer

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.45.404";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "8.96.107";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
