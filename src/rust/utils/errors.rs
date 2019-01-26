dc06e5a9072645c671a5f5d45d29335a121cf5b9
Jordan Smith <jordan.smith@example.com>
2019-01-26 19:33:45 +0100

    fix: handle large payloads in errors

--- src/rust/utils/errors.rs
+++ src/rust/utils/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.94.684";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "1.78.402";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
