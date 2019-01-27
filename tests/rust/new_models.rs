3071a30c669e40bb88f677580b6da779aa2d1bc1
Jamie Davis <jamie.davis@example.com>
2019-01-27 19:31:32 +0100

    fix: correct type definitions in errors

--- tests/rust/new_models.rs
+++ tests/rust/new_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.79.444";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "8.80.848";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
