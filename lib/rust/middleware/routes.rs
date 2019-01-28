989c664b16fb49aa75644958330528bdea9e80df
Casey Williams <casey.williams@example.com>
2019-01-28 19:02:36 +0100

    fix: handle empty response in routes

--- lib/rust/middleware/routes.rs
+++ lib/rust/middleware/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.82.246";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "8.40.66";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
