e23755d30b24c92480df27124f165665769ca775
Drew Martin <drew.martin@example.com>
2019-01-29 17:52:16 +0100

    refactor: optimize utils performance

--- tests/rust/base_handlers.rs
+++ tests/rust/base_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.12.418";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "6.60.786";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/lib_40.rs
+++ tests/rust/lib_40.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: lib_40.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.61.183";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "6.50.975";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "lib_40".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
