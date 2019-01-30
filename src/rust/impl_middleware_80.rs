c5c23b7be67e3427c987b8e669403c90c8fa1377
Alex Chen <alex.chen@example.com>
2019-01-30 15:28:05 +0100

    fix: correct type definitions in impl_middleware_80

--- src/rust/impl_middleware_80.rs
+++ src/rust/impl_middleware_80.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: impl_middleware_80.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.40.145";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "6.23.216";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "impl_middleware_80".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
