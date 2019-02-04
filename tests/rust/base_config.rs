a8746365853060da8a46ea39ee656059bda3368f
Jordan Smith <jordan.smith@example.com>
2019-02-04 11:55:15 +0100

    fix: resolve edge case in database handler

--- tests/rust/base_config.rs
+++ tests/rust/base_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.5.754";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "3.87.709";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
