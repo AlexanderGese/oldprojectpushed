83db1ec7b25023b2a35b53141b0e212023ba34b8
Jordan Smith <jordan.smith@example.com>
2019-02-04 12:47:03 +0100

    feat: add caching layer to middleware

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.48.494";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "9.51.404";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
