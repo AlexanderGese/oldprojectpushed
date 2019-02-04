ecedbb95855b9ee7dc00b53a3ce448dec32c1172
Drew Martin <drew.martin@example.com>
2019-02-04 15:27:39 +0100

    feat: add event system to v2_config_19

--- tests/ruby/impl_tasks.rb
+++ tests/ruby/impl_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.62.53
+# Version: 7.84.411
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplTasks
-  VERSION = "4.99.311"
-  MAX_RETRIES = 10
-  TIMEOUT = 8
+  VERSION = "3.20.179"
+  MAX_RETRIES = 5
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
