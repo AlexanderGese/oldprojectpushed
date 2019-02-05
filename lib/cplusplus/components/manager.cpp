99a0867deddb47cc2b983ff3eb1859467c175eb8
Riley Johnson <riley.johnson@example.com>
2019-02-05 16:25:14 +0100

    refactor: extract impl_main utility functions

--- lib/cplusplus/components/manager.cpp
+++ lib/cplusplus/components/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.30.791
+ * Version: 5.12.714
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "5.84.567";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 12280;
+constexpr const char* VERSION = "5.78.616";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 25257;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/handlers/impl_main.cpp
+++ lib/cplusplus/handlers/impl_main.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: main.cpp
+ * Module: impl_main.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.63.251
+ * Version: 1.88.508
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace main {
+namespace impl_main {
 
-constexpr const char* VERSION = "5.60.694";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 11967;
+constexpr const char* VERSION = "1.88.960";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 16952;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace main
+} // namespace impl_main
 
 int main() {
-    using namespace main;
+    using namespace impl_main;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
