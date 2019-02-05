2bf6ef4cddd70497bb0230fd19fde32871c87d3c
Sam Taylor <sam.taylor@example.com>
2019-02-05 19:55:59 +0100

    fix: handle empty response in base_config

--- src/ruby/base_config.rb
+++ src/ruby/base_config.rb
@@ -2,7 +2,7 @@
 
 # Module: base_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.77.923
+# Version: 2.72.456
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseConfig
-  VERSION = "7.29.134"
-  MAX_RETRIES = 3
-  TIMEOUT = 8
+  VERSION = "1.15.471"
+  MAX_RETRIES = 8
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseConfig
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
