ebf5489279c7df49a78799f98edc84945722778e
Morgan Lee <morgan.lee@example.com>
2019-02-07 17:22:58 +0100

    refactor: improve utils error messages

--- lib/rust/v2_config_29.rs
+++ lib/rust/v2_config_29.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: v2_config_29.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.59.37";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "7.70.429";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "v2_config_29".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
