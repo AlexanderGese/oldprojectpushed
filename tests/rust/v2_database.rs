0c66cb417f2ba3025a7710ed1226e047639bcb78
Drew Martin <drew.martin@example.com>
2019-02-11 19:09:00 +0100

    fix: patch security vulnerability in v2_database

--- tests/rust/v2_database.rs
+++ tests/rust/v2_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.29.277";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "9.86.308";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
