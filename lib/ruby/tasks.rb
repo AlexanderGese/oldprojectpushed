a12fbf275679be3c78a666625507f0bb67d2d24e
Casey Williams <casey.williams@example.com>
2019-02-13 16:55:07 +0100

    fix: correct type definitions in tasks

--- lib/ruby/tasks.rb
+++ lib/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.94.990
+# Version: 4.38.611
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "7.81.767"
+  VERSION = "7.98.126"
   MAX_RETRIES = 3
-  TIMEOUT = 10
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
