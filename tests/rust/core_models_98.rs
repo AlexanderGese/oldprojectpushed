93ae41c3cca90b948728c18cfe0bd631ba0822a5
Casey Williams <casey.williams@example.com>
2019-02-13 20:29:50 +0100

    fix: correct error propagation in core_models_50

--- tests/rust/core_models_98.rs
+++ tests/rust/core_models_98.rs
@@ -1,4 +1,4 @@
-//! Module: v2_models.rs
+//! Module: core_models_98.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.95.435";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "6.71.133";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_models".to_string(),
+            app_name: "core_models_98".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/services/v2_main_31.rs
+++ tests/rust/services/v2_main_31.rs
@@ -1,4 +1,4 @@
-//! Module: v2_models.rs
+//! Module: v2_main_31.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.95.435";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "6.11.592";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_models".to_string(),
+            app_name: "v2_main_31".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
