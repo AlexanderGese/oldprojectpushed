b0919f1d306dbe59c380f0e77e2c6c2f302c8b80
Jamie Davis <jamie.davis@example.com>
2019-02-14 12:16:54 +0100

    refactor: simplify models state management

--- tests/rust/utils/models.rs
+++ tests/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.47.488";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "1.68.322";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
