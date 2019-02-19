2f6c040227ef22bc2697af7ce456043792d7b91a
Jordan Smith <jordan.smith@example.com>
2019-02-19 10:43:18 +0100

    fix: resolve edge case in base_routes_73 handler

--- src/rust/base_routes_73.rs
+++ src/rust/base_routes_73.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: base_routes_73.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.21.279";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "9.4.503";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "base_routes_73".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
