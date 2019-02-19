29626b404544e2cce2a5e13a7840a59213197fe2
Alex Chen <alex.chen@example.com>
2019-02-19 08:49:17 +0100

    refactor: decouple core_app dependencies

--- lib/ruby/core_app.rb
+++ lib/ruby/core_app.rb
@@ -2,7 +2,7 @@
 
 # Module: core_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.69.247
+# Version: 3.38.980
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreApp
-  VERSION = "7.72.233"
-  MAX_RETRIES = 9
-  TIMEOUT = 7
+  VERSION = "6.55.812"
+  MAX_RETRIES = 4
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreApp
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
--- lib/ruby/middleware/validators.rb
+++ lib/ruby/middleware/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.95.93
+# Version: 6.17.29
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "2.16.646"
-  MAX_RETRIES = 7
-  TIMEOUT = 40
+  VERSION = "8.45.711"
+  MAX_RETRIES = 4
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
