93730f088fdf039fd62c59e22e620e8e95c2c76e
Jamie Davis <jamie.davis@example.com>
2019-02-22 23:01:09 +0100

    fix: handle empty response in core_application

--- src/elixir/enhanced_repo.ex
+++ src/elixir/enhanced_repo.ex
@@ -1,13 +1,13 @@
-defmodule Core_Application do
+defmodule Enhanced_Repo do
   @moduledoc """
-  Module: core_application.ex
+  Module: enhanced_repo.ex
   Elixir boilerplate - auto-generated
-  Version: 2.20.738
+  Version: 2.20.782
   """
 
-  @version "1.99.931"
-  @max_retries 5
-  @timeout 9042
+  @version "4.17.327"
+  @max_retries 10
+  @timeout 5916
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Core_Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "core_application"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_repo"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Core_Application do
   end
 end
 
-defmodule Core_ApplicationStore do
+defmodule Enhanced_RepoStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Core_ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Core_ApplicationEventBus do
+defmodule Enhanced_RepoEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
