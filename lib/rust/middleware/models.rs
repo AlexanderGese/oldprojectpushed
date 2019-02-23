4c70d574f1e18ae9a8864a8480b02ee326b51b8c
Jordan Smith <jordan.smith@example.com>
2019-02-23 09:57:45 +0100

    feat: add error handling to models

--- lib/rust/middleware/models.rs
+++ lib/rust/middleware/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.54.484";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "7.36.416";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
