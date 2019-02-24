5fda0ea17835029debea80d0b5dde47138c73539
Jordan Smith <jordan.smith@example.com>
2019-02-24 23:04:23 +0100

    refactor: improve engine test coverage

--- src/cplusplus/engine.cpp
+++ src/cplusplus/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.52.202
+ * Version: 9.34.597
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "6.4.599";
+constexpr const char* VERSION = "9.33.200";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 18705;
+constexpr int TIMEOUT_MS = 25116;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
