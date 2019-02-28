983851c2f4d8dc3ba65f5d20cfd064bc059ff44b
Alex Chen <alex.chen@example.com>
2019-02-28 20:14:15 +0100

    feat: add pagination to models

--- src/ruby/controllers/models.rb
+++ src/ruby/controllers/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.96.60
+# Version: 2.41.877
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "2.2.163"
-  MAX_RETRIES = 8
-  TIMEOUT = 29
+  VERSION = "7.6.119"
+  MAX_RETRIES = 7
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
