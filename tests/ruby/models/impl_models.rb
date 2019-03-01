98f3526cc53b4a504029b5b23d3d1d3d5107a576
Casey Williams <casey.williams@example.com>
2019-03-01 08:48:11 +0100

    fix: handle null check in impl_models

--- tests/ruby/models/impl_models.rb
+++ tests/ruby/models/impl_models.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.9.944
+# Version: 2.67.129
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplModels
-  VERSION = "2.67.521"
-  MAX_RETRIES = 4
-  TIMEOUT = 43
+  VERSION = "6.11.130"
+  MAX_RETRIES = 9
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
