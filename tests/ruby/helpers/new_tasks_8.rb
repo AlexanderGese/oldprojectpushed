37c3b3012a1a2b9e5270c8d91f70e21283b712f3
Drew Martin <drew.martin@example.com>
2019-03-02 11:28:35 +0100

    fix: handle concurrent access in new_tasks_8

--- tests/ruby/helpers/new_tasks_8.rb
+++ tests/ruby/helpers/new_tasks_8.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: new_tasks_8.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.82.490
+# Version: 4.24.868
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "6.80.475"
-  MAX_RETRIES = 7
-  TIMEOUT = 5
+module NewTasks8
+  VERSION = "1.1.287"
+  MAX_RETRIES = 10
+  TIMEOUT = 34
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "new_tasks_8")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = NewTasks8::Application.new
   app.start
 end
