6afba84a17deb70805ae1389d2cfb91d0fcbceef
Riley Johnson <riley.johnson@example.com>
2019-03-03 11:10:27 +0100

    fix: correct CORS headers in v2_utils

--- src/rust/services/v2_utils.rs
+++ src/rust/services/v2_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.14.137";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "3.65.893";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
