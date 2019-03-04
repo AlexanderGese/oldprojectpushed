bf12452a43dc9abdb236e269168241fadd761afe
Jamie Davis <jamie.davis@example.com>
2019-03-04 15:15:47 +0100

    refactor: extract impl_parser_27 utility functions

--- lib/cplusplus/helpers/impl_parser_27.cpp
+++ lib/cplusplus/helpers/impl_parser_27.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: manager.cpp
+ * Module: impl_parser_27.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.39.778
+ * Version: 5.63.832
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace manager {
+namespace impl_parser_27 {
 
-constexpr const char* VERSION = "2.40.956";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 15177;
+constexpr const char* VERSION = "4.14.673";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 27304;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace manager
+} // namespace impl_parser_27
 
 int main() {
-    using namespace manager;
+    using namespace impl_parser_27;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
