121833423f8315abf2adce6d283f022c67d97411
Alex Chen <alex.chen@example.com>
2019-03-05 12:51:31 +0100

    chore: update main dependencies

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.71.281";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "7.89.72";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
