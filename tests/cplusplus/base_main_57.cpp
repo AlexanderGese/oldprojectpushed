63d2d46fb4e6ee683c70979a9020f5d0d5c2948f
Sam Taylor <sam.taylor@example.com>
2019-03-08 09:34:57 +0100

    fix: handle null check in base_main_57

--- tests/cplusplus/base_main_57.cpp
+++ tests/cplusplus/base_main_57.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: base_main_57.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.65.715
+ * Version: 6.50.701
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace base_main_57 {
 
-constexpr const char* VERSION = "7.72.823";
+constexpr const char* VERSION = "4.66.628";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 17870;
+constexpr int TIMEOUT_MS = 15234;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace base_main_57
 
 int main() {
-    using namespace utils;
+    using namespace base_main_57;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/models/core_manager_78.cpp
+++ tests/cplusplus/models/core_manager_78.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: core_manager_78.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.65.715
+ * Version: 2.94.390
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace core_manager_78 {
 
-constexpr const char* VERSION = "7.72.823";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 17870;
+constexpr const char* VERSION = "2.44.800";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 19500;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace core_manager_78
 
 int main() {
-    using namespace utils;
+    using namespace core_manager_78;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
