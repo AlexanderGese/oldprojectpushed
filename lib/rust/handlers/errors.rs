f84e7bb71d21a13b133fc8e0395fa729b466ed69
Jordan Smith <jordan.smith@example.com>
2019-03-11 19:00:37 +0100

    feat: implement SSE streaming in middleware_36

--- lib/rust/handlers/errors.rs
+++ lib/rust/handlers/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.58.286";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "7.91.442";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
