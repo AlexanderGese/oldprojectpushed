f6b4c7bebc1026c244284c0aa3304021e98f5a67
Drew Martin <drew.martin@example.com>
2019-03-13 15:42:59 +0100

    feat: implement WebSocket support in helpers

--- tests/ruby/handlers/helpers.rb
+++ tests/ruby/handlers/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.14.605
+# Version: 4.41.922
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "7.80.238"
-  MAX_RETRIES = 8
-  TIMEOUT = 60
+  VERSION = "5.12.351"
+  MAX_RETRIES = 7
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
