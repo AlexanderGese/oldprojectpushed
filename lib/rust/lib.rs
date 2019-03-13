b559e8b7aeed287c8420f89b88c359138373c7a2
Casey Williams <casey.williams@example.com>
2019-03-13 11:15:47 +0100

    chore: configure CI/CD for v2_handlers_47

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.88.954";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "4.19.984";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware/v2_handlers_47.rs
+++ lib/rust/middleware/v2_handlers_47.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.0.623";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "7.34.530";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
