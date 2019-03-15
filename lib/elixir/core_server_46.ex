bc572faee07607ff432cff013095bfaa98ce3c51
Riley Johnson <riley.johnson@example.com>
2019-03-15 09:47:38 +0100

    test: add unit tests for core_server_46

--- lib/elixir/core_server_46.ex
+++ lib/elixir/core_server_46.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule Core_Server_46 do
   @moduledoc """
-  Module: worker.ex
+  Module: core_server_46.ex
   Elixir boilerplate - auto-generated
-  Version: 1.26.80
+  Version: 5.90.258
   """
 
-  @version "4.32.834"
-  @max_retries 7
-  @timeout 21637
+  @version "3.69.443"
+  @max_retries 10
+  @timeout 6985
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "core_server_46"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule Core_Server_46Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule Core_Server_46EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
