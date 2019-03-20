e78a65e47849f6dfe3380023aa0f37bcd3b93dee
Riley Johnson <riley.johnson@example.com>
2019-03-20 19:35:27 +0100

    feat: add error handling to config

--- tests/rust/config_81.rs
+++ tests/rust/config_81.rs
@@ -1,4 +1,4 @@
-//! Module: new_lib.rs
+//! Module: config_81.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.15.258";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "1.48.423";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_lib".to_string(),
+            app_name: "config_81".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
