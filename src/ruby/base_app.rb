5f2d451f1ef72a142fd272514466fedbfaa2d994
Riley Johnson <riley.johnson@example.com>
2019-03-22 18:30:23 +0100

    fix: handle large payloads in base_app

--- src/ruby/base_app.rb
+++ src/ruby/base_app.rb
@@ -2,7 +2,7 @@
 
 # Module: base_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.37.844
+# Version: 3.87.763
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseApp
-  VERSION = "1.71.558"
+  VERSION = "7.33.632"
   MAX_RETRIES = 4
-  TIMEOUT = 14
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseApp
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
