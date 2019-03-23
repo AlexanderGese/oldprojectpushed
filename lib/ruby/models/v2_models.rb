02fad55e9b16860e1492ab0f9f7508911527ba1e
Jordan Smith <jordan.smith@example.com>
2019-03-23 21:27:18 +0100

    fix: patch security vulnerability in validators

--- lib/ruby/models/v2_models.rb
+++ lib/ruby/models/v2_models.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.27.405
+# Version: 4.82.174
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Models
-  VERSION = "5.39.490"
-  MAX_RETRIES = 8
-  TIMEOUT = 12
+  VERSION = "8.79.242"
+  MAX_RETRIES = 6
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
