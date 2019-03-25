928a565fcec1a46f1a0c517180a7a40a15bfe7aa
Morgan Lee <morgan.lee@example.com>
2019-03-25 09:46:13 +0100

    chore: remove deprecated lib code

--- src/rust/controllers/handlers.rs
+++ src/rust/controllers/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.3.42";
+const VERSION: &str = "6.78.134";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 29;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
