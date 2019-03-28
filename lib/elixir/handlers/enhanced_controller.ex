f9c982f55d31287f3fd1d1d171a4fe15070de6b6
Sam Taylor <sam.taylor@example.com>
2019-03-28 15:23:56 +0100

    feat: add logging to server

--- lib/elixir/handlers/enhanced_controller.ex
+++ lib/elixir/handlers/enhanced_controller.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Enhanced_Controller do
   @moduledoc """
-  Module: server.ex
+  Module: enhanced_controller.ex
   Elixir boilerplate - auto-generated
-  Version: 7.33.988
+  Version: 7.27.673
   """
 
-  @version "9.52.122"
-  @max_retries 8
-  @timeout 19047
+  @version "6.69.724"
+  @max_retries 5
+  @timeout 8858
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_controller"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule Enhanced_ControllerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule Enhanced_ControllerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- lib/elixir/services/impl_context.ex
+++ lib/elixir/services/impl_context.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Impl_Context do
   @moduledoc """
-  Module: server.ex
+  Module: impl_context.ex
   Elixir boilerplate - auto-generated
-  Version: 7.33.988
+  Version: 9.90.369
   """
 
-  @version "9.52.122"
-  @max_retries 8
-  @timeout 19047
+  @version "1.52.729"
+  @max_retries 10
+  @timeout 10927
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "impl_context"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule Impl_ContextStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule Impl_ContextEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
