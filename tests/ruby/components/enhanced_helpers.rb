64fc7ddb9a2a8cce2493bfde4bd0cdb179fa900d
Sam Taylor <sam.taylor@example.com>
2019-03-31 22:29:18 +0200

    feat: implement SSE streaming in v2_middleware_5

--- tests/ruby/components/enhanced_helpers.rb
+++ tests/ruby/components/enhanced_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: enhanced_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.68.461
+# Version: 1.84.402
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "1.26.540"
-  MAX_RETRIES = 3
-  TIMEOUT = 7
+module EnhancedHelpers
+  VERSION = "9.14.369"
+  MAX_RETRIES = 8
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "enhanced_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = EnhancedHelpers::Application.new
   app.start
 end
