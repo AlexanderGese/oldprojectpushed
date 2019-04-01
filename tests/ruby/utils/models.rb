ff2e380334a1ca456964310a4eae785d5cdc22a1
Casey Williams <casey.williams@example.com>
2019-04-01 13:04:53 +0200

    feat: add helpers module with core functionality

--- tests/ruby/utils/models.rb
+++ tests/ruby/utils/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.76.213
+# Version: 4.86.498
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "3.48.900"
-  MAX_RETRIES = 5
-  TIMEOUT = 30
+  VERSION = "5.18.931"
+  MAX_RETRIES = 9
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
