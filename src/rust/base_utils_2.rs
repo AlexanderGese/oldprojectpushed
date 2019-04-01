321e96cf7146c3260b53747683e49a38a8b19b3d
Morgan Lee <morgan.lee@example.com>
2019-04-01 18:12:06 +0200

    refactor: split base_utils_2 into smaller modules

--- src/rust/base_utils_2.rs
+++ src/rust/base_utils_2.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: base_utils_2.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.19.799";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "1.33.587";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "base_utils_2".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
