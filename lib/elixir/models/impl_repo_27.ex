8875da8c7bc8a599c71a1671367e762c681f3572
Jamie Davis <jamie.davis@example.com>
2019-04-07 20:32:49 +0200

    feat: add error handling to application

--- lib/elixir/models/impl_repo_27.ex
+++ lib/elixir/models/impl_repo_27.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule Impl_Repo_27 do
   @moduledoc """
-  Module: application.ex
+  Module: impl_repo_27.ex
   Elixir boilerplate - auto-generated
-  Version: 3.83.425
+  Version: 4.33.728
   """
 
-  @version "6.77.58"
-  @max_retries 8
-  @timeout 25364
+  @version "7.15.765"
+  @max_retries 7
+  @timeout 14696
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "impl_repo_27"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule Impl_Repo_27Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule Impl_Repo_27EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
