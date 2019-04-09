0d1333e609d8f013d108d4b8859fde3092a95092
Riley Johnson <riley.johnson@example.com>
2019-04-09 09:43:05 +0200

    feat: add search functionality to routes

--- lib/rust/handlers/routes.rs
+++ lib/rust/handlers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.61.1";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "1.97.814";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
