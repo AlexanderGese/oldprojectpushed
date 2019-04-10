325294ee874bfc766b571cd1e8a2eb3ad4e19e9a
Jordan Smith <jordan.smith@example.com>
2019-04-10 12:57:09 +0200

    chore: configure CI/CD for core_worker

--- lib/elixir/core_worker.ex
+++ lib/elixir/core_worker.ex
@@ -2,12 +2,12 @@ defmodule Core_Worker do
   @moduledoc """
   Module: core_worker.ex
   Elixir boilerplate - auto-generated
-  Version: 7.31.292
+  Version: 9.28.514
   """
 
-  @version "1.54.94"
-  @max_retries 10
-  @timeout 8118
+  @version "1.54.398"
+  @max_retries 5
+  @timeout 6087
 
   defstruct [
     :app_name,
