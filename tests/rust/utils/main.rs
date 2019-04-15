77e5aad4e697f32b082e69d38f888e9193de62d0
Sam Taylor <sam.taylor@example.com>
2019-04-15 12:38:28 +0200

    feat: implement authentication in main

--- tests/rust/utils/main.rs
+++ tests/rust/utils/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.49.504";
+const VERSION: &str = "9.87.395";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 24;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
