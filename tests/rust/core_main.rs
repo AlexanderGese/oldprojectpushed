49f15232f14dced367435341895fb615e600de44
Sam Taylor <sam.taylor@example.com>
2019-04-15 23:21:35 +0200

    refactor: decouple new_routes dependencies

--- tests/rust/core_main.rs
+++ tests/rust/core_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.32.497";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "6.19.183";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.63.739";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "9.63.486";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
