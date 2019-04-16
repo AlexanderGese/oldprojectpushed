d64c07e22b13ede11b8b4f42c5feda9a5bea6f81
Alex Chen <alex.chen@example.com>
2019-04-16 11:24:10 +0200

    fix: correct CORS headers in enhanced_server

--- src/ruby/enhanced_server.rb
+++ src/ruby/enhanced_server.rb
@@ -2,7 +2,7 @@
 
 # Module: enhanced_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.82.621
+# Version: 5.43.782
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module EnhancedServer
-  VERSION = "9.71.560"
-  MAX_RETRIES = 6
-  TIMEOUT = 35
+  VERSION = "1.44.104"
+  MAX_RETRIES = 4
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module EnhancedServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
