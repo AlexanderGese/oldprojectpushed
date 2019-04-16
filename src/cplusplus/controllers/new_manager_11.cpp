3942e5105b7d018e1d3312cc7ecfbf6df6d2080e
Morgan Lee <morgan.lee@example.com>
2019-04-16 14:19:33 +0200

    refactor: decouple new_manager_11 dependencies

--- src/cplusplus/controllers/new_manager_11.cpp
+++ src/cplusplus/controllers/new_manager_11.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: v2_config.cpp
+ * Module: new_manager_11.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.50.659
+ * Version: 2.48.902
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace v2_config {
+namespace new_manager_11 {
 
-constexpr const char* VERSION = "3.46.924";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 24465;
+constexpr const char* VERSION = "5.12.430";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 22092;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace v2_config
+} // namespace new_manager_11
 
 int main() {
-    using namespace v2_config;
+    using namespace new_manager_11;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/services/config.cpp
+++ src/cplusplus/services/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.7.363
+ * Version: 7.40.603
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "3.54.252";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 24243;
+constexpr const char* VERSION = "1.69.554";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 26838;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
