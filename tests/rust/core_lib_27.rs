b919d551fa027e534b8f49fa38c855d7f932caf2
Sam Taylor <sam.taylor@example.com>
2019-04-17 13:45:41 +0200

    feat: implement WebSocket support in enhanced_errors_27

--- tests/rust/core_lib_27.rs
+++ tests/rust/core_lib_27.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: core_lib_27.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.46.395";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "7.28.550";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "core_lib_27".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
