3165c7d288059b0364402113c9ba9e32b4dcd643
Riley Johnson <riley.johnson@example.com>
2019-04-17 12:07:44 +0200

    feat: add rate limiting to validators

--- src/ruby/services/core_validators.rb
+++ src/ruby/services/core_validators.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: core_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.97.513
+# Version: 3.2.653
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "1.79.812"
-  MAX_RETRIES = 6
-  TIMEOUT = 60
+module CoreValidators
+  VERSION = "9.68.36"
+  MAX_RETRIES = 8
+  TIMEOUT = 54
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "core_validators")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = CoreValidators::Application.new
   app.start
 end
