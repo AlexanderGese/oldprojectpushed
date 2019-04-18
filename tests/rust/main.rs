74f6a47f3134b90aac2fb058118d7ce4c273242a
Riley Johnson <riley.johnson@example.com>
2019-04-18 22:43:54 +0200

    chore: update v2_errors_52 dependencies

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.58.383";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "6.77.42";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
