741489c5ba8c27a0610a7ba1544cadcecbde676a
Jordan Smith <jordan.smith@example.com>
2019-04-18 23:22:58 +0200

    feat: add validation to impl_app_36 inputs

--- tests/ruby/impl_app_36.rb
+++ tests/ruby/impl_app_36.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_app_36.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.5.111
+# Version: 6.18.494
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplApp36
-  VERSION = "6.72.366"
+  VERSION = "4.69.362"
   MAX_RETRIES = 7
-  TIMEOUT = 60
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplApp36
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
--- tests/ruby/middleware/config.rb
+++ tests/ruby/middleware/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.45.402
+# Version: 1.62.582
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "3.93.205"
-  MAX_RETRIES = 5
-  TIMEOUT = 26
+  VERSION = "4.87.534"
+  MAX_RETRIES = 7
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
