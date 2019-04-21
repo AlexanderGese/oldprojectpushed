0dfc21313ada881a5cec4eb9573c147f979dbb72
Jordan Smith <jordan.smith@example.com>
2019-04-21 09:16:38 +0200

    test: add unit tests for base_errors

--- lib/rust/middleware/base_errors.rs
+++ lib/rust/middleware/base_errors.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: base_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.65.891";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "3.74.120";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "base_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
