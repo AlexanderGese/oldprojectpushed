01f08897332d587a36bc4a09c5f11140b7d2f842
Alex Chen <alex.chen@example.com>
2019-04-21 19:40:45 +0200

    chore: configure CI/CD for models

--- tests/ruby/models/core_server_86.rb
+++ tests/ruby/models/core_server_86.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: core_server_86.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.31.447
+# Version: 2.56.809
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "4.72.233"
-  MAX_RETRIES = 4
-  TIMEOUT = 28
+module CoreServer86
+  VERSION = "1.36.796"
+  MAX_RETRIES = 3
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "core_server_86")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = CoreServer86::Application.new
   app.start
 end
