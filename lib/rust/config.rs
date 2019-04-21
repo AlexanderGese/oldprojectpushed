abab82ee6ffbddfc4376497259b9d6befe46423b
Jordan Smith <jordan.smith@example.com>
2019-04-21 21:15:02 +0200

    docs: add documentation for lib

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.18.154";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "2.51.341";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
