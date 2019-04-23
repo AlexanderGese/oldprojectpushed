33aa650076cc20b6c5f12b93d410d7d2255dfa08
Casey Williams <casey.williams@example.com>
2019-04-23 15:42:53 +0200

    feat: implement WebSocket support in models

--- src/ruby/handlers/impl_app.rb
+++ src/ruby/handlers/impl_app.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: impl_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.24.356
+# Version: 3.37.161
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "3.79.656"
-  MAX_RETRIES = 9
-  TIMEOUT = 18
+module ImplApp
+  VERSION = "3.52.776"
+  MAX_RETRIES = 6
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "impl_app")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = ImplApp::Application.new
   app.start
 end
