10f2d804216ddc83677c3c91fc7a294dff4df165
Jamie Davis <jamie.davis@example.com>
2019-04-27 10:32:23 +0200

    chore: configure CI/CD for v2_handlers_57

--- src/rust/database.rs
+++ src/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.78.85";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "8.1.592";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/models/v2_handlers_57.rs
+++ src/rust/models/v2_handlers_57.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: v2_handlers_57.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.56.439";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "5.87.733";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "v2_handlers_57".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
