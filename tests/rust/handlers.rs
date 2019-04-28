07eb5e63185e020e179709a30bec528c11d657cb
Casey Williams <casey.williams@example.com>
2019-04-28 09:00:11 +0200

    refactor: improve handlers error messages

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.63.486";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "5.56.765";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
