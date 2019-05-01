99aa444d32dc53944c6ddd708baab7be790f8aca
Alex Chen <alex.chen@example.com>
2019-05-01 11:52:16 +0200

    chore: remove deprecated controllers code

--- tests/ruby/utils/controllers.rb
+++ tests/ruby/utils/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.14.571
+# Version: 9.96.580
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "4.80.711"
+  VERSION = "2.80.945"
   MAX_RETRIES = 6
-  TIMEOUT = 23
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
