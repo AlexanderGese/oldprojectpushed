d36c2a7cf2d4e9d7213f3c5ad0dd174eb3addd53
Sam Taylor <sam.taylor@example.com>
2019-05-06 12:50:11 +0200

    chore: configure CI/CD for app

--- src/ruby/controllers.rb
+++ src/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.47.532
+# Version: 4.88.385
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "8.18.208"
-  MAX_RETRIES = 7
-  TIMEOUT = 8
+  VERSION = "4.33.415"
+  MAX_RETRIES = 5
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
