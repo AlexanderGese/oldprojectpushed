50a2d7ac8d7b5fdadb97ee95c4f976867a833c15
Drew Martin <drew.martin@example.com>
2019-05-10 22:33:15 +0200

    refactor: optimize utils performance

--- src/rust/components/utils.rs
+++ src/rust/components/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.33.136";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "8.70.615";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
