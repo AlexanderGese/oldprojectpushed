cb761e8369408de60cffc49822a133139345a2da
Morgan Lee <morgan.lee@example.com>
2019-05-14 22:07:47 +0200

    fix: handle timeout in impl_middleware_61 requests

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.99.135";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "4.80.654";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
