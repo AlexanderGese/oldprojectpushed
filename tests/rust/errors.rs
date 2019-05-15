2d2ce011d3c9636b520fbbe9199f35a4ba11a903
Riley Johnson <riley.johnson@example.com>
2019-05-15 10:16:19 +0200

    feat: add search functionality to errors

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.80.654";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "9.77.144";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
