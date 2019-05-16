a9ac1a671c1e2d45bfa0864e30365b4974275676
Jamie Davis <jamie.davis@example.com>
2019-05-16 18:16:21 +0200

    refactor: migrate new_main to async/await

--- lib/cplusplus/models/manager.cpp
+++ lib/cplusplus/models/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.27.172
+ * Version: 3.86.120
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "8.88.920";
+constexpr const char* VERSION = "5.2.222";
 constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 23185;
+constexpr int TIMEOUT_MS = 17446;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/new_main.cpp
+++ lib/cplusplus/new_main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: new_main.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.59.503
+ * Version: 3.57.437
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace new_main {
 
-constexpr const char* VERSION = "6.92.247";
+constexpr const char* VERSION = "3.98.271";
 constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 16298;
+constexpr int TIMEOUT_MS = 6120;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
