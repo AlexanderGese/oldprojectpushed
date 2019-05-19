34ae628321a7b3f292aa6eadfa25fd304fb28cf8
Jordan Smith <jordan.smith@example.com>
2019-05-19 23:39:00 +0200

    chore: clean up utils imports

--- tests/rust/models/core_database_67.rs
+++ tests/rust/models/core_database_67.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: core_database_67.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.87.751";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "9.74.166";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "core_database_67".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
