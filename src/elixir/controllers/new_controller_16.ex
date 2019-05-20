f00ee7b609e872b3e762b44b9c42b503b40da412
Jamie Davis <jamie.davis@example.com>
2019-05-20 18:35:16 +0200

    refactor: improve server error messages

--- src/elixir/controllers/new_controller_16.ex
+++ src/elixir/controllers/new_controller_16.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule New_Controller_16 do
   @moduledoc """
-  Module: server.ex
+  Module: new_controller_16.ex
   Elixir boilerplate - auto-generated
-  Version: 9.64.198
+  Version: 5.55.786
   """
 
-  @version "6.4.271"
-  @max_retries 9
-  @timeout 5117
+  @version "1.76.388"
+  @max_retries 10
+  @timeout 6610
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "new_controller_16"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule New_Controller_16Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule New_Controller_16EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
