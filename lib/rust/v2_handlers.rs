ba6b8aa8209da27102c3e80d19627dbcf868e200
Morgan Lee <morgan.lee@example.com>
2019-05-21 08:01:41 +0200

    chore: update enhanced_routes dependencies

--- lib/rust/v2_handlers.rs
+++ lib/rust/v2_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.25.923";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "5.23.470";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
