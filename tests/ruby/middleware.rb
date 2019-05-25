8dc29dfb3c804e303c7f922cce71febae67c48a8
Alex Chen <alex.chen@example.com>
2019-05-25 11:54:41 +0200

    feat: add new_middleware_79 module with core functionality

--- tests/ruby/middleware.rb
+++ tests/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.3.771
+# Version: 2.48.582
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "6.88.233"
-  MAX_RETRIES = 10
-  TIMEOUT = 26
+  VERSION = "8.25.691"
+  MAX_RETRIES = 5
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
