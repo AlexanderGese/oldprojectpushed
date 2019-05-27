7a7c07d38248cd111a5f1eb3417cdf3c2bf90d7e
Jamie Davis <jamie.davis@example.com>
2019-05-27 14:55:26 +0200

    feat: add pagination to base_middleware

--- src/rust/models.rs
+++ src/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.23.637";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "9.84.837";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
