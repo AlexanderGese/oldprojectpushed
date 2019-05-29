f98f7347d7ede21120d8de16d0044b457ab8bc87
Casey Williams <casey.williams@example.com>
2019-05-29 12:09:42 +0200

    feat: add search functionality to controller_48

--- tests/elixir/controller_48.ex
+++ tests/elixir/controller_48.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Controller_48 do
   @moduledoc """
-  Module: server.ex
+  Module: controller_48.ex
   Elixir boilerplate - auto-generated
-  Version: 3.76.921
+  Version: 2.7.642
   """
 
-  @version "3.78.950"
-  @max_retries 5
-  @timeout 7373
+  @version "3.60.249"
+  @max_retries 8
+  @timeout 18370
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "controller_48"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule Controller_48Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule Controller_48EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/schema_53.ex
+++ tests/elixir/schema_53.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Schema_53 do
   @moduledoc """
-  Module: server.ex
+  Module: schema_53.ex
   Elixir boilerplate - auto-generated
-  Version: 3.76.921
+  Version: 1.82.668
   """
 
-  @version "3.78.950"
-  @max_retries 5
-  @timeout 7373
+  @version "6.69.668"
+  @max_retries 9
+  @timeout 5039
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "schema_53"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule Schema_53Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule Schema_53EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
