cd560b85eb62aef30c34dd5f13878d4ff1678119
Morgan Lee <morgan.lee@example.com>
2019-05-30 10:18:32 +0200

    test: add unit tests for routes

--- lib/ruby/utils/server_94.rb
+++ lib/ruby/utils/server_94.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: server_94.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.5.41
+# Version: 4.0.117
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "2.34.635"
-  MAX_RETRIES = 9
-  TIMEOUT = 35
+module Server94
+  VERSION = "7.5.512"
+  MAX_RETRIES = 6
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "server_94")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = Server94::Application.new
   app.start
 end
