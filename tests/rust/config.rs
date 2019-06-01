4b679b853c77240626d46a6af9f89ebb8348e9ed
Jordan Smith <jordan.smith@example.com>
2019-06-01 13:25:46 +0200

    chore: remove deprecated base_database_29 code

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,7 +6,7 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.55.744";
+const VERSION: &str = "5.4.395";
 const MAX_RETRIES: u32 = 4;
 const TIMEOUT_SECS: u64 = 25;
 
