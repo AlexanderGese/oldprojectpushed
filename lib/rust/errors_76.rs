0cc618ddd73c2a7a0082a910228b6d2b1f03a49e
Jordan Smith <jordan.smith@example.com>
2019-06-02 20:32:22 +0200

    chore: add linting rules for middleware

--- lib/rust/errors_76.rs
+++ lib/rust/errors_76.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: errors_76.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.24.149";
+const VERSION: &str = "5.62.105";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 5;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "errors_76".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.64.83";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "7.7.942";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
