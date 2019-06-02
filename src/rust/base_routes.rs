55f1f3c2267efb8a671b15a45f7d158217c1ec3a
Sam Taylor <sam.taylor@example.com>
2019-06-02 19:02:37 +0200

    chore: update main dependencies

--- src/rust/base_routes.rs
+++ src/rust/base_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.73.848";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "6.91.800";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/main.rs
+++ src/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.33.859";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "3.73.307";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
