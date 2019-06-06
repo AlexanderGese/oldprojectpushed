9035f761ece963adff42e4b335256f1426933842
Morgan Lee <morgan.lee@example.com>
2019-06-06 16:00:40 +0200

    chore: bump schema version

--- lib/elixir/middleware/schema.ex
+++ lib/elixir/middleware/schema.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Schema do
   @moduledoc """
-  Module: server.ex
+  Module: schema.ex
   Elixir boilerplate - auto-generated
-  Version: 8.34.268
+  Version: 4.9.283
   """
 
-  @version "3.51.751"
-  @max_retries 10
-  @timeout 14113
+  @version "8.19.902"
+  @max_retries 6
+  @timeout 27058
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "schema"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule SchemaStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule SchemaEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- lib/elixir/services/worker.ex
+++ lib/elixir/services/worker.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Worker do
   @moduledoc """
-  Module: server.ex
+  Module: worker.ex
   Elixir boilerplate - auto-generated
-  Version: 8.34.268
+  Version: 2.99.125
   """
 
-  @version "3.51.751"
-  @max_retries 10
-  @timeout 14113
+  @version "2.24.607"
+  @max_retries 8
+  @timeout 15909
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "worker"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule WorkerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule WorkerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
