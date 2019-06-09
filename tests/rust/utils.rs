fc408609a6ccad1d9f3970d414b2f9af6587a6f1
Casey Williams <casey.williams@example.com>
2019-06-09 12:43:07 +0200

    fix: handle null check in lib

--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.81.794";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "2.57.356";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
