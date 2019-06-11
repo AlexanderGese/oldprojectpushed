7104b7e7500336129b710c468c5f1174183d27b3
Sam Taylor <sam.taylor@example.com>
2019-06-11 08:39:25 +0200

    fix: handle large payloads in server

--- lib/ruby/models/server.rb
+++ lib/ruby/models/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.60.851
+# Version: 4.93.444
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "2.30.723"
-  MAX_RETRIES = 8
-  TIMEOUT = 57
+  VERSION = "1.90.373"
+  MAX_RETRIES = 6
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
