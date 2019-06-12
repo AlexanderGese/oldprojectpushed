dc1a3a00e793d8903a864618c6450d9358276420
Casey Williams <casey.williams@example.com>
2019-06-12 08:14:55 +0200

    fix: correct error propagation in core_middleware

--- tests/ruby/handlers/v2_tasks.rb
+++ tests/ruby/handlers/v2_tasks.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_middleware.rb
+# Module: v2_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.82.442
+# Version: 8.91.390
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreMiddleware
-  VERSION = "1.2.735"
-  MAX_RETRIES = 5
-  TIMEOUT = 30
+module V2Tasks
+  VERSION = "1.55.282"
+  MAX_RETRIES = 8
+  TIMEOUT = 16
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_middleware")
+      @app_name = opts.fetch(:app_name, "v2_tasks")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreMiddleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreMiddleware::Application.new
+  app = V2Tasks::Application.new
   app.start
 end
