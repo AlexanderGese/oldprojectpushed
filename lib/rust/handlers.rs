bab2af0af529d376badaedc4feff3d50275c33c3
Jamie Davis <jamie.davis@example.com>
2019-06-12 11:49:29 +0200

    feat: add health check endpoint to errors

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.8.476";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "7.59.639";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
