e19d81d8994d301e9fcbad06e7f4d25f753f5c44
Sam Taylor <sam.taylor@example.com>
2019-06-15 09:14:02 +0200

    docs: add documentation for models

--- tests/rust/enhanced_errors.rs
+++ tests/rust/enhanced_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.23.964";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "3.79.445";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.81.598";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "1.3.417";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
