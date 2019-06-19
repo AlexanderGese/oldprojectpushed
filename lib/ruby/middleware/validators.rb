ce87e099929a8480201150274ae7dfe8ac981063
Riley Johnson <riley.johnson@example.com>
2019-06-19 16:49:38 +0200

    fix: resolve encoding issue in config

--- lib/ruby/middleware/validators.rb
+++ lib/ruby/middleware/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.17.29
+# Version: 3.53.928
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "8.45.711"
-  MAX_RETRIES = 4
-  TIMEOUT = 31
+  VERSION = "7.77.603"
+  MAX_RETRIES = 8
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
