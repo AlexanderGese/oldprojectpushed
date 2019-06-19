74cebd25ac7c4ffacee432b817de56770daec12b
Riley Johnson <riley.johnson@example.com>
2019-06-19 08:37:37 +0200

    feat: implement controllers service layer

--- tests/ruby/core_middleware_84.rb
+++ tests/ruby/core_middleware_84.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: core_middleware_84.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.2.469
+# Version: 9.67.982
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "1.16.22"
-  MAX_RETRIES = 9
-  TIMEOUT = 60
+module CoreMiddleware84
+  VERSION = "9.68.562"
+  MAX_RETRIES = 6
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "core_middleware_84")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = CoreMiddleware84::Application.new
   app.start
 end
