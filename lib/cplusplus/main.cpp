e5d76f74674bea97470e67655ed1fd4fd2d9e002
Jordan Smith <jordan.smith@example.com>
2019-06-19 18:16:37 +0200

    chore: update main dependencies

--- lib/cplusplus/main.cpp
+++ lib/cplusplus/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.18.213
+ * Version: 3.65.662
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "8.19.201";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 10765;
+constexpr const char* VERSION = "2.52.625";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 8934;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
