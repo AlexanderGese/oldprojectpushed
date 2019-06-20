8c14094c8a766a5a9540a5347c3ce7955bdd58cc
Alex Chen <alex.chen@example.com>
2019-06-20 08:57:20 +0200

    refactor: decouple server dependencies

--- tests/ruby/helpers/validators_45.rb
+++ tests/ruby/helpers/validators_45.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: validators_45.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.6.78
+# Version: 5.54.308
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "7.6.760"
-  MAX_RETRIES = 9
-  TIMEOUT = 49
+module Validators45
+  VERSION = "5.24.491"
+  MAX_RETRIES = 4
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "validators_45")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = Validators45::Application.new
   app.start
 end
