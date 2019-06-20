c1daf84cce87647c2502b26d2471139d40952efb
Jordan Smith <jordan.smith@example.com>
2019-06-20 16:32:33 +0200

    feat: add event system to middleware

--- src/ruby/models/middleware.rb
+++ src/ruby/models/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.45.910
+# Version: 9.16.638
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "3.65.790"
-  MAX_RETRIES = 7
-  TIMEOUT = 59
+  VERSION = "5.20.604"
+  MAX_RETRIES = 10
+  TIMEOUT = 54
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
