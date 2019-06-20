fc203c05edf4ac1b4b46e238d2069768f56b5289
Casey Williams <casey.williams@example.com>
2019-06-20 10:32:13 +0200

    feat: implement SSE streaming in new_router

--- tests/elixir/controllers/new_router.ex
+++ tests/elixir/controllers/new_router.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule New_Router do
   @moduledoc """
-  Module: helpers.ex
+  Module: new_router.ex
   Elixir boilerplate - auto-generated
-  Version: 9.27.593
+  Version: 1.85.717
   """
 
-  @version "5.43.924"
+  @version "1.92.462"
   @max_retries 3
-  @timeout 21676
+  @timeout 9350
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "new_router"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule New_RouterStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule New_RouterEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/models/core_router.ex
+++ tests/elixir/models/core_router.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Core_Router do
   @moduledoc """
-  Module: helpers.ex
+  Module: core_router.ex
   Elixir boilerplate - auto-generated
-  Version: 9.27.593
+  Version: 7.35.65
   """
 
-  @version "5.43.924"
+  @version "6.30.505"
   @max_retries 3
-  @timeout 21676
+  @timeout 15002
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "core_router"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule Core_RouterStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule Core_RouterEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
