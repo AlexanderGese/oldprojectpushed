c8071854f8753999baa6114685acbf8ffc8c6fc4
Jordan Smith <jordan.smith@example.com>
2019-06-25 09:22:54 +0200

    fix: correct error propagation in errors

--- lib/rust/handlers/errors.rs
+++ lib/rust/handlers/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.91.442";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "8.41.155";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
