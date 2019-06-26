4ddaf7678f97f32466ac1e7d0e5ca6d17202eacf
Jamie Davis <jamie.davis@example.com>
2019-06-26 11:16:16 +0200

    fix: correct CORS headers in v2_handlers_24

--- tests/rust/handlers/routes.rs
+++ tests/rust/handlers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.7.753";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "7.58.169";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/services/handlers_25.rs
+++ tests/rust/services/handlers_25.rs
@@ -1,4 +1,4 @@
-//! Module: new_utils.rs
+//! Module: handlers_25.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.75.786";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "4.82.190";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_utils".to_string(),
+            app_name: "handlers_25".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
