12559c873b36cbb268b635ba22382b8f0c1d3145
Drew Martin <drew.martin@example.com>
2019-06-28 16:24:43 +0200

    refactor: decouple core_lib dependencies

--- tests/rust/components/middleware.rs
+++ tests/rust/components/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.52.952";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "9.33.430";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/core_lib.rs
+++ tests/rust/core_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.21.670";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "5.37.570";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
