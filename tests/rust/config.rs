59953fc73269169ba4f8a0a7b6d7a0f5c060368b
Alex Chen <alex.chen@example.com>
2019-06-30 18:53:40 +0200

    refactor: migrate core_handlers_62 to async/await

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.43.330";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "8.48.963";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/core_handlers_62.rs
+++ tests/rust/core_handlers_62.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.73.676";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "8.68.889";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
