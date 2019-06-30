8acd0edcd59fcf47b5e68e002fc7e97735f736c4
Casey Williams <casey.williams@example.com>
2019-06-30 20:04:41 +0200

    feat: implement retry logic in v2_middleware

--- lib/ruby/v2_middleware.rb
+++ lib/ruby/v2_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.73.940
+# Version: 2.83.339
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Middleware
-  VERSION = "9.86.97"
-  MAX_RETRIES = 5
-  TIMEOUT = 15
+  VERSION = "3.62.108"
+  MAX_RETRIES = 7
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
