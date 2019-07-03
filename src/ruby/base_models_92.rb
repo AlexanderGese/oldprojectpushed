133a69f802603ed1562c3d73f50999e2647f3823
Jordan Smith <jordan.smith@example.com>
2019-07-03 13:16:10 +0200

    fix: correct error propagation in new_middleware

--- src/ruby/base_models_92.rb
+++ src/ruby/base_models_92.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_middleware.rb
+# Module: base_models_92.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.56.733
+# Version: 5.40.870
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewMiddleware
-  VERSION = "1.12.311"
-  MAX_RETRIES = 6
-  TIMEOUT = 53
+module BaseModels92
+  VERSION = "7.30.474"
+  MAX_RETRIES = 8
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_middleware")
+      @app_name = opts.fetch(:app_name, "base_models_92")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewMiddleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewMiddleware::Application.new
+  app = BaseModels92::Application.new
   app.start
 end
