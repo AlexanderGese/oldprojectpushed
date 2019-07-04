5f55c1933ddc2ce283294651437d720c5b567a9e
Alex Chen <alex.chen@example.com>
2019-07-04 19:44:16 +0200

    chore: bump base_validators_57 version

--- tests/ruby/base_validators_57.rb
+++ tests/ruby/base_validators_57.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: base_validators_57.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.2.440
+# Version: 2.46.663
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "1.32.85"
-  MAX_RETRIES = 10
-  TIMEOUT = 23
+module BaseValidators57
+  VERSION = "3.67.509"
+  MAX_RETRIES = 6
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "base_validators_57")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = BaseValidators57::Application.new
   app.start
 end
