7de5e4df89d80310cfba21e1296445abf3729acb
Jamie Davis <jamie.davis@example.com>
2019-07-04 21:05:04 +0200

    feat: add middleware support to routes

--- src/ruby/components/tasks.rb
+++ src/ruby/components/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.1.184
+# Version: 7.25.657
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "6.33.756"
-  MAX_RETRIES = 6
-  TIMEOUT = 20
+  VERSION = "9.34.305"
+  MAX_RETRIES = 10
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- src/ruby/routes.rb
+++ src/ruby/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.79.171
+# Version: 2.18.293
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "8.59.939"
-  MAX_RETRIES = 3
+  VERSION = "9.99.676"
+  MAX_RETRIES = 9
   TIMEOUT = 34
 
   class Config
@@ -25,7 +25,7 @@ module Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
