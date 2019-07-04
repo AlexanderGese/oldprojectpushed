2777c9e23f65f8dad909410e556d85c1a42ab36a
Jordan Smith <jordan.smith@example.com>
2019-07-04 14:59:16 +0200

    docs: add documentation for base_helpers_62

--- lib/ruby/services/controllers_45.rb
+++ lib/ruby/services/controllers_45.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: controllers_45.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.74.469
+# Version: 2.87.397
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "8.67.457"
-  MAX_RETRIES = 10
-  TIMEOUT = 22
+module Controllers45
+  VERSION = "8.0.934"
+  MAX_RETRIES = 7
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "controllers_45")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = Controllers45::Application.new
   app.start
 end
