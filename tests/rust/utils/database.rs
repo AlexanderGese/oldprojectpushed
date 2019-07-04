4180aa9550b9b021a5f87442544b4f5df36d8f18
Alex Chen <alex.chen@example.com>
2019-07-04 19:24:47 +0200

    feat: add error handling to database

--- tests/rust/utils/database.rs
+++ tests/rust/utils/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.4.809";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "1.70.78";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
