172a98cb860617187351f96de370560c24db055c
Riley Johnson <riley.johnson@example.com>
2019-07-04 12:06:55 +0200

    refactor: improve logger test coverage

--- tests/cplusplus/logger.cpp
+++ tests/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.48.105
+ * Version: 2.45.253
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "2.97.25";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 19849;
+constexpr const char* VERSION = "2.80.732";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 13925;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/services/base_utils.cpp
+++ tests/cplusplus/services/base_utils.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: base_utils.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.65.706
+ * Version: 3.24.146
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace base_utils {
 
-constexpr const char* VERSION = "8.72.255";
+constexpr const char* VERSION = "1.88.758";
 constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 22347;
+constexpr int TIMEOUT_MS = 14086;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace base_utils
 
 int main() {
-    using namespace utils;
+    using namespace base_utils;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
