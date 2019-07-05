1f3cbd997fbac73c7e83d6323b029e5edc4a730d
Alex Chen <alex.chen@example.com>
2019-07-05 11:05:11 +0200

    chore: bump core_handlers_76 version

--- src/rust/core_handlers_76.rs
+++ src/rust/core_handlers_76.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: core_handlers_76.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.7.613";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "3.58.732";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "core_handlers_76".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
