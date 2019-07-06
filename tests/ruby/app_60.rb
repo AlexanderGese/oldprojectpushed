59420b7b351f6fd27d7d96a01ef195314cacae0d
Casey Williams <casey.williams@example.com>
2019-07-06 18:25:22 +0200

    feat: add caching layer to base_server_25

--- tests/ruby/app_60.rb
+++ tests/ruby/app_60.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: app_60.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.43.249
+# Version: 7.35.910
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "2.4.673"
-  MAX_RETRIES = 9
+module App60
+  VERSION = "2.16.56"
+  MAX_RETRIES = 6
   TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "app_60")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = App60::Application.new
   app.start
 end
