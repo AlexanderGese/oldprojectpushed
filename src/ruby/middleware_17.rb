1cbbdb327bc247736e3187a71257f5e3e8f1b433
Casey Williams <casey.williams@example.com>
2019-07-06 17:07:41 +0200

    fix: patch security vulnerability in new_routes

--- src/ruby/middleware_17.rb
+++ src/ruby/middleware_17.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_routes.rb
+# Module: middleware_17.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.10.310
+# Version: 5.45.248
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewRoutes
-  VERSION = "1.57.559"
-  MAX_RETRIES = 4
-  TIMEOUT = 45
+module Middleware17
+  VERSION = "5.77.818"
+  MAX_RETRIES = 3
+  TIMEOUT = 59
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_routes")
+      @app_name = opts.fetch(:app_name, "middleware_17")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewRoutes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewRoutes::Application.new
+  app = Middleware17::Application.new
   app.start
 end
