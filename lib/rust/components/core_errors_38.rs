22cd469e2a0785db59801211edfcf9a3e37a43d9
Riley Johnson <riley.johnson@example.com>
2019-07-07 15:35:25 +0200

    fix: correct error propagation in new_utils

--- lib/rust/components/core_errors_38.rs
+++ lib/rust/components/core_errors_38.rs
@@ -1,4 +1,4 @@
-//! Module: new_utils.rs
+//! Module: core_errors_38.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.2.303";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "5.65.649";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_utils".to_string(),
+            app_name: "core_errors_38".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
