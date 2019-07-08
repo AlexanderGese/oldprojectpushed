e0e3b72ffb2fb36d7fa857a6d4db5e82a9b4bf98
Casey Williams <casey.williams@example.com>
2019-07-08 22:43:00 +0200

    feat: add batch processing to errors

--- tests/rust/models/errors.rs
+++ tests/rust/models/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.89.53";
+const VERSION: &str = "4.89.122";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 7;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
