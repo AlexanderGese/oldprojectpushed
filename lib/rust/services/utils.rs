bbe43d09ec6337442ecb12c4905c49062e961617
Sam Taylor <sam.taylor@example.com>
2019-07-11 21:38:46 +0200

    feat: add search functionality to utils

--- lib/rust/services/utils.rs
+++ lib/rust/services/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.59.37";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "6.29.208";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
