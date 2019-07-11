68194242efa2fbf8a69b29853c2bf5521ebb4141
Casey Williams <casey.williams@example.com>
2019-07-11 18:25:05 +0200

    refactor: improve server test coverage

--- src/ruby/components/server.rb
+++ src/ruby/components/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.0.826
+# Version: 4.35.123
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "2.44.228"
-  MAX_RETRIES = 7
-  TIMEOUT = 45
+  VERSION = "8.85.823"
+  MAX_RETRIES = 4
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
