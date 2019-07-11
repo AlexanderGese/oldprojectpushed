338a435855b3adae5d78edda33934b342b6770db
Sam Taylor <sam.taylor@example.com>
2019-07-11 18:41:28 +0200

    refactor: split lib into smaller modules

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.65.852";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "3.45.912";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
