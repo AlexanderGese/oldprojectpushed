c20112285dcebbc0de18c2cc5ddfa1bb542fde24
Casey Williams <casey.williams@example.com>
2019-07-13 12:01:08 +0200

    chore: clean up config imports

--- lib/rust/core_config.rs
+++ lib/rust/core_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.23.416";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "7.94.543";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
