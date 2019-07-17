78b8305db279a66c518d107d254d7af4ecc0fd07
Alex Chen <alex.chen@example.com>
2019-07-17 18:25:36 +0200

    fix: correct type definitions in v2_middleware_37

--- tests/rust/services/new_database_52.rs
+++ tests/rust/services/new_database_52.rs
@@ -1,4 +1,4 @@
-//! Module: impl_main.rs
+//! Module: new_database_52.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.67.239";
+const VERSION: &str = "6.28.516";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 20;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_main".to_string(),
+            app_name: "new_database_52".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
