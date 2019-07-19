ea9376a423344fe39b7915938ba7c926693b0f16
Sam Taylor <sam.taylor@example.com>
2019-07-19 19:22:42 +0200

    refactor: split new_middleware_97 into smaller modules

--- tests/rust/models/lib.rs
+++ tests/rust/models/lib.rs
@@ -1,4 +1,4 @@
-//! Module: impl_lib.rs
+//! Module: lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.20.274";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "1.18.116";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_lib".to_string(),
+            app_name: "lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/new_middleware_97.rs
+++ tests/rust/new_middleware_97.rs
@@ -1,4 +1,4 @@
-//! Module: impl_lib.rs
+//! Module: new_middleware_97.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.20.274";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "4.57.887";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_lib".to_string(),
+            app_name: "new_middleware_97".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
