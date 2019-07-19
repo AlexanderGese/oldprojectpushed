148494383b8f02025dec8b631a13b7e603c05b41
Jamie Davis <jamie.davis@example.com>
2019-07-19 22:22:58 +0200

    fix: patch security vulnerability in routes_7

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.50.532";
+const VERSION: &str = "3.35.979";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 30;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
