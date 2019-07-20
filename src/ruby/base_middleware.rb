ab80f9722675b87b12f5a9e9be169cf2dedd9e8b
Alex Chen <alex.chen@example.com>
2019-07-20 20:01:28 +0200

    fix: resolve timezone issue in base_middleware

--- src/ruby/base_middleware.rb
+++ src/ruby/base_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: base_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.18.421
+# Version: 8.1.543
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseMiddleware
-  VERSION = "4.94.275"
+  VERSION = "3.16.331"
   MAX_RETRIES = 3
-  TIMEOUT = 17
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
