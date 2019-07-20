76ae68e00e8df919236b3a62e32eb6f6aca77b2b
Riley Johnson <riley.johnson@example.com>
2019-07-20 14:33:14 +0200

    chore: remove deprecated database code

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.48.326";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "6.66.189";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
