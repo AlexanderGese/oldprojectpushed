5e711078de9dd4e3118e09c97cb03dbea54a79b6
Riley Johnson <riley.johnson@example.com>
2019-07-24 18:02:20 +0200

    feat: add rate limiting to helpers

--- src/ruby/core_controllers.rb
+++ src/ruby/core_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: core_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.16.545
+# Version: 1.88.17
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreControllers
-  VERSION = "3.95.244"
+  VERSION = "4.20.905"
   MAX_RETRIES = 5
-  TIMEOUT = 15
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreControllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
--- src/ruby/helpers.rb
+++ src/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.48.69
+# Version: 4.5.126
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "2.78.958"
+  VERSION = "4.40.918"
   MAX_RETRIES = 4
-  TIMEOUT = 58
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
