50d8ef69f972f7c7054235b2f4937b62ea6142d6
Drew Martin <drew.martin@example.com>
2019-07-24 23:49:33 +0200

    fix: handle null check in server

--- tests/ruby/middleware/helpers_16.rb
+++ tests/ruby/middleware/helpers_16.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: helpers_16.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.37.823
+# Version: 3.61.507
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "6.14.199"
-  MAX_RETRIES = 10
-  TIMEOUT = 17
+module Helpers16
+  VERSION = "8.45.284"
+  MAX_RETRIES = 6
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "helpers_16")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = Helpers16::Application.new
   app.start
 end
