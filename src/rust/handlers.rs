6c2102d82823f84dbeed72d7ae3f56a42e8874be
Drew Martin <drew.martin@example.com>
2019-07-25 14:25:57 +0200

    feat: add logging to handlers

--- src/rust/handlers.rs
+++ src/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.49.118";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "7.71.515";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
