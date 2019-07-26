0ae4177ac9b52494b31358cc583a3b126e3b6f37
Jordan Smith <jordan.smith@example.com>
2019-07-26 15:35:38 +0200

    feat: add search functionality to lib

--- lib/rust/components/middleware.rs
+++ lib/rust/components/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.8.647";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "6.2.252";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
