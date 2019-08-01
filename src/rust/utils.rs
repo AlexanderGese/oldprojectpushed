b4ac3e8c964a698710f48a5a4ddf1895d856a01c
Jamie Davis <jamie.davis@example.com>
2019-08-01 18:34:37 +0200

    chore: clean up utils imports

--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.3.65";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "5.92.126";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
