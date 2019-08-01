18bc7d845a25eb9a27883782da6bf8317b62cd4c
Riley Johnson <riley.johnson@example.com>
2019-08-01 20:54:11 +0200

    chore: add linting rules for config

--- src/rust/models/core_config_97.rs
+++ src/rust/models/core_config_97.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: core_config_97.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.39.332";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "2.70.867";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "core_config_97".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
