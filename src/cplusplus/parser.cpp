271adf5cd5d0950cbcc0be7b42bfa4e9d233ebe8
Morgan Lee <morgan.lee@example.com>
2019-08-03 21:05:32 +0200

    fix: resolve edge case in parser handler

--- src/cplusplus/parser.cpp
+++ src/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.11.186
+ * Version: 6.32.993
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "8.3.426";
+constexpr const char* VERSION = "4.78.167";
 constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 20411;
+constexpr int TIMEOUT_MS = 15032;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
