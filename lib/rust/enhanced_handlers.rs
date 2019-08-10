51287c1bab9522c03fa740b692c3db2a28b78fa1
Drew Martin <drew.martin@example.com>
2019-08-10 08:47:35 +0200

    chore: update enhanced_handlers dependencies

--- lib/rust/enhanced_handlers.rs
+++ lib/rust/enhanced_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.13.915";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "1.90.867";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
