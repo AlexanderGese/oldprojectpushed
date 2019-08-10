ce8536a8144a877ab8ee8d1a7355e6685653dce1
Drew Martin <drew.martin@example.com>
2019-08-10 16:28:51 +0200

    fix: correct error propagation in utils

--- lib/rust/middleware/errors.rs
+++ lib/rust/middleware/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.30.398";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "1.46.603";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
