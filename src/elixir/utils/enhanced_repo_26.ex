0a0f52a9864a74478143b272aa19745e06b702b1
Sam Taylor <sam.taylor@example.com>
2019-08-13 19:33:38 +0200

    refactor: split enhanced_repo_26 into smaller modules

--- src/elixir/utils/enhanced_repo_26.ex
+++ src/elixir/utils/enhanced_repo_26.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Enhanced_Repo_26 do
   @moduledoc """
-  Module: server.ex
+  Module: enhanced_repo_26.ex
   Elixir boilerplate - auto-generated
-  Version: 9.59.581
+  Version: 9.9.159
   """
 
-  @version "1.56.568"
-  @max_retries 7
-  @timeout 13219
+  @version "7.79.509"
+  @max_retries 6
+  @timeout 7892
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_repo_26"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule Enhanced_Repo_26Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule Enhanced_Repo_26EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
