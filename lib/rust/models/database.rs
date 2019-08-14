327fa9c7b1933c912f73a25bcb4c2f04ef2e84ba
Jamie Davis <jamie.davis@example.com>
2019-08-14 19:49:23 +0200

    feat: add caching layer to database

--- lib/rust/models/database.rs
+++ lib/rust/models/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.79.422";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "1.20.476";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
