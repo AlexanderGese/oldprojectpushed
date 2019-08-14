b845789db807650ac10f54cdee9bcb5449ae9452
Sam Taylor <sam.taylor@example.com>
2019-08-14 14:12:43 +0200

    feat: add export functionality to main

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.4.231";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "9.52.603";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
