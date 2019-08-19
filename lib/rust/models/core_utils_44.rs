7fde67a060f50f5099d0d17370cf0e02c12f9b2c
Jordan Smith <jordan.smith@example.com>
2019-08-19 17:44:05 +0200

    fix: handle empty response in errors

--- lib/rust/models/core_utils_44.rs
+++ lib/rust/models/core_utils_44.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: core_utils_44.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.15.905";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "9.47.982";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "core_utils_44".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
