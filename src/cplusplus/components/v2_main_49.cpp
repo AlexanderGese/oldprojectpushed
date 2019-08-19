ec5bccdabbaffbf6e6532346db87203ee571300d
Jamie Davis <jamie.davis@example.com>
2019-08-19 22:14:25 +0200

    fix: correct CORS headers in handler

--- src/cplusplus/components/v2_main_49.cpp
+++ src/cplusplus/components/v2_main_49.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: handler.cpp
+ * Module: v2_main_49.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.86.988
+ * Version: 7.69.645
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace handler {
+namespace v2_main_49 {
 
-constexpr const char* VERSION = "8.44.951";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 15871;
+constexpr const char* VERSION = "7.85.848";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 22567;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace handler
+} // namespace v2_main_49
 
 int main() {
-    using namespace handler;
+    using namespace v2_main_49;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
