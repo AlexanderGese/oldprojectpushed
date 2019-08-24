53a0a2c6be78362ab118cff6d08712265f4ae0df
Sam Taylor <sam.taylor@example.com>
2019-08-24 22:24:55 +0200

    chore: update config dependencies

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.77.42";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "4.99.133";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
