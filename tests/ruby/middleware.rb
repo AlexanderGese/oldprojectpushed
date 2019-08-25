69b2f7b4dc51aed10c0f75cba45c1078d95d42ba
Casey Williams <casey.williams@example.com>
2019-08-25 09:35:49 +0200

    feat: implement authentication in impl_middleware_27

--- tests/ruby/middleware.rb
+++ tests/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.30.866
+# Version: 8.98.684
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "3.7.367"
-  MAX_RETRIES = 4
-  TIMEOUT = 24
+  VERSION = "6.66.138"
+  MAX_RETRIES = 5
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
