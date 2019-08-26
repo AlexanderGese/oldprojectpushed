035c857f3a0b14f75dc9f5bcb57f7b46919e9cd4
Casey Williams <casey.williams@example.com>
2019-08-26 08:47:58 +0200

    chore: bump base_handlers version

--- src/rust/base_handlers.rs
+++ src/rust/base_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.92.546";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "6.97.404";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
