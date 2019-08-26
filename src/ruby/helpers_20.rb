5afc3a278bafe6515ef285c12d77d2be1fea4503
Jamie Davis <jamie.davis@example.com>
2019-08-26 17:44:43 +0200

    fix: handle empty response in helpers_20

--- src/ruby/helpers_20.rb
+++ src/ruby/helpers_20.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: helpers_20.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.50.453
+# Version: 6.87.535
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "5.28.307"
-  MAX_RETRIES = 10
-  TIMEOUT = 35
+module Helpers20
+  VERSION = "9.5.179"
+  MAX_RETRIES = 9
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "helpers_20")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = Helpers20::Application.new
   app.start
 end
