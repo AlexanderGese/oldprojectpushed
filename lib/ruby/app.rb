6621e89dfb37c3f02388dc4b9c125a2cea39e657
Jamie Davis <jamie.davis@example.com>
2019-08-26 08:15:51 +0200

    fix: correct type definitions in app

--- lib/ruby/app.rb
+++ lib/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.35.299
+# Version: 8.76.105
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "5.53.968"
-  MAX_RETRIES = 7
-  TIMEOUT = 41
+  VERSION = "7.52.343"
+  MAX_RETRIES = 9
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
