ef435968c10a236c29dc171a14ffd238f7d94355
Casey Williams <casey.williams@example.com>
2019-08-26 22:15:08 +0200

    fix: resolve edge case in base_engine handler

--- lib/cplusplus/components/base_engine.cpp
+++ lib/cplusplus/components/base_engine.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: engine.cpp
+ * Module: base_engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.2.305
+ * Version: 2.62.398
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace engine {
+namespace base_engine {
 
-constexpr const char* VERSION = "4.82.605";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 16566;
+constexpr const char* VERSION = "8.15.747";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 10500;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace engine
+} // namespace base_engine
 
 int main() {
-    using namespace engine;
+    using namespace base_engine;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
