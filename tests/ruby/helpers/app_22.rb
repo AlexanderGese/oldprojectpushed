3c3cae20286453c01ba818d0a8dfa92d9c6e4881
Alex Chen <alex.chen@example.com>
2019-08-27 14:13:09 +0200

    chore: clean up config imports

--- tests/ruby/helpers/app_22.rb
+++ tests/ruby/helpers/app_22.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_controllers.rb
+# Module: app_22.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.86.750
+# Version: 4.53.781
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreControllers
-  VERSION = "7.95.636"
+module App22
+  VERSION = "4.61.999"
   MAX_RETRIES = 10
-  TIMEOUT = 42
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_controllers")
+      @app_name = opts.fetch(:app_name, "app_22")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreControllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreControllers::Application.new
+  app = App22::Application.new
   app.start
 end
