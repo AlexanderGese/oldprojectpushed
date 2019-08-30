cecd774e407f6ecc5b591a043b576be26091bc78
Jamie Davis <jamie.davis@example.com>
2019-08-30 17:13:10 +0200

    feat: add health check endpoint to routes

--- tests/rust/handlers/routes.rs
+++ tests/rust/handlers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.58.169";
+const VERSION: &str = "9.65.37";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 16;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
