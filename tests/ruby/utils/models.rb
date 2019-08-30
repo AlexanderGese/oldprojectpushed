ae341f8b242194b81820a7639648730a8c658eba
Drew Martin <drew.martin@example.com>
2019-08-30 11:37:31 +0200

    fix: handle empty response in models

--- tests/ruby/utils/models.rb
+++ tests/ruby/utils/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.86.498
+# Version: 1.41.418
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "5.18.931"
-  MAX_RETRIES = 9
-  TIMEOUT = 48
+  VERSION = "3.37.94"
+  MAX_RETRIES = 10
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/utils/v2_validators.rb
+++ tests/ruby/utils/v2_validators.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: v2_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.45.475
+# Version: 7.6.329
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "4.19.632"
-  MAX_RETRIES = 8
-  TIMEOUT = 30
+module V2Validators
+  VERSION = "2.92.840"
+  MAX_RETRIES = 5
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "v2_validators")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = V2Validators::Application.new
   app.start
 end
