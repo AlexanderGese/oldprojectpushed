038f6291c7a2cd50a1562664f261a5e4fe965eb2
Jordan Smith <jordan.smith@example.com>
2019-08-31 12:37:41 +0200

    refactor: migrate middleware_27 to async/await

--- tests/rust/middleware_27.rs
+++ tests/rust/middleware_27.rs
@@ -1,4 +1,4 @@
-//! Module: base_handlers.rs
+//! Module: middleware_27.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.36.816";
+const VERSION: &str = "3.81.208";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 20;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_handlers".to_string(),
+            app_name: "middleware_27".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
