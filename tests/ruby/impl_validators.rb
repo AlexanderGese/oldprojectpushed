78240b0f46766e32ec26081ea38c0c6f9fd1a781
Drew Martin <drew.martin@example.com>
2019-08-31 11:48:22 +0200

    feat: add event system to impl_validators

--- tests/ruby/impl_validators.rb
+++ tests/ruby/impl_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.29.483
+# Version: 4.87.496
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplValidators
-  VERSION = "5.55.67"
-  MAX_RETRIES = 4
-  TIMEOUT = 37
+  VERSION = "6.64.348"
+  MAX_RETRIES = 8
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplValidators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
