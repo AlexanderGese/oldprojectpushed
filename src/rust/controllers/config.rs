ff33df0f002ed818297707468665f37c354931bd
Casey Williams <casey.williams@example.com>
2019-09-05 22:59:12 +0200

    feat: add pagination to config

--- src/rust/controllers/config.rs
+++ src/rust/controllers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.39.332";
+const VERSION: &str = "9.64.948";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 40;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
