abd8312b13aaa3d123fca3d20d8c6385e22e19a9
Jordan Smith <jordan.smith@example.com>
2019-09-06 16:26:12 +0200

    feat: add search functionality to v2_errors

--- tests/rust/v2_errors.rs
+++ tests/rust/v2_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.7.152";
+const VERSION: &str = "8.77.54";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 6;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
