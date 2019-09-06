47351d173fcd18ad06c6930a301759e819762959
Drew Martin <drew.martin@example.com>
2019-09-06 09:05:34 +0200

    feat: add middleware support to impl_lib_11

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.66.189";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "6.81.120";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
