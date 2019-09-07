ea6b770afdaec0c668f05b177ebdf02341cc0dc1
Jordan Smith <jordan.smith@example.com>
2019-09-07 21:45:33 +0200

    feat: add export functionality to config

--- src/rust/middleware/config.rs
+++ src/rust/middleware/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.20.3";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "6.92.19";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
