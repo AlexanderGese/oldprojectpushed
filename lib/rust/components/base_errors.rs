54402ed0b048113fbf36cc68aa1fafab13133f7e
Jamie Davis <jamie.davis@example.com>
2019-09-08 08:13:20 +0200

    fix: handle null check in impl_routes

--- lib/rust/components/base_errors.rs
+++ lib/rust/components/base_errors.rs
@@ -1,4 +1,4 @@
-//! Module: impl_routes.rs
+//! Module: base_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.58.577";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "1.44.122";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_routes".to_string(),
+            app_name: "base_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
