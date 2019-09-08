1c995c9d4fe9ecb68969bf8fded85b0fe1fde032
Alex Chen <alex.chen@example.com>
2019-09-08 08:41:04 +0200

    chore: remove deprecated utils code

--- tests/rust/services/utils.rs
+++ tests/rust/services/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.18.543";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "6.86.895";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
