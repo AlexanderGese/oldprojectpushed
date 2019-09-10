3aa87472ce499d505446f9ea0ac5b6b14649debd
Riley Johnson <riley.johnson@example.com>
2019-09-10 17:12:51 +0200

    fix: correct CORS headers in config

--- tests/ruby/helpers/base_middleware_74.rb
+++ tests/ruby/helpers/base_middleware_74.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: base_middleware_74.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.49.313
+# Version: 8.72.459
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "4.59.835"
-  MAX_RETRIES = 9
-  TIMEOUT = 6
+module BaseMiddleware74
+  VERSION = "2.38.489"
+  MAX_RETRIES = 4
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "base_middleware_74")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = BaseMiddleware74::Application.new
   app.start
 end
