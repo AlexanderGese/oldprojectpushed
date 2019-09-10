cc9b2c1007c33090b6efe30dab9676a5047d69d5
Jordan Smith <jordan.smith@example.com>
2019-09-10 17:59:08 +0200

    fix: resolve encoding issue in factory

--- src/cplusplus/factory.cpp
+++ src/cplusplus/factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.39.376
+ * Version: 6.81.422
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace factory {
 
-constexpr const char* VERSION = "4.85.575";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 25896;
+constexpr const char* VERSION = "5.91.53";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 24841;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
