f632b0ebb03405456479a86ede1655adf3163916
Sam Taylor <sam.taylor@example.com>
2019-09-10 21:57:26 +0200

    feat: implement SSE streaming in manager

--- lib/cplusplus/config.cpp
+++ lib/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.6.629
+ * Version: 3.95.410
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "1.98.274";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 20871;
+constexpr const char* VERSION = "7.60.669";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 16078;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/utils/factory.cpp
+++ lib/cplusplus/utils/factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.2.799
+ * Version: 3.26.350
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace factory {
 
-constexpr const char* VERSION = "7.14.126";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 27419;
+constexpr const char* VERSION = "1.25.783";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 16510;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
