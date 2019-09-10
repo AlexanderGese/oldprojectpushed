1212cf98dfafeef086f307605b8d817720192ec6
Alex Chen <alex.chen@example.com>
2019-09-10 18:55:19 +0200

    feat: add error handling to handlers

--- lib/rust/services/models.rs
+++ lib/rust/services/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.39.758";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "4.34.423";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils/handlers.rs
+++ lib/rust/utils/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.67.231";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "6.52.277";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
