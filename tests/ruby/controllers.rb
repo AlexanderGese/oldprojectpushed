7dcd96e3aed4b1a06c1ce49b06eb138b0eb04058
Casey Williams <casey.williams@example.com>
2019-09-11 19:41:39 +0200

    fix: handle large payloads in core_server_27

--- tests/ruby/controllers.rb
+++ tests/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.54.402
+# Version: 1.76.656
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "2.22.326"
-  MAX_RETRIES = 4
-  TIMEOUT = 56
+  VERSION = "3.13.669"
+  MAX_RETRIES = 9
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
