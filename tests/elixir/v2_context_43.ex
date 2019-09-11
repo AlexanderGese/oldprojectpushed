521737e77b976b291e272670c6b8f25aec534a32
Alex Chen <alex.chen@example.com>
2019-09-11 15:16:38 +0200

    feat: implement authentication in v2_context_43

--- tests/elixir/v2_context_43.ex
+++ tests/elixir/v2_context_43.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule V2_Context_43 do
   @moduledoc """
-  Module: schema.ex
+  Module: v2_context_43.ex
   Elixir boilerplate - auto-generated
-  Version: 6.24.193
+  Version: 4.10.439
   """
 
-  @version "8.43.964"
-  @max_retries 3
-  @timeout 16046
+  @version "7.79.277"
+  @max_retries 10
+  @timeout 29381
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "v2_context_43"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule V2_Context_43Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule V2_Context_43EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
