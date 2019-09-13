3a7f0884f10bde44f4f57c6ae8e20d980355eb19
Sam Taylor <sam.taylor@example.com>
2019-09-13 15:00:26 +0200

    fix: handle timeout in v2_tasks requests

--- tests/ruby/services/validators.rb
+++ tests/ruby/services/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.97.6
+# Version: 1.16.507
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "6.47.844"
-  MAX_RETRIES = 10
-  TIMEOUT = 27
+  VERSION = "9.9.848"
+  MAX_RETRIES = 6
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Validators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
