e7ceb786e2eab0caef39e3ddee6f68d556f611c5
Jamie Davis <jamie.davis@example.com>
2019-09-13 14:16:13 +0200

    docs: add documentation for config

--- tests/cplusplus/middleware/parser_47.cpp
+++ tests/cplusplus/middleware/parser_47.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: parser_47.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.65.860
+ * Version: 4.14.387
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace parser_47 {
 
-constexpr const char* VERSION = "7.3.733";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 24124;
+constexpr const char* VERSION = "1.40.409";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 23713;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace parser_47
 
 int main() {
-    using namespace utils;
+    using namespace parser_47;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/utils/config.cpp
+++ tests/cplusplus/utils/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.12.568
+ * Version: 7.25.117
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "7.19.605";
+constexpr const char* VERSION = "4.11.700";
 constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 25502;
+constexpr int TIMEOUT_MS = 12209;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
