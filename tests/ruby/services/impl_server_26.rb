e60902409be6f199e6beb554a4beefb5dfd7a943
Sam Taylor <sam.taylor@example.com>
2019-09-14 13:57:12 +0200

    fix: resolve edge case in app handler

--- tests/ruby/services/impl_server_26.rb
+++ tests/ruby/services/impl_server_26.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: impl_server_26.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.43.857
+# Version: 5.5.866
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "9.96.585"
-  MAX_RETRIES = 8
-  TIMEOUT = 24
+module ImplServer26
+  VERSION = "2.43.904"
+  MAX_RETRIES = 3
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "impl_server_26")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = ImplServer26::Application.new
   app.start
 end
