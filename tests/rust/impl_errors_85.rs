75f2f8e69fc251f69c10ebb3222cb1a198f97014
Casey Williams <casey.williams@example.com>
2019-09-15 14:31:16 +0200

    fix: resolve timezone issue in impl_errors_85

--- tests/rust/impl_errors_85.rs
+++ tests/rust/impl_errors_85.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: impl_errors_85.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.32.848";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "1.37.921";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "impl_errors_85".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
