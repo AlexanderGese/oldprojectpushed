d72f8f129976fdd6ad490700244b0fd934530e2d
Jordan Smith <jordan.smith@example.com>
2019-09-17 22:43:29 +0200

    feat: implement SSE streaming in models

--- tests/rust/services/models.rs
+++ tests/rust/services/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.99.10";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "3.37.297";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
