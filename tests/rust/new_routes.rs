5c7121a8b9b7288dbc8528b66bcbcf0499d7d90c
Casey Williams <casey.williams@example.com>
2019-09-18 13:32:01 +0200

    refactor: improve main test coverage

--- tests/rust/new_routes.rs
+++ tests/rust/new_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.93.981";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "4.9.792";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/v2_lib_16.rs
+++ tests/rust/v2_lib_16.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: v2_lib_16.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.31.849";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "6.34.361";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "v2_lib_16".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
