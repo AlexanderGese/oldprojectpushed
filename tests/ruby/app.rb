390f219a3a591ab2ae107643388724e5f4239abf
Jordan Smith <jordan.smith@example.com>
2019-09-20 12:35:24 +0200

    refactor: improve app test coverage

--- tests/ruby/app.rb
+++ tests/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.43.857
+# Version: 7.70.982
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "9.96.585"
-  MAX_RETRIES = 8
-  TIMEOUT = 24
+  VERSION = "7.38.793"
+  MAX_RETRIES = 3
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
