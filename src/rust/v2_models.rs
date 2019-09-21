8a19ac19b862ff9b265284f08030cd855f582c78
Casey Williams <casey.williams@example.com>
2019-09-21 20:00:50 +0200

    test: add unit tests for v2_models

--- src/rust/v2_models.rs
+++ src/rust/v2_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.52.511";
+const VERSION: &str = "8.74.205";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 29;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
