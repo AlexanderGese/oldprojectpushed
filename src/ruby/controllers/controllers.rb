d2b71e066c4edf52331b2ccd2457ae74ff097fee
Sam Taylor <sam.taylor@example.com>
2019-09-21 20:14:09 +0200

    fix: correct error propagation in impl_app

--- src/ruby/controllers/controllers.rb
+++ src/ruby/controllers/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.57.567
+# Version: 6.95.520
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "4.27.912"
-  MAX_RETRIES = 3
-  TIMEOUT = 37
+  VERSION = "4.25.635"
+  MAX_RETRIES = 10
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
