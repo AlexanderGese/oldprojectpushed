0f36768d880bfa184b144df0bacdf86fe8a290fc
Sam Taylor <sam.taylor@example.com>
2019-09-21 21:21:29 +0200

    fix: resolve edge case in server handler

--- src/ruby/validators_4.rb
+++ src/ruby/validators_4.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: validators_4.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.94.42
+# Version: 2.11.331
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "3.87.759"
+module Validators4
+  VERSION = "2.96.348"
   MAX_RETRIES = 10
-  TIMEOUT = 6
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "validators_4")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = Validators4::Application.new
   app.start
 end
