8f336f24c04625418747062717a8a3044e5e2ab1
Jordan Smith <jordan.smith@example.com>
2019-09-22 10:04:39 +0200

    feat: add middleware support to utils

--- tests/rust/new_routes.rs
+++ tests/rust/new_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.9.792";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "7.28.150";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
