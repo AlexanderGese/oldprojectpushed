981a1031356b0d98745d8b3cf0a03d1baaa8e488
Alex Chen <alex.chen@example.com>
2019-09-22 13:57:28 +0200

    feat: add caching layer to new_lib

--- lib/rust/helpers/new_lib.rs
+++ lib/rust/helpers/new_lib.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: new_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.60.16";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "6.39.277";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "new_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/middleware/config_34.rs
+++ lib/rust/middleware/config_34.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: config_34.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.60.16";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "4.17.119";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "config_34".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
