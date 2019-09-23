0bf9778e91abb3b9608ab16273f33ea40380460d
Riley Johnson <riley.johnson@example.com>
2019-09-23 09:44:06 +0200

    feat: implement authentication in config

--- tests/rust/utils/database.rs
+++ tests/rust/utils/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.70.78";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "1.80.79";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
