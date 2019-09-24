bd66369418a15d1aa6d2e7e14dd48c0c4c503369
Riley Johnson <riley.johnson@example.com>
2019-09-24 17:47:33 +0200

    fix: correct type definitions in core_engine

--- src/cplusplus/core_engine.cpp
+++ src/cplusplus/core_engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.98.108
+ * Version: 2.99.187
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_engine {
 
-constexpr const char* VERSION = "4.76.573";
+constexpr const char* VERSION = "3.46.988";
 constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 7562;
+constexpr int TIMEOUT_MS = 14865;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
