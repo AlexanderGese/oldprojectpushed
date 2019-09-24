98542b8cfa004800635b4fb3e80fa0a0d4cda78b
Alex Chen <alex.chen@example.com>
2019-09-24 16:28:15 +0200

    fix: handle concurrent access in utils_4

--- src/rust/core_lib_15.rs
+++ src/rust/core_lib_15.rs
@@ -1,4 +1,4 @@
-//! Module: core_errors.rs
+//! Module: core_lib_15.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.5.426";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "2.36.253";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_errors".to_string(),
+            app_name: "core_lib_15".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/utils_4.rs
+++ src/rust/utils_4.rs
@@ -1,4 +1,4 @@
-//! Module: core_errors.rs
+//! Module: utils_4.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.5.426";
+const VERSION: &str = "1.25.502";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 58;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_errors".to_string(),
+            app_name: "utils_4".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
