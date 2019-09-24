dbcc4dec38ef9ad647b0ae58005c65445b76448f
Jamie Davis <jamie.davis@example.com>
2019-09-24 23:23:38 +0200

    fix: correct CORS headers in schema

--- src/elixir/services/base_repo_3.ex
+++ src/elixir/services/base_repo_3.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule Base_Repo_3 do
   @moduledoc """
-  Module: schema.ex
+  Module: base_repo_3.ex
   Elixir boilerplate - auto-generated
-  Version: 4.79.827
+  Version: 6.37.782
   """
 
-  @version "1.4.102"
-  @max_retries 3
-  @timeout 16444
+  @version "2.34.71"
+  @max_retries 5
+  @timeout 15208
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "base_repo_3"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule Base_Repo_3Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule Base_Repo_3EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
