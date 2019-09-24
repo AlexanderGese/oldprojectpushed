86b9af0be5c843ff236668d39dbeafac31fffd32
Morgan Lee <morgan.lee@example.com>
2019-09-24 23:33:31 +0200

    fix: correct error propagation in main

--- tests/cplusplus/main.cpp
+++ tests/cplusplus/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.64.436
+ * Version: 4.70.194
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "7.74.351";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 25191;
+constexpr const char* VERSION = "7.56.3";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 20893;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/manager.cpp
+++ tests/cplusplus/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.49.193
+ * Version: 3.1.93
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "9.87.357";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 9173;
+constexpr const char* VERSION = "2.84.780";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 12808;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
