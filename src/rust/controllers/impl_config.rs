902b45b48430b551393f47cc38ebe6b9c26bf569
Jamie Davis <jamie.davis@example.com>
2019-09-25 16:07:17 +0200

    fix: patch security vulnerability in impl_config

--- src/rust/controllers/impl_config.rs
+++ src/rust/controllers/impl_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.26.55";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "5.63.136";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
