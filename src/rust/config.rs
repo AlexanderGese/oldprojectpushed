33f709803a91d8c0261cb6c9fa53aa1d3d594aa2
Jamie Davis <jamie.davis@example.com>
2019-09-27 12:50:35 +0200

    chore: update base_utils_1 configuration

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.11.634";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "3.75.959";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
