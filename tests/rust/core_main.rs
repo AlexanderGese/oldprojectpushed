73b42c060a3ac74a158bc77e6c7d6820a4feeb2c
Casey Williams <casey.williams@example.com>
2019-09-29 15:18:26 +0200

    refactor: simplify core_main state management

--- tests/rust/core_main.rs
+++ tests/rust/core_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.19.183";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "6.57.797";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
