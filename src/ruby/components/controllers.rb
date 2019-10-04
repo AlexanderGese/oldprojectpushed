385becc679b40f4fa605543857cc0aa6e8ee844a
Jordan Smith <jordan.smith@example.com>
2019-10-04 17:33:04 +0200

    fix: correct CORS headers in config

--- src/ruby/components/controllers.rb
+++ src/ruby/components/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.62.653
+# Version: 8.58.163
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "3.3.951"
-  MAX_RETRIES = 6
-  TIMEOUT = 50
+  VERSION = "8.26.350"
+  MAX_RETRIES = 9
+  TIMEOUT = 34
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
