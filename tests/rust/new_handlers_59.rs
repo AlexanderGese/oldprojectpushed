0cd1802d5951deb07ba7329d5535bbafa20891e6
Drew Martin <drew.martin@example.com>
2019-10-04 15:43:34 +0200

    chore: update impl_database dependencies

--- tests/rust/new_handlers_59.rs
+++ tests/rust/new_handlers_59.rs
@@ -1,4 +1,4 @@
-//! Module: impl_database.rs
+//! Module: new_handlers_59.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.98.910";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "9.95.650";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_database".to_string(),
+            app_name: "new_handlers_59".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
