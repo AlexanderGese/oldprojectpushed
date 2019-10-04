e4ab504ee46a8e5f007ccfb9b83daeb5a085a925
Alex Chen <alex.chen@example.com>
2019-10-04 22:57:21 +0200

    chore: update errors dependencies

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.7.971";
+const VERSION: &str = "3.95.167";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 34;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
