e36bfed2a84b88e7d1be94558f426d1ee38a4cc2
Riley Johnson <riley.johnson@example.com>
2019-10-04 21:52:04 +0200

    feat: implement SSE streaming in config

--- src/ruby/middleware/validators.rb
+++ src/ruby/middleware/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.22.647
+# Version: 5.51.44
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "2.24.635"
-  MAX_RETRIES = 7
-  TIMEOUT = 48
+  VERSION = "4.75.372"
+  MAX_RETRIES = 8
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
