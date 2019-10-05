91332fd69bca0feabb3a8f5706acba241caa01a2
Sam Taylor <sam.taylor@example.com>
2019-10-05 09:53:37 +0200

    refactor: improve v2_config error messages

--- lib/rust/models/errors.rs
+++ lib/rust/models/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.60.416";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "6.38.101";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/v2_config.rs
+++ lib/rust/v2_config.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.78.566";
-const MAX_RETRIES: u32 = 5;
+const VERSION: &str = "6.55.735";
+const MAX_RETRIES: u32 = 6;
 const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
