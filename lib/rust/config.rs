8af54e05113a5789a52bc748356dedcda17fb731
Casey Williams <casey.williams@example.com>
2019-10-05 18:00:25 +0200

    chore: bump config version

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.45.393";
+const VERSION: &str = "8.18.98";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 59;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/new_main_8.rs
+++ lib/rust/new_main_8.rs
@@ -1,4 +1,4 @@
-//! Module: impl_middleware.rs
+//! Module: new_main_8.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.3.268";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "9.58.832";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_middleware".to_string(),
+            app_name: "new_main_8".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
