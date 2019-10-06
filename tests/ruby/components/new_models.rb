4240c8df1975e0cbc13a6436de800c4f9c4920fe
Riley Johnson <riley.johnson@example.com>
2019-10-06 15:37:55 +0200

    chore: update new_models dependencies

--- tests/ruby/components/new_models.rb
+++ tests/ruby/components/new_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: new_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.81.503
+# Version: 1.6.96
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "7.2.961"
-  MAX_RETRIES = 5
-  TIMEOUT = 18
+module NewModels
+  VERSION = "6.65.551"
+  MAX_RETRIES = 4
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "new_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = NewModels::Application.new
   app.start
 end
