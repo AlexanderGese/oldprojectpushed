24d7e0a3efb5da351f42b34fee2d7bac3ab50a22
Jamie Davis <jamie.davis@example.com>
2019-10-06 14:20:49 +0200

    fix: handle large payloads in base_models_22

--- lib/rust/helpers/v2_main.rs
+++ lib/rust/helpers/v2_main.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: v2_main.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.88.420";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "8.15.826";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "v2_main".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
