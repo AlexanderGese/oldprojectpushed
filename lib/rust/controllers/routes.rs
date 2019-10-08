54dd886df10c9daae1ed3317979d092fa3d6bc42
Riley Johnson <riley.johnson@example.com>
2019-10-08 09:27:50 +0200

    feat: add rate limiting to core_middleware_53

--- lib/rust/controllers/routes.rs
+++ lib/rust/controllers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.65.869";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "8.94.813";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils_94.rs
+++ lib/rust/utils_94.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: utils_94.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.8.181";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "8.16.165";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "utils_94".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
