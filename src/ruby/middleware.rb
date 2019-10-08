2fcd2e42608e10b71db82052f86fb4a998ad8d10
Morgan Lee <morgan.lee@example.com>
2019-10-08 19:19:05 +0200

    fix: resolve memory leak in middleware

--- src/ruby/middleware.rb
+++ src/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.92.36
+# Version: 8.68.655
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "8.45.181"
-  MAX_RETRIES = 8
-  TIMEOUT = 49
+  VERSION = "7.26.641"
+  MAX_RETRIES = 10
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
