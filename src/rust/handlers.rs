54dfd38b126bad4244d8fe3f1bd673ae3af0391e
Sam Taylor <sam.taylor@example.com>
2019-10-09 11:06:45 +0200

    feat: add error handling to config

--- src/rust/handlers.rs
+++ src/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.49.12";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "9.4.777";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
