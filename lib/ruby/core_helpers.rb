48aeeffa4e1424beb0051996807c32adfe66d92b
Alex Chen <alex.chen@example.com>
2019-10-09 21:53:35 +0200

    refactor: split core_helpers into smaller modules

--- lib/ruby/core_helpers.rb
+++ lib/ruby/core_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: core_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.35.751
+# Version: 4.67.753
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreHelpers
-  VERSION = "4.21.104"
-  MAX_RETRIES = 10
-  TIMEOUT = 21
+  VERSION = "3.60.366"
+  MAX_RETRIES = 7
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreHelpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
