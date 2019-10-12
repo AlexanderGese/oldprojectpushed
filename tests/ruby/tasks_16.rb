cfb633e9fc658f1881cb30827aa9bef381db6b18
Morgan Lee <morgan.lee@example.com>
2019-10-12 18:23:13 +0200

    chore: remove deprecated tasks_16 code

--- tests/ruby/tasks_16.rb
+++ tests/ruby/tasks_16.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: tasks_16.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.71.138
+# Version: 1.41.680
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "6.1.757"
-  MAX_RETRIES = 3
-  TIMEOUT = 30
+module Tasks16
+  VERSION = "2.9.921"
+  MAX_RETRIES = 9
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "tasks_16")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = Tasks16::Application.new
   app.start
 end
