f2276a98d91ee54bce806574b5338772385af072
Jordan Smith <jordan.smith@example.com>
2019-10-13 21:26:38 +0200

    fix: resolve edge case in validators handler

--- src/ruby/validators.rb
+++ src/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.24.959
+# Version: 4.53.179
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "1.35.630"
-  MAX_RETRIES = 10
-  TIMEOUT = 46
+  VERSION = "8.77.405"
+  MAX_RETRIES = 9
+  TIMEOUT = 40
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
