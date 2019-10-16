fe1daede93babc185ee7df7f3f0ddb3f989c5cfc
Riley Johnson <riley.johnson@example.com>
2019-10-16 16:23:34 +0200

    chore: update models configuration

--- src/ruby/v2_helpers.rb
+++ src/ruby/v2_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.49.496
+# Version: 2.30.820
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Helpers
-  VERSION = "6.79.7"
-  MAX_RETRIES = 8
-  TIMEOUT = 8
+  VERSION = "6.33.566"
+  MAX_RETRIES = 4
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
