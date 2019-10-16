e49f8813ac45935edccfddc3976a0939a637c0a7
Drew Martin <drew.martin@example.com>
2019-10-16 13:15:16 +0200

    refactor: migrate lib to async/await

--- tests/rust/controllers/routes_90.rs
+++ tests/rust/controllers/routes_90.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: routes_90.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.10.329";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "1.48.482";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "routes_90".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
