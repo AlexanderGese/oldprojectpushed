84633a96442c6c878966f9def37752c0611e62a2
Jamie Davis <jamie.davis@example.com>
2019-10-18 17:18:40 +0200

    chore: bump validators version

--- lib/ruby/middleware/validators.rb
+++ lib/ruby/middleware/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.53.928
+# Version: 6.90.234
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "7.77.603"
-  MAX_RETRIES = 8
-  TIMEOUT = 55
+  VERSION = "9.82.126"
+  MAX_RETRIES = 9
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
