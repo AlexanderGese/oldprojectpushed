fec8d30cab22ba84da148d56f92a1c8e94a006cc
Casey Williams <casey.williams@example.com>
2019-10-19 23:04:54 +0200

    feat: add batch processing to core_middleware

--- tests/ruby/middleware.rb
+++ tests/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.48.950
+# Version: 6.15.461
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "6.76.788"
+  VERSION = "2.77.909"
   MAX_RETRIES = 3
-  TIMEOUT = 57
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
