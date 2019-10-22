51b6720c40b9480ca7661901adffa0cfeb4fb23e
Jordan Smith <jordan.smith@example.com>
2019-10-22 09:59:39 +0200

    fix: handle timeout in handlers_60 requests

--- lib/rust/handlers_60.rs
+++ lib/rust/handlers_60.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: handlers_60.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.86.356";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "4.52.658";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "handlers_60".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
