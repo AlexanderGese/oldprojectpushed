93ad51fac9f1c6a6b414768a581783e1a42104d5
Jordan Smith <jordan.smith@example.com>
2019-10-22 17:09:30 +0200

    fix: handle empty response in models

--- src/rust/middleware/models.rs
+++ src/rust/middleware/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.42.389";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "3.64.550";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
