2afa5cb54a544a906ed7dd23974812aaecf63435
Casey Williams <casey.williams@example.com>
2019-10-22 14:43:40 +0200

    test: add unit tests for lib

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.67.75";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "3.55.593";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
