7b86156e358f6e1d73f94d57ab266d4367a363b2
Sam Taylor <sam.taylor@example.com>
2019-10-24 16:24:55 +0200

    chore: update new_validators_65 configuration

--- tests/ruby/controllers/core_routes.rb
+++ tests/ruby/controllers/core_routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_validators_65.rb
+# Module: core_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.22.537
+# Version: 5.25.973
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewValidators65
-  VERSION = "2.37.439"
-  MAX_RETRIES = 3
-  TIMEOUT = 13
+module CoreRoutes
+  VERSION = "5.87.957"
+  MAX_RETRIES = 5
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_validators_65")
+      @app_name = opts.fetch(:app_name, "core_routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewValidators65
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewValidators65::Application.new
+  app = CoreRoutes::Application.new
   app.start
 end
--- tests/ruby/middleware/routes.rb
+++ tests/ruby/middleware/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.26.151
+# Version: 3.22.887
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "8.6.4"
-  MAX_RETRIES = 7
-  TIMEOUT = 52
+  VERSION = "2.83.406"
+  MAX_RETRIES = 9
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
