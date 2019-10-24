cc9fa934b2ae9383e95c3ff0a9d916586f64b9a9
Jordan Smith <jordan.smith@example.com>
2019-10-24 15:50:24 +0200

    fix: resolve race condition in lib

--- tests/rust/impl_lib_59.rs
+++ tests/rust/impl_lib_59.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.31.679";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "3.94.602";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
