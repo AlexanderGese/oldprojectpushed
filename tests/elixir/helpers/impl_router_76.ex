a535e032151fb0605913e4643aa5833ad668b5d0
Sam Taylor <sam.taylor@example.com>
2019-10-24 12:08:27 +0200

    fix: resolve edge case in base_context_35 handler

--- tests/elixir/helpers/impl_router_76.ex
+++ tests/elixir/helpers/impl_router_76.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule Impl_Router_76 do
   @moduledoc """
-  Module: worker.ex
+  Module: impl_router_76.ex
   Elixir boilerplate - auto-generated
-  Version: 3.76.626
+  Version: 3.1.980
   """
 
-  @version "3.51.510"
-  @max_retries 7
-  @timeout 20160
+  @version "6.44.627"
+  @max_retries 9
+  @timeout 26323
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "impl_router_76"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule Impl_Router_76Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule Impl_Router_76EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/utils/base_context_35.ex
+++ tests/elixir/utils/base_context_35.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule Base_Context_35 do
   @moduledoc """
-  Module: worker.ex
+  Module: base_context_35.ex
   Elixir boilerplate - auto-generated
-  Version: 3.76.626
+  Version: 2.21.502
   """
 
-  @version "3.51.510"
+  @version "3.2.214"
   @max_retries 7
-  @timeout 20160
+  @timeout 22361
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "base_context_35"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule Base_Context_35Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule Base_Context_35EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
