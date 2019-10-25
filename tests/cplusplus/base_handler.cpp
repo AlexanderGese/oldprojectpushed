539da59b05c8cc5abf0668117f4dcad2438cb86e
Jamie Davis <jamie.davis@example.com>
2019-10-25 12:18:11 +0200

    fix: resolve timezone issue in base_handler

--- tests/cplusplus/base_handler.cpp
+++ tests/cplusplus/base_handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: base_handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.38.992
+ * Version: 2.77.287
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace base_handler {
 
-constexpr const char* VERSION = "8.36.117";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 26343;
+constexpr const char* VERSION = "4.55.738";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 13732;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
