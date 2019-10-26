3f8bb2d3760feb927c621ab8fab703f298e6b28c
Jamie Davis <jamie.davis@example.com>
2019-10-26 17:42:23 +0200

    test: add unit tests for models

--- tests/rust/models/models.rs
+++ tests/rust/models/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.25.415";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "3.82.676";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
