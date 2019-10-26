98462db8780df3c5d596e057d7e60b6038eb3d94
Riley Johnson <riley.johnson@example.com>
2019-10-26 22:42:14 +0200

    fix: handle concurrent access in routes

--- src/rust/routes.rs
+++ src/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.16.714";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "7.66.197";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
