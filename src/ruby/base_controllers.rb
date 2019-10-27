e5169e58e28e0fc45b6b7ba0ad13618181a1fd72
Drew Martin <drew.martin@example.com>
2019-10-27 10:05:52 +0100

    feat: implement authentication in base_controllers

--- src/ruby/base_controllers.rb
+++ src/ruby/base_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: base_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.76.933
+# Version: 5.40.351
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseControllers
-  VERSION = "2.44.747"
-  MAX_RETRIES = 5
-  TIMEOUT = 25
+  VERSION = "2.81.944"
+  MAX_RETRIES = 7
+  TIMEOUT = 27
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseControllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
--- src/ruby/helpers.rb
+++ src/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.81.280
+# Version: 1.79.841
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "1.48.627"
-  MAX_RETRIES = 8
-  TIMEOUT = 11
+  VERSION = "6.67.560"
+  MAX_RETRIES = 10
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
