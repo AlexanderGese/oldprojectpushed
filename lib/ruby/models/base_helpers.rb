191c615392022dbee17d2ef2d60eeb2921c37554
Casey Williams <casey.williams@example.com>
2019-10-27 08:30:03 +0100

    chore: update enhanced_tasks_76 dependencies

--- lib/ruby/models/base_helpers.rb
+++ lib/ruby/models/base_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: base_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.81.229
+# Version: 8.82.402
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "3.50.181"
-  MAX_RETRIES = 8
-  TIMEOUT = 40
+module BaseHelpers
+  VERSION = "2.2.297"
+  MAX_RETRIES = 7
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "base_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = BaseHelpers::Application.new
   app.start
 end
