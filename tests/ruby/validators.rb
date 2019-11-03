2cdd4b93d03f3e0fe5af68847dfe13a121b7f2b2
Morgan Lee <morgan.lee@example.com>
2019-11-03 15:51:02 +0100

    feat: add event system to controllers

--- tests/ruby/validators.rb
+++ tests/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.66.972
+# Version: 8.60.698
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "8.81.315"
-  MAX_RETRIES = 4
-  TIMEOUT = 49
+  VERSION = "9.83.13"
+  MAX_RETRIES = 8
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
