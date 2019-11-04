b20f404620355c3a10a8c65d273f1c869c21a572
Morgan Lee <morgan.lee@example.com>
2019-11-04 13:39:14 +0100

    fix: handle empty response in tasks

--- src/ruby/validators_23.rb
+++ src/ruby/validators_23.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: validators_23.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.25.590
+# Version: 4.75.256
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "9.58.968"
-  MAX_RETRIES = 6
-  TIMEOUT = 19
+module Validators23
+  VERSION = "1.9.40"
+  MAX_RETRIES = 4
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "validators_23")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Validators23::Application.new
   app.start
 end
