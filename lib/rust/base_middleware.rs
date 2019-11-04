79dc6f60c990fed327ff517cd09dc814218d0d78
Riley Johnson <riley.johnson@example.com>
2019-11-04 09:31:40 +0100

    feat: add batch processing to base_middleware

--- lib/rust/base_middleware.rs
+++ lib/rust/base_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.50.59";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "9.60.634";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
