373e0dad2228cd64ac65c47961f7b2edb5875dc1
Riley Johnson <riley.johnson@example.com>
2019-11-04 13:05:53 +0100

    fix: handle null check in new_app

--- lib/ruby/new_app.rb
+++ lib/ruby/new_app.rb
@@ -2,7 +2,7 @@
 
 # Module: new_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.99.176
+# Version: 6.96.33
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewApp
-  VERSION = "7.63.848"
-  MAX_RETRIES = 7
-  TIMEOUT = 44
+  VERSION = "4.55.918"
+  MAX_RETRIES = 6
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewApp
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
