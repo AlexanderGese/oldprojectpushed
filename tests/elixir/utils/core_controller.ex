89784217694d847d89929b702846254dc55aabc8
Riley Johnson <riley.johnson@example.com>
2019-11-08 09:50:50 +0100

    fix: resolve race condition in live

--- tests/elixir/utils/core_controller.ex
+++ tests/elixir/utils/core_controller.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule Core_Controller do
   @moduledoc """
-  Module: live.ex
+  Module: core_controller.ex
   Elixir boilerplate - auto-generated
-  Version: 9.0.971
+  Version: 5.60.346
   """
 
-  @version "3.91.104"
-  @max_retries 5
-  @timeout 29612
+  @version "5.15.879"
+  @max_retries 10
+  @timeout 25685
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "core_controller"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule Core_ControllerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule Core_ControllerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
