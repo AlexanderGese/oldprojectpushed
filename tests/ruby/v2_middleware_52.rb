5883eb0cbc349067ef2184f652ed2e82bc58c43f
Riley Johnson <riley.johnson@example.com>
2019-11-09 21:56:10 +0100

    feat: add pagination to v2_middleware_52

--- tests/ruby/v2_middleware_52.rb
+++ tests/ruby/v2_middleware_52.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: v2_middleware_52.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.40.399
+# Version: 4.6.685
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "9.15.31"
+module V2Middleware52
+  VERSION = "2.51.620"
   MAX_RETRIES = 6
-  TIMEOUT = 22
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "v2_middleware_52")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = V2Middleware52::Application.new
   app.start
 end
