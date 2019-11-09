c64f9394e35994ab762106abccf54142f8ee9060
Alex Chen <alex.chen@example.com>
2019-11-09 16:53:23 +0100

    feat: add rate limiting to manager

--- lib/cplusplus/manager.cpp
+++ lib/cplusplus/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.6.754
+ * Version: 3.24.891
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "7.70.191";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 18846;
+constexpr const char* VERSION = "5.4.610";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 17200;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
