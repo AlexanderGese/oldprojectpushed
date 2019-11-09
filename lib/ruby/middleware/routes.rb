73c3be64453ba8d30e6ad572ae4c4ba2cbf0e51f
Sam Taylor <sam.taylor@example.com>
2019-11-09 21:15:50 +0100

    feat: add rate limiting to routes

--- lib/ruby/middleware/routes.rb
+++ lib/ruby/middleware/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.77.675
+# Version: 2.12.587
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "7.26.665"
-  MAX_RETRIES = 10
-  TIMEOUT = 43
+  VERSION = "9.66.761"
+  MAX_RETRIES = 5
+  TIMEOUT = 40
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
