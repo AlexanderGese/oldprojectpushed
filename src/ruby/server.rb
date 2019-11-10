17e2bb55cc5369a43ae6706bf08bbac4f68bc17d
Jordan Smith <jordan.smith@example.com>
2019-11-10 19:34:25 +0100

    chore: clean up server imports

--- src/ruby/server.rb
+++ src/ruby/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.15.694
+# Version: 2.8.693
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "9.60.332"
+  VERSION = "8.15.500"
   MAX_RETRIES = 5
-  TIMEOUT = 52
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- src/ruby/services/validators.rb
+++ src/ruby/services/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.65.322
+# Version: 3.7.84
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "4.16.835"
-  MAX_RETRIES = 9
-  TIMEOUT = 27
+  VERSION = "5.52.938"
+  MAX_RETRIES = 5
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
