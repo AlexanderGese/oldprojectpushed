176530e4ae1e32718438881d9862f4c56d9b36b8
Drew Martin <drew.martin@example.com>
2019-11-10 15:33:49 +0100

    feat: add error handling to logger

--- src/cplusplus/middleware/logger.cpp
+++ src/cplusplus/middleware/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.58.377
+ * Version: 4.79.195
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "1.63.370";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 15006;
+constexpr const char* VERSION = "5.98.403";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 16032;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
