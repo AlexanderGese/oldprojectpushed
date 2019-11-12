469e2dc1f9742528b667168564ad72bac0775da8
Sam Taylor <sam.taylor@example.com>
2019-11-12 13:02:32 +0100

    fix: handle large payloads in errors

--- src/rust/core_errors_10.rs
+++ src/rust/core_errors_10.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: core_errors_10.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.87.91";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "3.15.613";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "core_errors_10".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
