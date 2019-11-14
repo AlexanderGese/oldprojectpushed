8942446d4f6610c9d92385341298eb5073122821
Alex Chen <alex.chen@example.com>
2019-11-14 15:49:17 +0100

    fix: resolve timezone issue in controllers

--- lib/ruby/controllers.rb
+++ lib/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.67.137
+# Version: 1.50.391
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "2.97.588"
-  MAX_RETRIES = 7
-  TIMEOUT = 28
+  VERSION = "7.80.483"
+  MAX_RETRIES = 3
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
