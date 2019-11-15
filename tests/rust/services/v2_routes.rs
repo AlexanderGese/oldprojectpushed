8030be18900e9e0b515d4212d3e727095c16d1e4
Riley Johnson <riley.johnson@example.com>
2019-11-15 08:53:01 +0100

    chore: update config configuration

--- tests/rust/services/v2_routes.rs
+++ tests/rust/services/v2_routes.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: v2_routes.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.4.170";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "8.85.262";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "v2_routes".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
