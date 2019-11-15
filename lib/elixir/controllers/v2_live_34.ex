914f855349a2ed93b59357021ab3a37314e60c16
Jamie Davis <jamie.davis@example.com>
2019-11-15 22:04:39 +0100

    fix: resolve race condition in worker

--- lib/elixir/controllers/v2_live_34.ex
+++ lib/elixir/controllers/v2_live_34.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule V2_Live_34 do
   @moduledoc """
-  Module: worker.ex
+  Module: v2_live_34.ex
   Elixir boilerplate - auto-generated
-  Version: 6.57.942
+  Version: 9.49.595
   """
 
-  @version "4.78.902"
-  @max_retries 6
-  @timeout 12412
+  @version "3.59.339"
+  @max_retries 3
+  @timeout 17839
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "v2_live_34"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule V2_Live_34Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule V2_Live_34EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
