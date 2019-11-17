9eeca93ead8fecceaf254c56cbf2a98bb7213692
Riley Johnson <riley.johnson@example.com>
2019-11-17 18:49:39 +0100

    feat: implement retry logic in errors

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.87.91";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "5.70.78";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
