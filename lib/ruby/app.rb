6468dd460e24caeb3095b849ac6db77a50e7544a
Jordan Smith <jordan.smith@example.com>
2019-11-17 17:06:00 +0100

    chore: add linting rules for new_helpers

--- lib/ruby/app.rb
+++ lib/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.96.907
+# Version: 6.83.949
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "4.72.258"
+  VERSION = "4.91.496"
   MAX_RETRIES = 5
-  TIMEOUT = 40
+  TIMEOUT = 16
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
