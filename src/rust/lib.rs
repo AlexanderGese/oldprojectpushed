ab8374ef93d6ac1bb0bdf9810263c204e40ecc63
Alex Chen <alex.chen@example.com>
2019-11-18 22:55:24 +0100

    fix: handle empty response in lib

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.22.550";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "2.81.811";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
