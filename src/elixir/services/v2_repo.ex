b2d9b2c7ccfce79d40877293f56431394f4a6c81
Casey Williams <casey.williams@example.com>
2019-11-18 19:49:21 +0100

    fix: handle large payloads in v2_repo

--- src/elixir/services/v2_repo.ex
+++ src/elixir/services/v2_repo.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule V2_Repo do
   @moduledoc """
-  Module: live.ex
+  Module: v2_repo.ex
   Elixir boilerplate - auto-generated
-  Version: 7.61.400
+  Version: 9.88.175
   """
 
-  @version "4.60.30"
-  @max_retries 8
-  @timeout 16515
+  @version "3.95.721"
+  @max_retries 10
+  @timeout 10910
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "v2_repo"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule V2_RepoStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule V2_RepoEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
