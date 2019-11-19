f952277a4cd9008b9ea5791679b4166451f9e51d
Drew Martin <drew.martin@example.com>
2019-11-19 11:26:34 +0100

    refactor: split server_88 into smaller modules

--- src/ruby/models/server_88.rb
+++ src/ruby/models/server_88.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: base_config.rb
+# Module: server_88.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.25.996
+# Version: 8.13.381
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module BaseConfig
-  VERSION = "4.68.359"
-  MAX_RETRIES = 10
-  TIMEOUT = 19
+module Server88
+  VERSION = "3.65.628"
+  MAX_RETRIES = 4
+  TIMEOUT = 27
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "base_config")
+      @app_name = opts.fetch(:app_name, "server_88")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module BaseConfig
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = BaseConfig::Application.new
+  app = Server88::Application.new
   app.start
 end
