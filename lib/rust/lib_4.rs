289830c5f22fa272b610f13f03a86cd949b02931
Sam Taylor <sam.taylor@example.com>
2019-11-22 23:02:52 +0100

    refactor: extract lib_4 utility functions

--- lib/rust/lib_4.rs
+++ lib/rust/lib_4.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: lib_4.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.45.744";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "1.49.255";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "lib_4".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
