ffc48a64ae7ee819f9a7afe8b5b6e6b6bf9d5754
Sam Taylor <sam.taylor@example.com>
2019-11-24 09:19:24 +0100

    fix: correct date formatting in v2_handlers

--- lib/rust/core_config_2.rs
+++ lib/rust/core_config_2.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: core_config_2.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.5.498";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "4.47.779";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "core_config_2".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/v2_handlers.rs
+++ lib/rust/v2_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.49.775";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "2.88.275";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
