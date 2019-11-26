dd4dbf9133cd2f6887684dce6e51804d0451ad39
Jamie Davis <jamie.davis@example.com>
2019-11-26 12:17:17 +0100

    feat: add error handling to app

--- src/ruby/middleware/app.rb
+++ src/ruby/middleware/app.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_config.rb
+# Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.93.294
+# Version: 6.46.620
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplConfig
-  VERSION = "3.88.540"
-  MAX_RETRIES = 4
-  TIMEOUT = 16
+module App
+  VERSION = "8.10.38"
+  MAX_RETRIES = 9
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_config")
+      @app_name = opts.fetch(:app_name, "app")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module ImplConfig
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplConfig::Application.new
+  app = App::Application.new
   app.start
 end
