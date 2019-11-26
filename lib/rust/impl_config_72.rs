17b42f32764ac0091cdb7f54404061b50c91620a
Morgan Lee <morgan.lee@example.com>
2019-11-26 14:28:06 +0100

    fix: resolve edge case in impl_config_72 handler

--- lib/rust/impl_config_72.rs
+++ lib/rust/impl_config_72.rs
@@ -1,4 +1,4 @@
-//! Module: core_errors.rs
+//! Module: impl_config_72.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.3.862";
+const VERSION: &str = "1.36.456";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 32;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_errors".to_string(),
+            app_name: "impl_config_72".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
