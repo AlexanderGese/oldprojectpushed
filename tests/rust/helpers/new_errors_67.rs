2f72ab3e601bfa65891fcc80bf63d87d7ad422ac
Sam Taylor <sam.taylor@example.com>
2019-11-27 14:40:11 +0100

    fix: correct date formatting in impl_utils_79

--- tests/rust/helpers/new_errors_67.rs
+++ tests/rust/helpers/new_errors_67.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: new_errors_67.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.59.136";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "6.73.424";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "new_errors_67".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
