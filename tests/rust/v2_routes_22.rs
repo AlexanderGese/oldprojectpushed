0d1a87bb8b8463d0565f87aeb631118a2536810c
Riley Johnson <riley.johnson@example.com>
2019-11-27 10:20:11 +0100

    fix: handle empty response in handlers

--- tests/rust/v2_routes_22.rs
+++ tests/rust/v2_routes_22.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: v2_routes_22.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.27.774";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "3.26.8";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "v2_routes_22".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
