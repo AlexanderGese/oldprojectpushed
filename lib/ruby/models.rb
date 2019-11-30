847407374f01e2ef670102d0f0e7940ff73b2a01
Jamie Davis <jamie.davis@example.com>
2019-11-30 15:55:59 +0100

    refactor: improve models test coverage

--- lib/ruby/models.rb
+++ lib/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.91.699
+# Version: 3.84.815
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "8.39.431"
-  MAX_RETRIES = 5
-  TIMEOUT = 26
+  VERSION = "2.23.940"
+  MAX_RETRIES = 4
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
