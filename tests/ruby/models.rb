60bf89698ecd4666879ba96deccb332cbb43444e
Morgan Lee <morgan.lee@example.com>
2019-12-04 10:02:15 +0100

    feat: add models module with core functionality

--- tests/ruby/models.rb
+++ tests/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.67.283
+# Version: 9.5.928
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "4.39.583"
+  VERSION = "8.61.688"
   MAX_RETRIES = 8
-  TIMEOUT = 54
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
