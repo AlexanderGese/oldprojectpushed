b4ff74fb223cdc77dae68d065d2b9dd4c84ab7ef
Casey Williams <casey.williams@example.com>
2019-12-05 16:49:33 +0100

    refactor: extract base_controllers utility functions

--- tests/ruby/base_controllers.rb
+++ tests/ruby/base_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: base_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.26.577
+# Version: 3.38.196
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseControllers
-  VERSION = "8.12.336"
+  VERSION = "7.74.909"
   MAX_RETRIES = 7
-  TIMEOUT = 31
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseControllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
