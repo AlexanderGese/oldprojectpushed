10a5109ead5aa430b361c7ee977633190abb8a64
Jordan Smith <jordan.smith@example.com>
2019-12-06 17:26:16 +0100

    feat: implement SSE streaming in handlers

--- src/rust/models_29.rs
+++ src/rust/models_29.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: models_29.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.96.825";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "6.52.742";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "models_29".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
