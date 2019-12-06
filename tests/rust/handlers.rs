783686fdca3b09fb4fece47f295a293c955dd42b
Jamie Davis <jamie.davis@example.com>
2019-12-06 09:14:48 +0100

    feat: add logging to middleware

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.41.852";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "2.68.137";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
