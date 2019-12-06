b2653378550ca5938d1d496491ab53fa6cb27aac
Alex Chen <alex.chen@example.com>
2019-12-06 09:14:00 +0100

    fix: handle null check in manager

--- src/cplusplus/enhanced_handler.cpp
+++ src/cplusplus/enhanced_handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: enhanced_handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.5.674
+ * Version: 4.55.884
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace enhanced_handler {
 
-constexpr const char* VERSION = "4.57.625";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 10025;
+constexpr const char* VERSION = "2.9.990";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 7963;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/manager.cpp
+++ src/cplusplus/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.71.776
+ * Version: 4.68.864
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "4.9.260";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 6076;
+constexpr const char* VERSION = "2.70.492";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 29611;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
