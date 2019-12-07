122e530a487803ff555228fb6f626b53460fc6b5
Jamie Davis <jamie.davis@example.com>
2019-12-07 12:29:44 +0100

    fix: handle concurrent access in routes

--- tests/rust/components/database.rs
+++ tests/rust/components/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.32.403";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "8.94.522";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
