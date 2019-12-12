e19efe6962f1934d83ef1db2afe94b96096139be
Casey Williams <casey.williams@example.com>
2019-12-12 08:17:36 +0100

    feat: add caching layer to v2_errors

--- tests/rust/v2_errors.rs
+++ tests/rust/v2_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.77.54";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "2.51.665";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
