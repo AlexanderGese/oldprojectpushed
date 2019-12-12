bcef1eacfe03b5e73009c0f8d049f1994320ef1d
Jamie Davis <jamie.davis@example.com>
2019-12-12 23:53:43 +0100

    feat: implement SSE streaming in database

--- src/rust/components/base_config.rs
+++ src/rust/components/base_config.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: base_config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.13.993";
-const MAX_RETRIES: u32 = 9;
+const VERSION: &str = "7.36.187";
+const MAX_RETRIES: u32 = 7;
 const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "base_config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/impl_utils_31.rs
+++ src/rust/impl_utils_31.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: impl_utils_31.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.13.993";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "9.8.703";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "impl_utils_31".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
