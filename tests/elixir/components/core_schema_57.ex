79aff78a7b9249ede0a688018504a40bdc7bfec3
Jordan Smith <jordan.smith@example.com>
2019-12-13 12:59:16 +0100

    fix: handle concurrent access in schema

--- tests/elixir/components/core_schema_57.ex
+++ tests/elixir/components/core_schema_57.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule Core_Schema_57 do
   @moduledoc """
-  Module: schema.ex
+  Module: core_schema_57.ex
   Elixir boilerplate - auto-generated
-  Version: 4.48.176
+  Version: 1.45.712
   """
 
-  @version "2.61.250"
-  @max_retries 3
-  @timeout 28825
+  @version "2.51.89"
+  @max_retries 5
+  @timeout 5732
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "core_schema_57"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule Core_Schema_57Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule Core_Schema_57EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
