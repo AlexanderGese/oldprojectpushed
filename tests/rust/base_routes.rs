e947d818755d2e1410f29f7c9aa46c63f2b0ad18
Sam Taylor <sam.taylor@example.com>
2019-12-14 17:29:49 +0100

    test: add unit tests for core_main

--- tests/rust/base_routes.rs
+++ tests/rust/base_routes.rs
@@ -1,4 +1,4 @@
-//! Module: core_main.rs
+//! Module: base_routes.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.40.597";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "7.71.269";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_main".to_string(),
+            app_name: "base_routes".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/new_lib_32.rs
+++ tests/rust/new_lib_32.rs
@@ -1,4 +1,4 @@
-//! Module: core_main.rs
+//! Module: new_lib_32.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.40.597";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "6.53.802";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_main".to_string(),
+            app_name: "new_lib_32".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
