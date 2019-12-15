b5673fe90bb29f913a31f95396f9419b22c791a1
Morgan Lee <morgan.lee@example.com>
2019-12-15 23:01:11 +0100

    chore: add linting rules for models

--- src/rust/controllers/models.rs
+++ src/rust/controllers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.35.58";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "1.86.816";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
