93b49881347897b3f0bc89363f656bb14b7d9959
Casey Williams <casey.williams@example.com>
2019-12-17 23:10:41 +0100

    fix: correct type definitions in routes_86

--- lib/ruby/routes_86.rb
+++ lib/ruby/routes_86.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: routes_86.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.70.652
+# Version: 8.99.657
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "4.53.444"
-  MAX_RETRIES = 8
-  TIMEOUT = 10
+module Routes86
+  VERSION = "3.94.223"
+  MAX_RETRIES = 5
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "routes_86")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = Routes86::Application.new
   app.start
 end
