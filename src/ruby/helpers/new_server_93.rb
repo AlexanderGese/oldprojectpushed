83353be8ae0032b2f4758d36f5c3178015995a71
Alex Chen <alex.chen@example.com>
2019-12-17 09:16:30 +0100

    fix: patch security vulnerability in tasks

--- src/ruby/helpers/new_server_93.rb
+++ src/ruby/helpers/new_server_93.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_config.rb
+# Module: new_server_93.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.90.840
+# Version: 6.80.110
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreConfig
-  VERSION = "1.70.460"
-  MAX_RETRIES = 9
-  TIMEOUT = 6
+module NewServer93
+  VERSION = "5.92.887"
+  MAX_RETRIES = 10
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_config")
+      @app_name = opts.fetch(:app_name, "new_server_93")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreConfig
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreConfig::Application.new
+  app = NewServer93::Application.new
   app.start
 end
