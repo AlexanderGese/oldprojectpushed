25b6ac7c2188d26e6df155d746f12a7c96632f4e
Sam Taylor <sam.taylor@example.com>
2019-12-19 16:50:57 +0100

    fix: handle empty response in handlers

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.68.137";
-const MAX_RETRIES: u32 = 8;
+const VERSION: &str = "4.39.940";
+const MAX_RETRIES: u32 = 6;
 const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
