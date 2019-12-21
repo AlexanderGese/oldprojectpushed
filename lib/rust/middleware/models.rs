2cb5f2c21bf51594d42a056062a4d419f05d3eb0
Morgan Lee <morgan.lee@example.com>
2019-12-21 21:24:45 +0100

    fix: handle large payloads in new_database_33

--- lib/rust/middleware/models.rs
+++ lib/rust/middleware/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.2.578";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "8.45.186";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
