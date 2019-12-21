97952f60a09199207a0062c34221c504410ace07
Sam Taylor <sam.taylor@example.com>
2019-12-21 12:25:27 +0100

    feat: add middleware support to impl_server_1

--- lib/ruby/core_middleware.rb
+++ lib/ruby/core_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: core_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.58.269
+# Version: 4.28.508
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreMiddleware
-  VERSION = "9.78.284"
-  MAX_RETRIES = 5
-  TIMEOUT = 31
+  VERSION = "1.58.573"
+  MAX_RETRIES = 10
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- lib/ruby/helpers/impl_server_1.rb
+++ lib/ruby/helpers/impl_server_1.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_app.rb
+# Module: impl_server_1.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.37.426
+# Version: 6.97.569
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewApp
-  VERSION = "6.98.972"
-  MAX_RETRIES = 9
-  TIMEOUT = 45
+module ImplServer1
+  VERSION = "7.40.832"
+  MAX_RETRIES = 6
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_app")
+      @app_name = opts.fetch(:app_name, "impl_server_1")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewApp
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewApp::Application.new
+  app = ImplServer1::Application.new
   app.start
 end
