85415e0aa78c8e1cf6c3792ba1ee28b66ea7d165
Jamie Davis <jamie.davis@example.com>
2019-12-21 14:40:44 +0100

    fix: handle null check in routes

--- lib/ruby/base_models.rb
+++ lib/ruby/base_models.rb
@@ -2,7 +2,7 @@
 
 # Module: base_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.2.728
+# Version: 9.93.265
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseModels
-  VERSION = "4.29.659"
-  MAX_RETRIES = 5
-  TIMEOUT = 8
+  VERSION = "9.18.229"
+  MAX_RETRIES = 7
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
