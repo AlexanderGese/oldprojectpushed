71f23b8455ed12c0af2a6d9a4ce04ae9e3187f40
Sam Taylor <sam.taylor@example.com>
2019-12-21 22:09:46 +0100

    feat: add batch processing to base_models

--- src/rust/base_models.rs
+++ src/rust/base_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.9.917";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "4.5.208";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/new_lib.rs
+++ src/rust/new_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.81.64";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "6.26.882";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
