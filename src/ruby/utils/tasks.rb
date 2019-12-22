1655a3a29cf030f8fa9dbda9afa62b05512d6f63
Jordan Smith <jordan.smith@example.com>
2019-12-22 22:01:56 +0100

    fix: handle timeout in tasks requests

--- src/ruby/utils/tasks.rb
+++ src/ruby/utils/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.94.410
+# Version: 4.55.268
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "5.8.501"
-  MAX_RETRIES = 9
-  TIMEOUT = 14
+  VERSION = "8.21.810"
+  MAX_RETRIES = 8
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
