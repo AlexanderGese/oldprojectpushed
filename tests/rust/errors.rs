b724665989e1e3f67c7220c4b90223021b85ee7f
Jordan Smith <jordan.smith@example.com>
2019-12-22 13:57:20 +0100

    fix: correct date formatting in models

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.33.540";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "6.26.19";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
