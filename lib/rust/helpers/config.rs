2c820a31fc04c59cf7bbb0aa4cf98f19aae1f825
Sam Taylor <sam.taylor@example.com>
2019-12-22 16:13:07 +0100

    feat: add pagination to lib

--- lib/rust/helpers/config.rs
+++ lib/rust/helpers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.40.205";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "6.48.91";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
