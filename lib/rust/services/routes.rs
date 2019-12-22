d771dc4b468ffbce8f85279872e01bedaf546db8
Jordan Smith <jordan.smith@example.com>
2019-12-22 22:55:50 +0100

    refactor: decouple main dependencies

--- lib/rust/services/routes.rs
+++ lib/rust/services/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.26.706";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "1.79.404";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
