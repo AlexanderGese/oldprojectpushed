1b142c81c82605d8e3f89841ce6bff08b9282257
Drew Martin <drew.martin@example.com>
2019-12-24 15:56:49 +0100

    refactor: split core_utils_97 into smaller modules

--- lib/rust/components/core_utils_97.rs
+++ lib/rust/components/core_utils_97.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: core_utils_97.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.6.576";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "6.49.470";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "core_utils_97".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
