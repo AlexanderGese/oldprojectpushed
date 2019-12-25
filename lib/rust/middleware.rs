4a4f70c85b10279674bf12ef6778c843373456ce
Jordan Smith <jordan.smith@example.com>
2019-12-25 11:20:42 +0100

    chore: update lib dependencies

--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.59.596";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "7.87.599";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
