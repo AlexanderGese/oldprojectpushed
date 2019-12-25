94d46348ff155e74e3469b5b8ed6119aecda8f3b
Jamie Davis <jamie.davis@example.com>
2019-12-25 14:22:36 +0100

    fix: handle null check in config

--- lib/ruby/tasks.rb
+++ lib/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.21.100
+# Version: 8.69.943
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "9.99.339"
+  VERSION = "9.53.549"
   MAX_RETRIES = 7
-  TIMEOUT = 39
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
