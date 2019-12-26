75ee65324507b474f4f3e3b3e66c395ca8a2f300
Casey Williams <casey.williams@example.com>
2019-12-26 09:45:13 +0100

    chore: update context dependencies

--- tests/elixir/middleware/new_router_51.ex
+++ tests/elixir/middleware/new_router_51.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule New_Router_51 do
   @moduledoc """
-  Module: context.ex
+  Module: new_router_51.ex
   Elixir boilerplate - auto-generated
-  Version: 2.94.416
+  Version: 5.80.166
   """
 
-  @version "1.56.347"
-  @max_retries 10
-  @timeout 12768
+  @version "2.26.660"
+  @max_retries 4
+  @timeout 25402
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "new_router_51"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule New_Router_51Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule New_Router_51EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
