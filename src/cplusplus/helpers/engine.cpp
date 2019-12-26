7cc2bdb7bd34a7e7e583f925363f778ec6c1ccb0
Jamie Davis <jamie.davis@example.com>
2019-12-26 18:55:37 +0100

    test: add unit tests for logger

--- src/cplusplus/helpers/engine.cpp
+++ src/cplusplus/helpers/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.38.635
+ * Version: 5.9.874
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "6.2.551";
+constexpr const char* VERSION = "8.0.175";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 23586;
+constexpr int TIMEOUT_MS = 15674;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/helpers/logger.cpp
+++ src/cplusplus/helpers/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.2.805
+ * Version: 6.74.830
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "9.50.711";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 15423;
+constexpr const char* VERSION = "9.30.537";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 8541;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
