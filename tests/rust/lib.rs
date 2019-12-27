110ef63436f83de418ca5c1e1b51ee9e14e67774
Casey Williams <casey.williams@example.com>
2019-12-27 19:07:46 +0100

    docs: add documentation for lib

--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.28.964";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "5.46.966";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
