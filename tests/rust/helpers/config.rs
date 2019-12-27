7a9eeb85908658c0f99a9e59dfff27697b26c640
Drew Martin <drew.martin@example.com>
2019-12-27 15:41:40 +0100

    feat: add pagination to config

--- tests/rust/helpers/config.rs
+++ tests/rust/helpers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.15.2";
+const VERSION: &str = "2.95.457";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 16;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/new_routes_26.rs
+++ tests/rust/new_routes_26.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: new_routes_26.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.17.345";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "4.80.535";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "new_routes_26".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
