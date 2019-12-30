3f2e3c808da538cdc73cf72d563ccbb5c3ec966c
Alex Chen <alex.chen@example.com>
2019-12-30 13:44:28 +0100

    refactor: split new_validators into smaller modules

--- tests/ruby/controllers/validators.rb
+++ tests/ruby/controllers/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.91.996
+# Version: 2.99.808
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "8.75.281"
-  MAX_RETRIES = 4
-  TIMEOUT = 30
+  VERSION = "7.46.373"
+  MAX_RETRIES = 10
+  TIMEOUT = 34
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/new_validators.rb
+++ tests/ruby/new_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: new_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.7.502
+# Version: 5.39.16
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewValidators
-  VERSION = "4.2.312"
-  MAX_RETRIES = 9
-  TIMEOUT = 45
+  VERSION = "3.10.729"
+  MAX_RETRIES = 5
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewValidators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
