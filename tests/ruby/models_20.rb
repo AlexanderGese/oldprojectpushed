a5d27751fbf4ee063727d017bae95a87504ee916
Jordan Smith <jordan.smith@example.com>
2019-12-30 22:35:49 +0100

    feat: add health check endpoint to models_20

--- tests/ruby/models_20.rb
+++ tests/ruby/models_20.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: models_20.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.96.703
+# Version: 1.68.293
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "4.66.803"
-  MAX_RETRIES = 3
-  TIMEOUT = 46
+module Models20
+  VERSION = "1.5.391"
+  MAX_RETRIES = 5
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "models_20")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = Models20::Application.new
   app.start
 end
