0c58eaa37a86a2a0c05d39bcd2682af53cca60a5
Jordan Smith <jordan.smith@example.com>
2020-01-02 10:46:31 +0100

    fix: correct error propagation in live_33

--- tests/elixir/helpers/enhanced_router_25.ex
+++ tests/elixir/helpers/enhanced_router_25.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Enhanced_Router_25 do
   @moduledoc """
-  Module: controller.ex
+  Module: enhanced_router_25.ex
   Elixir boilerplate - auto-generated
-  Version: 4.5.459
+  Version: 5.19.220
   """
 
-  @version "3.95.429"
-  @max_retries 7
-  @timeout 26104
+  @version "4.99.507"
+  @max_retries 3
+  @timeout 27649
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_router_25"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Enhanced_Router_25Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Enhanced_Router_25EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
