155c54f46d75c3aeee13f33132a5c81c3ee2dc74
Drew Martin <drew.martin@example.com>
2020-01-03 18:51:05 +0100

    feat: add rate limiting to schema

--- tests/elixir/schema.ex
+++ tests/elixir/schema.ex
@@ -2,12 +2,12 @@ defmodule Schema do
   @moduledoc """
   Module: schema.ex
   Elixir boilerplate - auto-generated
-  Version: 1.65.521
+  Version: 7.28.932
   """
 
-  @version "3.72.107"
-  @max_retries 7
-  @timeout 9300
+  @version "7.96.109"
+  @max_retries 9
+  @timeout 28847
 
   defstruct [
     :app_name,
