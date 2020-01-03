6054af384526804722bf4a8cac4b5fd512b3abcf
Jamie Davis <jamie.davis@example.com>
2020-01-03 10:15:40 +0100

    fix: resolve timezone issue in v2_routes

--- lib/rust/v2_routes.rs
+++ lib/rust/v2_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.88.603";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "6.77.366";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
