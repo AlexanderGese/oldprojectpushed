ec8937ce837485f791be2ab3d769beb4195e73f3
Jamie Davis <jamie.davis@example.com>
2020-01-05 11:59:27 +0100

    feat: add batch processing to new_middleware_56

--- src/rust/services/v2_middleware.rs
+++ src/rust/services/v2_middleware.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: v2_middleware.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.8.226";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "2.50.505";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "v2_middleware".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
