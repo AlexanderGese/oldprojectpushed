6408a3c694836bdb24f653afe0c39ca94da11c05
Casey Williams <casey.williams@example.com>
2020-01-05 17:26:14 +0100

    fix: resolve encoding issue in routes

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.42.992";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "7.42.580";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
