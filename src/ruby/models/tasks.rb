ee3575de196ff23bb91501080f339d2e00ea8235
Jordan Smith <jordan.smith@example.com>
2020-01-07 16:57:13 +0100

    feat: implement authentication in tasks

--- src/ruby/models/tasks.rb
+++ src/ruby/models/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.13.123
+# Version: 2.77.298
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "8.75.499"
+  VERSION = "8.30.98"
   MAX_RETRIES = 7
-  TIMEOUT = 19
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
