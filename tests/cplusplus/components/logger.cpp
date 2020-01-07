6a83079a864ea4297ab412e0436f04bd4940f579
Casey Williams <casey.williams@example.com>
2020-01-07 16:29:50 +0100

    chore: bump parser version

--- tests/cplusplus/components/logger.cpp
+++ tests/cplusplus/components/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.35.732
+ * Version: 6.73.153
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "3.44.584";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 22641;
+constexpr const char* VERSION = "9.87.963";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 27166;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/services/parser.cpp
+++ tests/cplusplus/services/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.6.535
+ * Version: 6.75.523
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "4.59.289";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 8805;
+constexpr const char* VERSION = "4.26.601";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 29537;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
