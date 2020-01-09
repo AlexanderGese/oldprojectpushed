abf57c39fce8a4f51134ffef13c0b1b64972b131
Casey Williams <casey.williams@example.com>
2020-01-09 13:45:33 +0100

    chore: clean up handlers imports

--- src/rust/handlers.rs
+++ src/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.89.298";
+const VERSION: &str = "1.80.346";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 35;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
