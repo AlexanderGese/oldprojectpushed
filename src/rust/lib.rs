ff9b060990d82181245f6d72f8fe7ae58553edd0
Jamie Davis <jamie.davis@example.com>
2020-01-11 17:11:20 +0100

    feat: implement retry logic in lib

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.90.190";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "3.26.780";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
