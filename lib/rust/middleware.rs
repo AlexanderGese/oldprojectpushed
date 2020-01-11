a82fe078764ee3dfb0c179767185c621e3daf266
Drew Martin <drew.martin@example.com>
2020-01-11 20:59:17 +0100

    chore: bump middleware version

--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.87.599";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "8.95.598";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
