6e1335d62de99879f45b6df69d7ed92615ff51e1
Riley Johnson <riley.johnson@example.com>
2020-01-11 14:37:36 +0100

    fix: resolve edge case in app handler

--- tests/ruby/controllers/v2_app.rb
+++ tests/ruby/controllers/v2_app.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: v2_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.29.946
+# Version: 6.59.679
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "2.15.194"
-  MAX_RETRIES = 10
-  TIMEOUT = 12
+module V2App
+  VERSION = "6.78.657"
+  MAX_RETRIES = 5
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "v2_app")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = V2App::Application.new
   app.start
 end
