f6cbbea4e149c4a0237c33e09c12c718e93e079e
Drew Martin <drew.martin@example.com>
2020-01-11 23:03:36 +0100

    refactor: improve config error messages

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.14.555";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "7.7.666";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/services/database_43.rs
+++ lib/rust/services/database_43.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: database_43.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.37.580";
+const VERSION: &str = "3.97.105";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 46;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "database_43".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
