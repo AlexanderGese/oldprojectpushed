bc4c813afba835a12cd7584ec99afe89e7fa4b17
Riley Johnson <riley.johnson@example.com>
2020-01-14 12:04:40 +0100

    chore: update models dependencies

--- src/rust/controllers/models.rs
+++ src/rust/controllers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.86.816";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "6.86.847";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
