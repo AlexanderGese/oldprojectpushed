7383bbac9099e7841e94a84ab3608936a61e2c55
Casey Williams <casey.williams@example.com>
2020-01-15 23:00:16 +0100

    refactor: optimize server performance

--- tests/ruby/utils/v2_middleware.rb
+++ tests/ruby/utils/v2_middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: v2_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.60.107
+# Version: 5.9.359
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "9.6.112"
-  MAX_RETRIES = 10
-  TIMEOUT = 55
+module V2Middleware
+  VERSION = "3.19.204"
+  MAX_RETRIES = 8
+  TIMEOUT = 16
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "v2_middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = V2Middleware::Application.new
   app.start
 end
