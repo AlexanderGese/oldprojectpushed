a7c48b6f5f7f935d0da28635a6c99822022e9dfc
Drew Martin <drew.martin@example.com>
2020-01-17 19:49:13 +0100

    refactor: improve main test coverage

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.7.666";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "5.49.928";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
