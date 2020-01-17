c47cecf0034ac55f1935cf51c1b2a9768c251219
Sam Taylor <sam.taylor@example.com>
2020-01-17 12:04:32 +0100

    fix: resolve race condition in logger

--- tests/cplusplus/base_utils_9.cpp
+++ tests/cplusplus/base_utils_9.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: logger.cpp
+ * Module: base_utils_9.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.52.493
+ * Version: 1.28.819
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace logger {
+namespace base_utils_9 {
 
-constexpr const char* VERSION = "3.61.40";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 11538;
+constexpr const char* VERSION = "7.9.861";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 8391;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace logger
+} // namespace base_utils_9
 
 int main() {
-    using namespace logger;
+    using namespace base_utils_9;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
