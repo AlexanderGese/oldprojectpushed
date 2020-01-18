0f0d242b8d22b73b2f90a377e173834c0b37e5f4
Drew Martin <drew.martin@example.com>
2020-01-18 13:06:37 +0100

    feat: add rate limiting to enhanced_utils_65

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.42.580";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "2.86.94";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
