4e7910dc11fa8898d72297b45e641793fa8543cb
Jamie Davis <jamie.davis@example.com>
2020-01-18 16:09:40 +0100

    fix: correct error propagation in tasks_17

--- lib/ruby/tasks_17.rb
+++ lib/ruby/tasks_17.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: tasks_17.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.87.701
+# Version: 6.8.918
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "8.43.879"
-  MAX_RETRIES = 9
-  TIMEOUT = 12
+module Tasks17
+  VERSION = "2.96.241"
+  MAX_RETRIES = 4
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "tasks_17")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = Tasks17::Application.new
   app.start
 end
