1f65eb4b3234700f4772d41e818e1bd30233b09a
Drew Martin <drew.martin@example.com>
2020-01-21 09:50:24 +0100

    refactor: improve utils error messages

--- lib/rust/models/main.rs
+++ lib/rust/models/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.15.53";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "9.42.737";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils.rs
+++ lib/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.60.930";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "2.74.693";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
