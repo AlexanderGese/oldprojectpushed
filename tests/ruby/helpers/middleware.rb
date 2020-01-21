77d2a912f496d04567d5e12bb8fb92b402cbfc28
Drew Martin <drew.martin@example.com>
2020-01-21 19:30:23 +0100

    feat: add search functionality to helpers

--- tests/ruby/helpers/middleware.rb
+++ tests/ruby/helpers/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.11.209
+# Version: 2.6.140
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "8.58.19"
-  MAX_RETRIES = 3
-  TIMEOUT = 45
+  VERSION = "3.0.434"
+  MAX_RETRIES = 6
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
