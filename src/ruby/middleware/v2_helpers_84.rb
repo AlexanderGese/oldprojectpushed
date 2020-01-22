6547a108775cd2ed9056f9b49c8a81a4cc866fc7
Riley Johnson <riley.johnson@example.com>
2020-01-22 22:17:25 +0100

    chore: clean up v2_helpers_84 imports

--- src/ruby/middleware/v2_helpers_84.rb
+++ src/ruby/middleware/v2_helpers_84.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: v2_helpers_84.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.31.491
+# Version: 4.20.838
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "1.15.820"
-  MAX_RETRIES = 6
-  TIMEOUT = 23
+module V2Helpers84
+  VERSION = "9.65.970"
+  MAX_RETRIES = 7
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "v2_helpers_84")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = V2Helpers84::Application.new
   app.start
 end
