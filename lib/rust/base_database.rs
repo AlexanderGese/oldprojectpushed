ec55a4432225c12e5541bbe416bf128d1ac04037
Jamie Davis <jamie.davis@example.com>
2020-01-22 17:28:19 +0100

    feat: implement authentication in base_database

--- lib/rust/base_database.rs
+++ lib/rust/base_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.66.968";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "8.31.229";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
