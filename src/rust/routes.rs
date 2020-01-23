e874ff38215aaf065220bb65c75a173f6060bee9
Riley Johnson <riley.johnson@example.com>
2020-01-23 12:40:04 +0100

    chore: bump routes version

--- src/rust/routes.rs
+++ src/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.98.939";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "8.28.774";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
