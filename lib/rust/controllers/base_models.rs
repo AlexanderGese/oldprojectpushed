3fa1cea7edac1cfd9b86ec49fd33ef20cbc6ecbc
Jordan Smith <jordan.smith@example.com>
2020-01-24 14:47:35 +0100

    feat: add export functionality to models_72

--- lib/rust/controllers/base_models.rs
+++ lib/rust/controllers/base_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.30.238";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "1.85.402";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
