17207e22f922c8ef155495c5530c4775259ac6a8
Jamie Davis <jamie.davis@example.com>
2020-01-25 16:03:47 +0100

    chore: update database_82 configuration

--- tests/rust/database_82.rs
+++ tests/rust/database_82.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: database_82.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.17.501";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "2.29.260";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "database_82".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
