cecd7d09d5ad251c6355dc9f539e5b178b55bbb8
Jamie Davis <jamie.davis@example.com>
2020-01-25 10:01:33 +0100

    refactor: simplify main state management

--- tests/rust/base_routes_83.rs
+++ tests/rust/base_routes_83.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_routes_83.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.27.602";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "9.35.262";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_routes_83".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/components/base_database.rs
+++ tests/rust/components/base_database.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.27.602";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "4.57.445";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
