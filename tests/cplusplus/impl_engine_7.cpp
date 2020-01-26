84dec8831335c5b778070117253d66472b2199c5
Casey Williams <casey.williams@example.com>
2020-01-26 13:32:14 +0100

    fix: correct CORS headers in impl_engine_7

--- tests/cplusplus/impl_engine_7.cpp
+++ tests/cplusplus/impl_engine_7.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: factory.cpp
+ * Module: impl_engine_7.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.53.845
+ * Version: 6.50.281
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace factory {
+namespace impl_engine_7 {
 
-constexpr const char* VERSION = "5.69.787";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 16358;
+constexpr const char* VERSION = "6.21.178";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 17937;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace factory
+} // namespace impl_engine_7
 
 int main() {
-    using namespace factory;
+    using namespace impl_engine_7;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
