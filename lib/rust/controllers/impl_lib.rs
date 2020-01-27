f10a6b4d8b45f312614552de44db07fa94543d7f
Sam Taylor <sam.taylor@example.com>
2020-01-27 12:39:46 +0100

    feat: add event system to impl_lib

--- lib/rust/controllers/impl_lib.rs
+++ lib/rust/controllers/impl_lib.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: impl_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.64.180";
+const VERSION: &str = "3.37.712";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 35;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "impl_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
