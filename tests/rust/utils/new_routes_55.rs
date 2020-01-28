f3da7a7686199f30aa09e8f8825a5eb38a7d3eb1
Alex Chen <alex.chen@example.com>
2020-01-28 22:53:27 +0100

    fix: handle concurrent access in new_routes_55

--- tests/rust/utils/new_routes_55.rs
+++ tests/rust/utils/new_routes_55.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: new_routes_55.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.38.468";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "7.7.384";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "new_routes_55".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
