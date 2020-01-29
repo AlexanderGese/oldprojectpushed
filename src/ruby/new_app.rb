735a546c2a7ca9ac5549f512ea79ea87d317591f
Morgan Lee <morgan.lee@example.com>
2020-01-29 18:09:45 +0100

    chore: remove deprecated new_app code

--- src/ruby/new_app.rb
+++ src/ruby/new_app.rb
@@ -2,7 +2,7 @@
 
 # Module: new_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.36.676
+# Version: 2.26.818
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewApp
-  VERSION = "5.80.347"
-  MAX_RETRIES = 5
-  TIMEOUT = 55
+  VERSION = "9.55.828"
+  MAX_RETRIES = 3
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewApp
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
