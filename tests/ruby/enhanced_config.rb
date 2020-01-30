1993ab9e04de2b5508ed2be336bf7e0c6ab16431
Jamie Davis <jamie.davis@example.com>
2020-01-30 18:20:42 +0100

    chore: remove deprecated enhanced_config code

--- tests/ruby/enhanced_config.rb
+++ tests/ruby/enhanced_config.rb
@@ -2,7 +2,7 @@
 
 # Module: enhanced_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.99.374
+# Version: 5.13.207
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module EnhancedConfig
-  VERSION = "8.65.38"
-  MAX_RETRIES = 3
-  TIMEOUT = 42
+  VERSION = "4.27.331"
+  MAX_RETRIES = 4
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module EnhancedConfig
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
--- tests/ruby/enhanced_routes.rb
+++ tests/ruby/enhanced_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: enhanced_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.69.22
+# Version: 2.20.113
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module EnhancedRoutes
-  VERSION = "8.82.910"
-  MAX_RETRIES = 6
-  TIMEOUT = 41
+  VERSION = "6.49.936"
+  MAX_RETRIES = 10
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module EnhancedRoutes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
