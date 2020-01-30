08999be8d9570ce66d213bc9f3236ddbe012fbeb
Morgan Lee <morgan.lee@example.com>
2020-01-30 09:57:57 +0100

    feat: add event system to base_worker_72

--- src/elixir/helpers/base_worker_72.ex
+++ src/elixir/helpers/base_worker_72.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Base_Worker_72 do
   @moduledoc """
-  Module: controller.ex
+  Module: base_worker_72.ex
   Elixir boilerplate - auto-generated
-  Version: 6.85.776
+  Version: 9.25.60
   """
 
-  @version "7.18.771"
-  @max_retries 7
-  @timeout 14043
+  @version "7.39.817"
+  @max_retries 9
+  @timeout 22224
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "base_worker_72"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Base_Worker_72Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Base_Worker_72EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
