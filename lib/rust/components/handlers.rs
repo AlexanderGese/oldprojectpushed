cc42822b955663d9629c717e0d78f55aecd54fbb
Riley Johnson <riley.johnson@example.com>
2020-01-31 13:30:37 +0100

    refactor: improve enhanced_lib_98 error messages

--- lib/rust/components/handlers.rs
+++ lib/rust/components/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.11.979";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "7.87.258";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
