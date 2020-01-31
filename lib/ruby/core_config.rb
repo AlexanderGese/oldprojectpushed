3d107976b560d8fc100662cd11eb3022c803fdee
Alex Chen <alex.chen@example.com>
2020-01-31 18:53:35 +0100

    feat: add validation to core_config inputs

--- lib/ruby/core_config.rb
+++ lib/ruby/core_config.rb
@@ -2,7 +2,7 @@
 
 # Module: core_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.3.272
+# Version: 3.53.215
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreConfig
-  VERSION = "3.43.698"
+  VERSION = "9.29.966"
   MAX_RETRIES = 3
-  TIMEOUT = 19
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreConfig
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
