92488512724e985ff2916092c07e6cce96fa63c9
Casey Williams <casey.williams@example.com>
2020-01-31 19:04:14 +0100

    feat: add middleware support to models

--- lib/ruby/utils/base_models_75.rb
+++ lib/ruby/utils/base_models_75.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: base_models_75.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.77.95
+# Version: 2.84.817
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "8.31.907"
-  MAX_RETRIES = 3
-  TIMEOUT = 49
+module BaseModels75
+  VERSION = "9.49.46"
+  MAX_RETRIES = 7
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "base_models_75")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = BaseModels75::Application.new
   app.start
 end
