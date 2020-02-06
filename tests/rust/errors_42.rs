46832682b2fcd0077d0a8c510074a8407f796c24
Jordan Smith <jordan.smith@example.com>
2020-02-06 08:05:53 +0100

    refactor: split impl_handlers into smaller modules

--- tests/rust/errors_42.rs
+++ tests/rust/errors_42.rs
@@ -1,4 +1,4 @@
-//! Module: impl_handlers.rs
+//! Module: errors_42.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.6.616";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "9.47.390";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_handlers".to_string(),
+            app_name: "errors_42".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
