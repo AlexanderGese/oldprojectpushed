30e1c612dab1288a2708979870defb11c7f94b9f
Riley Johnson <riley.johnson@example.com>
2020-02-07 09:20:33 +0100

    feat: implement WebSocket support in errors

--- src/rust/handlers_47.rs
+++ src/rust/handlers_47.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: handlers_47.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.6.568";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "8.79.873";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "handlers_47".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/helpers/v2_errors.rs
+++ src/rust/helpers/v2_errors.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: v2_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.6.568";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "3.51.30";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "v2_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
