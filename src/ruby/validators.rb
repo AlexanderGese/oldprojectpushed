be85019ba7e0907cfd14e26df14b4b10632f6519
Riley Johnson <riley.johnson@example.com>
2020-02-07 23:20:40 +0100

    refactor: optimize validators performance

--- src/ruby/validators.rb
+++ src/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.89.652
+# Version: 3.50.652
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "6.36.41"
-  MAX_RETRIES = 4
-  TIMEOUT = 8
+  VERSION = "5.7.185"
+  MAX_RETRIES = 6
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
