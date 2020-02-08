7b312557d04eb69100edcc1fb5d00361b3de1d88
Jordan Smith <jordan.smith@example.com>
2020-02-08 16:11:56 +0100

    fix: correct CORS headers in enhanced_config

--- src/rust/enhanced_config.rs
+++ src/rust/enhanced_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.25.947";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "7.8.929";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
