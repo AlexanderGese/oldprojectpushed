f8304a865aea7a5c224dedabc60c3e4e058e5d1d
Riley Johnson <riley.johnson@example.com>
2020-02-08 15:35:38 +0100

    fix: correct date formatting in base_controllers_86

--- tests/ruby/base_controllers_86.rb
+++ tests/ruby/base_controllers_86.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: base_controllers_86.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.4.947
+# Version: 7.93.167
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "2.26.516"
-  MAX_RETRIES = 8
-  TIMEOUT = 38
+module BaseControllers86
+  VERSION = "2.5.488"
+  MAX_RETRIES = 5
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "base_controllers_86")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = BaseControllers86::Application.new
   app.start
 end
