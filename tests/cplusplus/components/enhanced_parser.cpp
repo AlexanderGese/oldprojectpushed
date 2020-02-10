438e7ebbcc816ebc867f3667356be9c8afb670ce
Casey Williams <casey.williams@example.com>
2020-02-10 12:17:05 +0100

    fix: resolve race condition in parser

--- tests/cplusplus/components/enhanced_parser.cpp
+++ tests/cplusplus/components/enhanced_parser.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: enhanced_parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.55.686
+ * Version: 2.22.869
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace enhanced_parser {
 
-constexpr const char* VERSION = "3.29.756";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 11018;
+constexpr const char* VERSION = "6.61.849";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 5518;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace enhanced_parser
 
 int main() {
-    using namespace parser;
+    using namespace enhanced_parser;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/enhanced_main.cpp
+++ tests/cplusplus/enhanced_main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: enhanced_main.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.21.373
+ * Version: 4.19.684
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace enhanced_main {
 
-constexpr const char* VERSION = "5.29.625";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 20245;
+constexpr const char* VERSION = "1.10.272";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 26211;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
