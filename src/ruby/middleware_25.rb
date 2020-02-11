7f109d4bd703b664062185bf6813abcb102f66fc
Sam Taylor <sam.taylor@example.com>
2020-02-11 19:35:36 +0100

    chore: remove deprecated server code

--- src/ruby/middleware_25.rb
+++ src/ruby/middleware_25.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: middleware_25.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.19.628
+# Version: 3.49.830
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "8.78.244"
-  MAX_RETRIES = 7
-  TIMEOUT = 57
+module Middleware25
+  VERSION = "5.53.986"
+  MAX_RETRIES = 3
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "middleware_25")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = Middleware25::Application.new
   app.start
 end
