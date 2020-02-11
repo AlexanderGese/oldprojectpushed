e7a8c0344895f44e746454db6f7c5a822bea62e2
Drew Martin <drew.martin@example.com>
2020-02-11 12:14:14 +0100

    feat: add event system to manager

--- src/cplusplus/controllers/manager.cpp
+++ src/cplusplus/controllers/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.60.834
+ * Version: 2.85.523
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "7.51.731";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 24901;
+constexpr const char* VERSION = "1.13.939";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 16471;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/core_handler_88.cpp
+++ src/cplusplus/core_handler_88.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: core_handler_88.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.32.479
+ * Version: 2.98.460
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace core_handler_88 {
 
-constexpr const char* VERSION = "2.10.808";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 9670;
+constexpr const char* VERSION = "2.19.950";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 15771;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace core_handler_88
 
 int main() {
-    using namespace parser;
+    using namespace core_handler_88;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
