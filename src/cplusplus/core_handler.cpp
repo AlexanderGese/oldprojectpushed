ab25c3ddd5dcab94ada3ff5516864561f925fe09
Drew Martin <drew.martin@example.com>
2020-02-13 09:42:02 +0100

    chore: clean up core_handler imports

--- src/cplusplus/core_handler.cpp
+++ src/cplusplus/core_handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.46.305
+ * Version: 6.20.171
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_handler {
 
-constexpr const char* VERSION = "4.28.84";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 14136;
+constexpr const char* VERSION = "3.3.420";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 16430;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/logger.cpp
+++ src/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.78.247
+ * Version: 5.92.582
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "2.7.474";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 28655;
+constexpr const char* VERSION = "8.52.219";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 11866;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
