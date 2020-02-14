91bc506574c8c7a9b222e1ecd4b61fd8b7f49530
Drew Martin <drew.martin@example.com>
2020-02-14 15:41:11 +0100

    feat: add validation to controllers inputs

--- tests/ruby/controllers.rb
+++ tests/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.31.110
+# Version: 6.6.642
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "8.62.801"
-  MAX_RETRIES = 3
-  TIMEOUT = 17
+  VERSION = "8.53.18"
+  MAX_RETRIES = 6
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
