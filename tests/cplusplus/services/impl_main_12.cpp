4d01ab0b7d82986b4bbc40eb1297569a094ff4c7
Riley Johnson <riley.johnson@example.com>
2020-02-14 12:21:11 +0100

    feat: add event system to logger

--- tests/cplusplus/services/impl_main_12.cpp
+++ tests/cplusplus/services/impl_main_12.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: logger.cpp
+ * Module: impl_main_12.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.72.438
+ * Version: 2.8.322
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace logger {
+namespace impl_main_12 {
 
-constexpr const char* VERSION = "9.35.864";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 15791;
+constexpr const char* VERSION = "5.44.518";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 17129;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace logger
+} // namespace impl_main_12
 
 int main() {
-    using namespace logger;
+    using namespace impl_main_12;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/services/manager.cpp
+++ tests/cplusplus/services/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.31.713
+ * Version: 5.94.604
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "4.9.829";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 12126;
+constexpr const char* VERSION = "7.89.212";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 12848;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
