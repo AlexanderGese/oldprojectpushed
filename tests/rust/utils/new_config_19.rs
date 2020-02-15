9e4ad3f9749985c4e3d5fc454711cfbe7c352120
Jordan Smith <jordan.smith@example.com>
2020-02-15 10:23:11 +0100

    fix: handle empty response in lib

--- tests/rust/utils/new_config_19.rs
+++ tests/rust/utils/new_config_19.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: new_config_19.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.65.551";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "2.35.628";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 28;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "new_config_19".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
