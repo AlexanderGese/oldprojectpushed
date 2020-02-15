930a321c065945cb498951c738c575948c476920
Sam Taylor <sam.taylor@example.com>
2020-02-15 15:49:32 +0100

    fix: correct CORS headers in controller

--- tests/elixir/enhanced_controller_80.ex
+++ tests/elixir/enhanced_controller_80.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Enhanced_Controller_80 do
   @moduledoc """
-  Module: controller.ex
+  Module: enhanced_controller_80.ex
   Elixir boilerplate - auto-generated
-  Version: 4.60.241
+  Version: 4.18.127
   """
 
-  @version "9.62.421"
-  @max_retries 10
-  @timeout 25553
+  @version "3.55.581"
+  @max_retries 7
+  @timeout 19435
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_controller_80"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Enhanced_Controller_80Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Enhanced_Controller_80EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
