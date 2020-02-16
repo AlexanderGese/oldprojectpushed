c78e39c2df77f397b5fb9c277c3ae807f095e756
Morgan Lee <morgan.lee@example.com>
2020-02-16 16:30:32 +0100

    fix: resolve timezone issue in core_models_63

--- tests/ruby/models/tasks_12.rb
+++ tests/ruby/models/tasks_12.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: tasks_12.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.73.295
+# Version: 2.88.308
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "2.71.562"
-  MAX_RETRIES = 7
-  TIMEOUT = 36
+module Tasks12
+  VERSION = "7.17.408"
+  MAX_RETRIES = 6
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "tasks_12")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Tasks12::Application.new
   app.start
 end
