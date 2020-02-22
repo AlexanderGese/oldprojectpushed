c4732b5c052e4887e37c3cce8990b0aa38071a06
Sam Taylor <sam.taylor@example.com>
2020-02-22 21:08:27 +0100

    feat: add error handling to core_helpers_94

--- src/ruby/core_validators_53.rb
+++ src/ruby/core_validators_53.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: core_validators_53.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.40.342
+# Version: 9.85.870
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "2.17.320"
-  MAX_RETRIES = 3
-  TIMEOUT = 42
+module CoreValidators53
+  VERSION = "6.43.840"
+  MAX_RETRIES = 4
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "core_validators_53")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = CoreValidators53::Application.new
   app.start
 end
