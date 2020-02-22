5d5794b027c83a27ae0c1dc73e5a88c4f50fb49e
Sam Taylor <sam.taylor@example.com>
2020-02-22 18:58:37 +0100

    chore: clean up models_63 imports

--- lib/rust/enhanced_models_30.rs
+++ lib/rust/enhanced_models_30.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: enhanced_models_30.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.61.336";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "6.72.990";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "enhanced_models_30".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
