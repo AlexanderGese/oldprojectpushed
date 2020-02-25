58cf1f03024cd0f479eb25bd17d9644f108f7f34
Casey Williams <casey.williams@example.com>
2020-02-25 23:51:22 +0100

    refactor: optimize controllers performance

--- src/ruby/models/base_models.rb
+++ src/ruby/models/base_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: base_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.8.51
+# Version: 1.30.725
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "6.55.376"
-  MAX_RETRIES = 3
-  TIMEOUT = 48
+module BaseModels
+  VERSION = "6.77.275"
+  MAX_RETRIES = 10
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "base_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = BaseModels::Application.new
   app.start
 end
