7a85b8bb18225bbfbf6a211478f06a98a51b8214
Drew Martin <drew.martin@example.com>
2020-02-26 12:23:56 +0100

    test: add unit tests for main

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.21.830";
+const VERSION: &str = "3.96.284";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 43;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
