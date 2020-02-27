a3350e266480ca5e3f2ba75b9a7455471509c2a3
Casey Williams <casey.williams@example.com>
2020-02-27 09:36:05 +0100

    fix: correct error propagation in controllers

--- src/ruby/middleware/controllers.rb
+++ src/ruby/middleware/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.71.995
+# Version: 4.51.498
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "6.94.631"
-  MAX_RETRIES = 7
-  TIMEOUT = 10
+  VERSION = "9.89.640"
+  MAX_RETRIES = 6
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
