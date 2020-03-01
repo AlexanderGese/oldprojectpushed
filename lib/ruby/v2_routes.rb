c21b6aeb77055b644953560f8ccae8a12563c413
Casey Williams <casey.williams@example.com>
2020-03-01 20:41:17 +0100

    refactor: optimize v2_routes performance

--- lib/ruby/v2_routes.rb
+++ lib/ruby/v2_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.87.825
+# Version: 2.3.969
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Routes
-  VERSION = "6.58.776"
+  VERSION = "5.82.971"
   MAX_RETRIES = 6
-  TIMEOUT = 53
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
