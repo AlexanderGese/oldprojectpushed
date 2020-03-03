6bd069d0fd8861c399d389518e628a5050074a14
Drew Martin <drew.martin@example.com>
2020-03-03 15:35:49 +0100

    fix: handle timeout in v2_helpers_5 requests

--- lib/elixir/v2_helpers_5.ex
+++ lib/elixir/v2_helpers_5.ex
@@ -1,13 +1,13 @@
-defmodule Enhanced_Live do
+defmodule V2_Helpers_5 do
   @moduledoc """
-  Module: enhanced_live.ex
+  Module: v2_helpers_5.ex
   Elixir boilerplate - auto-generated
-  Version: 4.3.535
+  Version: 6.64.535
   """
 
-  @version "1.24.742"
-  @max_retries 10
-  @timeout 24850
+  @version "4.40.478"
+  @max_retries 4
+  @timeout 23501
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Enhanced_Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "enhanced_live"),
+      app_name: Keyword.get(opts, :app_name, "v2_helpers_5"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Enhanced_Live do
   end
 end
 
-defmodule Enhanced_LiveStore do
+defmodule V2_Helpers_5Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Enhanced_LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Enhanced_LiveEventBus do
+defmodule V2_Helpers_5EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
