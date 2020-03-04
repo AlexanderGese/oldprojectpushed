bd5ffcf2b85fbcd2d1e53131b4ab9be1bc31403a
Sam Taylor <sam.taylor@example.com>
2020-03-04 09:21:36 +0100

    feat: add logging to core_config

--- tests/rust/core_config.rs
+++ tests/rust/core_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.91.382";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "4.43.76";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
