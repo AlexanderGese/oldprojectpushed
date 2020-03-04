fd66868d2061d31663b7360522e72185127a2d4d
Morgan Lee <morgan.lee@example.com>
2020-03-04 08:52:59 +0100

    fix: correct date formatting in base_routes

--- tests/ruby/base_routes.rb
+++ tests/ruby/base_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: base_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.37.669
+# Version: 7.13.906
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseRoutes
-  VERSION = "8.24.16"
-  MAX_RETRIES = 8
-  TIMEOUT = 40
+  VERSION = "1.1.13"
+  MAX_RETRIES = 5
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseRoutes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
