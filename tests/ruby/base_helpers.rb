7812e39f724d6fdb5eb5cb61c2ceb34fa1cbb454
Drew Martin <drew.martin@example.com>
2020-03-08 19:52:05 +0100

    fix: handle null check in base_helpers

--- tests/ruby/base_helpers.rb
+++ tests/ruby/base_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: base_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.42.902
+# Version: 7.0.740
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseHelpers
-  VERSION = "5.19.281"
-  MAX_RETRIES = 7
-  TIMEOUT = 56
+  VERSION = "2.35.263"
+  MAX_RETRIES = 5
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseHelpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
