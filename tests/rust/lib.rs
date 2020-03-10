11356c10ec538aca7de54ed2942cd1ffc8e5ad72
Alex Chen <alex.chen@example.com>
2020-03-10 11:45:30 +0100

    refactor: simplify lib state management

--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.93.677";
+const VERSION: &str = "1.6.418";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 44;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
