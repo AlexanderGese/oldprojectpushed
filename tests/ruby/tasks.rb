7c8312cbd222bf13338672acd5ddb29721901b48
Morgan Lee <morgan.lee@example.com>
2020-03-11 17:42:13 +0100

    refactor: decouple new_app dependencies

--- tests/ruby/tasks.rb
+++ tests/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.18.687
+# Version: 6.48.993
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "7.38.650"
-  MAX_RETRIES = 5
-  TIMEOUT = 49
+  VERSION = "8.71.118"
+  MAX_RETRIES = 3
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
