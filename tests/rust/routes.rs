be8b830626856ad8efcf2be77d0fdc107f47067e
Riley Johnson <riley.johnson@example.com>
2020-03-11 14:12:16 +0100

    feat: implement retry logic in routes

--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.69.265";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "8.98.4";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
