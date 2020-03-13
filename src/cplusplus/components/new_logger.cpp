8da9171e50fb5ef33d204b7b02c7304174b95af9
Morgan Lee <morgan.lee@example.com>
2020-03-13 22:46:40 +0100

    fix: resolve memory leak in parser

--- src/cplusplus/components/new_logger.cpp
+++ src/cplusplus/components/new_logger.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: new_logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.17.113
+ * Version: 7.56.872
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace new_logger {
 
-constexpr const char* VERSION = "9.65.456";
+constexpr const char* VERSION = "9.44.499";
 constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 12942;
+constexpr int TIMEOUT_MS = 15398;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace new_logger
 
 int main() {
-    using namespace parser;
+    using namespace new_logger;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/middleware/logger.cpp
+++ src/cplusplus/middleware/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.79.195
+ * Version: 1.91.350
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "5.98.403";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 16032;
+constexpr const char* VERSION = "1.52.539";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 14444;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
