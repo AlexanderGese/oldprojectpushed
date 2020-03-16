f434c6fca88b434e9878217b2739f78f402d5093
Sam Taylor <sam.taylor@example.com>
2020-03-16 19:19:13 +0100

    chore: remove deprecated v2_repo_9 code

--- lib/elixir/handlers/new_router.ex
+++ lib/elixir/handlers/new_router.ex
@@ -1,13 +1,13 @@
-defmodule Enhanced_Context do
+defmodule New_Router do
   @moduledoc """
-  Module: enhanced_context.ex
+  Module: new_router.ex
   Elixir boilerplate - auto-generated
-  Version: 4.24.678
+  Version: 8.28.44
   """
 
-  @version "7.34.914"
-  @max_retries 3
-  @timeout 17024
+  @version "9.22.232"
+  @max_retries 5
+  @timeout 28083
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Enhanced_Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "enhanced_context"),
+      app_name: Keyword.get(opts, :app_name, "new_router"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Enhanced_Context do
   end
 end
 
-defmodule Enhanced_ContextStore do
+defmodule New_RouterStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Enhanced_ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Enhanced_ContextEventBus do
+defmodule New_RouterEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
