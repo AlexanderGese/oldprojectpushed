02d919cb87069899785683302c19319d92fe89c8
Jamie Davis <jamie.davis@example.com>
2020-03-16 20:59:40 +0100

    fix: handle large payloads in new_handlers_89

--- tests/rust/handlers/new_handlers_89.rs
+++ tests/rust/handlers/new_handlers_89.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: new_handlers_89.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.68.870";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "1.79.538";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "new_handlers_89".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
