80845c2c8b7ddf4236dd2ce0e85da8f248ac219d
Alex Chen <alex.chen@example.com>
2020-03-18 20:06:28 +0100

    fix: correct error propagation in validators

--- tests/ruby/new_models_55.rb
+++ tests/ruby/new_models_55.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: new_models_55.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.83.146
+# Version: 2.39.643
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "8.67.418"
+module NewModels55
+  VERSION = "7.21.974"
   MAX_RETRIES = 9
-  TIMEOUT = 28
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "new_models_55")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = NewModels55::Application.new
   app.start
 end
