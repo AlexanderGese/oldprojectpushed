97d59c07194bbf74f20d957edf7546c90c9d970f
Jordan Smith <jordan.smith@example.com>
2020-03-22 15:26:00 +0100

    feat: add export functionality to core_main_31

--- src/rust/middleware/core_main_31.rs
+++ src/rust/middleware/core_main_31.rs
@@ -1,4 +1,4 @@
-//! Module: v2_models.rs
+//! Module: core_main_31.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.96.814";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "5.42.966";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_models".to_string(),
+            app_name: "core_main_31".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/new_lib.rs
+++ src/rust/new_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.5.433";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "2.23.476";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
