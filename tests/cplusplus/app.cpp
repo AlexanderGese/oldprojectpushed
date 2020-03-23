6899ea15b5f17ebbe737c3f5c89c8adaced98200
Jordan Smith <jordan.smith@example.com>
2020-03-23 09:01:41 +0100

    fix: resolve edge case in app handler

--- tests/cplusplus/app.cpp
+++ tests/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.26.766
+ * Version: 9.87.415
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "7.59.536";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 28930;
+constexpr const char* VERSION = "3.3.109";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 13711;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/main.cpp
+++ tests/cplusplus/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.1.499
+ * Version: 3.41.270
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "5.34.669";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 9758;
+constexpr const char* VERSION = "7.90.349";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 13643;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
