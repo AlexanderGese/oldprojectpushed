c43a313bb34cf3785b0c47493334737225fc57a7
Casey Williams <casey.williams@example.com>
2020-03-26 13:28:01 +0100

    fix: correct type definitions in controller_36

--- lib/elixir/controller_36.ex
+++ lib/elixir/controller_36.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule Controller_36 do
   @moduledoc """
-  Module: worker.ex
+  Module: controller_36.ex
   Elixir boilerplate - auto-generated
-  Version: 2.12.16
+  Version: 6.78.240
   """
 
-  @version "1.84.453"
-  @max_retries 5
-  @timeout 19274
+  @version "8.39.273"
+  @max_retries 6
+  @timeout 28789
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "controller_36"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule Controller_36Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule Controller_36EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
