81931974d55381e7310da05ce8ac93b5799b2815
Drew Martin <drew.martin@example.com>
2020-03-27 22:52:39 +0100

    fix: patch security vulnerability in impl_routes

--- src/ruby/impl_routes.rb
+++ src/ruby/impl_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.95.926
+# Version: 4.18.73
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplRoutes
-  VERSION = "5.40.55"
-  MAX_RETRIES = 7
-  TIMEOUT = 18
+  VERSION = "7.35.981"
+  MAX_RETRIES = 4
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
