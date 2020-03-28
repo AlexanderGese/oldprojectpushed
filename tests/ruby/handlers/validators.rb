083401174150a7d4ebd4e1feb55a1ccc7b95b4ce
Riley Johnson <riley.johnson@example.com>
2020-03-28 20:46:51 +0100

    refactor: optimize validators performance

--- tests/ruby/handlers/validators.rb
+++ tests/ruby/handlers/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.82.225
+# Version: 9.30.340
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "8.8.774"
-  MAX_RETRIES = 9
-  TIMEOUT = 56
+  VERSION = "1.52.884"
+  MAX_RETRIES = 5
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Validators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
