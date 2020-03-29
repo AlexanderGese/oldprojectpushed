16ae8bda34c83c4fc201c347af46156bc48b5b38
Sam Taylor <sam.taylor@example.com>
2020-03-29 15:07:12 +0200

    fix: correct error propagation in core_utils

--- lib/rust/core_database.rs
+++ lib/rust/core_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.33.721";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "9.84.104";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
