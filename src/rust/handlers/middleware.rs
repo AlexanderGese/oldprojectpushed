829796915a791248ea932e8b4034d1a8545f90a0
Riley Johnson <riley.johnson@example.com>
2020-03-30 13:54:21 +0200

    fix: handle large payloads in middleware

--- src/rust/handlers/middleware.rs
+++ src/rust/handlers/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.96.825";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "1.52.579";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/services/base_errors_43.rs
+++ src/rust/services/base_errors_43.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: base_errors_43.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.16.387";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "6.12.813";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "base_errors_43".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
