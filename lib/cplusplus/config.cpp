2fdfab58953fe6707c33684265fd7e77f19e0528
Morgan Lee <morgan.lee@example.com>
2020-04-04 20:18:46 +0200

    feat: add event system to logger

--- lib/cplusplus/config.cpp
+++ lib/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.79.535
+ * Version: 5.66.413
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "3.81.76";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 28165;
+constexpr const char* VERSION = "3.11.270";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 12831;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/logger.cpp
+++ lib/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.46.100
+ * Version: 5.97.548
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "2.16.670";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 10586;
+constexpr const char* VERSION = "5.38.802";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 22237;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
