6d98aea53b704349593eaf5632e20913951eaef9
Alex Chen <alex.chen@example.com>
2020-04-07 15:07:59 +0200

    refactor: optimize main performance

--- lib/rust/database.rs
+++ lib/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.53.3";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "3.94.56";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils/routes.rs
+++ lib/rust/utils/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.46.752";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "3.18.993";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
