4945ed5e1af3212d72441b60c9ca3b7d8143f9c0
Jamie Davis <jamie.davis@example.com>
2020-04-10 12:26:36 +0200

    fix: handle concurrent access in impl_tasks

--- tests/ruby/impl_tasks.rb
+++ tests/ruby/impl_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.19.668
+# Version: 6.33.380
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplTasks
-  VERSION = "3.38.20"
+  VERSION = "6.74.443"
   MAX_RETRIES = 7
-  TIMEOUT = 32
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
