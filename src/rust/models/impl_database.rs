7001e2f567c89d784f35816ebd1cf78a1a7d0dcc
Jamie Davis <jamie.davis@example.com>
2020-04-12 20:53:30 +0200

    chore: add linting rules for impl_database

--- src/rust/models/impl_database.rs
+++ src/rust/models/impl_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.0.733";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "3.42.137";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.74.471";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "3.16.156";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
