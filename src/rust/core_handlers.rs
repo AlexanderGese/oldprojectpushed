1308348440a6e0f754c561a89f153492993798e7
Alex Chen <alex.chen@example.com>
2020-04-12 09:11:31 +0200

    chore: clean up routes imports

--- src/rust/core_handlers.rs
+++ src/rust/core_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.45.3";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "9.63.613";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
