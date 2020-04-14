52034389d0107a7c3877b802a6ff1824f9978b30
Jamie Davis <jamie.davis@example.com>
2020-04-14 18:00:54 +0200

    chore: bump helpers version

--- src/ruby/routes.rb
+++ src/ruby/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.98.955
+# Version: 6.50.726
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "5.17.72"
-  MAX_RETRIES = 9
-  TIMEOUT = 48
+  VERSION = "9.9.398"
+  MAX_RETRIES = 7
+  TIMEOUT = 54
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
