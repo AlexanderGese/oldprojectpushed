d5dbf3d11acb45535b2058f6a358b82c8e2ee11a
Drew Martin <drew.martin@example.com>
2020-04-14 19:16:55 +0200

    feat: add middleware support to base_models

--- src/rust/base_models.rs
+++ src/rust/base_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.5.208";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "2.8.487";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.75.197";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "8.36.506";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
