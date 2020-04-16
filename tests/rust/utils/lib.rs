62a626bd405ddfc59c5b24533e0041a96bb23d46
Drew Martin <drew.martin@example.com>
2020-04-16 14:04:51 +0200

    feat: add event system to base_lib_25

--- tests/rust/utils/lib.rs
+++ tests/rust/utils/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.10.329";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "8.10.782";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
