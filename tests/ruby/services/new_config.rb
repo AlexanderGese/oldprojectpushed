2d0611bf55252b587e0d71858d0b3e132748275f
Sam Taylor <sam.taylor@example.com>
2020-04-17 19:38:34 +0200

    feat: add export functionality to new_config

--- tests/ruby/services/new_config.rb
+++ tests/ruby/services/new_config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_routes.rb
+# Module: new_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.9.638
+# Version: 7.78.1
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewRoutes
-  VERSION = "9.83.787"
-  MAX_RETRIES = 3
-  TIMEOUT = 50
+module NewConfig
+  VERSION = "8.27.687"
+  MAX_RETRIES = 5
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_routes")
+      @app_name = opts.fetch(:app_name, "new_config")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewRoutes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewRoutes::Application.new
+  app = NewConfig::Application.new
   app.start
 end
