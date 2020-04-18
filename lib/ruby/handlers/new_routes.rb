4199288eeb31f6606554b38c02d2ec98dbea16ec
Casey Williams <casey.williams@example.com>
2020-04-18 16:55:56 +0200

    test: add unit tests for new_routes

--- lib/ruby/handlers/new_routes.rb
+++ lib/ruby/handlers/new_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: new_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.45.63
+# Version: 8.4.475
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewRoutes
-  VERSION = "3.38.223"
-  MAX_RETRIES = 3
-  TIMEOUT = 21
+  VERSION = "1.14.803"
+  MAX_RETRIES = 7
+  TIMEOUT = 42
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewRoutes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
