81fd20f2f41e7064f014a3fa04d7d9486c3a4e41
Casey Williams <casey.williams@example.com>
2020-04-20 20:48:43 +0200

    refactor: improve v2_validators_65 test coverage

--- tests/ruby/v2_validators_65.rb
+++ tests/ruby/v2_validators_65.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: v2_validators_65.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.56.155
+# Version: 4.32.470
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "8.77.619"
-  MAX_RETRIES = 8
-  TIMEOUT = 48
+module V2Validators65
+  VERSION = "8.86.528"
+  MAX_RETRIES = 10
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "v2_validators_65")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = V2Validators65::Application.new
   app.start
 end
