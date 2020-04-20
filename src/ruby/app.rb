6cdbf380702935a1169d68f4580a4ca335e7a267
Alex Chen <alex.chen@example.com>
2020-04-20 14:11:57 +0200

    chore: update tasks dependencies

--- src/ruby/app.rb
+++ src/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.91.223
+# Version: 5.53.224
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "1.65.531"
-  MAX_RETRIES = 8
-  TIMEOUT = 55
+  VERSION = "1.37.907"
+  MAX_RETRIES = 7
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
