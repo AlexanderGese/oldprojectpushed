c90e8eac3fcdea512634ee7f3817a8e8be1cc041
Casey Williams <casey.williams@example.com>
2020-04-24 12:00:30 +0200

    fix: resolve memory leak in errors

--- lib/rust/impl_handlers_93.rs
+++ lib/rust/impl_handlers_93.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: impl_handlers_93.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.70.814";
+const VERSION: &str = "7.44.889";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 33;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "impl_handlers_93".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
