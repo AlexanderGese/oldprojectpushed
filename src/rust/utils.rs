b1f14b59c202dd6ffcab6916807201e5b460291b
Casey Williams <casey.williams@example.com>
2020-04-25 14:56:58 +0200

    fix: resolve encoding issue in database

--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.16.156";
+const VERSION: &str = "3.15.211";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 41;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
