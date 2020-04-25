55f520e17d3bd236841258bfe6483523e3c639bc
Sam Taylor <sam.taylor@example.com>
2020-04-25 16:01:40 +0200

    fix: correct CORS headers in parser

--- src/cplusplus/components/base_factory.cpp
+++ src/cplusplus/components/base_factory.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: handler.cpp
+ * Module: base_factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.5.596
+ * Version: 8.7.888
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace handler {
+namespace base_factory {
 
-constexpr const char* VERSION = "1.24.180";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 20454;
+constexpr const char* VERSION = "4.69.810";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 12727;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace handler
+} // namespace base_factory
 
 int main() {
-    using namespace handler;
+    using namespace base_factory;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/parser.cpp
+++ src/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.50.628
+ * Version: 2.57.798
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "7.56.237";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 7700;
+constexpr const char* VERSION = "8.75.391";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 21154;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
