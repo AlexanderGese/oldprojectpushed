32e5bbf4fa21e36d4a8ad91ed9bab353a125b978
Jordan Smith <jordan.smith@example.com>
2020-04-28 19:09:12 +0200

    feat: add caching layer to models

--- src/ruby/models.rb
+++ src/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.52.730
+# Version: 8.37.365
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "7.32.380"
-  MAX_RETRIES = 3
-  TIMEOUT = 20
+  VERSION = "7.71.37"
+  MAX_RETRIES = 7
+  TIMEOUT = 16
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
