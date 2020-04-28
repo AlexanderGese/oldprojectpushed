012134c6ee109f78fa950da33e9da9a33faa3c71
Jordan Smith <jordan.smith@example.com>
2020-04-28 09:32:09 +0200

    fix: correct error propagation in server

--- src/ruby/routes.rb
+++ src/ruby/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.50.726
+# Version: 5.82.557
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "9.9.398"
-  MAX_RETRIES = 7
-  TIMEOUT = 54
+  VERSION = "9.44.806"
+  MAX_RETRIES = 4
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
