89a7385db569a12d5079925bb0932d1fe9dc266d
Drew Martin <drew.martin@example.com>
2020-04-28 14:23:36 +0200

    fix: handle timeout in routes requests

--- tests/ruby/utils/base_server_50.rb
+++ tests/ruby/utils/base_server_50.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: base_server_50.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.83.344
+# Version: 1.9.674
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "9.36.831"
-  MAX_RETRIES = 6
-  TIMEOUT = 29
+module BaseServer50
+  VERSION = "9.11.908"
+  MAX_RETRIES = 3
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "base_server_50")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = BaseServer50::Application.new
   app.start
 end
