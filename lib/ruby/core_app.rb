3c4601fddc76dc3ff33f0b57170239f37effaf62
Jamie Davis <jamie.davis@example.com>
2020-04-29 22:14:22 +0200

    fix: resolve encoding issue in impl_config_18

--- lib/ruby/core_app.rb
+++ lib/ruby/core_app.rb
@@ -2,7 +2,7 @@
 
 # Module: core_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.38.980
+# Version: 6.22.340
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreApp
-  VERSION = "6.55.812"
-  MAX_RETRIES = 4
-  TIMEOUT = 57
+  VERSION = "1.57.528"
+  MAX_RETRIES = 9
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreApp
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
