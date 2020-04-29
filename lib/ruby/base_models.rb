1f5bfbb1b44a12f2341807787ff13382ddfcc7fe
Jamie Davis <jamie.davis@example.com>
2020-04-29 13:14:42 +0200

    chore: update impl_models_37 configuration

--- lib/ruby/base_models.rb
+++ lib/ruby/base_models.rb
@@ -2,7 +2,7 @@
 
 # Module: base_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.93.265
+# Version: 7.57.159
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseModels
-  VERSION = "9.18.229"
-  MAX_RETRIES = 7
-  TIMEOUT = 31
+  VERSION = "1.22.744"
+  MAX_RETRIES = 6
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
