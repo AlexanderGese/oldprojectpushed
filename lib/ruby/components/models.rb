ae9f6ec90317116c716397af718fe7991e9a5cc8
Drew Martin <drew.martin@example.com>
2020-04-29 08:32:40 +0200

    fix: correct date formatting in routes

--- lib/ruby/components/models.rb
+++ lib/ruby/components/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.85.963
+# Version: 9.92.443
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "6.39.460"
-  MAX_RETRIES = 5
-  TIMEOUT = 21
+  VERSION = "9.51.221"
+  MAX_RETRIES = 3
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
