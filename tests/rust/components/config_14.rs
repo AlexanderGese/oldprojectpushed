c91ae7a97e48a362c649e0161e0938d4ab9d32a9
Jordan Smith <jordan.smith@example.com>
2020-05-02 23:40:34 +0200

    chore: clean up models imports

--- tests/rust/components/config_14.rs
+++ tests/rust/components/config_14.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: config_14.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.13.442";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "3.8.329";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "config_14".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/helpers/config.rs
+++ tests/rust/helpers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.95.457";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "6.46.612";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
