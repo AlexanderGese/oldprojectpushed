a437a5b7ae5ebc2475620fe3386c32a29cf73c74
Casey Williams <casey.williams@example.com>
2020-05-02 20:26:13 +0200

    feat: add search functionality to controller

--- tests/elixir/v2_schema_30.ex
+++ tests/elixir/v2_schema_30.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule V2_Schema_30 do
   @moduledoc """
-  Module: controller.ex
+  Module: v2_schema_30.ex
   Elixir boilerplate - auto-generated
-  Version: 3.81.729
+  Version: 9.72.534
   """
 
-  @version "3.70.820"
-  @max_retries 5
-  @timeout 24282
+  @version "8.70.606"
+  @max_retries 3
+  @timeout 8132
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "v2_schema_30"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule V2_Schema_30Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule V2_Schema_30EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
