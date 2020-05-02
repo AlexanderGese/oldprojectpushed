55d4df1ff7c49746ce5cb2957e558a9f7020e69f
Casey Williams <casey.williams@example.com>
2020-05-02 17:30:03 +0200

    fix: resolve edge case in core_handlers handler

--- src/rust/handlers/v2_handlers_13.rs
+++ src/rust/handlers/v2_handlers_13.rs
@@ -1,4 +1,4 @@
-//! Module: core_handlers.rs
+//! Module: v2_handlers_13.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.40.935";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "7.20.762";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_handlers".to_string(),
+            app_name: "v2_handlers_13".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/new_main_69.rs
+++ src/rust/new_main_69.rs
@@ -1,4 +1,4 @@
-//! Module: core_handlers.rs
+//! Module: new_main_69.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.40.935";
+const VERSION: &str = "7.70.797";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 20;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_handlers".to_string(),
+            app_name: "new_main_69".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
