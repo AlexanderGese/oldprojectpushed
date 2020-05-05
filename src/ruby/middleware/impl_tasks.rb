c17ea2ab6007a6093905c7bd345dbb5267a4b435
Drew Martin <drew.martin@example.com>
2020-05-05 20:50:02 +0200

    chore: update impl_tasks configuration

--- src/ruby/middleware/impl_tasks.rb
+++ src/ruby/middleware/impl_tasks.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_controllers.rb
+# Module: impl_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.31.430
+# Version: 3.20.743
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Controllers
-  VERSION = "7.10.176"
-  MAX_RETRIES = 5
-  TIMEOUT = 6
+module ImplTasks
+  VERSION = "3.42.833"
+  MAX_RETRIES = 6
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_controllers")
+      @app_name = opts.fetch(:app_name, "impl_tasks")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module V2Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Controllers::Application.new
+  app = ImplTasks::Application.new
   app.start
 end
