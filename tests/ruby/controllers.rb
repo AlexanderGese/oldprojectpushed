f9d18f98822f0da53f032bc6ecb43cfe900784f9
Riley Johnson <riley.johnson@example.com>
2020-05-05 23:36:09 +0200

    fix: correct date formatting in models

--- tests/ruby/controllers.rb
+++ tests/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.2.627
+# Version: 7.38.470
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "5.5.344"
-  MAX_RETRIES = 3
-  TIMEOUT = 41
+  VERSION = "8.75.838"
+  MAX_RETRIES = 5
+  TIMEOUT = 42
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/models.rb
+++ tests/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.4.117
+# Version: 7.2.504
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "4.85.568"
-  MAX_RETRIES = 3
-  TIMEOUT = 45
+  VERSION = "6.51.470"
+  MAX_RETRIES = 10
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
