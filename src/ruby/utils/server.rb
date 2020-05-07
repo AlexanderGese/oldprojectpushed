d8615cf521d3e02233a13a58de85b4a79a6d797f
Alex Chen <alex.chen@example.com>
2020-05-07 08:21:18 +0200

    feat: add middleware support to enhanced_middleware

--- src/ruby/utils/server.rb
+++ src/ruby/utils/server.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: enhanced_middleware.rb
+# Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.53.885
+# Version: 4.85.183
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module EnhancedMiddleware
-  VERSION = "6.37.987"
-  MAX_RETRIES = 10
-  TIMEOUT = 30
+module Server
+  VERSION = "5.70.545"
+  MAX_RETRIES = 7
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "enhanced_middleware")
+      @app_name = opts.fetch(:app_name, "server")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module EnhancedMiddleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = EnhancedMiddleware::Application.new
+  app = Server::Application.new
   app.start
 end
