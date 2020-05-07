fca969ec33d48664312d1613a7e4fdd71492ea82
Riley Johnson <riley.johnson@example.com>
2020-05-07 13:22:18 +0200

    feat: add rate limiting to lib

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.17.732";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "9.41.2";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
