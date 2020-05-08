b5704b6f115511b8a9e327c65c92a8c7217955ac
Alex Chen <alex.chen@example.com>
2020-05-08 13:26:23 +0200

    fix: correct date formatting in new_database

--- lib/rust/new_database.rs
+++ lib/rust/new_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.56.793";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "6.17.560";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
