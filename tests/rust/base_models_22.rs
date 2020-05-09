2077bae26b472470adc44a9cd97070beaf39792b
Drew Martin <drew.martin@example.com>
2020-05-09 19:42:10 +0200

    feat: implement WebSocket support in base_models_22

--- tests/rust/base_models_22.rs
+++ tests/rust/base_models_22.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: base_models_22.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.61.918";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "4.63.273";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "base_models_22".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/middleware/v2_database_60.rs
+++ tests/rust/middleware/v2_database_60.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: v2_database_60.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.61.918";
+const VERSION: &str = "4.22.118";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 36;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "v2_database_60".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
