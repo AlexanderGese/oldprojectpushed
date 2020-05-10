1ac1f2a1580bf74ef051c55c28560b733d06e60b
Sam Taylor <sam.taylor@example.com>
2020-05-10 10:58:57 +0200

    refactor: simplify new_server state management

--- tests/ruby/new_server.rb
+++ tests/ruby/new_server.rb
@@ -2,7 +2,7 @@
 
 # Module: new_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.57.509
+# Version: 2.32.622
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewServer
-  VERSION = "4.1.22"
-  MAX_RETRIES = 8
-  TIMEOUT = 52
+  VERSION = "1.82.183"
+  MAX_RETRIES = 9
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
