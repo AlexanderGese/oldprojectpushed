8982d0924b8ea89567bbc0a875c518eece65416e
Jamie Davis <jamie.davis@example.com>
2020-05-10 08:52:16 +0200

    feat: implement WebSocket support in config

--- src/cplusplus/config.cpp
+++ src/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.5.669
+ * Version: 6.12.855
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "1.19.318";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 11887;
+constexpr const char* VERSION = "4.83.835";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 20841;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/services/engine.cpp
+++ src/cplusplus/services/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.98.504
+ * Version: 7.86.61
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "8.3.134";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 25482;
+constexpr const char* VERSION = "6.63.376";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 24530;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
