095c545c0844a5cdb1d73aa489af92a9be77f87e
Alex Chen <alex.chen@example.com>
2020-05-12 15:44:11 +0200

    feat: add middleware support to core_config

--- tests/cplusplus/core_config.cpp
+++ tests/cplusplus/core_config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_config.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.23.204
+ * Version: 2.35.934
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_config {
 
-constexpr const char* VERSION = "5.24.385";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 17087;
+constexpr const char* VERSION = "9.75.419";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 11713;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
