841ef840073ac26e1e78afcd028137b814aef09d
Sam Taylor <sam.taylor@example.com>
2020-05-17 13:59:42 +0200

    refactor: extract errors utility functions

--- src/rust/models/errors.rs
+++ src/rust/models/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.71.122";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "3.57.73";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/models/handlers.rs
+++ src/rust/models/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.36.504";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "1.3.112";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
