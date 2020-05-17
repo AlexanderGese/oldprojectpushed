37907d1442cbf72fca2f22560175486ac28cd7dc
Drew Martin <drew.martin@example.com>
2020-05-17 19:41:29 +0200

    feat: add handlers module with core functionality

--- src/rust/components/handlers.rs
+++ src/rust/components/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.2.173";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "8.59.822";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
