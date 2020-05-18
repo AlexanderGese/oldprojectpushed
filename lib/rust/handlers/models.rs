33d851bc82279c037bda8fb78199148fd33f6a9e
Jamie Davis <jamie.davis@example.com>
2020-05-18 23:42:48 +0200

    chore: update models configuration

--- lib/rust/handlers/models.rs
+++ lib/rust/handlers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.61.106";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "6.47.685";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
