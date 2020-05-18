23731e0f2f2995ccdd755e5c26e766e1125d90cb
Sam Taylor <sam.taylor@example.com>
2020-05-18 22:46:09 +0200

    chore: update lib dependencies

--- tests/rust/helpers/lib.rs
+++ tests/rust/helpers/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.38.468";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "3.56.621";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
