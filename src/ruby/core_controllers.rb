fdc3a91483e1aec8d800727e4f9a22af484af05f
Alex Chen <alex.chen@example.com>
2020-05-22 18:53:15 +0200

    feat: add rate limiting to helpers

--- src/ruby/core_controllers.rb
+++ src/ruby/core_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: core_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.88.17
+# Version: 3.93.408
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreControllers
-  VERSION = "4.20.905"
-  MAX_RETRIES = 5
-  TIMEOUT = 20
+  VERSION = "5.7.906"
+  MAX_RETRIES = 6
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreControllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
