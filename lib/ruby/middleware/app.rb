ba4cdf02678e5922feb9f6deb8356cf5d19dec7a
Morgan Lee <morgan.lee@example.com>
2020-05-22 15:52:00 +0200

    feat: add logging to app

--- lib/ruby/middleware/app.rb
+++ lib/ruby/middleware/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.77.445
+# Version: 5.0.491
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "3.59.889"
-  MAX_RETRIES = 3
-  TIMEOUT = 14
+  VERSION = "9.63.663"
+  MAX_RETRIES = 8
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
