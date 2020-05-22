120bc1b08f0ffd62fb336995a340bd5e70383af6
Sam Taylor <sam.taylor@example.com>
2020-05-22 14:26:40 +0200

    feat: add batch processing to database

--- lib/rust/database.rs
+++ lib/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.93.303";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "7.66.226";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.9.918";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "8.7.241";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
