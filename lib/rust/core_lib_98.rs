7400b29a78087ea5368e67a71e629b43c06db7aa
Jordan Smith <jordan.smith@example.com>
2020-05-28 10:04:38 +0200

    feat: implement SSE streaming in core_lib_98

--- lib/rust/core_lib_98.rs
+++ lib/rust/core_lib_98.rs
@@ -1,4 +1,4 @@
-//! Module: core_utils.rs
+//! Module: core_lib_98.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.61.747";
+const VERSION: &str = "7.43.419";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 36;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_utils".to_string(),
+            app_name: "core_lib_98".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
