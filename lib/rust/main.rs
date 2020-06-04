70289cf7f2b4c62b701f561c18ced72bb19556db
Sam Taylor <sam.taylor@example.com>
2020-06-04 22:15:30 +0200

    fix: resolve edge case in main handler

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.53.161";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "4.13.386";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware/errors.rs
+++ lib/rust/middleware/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.46.603";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "5.61.397";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
