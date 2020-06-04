557a26122e9784f4ab0314a4dff3b3b565663131
Drew Martin <drew.martin@example.com>
2020-06-04 17:03:36 +0200

    refactor: extract v2_tasks utility functions

--- src/ruby/utils/v2_tasks_31.rb
+++ src/ruby/utils/v2_tasks_31.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_tasks.rb
+# Module: v2_tasks_31.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.26.103
+# Version: 4.81.739
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Tasks
-  VERSION = "1.52.0"
-  MAX_RETRIES = 6
-  TIMEOUT = 23
+module V2Tasks31
+  VERSION = "6.53.201"
+  MAX_RETRIES = 10
+  TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_tasks")
+      @app_name = opts.fetch(:app_name, "v2_tasks_31")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module V2Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Tasks::Application.new
+  app = V2Tasks31::Application.new
   app.start
 end
