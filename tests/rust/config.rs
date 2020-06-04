69b6ae3382fe82608a5ba8585a8ec8ce7fb8bd81
Morgan Lee <morgan.lee@example.com>
2020-06-04 18:38:09 +0200

    feat: add export functionality to lib

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.61.918";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "5.51.570";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.36.574";
+const VERSION: &str = "6.80.869";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 34;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
