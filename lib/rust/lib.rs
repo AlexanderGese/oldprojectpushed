dbdf1dabf68099ef16512edbb0914cfa5074745d
Sam Taylor <sam.taylor@example.com>
2020-06-05 13:52:11 +0200

    feat: add middleware support to lib

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.7.241";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "8.64.892";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
