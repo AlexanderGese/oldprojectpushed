9cf4b997fadadc0ee3bad367b04d580566b4deea
Alex Chen <alex.chen@example.com>
2020-06-05 15:25:31 +0200

    chore: remove deprecated core_engine_8 code

--- src/cplusplus/controllers/impl_logger.cpp
+++ src/cplusplus/controllers/impl_logger.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: enhanced_logger.cpp
+ * Module: impl_logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.30.215
+ * Version: 2.71.398
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace enhanced_logger {
+namespace impl_logger {
 
-constexpr const char* VERSION = "1.84.430";
+constexpr const char* VERSION = "1.7.834";
 constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 13851;
+constexpr int TIMEOUT_MS = 6189;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace enhanced_logger
+} // namespace impl_logger
 
 int main() {
-    using namespace enhanced_logger;
+    using namespace impl_logger;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/core_engine_8.cpp
+++ src/cplusplus/core_engine_8.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: enhanced_logger.cpp
+ * Module: core_engine_8.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.30.215
+ * Version: 7.10.803
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace enhanced_logger {
+namespace core_engine_8 {
 
-constexpr const char* VERSION = "1.84.430";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 13851;
+constexpr const char* VERSION = "3.87.509";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 12742;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace enhanced_logger
+} // namespace core_engine_8
 
 int main() {
-    using namespace enhanced_logger;
+    using namespace core_engine_8;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
