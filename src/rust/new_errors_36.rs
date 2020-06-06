f8c6db7047256b3cb70f7e62ef08e0ace3f57349
Sam Taylor <sam.taylor@example.com>
2020-06-06 14:00:47 +0200

    fix: correct type definitions in errors

--- src/rust/new_errors_36.rs
+++ src/rust/new_errors_36.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: new_errors_36.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.32.939";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "1.8.529";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "new_errors_36".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
