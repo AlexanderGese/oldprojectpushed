b4667387a9ea2c58eab4698adeff61c65f41c6a7
Drew Martin <drew.martin@example.com>
2020-06-08 18:26:36 +0200

    refactor: migrate errors to async/await

--- src/rust/utils/v2_errors.rs
+++ src/rust/utils/v2_errors.rs
@@ -1,4 +1,4 @@
-//! Module: v2_lib.rs
+//! Module: v2_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.97.673";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "7.99.667";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_lib".to_string(),
+            app_name: "v2_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
