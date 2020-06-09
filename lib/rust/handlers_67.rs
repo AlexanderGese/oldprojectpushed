55c8b1fe86fa61c6825e3e30d343f7b80b083f84
Jamie Davis <jamie.davis@example.com>
2020-06-09 18:44:41 +0200

    fix: handle empty response in handlers_67

--- lib/rust/handlers_67.rs
+++ lib/rust/handlers_67.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: handlers_67.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.59.967";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "9.6.245";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "handlers_67".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/impl_utils.rs
+++ lib/rust/impl_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.13.887";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "3.17.772";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
