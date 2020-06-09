76b98e43767416c494cf4a56c6c47cb0c854092b
Casey Williams <casey.williams@example.com>
2020-06-09 08:29:12 +0200

    docs: add documentation for tasks

--- lib/ruby/helpers.rb
+++ lib/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.37.257
+# Version: 5.11.141
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "8.68.301"
-  MAX_RETRIES = 3
-  TIMEOUT = 42
+  VERSION = "8.61.306"
+  MAX_RETRIES = 8
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
