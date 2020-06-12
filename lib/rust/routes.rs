e1eee953296ad55f7c274d9951d442808fe33b4c
Jordan Smith <jordan.smith@example.com>
2020-06-12 08:24:55 +0200

    feat: add export functionality to database

--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.68.846";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "7.39.386";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
