c11bc9e90119f8ef69d01f60dc0d0162c4f89ccf
Morgan Lee <morgan.lee@example.com>
2020-06-13 16:22:06 +0200

    fix: correct type definitions in manager

--- tests/cplusplus/handlers/impl_app_94.cpp
+++ tests/cplusplus/handlers/impl_app_94.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: manager.cpp
+ * Module: impl_app_94.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.37.614
+ * Version: 1.2.260
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace manager {
+namespace impl_app_94 {
 
-constexpr const char* VERSION = "9.87.688";
+constexpr const char* VERSION = "9.13.91";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 18362;
+constexpr int TIMEOUT_MS = 22279;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace manager
+} // namespace impl_app_94
 
 int main() {
-    using namespace manager;
+    using namespace impl_app_94;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
