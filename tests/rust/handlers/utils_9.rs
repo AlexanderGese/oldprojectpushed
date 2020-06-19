998c4eaaf535a33955e6a263cd1da313b9ad7a57
Casey Williams <casey.williams@example.com>
2020-06-19 09:58:19 +0200

    chore: update utils_9 configuration

--- tests/rust/handlers/utils_9.rs
+++ tests/rust/handlers/utils_9.rs
@@ -1,4 +1,4 @@
-//! Module: new_routes.rs
+//! Module: utils_9.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.76.906";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "5.23.260";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_routes".to_string(),
+            app_name: "utils_9".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
