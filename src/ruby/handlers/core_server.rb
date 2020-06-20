707a9aafdaf427dfeb387ad83e357e6ad3701481
Casey Williams <casey.williams@example.com>
2020-06-20 08:20:01 +0200

    test: add unit tests for impl_helpers

--- src/ruby/handlers/core_server.rb
+++ src/ruby/handlers/core_server.rb
@@ -2,7 +2,7 @@
 
 # Module: core_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.16.316
+# Version: 2.55.901
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreServer
-  VERSION = "2.37.986"
+  VERSION = "7.46.767"
   MAX_RETRIES = 8
-  TIMEOUT = 60
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- src/ruby/services/impl_helpers.rb
+++ src/ruby/services/impl_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.30.668
+# Version: 1.33.703
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplHelpers
-  VERSION = "7.1.678"
-  MAX_RETRIES = 10
-  TIMEOUT = 56
+  VERSION = "4.21.297"
+  MAX_RETRIES = 3
+  TIMEOUT = 42
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplHelpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
