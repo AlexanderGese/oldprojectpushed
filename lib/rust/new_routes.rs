a6b8568f4f9bc2b313b51f28b30a582e690e754a
Sam Taylor <sam.taylor@example.com>
2020-06-20 18:13:08 +0200

    fix: handle empty response in v2_config_53

--- lib/rust/new_routes.rs
+++ lib/rust/new_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.23.207";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "4.95.757";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/v2_config_53.rs
+++ lib/rust/v2_config_53.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: v2_config_53.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.1.7";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "5.34.101";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "v2_config_53".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
