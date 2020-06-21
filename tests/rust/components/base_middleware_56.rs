c6366a2d89d5eb92c6536c4e8558b7f1b060e25d
Drew Martin <drew.martin@example.com>
2020-06-21 13:04:41 +0200

    fix: resolve timezone issue in base_middleware_56

--- tests/rust/components/base_middleware_56.rs
+++ tests/rust/components/base_middleware_56.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: base_middleware_56.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.35.348";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "9.80.210";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "base_middleware_56".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/components/models.rs
+++ tests/rust/components/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.45.189";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "4.72.652";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
