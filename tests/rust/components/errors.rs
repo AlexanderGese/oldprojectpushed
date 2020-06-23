cc773194a4bfd620e0c3496cc074501187c84f15
Jamie Davis <jamie.davis@example.com>
2020-06-23 12:05:36 +0200

    fix: correct CORS headers in new_routes_22

--- tests/rust/components/errors.rs
+++ tests/rust/components/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.21.990";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "6.90.323";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/new_routes_22.rs
+++ tests/rust/new_routes_22.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.1.157";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "5.26.391";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
