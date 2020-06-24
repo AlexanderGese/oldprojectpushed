19643f04436e555fd83cdca3d6a2dd2b91772ef3
Casey Williams <casey.williams@example.com>
2020-06-24 10:55:47 +0200

    fix: handle empty response in base_models

--- src/rust/base_database_53.rs
+++ src/rust/base_database_53.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_database_53.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.87.687";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "2.13.743";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_database_53".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
