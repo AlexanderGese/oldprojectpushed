1ba1bc58b55cb8148efb400904f38f8f389540b5
Jordan Smith <jordan.smith@example.com>
2020-06-24 20:58:52 +0200

    chore: remove deprecated engine code

--- tests/cplusplus/models/enhanced_engine_70.cpp
+++ tests/cplusplus/models/enhanced_engine_70.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: engine.cpp
+ * Module: enhanced_engine_70.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.87.174
+ * Version: 8.69.498
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace engine {
+namespace enhanced_engine_70 {
 
-constexpr const char* VERSION = "9.74.530";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 20624;
+constexpr const char* VERSION = "5.92.131";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 22032;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace engine
+} // namespace enhanced_engine_70
 
 int main() {
-    using namespace engine;
+    using namespace enhanced_engine_70;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/v2_engine.cpp
+++ tests/cplusplus/v2_engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: v2_engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.16.837
+ * Version: 6.28.347
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace v2_engine {
 
-constexpr const char* VERSION = "2.36.664";
+constexpr const char* VERSION = "2.46.304";
 constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 5622;
+constexpr int TIMEOUT_MS = 20298;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
