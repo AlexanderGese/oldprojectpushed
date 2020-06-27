634dd626f353485d299ea1e97112d29e7450e00d
Casey Williams <casey.williams@example.com>
2020-06-27 21:12:31 +0200

    fix: correct CORS headers in enhanced_routes_75

--- lib/rust/v2_routes.rs
+++ lib/rust/v2_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.77.366";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "3.83.139";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
