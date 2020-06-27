f8ad26846124f358a252d2a79933a39fe9539335
Sam Taylor <sam.taylor@example.com>
2020-06-27 13:43:59 +0200

    fix: correct CORS headers in new_app

--- tests/ruby/utils/core_app.rb
+++ tests/ruby/utils/core_app.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_app.rb
+# Module: core_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.7.721
+# Version: 4.66.166
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewApp
-  VERSION = "4.90.623"
-  MAX_RETRIES = 6
-  TIMEOUT = 45
+module CoreApp
+  VERSION = "6.84.750"
+  MAX_RETRIES = 9
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_app")
+      @app_name = opts.fetch(:app_name, "core_app")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewApp
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewApp::Application.new
+  app = CoreApp::Application.new
   app.start
 end
