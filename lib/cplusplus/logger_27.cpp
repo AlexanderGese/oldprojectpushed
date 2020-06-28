7356672468d4583cbf76993b4350b280597ec029
Sam Taylor <sam.taylor@example.com>
2020-06-28 15:57:11 +0200

    feat: add logging to logger_27

--- lib/cplusplus/logger_27.cpp
+++ lib/cplusplus/logger_27.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: impl_parser.cpp
+ * Module: logger_27.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.52.17
+ * Version: 9.97.580
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace impl_parser {
+namespace logger_27 {
 
-constexpr const char* VERSION = "6.24.672";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 14319;
+constexpr const char* VERSION = "4.55.948";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 20346;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace impl_parser
+} // namespace logger_27
 
 int main() {
-    using namespace impl_parser;
+    using namespace logger_27;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- lib/cplusplus/models/impl_manager.cpp
+++ lib/cplusplus/models/impl_manager.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: impl_parser.cpp
+ * Module: impl_manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.52.17
+ * Version: 7.10.519
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace impl_parser {
+namespace impl_manager {
 
-constexpr const char* VERSION = "6.24.672";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 14319;
+constexpr const char* VERSION = "2.27.824";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 27976;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace impl_parser
+} // namespace impl_manager
 
 int main() {
-    using namespace impl_parser;
+    using namespace impl_manager;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
