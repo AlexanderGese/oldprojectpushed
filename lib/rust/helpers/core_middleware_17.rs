40a87aef4933afd65b239d1b39cc6f6f8c7c5373
Alex Chen <alex.chen@example.com>
2020-07-02 10:58:44 +0200

    refactor: split base_utils into smaller modules

--- lib/rust/helpers/core_middleware_17.rs
+++ lib/rust/helpers/core_middleware_17.rs
@@ -1,4 +1,4 @@
-//! Module: base_utils.rs
+//! Module: core_middleware_17.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.34.45";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "1.44.534";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_utils".to_string(),
+            app_name: "core_middleware_17".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
