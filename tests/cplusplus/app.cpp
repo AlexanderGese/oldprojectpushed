9304368198a5ccd77b9ce205e341f526d8743a59
Drew Martin <drew.martin@example.com>
2020-07-02 09:06:45 +0200

    chore: update app configuration

--- tests/cplusplus/app.cpp
+++ tests/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.25.232
+ * Version: 3.36.659
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "7.12.5";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 6290;
+constexpr const char* VERSION = "5.49.981";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 11047;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
