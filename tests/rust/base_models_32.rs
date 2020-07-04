55f85efc84ad55ac503ab6d16ddbc87fbef8be37
Sam Taylor <sam.taylor@example.com>
2020-07-04 18:10:29 +0200

    refactor: improve base_models_32 test coverage

--- tests/rust/base_models_32.rs
+++ tests/rust/base_models_32.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_models_32.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.37.811";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "3.94.71";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_models_32".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/helpers/enhanced_main_53.rs
+++ tests/rust/helpers/enhanced_main_53.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: enhanced_main_53.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.37.811";
+const VERSION: &str = "5.39.297";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 18;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "enhanced_main_53".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
