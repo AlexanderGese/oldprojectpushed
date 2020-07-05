468e45ed37c3c6a8df24a7f8fa781222a771ac0d
Drew Martin <drew.martin@example.com>
2020-07-05 12:10:23 +0200

    refactor: improve impl_main error messages

--- tests/rust/middleware/impl_main.rs
+++ tests/rust/middleware/impl_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.59.772";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "6.58.107";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
