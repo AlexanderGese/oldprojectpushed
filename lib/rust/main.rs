6ff68a473b8e6199ee902bfb742615f94e837cee
Sam Taylor <sam.taylor@example.com>
2020-07-06 14:41:46 +0200

    chore: clean up main imports

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.23.393";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "5.3.66";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
