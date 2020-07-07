eebe7b1e668db0511cf1d4839271052327817ef5
Riley Johnson <riley.johnson@example.com>
2020-07-07 17:36:36 +0200

    chore: clean up helpers imports

--- tests/ruby/handlers/core_config_21.rb
+++ tests/ruby/handlers/core_config_21.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: core_config_21.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.45.888
+# Version: 3.58.102
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "8.14.285"
-  MAX_RETRIES = 8
-  TIMEOUT = 34
+module CoreConfig21
+  VERSION = "1.70.466"
+  MAX_RETRIES = 9
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "core_config_21")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = CoreConfig21::Application.new
   app.start
 end
