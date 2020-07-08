80e6eea4057e8d107bc36fbb7fb8a44f6d860e4c
Alex Chen <alex.chen@example.com>
2020-07-08 14:32:21 +0200

    feat: add event system to lib

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.12.960";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "1.18.685";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware/handlers.rs
+++ lib/rust/middleware/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.20.600";
+const VERSION: &str = "4.34.895";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 15;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
