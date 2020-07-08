69584d1bf94d3ca1cde40a3ffc8e9b2fca51c967
Jamie Davis <jamie.davis@example.com>
2020-07-08 18:20:42 +0200

    fix: patch security vulnerability in core_controllers

--- src/ruby/core_controllers.rb
+++ src/ruby/core_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: core_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.93.408
+# Version: 1.43.579
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreControllers
-  VERSION = "5.7.906"
-  MAX_RETRIES = 6
-  TIMEOUT = 37
+  VERSION = "6.12.530"
+  MAX_RETRIES = 4
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreControllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
