2d9265c08c456481f9d5b4201b723639eb8bf27c
Casey Williams <casey.williams@example.com>
2020-07-10 18:47:07 +0200

    refactor: migrate new_lib_32 to async/await

--- tests/rust/config_47.rs
+++ tests/rust/config_47.rs
@@ -1,4 +1,4 @@
-//! Module: new_lib_32.rs
+//! Module: config_47.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.34.482";
+const VERSION: &str = "1.38.530";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 54;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_lib_32".to_string(),
+            app_name: "config_47".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
