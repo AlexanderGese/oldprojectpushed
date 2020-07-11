c52f4f3c44819aaafad621b1bb33f7fa4335488d
Riley Johnson <riley.johnson@example.com>
2020-07-11 17:05:33 +0200

    fix: correct type definitions in routes

--- lib/ruby/components/v2_validators_35.rb
+++ lib/ruby/components/v2_validators_35.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: v2_validators_35.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.74.301
+# Version: 4.75.756
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "6.37.717"
-  MAX_RETRIES = 8
+module V2Validators35
+  VERSION = "4.43.754"
+  MAX_RETRIES = 5
   TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "v2_validators_35")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = V2Validators35::Application.new
   app.start
 end
