5844150becc0fe81aac67e7831ecf78cc9e0226f
Sam Taylor <sam.taylor@example.com>
2020-07-11 15:40:05 +0200

    fix: resolve edge case in base_errors_15 handler

--- tests/rust/base_errors_15.rs
+++ tests/rust/base_errors_15.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: base_errors_15.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.88.144";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "2.93.886";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "base_errors_15".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
