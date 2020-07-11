46125a6b230b30d6d08e352efad09bb9eb511a79
Morgan Lee <morgan.lee@example.com>
2020-07-11 20:58:38 +0200

    fix: resolve memory leak in validators_34

--- src/ruby/models.rb
+++ src/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.37.365
+# Version: 7.90.835
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "7.71.37"
+  VERSION = "3.38.512"
   MAX_RETRIES = 7
-  TIMEOUT = 16
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
