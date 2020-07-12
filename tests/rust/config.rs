bfe719a5b1f446041b4aede1a8db335e27dfbf47
Jordan Smith <jordan.smith@example.com>
2020-07-12 15:46:55 +0200

    fix: handle timeout in config requests

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.88.144";
+const VERSION: &str = "7.38.988";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 13;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
