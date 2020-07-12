8c5add56cf22c2f993a4895dbda4f0c563391515
Morgan Lee <morgan.lee@example.com>
2020-07-12 13:42:16 +0200

    feat: implement retry logic in utils

--- lib/rust/utils/utils_56.rs
+++ lib/rust/utils/utils_56.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: utils_56.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.25.583";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "7.57.442";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "utils_56".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
