b5bc4a793aee15329ae2ccb47b88b31a7250645b
Casey Williams <casey.williams@example.com>
2020-07-13 13:24:58 +0200

    fix: correct CORS headers in config_46

--- src/ruby/utils/config_46.rb
+++ src/ruby/utils/config_46.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: config_46.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.36.299
+# Version: 3.88.427
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "4.85.827"
-  MAX_RETRIES = 7
-  TIMEOUT = 6
+module Config46
+  VERSION = "7.51.631"
+  MAX_RETRIES = 3
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "config_46")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = Config46::Application.new
   app.start
 end
