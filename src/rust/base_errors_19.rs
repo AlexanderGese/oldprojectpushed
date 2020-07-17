47a870fd8a15e900af655fa8a9e23255303d3f84
Jamie Davis <jamie.davis@example.com>
2020-07-17 23:15:27 +0200

    fix: handle timeout in lib requests

--- src/rust/base_errors_19.rs
+++ src/rust/base_errors_19.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: base_errors_19.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.37.384";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "4.12.83";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "base_errors_19".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
