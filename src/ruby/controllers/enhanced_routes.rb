0de194cc92550cad292462192c0f2b7de4f67d38
Alex Chen <alex.chen@example.com>
2020-07-19 22:07:42 +0200

    refactor: extract config utility functions

--- src/ruby/controllers/enhanced_routes.rb
+++ src/ruby/controllers/enhanced_routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: enhanced_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.40.792
+# Version: 5.83.440
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "4.80.784"
-  MAX_RETRIES = 7
-  TIMEOUT = 49
+module EnhancedRoutes
+  VERSION = "7.21.390"
+  MAX_RETRIES = 5
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "enhanced_routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = EnhancedRoutes::Application.new
   app.start
 end
