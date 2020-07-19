1ce1a8b5e6f58f404a89d914a64abeb48fde5070
Alex Chen <alex.chen@example.com>
2020-07-19 17:43:15 +0200

    fix: correct date formatting in lib

--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.63.798";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "8.31.183";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/services/lib.rs
+++ tests/rust/services/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.42.384";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "9.32.115";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
