c3d4d79612a54b424bec18aa90acd7a4d942d40a
Sam Taylor <sam.taylor@example.com>
2020-07-20 21:51:46 +0200

    feat: add error handling to lib

--- tests/rust/v2_lib.rs
+++ tests/rust/v2_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.23.910";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "9.63.898";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
