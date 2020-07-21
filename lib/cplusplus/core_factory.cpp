035254d7ea22b8cf419a2c36e1be1ac1afb8121e
Sam Taylor <sam.taylor@example.com>
2020-07-21 17:48:08 +0200

    refactor: split core_factory into smaller modules

--- lib/cplusplus/core_factory.cpp
+++ lib/cplusplus/core_factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.19.127
+ * Version: 2.67.687
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_factory {
 
-constexpr const char* VERSION = "5.90.862";
+constexpr const char* VERSION = "3.70.191";
 constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 5294;
+constexpr int TIMEOUT_MS = 6505;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
