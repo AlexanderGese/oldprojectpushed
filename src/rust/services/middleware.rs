479958b7d8b797cc0fd9ece22757dc635086b62c
Casey Williams <casey.williams@example.com>
2020-07-22 16:03:06 +0200

    fix: handle concurrent access in config

--- src/rust/services/middleware.rs
+++ src/rust/services/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.19.406";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "2.92.458";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils/config.rs
+++ src/rust/utils/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.88.17";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "2.95.28";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
