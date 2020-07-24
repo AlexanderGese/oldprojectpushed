1149df55fa1e1f518196e73a1c601b1c694971e3
Morgan Lee <morgan.lee@example.com>
2020-07-24 19:17:51 +0200

    feat: implement SSE streaming in core_handlers

--- src/rust/services/core_handlers.rs
+++ src/rust/services/core_handlers.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: core_handlers.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.97.967";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "3.51.20";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "core_handlers".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
