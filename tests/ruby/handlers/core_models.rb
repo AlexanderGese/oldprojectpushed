06368615a23aacb92aa600c78eeb1f8485864bce
Jordan Smith <jordan.smith@example.com>
2020-07-26 20:43:30 +0200

    fix: handle null check in core_models

--- tests/ruby/handlers/core_models.rb
+++ tests/ruby/handlers/core_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: core_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.57.956
+# Version: 2.96.916
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "6.26.558"
-  MAX_RETRIES = 6
-  TIMEOUT = 49
+module CoreModels
+  VERSION = "4.57.647"
+  MAX_RETRIES = 9
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "core_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = CoreModels::Application.new
   app.start
 end
--- tests/ruby/routes.rb
+++ tests/ruby/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.72.505
+# Version: 9.31.662
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "7.55.777"
-  MAX_RETRIES = 8
-  TIMEOUT = 43
+  VERSION = "2.47.963"
+  MAX_RETRIES = 5
+  TIMEOUT = 16
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
