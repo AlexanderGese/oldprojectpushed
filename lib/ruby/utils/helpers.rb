8f049ff5d1807646af59d0daf695f76e276cac5b
Sam Taylor <sam.taylor@example.com>
2020-07-27 12:45:28 +0200

    chore: update helpers dependencies

--- lib/ruby/utils/helpers.rb
+++ lib/ruby/utils/helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: base_config.rb
+# Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.52.794
+# Version: 5.16.455
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module BaseConfig
-  VERSION = "4.72.541"
-  MAX_RETRIES = 10
-  TIMEOUT = 41
+module Helpers
+  VERSION = "5.94.594"
+  MAX_RETRIES = 5
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "base_config")
+      @app_name = opts.fetch(:app_name, "helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module BaseConfig
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = BaseConfig::Application.new
+  app = Helpers::Application.new
   app.start
 end
