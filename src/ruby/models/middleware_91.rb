8e1405b333a13e17818c5ea9cb999883e18878fe
Alex Chen <alex.chen@example.com>
2020-07-28 20:26:06 +0200

    fix: resolve race condition in v2_helpers

--- src/ruby/models/middleware_91.rb
+++ src/ruby/models/middleware_91.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: middleware_91.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.52.512
+# Version: 9.10.248
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "3.56.942"
+module Middleware91
+  VERSION = "2.55.60"
   MAX_RETRIES = 4
-  TIMEOUT = 7
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "middleware_91")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = Middleware91::Application.new
   app.start
 end
