5f6278eb995f5b1608771cf9be06a12b982fe375
Morgan Lee <morgan.lee@example.com>
2020-07-31 12:23:24 +0200

    chore: configure CI/CD for models

--- src/ruby/app.rb
+++ src/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.23.21
+# Version: 7.76.422
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "3.74.258"
-  MAX_RETRIES = 8
-  TIMEOUT = 8
+  VERSION = "8.65.861"
+  MAX_RETRIES = 9
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
