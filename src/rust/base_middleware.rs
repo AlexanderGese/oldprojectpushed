f03259bce031ad3e0e2d8a0adbb8486d4aead819
Drew Martin <drew.martin@example.com>
2020-08-03 19:56:06 +0200

    fix: resolve timezone issue in base_errors

--- src/rust/base_middleware.rs
+++ src/rust/base_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.71.770";
+const VERSION: &str = "6.92.412";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 57;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/middleware/base_errors.rs
+++ src/rust/middleware/base_errors.rs
@@ -1,4 +1,4 @@
-//! Module: impl_utils.rs
+//! Module: base_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.45.698";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "6.13.577";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_utils".to_string(),
+            app_name: "base_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
