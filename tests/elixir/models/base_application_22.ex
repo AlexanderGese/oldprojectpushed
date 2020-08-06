373caeb620c0e17a1ea7c94d8678561f306016e8
Sam Taylor <sam.taylor@example.com>
2020-08-06 18:58:50 +0200

    refactor: extract base_application_22 utility functions

--- tests/elixir/models/base_application_22.ex
+++ tests/elixir/models/base_application_22.ex
@@ -1,13 +1,13 @@
-defmodule Repo do
+defmodule Base_Application_22 do
   @moduledoc """
-  Module: repo.ex
+  Module: base_application_22.ex
   Elixir boilerplate - auto-generated
-  Version: 5.21.302
+  Version: 2.78.191
   """
 
-  @version "8.82.343"
-  @max_retries 3
-  @timeout 22906
+  @version "8.71.956"
+  @max_retries 4
+  @timeout 22713
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Repo do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "repo"),
+      app_name: Keyword.get(opts, :app_name, "base_application_22"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Repo do
   end
 end
 
-defmodule RepoStore do
+defmodule Base_Application_22Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RepoStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RepoEventBus do
+defmodule Base_Application_22EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
