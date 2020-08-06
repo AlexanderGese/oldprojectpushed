e7f08e294a63dddd60bbb9ffba5ecb1f5598218d
Alex Chen <alex.chen@example.com>
2020-08-06 10:32:48 +0200

    feat: implement server service layer

--- tests/ruby/models/base_controllers.rb
+++ tests/ruby/models/base_controllers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: base_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.15.670
+# Version: 9.19.673
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "5.97.286"
-  MAX_RETRIES = 3
-  TIMEOUT = 12
+module BaseControllers
+  VERSION = "5.48.696"
+  MAX_RETRIES = 10
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "base_controllers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = BaseControllers::Application.new
   app.start
 end
