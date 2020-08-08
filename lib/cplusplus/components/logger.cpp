9f28ab3e8540ce4a54e37f7ecb2f363357c8f5b0
Sam Taylor <sam.taylor@example.com>
2020-08-08 14:03:54 +0200

    feat: implement authentication in logger

--- lib/cplusplus/components/logger.cpp
+++ lib/cplusplus/components/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.80.609
+ * Version: 7.5.602
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "4.42.739";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 27161;
+constexpr const char* VERSION = "3.20.167";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 16833;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
