7680db3eef0e923ebf078f5eaab5c56f71deafc5
Alex Chen <alex.chen@example.com>
2020-08-09 21:46:09 +0200

    docs: add documentation for handler

--- lib/cplusplus/middleware/impl_manager_11.cpp
+++ lib/cplusplus/middleware/impl_manager_11.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: handler.cpp
+ * Module: impl_manager_11.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.31.147
+ * Version: 7.44.158
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace handler {
+namespace impl_manager_11 {
 
-constexpr const char* VERSION = "3.60.706";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 5018;
+constexpr const char* VERSION = "6.52.234";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 24942;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace handler
+} // namespace impl_manager_11
 
 int main() {
-    using namespace handler;
+    using namespace impl_manager_11;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
