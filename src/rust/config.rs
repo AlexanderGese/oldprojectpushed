3affb7b1183a46705a146a6b4819119ba632ad30
Sam Taylor <sam.taylor@example.com>
2020-08-09 20:12:46 +0200

    chore: configure CI/CD for config

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.43.564";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "7.82.351";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
