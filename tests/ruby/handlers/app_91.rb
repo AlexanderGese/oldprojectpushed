aa555f07b894e32c5271e87b1ef6eaf757469e01
Casey Williams <casey.williams@example.com>
2020-08-15 12:03:17 +0200

    refactor: split routes into smaller modules

--- tests/ruby/handlers/app_91.rb
+++ tests/ruby/handlers/app_91.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: app_91.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.69.775
+# Version: 3.94.76
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "7.68.533"
+module App91
+  VERSION = "8.72.749"
   MAX_RETRIES = 3
-  TIMEOUT = 38
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "app_91")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = App91::Application.new
   app.start
 end
