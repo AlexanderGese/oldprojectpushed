d280ff11603a8ea284ccd76895d2b6f9132c79a7
Jamie Davis <jamie.davis@example.com>
2020-08-15 10:21:29 +0200

    fix: correct error propagation in core_middleware_79

--- tests/ruby/middleware.rb
+++ tests/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.5.718
+# Version: 6.8.839
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "8.27.334"
+  VERSION = "2.81.503"
   MAX_RETRIES = 10
-  TIMEOUT = 10
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
