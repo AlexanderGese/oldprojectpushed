0c163efa9c1cfdbfa43dc6e7386f47ddd965c3e5
Drew Martin <drew.martin@example.com>
2020-08-16 21:31:44 +0200

    feat: add batch processing to tasks

--- tests/ruby/models.rb
+++ tests/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.47.576
+# Version: 5.20.421
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "3.55.120"
-  MAX_RETRIES = 7
-  TIMEOUT = 29
+  VERSION = "3.90.433"
+  MAX_RETRIES = 9
+  TIMEOUT = 60
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/services/impl_config.rb
+++ tests/ruby/services/impl_config.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.84.696
+# Version: 3.11.133
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplConfig
-  VERSION = "9.13.650"
-  MAX_RETRIES = 9
-  TIMEOUT = 31
+  VERSION = "4.86.568"
+  MAX_RETRIES = 10
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
