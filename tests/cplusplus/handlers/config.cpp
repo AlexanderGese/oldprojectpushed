ce7d43a40ab7161314958d8d39829fbe859876bf
Jordan Smith <jordan.smith@example.com>
2020-08-16 16:29:49 +0200

    fix: resolve timezone issue in config

--- tests/cplusplus/handlers/config.cpp
+++ tests/cplusplus/handlers/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.87.259
+ * Version: 9.34.389
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "9.35.72";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 20558;
+constexpr const char* VERSION = "8.91.941";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 5077;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/new_parser_27.cpp
+++ tests/cplusplus/new_parser_27.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: new_parser_27.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.83.198
+ * Version: 3.71.783
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace new_parser_27 {
 
-constexpr const char* VERSION = "2.36.714";
+constexpr const char* VERSION = "2.82.583";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 13793;
+constexpr int TIMEOUT_MS = 9295;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace new_parser_27
 
 int main() {
-    using namespace parser;
+    using namespace new_parser_27;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
