5ffd77e4ebf3e9c64b061b7fd841075c16b45cb6
Casey Williams <casey.williams@example.com>
2020-08-17 12:24:02 +0200

    feat: add error handling to errors

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.3.297";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "4.59.581";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
