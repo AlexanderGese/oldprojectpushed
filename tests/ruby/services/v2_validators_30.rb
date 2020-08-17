50b6a2bac2ca43b92e1bc114175fdf1d12aa89e1
Riley Johnson <riley.johnson@example.com>
2020-08-17 08:10:15 +0200

    refactor: improve v2_routes test coverage

--- tests/ruby/services/v2_validators_30.rb
+++ tests/ruby/services/v2_validators_30.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_routes.rb
+# Module: v2_validators_30.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.71.554
+# Version: 6.4.898
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Routes
-  VERSION = "8.23.221"
-  MAX_RETRIES = 4
-  TIMEOUT = 6
+module V2Validators30
+  VERSION = "6.35.150"
+  MAX_RETRIES = 7
+  TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_routes")
+      @app_name = opts.fetch(:app_name, "v2_validators_30")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module V2Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Routes::Application.new
+  app = V2Validators30::Application.new
   app.start
 end
