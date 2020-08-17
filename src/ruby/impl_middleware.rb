fcacffed7d52fcb213f9985ecc0fb15a6df19eab
Casey Williams <casey.williams@example.com>
2020-08-17 21:32:50 +0200

    fix: patch security vulnerability in base_helpers_43

--- src/ruby/impl_middleware.rb
+++ src/ruby/impl_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.49.486
+# Version: 2.97.751
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplMiddleware
-  VERSION = "4.97.890"
-  MAX_RETRIES = 4
-  TIMEOUT = 59
+  VERSION = "9.34.228"
+  MAX_RETRIES = 3
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
