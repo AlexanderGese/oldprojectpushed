97ac7ac8c64a05f1e7d16c686c839d1b6b2c6d53
Casey Williams <casey.williams@example.com>
2020-08-17 19:03:25 +0200

    fix: handle null check in controllers

--- tests/ruby/models/enhanced_config_85.rb
+++ tests/ruby/models/enhanced_config_85.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: enhanced_config_85.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.88.932
+# Version: 5.87.75
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "1.61.721"
-  MAX_RETRIES = 8
-  TIMEOUT = 45
+module EnhancedConfig85
+  VERSION = "3.70.571"
+  MAX_RETRIES = 6
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "enhanced_config_85")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = EnhancedConfig85::Application.new
   app.start
 end
--- tests/ruby/utils/routes_12.rb
+++ tests/ruby/utils/routes_12.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: routes_12.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.88.932
+# Version: 5.52.34
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "1.61.721"
-  MAX_RETRIES = 8
-  TIMEOUT = 45
+module Routes12
+  VERSION = "3.26.750"
+  MAX_RETRIES = 5
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "routes_12")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = Routes12::Application.new
   app.start
 end
