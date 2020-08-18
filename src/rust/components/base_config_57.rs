47383ce829a520a7aecf76f3879b0b8287076993
Alex Chen <alex.chen@example.com>
2020-08-18 21:22:08 +0200

    fix: correct CORS headers in base_config_57

--- src/rust/components/base_config_57.rs
+++ src/rust/components/base_config_57.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_config_57.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.11.469";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "7.16.155";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_config_57".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/controllers/handlers.rs
+++ src/rust/controllers/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.78.134";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "8.92.887";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
