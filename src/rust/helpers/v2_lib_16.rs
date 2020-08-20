cb3df46d5b3ab1d0d5b20cf1aba92d23165d8a25
Casey Williams <casey.williams@example.com>
2020-08-20 18:13:02 +0200

    fix: handle large payloads in v2_lib_16

--- src/rust/helpers/v2_lib_16.rs
+++ src/rust/helpers/v2_lib_16.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: v2_lib_16.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.89.244";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "7.49.474";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "v2_lib_16".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
