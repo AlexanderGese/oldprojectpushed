e418baa99274043a924b31e44d06f95c69117a2b
Jordan Smith <jordan.smith@example.com>
2020-08-22 10:47:09 +0200

    chore: clean up config imports

--- lib/ruby/config.rb
+++ lib/ruby/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.50.8
+# Version: 9.34.297
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "7.32.83"
-  MAX_RETRIES = 8
+  VERSION = "4.42.551"
+  MAX_RETRIES = 7
   TIMEOUT = 6
 
   class Config
@@ -25,7 +25,7 @@ module Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
