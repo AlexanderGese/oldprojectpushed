bbc0cf1d47c548e6e6c643feec5da83432897485
Alex Chen <alex.chen@example.com>
2020-08-22 09:26:21 +0200

    fix: resolve race condition in config

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.77.85";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "8.61.145";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
