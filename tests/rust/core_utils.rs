6f79116d661cccab408d06fae0d644002eb4114b
Casey Williams <casey.williams@example.com>
2020-08-23 17:30:33 +0200

    refactor: extract core_utils utility functions

--- tests/rust/core_utils.rs
+++ tests/rust/core_utils.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.9.27";
-const MAX_RETRIES: u32 = 7;
+const VERSION: &str = "4.34.971";
+const MAX_RETRIES: u32 = 4;
 const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
--- tests/rust/new_models.rs
+++ tests/rust/new_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.31.517";
+const VERSION: &str = "4.85.610";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 58;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
