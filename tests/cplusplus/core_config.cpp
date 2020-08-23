5936a63eb64459ef651de6b2acb1d07c94df70eb
Sam Taylor <sam.taylor@example.com>
2020-08-23 19:27:13 +0200

    feat: add health check endpoint to core_config

--- tests/cplusplus/core_config.cpp
+++ tests/cplusplus/core_config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_config.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.35.934
+ * Version: 3.49.178
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_config {
 
-constexpr const char* VERSION = "9.75.419";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 11713;
+constexpr const char* VERSION = "9.90.212";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 26374;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
