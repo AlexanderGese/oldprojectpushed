923ee5504f70d9b2d33fb0a8bce937875d277abe
Jamie Davis <jamie.davis@example.com>
2020-08-25 08:49:36 +0200

    feat: add event system to base_main_32

--- lib/rust/utils.rs
+++ lib/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.62.887";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "3.52.970";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
