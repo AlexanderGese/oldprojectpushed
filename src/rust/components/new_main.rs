93949dbeb695caf3a7aefa87e158dbc53602fea8
Alex Chen <alex.chen@example.com>
2020-08-25 20:52:05 +0200

    feat: implement retry logic in errors

--- src/rust/components/new_main.rs
+++ src/rust/components/new_main.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: new_main.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.90.941";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "2.32.235";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "new_main".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/models/database.rs
+++ src/rust/models/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.89.213";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "1.72.569";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
