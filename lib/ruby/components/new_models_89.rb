b0614a896ccac591a968809a3a82aa60141ab6f7
Casey Williams <casey.williams@example.com>
2020-08-26 16:53:47 +0200

    chore: clean up tasks imports

--- lib/ruby/components/new_models_89.rb
+++ lib/ruby/components/new_models_89.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: new_models_89.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.96.998
+# Version: 6.27.821
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "4.5.802"
-  MAX_RETRIES = 9
-  TIMEOUT = 9
+module NewModels89
+  VERSION = "8.9.665"
+  MAX_RETRIES = 7
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "new_models_89")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = NewModels89::Application.new
   app.start
 end
