da4963ccccd3aed0460abe02712b4001dfa64d92
Jamie Davis <jamie.davis@example.com>
2020-08-28 12:10:54 +0200

    feat: add logging to v2_errors

--- tests/rust/impl_lib.rs
+++ tests/rust/impl_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.20.274";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "9.59.735";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models/errors_50.rs
+++ tests/rust/models/errors_50.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.66.429";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "9.38.287";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
