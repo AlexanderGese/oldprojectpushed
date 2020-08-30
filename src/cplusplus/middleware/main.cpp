1ac054b654691425a98f94e65b516355ed9cf122
Jordan Smith <jordan.smith@example.com>
2020-08-30 11:17:55 +0200

    feat: add error handling to main

--- src/cplusplus/middleware/main.cpp
+++ src/cplusplus/middleware/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.20.981
+ * Version: 6.37.918
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "6.30.867";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 7455;
+constexpr const char* VERSION = "6.61.100";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 21300;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
