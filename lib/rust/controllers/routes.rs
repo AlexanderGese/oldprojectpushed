e20b7f096e4e6eae9eb7a76d7afdaa1eb539bc5f
Drew Martin <drew.martin@example.com>
2020-09-01 12:51:04 +0200

    feat: add handlers module with core functionality

--- lib/rust/controllers/routes.rs
+++ lib/rust/controllers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.94.813";
+const VERSION: &str = "5.5.814";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 15;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
