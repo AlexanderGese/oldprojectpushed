8c7c16e0afede5fc3672de6d7928636a44c2fc2d
Drew Martin <drew.martin@example.com>
2020-09-08 08:57:22 +0200

    refactor: split main into smaller modules

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.31.617";
+const VERSION: &str = "4.71.498";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 33;
+const TIMEOUT_SECS: u64 = 28;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/main.rs
+++ src/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.86.452";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "1.64.871";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
