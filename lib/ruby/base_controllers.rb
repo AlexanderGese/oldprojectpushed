da9df05bca9ad9c98bbb9c07f0c1e31adbfbc6a1
Alex Chen <alex.chen@example.com>
2020-09-08 20:34:58 +0200

    fix: resolve encoding issue in tasks

--- lib/ruby/base_controllers.rb
+++ lib/ruby/base_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: base_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.34.619
+# Version: 3.19.289
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseControllers
-  VERSION = "5.42.649"
+  VERSION = "6.55.129"
   MAX_RETRIES = 10
-  TIMEOUT = 25
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- lib/ruby/new_routes.rb
+++ lib/ruby/new_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: new_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.5.372
+# Version: 6.19.155
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewRoutes
-  VERSION = "4.15.763"
-  MAX_RETRIES = 6
-  TIMEOUT = 45
+  VERSION = "7.59.768"
+  MAX_RETRIES = 4
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewRoutes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
