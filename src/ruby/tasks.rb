3b2ec2b52f35fe54cf39ba48d917a6acccb22427
Riley Johnson <riley.johnson@example.com>
2020-09-08 17:59:03 +0200

    feat: add event system to models

--- src/ruby/tasks.rb
+++ src/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.42.499
+# Version: 4.33.164
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "2.31.711"
-  MAX_RETRIES = 7
-  TIMEOUT = 55
+  VERSION = "7.12.920"
+  MAX_RETRIES = 3
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
