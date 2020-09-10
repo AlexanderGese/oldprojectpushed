eaa0cb830021cbb0c5c85215a08db084e35799ec
Drew Martin <drew.martin@example.com>
2020-09-10 19:09:42 +0200

    fix: resolve edge case in core_context_43 handler

--- lib/elixir/repo_11.ex
+++ lib/elixir/repo_11.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule Repo_11 do
   @moduledoc """
-  Module: schema.ex
+  Module: repo_11.ex
   Elixir boilerplate - auto-generated
-  Version: 7.55.83
+  Version: 8.61.305
   """
 
-  @version "3.7.407"
-  @max_retries 4
-  @timeout 10468
+  @version "5.95.35"
+  @max_retries 5
+  @timeout 8640
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "repo_11"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule Repo_11Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule Repo_11EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
