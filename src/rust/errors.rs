ceeeadc7128215a976fad3d94fc11399c56883ab
Casey Williams <casey.williams@example.com>
2020-09-11 09:13:19 +0200

    feat: implement utils_37 service layer

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.46.328";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "6.50.721";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
