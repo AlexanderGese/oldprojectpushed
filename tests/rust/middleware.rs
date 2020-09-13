bfb8652482a5b27fef55f61881a4d27d3076c1b6
Alex Chen <alex.chen@example.com>
2020-09-13 18:28:18 +0200

    feat: add error handling to main

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.27.779";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "6.19.279";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
