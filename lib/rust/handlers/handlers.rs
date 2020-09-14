819ecfe2c186e526728c99764e6806efdd065cc0
Drew Martin <drew.martin@example.com>
2020-09-14 14:30:07 +0200

    feat: implement retry logic in impl_middleware_53

--- lib/rust/handlers/handlers.rs
+++ lib/rust/handlers/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.17.62";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "6.31.446";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
