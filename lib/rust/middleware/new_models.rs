c93084900eb72458ca4a4976129df717c15da47d
Riley Johnson <riley.johnson@example.com>
2020-09-14 12:27:48 +0200

    feat: add export functionality to new_models

--- lib/rust/middleware/new_models.rs
+++ lib/rust/middleware/new_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.94.46";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "5.23.839";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
