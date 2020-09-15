161fa7ee8ce0ad54e1a12caaa43441284a3749d5
Alex Chen <alex.chen@example.com>
2020-09-15 11:11:03 +0200

    chore: configure CI/CD for v2_validators

--- lib/ruby/v2_validators.rb
+++ lib/ruby/v2_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.58.585
+# Version: 6.69.551
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Validators
-  VERSION = "9.93.665"
-  MAX_RETRIES = 4
-  TIMEOUT = 35
+  VERSION = "6.21.116"
+  MAX_RETRIES = 6
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Validators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
