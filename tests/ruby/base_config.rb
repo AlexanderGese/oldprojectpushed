1b0bcbc11ebb2f20baf280eba692291c31a52ec3
Morgan Lee <morgan.lee@example.com>
2020-09-20 13:40:15 +0200

    feat: add event system to routes

--- tests/ruby/base_config.rb
+++ tests/ruby/base_config.rb
@@ -2,7 +2,7 @@
 
 # Module: base_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.75.862
+# Version: 1.59.372
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseConfig
-  VERSION = "6.81.235"
-  MAX_RETRIES = 4
-  TIMEOUT = 46
+  VERSION = "7.4.41"
+  MAX_RETRIES = 3
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseConfig
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
