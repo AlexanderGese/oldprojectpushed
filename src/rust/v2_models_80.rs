7c6184dc1114db5148bba355e1f901aa20e7dbda
Jordan Smith <jordan.smith@example.com>
2020-09-22 17:02:20 +0200

    chore: update lib_56 dependencies

--- src/rust/v2_models_80.rs
+++ src/rust/v2_models_80.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: v2_models_80.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.35.780";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "2.24.439";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "v2_models_80".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
