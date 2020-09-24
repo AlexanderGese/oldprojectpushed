8167b4980fb203abca8ec166f40b236c17e0e566
Jamie Davis <jamie.davis@example.com>
2020-09-24 23:18:01 +0200

    refactor: simplify new_config state management

--- lib/ruby/new_config.rb
+++ lib/ruby/new_config.rb
@@ -2,7 +2,7 @@
 
 # Module: new_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.92.78
+# Version: 1.0.973
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewConfig
-  VERSION = "6.93.453"
-  MAX_RETRIES = 6
-  TIMEOUT = 37
+  VERSION = "3.79.599"
+  MAX_RETRIES = 7
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewConfig
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
