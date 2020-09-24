4bece402a726fca6a54997e41a742307af36237f
Drew Martin <drew.martin@example.com>
2020-09-24 18:57:19 +0200

    feat: add caching layer to config

--- tests/ruby/tasks.rb
+++ tests/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.38.726
+# Version: 1.27.148
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "5.65.992"
-  MAX_RETRIES = 3
-  TIMEOUT = 9
+  VERSION = "2.45.730"
+  MAX_RETRIES = 6
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
