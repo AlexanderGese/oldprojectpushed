009b65b96abfc8d816359e47e79094c8e3b8bdbf
Jordan Smith <jordan.smith@example.com>
2020-09-24 10:04:39 +0200

    chore: configure CI/CD for server

--- src/ruby/server.rb
+++ src/ruby/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.3.793
+# Version: 3.12.454
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "5.19.849"
+  VERSION = "8.51.215"
   MAX_RETRIES = 10
-  TIMEOUT = 28
+  TIMEOUT = 59
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
