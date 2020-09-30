084cc18c708513a28a1ee7e98d6e13c3ff59f239
Alex Chen <alex.chen@example.com>
2020-09-30 23:25:20 +0200

    fix: handle empty response in tasks

--- lib/ruby/services/tasks.rb
+++ lib/ruby/services/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.48.946
+# Version: 3.57.803
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "5.88.246"
-  MAX_RETRIES = 6
-  TIMEOUT = 10
+  VERSION = "8.93.175"
+  MAX_RETRIES = 3
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
