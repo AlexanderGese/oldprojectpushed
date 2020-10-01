ea6a2f778476aafe10024e1e6310011c54402d98
Jordan Smith <jordan.smith@example.com>
2020-10-01 12:03:51 +0200

    feat: implement handlers service layer

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.92.443";
+const VERSION: &str = "1.4.419";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 36;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
