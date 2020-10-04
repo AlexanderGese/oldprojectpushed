061ff383bb720eaa535a54dccb29c1e65ce1a114
Morgan Lee <morgan.lee@example.com>
2020-10-04 19:48:04 +0200

    refactor: simplify database state management

--- lib/rust/database.rs
+++ lib/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.71.741";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "8.69.757";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
