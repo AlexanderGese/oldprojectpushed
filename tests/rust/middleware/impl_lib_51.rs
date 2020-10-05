394c5585b140154c2695bf4c85f4bb23f93aa2ae
Jamie Davis <jamie.davis@example.com>
2020-10-05 09:39:49 +0200

    chore: configure CI/CD for impl_lib_51

--- tests/rust/middleware/impl_lib_51.rs
+++ tests/rust/middleware/impl_lib_51.rs
@@ -1,4 +1,4 @@
-//! Module: new_lib.rs
+//! Module: impl_lib_51.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.78.246";
+const VERSION: &str = "8.8.627";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 22;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_lib".to_string(),
+            app_name: "impl_lib_51".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
