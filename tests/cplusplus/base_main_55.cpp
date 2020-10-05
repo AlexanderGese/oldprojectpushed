5e7e98229076b8d158a511f8c60c5d5b1aa759a4
Drew Martin <drew.martin@example.com>
2020-10-05 15:05:36 +0200

    fix: correct error propagation in factory

--- tests/cplusplus/base_main_55.cpp
+++ tests/cplusplus/base_main_55.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: factory.cpp
+ * Module: base_main_55.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.94.861
+ * Version: 3.20.442
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace factory {
+namespace base_main_55 {
 
-constexpr const char* VERSION = "4.92.803";
+constexpr const char* VERSION = "6.92.362";
 constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 14003;
+constexpr int TIMEOUT_MS = 8973;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace factory
+} // namespace base_main_55
 
 int main() {
-    using namespace factory;
+    using namespace base_main_55;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/models/utils_88.cpp
+++ tests/cplusplus/models/utils_88.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: factory.cpp
+ * Module: utils_88.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.94.861
+ * Version: 4.59.159
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace factory {
+namespace utils_88 {
 
-constexpr const char* VERSION = "4.92.803";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 14003;
+constexpr const char* VERSION = "6.34.150";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 16518;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace factory
+} // namespace utils_88
 
 int main() {
-    using namespace factory;
+    using namespace utils_88;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
