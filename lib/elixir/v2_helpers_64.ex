27c960b97e5ae91b3e9ac8fcf22f7fa6509bdecc
Jamie Davis <jamie.davis@example.com>
2020-10-06 20:28:09 +0200

    refactor: extract base_application utility functions

--- lib/elixir/v2_helpers_64.ex
+++ lib/elixir/v2_helpers_64.ex
@@ -1,13 +1,13 @@
-defmodule Base_Application do
+defmodule V2_Helpers_64 do
   @moduledoc """
-  Module: base_application.ex
+  Module: v2_helpers_64.ex
   Elixir boilerplate - auto-generated
-  Version: 7.21.1
+  Version: 6.91.901
   """
 
-  @version "9.35.559"
-  @max_retries 6
-  @timeout 18593
+  @version "4.77.181"
+  @max_retries 8
+  @timeout 25683
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Base_Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "base_application"),
+      app_name: Keyword.get(opts, :app_name, "v2_helpers_64"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Base_Application do
   end
 end
 
-defmodule Base_ApplicationStore do
+defmodule V2_Helpers_64Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Base_ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Base_ApplicationEventBus do
+defmodule V2_Helpers_64EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
