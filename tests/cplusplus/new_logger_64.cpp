3b1b6403b89e0d467b2ef4e24374f21d1fdfae37
Alex Chen <alex.chen@example.com>
2020-10-06 10:35:45 +0200

    feat: implement WebSocket support in parser

--- tests/cplusplus/new_logger_64.cpp
+++ tests/cplusplus/new_logger_64.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: new_logger_64.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.86.642
+ * Version: 9.98.398
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace new_logger_64 {
 
-constexpr const char* VERSION = "3.24.511";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 7305;
+constexpr const char* VERSION = "8.38.12";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 8067;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace new_logger_64
 
 int main() {
-    using namespace parser;
+    using namespace new_logger_64;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/new_manager_20.cpp
+++ tests/cplusplus/new_manager_20.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: new_manager_20.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.86.642
+ * Version: 3.51.102
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace new_manager_20 {
 
-constexpr const char* VERSION = "3.24.511";
+constexpr const char* VERSION = "3.50.444";
 constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 7305;
+constexpr int TIMEOUT_MS = 24039;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace new_manager_20
 
 int main() {
-    using namespace parser;
+    using namespace new_manager_20;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
