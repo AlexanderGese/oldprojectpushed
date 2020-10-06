e4cffd2ae9d80521e998f9a108665ccfd0951d04
Jordan Smith <jordan.smith@example.com>
2020-10-06 14:36:20 +0200

    feat: add logging to new_worker

--- src/elixir/utils/core_controller_16.ex
+++ src/elixir/utils/core_controller_16.ex
@@ -1,13 +1,13 @@
-defmodule New_Worker do
+defmodule Core_Controller_16 do
   @moduledoc """
-  Module: new_worker.ex
+  Module: core_controller_16.ex
   Elixir boilerplate - auto-generated
-  Version: 6.86.585
+  Version: 5.30.592
   """
 
-  @version "4.76.408"
-  @max_retries 4
-  @timeout 23889
+  @version "6.72.822"
+  @max_retries 10
+  @timeout 7547
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule New_Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "new_worker"),
+      app_name: Keyword.get(opts, :app_name, "core_controller_16"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule New_Worker do
   end
 end
 
-defmodule New_WorkerStore do
+defmodule Core_Controller_16Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule New_WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule New_WorkerEventBus do
+defmodule Core_Controller_16EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
