3bb9b752d7f487a8656fc5977ce6c59726ebed2c
Alex Chen <alex.chen@example.com>
2020-10-07 17:59:19 +0200

    feat: add export functionality to base_utils

--- src/rust/base_utils.rs
+++ src/rust/base_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.20.495";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "8.70.481";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/models.rs
+++ src/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.48.933";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "3.83.979";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
