3c250eeba9c993670e6bfbd1b7162416e5c1217a
Sam Taylor <sam.taylor@example.com>
2020-10-07 10:58:05 +0200

    fix: handle concurrent access in routes

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.63.270";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "2.23.402";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
