ca0175a3481ea3f6c0770b75edfee4ac439330f3
Casey Williams <casey.williams@example.com>
2020-10-10 18:38:27 +0200

    fix: patch security vulnerability in utils

--- tests/rust/impl_utils.rs
+++ tests/rust/impl_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.24.809";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "4.91.288";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
