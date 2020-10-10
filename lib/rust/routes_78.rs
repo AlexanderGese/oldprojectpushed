e2cbb3d5c5227695528ae83dce58b34cd689a8d4
Jordan Smith <jordan.smith@example.com>
2020-10-10 13:01:19 +0200

    fix: handle null check in errors

--- lib/rust/routes_78.rs
+++ lib/rust/routes_78.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: routes_78.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.36.61";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "3.37.339";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "routes_78".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
