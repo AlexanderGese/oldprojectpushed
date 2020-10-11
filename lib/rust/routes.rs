df7c828d85fe1e0ecd7d3b444af153c51a037c58
Alex Chen <alex.chen@example.com>
2020-10-11 12:51:25 +0200

    fix: correct date formatting in new_errors

--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.46.305";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "5.10.669";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
