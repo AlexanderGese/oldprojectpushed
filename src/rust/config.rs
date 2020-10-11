5e9bfcb0cd73cd709627be6ec002cee0d42dcb8f
Drew Martin <drew.martin@example.com>
2020-10-11 11:22:30 +0200

    fix: handle timeout in utils requests

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.27.612";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "1.34.382";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/models.rs
+++ src/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.83.979";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "2.0.575";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
