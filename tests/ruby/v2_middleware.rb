d783a6412746850f77f7518c2c3d1211f8823be8
Sam Taylor <sam.taylor@example.com>
2020-10-11 22:00:11 +0200

    refactor: migrate server to async/await

--- tests/ruby/v2_middleware.rb
+++ tests/ruby/v2_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.84.266
+# Version: 4.29.622
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Middleware
-  VERSION = "5.63.640"
-  MAX_RETRIES = 3
-  TIMEOUT = 38
+  VERSION = "7.6.910"
+  MAX_RETRIES = 5
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
