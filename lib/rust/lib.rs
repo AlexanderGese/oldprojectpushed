04932bebdaa8afc4ad3aaf400d7333bfd3b35cd1
Jamie Davis <jamie.davis@example.com>
2020-10-12 18:12:06 +0200

    fix: handle empty response in core_handlers_70

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.18.685";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "4.49.139";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
