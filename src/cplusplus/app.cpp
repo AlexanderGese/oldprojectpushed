efc0783dd5b61dfa7c403151cf391873be1aec45
Alex Chen <alex.chen@example.com>
2020-10-14 20:59:54 +0200

    feat: implement app service layer

--- src/cplusplus/app.cpp
+++ src/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.3.201
+ * Version: 2.94.920
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "3.19.426";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 18058;
+constexpr const char* VERSION = "3.44.223";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 6011;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/models/handler.cpp
+++ src/cplusplus/models/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.13.980
+ * Version: 5.99.626
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "5.31.820";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 14107;
+constexpr const char* VERSION = "4.47.348";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 29739;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
