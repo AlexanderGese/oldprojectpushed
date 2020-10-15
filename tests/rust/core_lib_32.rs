4c150fcfc00ba8f097e98b0939f6028a8fce44c8
Morgan Lee <morgan.lee@example.com>
2020-10-15 08:10:22 +0200

    chore: configure CI/CD for core_lib_32

--- tests/rust/core_lib_32.rs
+++ tests/rust/core_lib_32.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: core_lib_32.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.32.475";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "9.70.81";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "core_lib_32".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
