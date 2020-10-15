683b5b463dd27ba519d4dbfc1d786980c0b915ce
Sam Taylor <sam.taylor@example.com>
2020-10-15 16:20:05 +0200

    fix: resolve timezone issue in core_lib_1

--- tests/rust/components/database.rs
+++ tests/rust/components/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.52.326";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "3.79.606";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
