b8fb76a3034b82405953f460f36c0d23d85743b2
Sam Taylor <sam.taylor@example.com>
2020-10-22 21:32:10 +0200

    fix: resolve memory leak in enhanced_routes

--- src/rust/helpers/enhanced_utils.rs
+++ src/rust/helpers/enhanced_utils.rs
@@ -1,4 +1,4 @@
-//! Module: enhanced_routes.rs
+//! Module: enhanced_utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.69.763";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "5.21.133";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "enhanced_routes".to_string(),
+            app_name: "enhanced_utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
