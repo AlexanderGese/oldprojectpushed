988d15f1c9488672c87433f556e50022b7c0564e
Casey Williams <casey.williams@example.com>
2020-10-23 18:43:53 +0200

    fix: correct type definitions in database

--- tests/rust/utils/database.rs
+++ tests/rust/utils/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.40.134";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "1.79.530";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
