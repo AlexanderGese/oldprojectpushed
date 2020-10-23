022e9be1da42840f36b3697ab23462aea4557533
Jamie Davis <jamie.davis@example.com>
2020-10-23 19:40:17 +0200

    fix: handle concurrent access in main

--- lib/rust/core_middleware.rs
+++ lib/rust/core_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.91.773";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "6.93.959";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
