6e79af39676b7a305f65d2fb7a9fe6d5b2df42d2
Jordan Smith <jordan.smith@example.com>
2020-10-23 23:09:08 +0200

    refactor: improve enhanced_handlers_32 error messages

--- tests/rust/controllers/base_lib_14.rs
+++ tests/rust/controllers/base_lib_14.rs
@@ -1,4 +1,4 @@
-//! Module: impl_models.rs
+//! Module: base_lib_14.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.46.238";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "6.48.90";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_models".to_string(),
+            app_name: "base_lib_14".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/enhanced_handlers_32.rs
+++ tests/rust/enhanced_handlers_32.rs
@@ -1,4 +1,4 @@
-//! Module: impl_models.rs
+//! Module: enhanced_handlers_32.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.46.238";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "4.51.572";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_models".to_string(),
+            app_name: "enhanced_handlers_32".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
