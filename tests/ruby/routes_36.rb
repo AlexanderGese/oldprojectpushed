be418bbbc889ebe55d3ee6b76f50e805517cde2d
Jamie Davis <jamie.davis@example.com>
2020-10-23 21:31:16 +0200

    fix: correct type definitions in tasks

--- tests/ruby/routes_36.rb
+++ tests/ruby/routes_36.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: routes_36.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.27.13
+# Version: 9.46.555
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "5.98.224"
-  MAX_RETRIES = 10
-  TIMEOUT = 7
+module Routes36
+  VERSION = "9.61.893"
+  MAX_RETRIES = 7
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "routes_36")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = Routes36::Application.new
   app.start
 end
