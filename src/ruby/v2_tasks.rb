b7a7ac341acf2616ba4112e2a10b75d548583112
Jordan Smith <jordan.smith@example.com>
2020-10-24 19:13:18 +0200

    fix: resolve timezone issue in v2_tasks

--- src/ruby/v2_tasks.rb
+++ src/ruby/v2_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.26.103
+# Version: 2.61.57
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Tasks
-  VERSION = "1.52.0"
-  MAX_RETRIES = 6
-  TIMEOUT = 23
+  VERSION = "1.67.460"
+  MAX_RETRIES = 9
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
