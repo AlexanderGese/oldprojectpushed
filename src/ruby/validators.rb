8bcaf802376a449362a807fcde948fef4323d4e5
Morgan Lee <morgan.lee@example.com>
2020-10-24 11:14:08 +0200

    docs: add documentation for validators

--- src/ruby/validators.rb
+++ src/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.48.808
+# Version: 5.56.647
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "8.32.632"
-  MAX_RETRIES = 4
-  TIMEOUT = 7
+  VERSION = "2.11.471"
+  MAX_RETRIES = 8
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Validators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
