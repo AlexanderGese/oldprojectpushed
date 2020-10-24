ab1a92abc43479270710d4b1e6f2bb4f0367931a
Casey Williams <casey.williams@example.com>
2020-10-24 22:59:00 +0200

    refactor: optimize impl_repo_28 performance

--- tests/elixir/utils/impl_repo_28.ex
+++ tests/elixir/utils/impl_repo_28.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule Impl_Repo_28 do
   @moduledoc """
-  Module: live.ex
+  Module: impl_repo_28.ex
   Elixir boilerplate - auto-generated
-  Version: 7.97.237
+  Version: 1.14.119
   """
 
-  @version "3.93.374"
-  @max_retries 10
-  @timeout 5647
+  @version "6.72.704"
+  @max_retries 7
+  @timeout 11587
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "impl_repo_28"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule Impl_Repo_28Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule Impl_Repo_28EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
