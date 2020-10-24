cad854ca9a50ef68462e00d78cb037f8d2052615
Morgan Lee <morgan.lee@example.com>
2020-10-24 13:59:48 +0200

    refactor: improve new_config_16 test coverage

--- lib/cplusplus/new_config_16.cpp
+++ lib/cplusplus/new_config_16.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: main.cpp
+ * Module: new_config_16.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.44.669
+ * Version: 3.13.248
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace main {
+namespace new_config_16 {
 
-constexpr const char* VERSION = "7.46.235";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 5327;
+constexpr const char* VERSION = "6.61.70";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 12214;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace main
+} // namespace new_config_16
 
 int main() {
-    using namespace main;
+    using namespace new_config_16;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
