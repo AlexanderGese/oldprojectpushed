f5df8e14ee217182453713da4386052beb13c850
Jordan Smith <jordan.smith@example.com>
2020-10-24 16:55:13 +0200

    feat: add impl_utils module with core functionality

--- tests/rust/impl_utils.rs
+++ tests/rust/impl_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.91.288";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "5.87.766";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
