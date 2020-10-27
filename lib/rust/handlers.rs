8fc258b1ce602c9f8729fe8cfb6e59cb159aba8a
Casey Williams <casey.williams@example.com>
2020-10-27 12:53:15 +0100

    fix: resolve memory leak in handlers

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.14.948";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "1.62.710";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
