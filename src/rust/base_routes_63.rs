6877513d0df6ea9fc8bb6e58403068da5b52c032
Alex Chen <alex.chen@example.com>
2020-10-28 09:54:34 +0100

    fix: handle large payloads in routes

--- src/rust/base_routes_63.rs
+++ src/rust/base_routes_63.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: base_routes_63.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.10.404";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "9.3.33";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "base_routes_63".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/new_config_20.rs
+++ src/rust/new_config_20.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: new_config_20.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.10.404";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "2.56.578";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "new_config_20".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
