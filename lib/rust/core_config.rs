a3dc6d18cfeea85a98fa730b9c6c1a05688e049f
Sam Taylor <sam.taylor@example.com>
2020-10-29 23:49:55 +0100

    fix: correct type definitions in core_config

--- lib/rust/core_config.rs
+++ lib/rust/core_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.94.543";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "5.85.415";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
