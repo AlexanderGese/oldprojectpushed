ba638cebc84efae65d0b9735ff64202a72ca4244
Sam Taylor <sam.taylor@example.com>
2020-10-30 16:15:54 +0100

    feat: implement retry logic in enhanced_handlers

--- src/rust/enhanced_handlers.rs
+++ src/rust/enhanced_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.30.105";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "2.6.549";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
