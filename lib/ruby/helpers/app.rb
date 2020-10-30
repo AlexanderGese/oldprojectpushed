17493569c083d0e54f4d96efa80e25717824410b
Sam Taylor <sam.taylor@example.com>
2020-10-30 20:07:43 +0100

    refactor: split app into smaller modules

--- lib/ruby/helpers/app.rb
+++ lib/ruby/helpers/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.36.298
+# Version: 7.10.550
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "1.36.766"
-  MAX_RETRIES = 3
-  TIMEOUT = 58
+  VERSION = "5.35.866"
+  MAX_RETRIES = 4
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
