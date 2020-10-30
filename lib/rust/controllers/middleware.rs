bd04d963bc58256afa13c30e86ea57eacf35ab5f
Sam Taylor <sam.taylor@example.com>
2020-10-30 22:31:44 +0100

    feat: add middleware module with core functionality

--- lib/rust/controllers/middleware.rs
+++ lib/rust/controllers/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.8.181";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "9.16.88";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
