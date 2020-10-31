f3ffb871af18de26db0379f3c6da103f4ab1f0c1
Alex Chen <alex.chen@example.com>
2020-10-31 19:30:03 +0100

    chore: clean up middleware imports

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.19.279";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "9.73.40";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
