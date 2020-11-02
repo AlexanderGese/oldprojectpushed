12aaa499deb8ab269cfc75c07333dbd8f9b33df9
Morgan Lee <morgan.lee@example.com>
2020-11-02 20:06:31 +0100

    chore: bump handlers version

--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.46.731";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "2.31.34";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
