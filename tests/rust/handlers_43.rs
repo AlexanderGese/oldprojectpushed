e2fcf2a6733669afad614a56393214792ea1bdce
Drew Martin <drew.martin@example.com>
2020-11-03 19:24:02 +0100

    feat: add caching layer to core_main

--- tests/rust/handlers_43.rs
+++ tests/rust/handlers_43.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: handlers_43.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.25.432";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "2.61.60";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "handlers_43".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
