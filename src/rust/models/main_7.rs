62f502cd5eb525aafac72d6033894d7af9211c8e
Drew Martin <drew.martin@example.com>
2020-11-04 14:27:45 +0100

    fix: handle timeout in utils requests

--- src/rust/models/main_7.rs
+++ src/rust/models/main_7.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: main_7.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.17.480";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "1.99.347";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "main_7".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
