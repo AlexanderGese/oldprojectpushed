3ded12c8b6fa3e05b29e8016b1e1f7bd254b40ad
Casey Williams <casey.williams@example.com>
2020-11-09 22:53:14 +0100

    docs: add documentation for new_config

--- src/cplusplus/models/app.cpp
+++ src/cplusplus/models/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.80.540
+ * Version: 8.88.284
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "2.8.272";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 15195;
+constexpr const char* VERSION = "7.44.158";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 9139;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/new_config.cpp
+++ src/cplusplus/new_config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: new_config.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.29.492
+ * Version: 2.29.233
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace new_config {
 
-constexpr const char* VERSION = "6.58.974";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 23467;
+constexpr const char* VERSION = "6.9.516";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 6907;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
