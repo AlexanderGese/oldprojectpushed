8be85ad2b2f9fcb0f85f4c12ddce05df52e454a8
Morgan Lee <morgan.lee@example.com>
2020-11-12 11:17:50 +0100

    fix: handle timeout in base_middleware requests

--- tests/rust/enhanced_errors_49.rs
+++ tests/rust/enhanced_errors_49.rs
@@ -1,4 +1,4 @@
-//! Module: base_middleware.rs
+//! Module: enhanced_errors_49.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.98.653";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "1.92.435";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_middleware".to_string(),
+            app_name: "enhanced_errors_49".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
