1b19a2cced927e16255487babbb73d4a8bbc3309
Morgan Lee <morgan.lee@example.com>
2020-11-13 09:11:09 +0100

    refactor: split app into smaller modules

--- src/cplusplus/base_app_41.cpp
+++ src/cplusplus/base_app_41.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: app.cpp
+ * Module: base_app_41.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.53.162
+ * Version: 5.46.444
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace app {
+namespace base_app_41 {
 
-constexpr const char* VERSION = "3.9.411";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 10341;
+constexpr const char* VERSION = "3.40.882";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 25654;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace app
+} // namespace base_app_41
 
 int main() {
-    using namespace app;
+    using namespace base_app_41;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
