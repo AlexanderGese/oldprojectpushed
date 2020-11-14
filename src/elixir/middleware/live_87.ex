7bd57adad9abee84882219eb565db72d23921f43
Casey Williams <casey.williams@example.com>
2020-11-14 11:36:21 +0100

    docs: add documentation for live_87

--- src/elixir/middleware/live_87.ex
+++ src/elixir/middleware/live_87.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Live_87 do
   @moduledoc """
-  Module: controller.ex
+  Module: live_87.ex
   Elixir boilerplate - auto-generated
-  Version: 5.15.554
+  Version: 2.79.793
   """
 
-  @version "5.46.61"
-  @max_retries 3
-  @timeout 5385
+  @version "2.72.267"
+  @max_retries 4
+  @timeout 15688
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "live_87"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Live_87Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Live_87EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- src/elixir/server_97.ex
+++ src/elixir/server_97.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Server_97 do
   @moduledoc """
-  Module: controller.ex
+  Module: server_97.ex
   Elixir boilerplate - auto-generated
-  Version: 5.15.554
+  Version: 3.85.347
   """
 
-  @version "5.46.61"
-  @max_retries 3
-  @timeout 5385
+  @version "7.72.100"
+  @max_retries 8
+  @timeout 14549
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "server_97"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Server_97Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Server_97EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
