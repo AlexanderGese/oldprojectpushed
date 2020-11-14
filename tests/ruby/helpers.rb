8465ddd7f495505ed0b3456c9e55f86e436d5639
Jamie Davis <jamie.davis@example.com>
2020-11-14 11:13:49 +0100

    fix: correct date formatting in helpers

--- tests/ruby/helpers.rb
+++ tests/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.19.10
+# Version: 5.38.659
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "6.2.135"
-  MAX_RETRIES = 6
+  VERSION = "2.18.879"
+  MAX_RETRIES = 5
   TIMEOUT = 43
 
   class Config
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
