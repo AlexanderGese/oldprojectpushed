0d011ffa9318ea95f1584efd0a2ee6a3116ee41a
Jamie Davis <jamie.davis@example.com>
2020-11-15 10:44:44 +0100

    feat: add pagination to handlers

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.62.710";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "5.94.602";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
