7778721932345989a3e334ceb61ff94e91d34db8
Drew Martin <drew.martin@example.com>
2020-11-15 14:13:52 +0100

    fix: correct type definitions in impl_routes

--- src/ruby/core_tasks_41.rb
+++ src/ruby/core_tasks_41.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_middleware.rb
+# Module: core_tasks_41.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.99.502
+# Version: 2.70.800
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreMiddleware
-  VERSION = "6.88.55"
-  MAX_RETRIES = 6
-  TIMEOUT = 50
+module CoreTasks41
+  VERSION = "4.13.741"
+  MAX_RETRIES = 7
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_middleware")
+      @app_name = opts.fetch(:app_name, "core_tasks_41")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreMiddleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreMiddleware::Application.new
+  app = CoreTasks41::Application.new
   app.start
 end
