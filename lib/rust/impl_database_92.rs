ab025c120811d41dd440729056a078be80237c77
Morgan Lee <morgan.lee@example.com>
2020-11-17 12:37:19 +0100

    fix: correct type definitions in impl_database_92

--- lib/rust/impl_database_92.rs
+++ lib/rust/impl_database_92.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.88.504";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "9.36.593";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
