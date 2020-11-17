8b9c679f2fcbd2f21075921703a8796a963a389e
Sam Taylor <sam.taylor@example.com>
2020-11-17 15:30:15 +0100

    feat: add rate limiting to base_controllers

--- lib/ruby/base_controllers.rb
+++ lib/ruby/base_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: base_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.44.108
+# Version: 6.8.187
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseControllers
-  VERSION = "5.9.490"
-  MAX_RETRIES = 7
-  TIMEOUT = 38
+  VERSION = "3.23.545"
+  MAX_RETRIES = 3
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseControllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
--- lib/ruby/server.rb
+++ lib/ruby/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.52.278
+# Version: 4.28.538
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "7.3.765"
-  MAX_RETRIES = 9
-  TIMEOUT = 37
+  VERSION = "2.27.136"
+  MAX_RETRIES = 3
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
