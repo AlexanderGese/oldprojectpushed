43a10a4e4008ca7e0851eb62c70ae20e95ddf499
Drew Martin <drew.martin@example.com>
2020-11-18 20:12:35 +0100

    chore: add linting rules for validators

--- src/ruby/validators.rb
+++ src/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.55.250
+# Version: 8.47.104
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "3.28.92"
+  VERSION = "4.38.759"
   MAX_RETRIES = 4
-  TIMEOUT = 26
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
