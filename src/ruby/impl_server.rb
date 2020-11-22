0975a47cec60b0484d5553d051537574e6580418
Casey Williams <casey.williams@example.com>
2020-11-22 22:24:30 +0100

    feat: add rate limiting to new_tasks

--- src/ruby/impl_server.rb
+++ src/ruby/impl_server.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.12.252
+# Version: 9.27.301
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplServer
-  VERSION = "3.5.506"
-  MAX_RETRIES = 10
-  TIMEOUT = 31
+  VERSION = "9.49.348"
+  MAX_RETRIES = 9
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
