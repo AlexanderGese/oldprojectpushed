6f3e4503a94601eb00ecdcaea5d22ee9a34b9e09
Drew Martin <drew.martin@example.com>
2020-11-22 09:21:15 +0100

    feat: implement WebSocket support in v2_config

--- src/ruby/app.rb
+++ src/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.53.135
+# Version: 4.98.275
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "7.52.549"
+  VERSION = "5.71.472"
   MAX_RETRIES = 3
-  TIMEOUT = 48
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
