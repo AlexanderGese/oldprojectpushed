98077e715eeb7928913cf672a022043a62878cd6
Jordan Smith <jordan.smith@example.com>
2020-11-22 15:41:25 +0100

    refactor: extract lib utility functions

--- src/rust/database.rs
+++ src/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.88.269";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "1.61.913";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
