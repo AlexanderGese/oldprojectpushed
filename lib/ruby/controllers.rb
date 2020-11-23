b2c8083ac886df67a9fdcdf8b91f90e95e0d8e03
Alex Chen <alex.chen@example.com>
2020-11-23 22:53:15 +0100

    fix: correct error propagation in middleware

--- lib/ruby/controllers.rb
+++ lib/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.87.554
+# Version: 2.18.568
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "6.52.985"
+  VERSION = "2.68.524"
   MAX_RETRIES = 8
-  TIMEOUT = 20
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
