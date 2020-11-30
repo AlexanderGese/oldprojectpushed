85d0b69fe2f3c2031aa063cf69c28e8c02a9ca79
Alex Chen <alex.chen@example.com>
2020-11-30 21:36:40 +0100

    refactor: improve routes_91 error messages

--- src/ruby/handlers/routes_91.rb
+++ src/ruby/handlers/routes_91.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: routes_91.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.95.277
+# Version: 5.2.167
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "4.41.828"
-  MAX_RETRIES = 9
-  TIMEOUT = 12
+module Routes91
+  VERSION = "7.6.443"
+  MAX_RETRIES = 7
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "routes_91")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = Routes91::Application.new
   app.start
 end
