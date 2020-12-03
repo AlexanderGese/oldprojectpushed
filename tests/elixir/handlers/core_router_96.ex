ac76d2af30caf87365a81ea385d271a5da518dc5
Riley Johnson <riley.johnson@example.com>
2020-12-03 13:47:56 +0100

    fix: handle empty response in application

--- tests/elixir/handlers/core_router_96.ex
+++ tests/elixir/handlers/core_router_96.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule Core_Router_96 do
   @moduledoc """
-  Module: application.ex
+  Module: core_router_96.ex
   Elixir boilerplate - auto-generated
-  Version: 8.45.138
+  Version: 2.23.288
   """
 
-  @version "2.52.379"
-  @max_retries 10
-  @timeout 23177
+  @version "1.35.804"
+  @max_retries 3
+  @timeout 26222
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "core_router_96"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule Core_Router_96Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule Core_Router_96EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
