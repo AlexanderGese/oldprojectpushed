96d64453474ac8284ee7cd05d02a49e7c670df6b
Morgan Lee <morgan.lee@example.com>
2020-12-05 10:05:53 +0100

    fix: resolve edge case in v2_routes_71 handler

--- tests/rust/base_routes_47.rs
+++ tests/rust/base_routes_47.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: base_routes_47.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.97.318";
+const VERSION: &str = "7.50.333";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 24;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "base_routes_47".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.31.183";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "9.58.814";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
