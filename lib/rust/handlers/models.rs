0207221fb38b3c5fff40e94d991ef093911ef78d
Sam Taylor <sam.taylor@example.com>
2020-12-08 14:13:06 +0100

    refactor: split core_models_45 into smaller modules

--- lib/rust/handlers/models.rs
+++ lib/rust/handlers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.47.685";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "5.26.515";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
