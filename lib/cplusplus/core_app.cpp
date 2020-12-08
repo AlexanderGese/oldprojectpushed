0c03fb8f026bda7f98438842ccf0ae4d52bcb3cd
Casey Williams <casey.williams@example.com>
2020-12-08 18:29:47 +0100

    refactor: improve core_app test coverage

--- lib/cplusplus/core_app.cpp
+++ lib/cplusplus/core_app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_app.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.50.226
+ * Version: 1.47.740
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_app {
 
-constexpr const char* VERSION = "9.21.896";
+constexpr const char* VERSION = "1.8.520";
 constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 18364;
+constexpr int TIMEOUT_MS = 21506;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
