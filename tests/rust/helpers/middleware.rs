ead50a775de9cc6abf66ec0ea3d81b0f2093aa5c
Jordan Smith <jordan.smith@example.com>
2020-12-09 17:28:05 +0100

    refactor: optimize middleware performance

--- tests/rust/helpers/middleware.rs
+++ tests/rust/helpers/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.31.452";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "4.49.325";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
