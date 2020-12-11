06d79a9e3e511164e87124a15a537be4f1daa837
Riley Johnson <riley.johnson@example.com>
2020-12-11 14:37:11 +0100

    chore: add linting rules for enhanced_models_20

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.73.40";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "9.23.974";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
