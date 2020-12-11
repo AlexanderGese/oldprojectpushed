29cb2198560732b603d3312c7f84119ffda0f447
Jamie Davis <jamie.davis@example.com>
2020-12-11 19:56:32 +0100

    feat: add caching layer to new_config

--- lib/rust/models.rs
+++ lib/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.60.453";
+const VERSION: &str = "2.14.108";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 9;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/models/routes.rs
+++ lib/rust/models/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.84.286";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "7.15.250";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
