f3d33dcfc36565ab564ddfa33c613b3ab3c830d5
Jordan Smith <jordan.smith@example.com>
2020-12-12 10:04:27 +0100

    docs: add documentation for new_middleware

--- lib/rust/handlers/new_middleware.rs
+++ lib/rust/handlers/new_middleware.rs
@@ -1,4 +1,4 @@
-//! Module: v2_database.rs
+//! Module: new_middleware.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.39.920";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "1.4.531";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_database".to_string(),
+            app_name: "new_middleware".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
