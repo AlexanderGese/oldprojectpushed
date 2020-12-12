7ea0985596b4c0724b276c403e868a6f1d89917d
Alex Chen <alex.chen@example.com>
2020-12-12 21:57:37 +0100

    fix: resolve encoding issue in models

--- src/ruby/models.rb
+++ src/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.95.277
+# Version: 7.0.153
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "4.41.828"
-  MAX_RETRIES = 9
-  TIMEOUT = 12
+  VERSION = "7.32.379"
+  MAX_RETRIES = 5
+  TIMEOUT = 16
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
