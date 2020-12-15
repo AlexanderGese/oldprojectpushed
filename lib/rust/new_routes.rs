a57af3504239a1f91c9f750df98b6745d0af5398
Jordan Smith <jordan.smith@example.com>
2020-12-15 13:18:04 +0100

    feat: add export functionality to new_routes

--- lib/rust/new_routes.rs
+++ lib/rust/new_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.95.757";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "3.41.752";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
