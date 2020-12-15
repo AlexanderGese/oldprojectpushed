053a09cbc06b5885cfad858a61b5d7b28f7828b5
Drew Martin <drew.martin@example.com>
2020-12-15 17:32:04 +0100

    refactor: extract enhanced_errors utility functions

--- lib/rust/enhanced_errors.rs
+++ lib/rust/enhanced_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.96.416";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "9.67.639";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
