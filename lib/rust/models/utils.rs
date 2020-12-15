f6f08bc86caa63a9764304ed49b6b19a9fb4c05e
Jamie Davis <jamie.davis@example.com>
2020-12-15 21:08:16 +0100

    feat: add caching layer to utils

--- lib/rust/models/utils.rs
+++ lib/rust/models/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.1.7";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "6.9.854";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
