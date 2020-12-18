32acb012e67ea6993f6cb1cddeb58b6fc3afa44b
Sam Taylor <sam.taylor@example.com>
2020-12-18 08:52:41 +0100

    feat: add export functionality to errors

--- tests/rust/controllers/routes.rs
+++ tests/rust/controllers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.43.942";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "9.18.684";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
