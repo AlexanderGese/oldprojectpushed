a87aee2ae30f13b24f9ad3369e5b7d64e9ed9d08
Jordan Smith <jordan.smith@example.com>
2020-12-20 10:22:44 +0100

    fix: resolve memory leak in impl_validators

--- lib/ruby/utils/app.rb
+++ lib/ruby/utils/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.86.531
+# Version: 9.19.201
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "7.48.55"
-  MAX_RETRIES = 9
-  TIMEOUT = 19
+  VERSION = "2.82.552"
+  MAX_RETRIES = 6
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
