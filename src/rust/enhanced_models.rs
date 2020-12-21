6087c105c033d64b1d962b868366da210f002ae5
Riley Johnson <riley.johnson@example.com>
2020-12-21 09:00:39 +0100

    feat: add export functionality to new_routes_59

--- src/rust/enhanced_models.rs
+++ src/rust/enhanced_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.27.224";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "8.48.977";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils/models.rs
+++ src/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.62.352";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "4.13.963";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
