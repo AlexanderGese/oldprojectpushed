17b31bd9f82a060c2188432cf29493a7cfcb90b7
Alex Chen <alex.chen@example.com>
2020-12-21 18:21:35 +0100

    feat: add middleware support to impl_handlers

--- lib/rust/impl_handlers.rs
+++ lib/rust/impl_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.72.877";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "6.96.369";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
