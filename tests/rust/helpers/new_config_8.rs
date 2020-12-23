2fcecca71c488495d6e78ade5e028071aba61859
Sam Taylor <sam.taylor@example.com>
2020-12-23 13:35:46 +0100

    chore: update base_routes_31 dependencies

--- tests/rust/helpers/new_config_8.rs
+++ tests/rust/helpers/new_config_8.rs
@@ -1,4 +1,4 @@
-//! Module: new_main.rs
+//! Module: new_config_8.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.20.742";
-const MAX_RETRIES: u32 = 8;
+const VERSION: &str = "3.84.531";
+const MAX_RETRIES: u32 = 9;
 const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_main".to_string(),
+            app_name: "new_config_8".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
