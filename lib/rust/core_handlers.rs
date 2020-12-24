e2e52add6243725d063173a1195278e93d8e5026
Sam Taylor <sam.taylor@example.com>
2020-12-24 17:51:26 +0100

    refactor: simplify core_handlers state management

--- lib/rust/core_handlers.rs
+++ lib/rust/core_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.1.346";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "2.31.306";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
