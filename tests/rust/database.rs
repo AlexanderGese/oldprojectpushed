39a78966d123496bdea6dacd0bdd16dcf9c2573c
Sam Taylor <sam.taylor@example.com>
2020-12-24 09:18:05 +0100

    fix: handle timeout in core_config_6 requests

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.98.588";
+const VERSION: &str = "9.7.700";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 53;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
