68b925e5606b0599e0d26096b810a53144e958a3
Sam Taylor <sam.taylor@example.com>
2020-12-30 12:18:17 +0100

    fix: resolve memory leak in models

--- src/rust/helpers/main.rs
+++ src/rust/helpers/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.45.394";
+const VERSION: &str = "1.12.0";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 48;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/v2_models_27.rs
+++ src/rust/v2_models_27.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: v2_models_27.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.8.639";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "9.8.206";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "v2_models_27".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
