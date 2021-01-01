c0a17af861d7f4d5e9f7a6926c9b8daff45a10e3
Alex Chen <alex.chen@example.com>
2021-01-01 23:35:44 +0100

    fix: handle null check in v2_config_58

--- src/ruby/helpers/v2_config_58.rb
+++ src/ruby/helpers/v2_config_58.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_server.rb
+# Module: v2_config_58.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.45.977
+# Version: 1.68.435
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplServer
-  VERSION = "6.14.153"
-  MAX_RETRIES = 9
-  TIMEOUT = 33
+module V2Config58
+  VERSION = "4.37.704"
+  MAX_RETRIES = 8
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_server")
+      @app_name = opts.fetch(:app_name, "v2_config_58")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplServer
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplServer::Application.new
+  app = V2Config58::Application.new
   app.start
 end
--- src/ruby/validators_76.rb
+++ src/ruby/validators_76.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_server.rb
+# Module: validators_76.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.45.977
+# Version: 5.52.794
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplServer
-  VERSION = "6.14.153"
-  MAX_RETRIES = 9
-  TIMEOUT = 33
+module Validators76
+  VERSION = "3.62.85"
+  MAX_RETRIES = 6
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_server")
+      @app_name = opts.fetch(:app_name, "validators_76")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module ImplServer
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplServer::Application.new
+  app = Validators76::Application.new
   app.start
 end
