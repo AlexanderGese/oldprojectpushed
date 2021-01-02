21bfbb42734a4341bc2f3a6cf1c65f9f5411cbb7
Morgan Lee <morgan.lee@example.com>
2021-01-02 11:31:14 +0100

    fix: correct CORS headers in new_main

--- lib/rust/handlers/routes.rs
+++ lib/rust/handlers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.80.262";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "1.55.555";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/new_main.rs
+++ lib/rust/new_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.52.204";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "9.16.632";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
