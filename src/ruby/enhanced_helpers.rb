af481512f295777ef479112164c20a67f034863a
Casey Williams <casey.williams@example.com>
2021-01-03 17:24:46 +0100

    chore: remove deprecated enhanced_helpers code

--- src/ruby/enhanced_helpers.rb
+++ src/ruby/enhanced_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: enhanced_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.61.4
+# Version: 8.70.967
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module EnhancedHelpers
-  VERSION = "7.69.852"
-  MAX_RETRIES = 6
-  TIMEOUT = 37
+  VERSION = "1.37.569"
+  MAX_RETRIES = 10
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module EnhancedHelpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
