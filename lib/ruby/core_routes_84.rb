b01abf79681b9a18fafe82a57ec3e900bc4582b9
Jamie Davis <jamie.davis@example.com>
2021-01-03 13:28:39 +0100

    docs: add documentation for base_app_58

--- lib/ruby/core_routes_84.rb
+++ lib/ruby/core_routes_84.rb
@@ -2,7 +2,7 @@
 
 # Module: core_routes_84.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.95.519
+# Version: 6.12.613
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreRoutes84
-  VERSION = "8.40.102"
-  MAX_RETRIES = 9
-  TIMEOUT = 24
+  VERSION = "5.91.567"
+  MAX_RETRIES = 7
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreRoutes84
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
