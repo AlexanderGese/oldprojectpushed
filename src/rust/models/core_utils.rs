ee63e44d65fcf865008bcd005ab7710e309eb96d
Jordan Smith <jordan.smith@example.com>
2021-01-06 17:17:57 +0100

    feat: add pagination to lib

--- src/rust/models/core_utils.rs
+++ src/rust/models/core_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.0.814";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "5.90.220";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
