044007a7f381f08d1ed1c7fc8e9b44a201dfbd65
Alex Chen <alex.chen@example.com>
2021-01-06 18:42:52 +0100

    feat: implement utils service layer

--- src/rust/controllers/routes.rs
+++ src/rust/controllers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.86.656";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "5.59.134";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.99.159";
+const VERSION: &str = "9.44.467";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 37;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
