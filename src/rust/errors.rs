85943fa44eaaa5b25adf09a8f75082c3742b1f2e
Morgan Lee <morgan.lee@example.com>
2021-01-07 22:53:53 +0100

    feat: add middleware support to middleware

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.74.965";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "6.9.744";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/routes_3.rs
+++ src/rust/routes_3.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: routes_3.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.36.141";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "7.7.398";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "routes_3".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
