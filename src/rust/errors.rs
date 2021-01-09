5f76637a8aaed242204d6914c540aa518d14511a
Riley Johnson <riley.johnson@example.com>
2021-01-09 17:16:31 +0100

    refactor: split errors into smaller modules

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.9.744";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "7.99.551";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
