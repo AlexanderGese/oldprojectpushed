dac83fdc11dcc7f6800f55d929b8d82641ba3a71
Sam Taylor <sam.taylor@example.com>
2021-01-09 12:09:07 +0100

    chore: bump config version

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.0.537";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "8.19.104";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
