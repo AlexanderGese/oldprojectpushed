303d95266122b6e3a88da41b12c95b455dbc5bad
Jamie Davis <jamie.davis@example.com>
2021-01-10 11:59:38 +0100

    feat: add batch processing to core_helpers

--- src/ruby/handlers/core_models_92.rb
+++ src/ruby/handlers/core_models_92.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_helpers.rb
+# Module: core_models_92.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.96.673
+# Version: 7.39.162
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreHelpers
-  VERSION = "5.27.922"
-  MAX_RETRIES = 4
-  TIMEOUT = 5
+module CoreModels92
+  VERSION = "1.26.638"
+  MAX_RETRIES = 8
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_helpers")
+      @app_name = opts.fetch(:app_name, "core_models_92")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreHelpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreHelpers::Application.new
+  app = CoreModels92::Application.new
   app.start
 end
