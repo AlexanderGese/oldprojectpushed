aab71271da86898428639009cfd5744af14c68c0
Jordan Smith <jordan.smith@example.com>
2021-01-11 22:55:20 +0100

    refactor: improve impl_logger test coverage

--- tests/cplusplus/impl_logger.cpp
+++ tests/cplusplus/impl_logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: impl_logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.26.275
+ * Version: 9.63.252
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace impl_logger {
 
-constexpr const char* VERSION = "9.24.881";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 11080;
+constexpr const char* VERSION = "5.98.357";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 16393;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
