94e71b4a9654f35216e35d284a7da5695bc0b128
Riley Johnson <riley.johnson@example.com>
2021-01-14 15:23:00 +0100

    fix: handle large payloads in handlers

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.12.565";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "4.8.995";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
