5ba9f96277c2b9f8e0ea514a0cf31c944028d95a
Jamie Davis <jamie.davis@example.com>
2021-01-14 22:38:19 +0100

    chore: bump schema version

--- tests/elixir/new_application_10.ex
+++ tests/elixir/new_application_10.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule New_Application_10 do
   @moduledoc """
-  Module: schema.ex
+  Module: new_application_10.ex
   Elixir boilerplate - auto-generated
-  Version: 3.19.465
+  Version: 9.61.207
   """
 
-  @version "7.57.29"
+  @version "6.84.263"
   @max_retries 5
-  @timeout 10801
+  @timeout 23553
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "new_application_10"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule New_Application_10Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule New_Application_10EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
