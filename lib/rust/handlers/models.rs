279d436fcc6800595f1422db11f7cd8839b30dba
Morgan Lee <morgan.lee@example.com>
2021-01-15 09:28:01 +0100

    refactor: extract core_middleware utility functions

--- lib/rust/handlers/models.rs
+++ lib/rust/handlers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.26.515";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "9.70.32";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
