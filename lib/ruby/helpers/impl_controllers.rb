d0a6687789fd62f93ccc69d250d1b115f9fc8000
Jamie Davis <jamie.davis@example.com>
2021-01-17 08:29:58 +0100

    fix: correct error propagation in base_models_62

--- lib/ruby/helpers/impl_controllers.rb
+++ lib/ruby/helpers/impl_controllers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: impl_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.63.222
+# Version: 2.75.512
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "6.46.936"
-  MAX_RETRIES = 8
-  TIMEOUT = 50
+module ImplControllers
+  VERSION = "7.1.922"
+  MAX_RETRIES = 7
+  TIMEOUT = 16
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "impl_controllers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = ImplControllers::Application.new
   app.start
 end
--- lib/ruby/middleware/base_models_62.rb
+++ lib/ruby/middleware/base_models_62.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: base_models_62.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.63.222
+# Version: 2.68.415
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "6.46.936"
-  MAX_RETRIES = 8
-  TIMEOUT = 50
+module BaseModels62
+  VERSION = "9.31.910"
+  MAX_RETRIES = 5
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "base_models_62")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = BaseModels62::Application.new
   app.start
 end
