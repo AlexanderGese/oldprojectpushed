5bd51ad91726f802ca0176e944fff655ee04b163
Drew Martin <drew.martin@example.com>
2021-01-18 19:25:50 +0100

    refactor: improve routes test coverage

--- tests/rust/handlers/routes.rs
+++ tests/rust/handlers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.65.37";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "3.88.789";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.58.814";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "3.45.713";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
