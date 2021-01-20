eab251af0223ab69ce6d7600c9f5dca01adc4f39
Riley Johnson <riley.johnson@example.com>
2021-01-20 16:47:33 +0100

    fix: handle empty response in main

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.83.198";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "4.35.669";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
