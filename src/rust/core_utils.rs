eca918878fa51cf52ba0c61190bc9c944468861f
Drew Martin <drew.martin@example.com>
2021-01-22 11:40:30 +0100

    feat: add error handling to core_utils

--- src/rust/core_utils.rs
+++ src/rust/core_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.76.372";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "7.70.916";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
