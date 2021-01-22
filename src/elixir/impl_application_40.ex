cffb4644a7676ec844dd60a10949e022153b78e7
Sam Taylor <sam.taylor@example.com>
2021-01-22 12:55:05 +0100

    fix: patch security vulnerability in impl_schema

--- src/elixir/impl_application_40.ex
+++ src/elixir/impl_application_40.ex
@@ -1,13 +1,13 @@
-defmodule Impl_Schema do
+defmodule Impl_Application_40 do
   @moduledoc """
-  Module: impl_schema.ex
+  Module: impl_application_40.ex
   Elixir boilerplate - auto-generated
-  Version: 7.16.346
+  Version: 4.94.440
   """
 
-  @version "7.8.658"
-  @max_retries 4
-  @timeout 17193
+  @version "3.77.387"
+  @max_retries 3
+  @timeout 9330
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Impl_Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "impl_schema"),
+      app_name: Keyword.get(opts, :app_name, "impl_application_40"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Impl_Schema do
   end
 end
 
-defmodule Impl_SchemaStore do
+defmodule Impl_Application_40Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Impl_SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Impl_SchemaEventBus do
+defmodule Impl_Application_40EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
