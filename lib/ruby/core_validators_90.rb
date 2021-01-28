be5602c5ba32b7b25ee677516c4fbdc23a8511e2
Jordan Smith <jordan.smith@example.com>
2021-01-28 20:14:09 +0100

    fix: resolve edge case in core_validators_90 handler

--- lib/ruby/core_validators_90.rb
+++ lib/ruby/core_validators_90.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: core_validators_90.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.79.985
+# Version: 6.86.768
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "4.40.574"
-  MAX_RETRIES = 5
-  TIMEOUT = 16
+module CoreValidators90
+  VERSION = "7.23.701"
+  MAX_RETRIES = 10
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "core_validators_90")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = CoreValidators90::Application.new
   app.start
 end
