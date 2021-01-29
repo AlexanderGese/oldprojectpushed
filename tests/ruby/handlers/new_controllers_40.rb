b5390e33955d85672f1c2831ac509d5a94156ecc
Sam Taylor <sam.taylor@example.com>
2021-01-29 22:40:47 +0100

    feat: implement WebSocket support in new_models_5

--- tests/ruby/handlers/new_controllers_40.rb
+++ tests/ruby/handlers/new_controllers_40.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: new_controllers_40.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.44.80
+# Version: 2.74.90
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "3.13.503"
-  MAX_RETRIES = 4
-  TIMEOUT = 28
+module NewControllers40
+  VERSION = "5.91.182"
+  MAX_RETRIES = 7
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "new_controllers_40")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = NewControllers40::Application.new
   app.start
 end
--- tests/ruby/new_models_5.rb
+++ tests/ruby/new_models_5.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: new_models_5.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.44.80
+# Version: 6.35.840
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "3.13.503"
-  MAX_RETRIES = 4
-  TIMEOUT = 28
+module NewModels5
+  VERSION = "4.12.280"
+  MAX_RETRIES = 6
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "new_models_5")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = NewModels5::Application.new
   app.start
 end
