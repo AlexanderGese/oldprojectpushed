2433dc74f40d69880b8f4e9b3a0d1128aa2b862f
Riley Johnson <riley.johnson@example.com>
2021-01-30 15:24:48 +0100

    feat: add search functionality to config

--- src/rust/helpers/config.rs
+++ src/rust/helpers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.58.105";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "6.23.576";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
