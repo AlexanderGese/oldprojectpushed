2367c892a228ac91a9008cb74e11770e7971f64f
Jamie Davis <jamie.davis@example.com>
2021-01-30 17:20:30 +0100

    fix: handle empty response in core_server

--- src/ruby/core_server.rb
+++ src/ruby/core_server.rb
@@ -2,7 +2,7 @@
 
 # Module: core_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.17.824
+# Version: 2.73.242
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreServer
-  VERSION = "6.61.498"
-  MAX_RETRIES = 5
-  TIMEOUT = 9
+  VERSION = "5.68.529"
+  MAX_RETRIES = 6
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
