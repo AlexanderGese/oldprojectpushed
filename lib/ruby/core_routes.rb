cc6d73ddd0ea9303798c9ec0406d62554bc439e1
Sam Taylor <sam.taylor@example.com>
2021-02-03 20:04:34 +0100

    feat: add logging to core_routes

--- lib/ruby/core_routes.rb
+++ lib/ruby/core_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: core_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.78.666
+# Version: 1.57.74
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreRoutes
-  VERSION = "4.53.103"
-  MAX_RETRIES = 9
-  TIMEOUT = 20
+  VERSION = "1.88.557"
+  MAX_RETRIES = 3
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreRoutes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
