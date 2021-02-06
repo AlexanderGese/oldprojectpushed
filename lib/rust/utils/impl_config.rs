cfec0fc9c82ffea2b9589551bbfd8b479a7e9569
Morgan Lee <morgan.lee@example.com>
2021-02-06 21:56:23 +0100

    fix: handle null check in utils

--- lib/rust/utils/impl_config.rs
+++ lib/rust/utils/impl_config.rs
@@ -1,4 +1,4 @@
-//! Module: v2_routes.rs
+//! Module: impl_config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.65.727";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "9.10.707";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_routes".to_string(),
+            app_name: "impl_config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
