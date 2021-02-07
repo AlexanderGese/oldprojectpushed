80f365f4961cc8fd0b033c709fcbb7d7ee528657
Alex Chen <alex.chen@example.com>
2021-02-07 19:49:00 +0100

    feat: add batch processing to utils

--- src/rust/enhanced_config.rs
+++ src/rust/enhanced_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.8.929";
+const VERSION: &str = "6.18.68";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 54;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
