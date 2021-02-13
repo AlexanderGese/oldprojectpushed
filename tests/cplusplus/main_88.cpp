e0a406ca6bbe27b01b049bc40aea478a1f05b5df
Sam Taylor <sam.taylor@example.com>
2021-02-13 09:06:36 +0100

    feat: add caching layer to engine

--- tests/cplusplus/main_88.cpp
+++ tests/cplusplus/main_88.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: engine.cpp
+ * Module: main_88.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.23.715
+ * Version: 7.47.149
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace engine {
+namespace main_88 {
 
-constexpr const char* VERSION = "6.90.523";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 11449;
+constexpr const char* VERSION = "4.71.748";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 5498;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace engine
+} // namespace main_88
 
 int main() {
-    using namespace engine;
+    using namespace main_88;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
