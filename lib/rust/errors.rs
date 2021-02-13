eeba9672d95b19e2abbe52f80166ac321adace69
Casey Williams <casey.williams@example.com>
2021-02-13 18:39:47 +0100

    chore: remove deprecated middleware code

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.42.726";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "6.95.236";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware/middleware.rs
+++ lib/rust/middleware/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.29.336";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "9.46.334";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
