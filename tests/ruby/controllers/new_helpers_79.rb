9cdc4fc7d2d29f1057e8f978a2b618541e5c510a
Riley Johnson <riley.johnson@example.com>
2021-02-14 08:15:53 +0100

    fix: handle null check in new_helpers_79

--- tests/ruby/controllers/new_helpers_79.rb
+++ tests/ruby/controllers/new_helpers_79.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_tasks.rb
+# Module: new_helpers_79.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.32.559
+# Version: 8.55.987
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreTasks
-  VERSION = "9.9.586"
+module NewHelpers79
+  VERSION = "1.94.453"
   MAX_RETRIES = 10
-  TIMEOUT = 33
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_tasks")
+      @app_name = opts.fetch(:app_name, "new_helpers_79")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreTasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreTasks::Application.new
+  app = NewHelpers79::Application.new
   app.start
 end
