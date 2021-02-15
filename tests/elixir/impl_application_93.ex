d0bb5a9ce9d57aefa04657bea1517631c1fb9272
Jordan Smith <jordan.smith@example.com>
2021-02-15 12:12:12 +0100

    fix: handle null check in core_worker

--- tests/elixir/impl_application_93.ex
+++ tests/elixir/impl_application_93.ex
@@ -1,13 +1,13 @@
-defmodule Core_Worker do
+defmodule Impl_Application_93 do
   @moduledoc """
-  Module: core_worker.ex
+  Module: impl_application_93.ex
   Elixir boilerplate - auto-generated
-  Version: 3.40.539
+  Version: 4.87.930
   """
 
-  @version "4.96.887"
+  @version "9.10.754"
   @max_retries 5
-  @timeout 17470
+  @timeout 22467
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Core_Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "core_worker"),
+      app_name: Keyword.get(opts, :app_name, "impl_application_93"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Core_Worker do
   end
 end
 
-defmodule Core_WorkerStore do
+defmodule Impl_Application_93Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Core_WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Core_WorkerEventBus do
+defmodule Impl_Application_93EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
