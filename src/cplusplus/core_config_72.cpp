03d06a69ad51f2d8fdc7f373935939a1645e2512
Alex Chen <alex.chen@example.com>
2021-02-15 19:19:07 +0100

    fix: resolve timezone issue in factory

--- src/cplusplus/core_config_72.cpp
+++ src/cplusplus/core_config_72.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: config.cpp
+ * Module: core_config_72.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.71.554
+ * Version: 2.31.91
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace config {
+namespace core_config_72 {
 
-constexpr const char* VERSION = "4.23.147";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 9617;
+constexpr const char* VERSION = "7.11.133";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 16297;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace config
+} // namespace core_config_72
 
 int main() {
-    using namespace config;
+    using namespace core_config_72;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/middleware/factory.cpp
+++ src/cplusplus/middleware/factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.34.512
+ * Version: 1.14.203
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace factory {
 
-constexpr const char* VERSION = "9.10.616";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 5488;
+constexpr const char* VERSION = "1.52.870";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 27364;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
