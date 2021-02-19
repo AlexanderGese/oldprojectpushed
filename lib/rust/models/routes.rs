01ec84a17c1c695810010a21ab54c1354443b849
Drew Martin <drew.martin@example.com>
2021-02-19 22:46:33 +0100

    feat: add caching layer to routes

--- lib/rust/models/routes.rs
+++ lib/rust/models/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.15.250";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "4.18.673";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
