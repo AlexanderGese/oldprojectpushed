90a69158370c04788d91e7615f51c2bb7e789464
Sam Taylor <sam.taylor@example.com>
2021-02-20 11:12:52 +0100

    feat: add routes module with core functionality

--- tests/rust/handlers/routes.rs
+++ tests/rust/handlers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.88.789";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "8.48.519";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
