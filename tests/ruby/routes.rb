f68240d2069c968e823b95db85b7756a2bd34fad
Jamie Davis <jamie.davis@example.com>
2021-02-21 23:43:50 +0100

    fix: patch security vulnerability in v2_app

--- tests/ruby/routes.rb
+++ tests/ruby/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.78.921
+# Version: 8.55.339
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "5.93.221"
-  MAX_RETRIES = 5
-  TIMEOUT = 41
+  VERSION = "5.16.198"
+  MAX_RETRIES = 3
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
