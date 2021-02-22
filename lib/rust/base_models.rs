62dff87e791cb6fcfe703b645533d0db7e928619
Jordan Smith <jordan.smith@example.com>
2021-02-22 11:56:41 +0100

    refactor: improve base_models test coverage

--- lib/rust/base_models.rs
+++ lib/rust/base_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.59.232";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "1.87.445";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/new_errors_59.rs
+++ lib/rust/new_errors_59.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: new_errors_59.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.4.663";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "3.35.95";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "new_errors_59".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
