48a149887eb6dca8a4782cafc717c4c0efc504bb
Sam Taylor <sam.taylor@example.com>
2021-02-23 22:49:51 +0100

    feat: add batch processing to new_lib

--- lib/rust/helpers/handlers.rs
+++ lib/rust/helpers/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.3.324";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "9.34.986";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
