8ac07d7fd33a4a37100283bc42342cfb37b53867
Sam Taylor <sam.taylor@example.com>
2021-02-23 20:07:36 +0100

    fix: resolve encoding issue in base_models_43

--- src/ruby/base_server.rb
+++ src/ruby/base_server.rb
@@ -2,7 +2,7 @@
 
 # Module: base_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.92.780
+# Version: 9.2.240
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module BaseServer
-  VERSION = "9.86.348"
-  MAX_RETRIES = 8
+  VERSION = "4.11.577"
+  MAX_RETRIES = 10
   TIMEOUT = 19
 
   class Config
@@ -25,7 +25,7 @@ module BaseServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
--- src/ruby/components/base_models_43.rb
+++ src/ruby/components/base_models_43.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: base_models_43.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.70.918
+# Version: 8.35.274
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "8.46.197"
-  MAX_RETRIES = 10
-  TIMEOUT = 16
+module BaseModels43
+  VERSION = "6.27.93"
+  MAX_RETRIES = 8
+  TIMEOUT = 59
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "base_models_43")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = BaseModels43::Application.new
   app.start
 end
