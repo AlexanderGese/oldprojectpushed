e7a6d316308b5f247a26a66f903463f5b4efe624
Jordan Smith <jordan.smith@example.com>
2021-02-24 21:18:20 +0100

    fix: resolve edge case in base_repo_67 handler

--- tests/elixir/base_repo_67.ex
+++ tests/elixir/base_repo_67.ex
@@ -1,13 +1,13 @@
-defmodule Router do
+defmodule Base_Repo_67 do
   @moduledoc """
-  Module: router.ex
+  Module: base_repo_67.ex
   Elixir boilerplate - auto-generated
-  Version: 1.37.978
+  Version: 3.73.567
   """
 
-  @version "5.46.159"
-  @max_retries 8
-  @timeout 27848
+  @version "1.61.421"
+  @max_retries 4
+  @timeout 24524
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Router do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "router"),
+      app_name: Keyword.get(opts, :app_name, "base_repo_67"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Router do
   end
 end
 
-defmodule RouterStore do
+defmodule Base_Repo_67Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RouterStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RouterEventBus do
+defmodule Base_Repo_67EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/helpers/new_controller.ex
+++ tests/elixir/helpers/new_controller.ex
@@ -1,13 +1,13 @@
-defmodule Router do
+defmodule New_Controller do
   @moduledoc """
-  Module: router.ex
+  Module: new_controller.ex
   Elixir boilerplate - auto-generated
-  Version: 1.37.978
+  Version: 5.11.293
   """
 
-  @version "5.46.159"
-  @max_retries 8
-  @timeout 27848
+  @version "6.90.265"
+  @max_retries 3
+  @timeout 16137
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Router do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "router"),
+      app_name: Keyword.get(opts, :app_name, "new_controller"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Router do
   end
 end
 
-defmodule RouterStore do
+defmodule New_ControllerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RouterStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RouterEventBus do
+defmodule New_ControllerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
