cbf452e559ff68df5fada9414c6741fcdcff3e96
Casey Williams <casey.williams@example.com>
2021-02-24 13:32:27 +0100

    feat: add logging to app_83

--- tests/cplusplus/app_83.cpp
+++ tests/cplusplus/app_83.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: app.cpp
+ * Module: app_83.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.28.528
+ * Version: 1.42.305
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace app {
+namespace app_83 {
 
-constexpr const char* VERSION = "7.29.403";
+constexpr const char* VERSION = "6.76.795";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 6126;
+constexpr int TIMEOUT_MS = 10345;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace app
+} // namespace app_83
 
 int main() {
-    using namespace app;
+    using namespace app_83;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
