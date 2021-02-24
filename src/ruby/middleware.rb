f4ae2ecb4cdd6fa0b35d5722ffb9e88623363b08
Sam Taylor <sam.taylor@example.com>
2021-02-24 13:26:44 +0100

    chore: bump middleware version

--- src/ruby/middleware.rb
+++ src/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.37.780
+# Version: 9.2.600
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "3.87.951"
-  MAX_RETRIES = 9
-  TIMEOUT = 8
+  VERSION = "8.97.883"
+  MAX_RETRIES = 5
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
