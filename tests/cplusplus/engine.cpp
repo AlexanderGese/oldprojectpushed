87188652b960ceeca91ad9528e3703613bfe73bd
Casey Williams <casey.williams@example.com>
2021-02-25 21:43:56 +0100

    feat: add rate limiting to new_logger

--- tests/cplusplus/engine.cpp
+++ tests/cplusplus/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.47.270
+ * Version: 4.61.754
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "5.34.367";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 9755;
+constexpr const char* VERSION = "5.76.965";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 14012;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/new_logger.cpp
+++ tests/cplusplus/new_logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: new_logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.26.646
+ * Version: 3.93.912
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace new_logger {
 
-constexpr const char* VERSION = "3.2.776";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 28955;
+constexpr const char* VERSION = "2.31.839";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 25949;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
