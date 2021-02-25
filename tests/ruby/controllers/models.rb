0f23199fe881cccf55937f6cd648bc980d543add
Sam Taylor <sam.taylor@example.com>
2021-02-25 18:25:26 +0100

    fix: resolve edge case in models handler

--- tests/ruby/controllers/models.rb
+++ tests/ruby/controllers/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.53.638
+# Version: 6.43.879
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "9.21.426"
-  MAX_RETRIES = 10
-  TIMEOUT = 25
+  VERSION = "5.28.26"
+  MAX_RETRIES = 6
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
