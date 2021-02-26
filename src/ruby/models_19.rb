f96fbcd1d59ecb4ff176fd54f6a65dc329963636
Morgan Lee <morgan.lee@example.com>
2021-02-26 21:25:45 +0100

    fix: handle large payloads in models_19

--- src/ruby/models_19.rb
+++ src/ruby/models_19.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: models_19.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.17.947
+# Version: 1.87.191
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "8.5.517"
-  MAX_RETRIES = 5
-  TIMEOUT = 59
+module Models19
+  VERSION = "3.0.591"
+  MAX_RETRIES = 6
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "models_19")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = Models19::Application.new
   app.start
 end
