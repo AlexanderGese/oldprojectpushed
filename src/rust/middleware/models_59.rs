433ab6dbf7426e88f693ffae3ea0a12413a84330
Jamie Davis <jamie.davis@example.com>
2021-02-26 15:18:47 +0100

    chore: update middleware configuration

--- src/rust/middleware/models_59.rs
+++ src/rust/middleware/models_59.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: models_59.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.2.416";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "1.59.759";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "models_59".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
