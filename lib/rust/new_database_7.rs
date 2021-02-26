514423c729d16cdeea8b61b0c03d9efee80fda35
Jordan Smith <jordan.smith@example.com>
2021-02-26 15:25:17 +0100

    fix: handle large payloads in new_database

--- lib/rust/new_database_7.rs
+++ lib/rust/new_database_7.rs
@@ -1,4 +1,4 @@
-//! Module: new_database.rs
+//! Module: new_database_7.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.10.56";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "8.88.65";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_database".to_string(),
+            app_name: "new_database_7".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
