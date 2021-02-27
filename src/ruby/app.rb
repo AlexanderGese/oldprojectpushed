d3ecdf1e7cd4f8443d1f4ce76f2ceca96a5a0199
Jamie Davis <jamie.davis@example.com>
2021-02-27 11:59:10 +0100

    refactor: improve app error messages

--- src/ruby/app.rb
+++ src/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.32.425
+# Version: 2.64.83
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "9.34.996"
+  VERSION = "9.94.304"
   MAX_RETRIES = 7
-  TIMEOUT = 58
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
