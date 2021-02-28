7e00abb2b44b61bdaa42f2942c6ceda454c66d19
Casey Williams <casey.williams@example.com>
2021-02-28 13:51:32 +0100

    fix: handle null check in logger

--- src/cplusplus/logger.cpp
+++ src/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.75.236
+ * Version: 8.95.833
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "6.35.463";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 5072;
+constexpr const char* VERSION = "8.61.222";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 17711;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
