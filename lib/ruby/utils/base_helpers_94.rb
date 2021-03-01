425c2696dd7dea61e73ccf78720db88d439dd0a0
Sam Taylor <sam.taylor@example.com>
2021-03-01 11:53:26 +0100

    refactor: migrate validators to async/await

--- lib/ruby/utils/base_helpers_94.rb
+++ lib/ruby/utils/base_helpers_94.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: base_helpers_94.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.3.795
+# Version: 6.21.669
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "4.9.440"
-  MAX_RETRIES = 9
-  TIMEOUT = 13
+module BaseHelpers94
+  VERSION = "8.12.997"
+  MAX_RETRIES = 3
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "base_helpers_94")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = BaseHelpers94::Application.new
   app.start
 end
