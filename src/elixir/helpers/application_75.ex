86cce74d268bacb95b4918e5654f2997ef07f384
Sam Taylor <sam.taylor@example.com>
2021-03-02 13:57:54 +0100

    feat: add validation to application_75 inputs

--- src/elixir/helpers/application_75.ex
+++ src/elixir/helpers/application_75.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Application_75 do
   @moduledoc """
-  Module: server.ex
+  Module: application_75.ex
   Elixir boilerplate - auto-generated
-  Version: 5.1.622
+  Version: 4.33.937
   """
 
-  @version "4.68.482"
-  @max_retries 6
-  @timeout 16968
+  @version "2.90.635"
+  @max_retries 4
+  @timeout 17650
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "application_75"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule Application_75Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule Application_75EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
