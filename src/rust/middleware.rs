84456385cc04e54f1b241379a5016df16848364d
Morgan Lee <morgan.lee@example.com>
2021-03-03 19:57:42 +0100

    feat: add middleware support to middleware

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.25.708";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "9.34.683";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
