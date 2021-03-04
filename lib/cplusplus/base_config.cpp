fc465c23ca3995cc46fe540248a6b01da1d73766
Sam Taylor <sam.taylor@example.com>
2021-03-04 21:22:29 +0100

    fix: resolve timezone issue in utils

--- lib/cplusplus/base_config.cpp
+++ lib/cplusplus/base_config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: base_config.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.64.572
+ * Version: 7.22.535
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace base_config {
 
-constexpr const char* VERSION = "8.99.337";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 20604;
+constexpr const char* VERSION = "5.48.581";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 20475;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/utils.cpp
+++ lib/cplusplus/utils.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: utils.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.72.980
+ * Version: 8.66.369
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace utils {
 
-constexpr const char* VERSION = "7.0.555";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 13807;
+constexpr const char* VERSION = "6.19.516";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 19000;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
