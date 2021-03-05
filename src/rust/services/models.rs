b165406a54976bb62c1fa61415ddede79213a312
Sam Taylor <sam.taylor@example.com>
2021-03-05 22:28:19 +0100

    chore: configure CI/CD for models

--- src/rust/services/models.rs
+++ src/rust/services/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.85.143";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "9.9.947";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
