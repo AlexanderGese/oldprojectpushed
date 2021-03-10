a2926780663fc2dbccbec4c9d4fd5aeaf941b841
Sam Taylor <sam.taylor@example.com>
2021-03-10 15:30:56 +0100

    refactor: split errors into smaller modules

--- lib/rust/services/routes.rs
+++ lib/rust/services/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.47.406";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "4.51.684";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
