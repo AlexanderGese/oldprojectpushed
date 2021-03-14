938941a2a2d3cb3585d67d06400e8f2b689ad97c
Riley Johnson <riley.johnson@example.com>
2021-03-14 09:51:21 +0100

    refactor: improve routes test coverage

--- lib/ruby/core_app_5.rb
+++ lib/ruby/core_app_5.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: core_app_5.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.36.264
+# Version: 4.33.532
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "2.50.553"
-  MAX_RETRIES = 10
-  TIMEOUT = 14
+module CoreApp5
+  VERSION = "6.42.420"
+  MAX_RETRIES = 7
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "core_app_5")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = CoreApp5::Application.new
   app.start
 end
