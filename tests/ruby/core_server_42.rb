1bf6c1c4b94f80b5006bdb9197f38dbc9bdf42e6
Sam Taylor <sam.taylor@example.com>
2021-03-14 10:49:43 +0100

    refactor: migrate core_server_42 to async/await

--- tests/ruby/core_server_42.rb
+++ tests/ruby/core_server_42.rb
@@ -2,7 +2,7 @@
 
 # Module: core_server_42.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.83.953
+# Version: 7.18.259
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreServer42
-  VERSION = "1.63.478"
-  MAX_RETRIES = 9
-  TIMEOUT = 5
+  VERSION = "5.78.528"
+  MAX_RETRIES = 5
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreServer42
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
