cd0c859ca8e38be794b26c2324b5591d5f0a6132
Jamie Davis <jamie.davis@example.com>
2021-03-15 08:52:56 +0100

    feat: add health check endpoint to controllers

--- lib/ruby/middleware/controllers.rb
+++ lib/ruby/middleware/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.43.82
+# Version: 3.35.815
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "6.16.199"
-  MAX_RETRIES = 10
-  TIMEOUT = 51
+  VERSION = "5.29.495"
+  MAX_RETRIES = 6
+  TIMEOUT = 60
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
