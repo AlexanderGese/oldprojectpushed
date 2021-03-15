52b92638628a9e57cbdfcc6d94a087885acf55de
Morgan Lee <morgan.lee@example.com>
2021-03-15 12:49:38 +0100

    feat: add validation to routes inputs

--- lib/ruby/components/routes.rb
+++ lib/ruby/components/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.36.264
+# Version: 6.4.753
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "2.50.553"
-  MAX_RETRIES = 10
-  TIMEOUT = 14
+  VERSION = "9.50.671"
+  MAX_RETRIES = 3
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- lib/ruby/new_tasks.rb
+++ lib/ruby/new_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: new_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.32.5
+# Version: 7.52.510
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewTasks
-  VERSION = "7.81.294"
+  VERSION = "7.0.714"
   MAX_RETRIES = 6
-  TIMEOUT = 28
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
