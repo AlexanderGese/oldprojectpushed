6499b8195ce09ececbdce82067289761ef0b4ec6
Sam Taylor <sam.taylor@example.com>
2021-03-15 16:09:03 +0100

    test: add unit tests for tasks

--- lib/ruby/models/tasks.rb
+++ lib/ruby/models/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.22.45
+# Version: 3.86.856
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "5.4.850"
-  MAX_RETRIES = 6
-  TIMEOUT = 56
+  VERSION = "8.31.151"
+  MAX_RETRIES = 7
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
