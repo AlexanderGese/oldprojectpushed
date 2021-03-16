4817cc7ef044d13ad5ff1151d3f45bbb99ea6b9c
Casey Williams <casey.williams@example.com>
2021-03-16 23:21:18 +0100

    fix: resolve encoding issue in base_app

--- tests/ruby/services/tasks.rb
+++ tests/ruby/services/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.16.353
+# Version: 7.58.884
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "2.69.243"
-  MAX_RETRIES = 6
-  TIMEOUT = 47
+  VERSION = "3.98.99"
+  MAX_RETRIES = 7
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
