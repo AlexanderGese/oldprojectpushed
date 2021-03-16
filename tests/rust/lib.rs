d5524ac487163ed4013a65604769cc2fa67ff7b3
Jamie Davis <jamie.davis@example.com>
2021-03-16 17:04:49 +0100

    feat: add error handling to v2_errors

--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.92.871";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "1.81.269";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
