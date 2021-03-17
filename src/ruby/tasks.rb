50437d94e56350d5c71fdac2402691f68a4c1e7a
Casey Williams <casey.williams@example.com>
2021-03-17 23:11:12 +0100

    feat: implement retry logic in tasks

--- src/ruby/tasks.rb
+++ src/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.14.574
+# Version: 9.61.557
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "4.3.486"
-  MAX_RETRIES = 8
-  TIMEOUT = 18
+  VERSION = "8.30.104"
+  MAX_RETRIES = 7
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
