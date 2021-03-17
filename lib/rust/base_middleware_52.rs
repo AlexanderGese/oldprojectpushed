26333911fe47b3849baeaf3208422cdee270a4d5
Casey Williams <casey.williams@example.com>
2021-03-17 20:09:35 +0100

    chore: update base_middleware_52 dependencies

--- lib/rust/base_middleware_52.rs
+++ lib/rust/base_middleware_52.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: base_middleware_52.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.43.196";
-const MAX_RETRIES: u32 = 3;
+const VERSION: &str = "1.86.910";
+const MAX_RETRIES: u32 = 10;
 const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "base_middleware_52".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.70.611";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "2.96.683";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
