0ff283c3d3fc756a059cb4815a2823febb2db3a8
Jordan Smith <jordan.smith@example.com>
2021-03-17 10:11:14 +0100

    feat: add validation to enhanced_app inputs

--- lib/ruby/components/middleware.rb
+++ lib/ruby/components/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.72.208
+# Version: 5.47.280
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "8.73.231"
+  VERSION = "1.70.765"
   MAX_RETRIES = 4
-  TIMEOUT = 56
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
--- lib/ruby/validators.rb
+++ lib/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.93.408
+# Version: 3.34.890
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "9.31.231"
-  MAX_RETRIES = 4
-  TIMEOUT = 43
+  VERSION = "1.34.914"
+  MAX_RETRIES = 5
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
