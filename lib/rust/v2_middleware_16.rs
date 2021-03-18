37efff1f84b961ff2dfa349eb25407a077f9830c
Jordan Smith <jordan.smith@example.com>
2021-03-18 20:10:14 +0100

    test: add unit tests for new_lib

--- lib/rust/v2_middleware_16.rs
+++ lib/rust/v2_middleware_16.rs
@@ -1,4 +1,4 @@
-//! Module: new_lib.rs
+//! Module: v2_middleware_16.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.93.288";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "6.21.451";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_lib".to_string(),
+            app_name: "v2_middleware_16".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
