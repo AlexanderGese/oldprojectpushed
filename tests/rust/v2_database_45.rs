03c13b8cc3dcf31cfc2bccb697a84e695082258a
Alex Chen <alex.chen@example.com>
2021-03-19 22:13:36 +0100

    refactor: improve v2_database_45 test coverage

--- tests/rust/v2_database_45.rs
+++ tests/rust/v2_database_45.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: v2_database_45.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.85.605";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "8.12.745";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "v2_database_45".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
