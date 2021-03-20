b3dc694b2d558b0810078aebfea1382df0223bb0
Alex Chen <alex.chen@example.com>
2021-03-20 16:12:54 +0100

    fix: handle null check in core_handlers

--- lib/rust/helpers/database.rs
+++ lib/rust/helpers/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.52.119";
+const VERSION: &str = "9.26.171";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 25;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
