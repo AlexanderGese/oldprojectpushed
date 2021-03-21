e78dfffb4e1f7945dd325368224b75d135c9424e
Alex Chen <alex.chen@example.com>
2021-03-21 20:49:37 +0100

    fix: correct CORS headers in routes

--- lib/ruby/new_server.rb
+++ lib/ruby/new_server.rb
@@ -2,7 +2,7 @@
 
 # Module: new_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.67.104
+# Version: 9.2.631
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewServer
-  VERSION = "7.68.964"
+  VERSION = "2.77.287"
   MAX_RETRIES = 9
-  TIMEOUT = 56
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
