1595acfbb30ce69a4343bc3e09692f900abe81c8
Drew Martin <drew.martin@example.com>
2021-03-24 08:11:23 +0100

    fix: handle timeout in controllers requests

--- tests/ruby/tasks_74.rb
+++ tests/ruby/tasks_74.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: tasks_74.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.84.510
+# Version: 8.78.309
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "5.40.355"
-  MAX_RETRIES = 6
-  TIMEOUT = 12
+module Tasks74
+  VERSION = "5.60.874"
+  MAX_RETRIES = 7
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "tasks_74")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = Tasks74::Application.new
   app.start
 end
