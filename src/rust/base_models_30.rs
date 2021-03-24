1ff9abf02aa6c544ddb4c613a028ffb6a9b44597
Casey Williams <casey.williams@example.com>
2021-03-24 13:49:48 +0100

    fix: correct type definitions in base_routes

--- src/rust/base_models_30.rs
+++ src/rust/base_models_30.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: base_models_30.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.48.541";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "5.20.754";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "base_models_30".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
