40673696bf9ec7660efdeeb9a6157be351d2e86d
Jordan Smith <jordan.smith@example.com>
2021-03-24 13:41:07 +0100

    feat: add error handling to lib

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.56.59";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "2.88.960";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
