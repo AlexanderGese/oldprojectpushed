a0c15976f83b65d28a4fb0b45da254487227bc12
Riley Johnson <riley.johnson@example.com>
2021-03-27 19:18:55 +0100

    chore: remove deprecated server code

--- lib/ruby/v2_config.rb
+++ lib/ruby/v2_config.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.70.329
+# Version: 4.68.807
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Config
-  VERSION = "9.84.354"
-  MAX_RETRIES = 8
-  TIMEOUT = 47
+  VERSION = "1.84.634"
+  MAX_RETRIES = 4
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
