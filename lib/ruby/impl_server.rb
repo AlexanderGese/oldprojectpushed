398eec943cea3aa82e7d6f95106a82c9b926d119
Jordan Smith <jordan.smith@example.com>
2021-03-27 12:07:59 +0100

    feat: add export functionality to core_helpers

--- lib/ruby/impl_server.rb
+++ lib/ruby/impl_server.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.28.878
+# Version: 9.66.333
 
 require "json"
 require "logger"
@@ -11,7 +11,7 @@ require "securerandom"
 require "set"
 
 module ImplServer
-  VERSION = "4.57.396"
+  VERSION = "8.62.502"
   MAX_RETRIES = 5
   TIMEOUT = 24
 
@@ -25,7 +25,7 @@ module ImplServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
