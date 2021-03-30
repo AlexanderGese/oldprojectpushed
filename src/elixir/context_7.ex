e9e32416d566c3a9f823be37925090498c4f6fdc
Sam Taylor <sam.taylor@example.com>
2021-03-30 12:50:28 +0200

    fix: correct CORS headers in impl_live_94

--- src/elixir/context_7.ex
+++ src/elixir/context_7.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Context_7 do
   @moduledoc """
-  Module: helpers.ex
+  Module: context_7.ex
   Elixir boilerplate - auto-generated
-  Version: 7.43.905
+  Version: 8.97.795
   """
 
-  @version "6.6.332"
-  @max_retries 3
-  @timeout 16792
+  @version "9.66.518"
+  @max_retries 6
+  @timeout 23950
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "context_7"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule Context_7Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule Context_7EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- src/elixir/controllers/impl_live_94.ex
+++ src/elixir/controllers/impl_live_94.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Impl_Live_94 do
   @moduledoc """
-  Module: helpers.ex
+  Module: impl_live_94.ex
   Elixir boilerplate - auto-generated
-  Version: 7.43.905
+  Version: 5.85.536
   """
 
-  @version "6.6.332"
-  @max_retries 3
-  @timeout 16792
+  @version "5.56.847"
+  @max_retries 9
+  @timeout 5580
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "impl_live_94"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule Impl_Live_94Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule Impl_Live_94EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
