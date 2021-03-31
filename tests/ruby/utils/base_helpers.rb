73667f73e1bfdce149166d28e3098772cf555159
Jamie Davis <jamie.davis@example.com>
2021-03-31 08:53:39 +0200

    refactor: split base_helpers into smaller modules

--- tests/ruby/utils/base_helpers.rb
+++ tests/ruby/utils/base_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: base_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.34.354
+# Version: 9.39.36
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "7.36.801"
+module BaseHelpers
+  VERSION = "6.67.900"
   MAX_RETRIES = 6
-  TIMEOUT = 20
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "base_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = BaseHelpers::Application.new
   app.start
 end
