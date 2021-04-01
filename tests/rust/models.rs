d2c08febe1c43b5b7587466516451d187e7d9339
Alex Chen <alex.chen@example.com>
2021-04-01 21:52:36 +0200

    feat: add export functionality to models

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.48.677";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "5.20.89";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
