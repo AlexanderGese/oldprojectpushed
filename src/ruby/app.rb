21249b43d575b5010c7a301413fe99f297a13fae
Riley Johnson <riley.johnson@example.com>
2021-04-01 22:01:33 +0200

    refactor: extract new_middleware utility functions

--- src/ruby/app.rb
+++ src/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.73.734
+# Version: 5.10.484
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "7.97.264"
-  MAX_RETRIES = 10
-  TIMEOUT = 25
+  VERSION = "5.90.6"
+  MAX_RETRIES = 7
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
