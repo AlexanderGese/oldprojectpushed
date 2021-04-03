348a3785f5d1301efe21a4dbb2b8c808e5557363
Casey Williams <casey.williams@example.com>
2021-04-03 23:20:37 +0200

    feat: implement SSE streaming in app

--- src/ruby/controllers/base_middleware.rb
+++ src/ruby/controllers/base_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: base_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.88.101
+# Version: 7.25.588
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseMiddleware
-  VERSION = "6.93.794"
-  MAX_RETRIES = 10
-  TIMEOUT = 39
+  VERSION = "9.3.793"
+  MAX_RETRIES = 3
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
--- src/ruby/services/app.rb
+++ src/ruby/services/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.19.907
+# Version: 6.20.121
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "7.11.790"
+  VERSION = "1.11.745"
   MAX_RETRIES = 10
-  TIMEOUT = 17
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
