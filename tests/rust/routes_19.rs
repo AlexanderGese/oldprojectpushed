37495e60ccf89439841af1a19ef6c49bc68eb134
Alex Chen <alex.chen@example.com>
2021-04-03 10:38:28 +0200

    refactor: improve v2_errors error messages

--- tests/rust/routes_19.rs
+++ tests/rust/routes_19.rs
@@ -1,4 +1,4 @@
-//! Module: v2_errors.rs
+//! Module: routes_19.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.37.510";
+const VERSION: &str = "7.1.183";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 35;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_errors".to_string(),
+            app_name: "routes_19".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
