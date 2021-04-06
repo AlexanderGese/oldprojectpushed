49dcfdf84a1a6d775f4f250959ddf29cde0a9e3e
Casey Williams <casey.williams@example.com>
2021-04-06 12:59:16 +0200

    feat: add batch processing to lib

--- tests/rust/handlers/database.rs
+++ tests/rust/handlers/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.96.157";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "2.20.43";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.56.195";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "1.69.303";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
