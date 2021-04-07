83e5572b42f560a0027f57dd6a57c9f6b258b656
Drew Martin <drew.martin@example.com>
2021-04-07 22:07:47 +0200

    feat: add logging to config

--- src/rust/routes.rs
+++ src/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.23.861";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "6.19.906";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
