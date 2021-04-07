24f8e78ba566814282de5c14db0246880c4bce10
Casey Williams <casey.williams@example.com>
2021-04-07 19:29:59 +0200

    fix: handle null check in engine

--- tests/cplusplus/engine.cpp
+++ tests/cplusplus/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.29.689
+ * Version: 5.83.752
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "4.42.778";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 10324;
+constexpr const char* VERSION = "4.40.7";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 21147;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/enhanced_parser.cpp
+++ tests/cplusplus/enhanced_parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: enhanced_parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.46.534
+ * Version: 5.68.558
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace enhanced_parser {
 
-constexpr const char* VERSION = "2.36.377";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 8658;
+constexpr const char* VERSION = "6.19.867";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 15619;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
