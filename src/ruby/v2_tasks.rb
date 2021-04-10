076e479069ff2a94791abd4295baf45c68b11e08
Riley Johnson <riley.johnson@example.com>
2021-04-10 19:25:13 +0200

    chore: clean up controllers imports

--- src/ruby/v2_tasks.rb
+++ src/ruby/v2_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.61.57
+# Version: 1.75.918
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Tasks
-  VERSION = "1.67.460"
-  MAX_RETRIES = 9
-  TIMEOUT = 9
+  VERSION = "7.25.422"
+  MAX_RETRIES = 5
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
