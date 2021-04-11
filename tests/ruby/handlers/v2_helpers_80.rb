7799d4699be51f86de2616ef0394b99a0a83450e
Riley Johnson <riley.johnson@example.com>
2021-04-11 23:21:50 +0200

    feat: add health check endpoint to v2_helpers_80

--- tests/ruby/handlers/v2_helpers_80.rb
+++ tests/ruby/handlers/v2_helpers_80.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_middleware.rb
+# Module: v2_helpers_80.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.77.974
+# Version: 8.60.471
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewMiddleware
-  VERSION = "4.78.980"
-  MAX_RETRIES = 8
-  TIMEOUT = 45
+module V2Helpers80
+  VERSION = "3.35.398"
+  MAX_RETRIES = 7
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_middleware")
+      @app_name = opts.fetch(:app_name, "v2_helpers_80")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewMiddleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewMiddleware::Application.new
+  app = V2Helpers80::Application.new
   app.start
 end
