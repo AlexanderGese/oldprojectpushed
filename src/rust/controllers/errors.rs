82055e44418d3516171218b9bb5b88a9654b3f6a
Sam Taylor <sam.taylor@example.com>
2021-04-12 22:56:37 +0200

    fix: handle concurrent access in errors

--- src/rust/controllers/errors.rs
+++ src/rust/controllers/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.1.163";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "3.71.860";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/services/database.rs
+++ src/rust/services/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.37.391";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "9.0.268";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
