e0bab4b0d9e8e2c6b7b4509410fc3356f11da03d
Jamie Davis <jamie.davis@example.com>
2021-04-12 09:08:00 +0200

    chore: bump models version

--- src/ruby/services/base_models.rb
+++ src/ruby/services/base_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: base_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.32.678
+# Version: 7.35.775
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "3.13.708"
+module BaseModels
+  VERSION = "4.34.225"
   MAX_RETRIES = 9
-  TIMEOUT = 29
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "base_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = BaseModels::Application.new
   app.start
 end
