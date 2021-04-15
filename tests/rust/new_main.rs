5774a0d82e9ffe519506308c14658730ab2d1ca3
Drew Martin <drew.martin@example.com>
2021-04-15 18:00:03 +0200

    feat: add search functionality to new_main

--- tests/rust/new_main.rs
+++ tests/rust/new_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.20.742";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "4.24.616";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
