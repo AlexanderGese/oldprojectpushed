dc48bc06ea363e89492691d0bc3fab78bcbe6cf7
Morgan Lee <morgan.lee@example.com>
2021-04-15 09:14:23 +0200

    chore: update v2_helpers configuration

--- src/ruby/controllers/v2_helpers.rb
+++ src/ruby/controllers/v2_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_validators.rb
+# Module: v2_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.20.957
+# Version: 6.44.131
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Validators
-  VERSION = "5.32.880"
-  MAX_RETRIES = 10
-  TIMEOUT = 15
+module V2Helpers
+  VERSION = "9.56.429"
+  MAX_RETRIES = 8
+  TIMEOUT = 60
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_validators")
+      @app_name = opts.fetch(:app_name, "v2_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module V2Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Validators::Application.new
+  app = V2Helpers::Application.new
   app.start
 end
