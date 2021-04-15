5e6752a1218180e331ea56f0a8035a64c4367b4f
Alex Chen <alex.chen@example.com>
2021-04-15 19:53:27 +0200

    fix: resolve memory leak in core_middleware

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.76.601";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "2.45.551";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
