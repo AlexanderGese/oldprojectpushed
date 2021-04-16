11cefa30e4716d4f03a0f11fb61261a1750db92d
Alex Chen <alex.chen@example.com>
2021-04-16 11:13:32 +0200

    fix: handle null check in main_36

--- src/rust/helpers/main_36.rs
+++ src/rust/helpers/main_36.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: main_36.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.43.689";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "3.29.356";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "main_36".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
