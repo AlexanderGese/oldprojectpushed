2578c696ccc8d3936fca2338570793a20df6f196
Casey Williams <casey.williams@example.com>
2021-04-16 13:08:38 +0200

    fix: resolve timezone issue in handler

--- src/cplusplus/middleware/handler.cpp
+++ src/cplusplus/middleware/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.30.715
+ * Version: 8.21.567
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "6.67.510";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 11085;
+constexpr const char* VERSION = "5.68.223";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 12369;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
