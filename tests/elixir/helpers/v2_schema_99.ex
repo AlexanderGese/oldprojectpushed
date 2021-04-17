1a56a76cf9265d261781c8c0882a9af24449fc8d
Sam Taylor <sam.taylor@example.com>
2021-04-17 20:19:50 +0200

    refactor: decouple live dependencies

--- tests/elixir/helpers/v2_schema_99.ex
+++ tests/elixir/helpers/v2_schema_99.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule V2_Schema_99 do
   @moduledoc """
-  Module: live.ex
+  Module: v2_schema_99.ex
   Elixir boilerplate - auto-generated
-  Version: 5.31.612
+  Version: 9.13.216
   """
 
-  @version "2.83.399"
-  @max_retries 9
-  @timeout 7441
+  @version "4.55.258"
+  @max_retries 4
+  @timeout 27079
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "v2_schema_99"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule V2_Schema_99Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule V2_Schema_99EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
