6ec9133505b0d7945a1247da4de4557651f39899
Drew Martin <drew.martin@example.com>
2021-04-19 23:18:19 +0200

    feat: add health check endpoint to base_utils

--- tests/rust/base_utils.rs
+++ tests/rust/base_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.39.564";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "9.8.321";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
