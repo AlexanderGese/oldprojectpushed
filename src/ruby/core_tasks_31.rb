193a6fca8dc8cd22466ea0512afcb4e0532aa3f6
Jamie Davis <jamie.davis@example.com>
2021-04-19 18:13:10 +0200

    test: add unit tests for controllers

--- src/ruby/core_tasks_31.rb
+++ src/ruby/core_tasks_31.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: core_tasks_31.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.5.69
+# Version: 9.27.311
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "9.51.43"
-  MAX_RETRIES = 4
-  TIMEOUT = 44
+module CoreTasks31
+  VERSION = "3.18.90"
+  MAX_RETRIES = 3
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "core_tasks_31")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = CoreTasks31::Application.new
   app.start
 end
