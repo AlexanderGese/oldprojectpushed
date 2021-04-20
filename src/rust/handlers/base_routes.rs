12e2b31be7eae64f63414dac21a5f5f314558afc
Casey Williams <casey.williams@example.com>
2021-04-20 12:43:41 +0200

    refactor: split utils into smaller modules

--- src/rust/handlers/base_routes.rs
+++ src/rust/handlers/base_routes.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: base_routes.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.21.109";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "6.60.392";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "base_routes".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
