9b17382080e5e09cdd44f2f34d57240bff9415c1
Alex Chen <alex.chen@example.com>
2021-04-21 08:32:52 +0200

    refactor: optimize lib performance

--- tests/rust/handlers/models.rs
+++ tests/rust/handlers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.51.616";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "8.86.826";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
