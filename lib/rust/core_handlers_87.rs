eaf1156c1a2d6eccf162ec50c709747da0301a6a
Casey Williams <casey.williams@example.com>
2021-04-21 21:37:25 +0200

    fix: correct CORS headers in new_models

--- lib/rust/core_handlers_87.rs
+++ lib/rust/core_handlers_87.rs
@@ -1,4 +1,4 @@
-//! Module: new_models.rs
+//! Module: core_handlers_87.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.79.977";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "6.84.131";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_models".to_string(),
+            app_name: "core_handlers_87".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.46.645";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "7.3.204";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
