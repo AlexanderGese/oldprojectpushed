dba2ecc5052a10ad6827cdd3de382a6ac86abb93
Drew Martin <drew.martin@example.com>
2021-04-23 10:07:33 +0200

    feat: implement WebSocket support in tasks

--- src/ruby/middleware.rb
+++ src/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.51.626
+# Version: 4.79.453
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "4.33.889"
-  MAX_RETRIES = 7
-  TIMEOUT = 12
+  VERSION = "1.34.667"
+  MAX_RETRIES = 6
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
