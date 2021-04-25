042680ff32d9f7a29f5b06cb346f4ae9199e1dcf
Drew Martin <drew.martin@example.com>
2021-04-25 08:57:16 +0200

    feat: add export functionality to handlers

--- tests/rust/components/handlers.rs
+++ tests/rust/components/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.94.566";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "3.8.18";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
