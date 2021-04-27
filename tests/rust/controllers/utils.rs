7c5035832c8d6c2805aa47d6577b8f7187869eb2
Riley Johnson <riley.johnson@example.com>
2021-04-27 18:53:40 +0200

    refactor: improve utils test coverage

--- tests/rust/controllers/utils.rs
+++ tests/rust/controllers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.65.551";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "5.99.439";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
