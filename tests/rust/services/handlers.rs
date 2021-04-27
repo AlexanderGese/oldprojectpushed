b60cc430cd890382338eb532b9a4ebf943944ed9
Jamie Davis <jamie.davis@example.com>
2021-04-27 11:03:08 +0200

    chore: remove deprecated routes code

--- tests/rust/services/handlers.rs
+++ tests/rust/services/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.40.226";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "6.16.644";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
