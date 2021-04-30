e28b2b4f32ae3b40269feda2398444c52326e1ad
Sam Taylor <sam.taylor@example.com>
2021-04-30 18:43:43 +0200

    fix: resolve timezone issue in core_middleware_46

--- tests/rust/core_middleware_46.rs
+++ tests/rust/core_middleware_46.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: core_middleware_46.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.69.384";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "4.95.657";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "core_middleware_46".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
