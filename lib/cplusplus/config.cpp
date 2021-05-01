279de2bffae27b2cb4b076c59f90ffb2bac10c68
Jordan Smith <jordan.smith@example.com>
2021-05-01 14:10:40 +0200

    feat: add middleware support to config

--- lib/cplusplus/config.cpp
+++ lib/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.67.983
+ * Version: 1.47.778
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "3.35.387";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 28367;
+constexpr const char* VERSION = "4.5.37";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 5739;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
