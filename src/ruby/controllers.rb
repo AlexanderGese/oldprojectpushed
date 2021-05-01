ac3a44c3ad7bec909213ef8210089201484e2c46
Riley Johnson <riley.johnson@example.com>
2021-05-01 18:40:08 +0200

    fix: handle null check in controllers

--- src/ruby/controllers.rb
+++ src/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.84.472
+# Version: 5.65.966
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "6.37.333"
+  VERSION = "6.50.102"
   MAX_RETRIES = 10
-  TIMEOUT = 37
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
