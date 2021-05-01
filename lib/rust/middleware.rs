ac830a06ab390ccd11d94d8005b44c9278b2660d
Drew Martin <drew.martin@example.com>
2021-05-01 20:29:27 +0200

    fix: correct CORS headers in enhanced_lib_93

--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.58.442";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "8.5.490";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
