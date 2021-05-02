de7412725cc8c8cec65a4e42c3f44fcd86ae4366
Drew Martin <drew.martin@example.com>
2021-05-02 13:44:16 +0200

    docs: add documentation for enhanced_errors_46

--- tests/rust/helpers/impl_lib.rs
+++ tests/rust/helpers/impl_lib.rs
@@ -1,4 +1,4 @@
-//! Module: new_models.rs
+//! Module: impl_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.1.311";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "8.57.884";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_models".to_string(),
+            app_name: "impl_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/middleware/enhanced_errors_46.rs
+++ tests/rust/middleware/enhanced_errors_46.rs
@@ -1,4 +1,4 @@
-//! Module: new_models.rs
+//! Module: enhanced_errors_46.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.1.311";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "1.12.595";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_models".to_string(),
+            app_name: "enhanced_errors_46".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
