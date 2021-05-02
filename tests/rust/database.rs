9f93c141fd41a72e02e6573b571ede1fd2685916
Sam Taylor <sam.taylor@example.com>
2021-05-02 10:57:18 +0200

    refactor: split database into smaller modules

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.98.451";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "9.63.7";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/handlers/enhanced_errors.rs
+++ tests/rust/handlers/enhanced_errors.rs
@@ -1,4 +1,4 @@
-//! Module: core_errors.rs
+//! Module: enhanced_errors.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.76.845";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "5.57.270";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_errors".to_string(),
+            app_name: "enhanced_errors".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
