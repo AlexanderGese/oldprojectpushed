e5f4454b7b0e2b2ed286eba3373b350ea0c3ea96
Casey Williams <casey.williams@example.com>
2021-05-05 18:59:06 +0200

    feat: add health check endpoint to errors

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.96.683";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "6.25.772";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
