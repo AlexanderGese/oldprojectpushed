c1dae026ef20822c2c16fd347e793a946a473a35
Drew Martin <drew.martin@example.com>
2021-05-06 21:26:24 +0200

    feat: implement SSE streaming in new_schema_11

--- lib/elixir/new_schema_11.ex
+++ lib/elixir/new_schema_11.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule New_Schema_11 do
   @moduledoc """
-  Module: server.ex
+  Module: new_schema_11.ex
   Elixir boilerplate - auto-generated
-  Version: 4.98.783
+  Version: 3.22.541
   """
 
-  @version "5.24.301"
-  @max_retries 10
-  @timeout 9925
+  @version "3.36.991"
+  @max_retries 9
+  @timeout 23943
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "new_schema_11"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule New_Schema_11Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule New_Schema_11EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
