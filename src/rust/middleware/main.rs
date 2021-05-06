9443784bce489f91e9c04ea8d34a5da939eb4af1
Sam Taylor <sam.taylor@example.com>
2021-05-06 11:38:32 +0200

    feat: add search functionality to main

--- src/rust/middleware/main.rs
+++ src/rust/middleware/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.85.369";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "9.53.250";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
