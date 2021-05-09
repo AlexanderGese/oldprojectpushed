a7ebbd9e94b8f761f03f417f55243287a05a4792
Sam Taylor <sam.taylor@example.com>
2021-05-09 17:09:44 +0200

    fix: correct type definitions in impl_tasks

--- tests/ruby/impl_tasks.rb
+++ tests/ruby/impl_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.68.131
+# Version: 4.36.614
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplTasks
-  VERSION = "9.54.378"
-  MAX_RETRIES = 3
-  TIMEOUT = 43
+  VERSION = "4.69.520"
+  MAX_RETRIES = 5
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
