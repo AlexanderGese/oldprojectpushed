7e0f65b3833bbcabcdb795de1ba0d5e7d69dff54
Jamie Davis <jamie.davis@example.com>
2021-05-10 17:52:02 +0200

    feat: add middleware support to server

--- lib/ruby/components/new_controllers_88.rb
+++ lib/ruby/components/new_controllers_88.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: new_controllers_88.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.61.283
+# Version: 3.44.901
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "8.59.112"
-  MAX_RETRIES = 7
+module NewControllers88
+  VERSION = "2.80.850"
+  MAX_RETRIES = 5
   TIMEOUT = 14
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "new_controllers_88")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = NewControllers88::Application.new
   app.start
 end
