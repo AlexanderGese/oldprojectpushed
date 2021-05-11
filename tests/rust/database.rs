e4c8cb5a97d55d0d5df0ad7ce6f4658fe02927de
Sam Taylor <sam.taylor@example.com>
2021-05-11 23:14:07 +0200

    chore: remove deprecated models code

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.63.7";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "3.93.594";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.46.839";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "7.36.972";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
