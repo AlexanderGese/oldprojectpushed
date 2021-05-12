fef62b61856a87852a246e599f1392f060994d6a
Casey Williams <casey.williams@example.com>
2021-05-12 22:52:20 +0200

    refactor: improve v2_middleware_77 test coverage

--- tests/ruby/v2_middleware_77.rb
+++ tests/ruby/v2_middleware_77.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: v2_middleware_77.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.43.212
+# Version: 2.77.752
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "5.63.830"
+module V2Middleware77
+  VERSION = "1.47.648"
   MAX_RETRIES = 6
-  TIMEOUT = 35
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "v2_middleware_77")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = V2Middleware77::Application.new
   app.start
 end
