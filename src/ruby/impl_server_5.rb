f3ee71741f7ca00ebb23c1ccc8de5a7f99e0ece7
Drew Martin <drew.martin@example.com>
2021-05-14 12:58:27 +0200

    fix: resolve edge case in impl_server_5 handler

--- src/ruby/impl_server_5.rb
+++ src/ruby/impl_server_5.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_validators.rb
+# Module: impl_server_5.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.73.342
+# Version: 2.16.14
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplValidators
-  VERSION = "8.24.320"
-  MAX_RETRIES = 3
-  TIMEOUT = 22
+module ImplServer5
+  VERSION = "1.61.715"
+  MAX_RETRIES = 6
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_validators")
+      @app_name = opts.fetch(:app_name, "impl_server_5")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplValidators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplValidators::Application.new
+  app = ImplServer5::Application.new
   app.start
 end
