680fdd3959ecf3ac4e4dd174932b2d9e4a0bdc3e
Drew Martin <drew.martin@example.com>
2021-05-18 22:57:25 +0200

    fix: patch security vulnerability in routes

--- tests/ruby/handlers/v2_app.rb
+++ tests/ruby/handlers/v2_app.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_routes.rb
+# Module: v2_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.46.916
+# Version: 6.85.71
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreRoutes
-  VERSION = "6.7.222"
-  MAX_RETRIES = 9
-  TIMEOUT = 24
+module V2App
+  VERSION = "6.12.484"
+  MAX_RETRIES = 4
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_routes")
+      @app_name = opts.fetch(:app_name, "v2_app")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreRoutes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreRoutes::Application.new
+  app = V2App::Application.new
   app.start
 end
