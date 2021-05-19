178ea9c5ba03ee852bbd4655dca3a5299ff10b42
Sam Taylor <sam.taylor@example.com>
2021-05-19 20:41:39 +0200

    fix: handle timeout in enhanced_routes requests

--- lib/ruby/tasks.rb
+++ lib/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.43.585
+# Version: 6.61.82
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "2.12.941"
-  MAX_RETRIES = 5
-  TIMEOUT = 38
+  VERSION = "1.96.931"
+  MAX_RETRIES = 8
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
