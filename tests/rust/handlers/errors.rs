62be81928e3f0f358ab2a9146a41144421791574
Casey Williams <casey.williams@example.com>
2021-05-21 23:27:03 +0200

    feat: add batch processing to core_middleware

--- tests/rust/handlers/errors.rs
+++ tests/rust/handlers/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.33.693";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "2.33.459";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/new_middleware_86.rs
+++ tests/rust/new_middleware_86.rs
@@ -1,4 +1,4 @@
-//! Module: core_middleware.rs
+//! Module: new_middleware_86.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.58.872";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "9.79.140";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "core_middleware".to_string(),
+            app_name: "new_middleware_86".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
