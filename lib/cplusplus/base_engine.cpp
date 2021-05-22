18ab342715dff6b35763042203d3bf22883109ed
Jamie Davis <jamie.davis@example.com>
2021-05-22 23:52:54 +0200

    chore: remove deprecated base_engine code

--- lib/cplusplus/base_engine.cpp
+++ lib/cplusplus/base_engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: base_engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.3.892
+ * Version: 7.19.364
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace base_engine {
 
-constexpr const char* VERSION = "8.51.777";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 13364;
+constexpr const char* VERSION = "8.8.42";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 21965;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
