f90aa199ca9825fbfbdeeb0663d797486dbf0fb1
Riley Johnson <riley.johnson@example.com>
2021-05-26 20:46:20 +0200

    fix: patch security vulnerability in validators

--- lib/ruby/validators.rb
+++ lib/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.34.890
+# Version: 6.56.418
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "1.34.914"
-  MAX_RETRIES = 5
-  TIMEOUT = 11
+  VERSION = "8.97.528"
+  MAX_RETRIES = 7
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
