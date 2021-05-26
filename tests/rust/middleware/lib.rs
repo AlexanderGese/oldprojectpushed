995327cbfb2fae0a02e529433778243cba7876b3
Alex Chen <alex.chen@example.com>
2021-05-26 22:40:38 +0200

    fix: handle timeout in lib requests

--- tests/rust/middleware/lib.rs
+++ tests/rust/middleware/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.41.44";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "7.99.851";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
