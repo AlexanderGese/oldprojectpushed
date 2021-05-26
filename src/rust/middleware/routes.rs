4691dcf06c3c4cbbe928706453a9f32eff87366a
Sam Taylor <sam.taylor@example.com>
2021-05-26 09:08:30 +0200

    refactor: simplify impl_lib_93 state management

--- src/rust/middleware/routes.rs
+++ src/rust/middleware/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.9.482";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "9.38.323";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
