e33b4ee58606ef7e0b2fe6c464fc0e7ee23ef991
Alex Chen <alex.chen@example.com>
2021-05-28 08:55:21 +0200

    fix: resolve memory leak in v2_helpers

--- lib/ruby/core_tasks.rb
+++ lib/ruby/core_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: core_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.76.462
+# Version: 9.25.725
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreTasks
-  VERSION = "5.78.620"
-  MAX_RETRIES = 6
-  TIMEOUT = 14
+  VERSION = "7.13.449"
+  MAX_RETRIES = 7
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
