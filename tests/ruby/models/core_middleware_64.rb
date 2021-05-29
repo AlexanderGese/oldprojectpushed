3827cf12f2f6bf2902d137282fa3f8555ae21d78
Jamie Davis <jamie.davis@example.com>
2021-05-29 21:57:09 +0200

    feat: implement SSE streaming in tasks_9

--- tests/ruby/models/core_middleware_64.rb
+++ tests/ruby/models/core_middleware_64.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: core_middleware_64.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.39.213
+# Version: 7.78.874
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "2.84.880"
-  MAX_RETRIES = 9
-  TIMEOUT = 50
+module CoreMiddleware64
+  VERSION = "5.63.442"
+  MAX_RETRIES = 5
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "core_middleware_64")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = CoreMiddleware64::Application.new
   app.start
 end
