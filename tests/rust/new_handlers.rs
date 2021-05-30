2ef08bd2622b6fb5c5991e159793b9ccfe13a030
Sam Taylor <sam.taylor@example.com>
2021-05-30 19:46:24 +0200

    fix: handle concurrent access in utils

--- tests/rust/new_handlers.rs
+++ tests/rust/new_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.13.689";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "1.54.855";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/v2_main.rs
+++ tests/rust/v2_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.20.374";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "8.21.754";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
