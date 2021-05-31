367cb7d8023604cfa0bfda9b81df7cdc8dbf0efc
Riley Johnson <riley.johnson@example.com>
2021-05-31 22:42:46 +0200

    chore: update utils dependencies

--- lib/rust/handlers/config_29.rs
+++ lib/rust/handlers/config_29.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: config_29.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.84.588";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "8.19.498";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "config_29".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/utils/database.rs
+++ lib/rust/utils/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.78.81";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "2.12.325";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
