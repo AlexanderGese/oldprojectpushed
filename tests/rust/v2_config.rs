271a40e735570263f41a7c2a827d8e06b1e511d1
Casey Williams <casey.williams@example.com>
2021-06-02 22:30:24 +0200

    feat: add export functionality to core_middleware

--- tests/rust/v2_config.rs
+++ tests/rust/v2_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.1.533";
+const VERSION: &str = "5.39.890";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 33;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
