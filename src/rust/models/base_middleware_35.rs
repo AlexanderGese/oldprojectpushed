e5dd18c723af9455569609e2d34d0332e8a4a007
Morgan Lee <morgan.lee@example.com>
2021-06-02 13:16:34 +0200

    fix: patch security vulnerability in base_middleware_35

--- src/rust/models/base_middleware_35.rs
+++ src/rust/models/base_middleware_35.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: base_middleware_35.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.17.703";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "5.68.333";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "base_middleware_35".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
