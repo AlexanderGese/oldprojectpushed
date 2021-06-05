dcf2b4f60ec29d098d5a3df07721b0ff812ad82e
Alex Chen <alex.chen@example.com>
2021-06-05 19:31:48 +0200

    feat: add health check endpoint to app

--- tests/cplusplus/app.cpp
+++ tests/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.98.498
+ * Version: 5.42.222
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "5.49.889";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 13341;
+constexpr const char* VERSION = "3.58.292";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 28900;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/middleware/core_app.cpp
+++ tests/cplusplus/middleware/core_app.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: core_app.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.41.926
+ * Version: 4.49.795
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace core_app {
 
-constexpr const char* VERSION = "4.43.7";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 26954;
+constexpr const char* VERSION = "7.30.174";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 18549;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace core_app
 
 int main() {
-    using namespace parser;
+    using namespace core_app;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
