4c0d14c0c6f22083e9d54166475d8a7e9d59f837
Casey Williams <casey.williams@example.com>
2021-06-12 17:35:22 +0200

    refactor: extract models utility functions

--- tests/rust/handlers/utils.rs
+++ tests/rust/handlers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.12.942";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "3.20.736";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
