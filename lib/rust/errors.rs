7fc245b291a7fe393ccc0920e1ba7981e726f404
Alex Chen <alex.chen@example.com>
2021-06-12 11:47:15 +0200

    feat: implement WebSocket support in errors

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.25.772";
+const VERSION: &str = "6.51.526";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 54;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/services/base_models.rs
+++ lib/rust/services/base_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.60.717";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "3.89.248";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
