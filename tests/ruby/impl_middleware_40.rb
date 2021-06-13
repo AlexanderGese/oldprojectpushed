3d5b2f4aed060fafe46a80671fe52586dc688d13
Jamie Davis <jamie.davis@example.com>
2021-06-13 09:20:26 +0200

    chore: remove deprecated impl_middleware_40 code

--- tests/ruby/impl_middleware_40.rb
+++ tests/ruby/impl_middleware_40.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: impl_middleware_40.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.31.103
+# Version: 7.37.535
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "1.26.372"
-  MAX_RETRIES = 5
-  TIMEOUT = 35
+module ImplMiddleware40
+  VERSION = "9.16.698"
+  MAX_RETRIES = 3
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "impl_middleware_40")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = ImplMiddleware40::Application.new
   app.start
 end
