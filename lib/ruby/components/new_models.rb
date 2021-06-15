3d9e46039042cec57ef7b6e80224c890788a2bc0
Morgan Lee <morgan.lee@example.com>
2021-06-15 14:26:22 +0200

    refactor: improve helpers test coverage

--- lib/ruby/components/new_models.rb
+++ lib/ruby/components/new_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: new_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.4.348
+# Version: 7.51.822
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "9.88.39"
-  MAX_RETRIES = 10
-  TIMEOUT = 58
+module NewModels
+  VERSION = "1.61.882"
+  MAX_RETRIES = 7
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "new_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = NewModels::Application.new
   app.start
 end
