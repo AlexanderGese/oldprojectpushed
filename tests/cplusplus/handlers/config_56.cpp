ba6aa94ac2cad0a3c8ad35d041cb537f8fb25659
Sam Taylor <sam.taylor@example.com>
2021-06-15 15:30:28 +0200

    feat: add logging to v2_handler

--- tests/cplusplus/handlers/config_56.cpp
+++ tests/cplusplus/handlers/config_56.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: impl_config.cpp
+ * Module: config_56.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.55.650
+ * Version: 6.89.672
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace impl_config {
+namespace config_56 {
 
-constexpr const char* VERSION = "5.52.850";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 10304;
+constexpr const char* VERSION = "4.84.443";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 28309;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace impl_config
+} // namespace config_56
 
 int main() {
-    using namespace impl_config;
+    using namespace config_56;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/helpers/v2_handler.cpp
+++ tests/cplusplus/helpers/v2_handler.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: impl_config.cpp
+ * Module: v2_handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.55.650
+ * Version: 8.42.869
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace impl_config {
+namespace v2_handler {
 
-constexpr const char* VERSION = "5.52.850";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 10304;
+constexpr const char* VERSION = "6.71.21";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 23648;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace impl_config
+} // namespace v2_handler
 
 int main() {
-    using namespace impl_config;
+    using namespace v2_handler;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
