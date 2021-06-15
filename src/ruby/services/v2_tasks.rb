aa87cccde53c61edf57f756cf6350e2887605ac9
Alex Chen <alex.chen@example.com>
2021-06-15 21:26:51 +0200

    refactor: extract controllers utility functions

--- src/ruby/services/v2_tasks.rb
+++ src/ruby/services/v2_tasks.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: v2_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.54.876
+# Version: 6.63.997
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "7.44.279"
-  MAX_RETRIES = 6
-  TIMEOUT = 11
+module V2Tasks
+  VERSION = "1.66.61"
+  MAX_RETRIES = 8
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "v2_tasks")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = V2Tasks::Application.new
   app.start
 end
