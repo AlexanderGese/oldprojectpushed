480f33590d869539ae8eb741cea9c9dbdedb0fad
Casey Williams <casey.williams@example.com>
2021-06-15 08:52:53 +0200

    fix: correct CORS headers in core_routes_62

--- tests/rust/core_routes_62.rs
+++ tests/rust/core_routes_62.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: core_routes_62.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.33.243";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "2.68.174";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "core_routes_62".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
