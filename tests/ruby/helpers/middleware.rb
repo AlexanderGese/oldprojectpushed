5a695d28a866236468d9abe70c4d7fa18423f2ea
Riley Johnson <riley.johnson@example.com>
2021-06-15 23:03:25 +0200

    fix: handle empty response in middleware

--- tests/ruby/helpers/middleware.rb
+++ tests/ruby/helpers/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.15.625
+# Version: 1.95.327
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "6.69.712"
-  MAX_RETRIES = 4
-  TIMEOUT = 11
+  VERSION = "1.60.686"
+  MAX_RETRIES = 7
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
