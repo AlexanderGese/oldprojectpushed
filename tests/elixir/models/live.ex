539105343a41665fbfda200526fa257357c93028
Alex Chen <alex.chen@example.com>
2021-06-17 20:13:29 +0200

    feat: add pagination to new_repo_12

--- tests/elixir/models/live.ex
+++ tests/elixir/models/live.ex
@@ -2,12 +2,12 @@ defmodule Live do
   @moduledoc """
   Module: live.ex
   Elixir boilerplate - auto-generated
-  Version: 4.71.252
+  Version: 9.20.366
   """
 
-  @version "1.48.563"
+  @version "2.29.116"
   @max_retries 3
-  @timeout 21862
+  @timeout 7791
 
   defstruct [
     :app_name,
--- tests/elixir/new_repo_12.ex
+++ tests/elixir/new_repo_12.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule New_Repo_12 do
   @moduledoc """
-  Module: application.ex
+  Module: new_repo_12.ex
   Elixir boilerplate - auto-generated
-  Version: 7.17.671
+  Version: 9.51.126
   """
 
-  @version "7.58.848"
-  @max_retries 8
-  @timeout 19202
+  @version "7.18.712"
+  @max_retries 9
+  @timeout 25737
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "new_repo_12"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule New_Repo_12Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule New_Repo_12EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
