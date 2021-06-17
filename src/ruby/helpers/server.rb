2de2383f21cdaecc519144ab5bb1f5d5b7aba82c
Alex Chen <alex.chen@example.com>
2021-06-17 15:58:12 +0200

    docs: add documentation for server

--- src/ruby/helpers/server.rb
+++ src/ruby/helpers/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.49.892
+# Version: 9.50.550
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "8.68.745"
-  MAX_RETRIES = 5
+  VERSION = "9.82.863"
+  MAX_RETRIES = 10
   TIMEOUT = 48
 
   class Config
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
