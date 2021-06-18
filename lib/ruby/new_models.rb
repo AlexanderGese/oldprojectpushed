cc2b1294f894d4cf73d78bce789c8dcc8f4b6913
Sam Taylor <sam.taylor@example.com>
2021-06-18 10:27:46 +0200

    fix: resolve timezone issue in v2_tasks

--- lib/ruby/new_models.rb
+++ lib/ruby/new_models.rb
@@ -2,7 +2,7 @@
 
 # Module: new_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.23.570
+# Version: 4.15.932
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewModels
-  VERSION = "7.31.393"
-  MAX_RETRIES = 10
-  TIMEOUT = 36
+  VERSION = "9.73.430"
+  MAX_RETRIES = 6
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
