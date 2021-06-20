3596f02dbc3cc9284e494734b79ca66cbd35dde8
Morgan Lee <morgan.lee@example.com>
2021-06-20 19:34:10 +0200

    fix: correct CORS headers in tasks

--- lib/ruby/services/tasks.rb
+++ lib/ruby/services/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.71.434
+# Version: 4.51.498
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "4.0.990"
-  MAX_RETRIES = 7
-  TIMEOUT = 55
+  VERSION = "3.62.633"
+  MAX_RETRIES = 3
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
