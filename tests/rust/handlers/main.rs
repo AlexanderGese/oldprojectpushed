829c0fc25e66b948797684bf42ef4a0c1c3a07ea
Jamie Davis <jamie.davis@example.com>
2021-06-25 10:11:40 +0200

    feat: add logging to config

--- tests/rust/handlers/main.rs
+++ tests/rust/handlers/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.1.818";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "2.99.456";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 28;
 
 #[derive(Debug, Clone)]
 pub struct Config {
