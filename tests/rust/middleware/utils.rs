bdf5af406cf3616ea249ae42287fc42ca566ec9e
Morgan Lee <morgan.lee@example.com>
2021-06-25 14:35:54 +0200

    feat: add health check endpoint to utils

--- tests/rust/middleware/utils.rs
+++ tests/rust/middleware/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.56.887";
+const VERSION: &str = "5.55.198";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 15;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
