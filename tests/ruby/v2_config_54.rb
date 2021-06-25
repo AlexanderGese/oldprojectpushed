5bffb5194e46c2e9655a96511102f99bf0da1c4f
Morgan Lee <morgan.lee@example.com>
2021-06-25 16:39:40 +0200

    chore: update new_tasks dependencies

--- tests/ruby/v2_config_54.rb
+++ tests/ruby/v2_config_54.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: v2_config_54.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.96.544
+# Version: 6.34.192
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "8.88.617"
-  MAX_RETRIES = 3
-  TIMEOUT = 26
+module V2Config54
+  VERSION = "4.79.646"
+  MAX_RETRIES = 5
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "v2_config_54")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = V2Config54::Application.new
   app.start
 end
