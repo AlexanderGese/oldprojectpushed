e1098b7b936f8cbc7e99fe54253014dd8b8a3a0d
Morgan Lee <morgan.lee@example.com>
2021-06-26 21:35:19 +0200

    feat: add export functionality to helpers

--- lib/ruby/models_18.rb
+++ lib/ruby/models_18.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: models_18.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.80.50
+# Version: 5.96.840
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "9.7.703"
-  MAX_RETRIES = 3
-  TIMEOUT = 52
+module Models18
+  VERSION = "4.87.145"
+  MAX_RETRIES = 5
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "models_18")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = Models18::Application.new
   app.start
 end
