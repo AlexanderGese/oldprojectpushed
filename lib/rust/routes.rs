ffa12e2f5c06779bb3d954e04dfdb346d569eb36
Sam Taylor <sam.taylor@example.com>
2021-06-27 23:55:28 +0200

    feat: add health check endpoint to main

--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.54.556";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "7.48.223";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
