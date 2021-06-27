f24b96d2e2e38f2ec43db5add42407aa0b045823
Drew Martin <drew.martin@example.com>
2021-06-27 08:45:09 +0200

    fix: handle large payloads in impl_app

--- tests/cplusplus/impl_app.cpp
+++ tests/cplusplus/impl_app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: impl_app.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.96.483
+ * Version: 1.17.340
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace impl_app {
 
-constexpr const char* VERSION = "5.54.161";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 17519;
+constexpr const char* VERSION = "3.37.288";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 28655;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
