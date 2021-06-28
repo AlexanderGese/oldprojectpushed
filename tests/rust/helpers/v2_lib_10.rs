14153fe2257801dba111eb0a0a49d27f57996e24
Alex Chen <alex.chen@example.com>
2021-06-28 13:41:37 +0200

    fix: patch security vulnerability in middleware

--- tests/rust/helpers/v2_lib_10.rs
+++ tests/rust/helpers/v2_lib_10.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: v2_lib_10.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.74.454";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "3.66.877";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "v2_lib_10".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
