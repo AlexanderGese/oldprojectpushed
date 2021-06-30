04c0fd62d4481c96dde63e676095bcb491971947
Casey Williams <casey.williams@example.com>
2021-06-30 14:56:22 +0200

    feat: add rate limiting to lib

--- tests/rust/v2_routes_20.rs
+++ tests/rust/v2_routes_20.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.93.104";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "9.11.383";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
