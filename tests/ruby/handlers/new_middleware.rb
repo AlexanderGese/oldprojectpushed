26b614e2c8a40c8c79587565a08f67665d1beacf
Riley Johnson <riley.johnson@example.com>
2021-07-02 17:14:35 +0200

    feat: add pagination to new_config_52

--- tests/ruby/handlers/new_middleware.rb
+++ tests/ruby/handlers/new_middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: new_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.47.269
+# Version: 3.6.543
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "8.79.285"
-  MAX_RETRIES = 3
-  TIMEOUT = 12
+module NewMiddleware
+  VERSION = "1.72.342"
+  MAX_RETRIES = 5
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "new_middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = NewMiddleware::Application.new
   app.start
 end
