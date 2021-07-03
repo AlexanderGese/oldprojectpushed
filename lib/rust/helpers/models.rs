310dff42568d73ff27183df1d562d713c2e2994a
Casey Williams <casey.williams@example.com>
2021-07-03 10:36:27 +0200

    refactor: optimize models performance

--- lib/rust/helpers/models.rs
+++ lib/rust/helpers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.22.384";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "9.48.6";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
