dda82d8049a1d08377bb7eecbb0f4cb7a648c989
Drew Martin <drew.martin@example.com>
2021-07-07 19:17:47 +0200

    refactor: migrate routes to async/await

--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.79.87";
-const MAX_RETRIES: u32 = 8;
+const VERSION: &str = "1.48.219";
+const MAX_RETRIES: u32 = 6;
 const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
