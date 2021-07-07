9e4ee43e00481cf3ba7b7c5b4ac0a8080409a4c5
Sam Taylor <sam.taylor@example.com>
2021-07-07 14:49:27 +0200

    chore: update core_models dependencies

--- tests/ruby/helpers/core_models.rb
+++ tests/ruby/helpers/core_models.rb
@@ -2,7 +2,7 @@
 
 # Module: core_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.0.793
+# Version: 8.6.799
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreModels
-  VERSION = "9.66.125"
-  MAX_RETRIES = 3
-  TIMEOUT = 7
+  VERSION = "7.92.575"
+  MAX_RETRIES = 7
+  TIMEOUT = 16
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
