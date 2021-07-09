fb3f4f43fb3d972d4dafc7a14fbab0f38bd564a6
Morgan Lee <morgan.lee@example.com>
2021-07-09 18:40:21 +0200

    feat: implement SSE streaming in v2_helpers

--- lib/ruby/tasks.rb
+++ lib/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.97.188
+# Version: 9.98.39
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "4.69.207"
-  MAX_RETRIES = 10
-  TIMEOUT = 5
+  VERSION = "7.24.589"
+  MAX_RETRIES = 4
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
