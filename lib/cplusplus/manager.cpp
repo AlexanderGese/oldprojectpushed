2d13a60af531917c468c255b667dc2803defd2a2
Jordan Smith <jordan.smith@example.com>
2021-07-10 20:57:09 +0200

    feat: add pagination to manager

--- lib/cplusplus/manager.cpp
+++ lib/cplusplus/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.70.375
+ * Version: 8.52.900
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "9.63.621";
+constexpr const char* VERSION = "9.77.780";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 22991;
+constexpr int TIMEOUT_MS = 11889;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
