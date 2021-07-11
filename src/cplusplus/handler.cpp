4a74fad1a7daf0eb212c1326129de14a23599d38
Jamie Davis <jamie.davis@example.com>
2021-07-11 19:44:47 +0200

    feat: implement retry logic in handler

--- src/cplusplus/handler.cpp
+++ src/cplusplus/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.44.459
+ * Version: 7.76.694
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "8.99.829";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 26382;
+constexpr const char* VERSION = "8.11.652";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 20915;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
