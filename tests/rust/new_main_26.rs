bbac6da6df43b9dbcd8df9cd573321fc50ff6ac0
Jordan Smith <jordan.smith@example.com>
2021-07-12 11:53:53 +0200

    chore: bump database version

--- tests/rust/new_main_26.rs
+++ tests/rust/new_main_26.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: new_main_26.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.7.283";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "5.77.524";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "new_main_26".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
