27c52d1ad8292d3c4ac4ced1297a4d9df75e4aa4
Jamie Davis <jamie.davis@example.com>
2021-07-12 20:48:13 +0200

    fix: handle large payloads in database

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.80.915";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "9.50.597";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
