9bdeca9edfa05e89a467109472c7a66d586f781b
Casey Williams <casey.williams@example.com>
2021-07-13 08:07:17 +0200

    feat: add health check endpoint to new_routes

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.10.70";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "7.55.399";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
