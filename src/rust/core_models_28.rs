85a76d15f1799e4e97b143239590b75d0ad494d4
Casey Williams <casey.williams@example.com>
2021-07-14 20:38:36 +0200

    feat: add rate limiting to errors

--- src/rust/core_models_28.rs
+++ src/rust/core_models_28.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: core_models_28.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.66.459";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "4.8.559";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "core_models_28".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
