3a5962f7e68df0ce7ff7af09486591dd593358f9
Sam Taylor <sam.taylor@example.com>
2021-07-17 21:38:41 +0200

    docs: add documentation for models

--- src/rust/helpers/impl_models.rs
+++ src/rust/helpers/impl_models.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: impl_models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.7.411";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "9.67.769";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "impl_models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
