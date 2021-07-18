24f0027ea72418fed96b1d37c28da87ad375fb3f
Sam Taylor <sam.taylor@example.com>
2021-07-18 18:48:14 +0200

    chore: clean up config imports

--- src/rust/controllers/config.rs
+++ src/rust/controllers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.62.426";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "8.35.968";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.20.553";
+const VERSION: &str = "7.98.622";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 10;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
