a6d86456326f82a84026fb9e6aec1c4a4e6cc78d
Riley Johnson <riley.johnson@example.com>
2021-07-22 15:00:32 +0200

    chore: update base_schema_64 configuration

--- tests/elixir/context_66.ex
+++ tests/elixir/context_66.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule Context_66 do
   @moduledoc """
-  Module: live.ex
+  Module: context_66.ex
   Elixir boilerplate - auto-generated
-  Version: 4.42.242
+  Version: 2.77.515
   """
 
-  @version "9.65.229"
-  @max_retries 3
-  @timeout 25597
+  @version "3.51.52"
+  @max_retries 7
+  @timeout 13858
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "context_66"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule Context_66Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule Context_66EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/controllers/base_schema_64.ex
+++ tests/elixir/controllers/base_schema_64.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule Base_Schema_64 do
   @moduledoc """
-  Module: live.ex
+  Module: base_schema_64.ex
   Elixir boilerplate - auto-generated
-  Version: 4.42.242
+  Version: 6.57.265
   """
 
-  @version "9.65.229"
-  @max_retries 3
-  @timeout 25597
+  @version "7.45.460"
+  @max_retries 5
+  @timeout 19943
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "base_schema_64"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule Base_Schema_64Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule Base_Schema_64EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
