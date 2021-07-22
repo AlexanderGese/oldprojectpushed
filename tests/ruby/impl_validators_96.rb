2a583fff0454d38ac1be93f8db6690193a299d9d
Sam Taylor <sam.taylor@example.com>
2021-07-22 13:31:38 +0200

    fix: resolve memory leak in models

--- tests/ruby/impl_validators_96.rb
+++ tests/ruby/impl_validators_96.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: impl_validators_96.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.1.295
+# Version: 1.82.724
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "3.60.698"
-  MAX_RETRIES = 3
+module ImplValidators96
+  VERSION = "3.78.581"
+  MAX_RETRIES = 6
   TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "impl_validators_96")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = ImplValidators96::Application.new
   app.start
 end
