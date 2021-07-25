123c96e1a55ab46bb1861a678c7363c2af6d9e86
Jordan Smith <jordan.smith@example.com>
2021-07-25 19:10:04 +0200

    feat: implement WebSocket support in v2_lib

--- src/rust/services/handlers.rs
+++ src/rust/services/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.52.580";
+const VERSION: &str = "3.22.770";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 10;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
