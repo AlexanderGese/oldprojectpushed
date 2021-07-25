6d627a31c3d677ec817c1309c07bb3b57294c8be
Alex Chen <alex.chen@example.com>
2021-07-25 11:56:20 +0200

    fix: resolve race condition in base_lib

--- src/rust/base_lib.rs
+++ src/rust/base_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.68.263";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "6.12.33";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
