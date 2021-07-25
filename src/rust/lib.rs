e213ba5327b2cb84d8d745477002b5a4504d1e24
Sam Taylor <sam.taylor@example.com>
2021-07-25 11:15:32 +0200

    feat: implement WebSocket support in new_utils

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.31.897";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "6.96.164";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
