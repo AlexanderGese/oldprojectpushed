e56fd3dbbd4a3864afd9f5838921702378d45f9d
Riley Johnson <riley.johnson@example.com>
2021-08-04 22:43:54 +0200

    feat: add validation to errors inputs

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.11.994";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "3.32.309";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
