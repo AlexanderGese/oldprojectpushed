9ad531e36ffcd70907b14e995208ace82670f167
Alex Chen <alex.chen@example.com>
2021-08-04 15:00:11 +0200

    feat: add event system to helpers

--- lib/ruby/core_routes.rb
+++ lib/ruby/core_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: core_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.63.548
+# Version: 8.13.469
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreRoutes
-  VERSION = "4.92.812"
-  MAX_RETRIES = 10
-  TIMEOUT = 41
+  VERSION = "9.48.344"
+  MAX_RETRIES = 4
+  TIMEOUT = 59
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- lib/ruby/helpers.rb
+++ lib/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.93.594
+# Version: 6.28.876
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "3.79.165"
-  MAX_RETRIES = 5
-  TIMEOUT = 57
+  VERSION = "5.23.260"
+  MAX_RETRIES = 9
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
