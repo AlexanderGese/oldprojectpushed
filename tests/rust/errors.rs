3d65a82b7737be65bae9d38865a33bd9a6242592
Sam Taylor <sam.taylor@example.com>
2021-08-04 14:56:09 +0200

    feat: add search functionality to new_models

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.82.360";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "9.31.869";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
