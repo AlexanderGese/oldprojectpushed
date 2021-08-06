46beaccd86795319761533e9cf3364fe7f4e9f0c
Jamie Davis <jamie.davis@example.com>
2021-08-06 13:48:50 +0200

    feat: add batch processing to impl_errors

--- src/rust/impl_errors.rs
+++ src/rust/impl_errors.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.78.805";
-const MAX_RETRIES: u32 = 6;
+const VERSION: &str = "4.89.997";
+const MAX_RETRIES: u32 = 10;
 const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
--- src/rust/services/database.rs
+++ src/rust/services/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.0.268";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "9.39.396";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
