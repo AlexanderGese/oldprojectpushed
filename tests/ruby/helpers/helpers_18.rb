23fe2b643739a2ae8c23ddd376b55f76d575108d
Jamie Davis <jamie.davis@example.com>
2021-08-07 22:27:46 +0200

    feat: add caching layer to v2_tasks

--- tests/ruby/helpers/helpers_18.rb
+++ tests/ruby/helpers/helpers_18.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: helpers_18.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.64.275
+# Version: 1.50.751
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "7.0.809"
+module Helpers18
+  VERSION = "5.73.781"
   MAX_RETRIES = 9
-  TIMEOUT = 9
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "helpers_18")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = Helpers18::Application.new
   app.start
 end
--- tests/ruby/utils/impl_server.rb
+++ tests/ruby/utils/impl_server.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.76.919
+# Version: 6.50.530
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplServer
-  VERSION = "3.78.872"
+  VERSION = "4.41.462"
   MAX_RETRIES = 7
-  TIMEOUT = 10
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
