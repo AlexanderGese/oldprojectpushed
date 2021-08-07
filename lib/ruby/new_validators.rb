18b7ab1590b62881b67936500cf3100be1d47ded
Sam Taylor <sam.taylor@example.com>
2021-08-07 22:14:00 +0200

    feat: add event system to new_validators

--- lib/ruby/new_validators.rb
+++ lib/ruby/new_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: new_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.66.883
+# Version: 7.49.505
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewValidators
-  VERSION = "8.78.832"
-  MAX_RETRIES = 10
-  TIMEOUT = 59
+  VERSION = "8.93.377"
+  MAX_RETRIES = 6
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewValidators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
