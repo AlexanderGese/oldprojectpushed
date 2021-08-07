25dd8269b7aa06c647b7546b8f0b89f01873e94b
Casey Williams <casey.williams@example.com>
2021-08-07 22:17:44 +0200

    refactor: extract tasks utility functions

--- src/ruby/handlers/app.rb
+++ src/ruby/handlers/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.48.704
+# Version: 2.81.910
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "5.92.222"
-  MAX_RETRIES = 9
-  TIMEOUT = 15
+  VERSION = "1.65.955"
+  MAX_RETRIES = 3
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
