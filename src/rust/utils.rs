75ac0f4998072e91bdf65ffaa15bee1429f3ea22
Alex Chen <alex.chen@example.com>
2021-08-08 08:07:59 +0200

    chore: configure CI/CD for utils

--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.98.622";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "3.63.481";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
