8fdac6c868500d1645fa77be305954edc0e828df
Drew Martin <drew.martin@example.com>
2021-08-09 10:43:23 +0200

    fix: handle empty response in controller

--- tests/elixir/repo_78.ex
+++ tests/elixir/repo_78.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Repo_78 do
   @moduledoc """
-  Module: controller.ex
+  Module: repo_78.ex
   Elixir boilerplate - auto-generated
-  Version: 6.79.676
+  Version: 4.26.788
   """
 
-  @version "8.51.69"
-  @max_retries 7
-  @timeout 23231
+  @version "6.32.873"
+  @max_retries 6
+  @timeout 21531
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "repo_78"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Repo_78Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Repo_78EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
