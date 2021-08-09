405d25cd0810b583f07cac1a4794194a35b32fc8
Drew Martin <drew.martin@example.com>
2021-08-09 20:25:29 +0200

    chore: bump new_helpers_96 version

--- src/ruby/handlers/config.rb
+++ src/ruby/handlers/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.96.918
+# Version: 3.5.402
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "7.31.470"
-  MAX_RETRIES = 5
-  TIMEOUT = 49
+  VERSION = "9.51.995"
+  MAX_RETRIES = 6
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- src/ruby/middleware_27.rb
+++ src/ruby/middleware_27.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_tasks.rb
+# Module: middleware_27.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.6.121
+# Version: 8.35.621
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewTasks
-  VERSION = "3.3.497"
-  MAX_RETRIES = 5
-  TIMEOUT = 54
+module Middleware27
+  VERSION = "1.32.802"
+  MAX_RETRIES = 3
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_tasks")
+      @app_name = opts.fetch(:app_name, "middleware_27")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewTasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewTasks::Application.new
+  app = Middleware27::Application.new
   app.start
 end
