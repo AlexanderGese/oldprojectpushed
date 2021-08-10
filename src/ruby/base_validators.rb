155892328b31d21c93463db54303241ab144f19c
Sam Taylor <sam.taylor@example.com>
2021-08-10 15:11:15 +0200

    chore: clean up base_validators imports

--- src/ruby/base_validators.rb
+++ src/ruby/base_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: base_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.61.720
+# Version: 6.11.665
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseValidators
-  VERSION = "4.81.190"
-  MAX_RETRIES = 3
-  TIMEOUT = 20
+  VERSION = "6.94.986"
+  MAX_RETRIES = 5
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseValidators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
