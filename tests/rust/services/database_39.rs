01193acefb0b63c5c7b954ca66c7c7367063ee15
Morgan Lee <morgan.lee@example.com>
2021-08-11 21:36:04 +0200

    feat: implement SSE streaming in database_39

--- tests/rust/services/database_39.rs
+++ tests/rust/services/database_39.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: database_39.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.18.905";
-const MAX_RETRIES: u32 = 3;
+const VERSION: &str = "9.66.273";
+const MAX_RETRIES: u32 = 6;
 const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "database_39".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
