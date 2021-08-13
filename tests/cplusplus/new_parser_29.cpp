ca86ef29500eb339998aeb0c72e054d401547b91
Drew Martin <drew.martin@example.com>
2021-08-13 22:10:28 +0200

    feat: implement retry logic in config

--- tests/cplusplus/new_parser_29.cpp
+++ tests/cplusplus/new_parser_29.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: config.cpp
+ * Module: new_parser_29.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.37.578
+ * Version: 9.23.661
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace config {
+namespace new_parser_29 {
 
-constexpr const char* VERSION = "4.17.351";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 13943;
+constexpr const char* VERSION = "8.50.511";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 14946;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace config
+} // namespace new_parser_29
 
 int main() {
-    using namespace config;
+    using namespace new_parser_29;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
