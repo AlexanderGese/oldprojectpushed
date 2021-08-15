8367bec70ea54c27677788f46ad7b6e5d8b45519
Sam Taylor <sam.taylor@example.com>
2021-08-15 18:18:13 +0200

    refactor: optimize core_router_88 performance

--- tests/elixir/core_router_88.ex
+++ tests/elixir/core_router_88.ex
@@ -1,13 +1,13 @@
-defmodule Base_Schema do
+defmodule Core_Router_88 do
   @moduledoc """
-  Module: base_schema.ex
+  Module: core_router_88.ex
   Elixir boilerplate - auto-generated
-  Version: 5.90.504
+  Version: 8.60.596
   """
 
-  @version "6.92.742"
-  @max_retries 3
-  @timeout 12287
+  @version "4.36.43"
+  @max_retries 6
+  @timeout 17374
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Base_Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "base_schema"),
+      app_name: Keyword.get(opts, :app_name, "core_router_88"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Base_Schema do
   end
 end
 
-defmodule Base_SchemaStore do
+defmodule Core_Router_88Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Base_SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Base_SchemaEventBus do
+defmodule Core_Router_88EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
