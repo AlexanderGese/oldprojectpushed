da70950da86cd855fc3742b65689e32a37e04a79
Sam Taylor <sam.taylor@example.com>
2021-08-17 08:37:31 +0200

    feat: add event system to impl_tasks

--- lib/ruby/models_62.rb
+++ lib/ruby/models_62.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_tasks.rb
+# Module: models_62.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.66.987
+# Version: 6.31.596
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplTasks
-  VERSION = "1.67.655"
-  MAX_RETRIES = 6
-  TIMEOUT = 58
+module Models62
+  VERSION = "2.77.170"
+  MAX_RETRIES = 4
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_tasks")
+      @app_name = opts.fetch(:app_name, "models_62")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplTasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplTasks::Application.new
+  app = Models62::Application.new
   app.start
 end
