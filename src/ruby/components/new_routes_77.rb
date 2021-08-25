d77c319d9bea0b5c0f96cb2d561c82db47fb0489
Casey Williams <casey.williams@example.com>
2021-08-25 19:12:55 +0200

    feat: add export functionality to v2_controllers

--- src/ruby/components/new_routes_77.rb
+++ src/ruby/components/new_routes_77.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_controllers.rb
+# Module: new_routes_77.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.37.470
+# Version: 4.52.85
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Controllers
-  VERSION = "6.69.281"
-  MAX_RETRIES = 8
-  TIMEOUT = 25
+module NewRoutes77
+  VERSION = "3.27.804"
+  MAX_RETRIES = 9
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_controllers")
+      @app_name = opts.fetch(:app_name, "new_routes_77")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module V2Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Controllers::Application.new
+  app = NewRoutes77::Application.new
   app.start
 end
