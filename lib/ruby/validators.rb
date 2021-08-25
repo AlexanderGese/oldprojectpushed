0ff9b347ec06babbb08ee8bed6122a68e239de5c
Casey Williams <casey.williams@example.com>
2021-08-25 12:05:30 +0200

    chore: update models configuration

--- lib/ruby/validators.rb
+++ lib/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.43.19
+# Version: 8.27.158
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "5.4.942"
+  VERSION = "8.13.632"
   MAX_RETRIES = 6
-  TIMEOUT = 16
+  TIMEOUT = 59
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
