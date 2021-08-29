2334d90e9bfb1e63c01fdf1591b47fad0982bcb4
Riley Johnson <riley.johnson@example.com>
2021-08-29 23:48:49 +0200

    feat: implement impl_middleware service layer

--- tests/rust/services/core_routes.rs
+++ tests/rust/services/core_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.49.595";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "7.81.776";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
