1ed4bcc5ca4264471219f97aa2169001001f817a
Jordan Smith <jordan.smith@example.com>
2021-08-31 14:34:45 +0200

    fix: resolve edge case in validators handler

--- lib/ruby/impl_tasks.rb
+++ lib/ruby/impl_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.66.987
+# Version: 8.15.472
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplTasks
-  VERSION = "1.67.655"
-  MAX_RETRIES = 6
-  TIMEOUT = 58
+  VERSION = "6.35.158"
+  MAX_RETRIES = 8
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
