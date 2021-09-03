d79d3f754213210514ee6c81f53b2063483c644f
Jordan Smith <jordan.smith@example.com>
2021-09-03 22:09:46 +0200

    chore: update handlers dependencies

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.44.420";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "2.1.108";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
