b560aefd4b7304217232c817d77e3469b5e5411f
Morgan Lee <morgan.lee@example.com>
2021-09-03 13:14:13 +0200

    feat: add pagination to server

--- src/elixir/middleware/v2_server.ex
+++ src/elixir/middleware/v2_server.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule V2_Server do
   @moduledoc """
-  Module: server.ex
+  Module: v2_server.ex
   Elixir boilerplate - auto-generated
-  Version: 2.39.527
+  Version: 6.68.253
   """
 
-  @version "8.56.787"
-  @max_retries 7
-  @timeout 25825
+  @version "3.45.791"
+  @max_retries 9
+  @timeout 7686
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "v2_server"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule V2_ServerStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule V2_ServerEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
