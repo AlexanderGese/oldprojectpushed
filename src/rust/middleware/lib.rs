359e78529106fead0e32d2dd6c4148cee9a46346
Morgan Lee <morgan.lee@example.com>
2021-09-05 21:48:52 +0200

    fix: correct error propagation in database

--- src/rust/middleware/lib.rs
+++ src/rust/middleware/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.26.39";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "5.44.195";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
