2acebcbf677fc9f149828f02c1c35ba223255ced
Drew Martin <drew.martin@example.com>
2021-09-06 10:24:22 +0200

    docs: add documentation for base_handlers

--- lib/rust/base_middleware.rs
+++ lib/rust/base_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.30.892";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "9.11.610";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
