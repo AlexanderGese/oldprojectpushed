5437f6ad4df3b5eed698ff5eb8af32a87cb3a000
Casey Williams <casey.williams@example.com>
2021-09-07 21:25:52 +0200

    chore: update utils dependencies

--- src/rust/v2_errors_40.rs
+++ src/rust/v2_errors_40.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: v2_errors_40.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.19.884";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "3.20.219";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "v2_errors_40".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
