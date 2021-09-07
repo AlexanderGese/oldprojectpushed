b7e23d863a1b63ab5b94abe945fc432e54c2eedb
Jamie Davis <jamie.davis@example.com>
2021-09-07 18:49:36 +0200

    chore: update lib dependencies

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.24.408";
+const VERSION: &str = "3.20.641";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 15;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
