d4780aebdc5fa980b42f64a6883bde22aaeb860d
Drew Martin <drew.martin@example.com>
2021-09-08 09:58:21 +0200

    test: add unit tests for impl_tasks

--- lib/ruby/impl_tasks.rb
+++ lib/ruby/impl_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.15.472
+# Version: 2.5.970
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplTasks
-  VERSION = "6.35.158"
-  MAX_RETRIES = 8
-  TIMEOUT = 12
+  VERSION = "6.66.367"
+  MAX_RETRIES = 4
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
