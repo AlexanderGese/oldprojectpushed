697926f160a81330a7ae71a1b54ac2cb54cf55c6
Alex Chen <alex.chen@example.com>
2021-09-09 17:41:01 +0200

    chore: update new_server dependencies

--- src/ruby/new_server.rb
+++ src/ruby/new_server.rb
@@ -2,7 +2,7 @@
 
 # Module: new_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.52.646
+# Version: 3.37.793
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewServer
-  VERSION = "9.75.645"
-  MAX_RETRIES = 8
-  TIMEOUT = 50
+  VERSION = "5.91.665"
+  MAX_RETRIES = 6
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
