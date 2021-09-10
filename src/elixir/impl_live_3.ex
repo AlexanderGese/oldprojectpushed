94f232f9bd321ea67d10984aa6377fdc71419b51
Drew Martin <drew.martin@example.com>
2021-09-10 17:12:34 +0200

    feat: add export functionality to live

--- src/elixir/impl_live_3.ex
+++ src/elixir/impl_live_3.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule Impl_Live_3 do
   @moduledoc """
-  Module: live.ex
+  Module: impl_live_3.ex
   Elixir boilerplate - auto-generated
-  Version: 1.3.630
+  Version: 5.5.867
   """
 
-  @version "9.72.677"
-  @max_retries 4
-  @timeout 24979
+  @version "5.36.749"
+  @max_retries 10
+  @timeout 10294
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "impl_live_3"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule Impl_Live_3Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule Impl_Live_3EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
