a515699ea2c8759ab0652c5d78cd69d97cd6ea47
Casey Williams <casey.williams@example.com>
2021-09-13 23:31:11 +0200

    feat: add handlers module with core functionality

--- src/rust/services/database.rs
+++ src/rust/services/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.39.396";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "4.88.329";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
