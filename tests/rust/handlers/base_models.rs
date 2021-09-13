add12156b6242806e40e05ff2cdd0e5270a35f04
Jamie Davis <jamie.davis@example.com>
2021-09-13 08:19:50 +0200

    feat: add search functionality to base_models

--- tests/rust/handlers/base_models.rs
+++ tests/rust/handlers/base_models.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: base_models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.81.100";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "5.74.342";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "base_models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/v2_models.rs
+++ tests/rust/v2_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.63.262";
+const VERSION: &str = "6.25.525";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 16;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
