37a1ad23cea8889eb9602a62817c2bf42b1557e8
Sam Taylor <sam.taylor@example.com>
2021-09-17 15:28:37 +0200

    refactor: improve utils error messages

--- tests/cplusplus/handlers/main_56.cpp
+++ tests/cplusplus/handlers/main_56.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: main_56.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.20.915
+ * Version: 5.65.62
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace main_56 {
 
-constexpr const char* VERSION = "1.42.460";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 25635;
+constexpr const char* VERSION = "2.59.228";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 29026;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace main_56
 
 int main() {
-    using namespace utils;
+    using namespace main_56;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
