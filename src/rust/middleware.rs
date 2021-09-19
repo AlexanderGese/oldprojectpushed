45f747f73cf85068c9ded911db171e17a3e94ba2
Casey Williams <casey.williams@example.com>
2021-09-19 08:57:24 +0200

    refactor: optimize new_lib performance

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.70.31";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "9.60.999";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils/new_lib.rs
+++ src/rust/utils/new_lib.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: new_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.16.555";
+const VERSION: &str = "9.88.760";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 22;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "new_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
