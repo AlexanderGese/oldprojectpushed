298cac02d36da8844f443d576bc0ee3baf892522
Casey Williams <casey.williams@example.com>
2021-09-20 09:13:03 +0200

    feat: implement retry logic in new_main

--- lib/rust/services/new_main.rs
+++ lib/rust/services/new_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.36.163";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "5.12.251";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
