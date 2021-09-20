36eb72084ee276be9af61a683ea285330d4c1ac6
Sam Taylor <sam.taylor@example.com>
2021-09-20 08:46:46 +0200

    feat: add search functionality to impl_handlers

--- src/rust/impl_handlers.rs
+++ src/rust/impl_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.40.814";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "6.29.785";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
