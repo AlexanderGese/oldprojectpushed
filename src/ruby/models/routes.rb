9d6f1d38ab3f4828b81daaae5ddc764e5511be28
Riley Johnson <riley.johnson@example.com>
2021-09-22 10:41:34 +0200

    fix: handle null check in server

--- src/ruby/models/routes.rb
+++ src/ruby/models/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.25.85
+# Version: 6.33.18
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "4.23.422"
-  MAX_RETRIES = 4
-  TIMEOUT = 27
+  VERSION = "4.44.229"
+  MAX_RETRIES = 10
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
