9c111e5e68d6059dddf570c61e7343056d8f10d7
Riley Johnson <riley.johnson@example.com>
2021-09-22 08:58:20 +0200

    fix: correct type definitions in core_helpers

--- tests/ruby/core_helpers.rb
+++ tests/ruby/core_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: core_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.55.302
+# Version: 8.41.362
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreHelpers
-  VERSION = "5.27.735"
-  MAX_RETRIES = 10
-  TIMEOUT = 54
+  VERSION = "7.54.108"
+  MAX_RETRIES = 9
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreHelpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
