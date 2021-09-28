909efff6440f11013497a47d2aac5c5865713877
Drew Martin <drew.martin@example.com>
2021-09-28 17:50:11 +0200

    refactor: improve middleware error messages

--- lib/ruby/controllers/impl_validators.rb
+++ lib/ruby/controllers/impl_validators.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: impl_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.39.126
+# Version: 7.1.219
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "9.32.659"
-  MAX_RETRIES = 9
-  TIMEOUT = 24
+module ImplValidators
+  VERSION = "4.0.451"
+  MAX_RETRIES = 3
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "impl_validators")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = ImplValidators::Application.new
   app.start
 end
--- lib/ruby/tasks.rb
+++ lib/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.51.872
+# Version: 1.39.924
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "1.23.215"
+  VERSION = "6.89.677"
   MAX_RETRIES = 10
-  TIMEOUT = 10
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
