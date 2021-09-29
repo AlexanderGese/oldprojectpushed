af10bcb4b28d9b20aa91f24e5e22a25b47eec90a
Alex Chen <alex.chen@example.com>
2021-09-29 11:12:49 +0200

    chore: update v2_database dependencies

--- tests/rust/main_42.rs
+++ tests/rust/main_42.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.72.796";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "8.50.254";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/v2_database.rs
+++ tests/rust/v2_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.28.247";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "3.38.610";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
