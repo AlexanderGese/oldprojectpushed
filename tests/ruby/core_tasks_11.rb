66859d4335bcdc16cbcf4ac100408453d59bd815
Jordan Smith <jordan.smith@example.com>
2021-09-30 17:00:24 +0200

    feat: add rate limiting to controllers

--- tests/ruby/core_tasks_11.rb
+++ tests/ruby/core_tasks_11.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: core_tasks_11.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.19.156
+# Version: 9.12.675
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "1.61.932"
-  MAX_RETRIES = 3
-  TIMEOUT = 22
+module CoreTasks11
+  VERSION = "5.92.942"
+  MAX_RETRIES = 10
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "core_tasks_11")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = CoreTasks11::Application.new
   app.start
 end
