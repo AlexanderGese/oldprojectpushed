c629f0a9c5356edb8033b7d2bbb9d130e4d13a0d
Alex Chen <alex.chen@example.com>
2021-10-02 16:18:45 +0200

    fix: correct type definitions in handlers

--- src/rust/services/handlers.rs
+++ src/rust/services/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.22.770";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "7.39.560";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
