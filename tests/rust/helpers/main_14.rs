1a3b3f9ea4a003fab6da5490eba0b4e506f1c342
Drew Martin <drew.martin@example.com>
2021-10-03 09:18:12 +0200

    feat: add export functionality to main_14

--- tests/rust/helpers/main_14.rs
+++ tests/rust/helpers/main_14.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: main_14.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.92.669";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "7.56.189";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "main_14".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
