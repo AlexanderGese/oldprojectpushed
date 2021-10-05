b0ea3fc3c2379621b04d00f0e426085c7d28839c
Jamie Davis <jamie.davis@example.com>
2021-10-05 11:26:47 +0200

    feat: implement handlers service layer

--- lib/rust/services/handlers.rs
+++ lib/rust/services/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.11.902";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "2.38.589";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
