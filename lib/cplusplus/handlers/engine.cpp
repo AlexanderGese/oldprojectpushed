c92c159c1bcaebcc8d0859f5fc040ce0c1455d6d
Morgan Lee <morgan.lee@example.com>
2021-10-05 08:19:45 +0200

    fix: correct type definitions in engine

--- lib/cplusplus/handlers/engine.cpp
+++ lib/cplusplus/handlers/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.20.877
+ * Version: 8.31.502
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "2.70.643";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 24130;
+constexpr const char* VERSION = "8.92.695";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 10944;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
