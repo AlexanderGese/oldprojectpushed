8b33bbe0e846d84a117d4176997cd27bbd990a8d
Sam Taylor <sam.taylor@example.com>
2021-10-05 16:55:46 +0200

    fix: correct type definitions in manager

--- tests/cplusplus/components/core_engine_69.cpp
+++ tests/cplusplus/components/core_engine_69.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: main.cpp
+ * Module: core_engine_69.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.26.3
+ * Version: 8.60.579
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace main {
+namespace core_engine_69 {
 
-constexpr const char* VERSION = "7.36.687";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 9614;
+constexpr const char* VERSION = "2.27.60";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 5585;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace main
+} // namespace core_engine_69
 
 int main() {
-    using namespace main;
+    using namespace core_engine_69;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/manager.cpp
+++ tests/cplusplus/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.21.376
+ * Version: 9.60.1
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "3.44.38";
+constexpr const char* VERSION = "2.35.430";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 25585;
+constexpr int TIMEOUT_MS = 21159;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
