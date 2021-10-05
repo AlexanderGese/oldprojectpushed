6f7be43fd067be89840ceb507fbec7769cdaea9c
Sam Taylor <sam.taylor@example.com>
2021-10-05 09:55:12 +0200

    fix: handle concurrent access in new_models_81

--- src/ruby/new_models_81.rb
+++ src/ruby/new_models_81.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_app.rb
+# Module: new_models_81.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.63.218
+# Version: 8.71.736
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreApp
-  VERSION = "6.28.628"
+module NewModels81
+  VERSION = "6.21.552"
   MAX_RETRIES = 9
-  TIMEOUT = 14
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_app")
+      @app_name = opts.fetch(:app_name, "new_models_81")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreApp
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreApp::Application.new
+  app = NewModels81::Application.new
   app.start
 end
