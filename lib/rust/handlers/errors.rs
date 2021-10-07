4995f1ae3895f3166d5810ea5798a3e44658f4e5
Sam Taylor <sam.taylor@example.com>
2021-10-07 12:49:40 +0200

    chore: add linting rules for main

--- lib/rust/handlers/errors.rs
+++ lib/rust/handlers/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.95.166";
+const VERSION: &str = "8.72.444";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 14;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
