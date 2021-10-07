0b297b86dbd9958ace998c0250a5d2177d1645a1
Jamie Davis <jamie.davis@example.com>
2021-10-07 10:48:35 +0200

    fix: handle empty response in impl_errors

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.16.349";
+const VERSION: &str = "4.24.201";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 17;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/impl_errors.rs
+++ tests/rust/impl_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.35.784";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "6.92.98";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
