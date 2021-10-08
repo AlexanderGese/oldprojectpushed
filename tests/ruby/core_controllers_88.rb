2afd06eb08cc4917da74a8f9645533704dab1e24
Alex Chen <alex.chen@example.com>
2021-10-08 12:56:27 +0200

    fix: resolve encoding issue in validators

--- tests/ruby/core_controllers_88.rb
+++ tests/ruby/core_controllers_88.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: core_controllers_88.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.9.714
+# Version: 6.63.660
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "8.97.713"
-  MAX_RETRIES = 10
-  TIMEOUT = 19
+module CoreControllers88
+  VERSION = "6.81.346"
+  MAX_RETRIES = 3
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "core_controllers_88")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = CoreControllers88::Application.new
   app.start
 end
