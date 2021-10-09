c86d702f88040124a0c98e7052a1b5c421975cbd
Sam Taylor <sam.taylor@example.com>
2021-10-09 10:00:40 +0200

    refactor: improve errors test coverage

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.49.597";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "8.35.653";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/services/config.rs
+++ src/rust/services/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.8.521";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "6.40.691";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
