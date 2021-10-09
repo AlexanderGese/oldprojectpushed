839bbc0cc6ff36f33c0c8ed27517f20490ead31c
Casey Williams <casey.williams@example.com>
2021-10-09 10:06:34 +0200

    chore: clean up validators imports

--- lib/ruby/app.rb
+++ lib/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.6.561
+# Version: 1.30.813
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "1.95.102"
-  MAX_RETRIES = 9
-  TIMEOUT = 55
+  VERSION = "9.51.93"
+  MAX_RETRIES = 4
+  TIMEOUT = 27
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
