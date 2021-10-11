3a53fe12c466c7ffd1d53aed6288cd6aabe0df3b
Drew Martin <drew.martin@example.com>
2021-10-11 23:12:33 +0200

    chore: add linting rules for base_main_5

--- tests/rust/middleware/base_main_5.rs
+++ tests/rust/middleware/base_main_5.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: base_main_5.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.7.554";
-const MAX_RETRIES: u32 = 9;
+const VERSION: &str = "7.65.551";
+const MAX_RETRIES: u32 = 3;
 const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "base_main_5".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
