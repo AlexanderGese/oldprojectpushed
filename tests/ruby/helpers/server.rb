e7543f03f5c4cf13dac278c64201a9dbcc58fb52
Casey Williams <casey.williams@example.com>
2021-10-14 17:05:00 +0200

    feat: implement authentication in validators_54

--- tests/ruby/helpers/server.rb
+++ tests/ruby/helpers/server.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.81.275
+# Version: 3.28.186
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "8.30.654"
-  MAX_RETRIES = 6
-  TIMEOUT = 43
+module Server
+  VERSION = "5.85.62"
+  MAX_RETRIES = 7
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "server")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Server::Application.new
   app.start
 end
--- tests/ruby/validators_54.rb
+++ tests/ruby/validators_54.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: validators_54.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.81.275
+# Version: 1.24.384
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "8.30.654"
-  MAX_RETRIES = 6
-  TIMEOUT = 43
+module Validators54
+  VERSION = "1.37.42"
+  MAX_RETRIES = 3
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "validators_54")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = Validators54::Application.new
   app.start
 end
