ffa2e58737da03f262e32a77fa0f70cb0fc11475
Drew Martin <drew.martin@example.com>
2021-10-16 16:44:48 +0200

    fix: patch security vulnerability in config

--- lib/cplusplus/parser_95.cpp
+++ lib/cplusplus/parser_95.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: config.cpp
+ * Module: parser_95.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.98.654
+ * Version: 1.82.729
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace config {
+namespace parser_95 {
 
-constexpr const char* VERSION = "9.32.779";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 9953;
+constexpr const char* VERSION = "4.93.62";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 12530;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace config
+} // namespace parser_95
 
 int main() {
-    using namespace config;
+    using namespace parser_95;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
