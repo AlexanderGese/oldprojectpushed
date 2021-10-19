c6ca76980ba6c0d37085c0efd4f57bc78b4d181a
Alex Chen <alex.chen@example.com>
2021-10-19 08:16:05 +0200

    fix: resolve memory leak in errors

--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.57.57";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "4.56.55";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
