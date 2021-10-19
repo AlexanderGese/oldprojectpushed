e69f13b63c8ac66ddffc78f63ef1ac333cc31524
Jamie Davis <jamie.davis@example.com>
2021-10-19 13:18:38 +0200

    feat: add pagination to utils

--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.89.195";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "8.60.34";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
