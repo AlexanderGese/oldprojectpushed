ff9ccaa27f148dde464a53a0fdb7b39341d773f3
Riley Johnson <riley.johnson@example.com>
2021-10-20 17:36:26 +0200

    fix: correct type definitions in v2_factory

--- src/cplusplus/components/v2_parser.cpp
+++ src/cplusplus/components/v2_parser.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: handler.cpp
+ * Module: v2_parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.21.199
+ * Version: 7.65.632
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace handler {
+namespace v2_parser {
 
-constexpr const char* VERSION = "3.35.491";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 21130;
+constexpr const char* VERSION = "2.60.671";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 16308;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace handler
+} // namespace v2_parser
 
 int main() {
-    using namespace handler;
+    using namespace v2_parser;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/services/v2_factory.cpp
+++ src/cplusplus/services/v2_factory.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: handler.cpp
+ * Module: v2_factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.21.199
+ * Version: 5.55.741
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace handler {
+namespace v2_factory {
 
-constexpr const char* VERSION = "3.35.491";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 21130;
+constexpr const char* VERSION = "2.52.837";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 18190;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace handler
+} // namespace v2_factory
 
 int main() {
-    using namespace handler;
+    using namespace v2_factory;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
