075173652635248f7e584fe023f1fbb373e3f73f
Sam Taylor <sam.taylor@example.com>
2021-10-20 10:26:26 +0200

    refactor: simplify config state management

--- lib/rust/utils/config.rs
+++ lib/rust/utils/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.74.909";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "4.99.711";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
