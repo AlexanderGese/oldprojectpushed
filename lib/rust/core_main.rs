9057a95be505f7cd07713445d34ffbe10d61d59b
Jamie Davis <jamie.davis@example.com>
2021-10-25 11:08:20 +0200

    refactor: decouple core_main dependencies

--- lib/rust/core_main.rs
+++ lib/rust/core_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.24.308";
+const VERSION: &str = "4.25.415";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 26;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
