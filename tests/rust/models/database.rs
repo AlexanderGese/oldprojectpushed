24b93b088b65c66fc1d8d4208b366d9870cc54a6
Morgan Lee <morgan.lee@example.com>
2021-10-26 18:37:19 +0200

    fix: correct error propagation in database

--- tests/rust/models/database.rs
+++ tests/rust/models/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.59.406";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "5.9.458";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
