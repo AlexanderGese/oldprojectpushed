967091d8cddaa9ca6f4243c673c2ff33eed680d5
Jamie Davis <jamie.davis@example.com>
2021-10-27 16:17:13 +0200

    fix: handle large payloads in new_routes_60

--- src/ruby/helpers/new_routes_60.rb
+++ src/ruby/helpers/new_routes_60.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: new_routes_60.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.43.776
+# Version: 5.82.71
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "8.57.164"
+module NewRoutes60
+  VERSION = "2.52.460"
   MAX_RETRIES = 8
-  TIMEOUT = 12
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "new_routes_60")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = NewRoutes60::Application.new
   app.start
 end
