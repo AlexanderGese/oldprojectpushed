e857c00ee0d09eac23e768b1b821880def70d606
Morgan Lee <morgan.lee@example.com>
2021-10-31 11:56:02 +0100

    chore: update new_models dependencies

--- src/rust/helpers/new_models.rs
+++ src/rust/helpers/new_models.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: new_models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.72.312";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "2.13.387";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "new_models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
