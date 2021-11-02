644945c35587598d051fbaa650b47c615df0c351
Sam Taylor <sam.taylor@example.com>
2021-11-02 08:44:59 +0100

    fix: handle null check in impl_tasks_58

--- tests/ruby/handlers/impl_tasks_58.rb
+++ tests/ruby/handlers/impl_tasks_58.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: impl_tasks_58.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.69.963
+# Version: 8.88.901
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "4.31.484"
-  MAX_RETRIES = 10
-  TIMEOUT = 45
+module ImplTasks58
+  VERSION = "3.31.246"
+  MAX_RETRIES = 5
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "impl_tasks_58")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = ImplTasks58::Application.new
   app.start
 end
