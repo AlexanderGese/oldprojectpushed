e9a557446b5d838e38a3592dedbe0dd5e34ae416
Jordan Smith <jordan.smith@example.com>
2021-11-04 18:47:25 +0100

    feat: implement WebSocket support in enhanced_server_24

--- src/ruby/services/core_tasks.rb
+++ src/ruby/services/core_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: core_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.53.373
+# Version: 6.84.458
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreTasks
-  VERSION = "2.0.135"
-  MAX_RETRIES = 7
-  TIMEOUT = 29
+  VERSION = "1.0.851"
+  MAX_RETRIES = 4
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
