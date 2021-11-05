5f44bf06d72150f35b61e11bcd95c54b3dd3c8f9
Jamie Davis <jamie.davis@example.com>
2021-11-05 19:24:22 +0100

    refactor: decouple middleware dependencies

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.77.824";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "2.75.561";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/impl_config_82.rs
+++ lib/rust/impl_config_82.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: impl_config_82.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.90.341";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "9.40.140";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "impl_config_82".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
