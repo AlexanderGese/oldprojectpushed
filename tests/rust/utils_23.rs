b89424abc0d1a597fd61f423ab3b24b102e4cc04
Drew Martin <drew.martin@example.com>
2021-11-05 11:40:20 +0100

    refactor: improve utils_23 error messages

--- tests/rust/utils_23.rs
+++ tests/rust/utils_23.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: utils_23.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.18.737";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "6.48.343";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "utils_23".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
