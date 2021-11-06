4fdf511806a499d16efae400bc52e0ab4e876b38
Jordan Smith <jordan.smith@example.com>
2021-11-06 16:39:33 +0100

    chore: configure CI/CD for routes

--- src/ruby/middleware.rb
+++ src/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.37.769
+# Version: 5.81.976
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "1.32.912"
+  VERSION = "6.66.934"
   MAX_RETRIES = 10
-  TIMEOUT = 29
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
