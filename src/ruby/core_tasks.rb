13fd823bd0926c0c76422cc5f063a61bd3889f85
Morgan Lee <morgan.lee@example.com>
2021-11-08 12:50:55 +0100

    fix: resolve edge case in core_tasks handler

--- src/ruby/core_tasks.rb
+++ src/ruby/core_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: core_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.58.659
+# Version: 5.22.288
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreTasks
-  VERSION = "4.7.251"
-  MAX_RETRIES = 10
-  TIMEOUT = 12
+  VERSION = "4.73.937"
+  MAX_RETRIES = 8
+  TIMEOUT = 27
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
