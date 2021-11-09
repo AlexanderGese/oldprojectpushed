ec2c8506277e0be3fd9cf78301bb688495394114
Casey Williams <casey.williams@example.com>
2021-11-09 18:11:20 +0100

    refactor: improve impl_lib test coverage

--- lib/rust/components/v2_lib.rs
+++ lib/rust/components/v2_lib.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: v2_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.53.798";
+const VERSION: &str = "9.44.377";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 5;
+const TIMEOUT_SECS: u64 = 28;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "v2_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/impl_lib.rs
+++ lib/rust/impl_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.82.387";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "6.38.783";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
