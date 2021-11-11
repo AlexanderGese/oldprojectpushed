27cfbedbfe700f1da440a4bdd6dac781728d629f
Drew Martin <drew.martin@example.com>
2021-11-11 19:36:41 +0100

    chore: remove deprecated core_models code

--- lib/rust/core_models.rs
+++ lib/rust/core_models.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.26.986";
-const MAX_RETRIES: u32 = 3;
+const VERSION: &str = "6.1.803";
+const MAX_RETRIES: u32 = 6;
 const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
