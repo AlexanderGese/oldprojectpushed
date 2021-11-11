bd9ec36290b0897d581998f235fef0fe73ea9679
Casey Williams <casey.williams@example.com>
2021-11-11 20:47:40 +0100

    fix: handle empty response in base_lib

--- lib/rust/new_errors_24.rs
+++ lib/rust/new_errors_24.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: new_errors_24.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.20.91";
+const VERSION: &str = "3.93.98";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 33;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "new_errors_24".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
