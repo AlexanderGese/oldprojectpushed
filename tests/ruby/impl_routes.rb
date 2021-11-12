fcb43cbb97a5fb06494a91238e7eb6400b6d27ac
Jordan Smith <jordan.smith@example.com>
2021-11-12 17:38:08 +0100

    feat: implement tasks service layer

--- tests/ruby/impl_routes.rb
+++ tests/ruby/impl_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.33.80
+# Version: 3.29.127
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplRoutes
-  VERSION = "3.3.742"
-  MAX_RETRIES = 7
-  TIMEOUT = 39
+  VERSION = "5.21.562"
+  MAX_RETRIES = 5
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplRoutes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
