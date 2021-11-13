33aafecbb88ab2a62043ca823bca11ece95aab7a
Sam Taylor <sam.taylor@example.com>
2021-11-13 18:36:32 +0100

    refactor: extract v2_middleware utility functions

--- lib/rust/v2_middleware.rs
+++ lib/rust/v2_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.61.998";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "4.82.971";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
