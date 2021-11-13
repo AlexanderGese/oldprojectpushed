382b92ad98e929dcfa5e0a07f44025cca54daf44
Sam Taylor <sam.taylor@example.com>
2021-11-13 22:37:15 +0100

    fix: handle empty response in enhanced_models

--- src/rust/enhanced_models.rs
+++ src/rust/enhanced_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.14.570";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "8.9.93";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
