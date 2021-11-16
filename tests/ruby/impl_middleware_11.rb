63d786d7a7ad88665c56a1b46a916d755bf9949f
Jamie Davis <jamie.davis@example.com>
2021-11-16 11:42:04 +0100

    chore: update server dependencies

--- tests/ruby/impl_middleware_11.rb
+++ tests/ruby/impl_middleware_11.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: impl_middleware_11.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.9.877
+# Version: 3.60.305
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "1.86.306"
-  MAX_RETRIES = 4
-  TIMEOUT = 45
+module ImplMiddleware11
+  VERSION = "8.73.210"
+  MAX_RETRIES = 6
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "impl_middleware_11")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = ImplMiddleware11::Application.new
   app.start
 end
