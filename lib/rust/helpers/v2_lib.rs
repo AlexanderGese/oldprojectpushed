b4f4599f51079199bb0b36313a7096882801a455
Morgan Lee <morgan.lee@example.com>
2021-11-17 17:20:25 +0100

    fix: resolve encoding issue in v2_lib

--- lib/rust/helpers/v2_lib.rs
+++ lib/rust/helpers/v2_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.75.524";
+const VERSION: &str = "1.65.588";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 60;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
