bc05f1e3f7fde5dceb8553c765df16290305713a
Drew Martin <drew.martin@example.com>
2021-11-18 16:24:11 +0100

    feat: add health check endpoint to config

--- src/rust/models/core_models.rs
+++ src/rust/models/core_models.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: core_models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.51.132";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "4.61.596";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "core_models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/services/config.rs
+++ src/rust/services/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.40.691";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "9.63.850";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
