e8a2096a19dc5809114f47b538be80b4ebe54943
Alex Chen <alex.chen@example.com>
2021-11-20 20:20:29 +0100

    fix: patch security vulnerability in base_validators_23

--- src/ruby/base_validators_23.rb
+++ src/ruby/base_validators_23.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: base_validators_23.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.4.17
+# Version: 6.33.799
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "9.64.222"
-  MAX_RETRIES = 4
-  TIMEOUT = 26
+module BaseValidators23
+  VERSION = "7.59.944"
+  MAX_RETRIES = 6
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "base_validators_23")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = BaseValidators23::Application.new
   app.start
 end
