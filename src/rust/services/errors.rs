966c2fe378ffeb002148c017ccf122185b0f421a
Jordan Smith <jordan.smith@example.com>
2021-11-21 14:24:50 +0100

    test: add unit tests for errors

--- src/rust/services/errors.rs
+++ src/rust/services/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.63.896";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "9.76.68";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
