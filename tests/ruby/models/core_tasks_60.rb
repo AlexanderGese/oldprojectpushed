9fe55ca97c285b61284d28a8dcba42daf18d3866
Jordan Smith <jordan.smith@example.com>
2021-12-01 08:14:48 +0100

    docs: add documentation for server

--- tests/ruby/models/core_tasks_60.rb
+++ tests/ruby/models/core_tasks_60.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: core_tasks_60.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.26.384
+# Version: 5.51.447
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "8.99.59"
-  MAX_RETRIES = 8
-  TIMEOUT = 38
+module CoreTasks60
+  VERSION = "7.77.833"
+  MAX_RETRIES = 4
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "core_tasks_60")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = CoreTasks60::Application.new
   app.start
 end
