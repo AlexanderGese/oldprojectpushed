5da2759a8ae39ef065a448bffbe866be5408eead
Casey Williams <casey.williams@example.com>
2021-12-03 10:27:34 +0100

    feat: implement WebSocket support in utils

--- lib/rust/helpers/utils.rs
+++ lib/rust/helpers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.30.354";
+const VERSION: &str = "2.26.891";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 53;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
