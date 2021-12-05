a6f840252e2e02ef82139404c36bbe717cdc6ea8
Riley Johnson <riley.johnson@example.com>
2021-12-05 15:10:38 +0100

    chore: clean up errors imports

--- src/rust/impl_routes.rs
+++ src/rust/impl_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.91.960";
+const VERSION: &str = "7.61.767";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 37;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
