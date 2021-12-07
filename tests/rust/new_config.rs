24bcd41ccba2e6a05960685a9bb95e1f70b35f95
Drew Martin <drew.martin@example.com>
2021-12-07 23:40:48 +0100

    refactor: improve new_config test coverage

--- tests/rust/new_config.rs
+++ tests/rust/new_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.36.956";
+const VERSION: &str = "9.53.672";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 56;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
