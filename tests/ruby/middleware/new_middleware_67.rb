a97be75f6f8158d3abe5399eefbcdb94ca0e90dd
Drew Martin <drew.martin@example.com>
2021-12-12 17:52:29 +0100

    fix: resolve memory leak in new_middleware_67

--- tests/ruby/middleware/new_middleware_67.rb
+++ tests/ruby/middleware/new_middleware_67.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: new_middleware_67.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.79.20
+# Version: 8.27.596
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "3.16.717"
-  MAX_RETRIES = 8
-  TIMEOUT = 38
+module NewMiddleware67
+  VERSION = "1.41.275"
+  MAX_RETRIES = 10
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "new_middleware_67")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = NewMiddleware67::Application.new
   app.start
 end
