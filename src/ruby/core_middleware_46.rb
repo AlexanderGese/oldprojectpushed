bd1f40f10b7e04b24a60cc1258b16283e9fb5003
Drew Martin <drew.martin@example.com>
2021-12-13 19:48:54 +0100

    feat: add search functionality to middleware

--- src/ruby/core_middleware_46.rb
+++ src/ruby/core_middleware_46.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: core_middleware_46.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.20.22
+# Version: 9.46.172
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "6.33.559"
-  MAX_RETRIES = 10
-  TIMEOUT = 45
+module CoreMiddleware46
+  VERSION = "6.11.177"
+  MAX_RETRIES = 3
+  TIMEOUT = 54
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "core_middleware_46")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = CoreMiddleware46::Application.new
   app.start
 end
