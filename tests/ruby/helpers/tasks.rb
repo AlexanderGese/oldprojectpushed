17fff1d457445f2e518eac616e1e899a90ed6159
Drew Martin <drew.martin@example.com>
2021-12-13 18:22:42 +0100

    feat: add export functionality to new_middleware

--- tests/ruby/helpers/tasks.rb
+++ tests/ruby/helpers/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.85.990
+# Version: 6.7.976
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "7.22.974"
-  MAX_RETRIES = 7
-  TIMEOUT = 55
+  VERSION = "1.54.118"
+  MAX_RETRIES = 10
+  TIMEOUT = 14
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/new_middleware.rb
+++ tests/ruby/new_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: new_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.77.974
+# Version: 1.47.979
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewMiddleware
-  VERSION = "4.78.980"
-  MAX_RETRIES = 8
-  TIMEOUT = 45
+  VERSION = "6.67.5"
+  MAX_RETRIES = 6
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
