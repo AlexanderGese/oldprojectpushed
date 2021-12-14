16bdb3134e435e8efef2887e6b15499c69e57165
Jordan Smith <jordan.smith@example.com>
2021-12-14 08:48:51 +0100

    refactor: improve impl_lib test coverage

--- src/rust/middleware/impl_lib.rs
+++ src/rust/middleware/impl_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.40.780";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "9.83.852";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
