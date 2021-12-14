ddbb6c09de52be645d01ca5cea1d60f0ee105a80
Drew Martin <drew.martin@example.com>
2021-12-14 18:53:31 +0100

    refactor: simplify middleware state management

--- tests/rust/middleware/middleware.rs
+++ tests/rust/middleware/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.65.369";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "6.5.465";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
