349d9490d3631741ac81c915aae82536e397910e
Morgan Lee <morgan.lee@example.com>
2021-12-18 12:57:09 +0100

    chore: update impl_helpers_3 configuration

--- lib/ruby/handlers/validators.rb
+++ lib/ruby/handlers/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.34.285
+# Version: 9.95.596
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "4.53.39"
-  MAX_RETRIES = 7
-  TIMEOUT = 10
+  VERSION = "4.18.544"
+  MAX_RETRIES = 4
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
