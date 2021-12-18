72529d23d31dbb376a6295da761d65e06e742cbd
Sam Taylor <sam.taylor@example.com>
2021-12-18 23:19:05 +0100

    fix: handle large payloads in base_utils

--- lib/rust/core_utils.rs
+++ lib/rust/core_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.17.22";
+const VERSION: &str = "8.66.625";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 19;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
