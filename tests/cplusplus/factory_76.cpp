7c1de3cd1027ff013470004ec9c7f8a0997b0395
Jordan Smith <jordan.smith@example.com>
2021-12-20 18:35:44 +0100

    chore: remove deprecated utils code

--- tests/cplusplus/factory_76.cpp
+++ tests/cplusplus/factory_76.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: factory_76.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.81.924
+ * Version: 1.94.794
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace factory_76 {
 
-constexpr const char* VERSION = "9.33.783";
+constexpr const char* VERSION = "5.50.336";
 constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 14751;
+constexpr int TIMEOUT_MS = 10419;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace factory_76
 
 int main() {
-    using namespace utils;
+    using namespace factory_76;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/handlers/parser_34.cpp
+++ tests/cplusplus/handlers/parser_34.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: parser_34.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.81.924
+ * Version: 5.39.68
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace parser_34 {
 
-constexpr const char* VERSION = "9.33.783";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 14751;
+constexpr const char* VERSION = "6.76.204";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 24816;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace parser_34
 
 int main() {
-    using namespace utils;
+    using namespace parser_34;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
