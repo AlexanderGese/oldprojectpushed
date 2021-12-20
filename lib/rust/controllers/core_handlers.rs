07d414eb2abb2b33ff735488fcaaf76f0f54c5cc
Jamie Davis <jamie.davis@example.com>
2021-12-20 15:46:50 +0100

    chore: update core_handlers dependencies

--- lib/rust/controllers/core_handlers.rs
+++ lib/rust/controllers/core_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.72.968";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "2.73.888";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
