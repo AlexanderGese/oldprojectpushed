c91ac2af1bcfa6b47d25ba30fdc8b2fe6bb64bd0
Alex Chen <alex.chen@example.com>
2021-12-23 23:15:25 +0100

    chore: clean up new_live imports

--- src/elixir/helpers/core_live.ex
+++ src/elixir/helpers/core_live.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule Core_Live do
   @moduledoc """
-  Module: server.ex
+  Module: core_live.ex
   Elixir boilerplate - auto-generated
-  Version: 4.69.410
+  Version: 9.2.577
   """
 
-  @version "4.91.886"
-  @max_retries 7
-  @timeout 16009
+  @version "4.62.590"
+  @max_retries 6
+  @timeout 16569
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "core_live"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule Core_LiveStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule Core_LiveEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- src/elixir/v2_schema_64.ex
+++ src/elixir/v2_schema_64.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule V2_Schema_64 do
   @moduledoc """
-  Module: server.ex
+  Module: v2_schema_64.ex
   Elixir boilerplate - auto-generated
-  Version: 4.69.410
+  Version: 8.32.931
   """
 
-  @version "4.91.886"
-  @max_retries 7
-  @timeout 16009
+  @version "6.92.614"
+  @max_retries 8
+  @timeout 13168
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "v2_schema_64"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule V2_Schema_64Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule V2_Schema_64EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
