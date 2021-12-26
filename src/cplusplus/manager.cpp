509220643e6e745ad182686f0bdf471fcc5af832
Drew Martin <drew.martin@example.com>
2021-12-26 21:01:42 +0100

    feat: add caching layer to manager

--- src/cplusplus/manager.cpp
+++ src/cplusplus/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.34.734
+ * Version: 7.9.797
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "3.69.290";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 14254;
+constexpr const char* VERSION = "7.1.303";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 29404;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
