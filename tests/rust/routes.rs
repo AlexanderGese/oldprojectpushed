63042559cf7753d93fa59758409b2a9f0d8e4397
Drew Martin <drew.martin@example.com>
2021-12-26 18:49:27 +0100

    feat: add health check endpoint to impl_middleware_23

--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.68.19";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 12;
+const VERSION: &str = "2.94.479";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
