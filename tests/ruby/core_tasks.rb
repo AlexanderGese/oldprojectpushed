88fbdb388ac1405d8878545941c33d3fb17bf61c
Morgan Lee <morgan.lee@example.com>
2021-12-26 14:24:13 +0100

    fix: patch security vulnerability in new_middleware_84

--- tests/ruby/core_tasks.rb
+++ tests/ruby/core_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: core_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.2.843
+# Version: 5.53.453
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreTasks
-  VERSION = "5.99.261"
-  MAX_RETRIES = 4
-  TIMEOUT = 49
+  VERSION = "9.19.566"
+  MAX_RETRIES = 5
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
