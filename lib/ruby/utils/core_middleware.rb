b76d6745e5796d6ce454d49eba53ca24b751078a
Drew Martin <drew.martin@example.com>
2021-12-27 15:26:54 +0100

    fix: correct type definitions in core_middleware

--- lib/ruby/utils/core_middleware.rb
+++ lib/ruby/utils/core_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: core_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.96.157
+# Version: 4.61.259
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreMiddleware
-  VERSION = "5.38.837"
-  MAX_RETRIES = 7
-  TIMEOUT = 34
+  VERSION = "3.88.989"
+  MAX_RETRIES = 4
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
