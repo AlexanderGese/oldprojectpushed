6ed504d4d0d91f5d4a382251b629900162cc41f7
Riley Johnson <riley.johnson@example.com>
2021-12-30 15:00:22 +0100

    refactor: extract routes utility functions

--- src/ruby/middleware/models.rb
+++ src/ruby/middleware/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.77.343
+# Version: 9.1.157
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "3.8.568"
-  MAX_RETRIES = 10
-  TIMEOUT = 27
+  VERSION = "1.96.971"
+  MAX_RETRIES = 5
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
