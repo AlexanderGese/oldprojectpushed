f95446d7d16ea46a01a048af5dac5e04dc13be5f
Casey Williams <casey.williams@example.com>
2022-01-02 15:25:17 +0100

    refactor: split helpers into smaller modules

--- src/ruby/controllers.rb
+++ src/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.15.997
+# Version: 9.59.988
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "1.97.392"
-  MAX_RETRIES = 3
-  TIMEOUT = 43
+  VERSION = "9.6.987"
+  MAX_RETRIES = 4
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
