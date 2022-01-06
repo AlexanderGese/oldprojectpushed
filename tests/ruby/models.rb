c5f68de42ad3d516b30d7bbc1d6c8ac5b3433d8d
Morgan Lee <morgan.lee@example.com>
2022-01-06 10:16:36 +0100

    fix: patch security vulnerability in models

--- tests/ruby/models.rb
+++ tests/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.54.916
+# Version: 2.10.749
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "1.22.921"
-  MAX_RETRIES = 4
-  TIMEOUT = 56
+  VERSION = "3.65.110"
+  MAX_RETRIES = 6
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
