44ff9d313962652e7bef93b17eba8c659f046bc1
Riley Johnson <riley.johnson@example.com>
2022-01-07 15:10:17 +0100

    chore: configure CI/CD for new_middleware

--- tests/ruby/core_models_36.rb
+++ tests/ruby/core_models_36.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_middleware.rb
+# Module: core_models_36.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.57.529
+# Version: 2.15.223
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewMiddleware
-  VERSION = "5.40.628"
-  MAX_RETRIES = 9
-  TIMEOUT = 15
+module CoreModels36
+  VERSION = "5.29.217"
+  MAX_RETRIES = 4
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_middleware")
+      @app_name = opts.fetch(:app_name, "core_models_36")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewMiddleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewMiddleware::Application.new
+  app = CoreModels36::Application.new
   app.start
 end
