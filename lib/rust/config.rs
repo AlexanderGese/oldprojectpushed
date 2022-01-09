bfa305732ae5b9160383f4d56a8ad9955fa23802
Jamie Davis <jamie.davis@example.com>
2022-01-09 21:46:55 +0100

    feat: add middleware support to middleware

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.66.413";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "6.41.12";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.30.725";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "5.98.12";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
