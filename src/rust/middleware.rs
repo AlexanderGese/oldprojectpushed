1b1035c6dd2bf4cd676a6f26cfcfda8707db662d
Alex Chen <alex.chen@example.com>
2022-01-09 10:19:52 +0100

    fix: correct date formatting in utils

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.44.695";
+const VERSION: &str = "2.99.187";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 56;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
