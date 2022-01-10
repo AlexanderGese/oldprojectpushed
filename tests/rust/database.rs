2030387c7c29bb3e0acb3d9450084e49ec17a724
Sam Taylor <sam.taylor@example.com>
2022-01-10 18:07:31 +0100

    fix: resolve encoding issue in handlers

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.1.751";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "2.16.724";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.38.311";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "4.78.946";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
