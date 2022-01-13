beb5960252b43dbc63c5df61b3383545c6144fa8
Casey Williams <casey.williams@example.com>
2022-01-13 19:56:41 +0100

    fix: handle timeout in app requests

--- src/ruby/impl_config_36.rb
+++ src/ruby/impl_config_36.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: impl_config_36.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.88.51
+# Version: 6.57.901
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "7.54.212"
-  MAX_RETRIES = 5
-  TIMEOUT = 58
+module ImplConfig36
+  VERSION = "1.5.584"
+  MAX_RETRIES = 7
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "impl_config_36")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = ImplConfig36::Application.new
   app.start
 end
