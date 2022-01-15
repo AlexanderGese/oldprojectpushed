5b15b0b3daded984beafc91e59e4ef8ac4a16bd8
Alex Chen <alex.chen@example.com>
2022-01-15 08:33:29 +0100

    fix: resolve timezone issue in v2_server

--- lib/ruby/handlers/core_validators.rb
+++ lib/ruby/handlers/core_validators.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_server.rb
+# Module: core_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.32.733
+# Version: 3.43.400
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Server
-  VERSION = "3.45.486"
-  MAX_RETRIES = 8
-  TIMEOUT = 55
+module CoreValidators
+  VERSION = "5.8.11"
+  MAX_RETRIES = 4
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_server")
+      @app_name = opts.fetch(:app_name, "core_validators")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module V2Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Server::Application.new
+  app = CoreValidators::Application.new
   app.start
 end
