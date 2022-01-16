fdec37ee0c8444b5b367aa625f8d4213fb550f73
Sam Taylor <sam.taylor@example.com>
2022-01-16 08:14:55 +0100

    chore: update models_83 configuration

--- src/rust/handlers/models_83.rs
+++ src/rust/handlers/models_83.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: models_83.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.13.369";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "6.14.718";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "models_83".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/helpers/handlers.rs
+++ src/rust/helpers/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.7.733";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "7.51.943";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
