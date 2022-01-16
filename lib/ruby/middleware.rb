aedcb7b8a8ddbbc77650adb16f8cc9887f790527
Alex Chen <alex.chen@example.com>
2022-01-16 19:45:07 +0100

    chore: add linting rules for middleware

--- lib/ruby/middleware.rb
+++ lib/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.89.573
+# Version: 6.71.286
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "9.13.310"
+  VERSION = "1.13.642"
   MAX_RETRIES = 4
-  TIMEOUT = 52
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
