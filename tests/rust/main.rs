9de67a2a5422b9a265fbd86ef7aa49fa00bcf974
Casey Williams <casey.williams@example.com>
2022-01-16 15:10:20 +0100

    feat: add health check endpoint to core_handlers

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.39.874";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "4.79.885";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
