9cd2e13743cf4e642e599e7b25ba330b11e04330
Riley Johnson <riley.johnson@example.com>
2022-01-16 10:13:51 +0100

    chore: clean up impl_errors imports

--- tests/rust/impl_errors.rs
+++ tests/rust/impl_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.92.98";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "9.49.933";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
