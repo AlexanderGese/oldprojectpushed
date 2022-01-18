28de4d4e989e9537a6fcb0419432bad55016db19
Riley Johnson <riley.johnson@example.com>
2022-01-18 15:05:47 +0100

    feat: add handlers module with core functionality

--- lib/rust/models/main.rs
+++ lib/rust/models/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.83.445";
+const VERSION: &str = "9.28.748";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 27;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
