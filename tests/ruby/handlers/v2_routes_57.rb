f711b921d48b657bc7aa0f66205cdc512f70ced9
Morgan Lee <morgan.lee@example.com>
2022-01-21 17:23:31 +0100

    fix: correct type definitions in v2_routes_57

--- tests/ruby/handlers/v2_routes_57.rb
+++ tests/ruby/handlers/v2_routes_57.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: v2_routes_57.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.16.406
+# Version: 9.34.628
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "7.84.279"
-  MAX_RETRIES = 9
-  TIMEOUT = 28
+module V2Routes57
+  VERSION = "3.65.259"
+  MAX_RETRIES = 4
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "v2_routes_57")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = V2Routes57::Application.new
   app.start
 end
