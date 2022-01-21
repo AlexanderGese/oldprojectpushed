e08ff8b1af5903dd6f7a4b08bf3973378c15e232
Jamie Davis <jamie.davis@example.com>
2022-01-21 19:27:04 +0100

    chore: add linting rules for server

--- tests/ruby/validators.rb
+++ tests/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.51.632
+# Version: 1.58.661
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "9.83.548"
-  MAX_RETRIES = 3
-  TIMEOUT = 52
+  VERSION = "7.31.414"
+  MAX_RETRIES = 8
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
