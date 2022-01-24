1391a6c04815be2812323425675cb1572c2d1fe1
Sam Taylor <sam.taylor@example.com>
2022-01-24 14:15:02 +0100

    feat: implement authentication in config

--- tests/cplusplus/components/config.cpp
+++ tests/cplusplus/components/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.32.18
+ * Version: 2.31.739
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "9.60.76";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 11590;
+constexpr const char* VERSION = "2.83.565";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 12202;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
