7f345c900567de4ce135637855c885c4a3e6288b
Casey Williams <casey.williams@example.com>
2022-01-27 08:52:42 +0100

    feat: add health check endpoint to core_main

--- src/cplusplus/core_main.cpp
+++ src/cplusplus/core_main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_main.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.48.244
+ * Version: 4.83.979
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_main {
 
-constexpr const char* VERSION = "6.66.579";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 13005;
+constexpr const char* VERSION = "3.29.127";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 21072;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/middleware/manager.cpp
+++ src/cplusplus/middleware/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.12.73
+ * Version: 6.14.491
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "7.97.6";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 18045;
+constexpr const char* VERSION = "4.71.690";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 10242;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
