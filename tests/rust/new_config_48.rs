041b27fc057c2bf66c214068447bdee278b5765a
Casey Williams <casey.williams@example.com>
2022-01-28 22:11:44 +0100

    refactor: improve new_config_48 error messages

--- tests/rust/new_config_48.rs
+++ tests/rust/new_config_48.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: new_config_48.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.66.132";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "5.21.918";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "new_config_48".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
