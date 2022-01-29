53a37ef8f3cf5cb5de043531f6238b9481bf0a25
Riley Johnson <riley.johnson@example.com>
2022-01-29 11:46:29 +0100

    feat: add logging to middleware

--- tests/ruby/middleware.rb
+++ tests/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.95.263
+# Version: 2.92.846
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "8.80.396"
+  VERSION = "7.71.644"
   MAX_RETRIES = 10
-  TIMEOUT = 47
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
