8e4e641cb34368e0b2921feab959894eb4682584
Drew Martin <drew.martin@example.com>
2022-01-30 11:38:26 +0100

    refactor: decouple impl_utils dependencies

--- lib/cplusplus/impl_utils.cpp
+++ lib/cplusplus/impl_utils.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: impl_utils.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.7.635
+ * Version: 2.28.191
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace impl_utils {
 
-constexpr const char* VERSION = "4.21.37";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 15977;
+constexpr const char* VERSION = "9.56.748";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 19135;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
