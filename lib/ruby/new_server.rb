646b9a387c8bd861c5bf30fd4f636d2b3d28d5ee
Jamie Davis <jamie.davis@example.com>
2022-01-31 08:37:03 +0100

    fix: handle timeout in core_config requests

--- lib/ruby/new_server.rb
+++ lib/ruby/new_server.rb
@@ -2,7 +2,7 @@
 
 # Module: new_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.13.52
+# Version: 4.95.831
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewServer
-  VERSION = "2.64.514"
-  MAX_RETRIES = 7
-  TIMEOUT = 33
+  VERSION = "8.68.558"
+  MAX_RETRIES = 8
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
