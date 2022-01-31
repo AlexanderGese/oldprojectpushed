5a94d37f505e17b3364bfc8d9551b5d0512f32e4
Casey Williams <casey.williams@example.com>
2022-01-31 20:01:15 +0100

    feat: add logging to models

--- tests/ruby/models.rb
+++ tests/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.82.701
+# Version: 1.15.924
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "7.38.945"
-  MAX_RETRIES = 7
-  TIMEOUT = 58
+  VERSION = "3.3.82"
+  MAX_RETRIES = 4
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
