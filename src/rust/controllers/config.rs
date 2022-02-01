9e10258f16ddc504e90c1c6628f29f619feb0372
Jamie Davis <jamie.davis@example.com>
2022-02-01 22:57:15 +0100

    fix: correct date formatting in config

--- src/rust/controllers/config.rs
+++ src/rust/controllers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.41.701";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "9.80.391";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
