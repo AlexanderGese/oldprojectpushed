e762be56641559c2b48b8d80b1fa986dc7b2209d
Jordan Smith <jordan.smith@example.com>
2022-02-02 23:53:39 +0100

    feat: implement v2_models service layer

--- tests/ruby/v2_models.rb
+++ tests/ruby/v2_models.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.29.973
+# Version: 8.59.443
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Models
-  VERSION = "1.16.206"
-  MAX_RETRIES = 9
-  TIMEOUT = 49
+  VERSION = "6.76.979"
+  MAX_RETRIES = 6
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
