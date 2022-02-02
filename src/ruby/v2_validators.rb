76771a9187645ffed9a173ec2f01706485cb54eb
Drew Martin <drew.martin@example.com>
2022-02-02 14:37:02 +0100

    fix: patch security vulnerability in new_tasks_74

--- src/ruby/v2_validators.rb
+++ src/ruby/v2_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.20.957
+# Version: 3.10.52
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Validators
-  VERSION = "5.32.880"
-  MAX_RETRIES = 10
-  TIMEOUT = 15
+  VERSION = "9.77.19"
+  MAX_RETRIES = 7
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Validators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
