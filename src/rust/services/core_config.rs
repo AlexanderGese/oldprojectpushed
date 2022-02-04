5db0d02e2a3f7d763f8d7cb6b9ba43b60394be2e
Alex Chen <alex.chen@example.com>
2022-02-04 16:27:13 +0100

    docs: add documentation for core_config

--- src/rust/services/core_config.rs
+++ src/rust/services/core_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.30.212";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "1.85.193";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
