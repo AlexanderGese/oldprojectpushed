a21979bf6c250d7a871d8ba32529478d92afe8cf
Casey Williams <casey.williams@example.com>
2022-02-04 16:47:43 +0100

    fix: correct CORS headers in middleware

--- tests/ruby/enhanced_middleware.rb
+++ tests/ruby/enhanced_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: enhanced_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.48.866
+# Version: 2.26.150
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module EnhancedMiddleware
-  VERSION = "9.91.892"
-  MAX_RETRIES = 8
-  TIMEOUT = 49
+  VERSION = "7.64.358"
+  MAX_RETRIES = 6
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module EnhancedMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
