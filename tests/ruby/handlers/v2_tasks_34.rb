5a27b08aa3f5885c99ca2c95309058b8380b3c04
Sam Taylor <sam.taylor@example.com>
2022-02-04 21:48:35 +0100

    refactor: migrate config to async/await

--- tests/ruby/handlers/v2_tasks_34.rb
+++ tests/ruby/handlers/v2_tasks_34.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: v2_tasks_34.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.91.506
+# Version: 2.51.911
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "6.47.117"
-  MAX_RETRIES = 10
-  TIMEOUT = 47
+module V2Tasks34
+  VERSION = "9.56.243"
+  MAX_RETRIES = 7
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "v2_tasks_34")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = V2Tasks34::Application.new
   app.start
 end
