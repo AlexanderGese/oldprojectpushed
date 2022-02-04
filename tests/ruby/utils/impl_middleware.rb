b7c1da32774126587b117023c6a589eefa90cbf7
Morgan Lee <morgan.lee@example.com>
2022-02-04 22:38:35 +0100

    chore: add linting rules for tasks

--- tests/ruby/utils/impl_middleware.rb
+++ tests/ruby/utils/impl_middleware.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: impl_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.41.985
+# Version: 7.70.995
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "3.18.360"
+module ImplMiddleware
+  VERSION = "3.47.530"
   MAX_RETRIES = 3
-  TIMEOUT = 29
+  TIMEOUT = 40
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "impl_middleware")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = ImplMiddleware::Application.new
   app.start
 end
