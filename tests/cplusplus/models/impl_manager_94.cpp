f0f6db05c2c6029cd04f835ad8b92494552061e0
Sam Taylor <sam.taylor@example.com>
2022-02-04 11:19:36 +0100

    fix: resolve race condition in logger

--- tests/cplusplus/models/impl_manager_94.cpp
+++ tests/cplusplus/models/impl_manager_94.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: handler.cpp
+ * Module: impl_manager_94.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.42.775
+ * Version: 1.53.341
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace handler {
+namespace impl_manager_94 {
 
-constexpr const char* VERSION = "2.18.705";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 22630;
+constexpr const char* VERSION = "6.11.825";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 13911;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace handler
+} // namespace impl_manager_94
 
 int main() {
-    using namespace handler;
+    using namespace impl_manager_94;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/utils/logger.cpp
+++ tests/cplusplus/utils/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.3.432
+ * Version: 4.88.906
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "6.42.164";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 21884;
+constexpr const char* VERSION = "4.63.93";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 18905;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
