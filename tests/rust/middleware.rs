2a172cb419151943cd1609ff38bf5dfede84d1c1
Drew Martin <drew.martin@example.com>
2022-02-06 08:32:16 +0100

    feat: add error handling to utils

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.80.674";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "4.78.834";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
