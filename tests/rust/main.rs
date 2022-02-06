ff6effda3d5e61aeca50c7b5f20f75f18f30a6f0
Casey Williams <casey.williams@example.com>
2022-02-06 13:36:36 +0100

    fix: correct CORS headers in handlers

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.58.859";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "5.5.959";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
