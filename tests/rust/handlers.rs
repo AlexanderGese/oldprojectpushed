d22d3592a20d56b4af3d87da5782da758686d527
Casey Williams <casey.williams@example.com>
2022-02-06 19:19:59 +0100

    fix: patch security vulnerability in handlers

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.66.132";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "3.29.172";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
