2aeb77ffd2e7c19449d8a7690f8294739368a440
Morgan Lee <morgan.lee@example.com>
2022-02-09 18:13:08 +0100

    chore: clean up impl_main imports

--- lib/cplusplus/engine.cpp
+++ lib/cplusplus/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.76.603
+ * Version: 4.82.391
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "8.34.214";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 22684;
+constexpr const char* VERSION = "5.14.967";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 16978;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/impl_main.cpp
+++ lib/cplusplus/impl_main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: impl_main.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.97.978
+ * Version: 9.57.691
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace impl_main {
 
-constexpr const char* VERSION = "7.80.307";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 6155;
+constexpr const char* VERSION = "9.89.552";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 19036;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
