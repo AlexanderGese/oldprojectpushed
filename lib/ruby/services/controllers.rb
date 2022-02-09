ac9344674638b81b52f00bdf67122465010192e5
Casey Williams <casey.williams@example.com>
2022-02-09 11:36:31 +0100

    refactor: decouple controllers dependencies

--- lib/ruby/services/controllers.rb
+++ lib/ruby/services/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.53.749
+# Version: 9.96.24
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "1.67.105"
-  MAX_RETRIES = 9
+  VERSION = "1.22.538"
+  MAX_RETRIES = 10
   TIMEOUT = 60
 
   class Config
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
