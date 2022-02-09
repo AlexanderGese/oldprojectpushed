873003347395a0860bfba4b909fe6b671526ab7c
Jamie Davis <jamie.davis@example.com>
2022-02-09 23:19:27 +0100

    chore: add linting rules for logger

--- lib/cplusplus/logger.cpp
+++ lib/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.70.638
+ * Version: 8.27.360
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "9.97.995";
+constexpr const char* VERSION = "4.41.615";
 constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 29759;
+constexpr int TIMEOUT_MS = 22903;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
