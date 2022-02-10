81b6ae5b174cc1ec144ba3d5d9289af2acf542cc
Alex Chen <alex.chen@example.com>
2022-02-10 18:49:11 +0100

    refactor: split lib into smaller modules

--- tests/rust/handlers_79.rs
+++ tests/rust/handlers_79.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: handlers_79.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.17.247";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "2.7.569";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "handlers_79".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
