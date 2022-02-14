9ea0b78c96a72f121e559c668481740cf8e3f7dc
Riley Johnson <riley.johnson@example.com>
2022-02-14 20:59:24 +0100

    fix: patch security vulnerability in core_routes_17

--- tests/ruby/controllers.rb
+++ tests/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.97.105
+# Version: 8.17.597
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "9.76.108"
-  MAX_RETRIES = 4
-  TIMEOUT = 27
+  VERSION = "7.51.210"
+  MAX_RETRIES = 7
+  TIMEOUT = 40
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
