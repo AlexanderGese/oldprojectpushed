26a48403dacdbce1c50ce62da8bbf45823a9c271
Alex Chen <alex.chen@example.com>
2022-02-14 09:51:55 +0100

    fix: resolve race condition in middleware_55

--- tests/rust/middleware/middleware_55.rs
+++ tests/rust/middleware/middleware_55.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: middleware_55.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.14.383";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "6.28.408";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "middleware_55".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
