d5f11e8c9f4a3f245f2a5be89c81e13709d4f655
Sam Taylor <sam.taylor@example.com>
2022-02-15 13:34:42 +0100

    feat: add routes module with core functionality

--- tests/ruby/controllers/config.rb
+++ tests/ruby/controllers/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.21.623
+# Version: 3.96.173
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "8.89.948"
-  MAX_RETRIES = 9
-  TIMEOUT = 13
+  VERSION = "8.37.44"
+  MAX_RETRIES = 5
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
