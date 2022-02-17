64af9a59f7dba1d7bb54caa68ffa9c04a916c186
Alex Chen <alex.chen@example.com>
2022-02-17 15:43:07 +0100

    test: add unit tests for main

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.16.724";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "6.24.471";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.5.959";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "5.61.476";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
