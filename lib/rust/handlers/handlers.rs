5f49f9644817334775f42ee3b478e03ccace1ee9
Casey Williams <casey.williams@example.com>
2022-02-17 13:52:17 +0100

    refactor: migrate v2_models to async/await

--- lib/rust/handlers/handlers.rs
+++ lib/rust/handlers/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.31.446";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "1.30.6";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.75.0";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "9.28.579";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
