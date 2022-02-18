ef14ae834f9f47972f132fa097ec60d8b3f080f5
Jamie Davis <jamie.davis@example.com>
2022-02-18 08:53:48 +0100

    chore: update utils configuration

--- lib/rust/handlers/config.rs
+++ lib/rust/handlers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.78.76";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "9.90.550";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
