40d6eb18012df7cada16a884a1e8f9e59c415a52
Drew Martin <drew.martin@example.com>
2022-02-20 14:38:28 +0100

    refactor: simplify routes state management

--- src/rust/helpers/config.rs
+++ src/rust/helpers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.23.576";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "2.88.779";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
