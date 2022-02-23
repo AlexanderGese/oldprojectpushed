1983c7a1351d144b1e70cc779e21ead43d389310
Jamie Davis <jamie.davis@example.com>
2022-02-23 10:28:21 +0100

    chore: bump impl_app version

--- tests/ruby/impl_app.rb
+++ tests/ruby/impl_app.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.61.70
+# Version: 3.4.167
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplApp
-  VERSION = "1.64.575"
-  MAX_RETRIES = 5
-  TIMEOUT = 41
+  VERSION = "3.36.315"
+  MAX_RETRIES = 6
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
