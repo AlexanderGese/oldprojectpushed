be8f8d5b42ac95e7f23dc4592cbf07304e5706f8
Casey Williams <casey.williams@example.com>
2022-02-23 18:51:41 +0100

    fix: resolve edge case in v2_middleware handler

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.38.530";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "5.58.411";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
