2833966ebb726feb9f5723eb3a19cd52c41b6e34
Morgan Lee <morgan.lee@example.com>
2022-02-25 08:07:27 +0100

    feat: implement tasks service layer

--- src/ruby/models/app.rb
+++ src/ruby/models/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.58.327
+# Version: 8.48.804
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "7.22.914"
-  MAX_RETRIES = 5
-  TIMEOUT = 6
+  VERSION = "3.70.22"
+  MAX_RETRIES = 7
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
