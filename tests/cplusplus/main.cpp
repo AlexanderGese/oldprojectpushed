8759e2d0a9a03b20b5fedd5402e7070bab18f811
Casey Williams <casey.williams@example.com>
2022-02-26 12:47:26 +0100

    refactor: extract main utility functions

--- tests/cplusplus/main.cpp
+++ tests/cplusplus/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.2.570
+ * Version: 4.70.492
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "3.38.898";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 22961;
+constexpr const char* VERSION = "6.83.939";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 18059;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
