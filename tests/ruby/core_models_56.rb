636e384239faae55d3be53fcba6023d598789547
Jamie Davis <jamie.davis@example.com>
2022-02-26 17:55:13 +0100

    feat: implement WebSocket support in core_routes_19

--- tests/ruby/core_models_56.rb
+++ tests/ruby/core_models_56.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_config.rb
+# Module: core_models_56.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.22.71
+# Version: 9.34.444
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplConfig
-  VERSION = "3.46.323"
-  MAX_RETRIES = 3
-  TIMEOUT = 39
+module CoreModels56
+  VERSION = "8.26.831"
+  MAX_RETRIES = 8
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_config")
+      @app_name = opts.fetch(:app_name, "core_models_56")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplConfig
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplConfig::Application.new
+  app = CoreModels56::Application.new
   app.start
 end
--- tests/ruby/models/core_routes_19.rb
+++ tests/ruby/models/core_routes_19.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_config.rb
+# Module: core_routes_19.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.22.71
+# Version: 2.85.371
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplConfig
-  VERSION = "3.46.323"
-  MAX_RETRIES = 3
-  TIMEOUT = 39
+module CoreRoutes19
+  VERSION = "7.31.98"
+  MAX_RETRIES = 4
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_config")
+      @app_name = opts.fetch(:app_name, "core_routes_19")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplConfig
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplConfig::Application.new
+  app = CoreRoutes19::Application.new
   app.start
 end
