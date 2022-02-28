dcc6adedbb1bc0c758213abde9ce9a8842006dc3
Casey Williams <casey.williams@example.com>
2022-02-28 11:56:58 +0100

    fix: handle null check in handlers

--- src/rust/handlers.rs
+++ src/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.86.945";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "4.53.291";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
