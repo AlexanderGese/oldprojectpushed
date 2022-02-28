c808710cfa0d33d1da905963be0a0577697db5a4
Jordan Smith <jordan.smith@example.com>
2022-02-28 22:55:39 +0100

    chore: update handlers configuration

--- lib/rust/handlers/routes.rs
+++ lib/rust/handlers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.72.132";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "1.49.363";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
