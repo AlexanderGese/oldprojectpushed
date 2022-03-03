faaf0048c9f1ead49c1d45cb51b052e2aa6d18f8
Jamie Davis <jamie.davis@example.com>
2022-03-03 14:29:41 +0100

    fix: resolve memory leak in controllers

--- tests/ruby/app.rb
+++ tests/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.50.640
+# Version: 1.27.634
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "6.28.352"
-  MAX_RETRIES = 6
-  TIMEOUT = 55
+  VERSION = "1.57.921"
+  MAX_RETRIES = 4
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
--- tests/ruby/controllers/controllers.rb
+++ tests/ruby/controllers/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.39.370
+# Version: 8.84.483
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "4.87.430"
-  MAX_RETRIES = 5
-  TIMEOUT = 23
+  VERSION = "4.71.143"
+  MAX_RETRIES = 4
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
