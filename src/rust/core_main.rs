76d49eac3fe40646edb6c4995611747d70437615
Morgan Lee <morgan.lee@example.com>
2022-03-06 22:51:09 +0100

    feat: add health check endpoint to core_main

--- src/rust/core_main.rs
+++ src/rust/core_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.60.418";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "7.22.421";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
