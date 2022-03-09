d57f1ca9c1e182bed9ff49ed503b1dcc263638f8
Casey Williams <casey.williams@example.com>
2022-03-09 20:30:40 +0100

    feat: add event system to models

--- lib/rust/helpers/main.rs
+++ lib/rust/helpers/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.65.564";
+const VERSION: &str = "6.2.212";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 49;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
