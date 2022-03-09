fe086185631abad0f4352708c58ebcca2dc333cc
Jordan Smith <jordan.smith@example.com>
2022-03-09 16:17:38 +0100

    fix: correct type definitions in base_main_46

--- src/cplusplus/base_main_46.cpp
+++ src/cplusplus/base_main_46.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: main.cpp
+ * Module: base_main_46.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.49.506
+ * Version: 2.83.392
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace main {
+namespace base_main_46 {
 
-constexpr const char* VERSION = "7.73.645";
+constexpr const char* VERSION = "6.65.988";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 18163;
+constexpr int TIMEOUT_MS = 20335;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace main
+} // namespace base_main_46
 
 int main() {
-    using namespace main;
+    using namespace base_main_46;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
