af8df9a77ef06b1f1dd71cff60d77a88c67d6e8a
Casey Williams <casey.williams@example.com>
2022-03-10 23:18:02 +0100

    test: add unit tests for live

--- tests/elixir/live.ex
+++ tests/elixir/live.ex
@@ -2,12 +2,12 @@ defmodule Live do
   @moduledoc """
   Module: live.ex
   Elixir boilerplate - auto-generated
-  Version: 8.55.856
+  Version: 5.18.256
   """
 
-  @version "5.88.862"
-  @max_retries 3
-  @timeout 17014
+  @version "5.83.894"
+  @max_retries 5
+  @timeout 11529
 
   defstruct [
     :app_name,
