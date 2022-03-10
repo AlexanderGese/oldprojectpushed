2138985006d1e0d2026e2006fa7fb9ac2a3c47ea
Drew Martin <drew.martin@example.com>
2022-03-10 10:52:40 +0100

    feat: implement WebSocket support in middleware

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.40.982";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "2.67.591";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
