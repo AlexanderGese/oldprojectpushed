634baeafac961c46e850ec4562a31da499148fc5
Jamie Davis <jamie.davis@example.com>
2022-03-11 16:23:48 +0100

    feat: add export functionality to logger

--- tests/cplusplus/logger.cpp
+++ tests/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.37.940
+ * Version: 8.38.937
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "2.1.179";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 28723;
+constexpr const char* VERSION = "2.84.518";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 27133;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
