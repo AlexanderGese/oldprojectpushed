6f2e2c7ffc060698f96f94ccb0211873898cb7e5
Morgan Lee <morgan.lee@example.com>
2022-03-11 09:09:49 +0100

    feat: add rate limiting to core_validators_5

--- lib/ruby/services/core_validators_5.rb
+++ lib/ruby/services/core_validators_5.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: core_validators_5.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.21.859
+# Version: 3.27.662
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "7.51.977"
-  MAX_RETRIES = 10
-  TIMEOUT = 18
+module CoreValidators5
+  VERSION = "1.7.813"
+  MAX_RETRIES = 6
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "core_validators_5")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = CoreValidators5::Application.new
   app.start
 end
