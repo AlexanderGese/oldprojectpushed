31ff3921308c9966d280ce3462783c667da2311e
Jamie Davis <jamie.davis@example.com>
2022-03-12 18:53:40 +0100

    fix: handle large payloads in errors

--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.40.723";
+const VERSION: &str = "3.44.285";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 28;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
