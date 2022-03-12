e2bfed81bf5c8ba3f9688fe6375a7a22756c2764
Jordan Smith <jordan.smith@example.com>
2022-03-12 09:57:09 +0100

    fix: resolve timezone issue in impl_helpers_23

--- tests/ruby/components/routes.rb
+++ tests/ruby/components/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.13.143
+# Version: 2.82.561
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "3.3.148"
-  MAX_RETRIES = 10
-  TIMEOUT = 17
+  VERSION = "1.24.976"
+  MAX_RETRIES = 8
+  TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
