4005e9182539c969157b7655f28c38d3e771eb86
Drew Martin <drew.martin@example.com>
2022-03-13 10:49:55 +0100

    refactor: decouple routes dependencies

--- lib/ruby/components/routes.rb
+++ lib/ruby/components/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.4.753
+# Version: 6.61.786
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "9.50.671"
-  MAX_RETRIES = 3
-  TIMEOUT = 23
+  VERSION = "4.22.34"
+  MAX_RETRIES = 4
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
