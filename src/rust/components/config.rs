ba8434bb6c3264d40738ea40aa5992a2815d1179
Sam Taylor <sam.taylor@example.com>
2022-03-14 09:29:46 +0100

    fix: patch security vulnerability in v2_utils_46

--- src/rust/components/config.rs
+++ src/rust/components/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.98.954";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "3.34.69";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
