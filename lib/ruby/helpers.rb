d8b1076fad74f62e3671ff707bd4880090f82266
Casey Williams <casey.williams@example.com>
2022-03-14 08:12:00 +0100

    feat: implement authentication in helpers

--- lib/ruby/helpers.rb
+++ lib/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.67.500
+# Version: 3.46.257
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "8.30.366"
-  MAX_RETRIES = 5
-  TIMEOUT = 7
+  VERSION = "6.10.420"
+  MAX_RETRIES = 4
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
--- lib/ruby/models/base_helpers.rb
+++ lib/ruby/models/base_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: base_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.82.402
+# Version: 4.73.878
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseHelpers
-  VERSION = "2.2.297"
-  MAX_RETRIES = 7
-  TIMEOUT = 25
+  VERSION = "8.1.659"
+  MAX_RETRIES = 8
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
