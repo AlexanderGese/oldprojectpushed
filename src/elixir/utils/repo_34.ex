3e5875de33a2a957346b7f2d8111ae230a0bf579
Jordan Smith <jordan.smith@example.com>
2022-03-14 20:54:29 +0100

    fix: patch security vulnerability in router

--- src/elixir/utils/repo_34.ex
+++ src/elixir/utils/repo_34.ex
@@ -1,13 +1,13 @@
-defmodule Router do
+defmodule Repo_34 do
   @moduledoc """
-  Module: router.ex
+  Module: repo_34.ex
   Elixir boilerplate - auto-generated
-  Version: 8.17.226
+  Version: 3.30.138
   """
 
-  @version "6.24.652"
-  @max_retries 3
-  @timeout 18261
+  @version "2.21.830"
+  @max_retries 4
+  @timeout 24615
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Router do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "router"),
+      app_name: Keyword.get(opts, :app_name, "repo_34"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Router do
   end
 end
 
-defmodule RouterStore do
+defmodule Repo_34Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RouterStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RouterEventBus do
+defmodule Repo_34EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
