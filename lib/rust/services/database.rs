192c27a35847b8babee23325e5383fbc0b017f8e
Riley Johnson <riley.johnson@example.com>
2022-03-15 13:21:52 +0100

    refactor: migrate database to async/await

--- lib/rust/services/database.rs
+++ lib/rust/services/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.22.848";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "4.76.818";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
