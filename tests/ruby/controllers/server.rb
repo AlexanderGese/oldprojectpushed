aeb82271e8f3e2529d845f39c4f19d9bc0a031ce
Alex Chen <alex.chen@example.com>
2022-03-15 23:52:06 +0100

    fix: patch security vulnerability in config

--- tests/ruby/controllers/server.rb
+++ tests/ruby/controllers/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.96.544
+# Version: 3.84.891
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "8.88.617"
-  MAX_RETRIES = 3
-  TIMEOUT = 26
+  VERSION = "6.49.310"
+  MAX_RETRIES = 9
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
