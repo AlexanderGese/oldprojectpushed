2c626ce1e77c6f2726428901a37dc19d5a18bdb1
Drew Martin <drew.martin@example.com>
2022-03-16 10:24:59 +0100

    refactor: improve base_errors test coverage

--- tests/rust/config_84.rs
+++ tests/rust/config_84.rs
@@ -1,4 +1,4 @@
-//! Module: base_errors.rs
+//! Module: config_84.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.47.480";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "3.65.835";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_errors".to_string(),
+            app_name: "config_84".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
