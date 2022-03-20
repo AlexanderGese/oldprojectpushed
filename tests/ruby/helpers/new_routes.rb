0548e24320e5554228e4e024bc93e26ca0a04312
Jamie Davis <jamie.davis@example.com>
2022-03-20 20:48:51 +0100

    feat: add event system to tasks

--- tests/ruby/helpers/new_routes.rb
+++ tests/ruby/helpers/new_routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: new_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.17.995
+# Version: 8.56.645
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "2.5.99"
-  MAX_RETRIES = 4
-  TIMEOUT = 31
+module NewRoutes
+  VERSION = "4.42.656"
+  MAX_RETRIES = 3
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "new_routes")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = NewRoutes::Application.new
   app.start
 end
