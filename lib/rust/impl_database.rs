2791a4fdb8657e1e13b33bd72b62b8005ee4888a
Jordan Smith <jordan.smith@example.com>
2022-03-21 18:19:01 +0100

    fix: handle concurrent access in impl_database

--- lib/rust/impl_database.rs
+++ lib/rust/impl_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.38.529";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "7.90.89";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
