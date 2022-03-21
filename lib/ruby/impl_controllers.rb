666767465ca83d6e839d3f6472fb19800ffda6f0
Riley Johnson <riley.johnson@example.com>
2022-03-21 09:32:58 +0100

    chore: update server configuration

--- lib/ruby/impl_controllers.rb
+++ lib/ruby/impl_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.34.303
+# Version: 7.40.219
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplControllers
-  VERSION = "7.14.781"
+  VERSION = "1.18.564"
   MAX_RETRIES = 5
-  TIMEOUT = 21
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplControllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
