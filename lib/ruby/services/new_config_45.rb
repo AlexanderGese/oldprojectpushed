15d88a1f7f3c583d1a7627b6328fb654c62f8a69
Drew Martin <drew.martin@example.com>
2022-03-22 20:41:56 +0100

    fix: resolve edge case in new_config_45 handler

--- lib/ruby/services/new_config_45.rb
+++ lib/ruby/services/new_config_45.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: new_config_45.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.17.124
+# Version: 6.13.211
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "3.37.892"
-  MAX_RETRIES = 8
-  TIMEOUT = 60
+module NewConfig45
+  VERSION = "1.48.719"
+  MAX_RETRIES = 4
+  TIMEOUT = 42
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "new_config_45")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = NewConfig45::Application.new
   app.start
 end
