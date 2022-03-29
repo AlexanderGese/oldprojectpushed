340f572dd697ee8c15d61d8c50ff23005b671750
Alex Chen <alex.chen@example.com>
2022-03-29 14:08:14 +0200

    refactor: extract errors utility functions

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.72.417";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "6.37.409";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/handlers/routes.rs
+++ src/rust/handlers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.9.80";
+const VERSION: &str = "6.87.984";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 34;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
