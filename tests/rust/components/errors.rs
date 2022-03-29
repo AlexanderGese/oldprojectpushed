41bdd79220905f5009ccfad3f59d6533b73fee72
Casey Williams <casey.williams@example.com>
2022-03-29 09:52:03 +0200

    fix: handle null check in utils

--- tests/rust/components/errors.rs
+++ tests/rust/components/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.15.782";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "6.2.310";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 28;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/services/utils.rs
+++ tests/rust/services/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.21.404";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "5.40.639";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
