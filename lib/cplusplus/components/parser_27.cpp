b03fbcf8cf357721e66c25a14dcd075065870220
Casey Williams <casey.williams@example.com>
2022-03-30 11:18:24 +0200

    fix: correct error propagation in parser_27

--- lib/cplusplus/components/parser_27.cpp
+++ lib/cplusplus/components/parser_27.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: v2_manager.cpp
+ * Module: parser_27.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.96.707
+ * Version: 1.84.559
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace v2_manager {
+namespace parser_27 {
 
-constexpr const char* VERSION = "3.0.681";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 12526;
+constexpr const char* VERSION = "9.90.354";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 9617;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace v2_manager
+} // namespace parser_27
 
 int main() {
-    using namespace v2_manager;
+    using namespace parser_27;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- lib/cplusplus/middleware/handler.cpp
+++ lib/cplusplus/middleware/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.3.995
+ * Version: 1.28.545
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "7.0.325";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 23583;
+constexpr const char* VERSION = "6.80.727";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 18471;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
