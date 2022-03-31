9a920b0248db33a5b41ef55e3b9574fae712f434
Jordan Smith <jordan.smith@example.com>
2022-03-31 17:28:55 +0200

    chore: add linting rules for core_database_15

--- tests/rust/core_database_15.rs
+++ tests/rust/core_database_15.rs
@@ -1,4 +1,4 @@
-//! Module: base_models.rs
+//! Module: core_database_15.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.28.629";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "2.66.493";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_models".to_string(),
+            app_name: "core_database_15".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
