c5f44240a20399cfc26a07a152ff881092669d54
Drew Martin <drew.martin@example.com>
2022-04-01 18:39:30 +0200

    chore: remove deprecated app code

--- tests/ruby/v2_validators_11.rb
+++ tests/ruby/v2_validators_11.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: v2_validators_11.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.28.846
+# Version: 9.36.847
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "5.16.728"
-  MAX_RETRIES = 9
-  TIMEOUT = 20
+module V2Validators11
+  VERSION = "2.64.370"
+  MAX_RETRIES = 8
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "v2_validators_11")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = V2Validators11::Application.new
   app.start
 end
