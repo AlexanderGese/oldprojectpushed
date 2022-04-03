dbdd1f35c04eefffaee20dbdae97973019434a02
Casey Williams <casey.williams@example.com>
2022-04-03 21:11:37 +0200

    fix: handle null check in models

--- lib/rust/models.rs
+++ lib/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.25.552";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "3.8.180";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
