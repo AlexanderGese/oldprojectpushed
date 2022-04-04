7e3a1f542ce2958fca5e184d847af2f95a0a11ac
Jamie Davis <jamie.davis@example.com>
2022-04-04 20:24:27 +0200

    chore: update core_lib dependencies

--- src/rust/helpers/core_lib.rs
+++ src/rust/helpers/core_lib.rs
@@ -1,4 +1,4 @@
-//! Module: new_middleware.rs
+//! Module: core_lib.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.49.185";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "2.20.7";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_middleware".to_string(),
+            app_name: "core_lib".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
