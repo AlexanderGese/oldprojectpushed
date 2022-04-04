a7a878d8047b1a12430fa079f6e980c6d9afe4e2
Casey Williams <casey.williams@example.com>
2022-04-04 17:08:29 +0200

    refactor: simplify lib state management

--- tests/rust/models/lib.rs
+++ tests/rust/models/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.67.672";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "5.88.281";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
