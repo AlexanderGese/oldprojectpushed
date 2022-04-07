d1dff2dd6cf5c47fab57891f98fc6a0ba46aecaf
Jamie Davis <jamie.davis@example.com>
2022-04-07 08:28:58 +0200

    chore: bump utils version

--- src/cplusplus/controllers/utils.cpp
+++ src/cplusplus/controllers/utils.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: utils.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.73.651
+ * Version: 7.90.294
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace utils {
 
-constexpr const char* VERSION = "8.51.737";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 7098;
+constexpr const char* VERSION = "4.30.855";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 13985;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/engine.cpp
+++ src/cplusplus/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.62.428
+ * Version: 9.74.342
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "2.26.65";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 5569;
+constexpr const char* VERSION = "2.46.273";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 10652;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
