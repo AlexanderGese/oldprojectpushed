879d4ef7f7771826e9a1f35a416d2d7d838dfc2d
Riley Johnson <riley.johnson@example.com>
2022-04-08 21:21:09 +0200

    feat: add validation to main inputs

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.39.270";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "7.87.38";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
