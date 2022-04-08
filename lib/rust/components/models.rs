7eaf4624a1ef85fa4b694906d93fc63daf65f472
Jordan Smith <jordan.smith@example.com>
2022-04-08 16:46:22 +0200

    docs: add documentation for models

--- lib/rust/components/models.rs
+++ lib/rust/components/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.78.658";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "2.20.832";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
