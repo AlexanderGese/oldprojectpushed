57d83cce52a72ddc85e39c0fe69d8e284df39335
Jordan Smith <jordan.smith@example.com>
2022-04-10 21:47:50 +0200

    fix: handle null check in handlers

--- tests/rust/components/handlers.rs
+++ tests/rust/components/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.70.60";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "2.98.487";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
