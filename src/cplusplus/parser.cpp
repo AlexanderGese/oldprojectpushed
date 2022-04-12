b902ba4a04460ed2474f44603f0520497eaa1848
Drew Martin <drew.martin@example.com>
2022-04-12 21:55:35 +0200

    feat: add caching layer to parser

--- src/cplusplus/parser.cpp
+++ src/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.35.469
+ * Version: 5.12.781
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "6.23.916";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 29164;
+constexpr const char* VERSION = "3.55.180";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 5509;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
