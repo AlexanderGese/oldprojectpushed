855e2302e300b25591235a0410d75ecb29131a5a
Jamie Davis <jamie.davis@example.com>
2022-04-13 15:05:41 +0200

    fix: resolve memory leak in worker

--- tests/elixir/base_worker_32.ex
+++ tests/elixir/base_worker_32.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule Base_Worker_32 do
   @moduledoc """
-  Module: worker.ex
+  Module: base_worker_32.ex
   Elixir boilerplate - auto-generated
-  Version: 6.38.117
+  Version: 6.39.565
   """
 
-  @version "1.77.323"
-  @max_retries 7
-  @timeout 20103
+  @version "9.50.92"
+  @max_retries 3
+  @timeout 16746
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "base_worker_32"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule Base_Worker_32Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule Base_Worker_32EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
