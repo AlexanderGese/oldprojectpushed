b97d0de9b0b5093bfdba2f997e85d76072c4443e
Sam Taylor <sam.taylor@example.com>
2022-04-15 22:35:35 +0200

    feat: add batch processing to core_database

--- tests/rust/handlers/core_database.rs
+++ tests/rust/handlers/core_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.25.56";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "9.92.76";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
