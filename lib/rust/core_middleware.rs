db3ca355ad73eecb294511889047941926b6d04d
Jamie Davis <jamie.davis@example.com>
2022-04-16 13:41:56 +0200

    feat: add search functionality to v2_config_32

--- lib/rust/core_middleware.rs
+++ lib/rust/core_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.26.612";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "4.61.313";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
