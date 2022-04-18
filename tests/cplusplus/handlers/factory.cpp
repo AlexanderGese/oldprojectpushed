b56838bd76227ae24aef54c06ca27e7aab4efd6d
Sam Taylor <sam.taylor@example.com>
2022-04-18 20:05:34 +0200

    feat: implement SSE streaming in factory

--- tests/cplusplus/handlers/factory.cpp
+++ tests/cplusplus/handlers/factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.61.175
+ * Version: 5.5.527
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace factory {
 
-constexpr const char* VERSION = "1.24.442";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 18799;
+constexpr const char* VERSION = "8.77.351";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 6428;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
