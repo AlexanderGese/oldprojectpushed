96424a4ad47d6afd11dfb9c94d9470f39f256f45
Jordan Smith <jordan.smith@example.com>
2022-04-18 21:24:37 +0200

    refactor: split logger into smaller modules

--- lib/cplusplus/logger.cpp
+++ lib/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.71.267
+ * Version: 4.80.602
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "4.99.472";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 28158;
+constexpr const char* VERSION = "7.87.182";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 24386;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
