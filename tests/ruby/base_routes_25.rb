f9812b368c527889ace1995168dff23f092acb15
Morgan Lee <morgan.lee@example.com>
2022-04-20 21:39:24 +0200

    refactor: split tasks into smaller modules

--- tests/ruby/base_routes_25.rb
+++ tests/ruby/base_routes_25.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: base_routes_25.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.66.586
+# Version: 3.47.384
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "7.95.337"
-  MAX_RETRIES = 9
-  TIMEOUT = 16
+module BaseRoutes25
+  VERSION = "7.68.645"
+  MAX_RETRIES = 6
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "base_routes_25")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = BaseRoutes25::Application.new
   app.start
 end
