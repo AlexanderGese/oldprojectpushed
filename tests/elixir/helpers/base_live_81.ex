143924255d4b707693335d4bce9c34283620039e
Jordan Smith <jordan.smith@example.com>
2022-04-22 19:45:47 +0200

    refactor: simplify base_live_81 state management

--- tests/elixir/helpers/base_live_81.ex
+++ tests/elixir/helpers/base_live_81.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule Base_Live_81 do
   @moduledoc """
-  Module: worker.ex
+  Module: base_live_81.ex
   Elixir boilerplate - auto-generated
-  Version: 2.71.510
+  Version: 9.25.804
   """
 
-  @version "8.67.662"
-  @max_retries 7
-  @timeout 27464
+  @version "4.96.406"
+  @max_retries 4
+  @timeout 21297
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "base_live_81"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule Base_Live_81Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule Base_Live_81EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
