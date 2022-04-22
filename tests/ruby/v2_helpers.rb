7a49bf4a175b5b801675ca7a7104098cbda885a5
Riley Johnson <riley.johnson@example.com>
2022-04-22 11:36:12 +0200

    fix: resolve memory leak in validators

--- tests/ruby/v2_helpers.rb
+++ tests/ruby/v2_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.13.751
+# Version: 1.1.790
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Helpers
-  VERSION = "3.10.656"
-  MAX_RETRIES = 5
-  TIMEOUT = 5
+  VERSION = "7.51.509"
+  MAX_RETRIES = 9
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
