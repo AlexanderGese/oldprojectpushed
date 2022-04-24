bff2e3359f70d982a00e275129568f13f10cec12
Jordan Smith <jordan.smith@example.com>
2022-04-24 09:12:35 +0200

    feat: add pagination to base_routes

--- tests/rust/base_routes.rs
+++ tests/rust/base_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.96.606";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "9.66.484";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
