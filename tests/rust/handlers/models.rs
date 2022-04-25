02e4f9508c2d895cb15f71b3d002673f82a96a38
Drew Martin <drew.martin@example.com>
2022-04-25 11:38:25 +0200

    refactor: split enhanced_errors_52 into smaller modules

--- tests/rust/handlers/models.rs
+++ tests/rust/handlers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.86.826";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "8.5.502";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
