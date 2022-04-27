937463db929030ee0119d526b808136dd0295ea5
Casey Williams <casey.williams@example.com>
2022-04-27 17:24:36 +0200

    feat: implement WebSocket support in tasks

--- src/ruby/models/tasks.rb
+++ src/ruby/models/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.14.279
+# Version: 3.50.353
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "2.8.808"
+  VERSION = "5.2.739"
   MAX_RETRIES = 4
-  TIMEOUT = 37
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
