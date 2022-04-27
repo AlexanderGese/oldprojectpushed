3dbb0a587302caedb36f5401631b140c9cded5a6
Drew Martin <drew.martin@example.com>
2022-04-27 18:03:09 +0200

    fix: resolve memory leak in core_errors

--- lib/rust/core_errors.rs
+++ lib/rust/core_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.34.604";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "5.20.558";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
