06c686d39a5c68916dbfd016b8feae977c43281b
Riley Johnson <riley.johnson@example.com>
2022-04-29 23:46:23 +0200

    fix: handle concurrent access in models

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.33.268";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "3.6.436";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.22.490";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "4.24.794";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
