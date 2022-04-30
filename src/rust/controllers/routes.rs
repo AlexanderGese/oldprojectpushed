8b5f045a4f5b5927c1de6d22511771dbe339b03e
Sam Taylor <sam.taylor@example.com>
2022-04-30 09:25:56 +0200

    fix: handle empty response in routes

--- src/rust/controllers/routes.rs
+++ src/rust/controllers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.63.245";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "3.22.641";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils/core_database.rs
+++ src/rust/utils/core_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.37.623";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "6.4.897";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
