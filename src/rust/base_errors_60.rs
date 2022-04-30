45f7cac65fe78077f49c9a2652ab96466f6ad153
Morgan Lee <morgan.lee@example.com>
2022-04-30 16:50:24 +0200

    feat: implement WebSocket support in database

--- src/rust/base_errors_60.rs
+++ src/rust/base_errors_60.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: base_errors_60.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.53.826";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "2.99.385";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "base_errors_60".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/utils/models.rs
+++ src/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.71.958";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "2.44.437";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
