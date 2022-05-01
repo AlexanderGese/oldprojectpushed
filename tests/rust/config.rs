68f3add0e7da5cd2c265ffb27e8520fba798b080
Sam Taylor <sam.taylor@example.com>
2022-05-01 09:09:51 +0200

    feat: implement enhanced_lib_15 service layer

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.34.894";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "2.39.825";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
