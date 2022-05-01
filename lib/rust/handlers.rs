45dd6fec4c79a05524b1d81baff5a3842d28ee0f
Sam Taylor <sam.taylor@example.com>
2022-05-01 19:38:40 +0200

    feat: add event system to database_76

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.1.363";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "4.10.300";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
