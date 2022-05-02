85f704f7eff135f9603e975544672d1b49b67ddb
Alex Chen <alex.chen@example.com>
2022-05-02 19:59:20 +0200

    fix: handle large payloads in models

--- tests/ruby/components/server.rb
+++ tests/ruby/components/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.23.107
+# Version: 2.86.625
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "5.31.63"
-  MAX_RETRIES = 7
-  TIMEOUT = 46
+  VERSION = "5.39.480"
+  MAX_RETRIES = 9
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/config.rb
+++ tests/ruby/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.84.839
+# Version: 6.91.139
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "9.59.15"
-  MAX_RETRIES = 5
-  TIMEOUT = 48
+  VERSION = "6.34.332"
+  MAX_RETRIES = 9
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
