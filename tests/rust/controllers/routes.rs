6a24763dfc6d72bcf5f2652b9ac57a40ff4fbcb8
Morgan Lee <morgan.lee@example.com>
2022-05-04 11:50:01 +0200

    feat: add batch processing to routes

--- tests/rust/controllers/routes.rs
+++ tests/rust/controllers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.7.554";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "5.80.132";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
