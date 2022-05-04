e7ac49b885a4b4ae1ce6548968c7ba3c6fdffb2b
Riley Johnson <riley.johnson@example.com>
2022-05-04 20:20:31 +0200

    feat: implement retry logic in core_models

--- src/ruby/impl_app.rb
+++ src/ruby/impl_app.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_app.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.84.406
+# Version: 7.60.82
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplApp
-  VERSION = "3.68.380"
-  MAX_RETRIES = 10
-  TIMEOUT = 55
+  VERSION = "9.58.313"
+  MAX_RETRIES = 5
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplApp
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
