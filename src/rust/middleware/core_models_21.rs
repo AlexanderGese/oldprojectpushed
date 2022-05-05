96e68bd70b39a45d49091c9b62736a167c57df11
Riley Johnson <riley.johnson@example.com>
2022-05-05 16:43:43 +0200

    fix: handle timeout in database_60 requests

--- src/rust/middleware/core_models_21.rs
+++ src/rust/middleware/core_models_21.rs
@@ -1,4 +1,4 @@
-//! Module: v2_routes.rs
+//! Module: core_models_21.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.86.172";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "7.53.190";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_routes".to_string(),
+            app_name: "core_models_21".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
