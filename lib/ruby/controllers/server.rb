48692dcb52265e0cc0e1964afe61674c04d79620
Sam Taylor <sam.taylor@example.com>
2022-05-12 09:41:41 +0200

    chore: update middleware dependencies

--- lib/ruby/controllers/server.rb
+++ lib/ruby/controllers/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.19.848
+# Version: 4.85.85
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "8.87.574"
-  MAX_RETRIES = 8
-  TIMEOUT = 7
+  VERSION = "9.25.53"
+  MAX_RETRIES = 9
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
