d26118ca10a582556f4103392e6c9c255312494a
Morgan Lee <morgan.lee@example.com>
2022-05-13 19:29:20 +0200

    fix: handle concurrent access in v2_validators_92

--- tests/ruby/v2_validators_92.rb
+++ tests/ruby/v2_validators_92.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_validators_92.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.16.949
+# Version: 7.78.412
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Validators92
-  VERSION = "3.40.519"
+  VERSION = "1.39.189"
   MAX_RETRIES = 8
-  TIMEOUT = 40
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Validators92
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
