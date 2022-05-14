6cd50aa4e0094af87faf9ee738d556a51e02a806
Jamie Davis <jamie.davis@example.com>
2022-05-14 12:23:53 +0200

    chore: bump helpers version

--- lib/ruby/models/helpers.rb
+++ lib/ruby/models/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.22.64
+# Version: 8.59.639
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "6.26.602"
-  MAX_RETRIES = 7
-  TIMEOUT = 52
+  VERSION = "3.81.459"
+  MAX_RETRIES = 10
+  TIMEOUT = 7
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
