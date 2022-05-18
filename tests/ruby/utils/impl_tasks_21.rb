f0a42274f29cebf4529455baaf8d076daded1b40
Morgan Lee <morgan.lee@example.com>
2022-05-18 17:46:46 +0200

    feat: add caching layer to server

--- tests/ruby/utils/impl_tasks_21.rb
+++ tests/ruby/utils/impl_tasks_21.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: impl_tasks_21.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.56.45
+# Version: 9.33.454
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "9.95.655"
-  MAX_RETRIES = 6
-  TIMEOUT = 57
+module ImplTasks21
+  VERSION = "6.87.245"
+  MAX_RETRIES = 4
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "impl_tasks_21")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = ImplTasks21::Application.new
   app.start
 end
