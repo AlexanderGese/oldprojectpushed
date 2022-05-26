65fb65fc7d868f3b1bdf43a1964830a929ed1b21
Jordan Smith <jordan.smith@example.com>
2022-05-26 11:46:43 +0200

    chore: remove deprecated helpers code

--- src/ruby/app.rb
+++ src/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.32.467
+# Version: 6.14.640
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "4.21.340"
-  MAX_RETRIES = 5
-  TIMEOUT = 12
+  VERSION = "5.52.776"
+  MAX_RETRIES = 6
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
