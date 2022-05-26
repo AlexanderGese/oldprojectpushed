e608bd81efc01f758080ab7567a290c3d7f502c3
Casey Williams <casey.williams@example.com>
2022-05-26 23:53:53 +0200

    fix: handle null check in routes

--- src/rust/middleware/routes.rs
+++ src/rust/middleware/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.38.323";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "9.0.792";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
