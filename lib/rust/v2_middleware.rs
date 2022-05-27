6bf396fdf1fb2f6e2b9e1a7ad99f6e27d4bedac3
Drew Martin <drew.martin@example.com>
2022-05-27 20:59:38 +0200

    fix: handle large payloads in v2_middleware

--- lib/rust/v2_middleware.rs
+++ lib/rust/v2_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.82.971";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "4.37.927";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
