8dbe84bd5f142d2dd3ae367901c94a0c0aaacb9e
Drew Martin <drew.martin@example.com>
2022-05-27 18:29:15 +0200

    chore: configure CI/CD for lib

--- src/rust/controllers/lib.rs
+++ src/rust/controllers/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.98.548";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "5.6.844";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 28;
 
 #[derive(Debug, Clone)]
 pub struct Config {
