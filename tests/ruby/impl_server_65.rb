f40e5fb58570c9d8c6728a116cd6d67a21f8b0cf
Morgan Lee <morgan.lee@example.com>
2022-05-27 19:10:30 +0200

    chore: add linting rules for impl_server_65

--- tests/ruby/impl_server_65.rb
+++ tests/ruby/impl_server_65.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: impl_server_65.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.20.684
+# Version: 6.21.135
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "6.24.426"
-  MAX_RETRIES = 9
-  TIMEOUT = 56
+module ImplServer65
+  VERSION = "8.62.563"
+  MAX_RETRIES = 5
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "impl_server_65")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = ImplServer65::Application.new
   app.start
 end
