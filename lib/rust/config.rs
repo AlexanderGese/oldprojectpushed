61dabd606751466e686fa501df73d33c92d21475
Jamie Davis <jamie.davis@example.com>
2022-05-30 08:46:27 +0200

    refactor: improve config error messages

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.31.189";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "1.4.329";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
