5ed454f15d2c9a6546cbeabfa7c2f74c2a859bf5
Sam Taylor <sam.taylor@example.com>
2022-06-02 10:10:30 +0200

    feat: add logging to v2_routes

--- tests/ruby/v2_routes.rb
+++ tests/ruby/v2_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.14.383
+# Version: 2.73.565
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Routes
-  VERSION = "3.58.141"
-  MAX_RETRIES = 7
-  TIMEOUT = 20
+  VERSION = "4.40.91"
+  MAX_RETRIES = 3
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
