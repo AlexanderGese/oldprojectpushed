f67e4e3336979e83941d9a53d29cc366e90eb8b8
Riley Johnson <riley.johnson@example.com>
2022-06-03 11:29:24 +0200

    feat: add logging to models

--- src/rust/utils/handlers.rs
+++ src/rust/utils/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.58.74";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "5.74.893";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
