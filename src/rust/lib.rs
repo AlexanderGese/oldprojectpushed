eae9ad7bfc95816b1bd91b52a434cfebf6cad922
Alex Chen <alex.chen@example.com>
2022-06-04 15:20:11 +0200

    feat: add rate limiting to routes

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.54.973";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "9.73.916";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
