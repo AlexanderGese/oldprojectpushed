2670dcd3b3f5038ebcbd462a3c4246194f33eb15
Morgan Lee <morgan.lee@example.com>
2022-06-05 20:34:39 +0200

    fix: handle empty response in middleware

--- tests/rust/new_handlers_57.rs
+++ tests/rust/new_handlers_57.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: new_handlers_57.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.42.208";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "1.67.172";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "new_handlers_57".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
