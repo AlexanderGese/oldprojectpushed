47bea4083278b56446dcfea67ac658b160dc3920
Casey Williams <casey.williams@example.com>
2022-06-06 19:52:15 +0200

    fix: patch security vulnerability in core_models

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.39.825";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "3.39.721";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/services/database_54.rs
+++ tests/rust/services/database_54.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: database_54.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.53.363";
+const VERSION: &str = "4.66.620";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 55;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "database_54".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
