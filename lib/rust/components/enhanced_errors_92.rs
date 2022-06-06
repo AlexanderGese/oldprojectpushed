7c07cdd673a9b447e1dcdf8dca05f15e112b3a40
Casey Williams <casey.williams@example.com>
2022-06-06 20:23:26 +0200

    chore: update errors dependencies

--- lib/rust/components/enhanced_errors_92.rs
+++ lib/rust/components/enhanced_errors_92.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: enhanced_errors_92.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.25.892";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "2.14.182";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "enhanced_errors_92".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/utils_26.rs
+++ lib/rust/utils_26.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: utils_26.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.25.892";
+const VERSION: &str = "3.50.6";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 28;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "utils_26".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
