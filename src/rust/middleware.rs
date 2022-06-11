44da4288d440d6edca5cd96d420aa08b5fb33c47
Sam Taylor <sam.taylor@example.com>
2022-06-11 14:04:11 +0200

    fix: handle empty response in middleware

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.21.522";
+const VERSION: &str = "7.83.629";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 57;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
