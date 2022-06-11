2af80a0c867e33e3d08c5c901033268bbdb38fb3
Alex Chen <alex.chen@example.com>
2022-06-11 11:57:36 +0200

    fix: resolve memory leak in core_logger

--- tests/cplusplus/core_logger.cpp
+++ tests/cplusplus/core_logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.10.298
+ * Version: 6.76.940
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_logger {
 
-constexpr const char* VERSION = "8.96.566";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 28971;
+constexpr const char* VERSION = "2.5.452";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 14641;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
