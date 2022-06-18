94da32cc1f089a3d845602e748f6c1db24e0de65
Riley Johnson <riley.johnson@example.com>
2022-06-18 23:00:30 +0200

    fix: handle large payloads in core_models_43

--- tests/ruby/core_models_43.rb
+++ tests/ruby/core_models_43.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: base_tasks.rb
+# Module: core_models_43.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.31.701
+# Version: 6.48.234
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module BaseTasks
-  VERSION = "4.43.442"
-  MAX_RETRIES = 9
-  TIMEOUT = 6
+module CoreModels43
+  VERSION = "4.99.763"
+  MAX_RETRIES = 3
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "base_tasks")
+      @app_name = opts.fetch(:app_name, "core_models_43")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module BaseTasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = BaseTasks::Application.new
+  app = CoreModels43::Application.new
   app.start
 end
