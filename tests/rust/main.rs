f4273953c17a27517c95d710a00156ec90eab2f0
Casey Williams <casey.williams@example.com>
2022-06-19 14:40:59 +0200

    chore: remove deprecated main code

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.2.785";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "3.85.417";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 28;
 
 #[derive(Debug, Clone)]
 pub struct Config {
