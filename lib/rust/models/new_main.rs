9eb228fd44e4fbfc6d24057c4e266eac12107f2a
Morgan Lee <morgan.lee@example.com>
2022-06-20 14:52:58 +0200

    chore: add linting rules for new_main

--- lib/rust/models/new_main.rs
+++ lib/rust/models/new_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.69.469";
+const VERSION: &str = "5.65.596";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 39;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
