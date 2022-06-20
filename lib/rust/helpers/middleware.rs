0f3c3ff8bee70fa8c2039434fdf8cdca70885ccc
Riley Johnson <riley.johnson@example.com>
2022-06-20 20:41:20 +0200

    feat: add rate limiting to middleware

--- lib/rust/helpers/middleware.rs
+++ lib/rust/helpers/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.95.191";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 55;
+const VERSION: &str = "4.31.571";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
