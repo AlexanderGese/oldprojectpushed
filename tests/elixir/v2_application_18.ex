b4bce057d255f903983d9d4828e65e3d8a320de7
Morgan Lee <morgan.lee@example.com>
2022-06-22 08:32:07 +0200

    test: add unit tests for router

--- tests/elixir/v2_application_18.ex
+++ tests/elixir/v2_application_18.ex
@@ -1,13 +1,13 @@
-defmodule Router do
+defmodule V2_Application_18 do
   @moduledoc """
-  Module: router.ex
+  Module: v2_application_18.ex
   Elixir boilerplate - auto-generated
-  Version: 1.72.834
+  Version: 1.41.25
   """
 
-  @version "7.58.984"
-  @max_retries 9
-  @timeout 5973
+  @version "6.47.901"
+  @max_retries 3
+  @timeout 25464
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Router do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "router"),
+      app_name: Keyword.get(opts, :app_name, "v2_application_18"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Router do
   end
 end
 
-defmodule RouterStore do
+defmodule V2_Application_18Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RouterStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RouterEventBus do
+defmodule V2_Application_18EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
