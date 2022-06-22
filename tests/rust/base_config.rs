0e7de0307f90fa85b9f9f788e864249c0a279674
Alex Chen <alex.chen@example.com>
2022-06-22 17:04:20 +0200

    fix: correct date formatting in base_config

--- tests/rust/base_config.rs
+++ tests/rust/base_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.57.309";
+const VERSION: &str = "9.14.758";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 41;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
