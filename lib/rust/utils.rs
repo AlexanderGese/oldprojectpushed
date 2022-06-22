e56145db9beaef55fbe78f30a89766bebe2bbec9
Sam Taylor <sam.taylor@example.com>
2022-06-22 14:40:36 +0200

    fix: handle concurrent access in utils

--- lib/rust/utils.rs
+++ lib/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.13.142";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "2.92.0";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
