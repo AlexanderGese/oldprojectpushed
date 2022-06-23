87879f62aaac1f5e18fadbde1cd375f6dabadd10
Sam Taylor <sam.taylor@example.com>
2022-06-23 13:53:08 +0200

    refactor: improve enhanced_routes test coverage

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.87.518";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "1.96.860";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
