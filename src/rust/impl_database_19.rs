c92397af90d678310158413f69a63a1386d12b30
Sam Taylor <sam.taylor@example.com>
2022-06-24 09:01:20 +0200

    chore: remove deprecated database code

--- src/rust/impl_database_19.rs
+++ src/rust/impl_database_19.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: impl_database_19.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.28.744";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "7.99.627";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "impl_database_19".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
