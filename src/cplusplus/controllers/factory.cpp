65e48e84b2e27c6142d4fe03ac309175975abe09
Casey Williams <casey.williams@example.com>
2022-06-25 09:54:45 +0200

    feat: add export functionality to parser

--- src/cplusplus/controllers/factory.cpp
+++ src/cplusplus/controllers/factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.69.212
+ * Version: 6.93.769
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace factory {
 
-constexpr const char* VERSION = "2.59.938";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 24313;
+constexpr const char* VERSION = "7.95.412";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 12769;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/parser.cpp
+++ src/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.71.316
+ * Version: 3.37.403
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "9.85.954";
+constexpr const char* VERSION = "9.41.422";
 constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 26436;
+constexpr int TIMEOUT_MS = 17560;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
