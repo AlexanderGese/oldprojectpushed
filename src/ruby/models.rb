3c8ec0c683e7c55babc4d3a7e22583e5ead25acd
Jordan Smith <jordan.smith@example.com>
2022-06-26 19:39:14 +0200

    refactor: extract models utility functions

--- src/ruby/models.rb
+++ src/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.44.991
+# Version: 9.61.479
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "4.61.549"
-  MAX_RETRIES = 6
-  TIMEOUT = 30
+  VERSION = "2.1.61"
+  MAX_RETRIES = 7
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- src/ruby/services/core_routes.rb
+++ src/ruby/services/core_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: core_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.54.844
+# Version: 5.21.383
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreRoutes
-  VERSION = "5.34.647"
-  MAX_RETRIES = 6
-  TIMEOUT = 10
+  VERSION = "6.77.738"
+  MAX_RETRIES = 4
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreRoutes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
