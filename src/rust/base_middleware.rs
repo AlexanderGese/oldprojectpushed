027b61853a423d03695e28740a47640e96d99032
Casey Williams <casey.williams@example.com>
2022-06-26 11:17:10 +0200

    feat: add validation to config inputs

--- src/rust/base_middleware.rs
+++ src/rust/base_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.6.322";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "8.83.914";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
