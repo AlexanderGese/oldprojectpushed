65a8056117709710b6a8e6184836e975b0a35398
Sam Taylor <sam.taylor@example.com>
2022-06-28 19:50:30 +0200

    feat: implement SSE streaming in new_lib

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.96.860";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "4.38.909";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
