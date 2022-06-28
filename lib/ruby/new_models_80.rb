1f8dd9bde61941f3d65ce945d627b37db7c772d3
Sam Taylor <sam.taylor@example.com>
2022-06-28 09:17:48 +0200

    refactor: simplify v2_middleware state management

--- lib/ruby/new_models_80.rb
+++ lib/ruby/new_models_80.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: v2_middleware.rb
+# Module: new_models_80.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.95.671
+# Version: 3.17.83
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module V2Middleware
-  VERSION = "8.49.419"
+module NewModels80
+  VERSION = "2.44.661"
   MAX_RETRIES = 4
-  TIMEOUT = 8
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "v2_middleware")
+      @app_name = opts.fetch(:app_name, "new_models_80")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module V2Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = V2Middleware::Application.new
+  app = NewModels80::Application.new
   app.start
 end
