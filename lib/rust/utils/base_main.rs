3bf3cc23d1519ebd1a1cc854af3eaaba7aefe9b3
Jamie Davis <jamie.davis@example.com>
2022-07-01 11:36:19 +0200

    fix: patch security vulnerability in base_main

--- lib/rust/utils/base_main.rs
+++ lib/rust/utils/base_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.74.3";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "8.55.899";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
