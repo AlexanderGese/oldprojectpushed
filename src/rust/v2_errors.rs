94266001112de2775e7e4a63b6896227b3fbe730
Riley Johnson <riley.johnson@example.com>
2022-07-04 19:05:01 +0200

    chore: remove deprecated models code

--- src/rust/v2_errors.rs
+++ src/rust/v2_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.40.958";
+const VERSION: &str = "6.96.951";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 18;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
