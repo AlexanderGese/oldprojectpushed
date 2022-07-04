18215682bd636a1882ff512de8f3009812a91a10
Jamie Davis <jamie.davis@example.com>
2022-07-04 18:40:33 +0200

    chore: add linting rules for middleware

--- src/ruby/middleware.rb
+++ src/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.55.791
+# Version: 1.48.871
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "9.78.160"
-  MAX_RETRIES = 3
-  TIMEOUT = 39
+  VERSION = "4.46.245"
+  MAX_RETRIES = 9
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
