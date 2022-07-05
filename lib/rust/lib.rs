719d12ac0ec2a609b3513973b093bfcc37a1a69b
Riley Johnson <riley.johnson@example.com>
2022-07-05 23:02:55 +0200

    fix: handle concurrent access in new_database

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.24.14";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "7.37.975";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
