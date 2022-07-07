7542948f1c875ab5fa9804a6757b2a269abaa2ba
Sam Taylor <sam.taylor@example.com>
2022-07-07 18:52:27 +0200

    feat: add caching layer to v2_config

--- src/rust/v2_config.rs
+++ src/rust/v2_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.16.883";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "4.96.808";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
