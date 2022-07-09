4dde9d68ffc0cb5b6a38b779eec9ca8aafa7e27e
Morgan Lee <morgan.lee@example.com>
2022-07-09 21:40:42 +0200

    fix: resolve memory leak in controllers

--- tests/ruby/controllers.rb
+++ tests/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.98.291
+# Version: 4.99.650
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "2.51.819"
-  MAX_RETRIES = 3
-  TIMEOUT = 23
+  VERSION = "1.87.213"
+  MAX_RETRIES = 4
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
