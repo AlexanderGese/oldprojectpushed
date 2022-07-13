e5f9316fcf896aae915afa46583924c98b65463f
Casey Williams <casey.williams@example.com>
2022-07-13 22:16:53 +0200

    feat: implement authentication in lib

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.3.357";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "6.50.438";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
