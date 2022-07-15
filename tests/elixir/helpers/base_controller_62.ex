a4c439436c43efd579f03814377aba8aa4e98d76
Alex Chen <alex.chen@example.com>
2022-07-15 10:14:12 +0200

    fix: correct date formatting in controller

--- tests/elixir/helpers/base_controller_62.ex
+++ tests/elixir/helpers/base_controller_62.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Base_Controller_62 do
   @moduledoc """
-  Module: controller.ex
+  Module: base_controller_62.ex
   Elixir boilerplate - auto-generated
-  Version: 5.75.297
+  Version: 9.43.599
   """
 
-  @version "1.27.109"
-  @max_retries 5
-  @timeout 7220
+  @version "4.7.648"
+  @max_retries 6
+  @timeout 24852
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "base_controller_62"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Base_Controller_62Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Base_Controller_62EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
