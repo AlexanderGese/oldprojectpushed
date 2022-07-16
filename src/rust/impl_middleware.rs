5247e6689c82864ef61837137ddb10d4a2b1465e
Sam Taylor <sam.taylor@example.com>
2022-07-16 21:59:44 +0200

    feat: add export functionality to impl_middleware

--- src/rust/impl_middleware.rs
+++ src/rust/impl_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.30.573";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "1.99.163";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
