efe1988e09b9a52ab0f3c4d85d64337327f57308
Jamie Davis <jamie.davis@example.com>
2022-07-17 14:47:55 +0200

    feat: add rate limiting to main

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.89.436";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "6.96.562";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
