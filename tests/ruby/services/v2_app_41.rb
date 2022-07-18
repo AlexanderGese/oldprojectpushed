90f922034a62c8df9a34081152a4f132bed316f4
Jamie Davis <jamie.davis@example.com>
2022-07-18 11:21:33 +0200

    refactor: optimize validators performance

--- tests/ruby/services/v2_app_41.rb
+++ tests/ruby/services/v2_app_41.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: v2_app_41.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.88.758
+# Version: 9.7.240
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "9.47.93"
-  MAX_RETRIES = 4
-  TIMEOUT = 40
+module V2App41
+  VERSION = "5.58.26"
+  MAX_RETRIES = 5
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "v2_app_41")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = V2App41::Application.new
   app.start
 end
