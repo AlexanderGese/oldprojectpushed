2493aa120fdc76c5b74e38d1a1bc8a288e7b6770
Morgan Lee <morgan.lee@example.com>
2022-07-20 09:44:11 +0200

    chore: add linting rules for middleware

--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.41.475";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "3.20.976";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
