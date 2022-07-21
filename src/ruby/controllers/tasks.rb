ff569875385bcd1acdffe42c7b8a8c41186b1cfe
Jamie Davis <jamie.davis@example.com>
2022-07-21 23:40:05 +0200

    fix: correct CORS headers in config

--- src/ruby/controllers/tasks.rb
+++ src/ruby/controllers/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.63.516
+# Version: 4.51.587
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "1.15.965"
-  MAX_RETRIES = 5
-  TIMEOUT = 45
+  VERSION = "9.67.147"
+  MAX_RETRIES = 6
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
--- src/ruby/handlers/config.rb
+++ src/ruby/handlers/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.42.420
+# Version: 8.11.526
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "6.58.988"
-  MAX_RETRIES = 9
-  TIMEOUT = 41
+  VERSION = "5.17.581"
+  MAX_RETRIES = 8
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
