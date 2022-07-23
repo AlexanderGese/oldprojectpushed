3208e4b520ef6bb1903dabc96e9bd96a9a9f59eb
Alex Chen <alex.chen@example.com>
2022-07-23 17:11:40 +0200

    fix: handle timeout in app requests

--- src/cplusplus/controllers/app.cpp
+++ src/cplusplus/controllers/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.43.669
+ * Version: 5.26.989
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "2.82.605";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 24893;
+constexpr const char* VERSION = "2.66.663";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 19507;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/manager.cpp
+++ src/cplusplus/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.62.833
+ * Version: 2.27.207
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "6.19.224";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 26795;
+constexpr const char* VERSION = "3.73.595";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 10730;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
