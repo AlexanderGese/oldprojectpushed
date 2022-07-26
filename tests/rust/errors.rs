9dc7e19e21e2ef5690406e597c76ccfc2d6be974
Sam Taylor <sam.taylor@example.com>
2022-07-26 21:05:58 +0200

    feat: add rate limiting to core_routes

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.96.562";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "9.93.464";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
