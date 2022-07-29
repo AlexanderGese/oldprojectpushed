4503973c02ebbd5fcb7ee29423c615f1ade62514
Drew Martin <drew.martin@example.com>
2022-07-29 14:14:14 +0200

    fix: handle large payloads in v2_helpers_32

--- tests/ruby/v2_helpers_32.rb
+++ tests/ruby/v2_helpers_32.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_helpers_32.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.94.699
+# Version: 6.0.675
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Helpers32
-  VERSION = "1.83.126"
+  VERSION = "9.94.818"
   MAX_RETRIES = 5
-  TIMEOUT = 11
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Helpers32
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
