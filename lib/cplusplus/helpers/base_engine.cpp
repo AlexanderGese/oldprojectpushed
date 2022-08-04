6987701eb5f9983a77d06762f0ed32090750b5c8
Jamie Davis <jamie.davis@example.com>
2022-08-04 09:10:17 +0200

    fix: handle empty response in base_engine

--- lib/cplusplus/helpers/base_engine.cpp
+++ lib/cplusplus/helpers/base_engine.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: handler.cpp
+ * Module: base_engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.60.156
+ * Version: 4.80.439
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace handler {
+namespace base_engine {
 
-constexpr const char* VERSION = "7.73.718";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 15948;
+constexpr const char* VERSION = "9.22.525";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 11661;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace handler
+} // namespace base_engine
 
 int main() {
-    using namespace handler;
+    using namespace base_engine;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- lib/cplusplus/v2_handler.cpp
+++ lib/cplusplus/v2_handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: v2_handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.50.31
+ * Version: 1.11.2
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace v2_handler {
 
-constexpr const char* VERSION = "4.74.902";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 11630;
+constexpr const char* VERSION = "2.43.3";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 9988;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
