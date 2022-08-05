4f128dbe97aef00557fdf1c067dcf41315c362a7
Sam Taylor <sam.taylor@example.com>
2022-08-05 23:46:02 +0200

    fix: handle null check in v2_database

--- tests/rust/handlers/utils.rs
+++ tests/rust/handlers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.20.736";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "4.25.589";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
