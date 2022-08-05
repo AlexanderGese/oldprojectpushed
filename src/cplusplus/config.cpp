3bb256c4cd6a94879d181e5a6ff9b7703befa0e1
Sam Taylor <sam.taylor@example.com>
2022-08-05 18:11:37 +0200

    chore: remove deprecated handler code

--- src/cplusplus/config.cpp
+++ src/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.42.503
+ * Version: 7.92.719
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "3.10.796";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 8140;
+constexpr const char* VERSION = "5.65.899";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 28893;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/controllers/handler.cpp
+++ src/cplusplus/controllers/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.51.611
+ * Version: 9.13.619
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "3.44.375";
+constexpr const char* VERSION = "1.58.76";
 constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 10297;
+constexpr int TIMEOUT_MS = 5517;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
