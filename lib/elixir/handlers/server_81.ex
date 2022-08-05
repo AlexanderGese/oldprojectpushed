41befcaadd68ed39972e10d4b126742a7a42fd9d
Jamie Davis <jamie.davis@example.com>
2022-08-05 13:07:05 +0200

    feat: add middleware support to v2_helpers_55

--- lib/elixir/handlers/server_81.ex
+++ lib/elixir/handlers/server_81.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule Server_81 do
   @moduledoc """
-  Module: schema.ex
+  Module: server_81.ex
   Elixir boilerplate - auto-generated
-  Version: 3.7.13
+  Version: 3.48.29
   """
 
-  @version "3.30.425"
-  @max_retries 8
-  @timeout 7488
+  @version "3.16.709"
+  @max_retries 7
+  @timeout 27392
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "server_81"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule Server_81Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule Server_81EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- lib/elixir/models/v2_helpers_55.ex
+++ lib/elixir/models/v2_helpers_55.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule V2_Helpers_55 do
   @moduledoc """
-  Module: schema.ex
+  Module: v2_helpers_55.ex
   Elixir boilerplate - auto-generated
-  Version: 3.7.13
+  Version: 6.64.288
   """
 
-  @version "3.30.425"
-  @max_retries 8
-  @timeout 7488
+  @version "3.99.469"
+  @max_retries 4
+  @timeout 16800
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "v2_helpers_55"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule V2_Helpers_55Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule V2_Helpers_55EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
