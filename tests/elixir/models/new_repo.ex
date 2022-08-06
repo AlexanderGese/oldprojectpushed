de58b1211f45f766a54cd2f5e7f61e6a66a86ec2
Morgan Lee <morgan.lee@example.com>
2022-08-06 17:03:03 +0200

    docs: add documentation for new_repo

--- tests/elixir/models/new_repo.ex
+++ tests/elixir/models/new_repo.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule New_Repo do
   @moduledoc """
-  Module: server.ex
+  Module: new_repo.ex
   Elixir boilerplate - auto-generated
-  Version: 5.44.140
+  Version: 8.39.581
   """
 
-  @version "4.19.429"
-  @max_retries 7
-  @timeout 27586
+  @version "7.1.800"
+  @max_retries 10
+  @timeout 21279
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "new_repo"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule New_RepoStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule New_RepoEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
