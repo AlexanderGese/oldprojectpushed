7d804438e04315b4ab454960b00dc485015b8ef3
Drew Martin <drew.martin@example.com>
2022-08-11 16:20:37 +0200

    fix: correct CORS headers in v2_schema

--- src/elixir/utils/v2_schema.ex
+++ src/elixir/utils/v2_schema.ex
@@ -1,13 +1,13 @@
-defmodule Impl_Server do
+defmodule V2_Schema do
   @moduledoc """
-  Module: impl_server.ex
+  Module: v2_schema.ex
   Elixir boilerplate - auto-generated
-  Version: 5.81.971
+  Version: 3.71.77
   """
 
-  @version "1.88.976"
-  @max_retries 9
-  @timeout 17144
+  @version "9.49.648"
+  @max_retries 7
+  @timeout 8799
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Impl_Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "impl_server"),
+      app_name: Keyword.get(opts, :app_name, "v2_schema"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Impl_Server do
   end
 end
 
-defmodule Impl_ServerStore do
+defmodule V2_SchemaStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule Impl_ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule Impl_ServerEventBus do
+defmodule V2_SchemaEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
