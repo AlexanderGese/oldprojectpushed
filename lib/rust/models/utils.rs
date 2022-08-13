0b2e5caf95e0b9e5b8387146f1e2be9db086db57
Jordan Smith <jordan.smith@example.com>
2022-08-13 14:27:16 +0200

    feat: add batch processing to utils

--- lib/rust/models/utils.rs
+++ lib/rust/models/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.46.113";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "5.2.18";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
