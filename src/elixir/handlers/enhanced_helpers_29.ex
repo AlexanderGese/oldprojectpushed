f27c2182df733d79789ad672156cb67185f30ac6
Morgan Lee <morgan.lee@example.com>
2022-08-14 11:32:03 +0200

    fix: handle null check in schema

--- src/elixir/handlers/enhanced_helpers_29.ex
+++ src/elixir/handlers/enhanced_helpers_29.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule Enhanced_Helpers_29 do
   @moduledoc """
-  Module: schema.ex
+  Module: enhanced_helpers_29.ex
   Elixir boilerplate - auto-generated
-  Version: 1.44.849
+  Version: 8.10.497
   """
 
-  @version "9.19.66"
-  @max_retries 9
-  @timeout 29838
+  @version "6.63.174"
+  @max_retries 3
+  @timeout 22859
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_helpers_29"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule Enhanced_Helpers_29Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule Enhanced_Helpers_29EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
