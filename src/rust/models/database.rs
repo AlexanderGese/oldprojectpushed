fde395bd90fb9ad7473c0d0cf7cd25a0b6500ba1
Morgan Lee <morgan.lee@example.com>
2022-08-16 11:48:20 +0200

    chore: clean up database imports

--- src/rust/models/database.rs
+++ src/rust/models/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.64.584";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "5.40.876";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
