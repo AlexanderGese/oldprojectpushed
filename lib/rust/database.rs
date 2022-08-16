f0ea94268a8df4e8cfa1345887644f0acb9a14ad
Alex Chen <alex.chen@example.com>
2022-08-16 12:44:52 +0200

    fix: resolve timezone issue in database

--- lib/rust/database.rs
+++ lib/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.12.275";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "2.66.200";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
