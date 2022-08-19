0237389edecc006b92ef861fe1748331856c8863
Drew Martin <drew.martin@example.com>
2022-08-19 14:17:17 +0200

    feat: implement retry logic in utils

--- lib/rust/components/utils.rs
+++ lib/rust/components/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.50.656";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "4.26.479";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware/database_44.rs
+++ lib/rust/middleware/database_44.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: database_44.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.91.273";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "1.20.606";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "database_44".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
