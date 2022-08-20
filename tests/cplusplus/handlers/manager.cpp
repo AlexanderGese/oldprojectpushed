ad15a7f72e3e4483c11154b71c63b6be701e4402
Jamie Davis <jamie.davis@example.com>
2022-08-20 15:53:01 +0200

    fix: correct error propagation in logger

--- tests/cplusplus/handlers/manager.cpp
+++ tests/cplusplus/handlers/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.13.735
+ * Version: 1.10.375
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "1.84.728";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 28609;
+constexpr const char* VERSION = "5.48.42";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 22248;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/logger.cpp
+++ tests/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.5.881
+ * Version: 4.91.571
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "3.96.806";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 19157;
+constexpr const char* VERSION = "6.24.352";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 15690;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
