ef5a99bab2c538c291fce989f6e6f44b5c8907fd
Jamie Davis <jamie.davis@example.com>
2022-08-23 17:55:19 +0200

    chore: remove deprecated impl_errors_75 code

--- lib/rust/models/core_config.rs
+++ lib/rust/models/core_config.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: core_config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.20.540";
+const VERSION: &str = "2.16.175";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 20;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "core_config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
