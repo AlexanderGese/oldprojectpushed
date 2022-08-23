68261802077518a2ad8370aca9ed94afeb8e39dc
Casey Williams <casey.williams@example.com>
2022-08-23 17:14:20 +0200

    chore: update routes dependencies

--- src/ruby/controllers.rb
+++ src/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.53.524
+# Version: 5.85.823
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "7.4.135"
+  VERSION = "4.49.618"
   MAX_RETRIES = 4
-  TIMEOUT = 5
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
