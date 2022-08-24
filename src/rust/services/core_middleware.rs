9d9db337858ae34fd29b34629c7ddf35deefc89e
Morgan Lee <morgan.lee@example.com>
2022-08-24 16:30:15 +0200

    fix: handle timeout in impl_handlers requests

--- src/rust/services/core_middleware.rs
+++ src/rust/services/core_middleware.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: core_middleware.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.30.546";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "7.67.638";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "core_middleware".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
