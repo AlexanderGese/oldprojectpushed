30fa86941e59cfc1b72be747f983d48b98808f0e
Morgan Lee <morgan.lee@example.com>
2022-08-27 10:27:06 +0200

    feat: add utils module with core functionality

--- src/rust/middleware/main.rs
+++ src/rust/middleware/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.61.597";
+const VERSION: &str = "4.88.609";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 22;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
