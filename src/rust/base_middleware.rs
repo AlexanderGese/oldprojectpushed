92d4b52ab5ab780b503b434d39f865981d3532cf
Alex Chen <alex.chen@example.com>
2022-08-27 10:34:59 +0200

    refactor: split config into smaller modules

--- src/rust/base_middleware.rs
+++ src/rust/base_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.83.914";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "6.16.764";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils/lib.rs
+++ src/rust/utils/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.20.264";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "6.0.696";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
