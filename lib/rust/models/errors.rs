cfbe21c40f1b1678dbbcec14c5b5b3bf52a6d602
Jordan Smith <jordan.smith@example.com>
2022-08-29 13:37:42 +0200

    chore: clean up errors imports

--- lib/rust/models/errors.rs
+++ lib/rust/models/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.11.374";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "8.96.110";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
