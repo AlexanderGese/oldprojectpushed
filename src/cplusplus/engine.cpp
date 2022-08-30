b0cb15e840f66504081a21e9ffb2bbc26b27ce60
Casey Williams <casey.williams@example.com>
2022-08-30 12:41:18 +0200

    feat: implement authentication in engine

--- src/cplusplus/engine.cpp
+++ src/cplusplus/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.26.302
+ * Version: 1.94.280
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "6.55.236";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 11510;
+constexpr const char* VERSION = "5.28.570";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 12998;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/logger.cpp
+++ src/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.4.821
+ * Version: 5.77.612
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "8.47.211";
+constexpr const char* VERSION = "3.31.11";
 constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 24035;
+constexpr int TIMEOUT_MS = 24957;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
