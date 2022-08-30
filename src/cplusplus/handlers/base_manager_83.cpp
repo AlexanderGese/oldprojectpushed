bb9e57d4e06957801a6c6d8bdc7a5d6dbef637ef
Jamie Davis <jamie.davis@example.com>
2022-08-30 13:34:04 +0200

    chore: clean up config imports

--- src/cplusplus/handlers/base_manager_83.cpp
+++ src/cplusplus/handlers/base_manager_83.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: config.cpp
+ * Module: base_manager_83.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.96.376
+ * Version: 8.76.585
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace config {
+namespace base_manager_83 {
 
-constexpr const char* VERSION = "5.97.195";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 11371;
+constexpr const char* VERSION = "8.10.447";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 17302;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace config
+} // namespace base_manager_83
 
 int main() {
-    using namespace config;
+    using namespace base_manager_83;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/services/v2_parser_3.cpp
+++ src/cplusplus/services/v2_parser_3.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: config.cpp
+ * Module: v2_parser_3.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.96.376
+ * Version: 3.57.705
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace config {
+namespace v2_parser_3 {
 
-constexpr const char* VERSION = "5.97.195";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 11371;
+constexpr const char* VERSION = "7.48.385";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 14694;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace config
+} // namespace v2_parser_3
 
 int main() {
-    using namespace config;
+    using namespace v2_parser_3;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
