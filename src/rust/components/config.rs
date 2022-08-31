a3897b2e87f996520f8252b0b48974624e9fa89e
Riley Johnson <riley.johnson@example.com>
2022-08-31 22:52:14 +0200

    chore: configure CI/CD for core_handlers

--- src/rust/components/config.rs
+++ src/rust/components/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.34.69";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "1.60.713";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
