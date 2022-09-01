d95c15c99d7637436b1e000cff02eb732792b34e
Jordan Smith <jordan.smith@example.com>
2022-09-01 20:55:56 +0200

    fix: resolve race condition in v2_main_86

--- tests/cplusplus/controllers/v2_main_86.cpp
+++ tests/cplusplus/controllers/v2_main_86.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: new_parser.cpp
+ * Module: v2_main_86.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.10.545
+ * Version: 3.89.375
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace new_parser {
+namespace v2_main_86 {
 
-constexpr const char* VERSION = "3.75.43";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 21030;
+constexpr const char* VERSION = "4.18.291";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 12058;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace new_parser
+} // namespace v2_main_86
 
 int main() {
-    using namespace new_parser;
+    using namespace v2_main_86;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/utils/manager_66.cpp
+++ tests/cplusplus/utils/manager_66.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: new_parser.cpp
+ * Module: manager_66.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.10.545
+ * Version: 4.14.476
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace new_parser {
+namespace manager_66 {
 
-constexpr const char* VERSION = "3.75.43";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 21030;
+constexpr const char* VERSION = "7.6.626";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 28341;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace new_parser
+} // namespace manager_66
 
 int main() {
-    using namespace new_parser;
+    using namespace manager_66;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
