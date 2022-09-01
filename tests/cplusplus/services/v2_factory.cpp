7882a701b7d1f9ad22f999b4ba78081bd5ecc5fc
Sam Taylor <sam.taylor@example.com>
2022-09-01 14:20:37 +0200

    fix: handle large payloads in main

--- tests/cplusplus/services/v2_factory.cpp
+++ tests/cplusplus/services/v2_factory.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: main.cpp
+ * Module: v2_factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.69.299
+ * Version: 4.11.268
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace main {
+namespace v2_factory {
 
-constexpr const char* VERSION = "6.60.541";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 16358;
+constexpr const char* VERSION = "3.45.209";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 9057;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace main
+} // namespace v2_factory
 
 int main() {
-    using namespace main;
+    using namespace v2_factory;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- tests/cplusplus/services/v2_handler.cpp
+++ tests/cplusplus/services/v2_handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: v2_handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.24.676
+ * Version: 7.85.227
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace v2_handler {
 
-constexpr const char* VERSION = "6.77.582";
+constexpr const char* VERSION = "1.19.329";
 constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 11250;
+constexpr int TIMEOUT_MS = 5685;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
