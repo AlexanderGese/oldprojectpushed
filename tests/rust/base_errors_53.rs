dff115553bfebf4673bbb1daae62775b97a92ff8
Riley Johnson <riley.johnson@example.com>
2022-09-03 09:54:50 +0200

    refactor: improve base_middleware error messages

--- tests/rust/base_errors_53.rs
+++ tests/rust/base_errors_53.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_errors_53.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.22.503";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "7.20.878";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_errors_53".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/base_middleware.rs
+++ tests/rust/base_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.98.653";
+const VERSION: &str = "9.89.329";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 38;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
