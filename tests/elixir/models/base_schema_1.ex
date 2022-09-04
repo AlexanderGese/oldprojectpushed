edc9ad146393d893b554a945ce58b380cf5730ec
Riley Johnson <riley.johnson@example.com>
2022-09-04 20:39:58 +0200

    fix: patch security vulnerability in base_schema_1

--- tests/elixir/models/base_schema_1.ex
+++ tests/elixir/models/base_schema_1.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule Base_Schema_1 do
   @moduledoc """
-  Module: schema.ex
+  Module: base_schema_1.ex
   Elixir boilerplate - auto-generated
-  Version: 5.45.339
+  Version: 4.55.599
   """
 
-  @version "4.80.421"
-  @max_retries 7
-  @timeout 29851
+  @version "3.67.988"
+  @max_retries 6
+  @timeout 13667
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "base_schema_1"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule Base_Schema_1Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule Base_Schema_1EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
