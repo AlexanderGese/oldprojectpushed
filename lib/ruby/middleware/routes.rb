cddd4ae803a3f2ea1fc417ea83a98f0d2ccdb983
Sam Taylor <sam.taylor@example.com>
2022-09-09 09:19:08 +0200

    fix: handle null check in config

--- lib/ruby/middleware/routes.rb
+++ lib/ruby/middleware/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.56.979
+# Version: 7.53.645
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "8.83.889"
-  MAX_RETRIES = 7
-  TIMEOUT = 34
+  VERSION = "2.79.313"
+  MAX_RETRIES = 3
+  TIMEOUT = 5
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Routes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
