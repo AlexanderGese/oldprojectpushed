9d61d1821db9061c35b5446debdbbffec81d7190
Riley Johnson <riley.johnson@example.com>
2022-09-11 19:15:39 +0200

    fix: resolve encoding issue in logger

--- tests/cplusplus/config.cpp
+++ tests/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.51.4
+ * Version: 1.28.13
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "8.28.643";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 13701;
+constexpr const char* VERSION = "3.97.146";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 11657;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/logger.cpp
+++ tests/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.83.4
+ * Version: 3.37.940
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "6.76.768";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 6043;
+constexpr const char* VERSION = "8.74.255";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 22765;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
