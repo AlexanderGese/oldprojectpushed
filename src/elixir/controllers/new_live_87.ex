1ea54fd7b59541439fb625ca3ff8efb8eb2e48c3
Riley Johnson <riley.johnson@example.com>
2022-09-11 21:17:31 +0200

    refactor: improve new_live_87 test coverage

--- src/elixir/controllers/new_live_87.ex
+++ src/elixir/controllers/new_live_87.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule New_Live_87 do
   @moduledoc """
-  Module: server.ex
+  Module: new_live_87.ex
   Elixir boilerplate - auto-generated
-  Version: 6.84.964
+  Version: 7.19.803
   """
 
-  @version "5.28.274"
-  @max_retries 7
-  @timeout 21524
+  @version "4.71.569"
+  @max_retries 6
+  @timeout 29739
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "new_live_87"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule New_Live_87Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule New_Live_87EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
