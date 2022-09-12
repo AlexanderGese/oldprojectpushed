1a37d0ac4a3c34d5f697b9b9185b37f5461dcb04
Sam Taylor <sam.taylor@example.com>
2022-09-12 11:25:53 +0200

    feat: add pagination to impl_middleware_78

--- tests/rust/components/lib.rs
+++ tests/rust/components/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.60.315";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "4.11.923";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
