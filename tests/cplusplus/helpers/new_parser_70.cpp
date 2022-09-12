915c3c516520f53f44bba465571f5998bd86def4
Drew Martin <drew.martin@example.com>
2022-09-12 15:09:54 +0200

    feat: add caching layer to base_engine

--- tests/cplusplus/helpers/new_parser_70.cpp
+++ tests/cplusplus/helpers/new_parser_70.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: base_engine.cpp
+ * Module: new_parser_70.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.86.441
+ * Version: 1.53.809
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace base_engine {
+namespace new_parser_70 {
 
-constexpr const char* VERSION = "1.92.416";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 12342;
+constexpr const char* VERSION = "7.94.480";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 10388;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace base_engine
+} // namespace new_parser_70
 
 int main() {
-    using namespace base_engine;
+    using namespace new_parser_70;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
