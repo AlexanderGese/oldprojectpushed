4e450b162135022f104fd8a186b009d814669aa4
Sam Taylor <sam.taylor@example.com>
2022-09-13 20:07:19 +0200

    refactor: migrate new_controllers to async/await

--- lib/ruby/new_controllers.rb
+++ lib/ruby/new_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: new_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.61.422
+# Version: 6.5.21
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewControllers
-  VERSION = "7.72.227"
-  MAX_RETRIES = 5
-  TIMEOUT = 29
+  VERSION = "1.48.747"
+  MAX_RETRIES = 7
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewControllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
