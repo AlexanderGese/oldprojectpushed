ddc889f992d194cf1808a5be3b60f50cd91ded78
Jordan Smith <jordan.smith@example.com>
2022-09-16 22:46:41 +0200

    docs: add documentation for handler

--- tests/cplusplus/new_utils.cpp
+++ tests/cplusplus/new_utils.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: new_utils.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.32.765
+ * Version: 9.69.790
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace new_utils {
 
-constexpr const char* VERSION = "2.82.186";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 5022;
+constexpr const char* VERSION = "4.45.565";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 25293;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/parser.cpp
+++ tests/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.72.858
+ * Version: 9.59.7
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "1.55.450";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 5201;
+constexpr const char* VERSION = "1.74.171";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 17507;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
