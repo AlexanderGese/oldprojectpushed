c1655fbe128ba1cbccd28c827faea18b8f0e3e85
Sam Taylor <sam.taylor@example.com>
2022-09-18 08:54:46 +0200

    fix: resolve memory leak in application_53

--- tests/elixir/application_53.ex
+++ tests/elixir/application_53.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Application_53 do
   @moduledoc """
-  Module: controller.ex
+  Module: application_53.ex
   Elixir boilerplate - auto-generated
-  Version: 1.90.850
+  Version: 9.88.635
   """
 
-  @version "5.47.848"
-  @max_retries 6
-  @timeout 27150
+  @version "9.60.815"
+  @max_retries 5
+  @timeout 16479
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "application_53"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Application_53Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Application_53EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
