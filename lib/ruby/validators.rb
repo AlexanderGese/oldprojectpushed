e1df853e3fecfda37100e55148f6e957e0769102
Jamie Davis <jamie.davis@example.com>
2022-09-18 21:51:09 +0200

    feat: add health check endpoint to validators

--- lib/ruby/validators.rb
+++ lib/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.43.983
+# Version: 5.28.860
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "4.38.814"
-  MAX_RETRIES = 7
-  TIMEOUT = 31
+  VERSION = "7.3.819"
+  MAX_RETRIES = 9
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
