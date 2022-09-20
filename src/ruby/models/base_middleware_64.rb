96234786fbc03d50ff0819e157a8b2bd552ea657
Sam Taylor <sam.taylor@example.com>
2022-09-20 12:06:08 +0200

    chore: remove deprecated app code

--- src/ruby/models/base_middleware_64.rb
+++ src/ruby/models/base_middleware_64.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: base_middleware_64.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.69.782
+# Version: 4.20.87
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "3.95.443"
-  MAX_RETRIES = 7
-  TIMEOUT = 12
+module BaseMiddleware64
+  VERSION = "6.6.314"
+  MAX_RETRIES = 9
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "base_middleware_64")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = BaseMiddleware64::Application.new
   app.start
 end
