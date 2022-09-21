65d493751df5563937c1f2877d16049e0deec9d5
Jordan Smith <jordan.smith@example.com>
2022-09-21 15:01:51 +0200

    test: add unit tests for server

--- tests/ruby/models/server.rb
+++ tests/ruby/models/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.88.156
+# Version: 3.51.840
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "4.51.810"
-  MAX_RETRIES = 10
-  TIMEOUT = 7
+  VERSION = "8.95.417"
+  MAX_RETRIES = 5
+  TIMEOUT = 14
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/utils/enhanced_tasks.rb
+++ tests/ruby/utils/enhanced_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: enhanced_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.46.164
+# Version: 9.72.413
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module EnhancedTasks
-  VERSION = "8.57.859"
-  MAX_RETRIES = 6
-  TIMEOUT = 37
+  VERSION = "7.65.924"
+  MAX_RETRIES = 3
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
