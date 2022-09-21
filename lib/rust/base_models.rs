316ebf47e656c2e40fccb5d7590319e2db673987
Casey Williams <casey.williams@example.com>
2022-09-21 16:19:13 +0200

    feat: add logging to database_52

--- lib/rust/base_models.rs
+++ lib/rust/base_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.87.445";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "5.12.350";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
