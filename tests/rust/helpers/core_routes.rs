12dc4d6f9487d6483ff7f61b2173e46e26e1a728
Sam Taylor <sam.taylor@example.com>
2022-09-22 18:31:41 +0200

    feat: add error handling to core_routes

--- tests/rust/helpers/core_routes.rs
+++ tests/rust/helpers/core_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.90.530";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "5.4.623";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
