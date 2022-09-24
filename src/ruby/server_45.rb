edfd3da6dda8e2e208943c8b9632acf18dcf8112
Riley Johnson <riley.johnson@example.com>
2022-09-24 23:08:32 +0200

    feat: add middleware support to middleware

--- src/ruby/server_45.rb
+++ src/ruby/server_45.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: server_45.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.49.747
+# Version: 3.58.422
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "7.7.969"
-  MAX_RETRIES = 10
-  TIMEOUT = 57
+module Server45
+  VERSION = "9.32.656"
+  MAX_RETRIES = 5
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "server_45")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = Server45::Application.new
   app.start
 end
