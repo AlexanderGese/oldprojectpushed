59dd322c9743e327349dfe77da37e71a2e3582a8
Casey Williams <casey.williams@example.com>
2022-09-24 09:44:44 +0200

    fix: handle null check in middleware

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.96.137";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "3.16.864";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
