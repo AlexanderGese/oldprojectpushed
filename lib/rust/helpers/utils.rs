c14bc4731bcfb3b041be3a919a3bf7242e7672d5
Morgan Lee <morgan.lee@example.com>
2022-09-29 19:06:06 +0200

    feat: implement retry logic in utils

--- lib/rust/helpers/utils.rs
+++ lib/rust/helpers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.62.946";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "6.89.479";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
