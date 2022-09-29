944021673312b07ca1c18010a6aa14391da65850
Jordan Smith <jordan.smith@example.com>
2022-09-29 12:19:08 +0200

    fix: resolve timezone issue in impl_lib

--- src/rust/handlers/models.rs
+++ src/rust/handlers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.46.52";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "7.68.519";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/impl_lib.rs
+++ src/rust/impl_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.45.999";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "1.49.816";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
