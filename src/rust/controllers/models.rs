df7ac173e0c85bb9347bce969801c2ab186f0bd8
Riley Johnson <riley.johnson@example.com>
2022-09-30 23:38:45 +0200

    refactor: decouple models dependencies

--- src/rust/controllers/models.rs
+++ src/rust/controllers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.59.48";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "1.79.646";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
