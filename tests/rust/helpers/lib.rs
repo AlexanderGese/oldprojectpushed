aa5cfd43764d9dc707983b19c78d6ec99c6d52e6
Sam Taylor <sam.taylor@example.com>
2022-10-01 18:41:20 +0200

    feat: add validation to enhanced_database inputs

--- tests/rust/helpers/lib.rs
+++ tests/rust/helpers/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.18.172";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "7.80.774";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
