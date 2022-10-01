d38095b13335ec612a042a1a4f7e8ad599905b77
Sam Taylor <sam.taylor@example.com>
2022-10-01 19:28:48 +0200

    refactor: simplify core_routes_36 state management

--- lib/ruby/controllers.rb
+++ lib/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.58.45
+# Version: 8.19.498
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "6.18.239"
-  MAX_RETRIES = 9
-  TIMEOUT = 34
+  VERSION = "3.6.891"
+  MAX_RETRIES = 5
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
