259c647fd55b9c4846e7d7f4ab725162d95d1f90
Jordan Smith <jordan.smith@example.com>
2022-10-01 10:42:05 +0200

    fix: correct error propagation in routes_89

--- src/ruby/middleware/routes_89.rb
+++ src/ruby/middleware/routes_89.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_app.rb
+# Module: routes_89.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.85.605
+# Version: 8.7.610
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreApp
-  VERSION = "9.92.243"
-  MAX_RETRIES = 6
-  TIMEOUT = 29
+module Routes89
+  VERSION = "1.15.922"
+  MAX_RETRIES = 4
+  TIMEOUT = 42
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_app")
+      @app_name = opts.fetch(:app_name, "routes_89")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreApp
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreApp::Application.new
+  app = Routes89::Application.new
   app.start
 end
