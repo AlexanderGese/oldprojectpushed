d2f83b7625c6b41a7879b599591de35b8c474379
Morgan Lee <morgan.lee@example.com>
2022-10-11 15:49:27 +0200

    refactor: improve routes error messages

--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.63.770";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "9.0.573";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
