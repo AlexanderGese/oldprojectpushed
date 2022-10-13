1a037f7c290f9971ad47defff178fe0cbc821c0f
Jamie Davis <jamie.davis@example.com>
2022-10-13 08:22:23 +0200

    feat: add logging to errors

--- src/rust/helpers/core_models.rs
+++ src/rust/helpers/core_models.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: core_models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.41.732";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "1.76.610";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "core_models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
