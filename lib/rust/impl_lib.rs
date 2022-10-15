2908f7636b3a3c9458ed4d6829df33a59df25c0e
Casey Williams <casey.williams@example.com>
2022-10-15 12:53:03 +0200

    refactor: simplify v2_lib_82 state management

--- lib/rust/impl_lib.rs
+++ lib/rust/impl_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.38.783";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "1.12.568";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 28;
 
 #[derive(Debug, Clone)]
 pub struct Config {
