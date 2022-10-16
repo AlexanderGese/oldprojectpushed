4a5f253edd4576fade6561d0337c59a1f75e3867
Drew Martin <drew.martin@example.com>
2022-10-16 22:05:40 +0200

    refactor: improve routes test coverage

--- tests/ruby/handlers/core_validators.rb
+++ tests/ruby/handlers/core_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: core_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.45.721
+# Version: 8.46.493
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreValidators
-  VERSION = "6.74.261"
-  MAX_RETRIES = 5
-  TIMEOUT = 17
+  VERSION = "1.99.768"
+  MAX_RETRIES = 10
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreValidators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
--- tests/ruby/tasks.rb
+++ tests/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.13.21
+# Version: 5.22.925
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "7.85.218"
-  MAX_RETRIES = 7
-  TIMEOUT = 33
+  VERSION = "6.99.15"
+  MAX_RETRIES = 6
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
