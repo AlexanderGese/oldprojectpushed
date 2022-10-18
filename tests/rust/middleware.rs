34a2db7e594a5d3725cd23e79c37e32fff1523cf
Jordan Smith <jordan.smith@example.com>
2022-10-18 09:49:51 +0200

    feat: add export functionality to v2_routes_59

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.52.661";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "5.55.582";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
