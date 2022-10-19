abc3b51f6cc5c92490ceb1be96e83f69a92a1725
Casey Williams <casey.williams@example.com>
2022-10-19 17:31:56 +0200

    fix: resolve edge case in lib handler

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.6.766";
+const VERSION: &str = "7.5.113";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 32;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
