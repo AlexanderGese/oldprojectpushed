074e99803bc5801ba92ac8901e15105cb32fcb31
Jamie Davis <jamie.davis@example.com>
2022-10-27 11:28:10 +0200

    fix: patch security vulnerability in app

--- tests/ruby/app.rb
+++ tests/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.23.99
+# Version: 6.90.985
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "7.27.81"
-  MAX_RETRIES = 8
-  TIMEOUT = 5
+  VERSION = "5.60.296"
+  MAX_RETRIES = 5
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
