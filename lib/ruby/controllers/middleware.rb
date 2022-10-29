8c8348b7ea2b04ab67446155b33e19e57e6e2b40
Casey Williams <casey.williams@example.com>
2022-10-29 13:42:51 +0200

    feat: add pagination to middleware

--- lib/ruby/controllers/middleware.rb
+++ lib/ruby/controllers/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.39.424
+# Version: 4.35.842
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "7.38.566"
+  VERSION = "4.90.854"
   MAX_RETRIES = 4
-  TIMEOUT = 53
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
