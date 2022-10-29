47f8fb41a61813bc496774c7a92595b5b147de19
Sam Taylor <sam.taylor@example.com>
2022-10-29 23:04:04 +0200

    feat: add export functionality to handlers

--- lib/rust/controllers/handlers.rs
+++ lib/rust/controllers/handlers.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.29.41";
-const MAX_RETRIES: u32 = 3;
+const VERSION: &str = "7.84.516";
+const MAX_RETRIES: u32 = 10;
 const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
--- lib/rust/models/config.rs
+++ lib/rust/models/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.89.735";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "8.10.620";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
