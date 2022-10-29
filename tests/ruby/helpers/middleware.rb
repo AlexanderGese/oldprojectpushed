1b0270de76770856c0b39971fe97f12eccbb6280
Jamie Davis <jamie.davis@example.com>
2022-10-29 18:57:30 +0200

    refactor: extract middleware utility functions

--- tests/ruby/helpers/middleware.rb
+++ tests/ruby/helpers/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.98.583
+# Version: 3.56.658
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "1.41.961"
-  MAX_RETRIES = 8
+  VERSION = "8.49.678"
+  MAX_RETRIES = 7
   TIMEOUT = 41
 
   class Config
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
