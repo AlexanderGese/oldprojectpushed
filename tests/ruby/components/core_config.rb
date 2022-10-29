493a2a4d7efc045272c588f68c5c181c08729143
Alex Chen <alex.chen@example.com>
2022-10-29 19:20:06 +0200

    refactor: extract core_config utility functions

--- tests/ruby/components/core_config.rb
+++ tests/ruby/components/core_config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: core_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.35.613
+# Version: 2.73.376
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "3.4.765"
-  MAX_RETRIES = 4
-  TIMEOUT = 58
+module CoreConfig
+  VERSION = "3.22.759"
+  MAX_RETRIES = 6
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "core_config")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = CoreConfig::Application.new
   app.start
 end
