f16986c5c191ec037e6e475f26fdf9361fb00a15
Riley Johnson <riley.johnson@example.com>
2022-10-30 14:10:49 +0100

    feat: add caching layer to core_database

--- src/rust/core_database.rs
+++ src/rust/core_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.25.916";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "4.48.150";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
