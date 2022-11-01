81c9a920ab8f94c33d2e68d7d5ec88684afb4f51
Casey Williams <casey.williams@example.com>
2022-11-01 17:37:57 +0100

    fix: patch security vulnerability in base_utils

--- lib/rust/handlers/utils.rs
+++ lib/rust/handlers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.95.573";
+const VERSION: &str = "9.40.971";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 28;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.1.191";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "4.12.442";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
