720ab53b102e943550fae7c6a7cf98654a64ab39
Morgan Lee <morgan.lee@example.com>
2022-11-01 19:49:27 +0100

    feat: add middleware support to v2_middleware

--- src/rust/handlers/v2_middleware.rs
+++ src/rust/handlers/v2_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.81.379";
+const VERSION: &str = "2.81.737";
 const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 34;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
