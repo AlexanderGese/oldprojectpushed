4fb9bf76df75e4cd5d857c19b8648dddfdd9326c
Sam Taylor <sam.taylor@example.com>
2022-11-04 11:24:45 +0100

    fix: resolve encoding issue in base_tasks

--- tests/ruby/helpers/base_tasks.rb
+++ tests/ruby/helpers/base_tasks.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: base_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.65.531
+# Version: 1.0.754
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "3.41.163"
-  MAX_RETRIES = 5
-  TIMEOUT = 29
+module BaseTasks
+  VERSION = "4.73.516"
+  MAX_RETRIES = 3
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "base_tasks")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = BaseTasks::Application.new
   app.start
 end
--- tests/ruby/v2_middleware_61.rb
+++ tests/ruby/v2_middleware_61.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: v2_middleware_61.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.65.531
+# Version: 4.65.829
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "3.41.163"
-  MAX_RETRIES = 5
-  TIMEOUT = 29
+module V2Middleware61
+  VERSION = "7.40.924"
+  MAX_RETRIES = 8
+  TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "v2_middleware_61")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = V2Middleware61::Application.new
   app.start
 end
