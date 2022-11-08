a88fa67992fbeef4a5c32970255fd4cb42dd11a4
Riley Johnson <riley.johnson@example.com>
2022-11-08 14:26:43 +0100

    fix: patch security vulnerability in middleware

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.35.458";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "8.34.878";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
