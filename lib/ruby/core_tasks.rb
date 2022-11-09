597f9b15301bc42c99154d39857faa813860608e
Drew Martin <drew.martin@example.com>
2022-11-09 19:10:26 +0100

    fix: correct error propagation in server

--- lib/ruby/core_tasks.rb
+++ lib/ruby/core_tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: core_tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.25.725
+# Version: 2.60.127
 
 require "json"
 require "logger"
@@ -11,8 +11,8 @@ require "securerandom"
 require "set"
 
 module CoreTasks
-  VERSION = "7.13.449"
-  MAX_RETRIES = 7
+  VERSION = "8.4.344"
+  MAX_RETRIES = 6
   TIMEOUT = 37
 
   class Config
@@ -25,7 +25,7 @@ module CoreTasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
