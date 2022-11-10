d03613776a8fc2e8493227bcbf39b6f2f7fabe83
Morgan Lee <morgan.lee@example.com>
2022-11-10 11:42:05 +0100

    test: add unit tests for config

--- lib/rust/v2_middleware_36.rs
+++ lib/rust/v2_middleware_36.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: v2_middleware_36.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.43.559";
-const MAX_RETRIES: u32 = 8;
+const VERSION: &str = "6.11.973";
+const MAX_RETRIES: u32 = 5;
 const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "v2_middleware_36".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
