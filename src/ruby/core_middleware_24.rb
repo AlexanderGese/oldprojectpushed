e911ffc6ad05cc78d9886457b9f6551fb846a477
Riley Johnson <riley.johnson@example.com>
2022-11-12 16:17:11 +0100

    feat: add error handling to core_middleware_24

--- src/ruby/core_middleware_24.rb
+++ src/ruby/core_middleware_24.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: core_middleware_24.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.72.178
+# Version: 7.61.720
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "6.46.547"
-  MAX_RETRIES = 6
-  TIMEOUT = 22
+module CoreMiddleware24
+  VERSION = "3.34.955"
+  MAX_RETRIES = 9
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "core_middleware_24")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = CoreMiddleware24::Application.new
   app.start
 end
--- src/ruby/server.rb
+++ src/ruby/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.62.357
+# Version: 3.39.129
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "1.52.751"
+  VERSION = "5.82.319"
   MAX_RETRIES = 8
-  TIMEOUT = 40
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
