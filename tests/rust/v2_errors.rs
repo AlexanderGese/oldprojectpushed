9cbdf6fd6ec41472223f3e0d315c1e2b400e66b2
Sam Taylor <sam.taylor@example.com>
2022-11-15 08:24:23 +0100

    feat: add health check endpoint to v2_errors

--- tests/rust/v2_errors.rs
+++ tests/rust/v2_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.53.67";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "3.98.905";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
