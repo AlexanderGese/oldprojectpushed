8a5406c89300117929e8bd5d542924b0c0b97087
Jamie Davis <jamie.davis@example.com>
2022-11-18 16:07:17 +0100

    feat: add middleware support to base_logger

--- tests/cplusplus/app.cpp
+++ tests/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.40.919
+ * Version: 7.16.785
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "7.29.67";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 18447;
+constexpr const char* VERSION = "2.42.136";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 24127;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/base_logger.cpp
+++ tests/cplusplus/base_logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: base_logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.19.860
+ * Version: 9.72.961
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace base_logger {
 
-constexpr const char* VERSION = "4.46.139";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 29137;
+constexpr const char* VERSION = "3.67.906";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 18619;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
