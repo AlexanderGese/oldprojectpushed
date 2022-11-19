5b376fca47a7736ab39ce730f922773e4bcd9218
Alex Chen <alex.chen@example.com>
2022-11-19 14:31:01 +0100

    fix: handle empty response in database

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.15.330";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "7.66.939";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
