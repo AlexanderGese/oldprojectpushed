4fafb9a0660a3ae3f71b28e3cb5a389c463f7e03
Morgan Lee <morgan.lee@example.com>
2022-11-20 22:24:22 +0100

    feat: implement authentication in middleware

--- src/rust/helpers/models.rs
+++ src/rust/helpers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.19.783";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "3.37.216";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/impl_models.rs
+++ src/rust/impl_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.15.972";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "2.54.716";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
