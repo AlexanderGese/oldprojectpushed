beea1312be150603cf09a21802e3c8c29eb985bd
Casey Williams <casey.williams@example.com>
2022-11-21 08:02:55 +0100

    chore: remove deprecated core_handlers code

--- tests/rust/core_handlers.rs
+++ tests/rust/core_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.41.325";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "5.90.122";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
