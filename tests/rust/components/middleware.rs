80838275d45a3d745273535d777329310e53a897
Morgan Lee <morgan.lee@example.com>
2022-11-22 14:57:15 +0100

    chore: clean up enhanced_utils imports

--- tests/rust/components/middleware.rs
+++ tests/rust/components/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.99.42";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "2.38.606";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
