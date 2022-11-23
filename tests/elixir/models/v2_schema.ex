3f05bd1cce2aeec465d5a0889db13faff886bb1d
Sam Taylor <sam.taylor@example.com>
2022-11-23 17:00:03 +0100

    chore: update v2_live_98 dependencies

--- tests/elixir/models/v2_schema.ex
+++ tests/elixir/models/v2_schema.ex
@@ -1,13 +1,13 @@
-defmodule Router do
+defmodule V2_Schema do
   @moduledoc """
-  Module: router.ex
+  Module: v2_schema.ex
   Elixir boilerplate - auto-generated
-  Version: 6.91.112
+  Version: 6.2.239
   """
 
-  @version "5.7.720"
-  @max_retries 9
-  @timeout 29985
+  @version "5.29.975"
+  @max_retries 7
+  @timeout 25575
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Router do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "router"),
+      app_name: Keyword.get(opts, :app_name, "v2_schema"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Router do
   end
 end
 
-defmodule RouterStore do
+defmodule V2_SchemaStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule RouterStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule RouterEventBus do
+defmodule V2_SchemaEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
