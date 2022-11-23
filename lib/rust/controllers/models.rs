a64d50afbccd8cb0e0b76dbc53658c5c3082b257
Drew Martin <drew.martin@example.com>
2022-11-23 14:54:35 +0100

    fix: correct type definitions in models

--- lib/rust/controllers/models.rs
+++ lib/rust/controllers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.57.274";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "6.89.545";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
