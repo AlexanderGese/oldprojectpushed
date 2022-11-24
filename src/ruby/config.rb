0f5686a9b9776f14e2540e462eaf045a6f721f08
Riley Johnson <riley.johnson@example.com>
2022-11-24 17:10:21 +0100

    chore: update base_config configuration

--- src/ruby/config.rb
+++ src/ruby/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.48.335
+# Version: 8.10.946
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "3.29.804"
-  MAX_RETRIES = 3
-  TIMEOUT = 17
+  VERSION = "3.94.728"
+  MAX_RETRIES = 10
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
