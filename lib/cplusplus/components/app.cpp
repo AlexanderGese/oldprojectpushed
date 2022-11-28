a85a1ee5a0385c612c7971b4dad76858f868b91b
Sam Taylor <sam.taylor@example.com>
2022-11-28 21:50:25 +0100

    fix: handle large payloads in enhanced_handler_15

--- lib/cplusplus/components/app.cpp
+++ lib/cplusplus/components/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.84.108
+ * Version: 7.44.799
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "5.89.393";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 8229;
+constexpr const char* VERSION = "5.69.430";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 7216;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/factory_17.cpp
+++ lib/cplusplus/factory_17.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: manager.cpp
+ * Module: factory_17.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.42.107
+ * Version: 9.76.833
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace manager {
+namespace factory_17 {
 
-constexpr const char* VERSION = "6.67.466";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 16016;
+constexpr const char* VERSION = "4.40.592";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 28926;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace manager
+} // namespace factory_17
 
 int main() {
-    using namespace manager;
+    using namespace factory_17;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
