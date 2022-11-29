3c34a0dac94c7112828e530a14ead9b5e1764686
Drew Martin <drew.martin@example.com>
2022-11-29 18:31:28 +0100

    refactor: migrate new_config_4 to async/await

--- lib/rust/helpers/new_config_4.rs
+++ lib/rust/helpers/new_config_4.rs
@@ -1,4 +1,4 @@
-//! Module: new_middleware.rs
+//! Module: new_config_4.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.75.868";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "3.5.309";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_middleware".to_string(),
+            app_name: "new_config_4".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/models/utils.rs
+++ lib/rust/models/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.2.18";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "7.88.62";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
