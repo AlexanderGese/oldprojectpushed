45333837b32d24ee1bb1c762f9f5e97d27d73bac
Sam Taylor <sam.taylor@example.com>
2022-11-29 16:19:56 +0100

    fix: resolve race condition in middleware

--- tests/rust/controllers/models_63.rs
+++ tests/rust/controllers/models_63.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: models_63.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.62.897";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "4.62.125";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "models_63".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
