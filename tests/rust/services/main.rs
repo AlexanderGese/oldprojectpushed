1769d614310ccb5d61a92231a0f4980eb64abde6
Jamie Davis <jamie.davis@example.com>
2022-11-30 11:17:43 +0100

    feat: add event system to main

--- tests/rust/services/main.rs
+++ tests/rust/services/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.12.224";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "6.75.245";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
