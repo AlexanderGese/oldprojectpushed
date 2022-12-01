126aac172f075a1bf0050a426180fdbbeb6d123f
Alex Chen <alex.chen@example.com>
2022-12-01 17:22:59 +0100

    chore: clean up new_handlers_13 imports

--- lib/rust/base_lib.rs
+++ lib/rust/base_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.35.328";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "7.56.144";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/new_handlers_13.rs
+++ lib/rust/new_handlers_13.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.73.710";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "2.22.774";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
