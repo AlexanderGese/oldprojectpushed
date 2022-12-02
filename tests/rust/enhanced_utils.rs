a064dae70f92338ff65b5663dd73bed99ef60948
Morgan Lee <morgan.lee@example.com>
2022-12-02 16:07:38 +0100

    fix: correct type definitions in utils

--- tests/rust/enhanced_utils.rs
+++ tests/rust/enhanced_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.15.974";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "2.33.401";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
