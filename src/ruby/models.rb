5fbc14db93ad95bd197f40e1115fe683ef0bc8b1
Morgan Lee <morgan.lee@example.com>
2022-12-04 20:12:40 +0100

    fix: resolve race condition in models

--- src/ruby/models.rb
+++ src/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.13.249
+# Version: 6.34.41
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "2.5.711"
-  MAX_RETRIES = 4
-  TIMEOUT = 35
+  VERSION = "6.55.931"
+  MAX_RETRIES = 7
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
