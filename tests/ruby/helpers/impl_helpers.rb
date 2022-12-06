44c03c513dc56c0f0ffaf2cffe2ee6af194cf8b7
Casey Williams <casey.williams@example.com>
2022-12-06 22:35:16 +0100

    feat: implement authentication in models

--- tests/ruby/helpers/impl_helpers.rb
+++ tests/ruby/helpers/impl_helpers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: impl_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.3.442
+# Version: 9.57.316
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "9.88.296"
+module ImplHelpers
+  VERSION = "3.2.58"
   MAX_RETRIES = 3
-  TIMEOUT = 49
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "impl_helpers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = ImplHelpers::Application.new
   app.start
 end
