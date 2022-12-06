eb16a3a76503cb1e7bc8ae96e7fddd93872e2990
Alex Chen <alex.chen@example.com>
2022-12-06 20:57:19 +0100

    fix: handle timeout in v2_tasks_58 requests

--- tests/ruby/middleware/app.rb
+++ tests/ruby/middleware/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.60.272
+# Version: 9.36.750
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "1.13.490"
-  MAX_RETRIES = 10
-  TIMEOUT = 45
+  VERSION = "4.31.781"
+  MAX_RETRIES = 8
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/v2_tasks_58.rb
+++ tests/ruby/v2_tasks_58.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: v2_tasks_58.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.4.983
+# Version: 6.71.438
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "5.8.860"
-  MAX_RETRIES = 4
-  TIMEOUT = 36
+module V2Tasks58
+  VERSION = "6.75.608"
+  MAX_RETRIES = 9
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "v2_tasks_58")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = V2Tasks58::Application.new
   app.start
 end
