fd56be9ac117ac7123caf5412c8c1783b9e4e390
Alex Chen <alex.chen@example.com>
2022-12-06 20:10:29 +0100

    refactor: migrate base_routes to async/await

--- lib/rust/base_routes.rs
+++ lib/rust/base_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.85.121";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "8.29.175";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/models/base_main_65.rs
+++ lib/rust/models/base_main_65.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: base_main_65.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.12.858";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "5.49.173";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "base_main_65".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
