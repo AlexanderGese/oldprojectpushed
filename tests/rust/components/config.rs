4132f0cb6aec84df9aeed9ed70a79e275c444be1
Alex Chen <alex.chen@example.com>
2022-12-07 11:25:59 +0100

    feat: implement retry logic in config

--- tests/rust/components/config.rs
+++ tests/rust/components/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.33.962";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "7.62.238";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
