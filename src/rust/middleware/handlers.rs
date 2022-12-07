799c24ada9fc5ba48b74bc2b9feeb57513301770
Riley Johnson <riley.johnson@example.com>
2022-12-07 16:05:43 +0100

    fix: patch security vulnerability in handlers

--- src/rust/middleware/handlers.rs
+++ src/rust/middleware/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.7.681";
+const VERSION: &str = "7.15.592";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 14;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
