2c6d1885e91262673379ff496356c8baaf336e85
Casey Williams <casey.williams@example.com>
2022-12-08 21:11:03 +0100

    fix: resolve encoding issue in new_config

--- tests/ruby/middleware/new_config.rb
+++ tests/ruby/middleware/new_config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: base_app.rb
+# Module: new_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.45.352
+# Version: 1.90.442
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module BaseApp
-  VERSION = "5.18.630"
-  MAX_RETRIES = 7
-  TIMEOUT = 50
+module NewConfig
+  VERSION = "9.4.452"
+  MAX_RETRIES = 5
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "base_app")
+      @app_name = opts.fetch(:app_name, "new_config")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module BaseApp
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = BaseApp::Application.new
+  app = NewConfig::Application.new
   app.start
 end
