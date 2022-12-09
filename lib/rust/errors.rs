60ac81bf0addb0af80719caa0d298526b55b33f1
Riley Johnson <riley.johnson@example.com>
2022-12-09 14:54:22 +0100

    feat: add export functionality to errors

--- lib/rust/errors.rs
+++ lib/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.54.991";
+const VERSION: &str = "4.38.329";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 45;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
