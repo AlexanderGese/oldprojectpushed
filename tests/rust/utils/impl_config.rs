204fb019557e0c3a92ed67ee79abe333745a7a36
Casey Williams <casey.williams@example.com>
2022-12-09 15:54:34 +0100

    chore: update database configuration

--- tests/rust/utils/impl_config.rs
+++ tests/rust/utils/impl_config.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: impl_config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.92.376";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "6.79.71";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "impl_config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
