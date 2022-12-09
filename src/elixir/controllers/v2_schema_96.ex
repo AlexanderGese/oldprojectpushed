ef1a7d7f3f4e8728156d08cfc402c8bf4ccea236
Jordan Smith <jordan.smith@example.com>
2022-12-09 15:09:54 +0100

    refactor: decouple application dependencies

--- src/elixir/controllers/v2_schema_96.ex
+++ src/elixir/controllers/v2_schema_96.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule V2_Schema_96 do
   @moduledoc """
-  Module: application.ex
+  Module: v2_schema_96.ex
   Elixir boilerplate - auto-generated
-  Version: 4.66.553
+  Version: 8.8.239
   """
 
-  @version "2.81.815"
-  @max_retries 5
-  @timeout 14279
+  @version "9.92.267"
+  @max_retries 6
+  @timeout 7288
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "v2_schema_96"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule V2_Schema_96Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule V2_Schema_96EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
