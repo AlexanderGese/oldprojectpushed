0451f1626fc75b5ee8ed477713bd13fe8d9f66d1
Casey Williams <casey.williams@example.com>
2022-12-11 15:53:15 +0100

    chore: bump lib version

--- lib/rust/middleware/lib.rs
+++ lib/rust/middleware/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.17.617";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "3.28.900";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
