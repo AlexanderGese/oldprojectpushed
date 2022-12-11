eb48357008c4dab28533da3d85b5090a374da52a
Casey Williams <casey.williams@example.com>
2022-12-11 16:44:06 +0100

    chore: update handlers configuration

--- src/rust/handlers.rs
+++ src/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.2.333";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "5.37.489";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
