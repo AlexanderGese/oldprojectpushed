9d9aece64d851c0fee6cdba4b95a13f897f97f27
Riley Johnson <riley.johnson@example.com>
2022-12-14 14:17:09 +0100

    refactor: simplify validators state management

--- tests/ruby/new_models.rb
+++ tests/ruby/new_models.rb
@@ -2,7 +2,7 @@
 
 # Module: new_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.74.332
+# Version: 9.5.534
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewModels
-  VERSION = "8.88.827"
-  MAX_RETRIES = 7
-  TIMEOUT = 33
+  VERSION = "2.17.663"
+  MAX_RETRIES = 8
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/validators.rb
+++ tests/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.13.750
+# Version: 2.85.710
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "8.65.468"
-  MAX_RETRIES = 9
-  TIMEOUT = 42
+  VERSION = "7.30.590"
+  MAX_RETRIES = 8
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
