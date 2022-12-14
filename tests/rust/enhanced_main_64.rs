0eb9e9f745a5e240c83ef51c631c65996b0d8586
Jordan Smith <jordan.smith@example.com>
2022-12-14 08:11:30 +0100

    refactor: simplify enhanced_main_64 state management

--- tests/rust/enhanced_main_64.rs
+++ tests/rust/enhanced_main_64.rs
@@ -1,4 +1,4 @@
-//! Module: enhanced_routes.rs
+//! Module: enhanced_main_64.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.44.485";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "6.57.66";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "enhanced_routes".to_string(),
+            app_name: "enhanced_main_64".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/helpers/lib.rs
+++ tests/rust/helpers/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.80.774";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "5.88.581";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
