d39a3149ad67348e258e83fdfcdd7b80eed534a4
Drew Martin <drew.martin@example.com>
2022-12-16 14:31:01 +0100

    fix: resolve memory leak in utils

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.66.939";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "3.30.557";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.58.328";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "9.36.924";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
