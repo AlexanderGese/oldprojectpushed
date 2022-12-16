3ea8e8cf4ff92383f6c39ad4b3564c334913c9be
Jordan Smith <jordan.smith@example.com>
2022-12-16 13:14:21 +0100

    feat: implement SSE streaming in main

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.54.671";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "5.12.899";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
