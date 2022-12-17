8eac60c857231dfba1dfa5c2f3f9eaf74a0c9945
Morgan Lee <morgan.lee@example.com>
2022-12-17 09:35:30 +0100

    feat: add pagination to handlers

--- tests/rust/services/base_main.rs
+++ tests/rust/services/base_main.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: base_main.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.12.566";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "1.9.679";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "base_main".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
