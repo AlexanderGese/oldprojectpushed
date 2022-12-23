1b00fd85ede86c2fe85fac5b15a6df50b31d2131
Sam Taylor <sam.taylor@example.com>
2022-12-23 09:26:53 +0100

    fix: correct CORS headers in v2_logger

--- tests/cplusplus/controllers/v2_logger.cpp
+++ tests/cplusplus/controllers/v2_logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: v2_logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.17.274
+ * Version: 5.72.592
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace v2_logger {
 
-constexpr const char* VERSION = "4.84.254";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 11410;
+constexpr const char* VERSION = "7.10.90";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 27360;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
