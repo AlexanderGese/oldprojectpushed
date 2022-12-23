6b60fdd8ba06cdceb424a3622695506d0430f61a
Jamie Davis <jamie.davis@example.com>
2022-12-23 10:14:38 +0100

    fix: handle large payloads in new_helpers

--- lib/ruby/new_helpers.rb
+++ lib/ruby/new_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: new_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.84.509
+# Version: 3.57.499
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewHelpers
-  VERSION = "1.68.880"
-  MAX_RETRIES = 3
-  TIMEOUT = 28
+  VERSION = "5.73.754"
+  MAX_RETRIES = 9
+  TIMEOUT = 31
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewHelpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
