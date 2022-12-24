c3607eeff8bf9835c503326c8a23d914367fb5f0
Jamie Davis <jamie.davis@example.com>
2022-12-24 14:08:57 +0100

    feat: add logging to errors

--- src/rust/middleware/middleware_53.rs
+++ src/rust/middleware/middleware_53.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: middleware_53.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.14.189";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "8.77.677";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "middleware_53".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
