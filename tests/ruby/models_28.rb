5434cd8407cb3182c913ac053c5e0280d8be3188
Jordan Smith <jordan.smith@example.com>
2022-12-24 12:40:19 +0100

    fix: patch security vulnerability in models_28

--- tests/ruby/models_28.rb
+++ tests/ruby/models_28.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: server.rb
+# Module: models_28.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.60.377
+# Version: 1.12.811
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Server
-  VERSION = "9.90.28"
-  MAX_RETRIES = 4
-  TIMEOUT = 34
+module Models28
+  VERSION = "4.64.966"
+  MAX_RETRIES = 5
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "server")
+      @app_name = opts.fetch(:app_name, "models_28")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Server::Application.new
+  app = Models28::Application.new
   app.start
 end
