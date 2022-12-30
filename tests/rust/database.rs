560b273025250a4ab6b7fc4864a820826d1494da
Riley Johnson <riley.johnson@example.com>
2022-12-30 19:28:12 +0100

    fix: resolve race condition in database

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.58.855";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "7.71.564";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
