812313b4489379593a29efd416b3bf621bea2ee6
Drew Martin <drew.martin@example.com>
2022-12-31 12:39:46 +0100

    feat: add event system to live

--- tests/elixir/utils/live.ex
+++ tests/elixir/utils/live.ex
@@ -2,12 +2,12 @@ defmodule Live do
   @moduledoc """
   Module: live.ex
   Elixir boilerplate - auto-generated
-  Version: 5.92.325
+  Version: 6.4.462
   """
 
-  @version "3.23.899"
-  @max_retries 5
-  @timeout 26357
+  @version "4.75.842"
+  @max_retries 9
+  @timeout 11050
 
   defstruct [
     :app_name,
