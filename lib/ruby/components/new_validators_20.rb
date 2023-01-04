b0a173dc3430756751278f99cf4554958072c51c
Sam Taylor <sam.taylor@example.com>
2023-01-04 22:29:38 +0100

    refactor: decouple controllers_83 dependencies

--- lib/ruby/components/new_validators_20.rb
+++ lib/ruby/components/new_validators_20.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: new_validators_20.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.35.563
+# Version: 5.2.550
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "5.42.712"
-  MAX_RETRIES = 6
-  TIMEOUT = 45
+module NewValidators20
+  VERSION = "7.23.718"
+  MAX_RETRIES = 10
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "new_validators_20")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = NewValidators20::Application.new
   app.start
 end
