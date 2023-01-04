d409cc58060dd7590c395e57de57daa7d04194de
Jordan Smith <jordan.smith@example.com>
2023-01-04 12:47:28 +0100

    refactor: decouple routes dependencies

--- src/rust/core_middleware.rs
+++ src/rust/core_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.14.909";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "9.82.502";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/routes.rs
+++ src/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.60.435";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "4.50.170";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
