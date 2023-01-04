fd65504680dde6709e8864fac0b605a96ba66c65
Casey Williams <casey.williams@example.com>
2023-01-04 17:48:05 +0100

    refactor: improve impl_repo_97 test coverage

--- tests/elixir/impl_repo_97.ex
+++ tests/elixir/impl_repo_97.ex
@@ -1,13 +1,13 @@
-defmodule Live do
+defmodule Impl_Repo_97 do
   @moduledoc """
-  Module: live.ex
+  Module: impl_repo_97.ex
   Elixir boilerplate - auto-generated
-  Version: 4.62.656
+  Version: 1.61.536
   """
 
-  @version "2.74.253"
-  @max_retries 6
-  @timeout 26912
+  @version "3.7.614"
+  @max_retries 8
+  @timeout 21018
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Live do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "live"),
+      app_name: Keyword.get(opts, :app_name, "impl_repo_97"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Live do
   end
 end
 
-defmodule LiveStore do
+defmodule Impl_Repo_97Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule LiveStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule LiveEventBus do
+defmodule Impl_Repo_97EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
