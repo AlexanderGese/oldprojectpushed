d60eee203468368304a899c6b64d35e7c31f9e66
Jamie Davis <jamie.davis@example.com>
2023-01-04 11:42:00 +0100

    fix: handle concurrent access in models

--- lib/ruby/models/models.rb
+++ lib/ruby/models/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.14.703
+# Version: 6.35.667
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "2.44.169"
+  VERSION = "6.21.408"
   MAX_RETRIES = 6
-  TIMEOUT = 28
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
