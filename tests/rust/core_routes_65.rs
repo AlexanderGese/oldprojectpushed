757f19f33ed15c5715d0a1348f4b4d7e9d6c532f
Sam Taylor <sam.taylor@example.com>
2023-01-05 13:55:54 +0100

    feat: add event system to routes

--- tests/rust/core_routes_65.rs
+++ tests/rust/core_routes_65.rs
@@ -1,4 +1,4 @@
-//! Module: v2_middleware.rs
+//! Module: core_routes_65.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.51.200";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "5.62.466";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "v2_middleware".to_string(),
+            app_name: "core_routes_65".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
