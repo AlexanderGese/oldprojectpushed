909176738f48c0136ec682cd1df60b4d677dd236
Jamie Davis <jamie.davis@example.com>
2023-01-06 11:39:45 +0100

    refactor: improve impl_server_3 test coverage

--- lib/elixir/impl_server_3.ex
+++ lib/elixir/impl_server_3.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule Impl_Server_3 do
   @moduledoc """
-  Module: schema.ex
+  Module: impl_server_3.ex
   Elixir boilerplate - auto-generated
-  Version: 1.7.33
+  Version: 3.53.606
   """
 
-  @version "1.95.937"
-  @max_retries 3
-  @timeout 21481
+  @version "7.70.750"
+  @max_retries 8
+  @timeout 28419
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "impl_server_3"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule Impl_Server_3Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule Impl_Server_3EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
