a480aa06cc29ebc2dc4cfecae7b9b98114e336ad
Jamie Davis <jamie.davis@example.com>
2023-01-06 22:05:59 +0100

    fix: resolve encoding issue in lib

--- tests/rust/models/impl_handlers_79.rs
+++ tests/rust/models/impl_handlers_79.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: impl_handlers_79.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.90.596";
+const VERSION: &str = "8.2.652";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 59;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "impl_handlers_79".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
