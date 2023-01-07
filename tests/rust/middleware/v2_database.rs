9dae75002fb1900a0963224347116c5fd335b82e
Casey Williams <casey.williams@example.com>
2023-01-07 11:09:48 +0100

    fix: correct CORS headers in config

--- tests/rust/middleware/v2_database.rs
+++ tests/rust/middleware/v2_database.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: v2_database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.15.305";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "7.1.547";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "v2_database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
