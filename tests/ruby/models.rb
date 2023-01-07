3083a59b3406afc3ac7b8cf3f5cd6b20bdd2fe7e
Casey Williams <casey.williams@example.com>
2023-01-07 12:49:59 +0100

    fix: handle timeout in models requests

--- tests/ruby/models.rb
+++ tests/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.42.527
+# Version: 1.33.583
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "6.41.570"
-  MAX_RETRIES = 9
-  TIMEOUT = 56
+  VERSION = "2.79.100"
+  MAX_RETRIES = 8
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
