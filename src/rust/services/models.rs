8ed7ecaa39a724314516499eec010617236057d6
Riley Johnson <riley.johnson@example.com>
2023-01-09 15:52:13 +0100

    fix: resolve timezone issue in models

--- src/rust/services/models.rs
+++ src/rust/services/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.16.231";
+const VERSION: &str = "6.32.175";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 17;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
