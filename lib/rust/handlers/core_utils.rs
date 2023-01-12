b5235372ba4aaa5a3baa478f7e79f26304801d29
Morgan Lee <morgan.lee@example.com>
2023-01-12 11:45:22 +0100

    refactor: simplify core_utils state management

--- lib/rust/handlers/core_utils.rs
+++ lib/rust/handlers/core_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.61.747";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "4.27.512";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
