00908c88cc2e8b5c060f7499dbea3872ac3f15f8
Jordan Smith <jordan.smith@example.com>
2023-01-12 15:06:54 +0100

    feat: add health check endpoint to base_database

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.15.305";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 39;
+const VERSION: &str = "3.2.903";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
