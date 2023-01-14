70bde965db024f6089abaf2bc1b1d24c0aa81fde
Casey Williams <casey.williams@example.com>
2023-01-14 22:40:26 +0100

    feat: add rate limiting to server

--- tests/ruby/models/config.rb
+++ tests/ruby/models/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.82.922
+# Version: 6.0.881
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "8.62.854"
-  MAX_RETRIES = 8
-  TIMEOUT = 44
+  VERSION = "5.34.40"
+  MAX_RETRIES = 7
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
