c1f8458df88c7f07c56e04c126ab632ce8a7813b
Jordan Smith <jordan.smith@example.com>
2023-01-14 17:37:01 +0100

    feat: add rate limiting to impl_utils_6

--- tests/rust/impl_utils_6.rs
+++ tests/rust/impl_utils_6.rs
@@ -1,4 +1,4 @@
-//! Module: impl_utils.rs
+//! Module: impl_utils_6.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.26.596";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "8.90.417";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_utils".to_string(),
+            app_name: "impl_utils_6".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
