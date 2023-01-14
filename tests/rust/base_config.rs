bea20fd222d7df364368857af172707d4d8f8a3b
Morgan Lee <morgan.lee@example.com>
2023-01-14 11:01:04 +0100

    fix: correct error propagation in base_config

--- tests/rust/base_config.rs
+++ tests/rust/base_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.31.599";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "2.64.421";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/services/models.rs
+++ tests/rust/services/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.91.988";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "7.32.415";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
