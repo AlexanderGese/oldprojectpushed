71079751e608337588e27393324efa21e979f192
Drew Martin <drew.martin@example.com>
2023-01-15 14:15:34 +0100

    chore: add linting rules for database

--- src/rust/database.rs
+++ src/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.75.334";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "7.97.852";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
