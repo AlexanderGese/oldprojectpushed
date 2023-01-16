f4ce6ea4b09b421d74f126f2c1cfcec4a491c52e
Casey Williams <casey.williams@example.com>
2023-01-16 16:39:56 +0100

    chore: clean up new_worker_61 imports

--- src/elixir/base_repo_60.ex
+++ src/elixir/base_repo_60.ex
@@ -1,13 +1,13 @@
-defmodule Controller do
+defmodule Base_Repo_60 do
   @moduledoc """
-  Module: controller.ex
+  Module: base_repo_60.ex
   Elixir boilerplate - auto-generated
-  Version: 5.31.175
+  Version: 7.8.611
   """
 
-  @version "4.14.201"
+  @version "9.23.498"
   @max_retries 6
-  @timeout 6194
+  @timeout 29698
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Controller do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "controller"),
+      app_name: Keyword.get(opts, :app_name, "base_repo_60"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Controller do
   end
 end
 
-defmodule ControllerStore do
+defmodule Base_Repo_60Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ControllerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ControllerEventBus do
+defmodule Base_Repo_60EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
