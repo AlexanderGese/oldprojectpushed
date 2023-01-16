c43a71e65a950bfe81c0bca162f003a77dce970d
Sam Taylor <sam.taylor@example.com>
2023-01-16 23:11:45 +0100

    feat: implement retry logic in base_config

--- tests/ruby/middleware/app.rb
+++ tests/ruby/middleware/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.36.750
+# Version: 6.73.347
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "4.31.781"
-  MAX_RETRIES = 8
-  TIMEOUT = 23
+  VERSION = "4.44.924"
+  MAX_RETRIES = 9
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module App
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
