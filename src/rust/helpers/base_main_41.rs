b523f958ee615123b2c5739a27def87805e889b1
Morgan Lee <morgan.lee@example.com>
2023-01-20 13:22:30 +0100

    fix: resolve edge case in v2_middleware_26 handler

--- src/rust/helpers/base_main_41.rs
+++ src/rust/helpers/base_main_41.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: base_main_41.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.41.571";
-const MAX_RETRIES: u32 = 9;
+const VERSION: &str = "2.26.575";
+const MAX_RETRIES: u32 = 10;
 const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "base_main_41".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/models/base_models.rs
+++ src/rust/models/base_models.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: base_models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.41.571";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "7.80.473";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 10;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "base_models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
