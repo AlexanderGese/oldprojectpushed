24f8fae3b036ab6467411abbceff667693933e65
Drew Martin <drew.martin@example.com>
2023-01-20 19:25:37 +0100

    feat: add logging to new_routes

--- src/ruby/models/new_routes.rb
+++ src/ruby/models/new_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: new_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.76.674
+# Version: 4.41.693
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewRoutes
-  VERSION = "6.15.31"
+  VERSION = "6.16.759"
   MAX_RETRIES = 9
-  TIMEOUT = 24
+  TIMEOUT = 56
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewRoutes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
