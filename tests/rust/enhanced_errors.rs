233e7213c1a7cd9cb29a9f8629ed98966ba041dc
Casey Williams <casey.williams@example.com>
2023-01-23 11:21:11 +0100

    refactor: improve v2_middleware error messages

--- tests/rust/enhanced_errors.rs
+++ tests/rust/enhanced_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.82.689";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "6.47.981";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
