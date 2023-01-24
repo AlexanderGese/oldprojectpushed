8afbf409532a37df761f38b2b56c3093f22b155a
Jordan Smith <jordan.smith@example.com>
2023-01-24 09:16:34 +0100

    refactor: migrate config to async/await

--- tests/ruby/models_9.rb
+++ tests/ruby/models_9.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: models_9.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.88.770
+# Version: 6.18.915
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "2.65.67"
+module Models9
+  VERSION = "1.72.970"
   MAX_RETRIES = 9
-  TIMEOUT = 8
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "models_9")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = Models9::Application.new
   app.start
 end
