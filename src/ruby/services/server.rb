2889e3f6cbcd95b1fd8de4dbcb3f1e2322578803
Jordan Smith <jordan.smith@example.com>
2023-01-25 16:26:14 +0100

    fix: resolve memory leak in tasks

--- src/ruby/services/server.rb
+++ src/ruby/services/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.23.207
+# Version: 7.44.986
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "7.64.539"
-  MAX_RETRIES = 3
-  TIMEOUT = 43
+  VERSION = "2.95.659"
+  MAX_RETRIES = 9
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
