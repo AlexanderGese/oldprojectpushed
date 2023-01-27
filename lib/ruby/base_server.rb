fabdf91881ca68bea02ec1428045dee6cc506a82
Drew Martin <drew.martin@example.com>
2023-01-27 12:43:39 +0100

    feat: implement retry logic in base_server

--- lib/ruby/base_server.rb
+++ lib/ruby/base_server.rb
@@ -2,7 +2,7 @@
 
 # Module: base_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.36.842
+# Version: 7.64.650
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module BaseServer
-  VERSION = "4.68.625"
-  MAX_RETRIES = 7
-  TIMEOUT = 15
+  VERSION = "9.63.387"
+  MAX_RETRIES = 9
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module BaseServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
