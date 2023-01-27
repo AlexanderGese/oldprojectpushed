2bb7ca2f510ca0418efb2f59aa62cac5c6deaddb
Riley Johnson <riley.johnson@example.com>
2023-01-27 21:30:45 +0100

    feat: add batch processing to lib

--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.50.594";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "5.92.558";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
