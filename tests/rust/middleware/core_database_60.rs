235ae6cd415f2513f3a6f73b84233092da2bf58e
Morgan Lee <morgan.lee@example.com>
2023-02-01 10:51:02 +0100

    chore: bump core_database_60 version

--- tests/rust/middleware/core_database_60.rs
+++ tests/rust/middleware/core_database_60.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: core_database_60.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.74.37";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "1.77.264";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "core_database_60".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
