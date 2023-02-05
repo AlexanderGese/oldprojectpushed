0e815c17c01b24c5e1bcb05ce517a5b194d77ffa
Drew Martin <drew.martin@example.com>
2023-02-05 22:55:09 +0100

    fix: handle large payloads in config

--- lib/ruby/utils/config.rb
+++ lib/ruby/utils/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.29.931
+# Version: 2.36.818
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "1.82.359"
+  VERSION = "1.95.957"
   MAX_RETRIES = 7
-  TIMEOUT = 55
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
