bba16a619f66ff07d41d7d627dacaa6973cd8bf1
Drew Martin <drew.martin@example.com>
2023-02-10 12:42:55 +0100

    chore: remove deprecated new_middleware code

--- src/ruby/new_middleware.rb
+++ src/ruby/new_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: new_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.42.159
+# Version: 3.94.711
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewMiddleware
-  VERSION = "2.59.996"
-  MAX_RETRIES = 4
-  TIMEOUT = 33
+  VERSION = "2.31.626"
+  MAX_RETRIES = 9
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
