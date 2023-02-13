93410b1e665c097ecde160b4a3726f6cbd78127d
Casey Williams <casey.williams@example.com>
2023-02-13 13:50:40 +0100

    chore: configure CI/CD for base_routes_79

--- src/rust/core_errors.rs
+++ src/rust/core_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.95.763";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "8.10.573";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
