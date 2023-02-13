b5690c89b7221ad4b14bd178b5ec448e1e24cd91
Jordan Smith <jordan.smith@example.com>
2023-02-13 11:08:53 +0100

    chore: add linting rules for lib

--- lib/rust/services/lib.rs
+++ lib/rust/services/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.30.411";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 15;
+const VERSION: &str = "1.99.961";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
