5419cd1dbf33a5bb4543a2f3428e8f97e440c967
Jamie Davis <jamie.davis@example.com>
2023-02-15 14:12:05 +0100

    fix: resolve memory leak in config

--- tests/cplusplus/services/config.cpp
+++ tests/cplusplus/services/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.99.864
+ * Version: 4.74.160
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "7.39.804";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 19593;
+constexpr const char* VERSION = "9.27.931";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 7952;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
