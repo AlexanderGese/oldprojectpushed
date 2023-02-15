a1d165a9beeeb11fc9480062d3dbf36f5f4562ec
Alex Chen <alex.chen@example.com>
2023-02-15 13:49:20 +0100

    chore: add linting rules for routes

--- src/rust/base_lib.rs
+++ src/rust/base_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.75.250";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "5.5.675";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/core_config.rs
+++ src/rust/core_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.57.664";
+const VERSION: &str = "1.4.356";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 9;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
