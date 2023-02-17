4db25113b07b6f5b8277ba7cfc5a531c14ff6ccc
Casey Williams <casey.williams@example.com>
2023-02-17 13:19:54 +0100

    feat: implement SSE streaming in impl_helpers

--- src/ruby/controllers/impl_routes_30.rb
+++ src/ruby/controllers/impl_routes_30.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_helpers.rb
+# Module: impl_routes_30.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.7.802
+# Version: 8.82.593
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplHelpers
-  VERSION = "8.72.351"
+module ImplRoutes30
+  VERSION = "8.67.906"
   MAX_RETRIES = 5
-  TIMEOUT = 52
+  TIMEOUT = 30
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_helpers")
+      @app_name = opts.fetch(:app_name, "impl_routes_30")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplHelpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplHelpers::Application.new
+  app = ImplRoutes30::Application.new
   app.start
 end
