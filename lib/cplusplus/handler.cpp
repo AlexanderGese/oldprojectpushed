06e062a785d90c39eec0124031d25c3937ef59e6
Jamie Davis <jamie.davis@example.com>
2023-02-17 15:02:36 +0100

    chore: bump handler version

--- lib/cplusplus/handler.cpp
+++ lib/cplusplus/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.37.647
+ * Version: 9.63.689
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "9.90.400";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 21822;
+constexpr const char* VERSION = "2.71.325";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 15762;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
