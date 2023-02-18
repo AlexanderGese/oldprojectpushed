6eefd5a5c834fd9139d10463d5f4e7d9f330f666
Morgan Lee <morgan.lee@example.com>
2023-02-18 17:45:16 +0100

    refactor: extract errors utility functions

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.31.415";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "1.93.37";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
