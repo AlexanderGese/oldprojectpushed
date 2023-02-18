17570e44081fac01e391bc4aa3849111cb3abb3b
Jordan Smith <jordan.smith@example.com>
2023-02-18 21:49:48 +0100

    refactor: decouple config dependencies

--- tests/rust/config.rs
+++ tests/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.31.909";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "3.42.645";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
