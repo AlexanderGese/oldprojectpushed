1407132328c17861fad15e047ad6e64d3ca02c30
Alex Chen <alex.chen@example.com>
2023-02-19 16:13:06 +0100

    refactor: improve new_helpers_76 error messages

--- tests/elixir/helpers/new_helpers_76.ex
+++ tests/elixir/helpers/new_helpers_76.ex
@@ -1,13 +1,13 @@
-defmodule Server do
+defmodule New_Helpers_76 do
   @moduledoc """
-  Module: server.ex
+  Module: new_helpers_76.ex
   Elixir boilerplate - auto-generated
-  Version: 9.9.284
+  Version: 2.17.781
   """
 
-  @version "8.87.568"
-  @max_retries 4
-  @timeout 23653
+  @version "7.96.543"
+  @max_retries 7
+  @timeout 5508
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Server do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "server"),
+      app_name: Keyword.get(opts, :app_name, "new_helpers_76"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Server do
   end
 end
 
-defmodule ServerStore do
+defmodule New_Helpers_76Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ServerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ServerEventBus do
+defmodule New_Helpers_76EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
