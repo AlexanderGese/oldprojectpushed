8db9546088f415a4f59ef4e93ec8b4f2086be59f
Sam Taylor <sam.taylor@example.com>
2023-02-20 17:31:25 +0100

    chore: configure CI/CD for enhanced_middleware

--- tests/ruby/models/enhanced_middleware.rb
+++ tests/ruby/models/enhanced_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: enhanced_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.50.539
+# Version: 9.14.440
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module EnhancedMiddleware
-  VERSION = "5.86.410"
-  MAX_RETRIES = 3
-  TIMEOUT = 36
+  VERSION = "4.71.978"
+  MAX_RETRIES = 7
+  TIMEOUT = 43
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module EnhancedMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
