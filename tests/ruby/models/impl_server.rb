14b812371818f2dabc7392a717827718ce85ac73
Jamie Davis <jamie.davis@example.com>
2023-02-20 18:18:39 +0100

    feat: add search functionality to impl_server

--- tests/ruby/models/impl_server.rb
+++ tests/ruby/models/impl_server.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.87.591
+# Version: 1.51.77
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplServer
-  VERSION = "1.60.555"
-  MAX_RETRIES = 8
-  TIMEOUT = 46
+  VERSION = "4.33.152"
+  MAX_RETRIES = 4
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
