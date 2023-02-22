f1d9c8d8edb5842f1495639fcf24d7d91b0b53b0
Alex Chen <alex.chen@example.com>
2023-02-22 09:37:22 +0100

    fix: correct type definitions in v2_handlers

--- src/rust/v2_handlers.rs
+++ src/rust/v2_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.8.441";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "1.71.505";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
