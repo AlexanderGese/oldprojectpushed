d574868e4c8b04cebe0816bb7740dd3eac11019f
Casey Williams <casey.williams@example.com>
2023-02-22 13:00:18 +0100

    fix: handle large payloads in new_models

--- tests/ruby/new_models.rb
+++ tests/ruby/new_models.rb
@@ -2,7 +2,7 @@
 
 # Module: new_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.5.534
+# Version: 3.39.636
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewModels
-  VERSION = "2.17.663"
-  MAX_RETRIES = 8
-  TIMEOUT = 50
+  VERSION = "9.29.449"
+  MAX_RETRIES = 3
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
