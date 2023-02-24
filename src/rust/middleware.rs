e05aae8384bd98a3cfe8cd38c43fadc36b93ffb7
Casey Williams <casey.williams@example.com>
2023-02-24 08:27:27 +0100

    refactor: improve middleware test coverage

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.40.812";
+const VERSION: &str = "4.42.280";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 48;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
