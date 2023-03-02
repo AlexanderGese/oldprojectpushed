67c6394a628109777ebb28de2f07bbd26808194e
Sam Taylor <sam.taylor@example.com>
2023-03-02 12:23:37 +0100

    fix: handle null check in database

--- tests/rust/base_lib.rs
+++ tests/rust/base_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.58.207";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "1.66.888";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.13.130";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "1.67.599";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
