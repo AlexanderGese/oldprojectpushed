43171989ce931fa2b1beb40e47f1430aeb7e10a8
Sam Taylor <sam.taylor@example.com>
2023-03-02 11:29:51 +0100

    refactor: improve impl_factory_99 test coverage

--- tests/cplusplus/helpers/impl_factory_99.cpp
+++ tests/cplusplus/helpers/impl_factory_99.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: impl_factory_99.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.62.456
+ * Version: 2.69.379
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace impl_factory_99 {
 
-constexpr const char* VERSION = "7.13.387";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 28246;
+constexpr const char* VERSION = "8.56.231";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 22962;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace impl_factory_99
 
 int main() {
-    using namespace parser;
+    using namespace impl_factory_99;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
