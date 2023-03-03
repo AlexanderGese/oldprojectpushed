b71d8169753d4dccdcbfebaf20fc9487c4335181
Sam Taylor <sam.taylor@example.com>
2023-03-03 17:48:29 +0100

    fix: handle timeout in errors requests

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.40.619";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "6.63.318";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 28;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/helpers/models.rs
+++ tests/rust/helpers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.98.155";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "1.25.350";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
