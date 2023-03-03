8a6a7891c2da7526413bb7d6ffc433ac12354121
Jordan Smith <jordan.smith@example.com>
2023-03-03 18:57:40 +0100

    feat: add rate limiting to config

--- src/ruby/v2_middleware.rb
+++ src/ruby/v2_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.62.240
+# Version: 1.80.295
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Middleware
-  VERSION = "6.7.971"
-  MAX_RETRIES = 4
-  TIMEOUT = 54
+  VERSION = "2.74.834"
+  MAX_RETRIES = 5
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
