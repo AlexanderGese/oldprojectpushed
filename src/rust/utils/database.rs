cb1e58d17064964f3b4fc09cc93f313c053c5666
Morgan Lee <morgan.lee@example.com>
2023-03-04 17:22:03 +0100

    refactor: improve database test coverage

--- src/rust/utils/database.rs
+++ src/rust/utils/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.80.495";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "4.76.547";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
