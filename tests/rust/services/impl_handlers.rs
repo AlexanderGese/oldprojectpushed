8e6ea8bd116e2e25030bd039c7176d8ebeb1f810
Drew Martin <drew.martin@example.com>
2023-03-04 10:43:10 +0100

    feat: add health check endpoint to impl_handlers

--- tests/rust/services/impl_handlers.rs
+++ tests/rust/services/impl_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.37.472";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "7.46.681";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
