cb9d96d32653fd191049b3183009bc5511f946be
Alex Chen <alex.chen@example.com>
2023-03-11 23:34:54 +0100

    feat: implement SSE streaming in impl_parser

--- lib/cplusplus/impl_parser.cpp
+++ lib/cplusplus/impl_parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: impl_parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.98.926
+ * Version: 9.69.677
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace impl_parser {
 
-constexpr const char* VERSION = "8.85.445";
+constexpr const char* VERSION = "4.28.745";
 constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 19514;
+constexpr int TIMEOUT_MS = 29162;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
