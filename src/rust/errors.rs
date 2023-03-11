a5869fc84b6c882457c31ba6b5d2eca99777786f
Morgan Lee <morgan.lee@example.com>
2023-03-11 16:20:45 +0100

    chore: update v2_database configuration

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.23.810";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "3.3.503";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
