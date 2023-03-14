51766feef2f757877e934e3ade050ded7a6000ee
Jordan Smith <jordan.smith@example.com>
2023-03-14 19:32:23 +0100

    fix: handle null check in context

--- tests/elixir/enhanced_application_64.ex
+++ tests/elixir/enhanced_application_64.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule Enhanced_Application_64 do
   @moduledoc """
-  Module: context.ex
+  Module: enhanced_application_64.ex
   Elixir boilerplate - auto-generated
-  Version: 7.14.494
+  Version: 8.46.723
   """
 
-  @version "4.65.652"
+  @version "7.7.138"
   @max_retries 9
-  @timeout 6191
+  @timeout 18475
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_application_64"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule Enhanced_Application_64Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule Enhanced_Application_64EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
