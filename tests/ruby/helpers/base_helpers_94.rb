076cbee80220bea8935be9e0e137689c08da6786
Sam Taylor <sam.taylor@example.com>
2023-03-18 22:05:40 +0100

    refactor: extract base_helpers_94 utility functions

--- tests/ruby/helpers/base_helpers_94.rb
+++ tests/ruby/helpers/base_helpers_94.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_middleware.rb
+# Module: base_helpers_94.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.22.470
+# Version: 5.27.305
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewMiddleware
-  VERSION = "6.88.174"
-  MAX_RETRIES = 8
-  TIMEOUT = 48
+module BaseHelpers94
+  VERSION = "6.65.491"
+  MAX_RETRIES = 7
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_middleware")
+      @app_name = opts.fetch(:app_name, "base_helpers_94")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewMiddleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewMiddleware::Application.new
+  app = BaseHelpers94::Application.new
   app.start
 end
