807fffdb4ef494b2ef4a5bdafd961f5c9a531ed5
Alex Chen <alex.chen@example.com>
2023-03-18 18:55:25 +0100

    feat: add event system to errors

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.63.318";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "7.57.320";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
