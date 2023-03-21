cfbe34260868e61f84047b84ea8303f85f9404d2
Alex Chen <alex.chen@example.com>
2023-03-21 23:46:35 +0100

    fix: patch security vulnerability in models

--- lib/rust/utils/models.rs
+++ lib/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.34.858";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "4.3.897";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
