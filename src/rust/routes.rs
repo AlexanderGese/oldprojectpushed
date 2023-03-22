3c87daa0df5217b4a0cb5a3ae69204af5da023e0
Jordan Smith <jordan.smith@example.com>
2023-03-22 16:21:23 +0100

    fix: resolve timezone issue in routes

--- src/rust/routes.rs
+++ src/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.13.734";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "4.27.459";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
