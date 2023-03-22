6db37927efc2429cbcb7b9779ba7817bee33652c
Sam Taylor <sam.taylor@example.com>
2023-03-22 14:01:00 +0100

    feat: add error handling to core_controllers

--- lib/ruby/new_helpers_83.rb
+++ lib/ruby/new_helpers_83.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_controllers.rb
+# Module: new_helpers_83.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.61.446
+# Version: 2.68.850
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreControllers
-  VERSION = "9.87.711"
-  MAX_RETRIES = 3
-  TIMEOUT = 22
+module NewHelpers83
+  VERSION = "3.21.902"
+  MAX_RETRIES = 4
+  TIMEOUT = 29
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_controllers")
+      @app_name = opts.fetch(:app_name, "new_helpers_83")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreControllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreControllers::Application.new
+  app = NewHelpers83::Application.new
   app.start
 end
