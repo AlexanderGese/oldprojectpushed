b58c0d7bcfcebee346645d8e442d2de6a9a72e46
Jamie Davis <jamie.davis@example.com>
2023-03-24 22:14:26 +0100

    feat: implement SSE streaming in models

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.29.224";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "6.85.697";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils/models.rs
+++ lib/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.3.897";
+const VERSION: &str = "7.91.879";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 53;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
