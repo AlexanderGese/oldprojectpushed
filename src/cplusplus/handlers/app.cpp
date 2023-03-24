dad9a14b2932047e3cb5a75d467e0978e76dd432
Alex Chen <alex.chen@example.com>
2023-03-24 16:33:12 +0100

    feat: add export functionality to handler

--- src/cplusplus/handlers/app.cpp
+++ src/cplusplus/handlers/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.35.244
+ * Version: 2.90.969
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "5.4.173";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 23003;
+constexpr const char* VERSION = "9.40.770";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 11949;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- src/cplusplus/v2_app_80.cpp
+++ src/cplusplus/v2_app_80.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: handler.cpp
+ * Module: v2_app_80.cpp
  * C++ boilerplate - auto-generated
- * Version: 7.40.999
+ * Version: 2.69.302
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace handler {
+namespace v2_app_80 {
 
-constexpr const char* VERSION = "4.30.383";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 14706;
+constexpr const char* VERSION = "9.45.418";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 23042;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace handler
+} // namespace v2_app_80
 
 int main() {
-    using namespace handler;
+    using namespace v2_app_80;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
