57e1d2dacf5797b39c0fd13f3da6504a3df22201
Riley Johnson <riley.johnson@example.com>
2023-03-25 12:24:17 +0100

    chore: configure CI/CD for middleware

--- tests/ruby/app.rb
+++ tests/ruby/app.rb
@@ -2,7 +2,7 @@
 
 # Module: app.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.8.941
+# Version: 9.31.947
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module App
-  VERSION = "4.30.365"
-  MAX_RETRIES = 3
-  TIMEOUT = 43
+  VERSION = "2.12.489"
+  MAX_RETRIES = 5
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/impl_models.rb
+++ tests/ruby/impl_models.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.70.523
+# Version: 1.58.769
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplModels
-  VERSION = "6.25.282"
-  MAX_RETRIES = 6
-  TIMEOUT = 12
+  VERSION = "5.37.915"
+  MAX_RETRIES = 9
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
