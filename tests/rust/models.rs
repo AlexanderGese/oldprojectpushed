acd9bc990046f5fc4fde93592b0dfd962a19d473
Morgan Lee <morgan.lee@example.com>
2023-03-25 20:22:26 +0100

    refactor: migrate models to async/await

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.22.423";
+const VERSION: &str = "6.57.300";
 const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 37;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
