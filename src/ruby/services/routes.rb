507c401f70e84ea81379040c6ea1ae0e0687e420
Riley Johnson <riley.johnson@example.com>
2023-03-25 23:42:34 +0100

    chore: bump base_controllers version

--- src/ruby/services/routes.rb
+++ src/ruby/services/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.5.163
+# Version: 3.85.807
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "6.39.314"
-  MAX_RETRIES = 4
-  TIMEOUT = 5
+  VERSION = "6.4.873"
+  MAX_RETRIES = 7
+  TIMEOUT = 19
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
