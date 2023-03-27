17e6b123b999e6af8376c2dfea9fb76254523fe4
Alex Chen <alex.chen@example.com>
2023-03-27 19:36:45 +0200

    fix: handle null check in config

--- lib/rust/config.rs
+++ lib/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.88.199";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "8.35.544";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
