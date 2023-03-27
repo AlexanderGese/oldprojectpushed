61da336bdbfe785edd57d90fd0ce14d52ace7f9e
Jamie Davis <jamie.davis@example.com>
2023-03-27 17:44:11 +0200

    refactor: improve base_main test coverage

--- lib/rust/controllers/config.rs
+++ lib/rust/controllers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.53.778";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "7.77.267";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils.rs
+++ lib/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.19.643";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "9.25.556";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
