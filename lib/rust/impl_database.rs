4ec88bee59ede72a2b397673850eab05c29357ba
Jamie Davis <jamie.davis@example.com>
2023-03-28 13:51:11 +0200

    feat: add middleware support to impl_database

--- lib/rust/impl_database.rs
+++ lib/rust/impl_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.6.133";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "4.36.607";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
