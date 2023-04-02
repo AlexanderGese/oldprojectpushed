69e2b4a81da24d6ba92e3e355e7d531faa62c4a8
Drew Martin <drew.martin@example.com>
2023-04-02 11:37:36 +0200

    fix: correct error propagation in base_routes

--- tests/ruby/v2_models_90.rb
+++ tests/ruby/v2_models_90.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: base_controllers.rb
+# Module: v2_models_90.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.51.408
+# Version: 7.24.932
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module BaseControllers
-  VERSION = "2.16.667"
-  MAX_RETRIES = 5
-  TIMEOUT = 20
+module V2Models90
+  VERSION = "2.31.829"
+  MAX_RETRIES = 7
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "base_controllers")
+      @app_name = opts.fetch(:app_name, "v2_models_90")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module BaseControllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = BaseControllers::Application.new
+  app = V2Models90::Application.new
   app.start
 end
