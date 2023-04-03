5174629979cfad07e1c088c81082bf80a3498430
Jordan Smith <jordan.smith@example.com>
2023-04-03 10:37:57 +0200

    fix: resolve encoding issue in impl_utils

--- src/rust/enhanced_middleware.rs
+++ src/rust/enhanced_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.6.259";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "5.54.131";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/impl_lib.rs
+++ src/rust/impl_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.49.816";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "5.10.305";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
