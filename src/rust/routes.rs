3dca41600611dd49fcaffe14c9699505f8fa8650
Jordan Smith <jordan.smith@example.com>
2023-04-05 13:53:04 +0200

    fix: correct CORS headers in routes

--- src/rust/routes.rs
+++ src/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.49.574";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "2.81.159";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
