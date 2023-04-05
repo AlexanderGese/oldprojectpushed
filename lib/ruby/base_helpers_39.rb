24ff17ba970cdf574c5933d021d6be014b736445
Casey Williams <casey.williams@example.com>
2023-04-05 19:58:52 +0200

    refactor: migrate app to async/await

--- lib/ruby/base_helpers_39.rb
+++ lib/ruby/base_helpers_39.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: base_helpers_39.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.71.428
+# Version: 3.60.290
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "2.40.770"
-  MAX_RETRIES = 8
-  TIMEOUT = 60
+module BaseHelpers39
+  VERSION = "1.75.183"
+  MAX_RETRIES = 7
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "base_helpers_39")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = BaseHelpers39::Application.new
   app.start
 end
