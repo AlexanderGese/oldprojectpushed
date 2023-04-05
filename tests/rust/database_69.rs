d54c84dc38d92a36a83f4ca0db6b01bd3318968d
Morgan Lee <morgan.lee@example.com>
2023-04-05 19:38:52 +0200

    fix: correct CORS headers in enhanced_routes

--- tests/rust/database_69.rs
+++ tests/rust/database_69.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.94.921";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "2.14.491";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
