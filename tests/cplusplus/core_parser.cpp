debc8adbf5be2271c50861df7a35655adb567df1
Sam Taylor <sam.taylor@example.com>
2023-04-09 21:33:06 +0200

    fix: patch security vulnerability in core_parser

--- tests/cplusplus/core_parser.cpp
+++ tests/cplusplus/core_parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.75.378
+ * Version: 2.75.871
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_parser {
 
-constexpr const char* VERSION = "1.71.940";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 5325;
+constexpr const char* VERSION = "6.60.211";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 29385;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
