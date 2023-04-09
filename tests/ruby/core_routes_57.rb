5f8f9fd38c9c10fb58484a2b68ca77320c9110b5
Morgan Lee <morgan.lee@example.com>
2023-04-09 15:24:44 +0200

    fix: resolve timezone issue in core_routes_57

--- tests/ruby/core_routes_57.rb
+++ tests/ruby/core_routes_57.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: core_routes_57.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.40.365
+# Version: 9.92.112
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "4.2.222"
-  MAX_RETRIES = 4
-  TIMEOUT = 51
+module CoreRoutes57
+  VERSION = "6.35.300"
+  MAX_RETRIES = 5
+  TIMEOUT = 58
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "core_routes_57")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = CoreRoutes57::Application.new
   app.start
 end
--- tests/ruby/utils/server_47.rb
+++ tests/ruby/utils/server_47.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: server_47.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.40.365
+# Version: 1.13.469
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "4.2.222"
-  MAX_RETRIES = 4
-  TIMEOUT = 51
+module Server47
+  VERSION = "4.40.457"
+  MAX_RETRIES = 5
+  TIMEOUT = 14
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "server_47")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = Server47::Application.new
   app.start
 end
