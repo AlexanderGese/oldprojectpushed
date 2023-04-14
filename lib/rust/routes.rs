7e550482ff755d3ada532d10f281414c064b1baf
Drew Martin <drew.martin@example.com>
2023-04-14 19:52:32 +0200

    chore: clean up middleware imports

--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.92.204";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "9.13.996";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
 pub struct Config {
