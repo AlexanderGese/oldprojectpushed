a63204a18b3aac64da5319f7605b2ca22ca22989
Casey Williams <casey.williams@example.com>
2023-04-14 13:28:20 +0200

    fix: resolve edge case in lib_82 handler

--- lib/rust/lib_82.rs
+++ lib/rust/lib_82.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.82.298";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "5.8.186";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware/enhanced_handlers.rs
+++ lib/rust/middleware/enhanced_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.75.108";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "8.14.367";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
