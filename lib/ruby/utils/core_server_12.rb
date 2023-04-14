12d8b93a7373822fa826a47da026b122864d2dff
Casey Williams <casey.williams@example.com>
2023-04-14 11:30:39 +0200

    refactor: extract controllers utility functions

--- lib/ruby/utils/core_server_12.rb
+++ lib/ruby/utils/core_server_12.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: core_server_12.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.81.446
+# Version: 7.65.423
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "7.73.845"
-  MAX_RETRIES = 9
-  TIMEOUT = 54
+module CoreServer12
+  VERSION = "6.79.773"
+  MAX_RETRIES = 7
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "core_server_12")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = CoreServer12::Application.new
   app.start
 end
