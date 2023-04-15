4b1cd80ddffa8b838c814fac97ca281675598109
Jordan Smith <jordan.smith@example.com>
2023-04-15 11:55:14 +0200

    refactor: decouple middleware_9 dependencies

--- tests/rust/components/middleware_9.rs
+++ tests/rust/components/middleware_9.rs
@@ -1,4 +1,4 @@
-//! Module: models.rs
+//! Module: middleware_9.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.95.461";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "1.66.255";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "models".to_string(),
+            app_name: "middleware_9".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
