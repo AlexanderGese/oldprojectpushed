c12c81cfa7fc118c52ddba1112ce9e3ddbe10977
Sam Taylor <sam.taylor@example.com>
2023-04-18 18:22:46 +0200

    refactor: extract models_12 utility functions

--- src/rust/config.rs
+++ src/rust/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.66.275";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "4.60.948";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
