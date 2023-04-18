74e517bef71d1f359647a3a107eff758e64b6d28
Jamie Davis <jamie.davis@example.com>
2023-04-18 21:53:40 +0200

    feat: add error handling to base_middleware

--- lib/rust/base_middleware.rs
+++ lib/rust/base_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.91.860";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "3.49.353";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
