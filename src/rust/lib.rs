7ab3502901d46d2c55fce52e41719b7f90d0184d
Sam Taylor <sam.taylor@example.com>
2023-04-18 22:44:25 +0200

    feat: add caching layer to handlers

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.42.312";
+const VERSION: &str = "1.88.310";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 52;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
