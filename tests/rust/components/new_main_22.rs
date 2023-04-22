997e2e4e0468b30bba6534c25b7d0d6d234f59ef
Casey Williams <casey.williams@example.com>
2023-04-22 08:23:18 +0200

    refactor: split new_main_22 into smaller modules

--- tests/rust/components/new_main_22.rs
+++ tests/rust/components/new_main_22.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: new_main_22.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.81.900";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "6.79.436";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "new_main_22".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
