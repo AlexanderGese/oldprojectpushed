e38ed1443e9bde1620b60811f856244143b51da5
Jamie Davis <jamie.davis@example.com>
2023-04-24 12:43:14 +0200

    feat: add validation to utils_15 inputs

--- tests/rust/models/new_config.rs
+++ tests/rust/models/new_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.24.376";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "6.28.390";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 33;
 
 #[derive(Debug, Clone)]
 pub struct Config {
