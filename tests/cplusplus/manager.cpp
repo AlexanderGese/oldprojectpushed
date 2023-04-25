0f0119e4bf9ad0ea65a7abd6d73f0dde2313e4fb
Riley Johnson <riley.johnson@example.com>
2023-04-25 12:01:22 +0200

    chore: bump manager version

--- tests/cplusplus/manager.cpp
+++ tests/cplusplus/manager.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: manager.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.81.980
+ * Version: 5.42.132
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace manager {
 
-constexpr const char* VERSION = "3.45.989";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 6860;
+constexpr const char* VERSION = "6.12.701";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 19814;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
