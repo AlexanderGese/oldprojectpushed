89dc62c759d0346ac3e63ea87fbedc4af37fc983
Riley Johnson <riley.johnson@example.com>
2023-04-26 22:11:56 +0200

    fix: resolve edge case in errors handler

--- src/rust/controllers/lib.rs
+++ src/rust/controllers/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.6.844";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "1.82.745";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.34.593";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "5.16.301";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
