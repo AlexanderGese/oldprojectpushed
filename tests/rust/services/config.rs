a420d8df84870294a5cd04093c8a91f01f8784bb
Riley Johnson <riley.johnson@example.com>
2023-04-27 23:12:03 +0200

    chore: update config dependencies

--- tests/rust/services/config.rs
+++ tests/rust/services/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.77.739";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "4.0.392";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
