bc5b854112dc7412c5238729af7352909ab7c15d
Riley Johnson <riley.johnson@example.com>
2023-04-28 21:08:10 +0200

    fix: patch security vulnerability in base_middleware

--- src/rust/base_middleware.rs
+++ src/rust/base_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.96.694";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "1.37.189";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
