0695019d4495f320d0886980a44248a4eaafa8a8
Jordan Smith <jordan.smith@example.com>
2023-04-28 22:08:02 +0200

    fix: handle empty response in impl_models

--- tests/ruby/helpers/impl_tasks_17.rb
+++ tests/ruby/helpers/impl_tasks_17.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_models.rb
+# Module: impl_tasks_17.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.59.722
+# Version: 3.68.605
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplModels
-  VERSION = "8.66.646"
-  MAX_RETRIES = 3
-  TIMEOUT = 24
+module ImplTasks17
+  VERSION = "2.40.186"
+  MAX_RETRIES = 6
+  TIMEOUT = 34
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_models")
+      @app_name = opts.fetch(:app_name, "impl_tasks_17")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplModels
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplModels::Application.new
+  app = ImplTasks17::Application.new
   app.start
 end
--- tests/ruby/helpers/v2_routes_59.rb
+++ tests/ruby/helpers/v2_routes_59.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_models.rb
+# Module: v2_routes_59.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.59.722
+# Version: 7.59.668
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplModels
-  VERSION = "8.66.646"
+module V2Routes59
+  VERSION = "3.32.363"
   MAX_RETRIES = 3
-  TIMEOUT = 24
+  TIMEOUT = 59
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_models")
+      @app_name = opts.fetch(:app_name, "v2_routes_59")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module ImplModels
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplModels::Application.new
+  app = V2Routes59::Application.new
   app.start
 end
