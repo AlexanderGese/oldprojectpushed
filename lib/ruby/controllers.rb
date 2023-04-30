fe7802aa87f3097ecf682e5947536ab873494ba8
Drew Martin <drew.martin@example.com>
2023-04-30 15:50:16 +0200

    feat: add error handling to new_server

--- lib/ruby/controllers.rb
+++ lib/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.92.725
+# Version: 7.13.30
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "1.18.987"
-  MAX_RETRIES = 5
-  TIMEOUT = 41
+  VERSION = "4.85.953"
+  MAX_RETRIES = 4
+  TIMEOUT = 49
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
