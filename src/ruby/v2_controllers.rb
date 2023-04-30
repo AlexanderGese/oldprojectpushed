86345cb151b168b639e354cc0ebed18b198b3636
Jordan Smith <jordan.smith@example.com>
2023-04-30 19:08:08 +0200

    fix: resolve edge case in models handler

--- src/ruby/v2_controllers.rb
+++ src/ruby/v2_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.37.470
+# Version: 8.57.239
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Controllers
-  VERSION = "6.69.281"
-  MAX_RETRIES = 8
-  TIMEOUT = 25
+  VERSION = "3.12.125"
+  MAX_RETRIES = 3
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
