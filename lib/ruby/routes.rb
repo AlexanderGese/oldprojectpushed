72a67a37e5f0a55d4a6029d03020ba3603d478f7
Riley Johnson <riley.johnson@example.com>
2023-05-03 23:45:48 +0200

    fix: handle large payloads in tasks

--- lib/ruby/routes.rb
+++ lib/ruby/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.70.916
+# Version: 7.86.653
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "4.42.660"
-  MAX_RETRIES = 7
-  TIMEOUT = 15
+  VERSION = "8.27.913"
+  MAX_RETRIES = 4
+  TIMEOUT = 23
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
