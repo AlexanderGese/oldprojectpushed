580ea44acfdf6c8000a1f191fa2bbd9975bfc378
Sam Taylor <sam.taylor@example.com>
2023-05-07 18:18:01 +0200

    refactor: decouple helpers dependencies

--- lib/ruby/components/helpers.rb
+++ lib/ruby/components/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.75.706
+# Version: 2.48.307
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "9.20.152"
-  MAX_RETRIES = 5
-  TIMEOUT = 58
+  VERSION = "5.90.817"
+  MAX_RETRIES = 9
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
