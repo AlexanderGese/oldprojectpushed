f323c3e13c39ca9081118eb39cd6f1182413b6cf
Sam Taylor <sam.taylor@example.com>
2023-05-09 18:17:08 +0200

    fix: correct error propagation in app

--- lib/cplusplus/app.cpp
+++ lib/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.26.488
+ * Version: 9.44.954
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "7.97.325";
+constexpr const char* VERSION = "4.32.537";
 constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 9877;
+constexpr int TIMEOUT_MS = 20885;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
