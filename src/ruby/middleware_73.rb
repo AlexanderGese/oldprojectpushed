e80d0a15ef923465773fcc2a8cb2ded8eedbcef3
Morgan Lee <morgan.lee@example.com>
2023-05-09 13:15:46 +0200

    fix: handle large payloads in middleware

--- src/ruby/middleware_73.rb
+++ src/ruby/middleware_73.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: middleware_73.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.37.356
+# Version: 6.18.751
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "3.79.318"
-  MAX_RETRIES = 6
-  TIMEOUT = 23
+module Middleware73
+  VERSION = "2.39.756"
+  MAX_RETRIES = 9
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "middleware_73")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = Middleware73::Application.new
   app.start
 end
