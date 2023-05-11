c1c8e4fdbc08c25b1b4c7db3d39c60e5d2f84956
Jamie Davis <jamie.davis@example.com>
2023-05-11 23:11:01 +0200

    refactor: improve validators test coverage

--- src/ruby/middleware/validators.rb
+++ src/ruby/middleware/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.55.94
+# Version: 1.38.378
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "8.35.868"
-  MAX_RETRIES = 8
-  TIMEOUT = 9
+  VERSION = "8.54.479"
+  MAX_RETRIES = 6
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
