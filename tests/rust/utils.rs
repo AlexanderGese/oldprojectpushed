2375db7fa197233692328f9d8be65a752ac60610
Casey Williams <casey.williams@example.com>
2023-05-13 15:32:46 +0200

    chore: clean up utils imports

--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.53.303";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "2.46.697";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
