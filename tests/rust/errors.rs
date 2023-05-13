c1cfffbe5b83f4aa28c32889e087eadc9bbbde66
Jamie Davis <jamie.davis@example.com>
2023-05-13 18:33:10 +0200

    feat: implement authentication in errors

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.32.20";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "4.87.15";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
