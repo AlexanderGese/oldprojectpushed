cc89417007044280c3b23aac5651873b3672236e
Jordan Smith <jordan.smith@example.com>
2023-05-16 11:50:12 +0200

    fix: resolve memory leak in models

--- lib/rust/models.rs
+++ lib/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.73.808";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "3.82.630";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/v2_routes.rs
+++ lib/rust/v2_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.65.727";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "7.68.167";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
