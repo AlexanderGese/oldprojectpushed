dcb3659516ca29949223dcac7b0fdc449c933363
Jamie Davis <jamie.davis@example.com>
2023-05-16 09:10:03 +0200

    chore: update database_20 dependencies

--- lib/rust/models.rs
+++ lib/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.82.630";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "6.94.365";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/services/new_database.rs
+++ lib/rust/services/new_database.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: new_database.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.4.617";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "2.99.176";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "new_database".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
