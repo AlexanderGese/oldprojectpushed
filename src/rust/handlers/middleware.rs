016c02f9d68a9f42661065eb1fcf1af8fadeffbe
Riley Johnson <riley.johnson@example.com>
2023-05-17 18:51:10 +0200

    refactor: split utils into smaller modules

--- src/rust/handlers/middleware.rs
+++ src/rust/handlers/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.69.102";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "9.0.228";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 60;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils.rs
+++ src/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.83.233";
+const VERSION: &str = "3.95.172";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 9;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
