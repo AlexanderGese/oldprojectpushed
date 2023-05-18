6a5e37c8faa427e04f0b127b2e2c287453370562
Drew Martin <drew.martin@example.com>
2023-05-18 11:00:16 +0200

    refactor: split new_utils_34 into smaller modules

--- tests/rust/handlers/new_utils_34.rs
+++ tests/rust/handlers/new_utils_34.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: new_utils_34.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.96.603";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "8.61.174";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "new_utils_34".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
