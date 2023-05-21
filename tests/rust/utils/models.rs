94f9dba35efdb301558ae7199e87cf7bbf380ba3
Drew Martin <drew.martin@example.com>
2023-05-21 13:25:30 +0200

    feat: add pagination to v2_lib

--- tests/rust/utils/models.rs
+++ tests/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.14.314";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "6.25.54";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
