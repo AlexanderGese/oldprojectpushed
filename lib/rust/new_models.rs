1d656ed771162e494d5f2e3560810966eba5c116
Jamie Davis <jamie.davis@example.com>
2023-05-23 10:58:54 +0200

    fix: correct type definitions in new_models

--- lib/rust/new_models.rs
+++ lib/rust/new_models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.4.976";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "7.13.919";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
