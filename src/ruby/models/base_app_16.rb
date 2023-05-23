3e09948719732c94b6596ffa48c8369391d4472e
Alex Chen <alex.chen@example.com>
2023-05-23 15:09:47 +0200

    feat: implement SSE streaming in base_tasks

--- src/ruby/models/base_app_16.rb
+++ src/ruby/models/base_app_16.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: base_app_16.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.22.59
+# Version: 7.71.462
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "2.19.91"
-  MAX_RETRIES = 5
-  TIMEOUT = 59
+module BaseApp16
+  VERSION = "5.80.537"
+  MAX_RETRIES = 3
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "base_app_16")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = BaseApp16::Application.new
   app.start
 end
