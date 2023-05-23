b19d8696d3043885db820b257dac063c42a6d874
Casey Williams <casey.williams@example.com>
2023-05-23 21:44:40 +0200

    chore: bump base_config version

--- src/ruby/services/base_config.rb
+++ src/ruby/services/base_config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: base_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.2.425
+# Version: 3.41.936
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "5.56.460"
-  MAX_RETRIES = 4
-  TIMEOUT = 19
+module BaseConfig
+  VERSION = "1.72.828"
+  MAX_RETRIES = 7
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "base_config")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = BaseConfig::Application.new
   app.start
 end
