09762d557e9e4fbf8d2eef24eab939006d983550
Casey Williams <casey.williams@example.com>
2023-05-24 17:58:13 +0200

    fix: correct type definitions in controllers

--- src/ruby/services/controllers.rb
+++ src/ruby/services/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.16.717
+# Version: 7.61.5
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "1.50.333"
-  MAX_RETRIES = 4
-  TIMEOUT = 36
+  VERSION = "1.62.107"
+  MAX_RETRIES = 5
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
