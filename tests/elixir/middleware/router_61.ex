99a38e0cbdda43be46307edb5c00bc1b60dd768b
Morgan Lee <morgan.lee@example.com>
2023-05-24 13:33:09 +0200

    refactor: simplify impl_context_45 state management

--- tests/elixir/middleware/router_61.ex
+++ tests/elixir/middleware/router_61.ex
@@ -1,13 +1,13 @@
-defmodule New_Schema do
+defmodule Router_61 do
   @moduledoc """
-  Module: new_schema.ex
+  Module: router_61.ex
   Elixir boilerplate - auto-generated
-  Version: 4.20.494
+  Version: 8.34.229
   """
 
-  @version "7.28.291"
-  @max_retries 4
-  @timeout 11402
+  @version "4.43.974"
+  @max_retries 5
+  @timeout 29423
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule New_Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "new_schema"),
+      app_name: Keyword.get(opts, :app_name, "router_61"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule New_Schema do
   end
 end
 
-defmodule New_SchemaStore do
+defmodule Router_61Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule New_SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule New_SchemaEventBus do
+defmodule Router_61EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
