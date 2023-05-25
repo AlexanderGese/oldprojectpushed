dce189972fb8f75914122f2012d6efefc4f37802
Drew Martin <drew.martin@example.com>
2023-05-25 21:29:12 +0200

    refactor: split middleware into smaller modules

--- lib/ruby/components/middleware.rb
+++ lib/ruby/components/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.28.948
+# Version: 4.18.268
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "1.90.782"
-  MAX_RETRIES = 10
-  TIMEOUT = 49
+  VERSION = "5.49.838"
+  MAX_RETRIES = 4
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
