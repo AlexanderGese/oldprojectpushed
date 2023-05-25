f91cab3148a8208335fcb643d0db85a66ac926c1
Morgan Lee <morgan.lee@example.com>
2023-05-25 20:28:04 +0200

    feat: implement SSE streaming in config

--- src/cplusplus/config.cpp
+++ src/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.98.453
+ * Version: 3.27.179
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "1.58.418";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 18474;
+constexpr const char* VERSION = "9.5.843";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 28526;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
