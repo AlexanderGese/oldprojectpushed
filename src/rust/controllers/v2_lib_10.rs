71192b82479e2369277bdd31933e37793fe096bd
Drew Martin <drew.martin@example.com>
2023-05-26 08:55:26 +0200

    fix: correct type definitions in utils

--- src/rust/controllers/v2_lib_10.rs
+++ src/rust/controllers/v2_lib_10.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: v2_lib_10.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.18.951";
+const VERSION: &str = "3.35.686";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 37;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "v2_lib_10".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/core_routes_67.rs
+++ src/rust/core_routes_67.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: core_routes_67.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.18.951";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "5.64.992";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "core_routes_67".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
