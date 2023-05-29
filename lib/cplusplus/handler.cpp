9c76b42e9ae4eae19ce9ef1e97868c93a29d9eb5
Morgan Lee <morgan.lee@example.com>
2023-05-29 12:54:37 +0200

    test: add unit tests for handler

--- lib/cplusplus/handler.cpp
+++ lib/cplusplus/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.15.225
+ * Version: 9.90.386
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "2.78.639";
+constexpr const char* VERSION = "8.44.942";
 constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 27480;
+constexpr int TIMEOUT_MS = 15569;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/v2_parser.cpp
+++ lib/cplusplus/v2_parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: v2_parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.80.822
+ * Version: 7.4.264
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace v2_parser {
 
-constexpr const char* VERSION = "5.40.152";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 22004;
+constexpr const char* VERSION = "4.1.809";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 28684;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
