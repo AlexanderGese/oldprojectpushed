7bf7b3256c90de1dc898e1752839ef1f7293ae36
Alex Chen <alex.chen@example.com>
2023-05-30 15:44:28 +0200

    refactor: improve middleware test coverage

--- lib/ruby/new_config.rb
+++ lib/ruby/new_config.rb
@@ -2,7 +2,7 @@
 
 # Module: new_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.33.203
+# Version: 6.79.254
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewConfig
-  VERSION = "3.99.566"
-  MAX_RETRIES = 6
-  TIMEOUT = 28
+  VERSION = "9.4.99"
+  MAX_RETRIES = 5
+  TIMEOUT = 10
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- lib/ruby/new_helpers_99.rb
+++ lib/ruby/new_helpers_99.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: new_helpers_99.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.63.227
+# Version: 6.97.341
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "6.46.789"
+module NewHelpers99
+  VERSION = "8.59.473"
   MAX_RETRIES = 7
-  TIMEOUT = 21
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "new_helpers_99")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = NewHelpers99::Application.new
   app.start
 end
