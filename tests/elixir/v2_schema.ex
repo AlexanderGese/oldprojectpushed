77a763160420fb61517ba6d41e235759eb3f6d1b
Morgan Lee <morgan.lee@example.com>
2023-06-01 10:32:19 +0200

    refactor: decouple v2_schema dependencies

--- tests/elixir/v2_schema.ex
+++ tests/elixir/v2_schema.ex
@@ -2,12 +2,12 @@ defmodule V2_Schema do
   @moduledoc """
   Module: v2_schema.ex
   Elixir boilerplate - auto-generated
-  Version: 4.9.774
+  Version: 8.29.927
   """
 
-  @version "3.16.970"
-  @max_retries 6
-  @timeout 8916
+  @version "4.2.89"
+  @max_retries 3
+  @timeout 21075
 
   defstruct [
     :app_name,
