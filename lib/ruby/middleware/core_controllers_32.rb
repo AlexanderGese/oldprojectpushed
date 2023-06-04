b9280c78ad552628ff34824cb8afc78b41489843
Drew Martin <drew.martin@example.com>
2023-06-04 11:18:36 +0200

    fix: handle timeout in tasks requests

--- lib/ruby/middleware/core_controllers_32.rb
+++ lib/ruby/middleware/core_controllers_32.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: core_controllers_32.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.27.18
+# Version: 1.86.127
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "7.93.233"
-  MAX_RETRIES = 7
-  TIMEOUT = 49
+module CoreControllers32
+  VERSION = "8.8.123"
+  MAX_RETRIES = 5
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "core_controllers_32")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = CoreControllers32::Application.new
   app.start
 end
