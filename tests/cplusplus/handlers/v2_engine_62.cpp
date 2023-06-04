924e74b7e56efed006ddb2cc75836c70289758c6
Drew Martin <drew.martin@example.com>
2023-06-04 09:01:34 +0200

    refactor: extract v2_engine_62 utility functions

--- tests/cplusplus/handlers/v2_engine_62.cpp
+++ tests/cplusplus/handlers/v2_engine_62.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: v2_engine_62.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.88.214
+ * Version: 2.75.727
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace v2_engine_62 {
 
-constexpr const char* VERSION = "4.64.217";
+constexpr const char* VERSION = "3.17.522";
 constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 29302;
+constexpr int TIMEOUT_MS = 22298;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace v2_engine_62
 
 int main() {
-    using namespace parser;
+    using namespace v2_engine_62;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
