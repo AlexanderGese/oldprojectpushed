d5443a3b0498ad5b4f484248a078ad370a504034
Alex Chen <alex.chen@example.com>
2023-06-05 17:23:12 +0200

    fix: resolve encoding issue in controllers

--- tests/ruby/services/core_models.rb
+++ tests/ruby/services/core_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_routes.rb
+# Module: core_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.92.357
+# Version: 5.41.580
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplRoutes
-  VERSION = "3.36.64"
-  MAX_RETRIES = 7
-  TIMEOUT = 29
+module CoreModels
+  VERSION = "5.11.458"
+  MAX_RETRIES = 9
+  TIMEOUT = 34
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_routes")
+      @app_name = opts.fetch(:app_name, "core_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplRoutes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplRoutes::Application.new
+  app = CoreModels::Application.new
   app.start
 end
