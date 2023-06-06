b45b5426fbb9ab5a12cea4fddf6f63331120f775
Sam Taylor <sam.taylor@example.com>
2023-06-06 21:12:52 +0200

    chore: bump middleware version

--- src/rust/controllers/middleware.rs
+++ src/rust/controllers/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.4.37";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "4.71.264";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/main.rs
+++ src/rust/main.rs
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.12.344";
-const MAX_RETRIES: u32 = 3;
+const VERSION: &str = "6.60.808";
+const MAX_RETRIES: u32 = 8;
 const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
