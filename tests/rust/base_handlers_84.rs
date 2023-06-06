91b72ec628654ceb4a6f5b34c9ad0c2409c8e010
Morgan Lee <morgan.lee@example.com>
2023-06-06 13:54:57 +0200

    fix: handle null check in models

--- tests/rust/base_handlers_84.rs
+++ tests/rust/base_handlers_84.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: base_handlers_84.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.69.233";
+const VERSION: &str = "8.2.503";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 22;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "base_handlers_84".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/controllers/models.rs
+++ tests/rust/controllers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.44.613";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 31;
+const VERSION: &str = "3.23.915";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
