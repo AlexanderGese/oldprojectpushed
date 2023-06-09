b6d74f0eefed29aab317f6c4767ec4acf2d97e47
Morgan Lee <morgan.lee@example.com>
2023-06-09 17:54:30 +0200

    feat: implement utils service layer

--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.46.697";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "5.86.804";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
