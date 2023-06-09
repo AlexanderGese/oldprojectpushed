ba43b85d5845f553e755b9376bac02ade8c776c3
Sam Taylor <sam.taylor@example.com>
2023-06-09 21:10:55 +0200

    test: add unit tests for config

--- src/rust/components/enhanced_config.rs
+++ src/rust/components/enhanced_config.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: enhanced_config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.22.804";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "7.9.533";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "enhanced_config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/core_config_71.rs
+++ src/rust/core_config_71.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: core_config_71.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.22.804";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "8.63.134";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "core_config_71".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
