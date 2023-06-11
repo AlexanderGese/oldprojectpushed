4527627220662cc1137e77f847b2e860b1d53606
Drew Martin <drew.martin@example.com>
2023-06-11 15:43:31 +0200

    fix: handle null check in v2_config_98

--- src/rust/helpers/config.rs
+++ src/rust/helpers/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.5.722";
+const VERSION: &str = "2.21.239";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 44;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/helpers/v2_config_98.rs
+++ src/rust/helpers/v2_config_98.rs
@@ -1,4 +1,4 @@
-//! Module: errors.rs
+//! Module: v2_config_98.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.79.798";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "7.38.876";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "errors".to_string(),
+            app_name: "v2_config_98".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
