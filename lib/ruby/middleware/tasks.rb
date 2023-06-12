f96d29e1af683c4b809f4047e78d4ae69312f7a1
Alex Chen <alex.chen@example.com>
2023-06-12 08:17:01 +0200

    refactor: improve tasks error messages

--- lib/ruby/middleware/tasks.rb
+++ lib/ruby/middleware/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.10.92
+# Version: 2.14.143
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "1.93.672"
-  MAX_RETRIES = 3
-  TIMEOUT = 21
+  VERSION = "6.11.955"
+  MAX_RETRIES = 4
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
