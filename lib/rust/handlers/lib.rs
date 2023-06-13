04b08cd6a565fb159558652ae204d3ab914ef999
Jamie Davis <jamie.davis@example.com>
2023-06-13 18:42:43 +0200

    refactor: migrate core_middleware to async/await

--- lib/rust/handlers/lib.rs
+++ lib/rust/handlers/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.41.83";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "7.50.543";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
