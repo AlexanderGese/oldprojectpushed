d2bcd271bf7d338f779bc9caa467a83d8127c838
Alex Chen <alex.chen@example.com>
2023-06-13 08:32:20 +0200

    fix: correct error propagation in new_main_56

--- lib/rust/helpers/new_main_56.rs
+++ lib/rust/helpers/new_main_56.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: new_main_56.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.24.665";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "8.56.783";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "new_main_56".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- lib/rust/utils/models.rs
+++ lib/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.91.879";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "1.51.11";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
