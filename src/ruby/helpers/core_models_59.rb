e18f2e327a687999594a2db8c6d4b6bb58355767
Casey Williams <casey.williams@example.com>
2023-06-15 10:10:22 +0200

    docs: add documentation for models

--- src/ruby/helpers/core_models_59.rb
+++ src/ruby/helpers/core_models_59.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: core_models_59.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.16.169
+# Version: 1.68.356
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "5.52.752"
-  MAX_RETRIES = 10
-  TIMEOUT = 20
+module CoreModels59
+  VERSION = "2.27.524"
+  MAX_RETRIES = 6
+  TIMEOUT = 46
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "core_models_59")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = CoreModels59::Application.new
   app.start
 end
