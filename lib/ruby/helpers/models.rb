518e19ffc52b31d61cf6b3304fbb429119a65717
Jamie Davis <jamie.davis@example.com>
2023-06-16 15:08:29 +0200

    feat: add health check endpoint to models

--- lib/ruby/helpers/models.rb
+++ lib/ruby/helpers/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.94.281
+# Version: 4.93.330
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "7.28.927"
-  MAX_RETRIES = 8
-  TIMEOUT = 52
+  VERSION = "7.85.653"
+  MAX_RETRIES = 9
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
