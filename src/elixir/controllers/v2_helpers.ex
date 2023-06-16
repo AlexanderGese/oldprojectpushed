e3dbb69cec43028355267f5abfc8eaacff295e93
Riley Johnson <riley.johnson@example.com>
2023-06-16 08:57:39 +0200

    feat: add error handling to v2_helpers

--- src/elixir/controllers/v2_helpers.ex
+++ src/elixir/controllers/v2_helpers.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule V2_Helpers do
   @moduledoc """
-  Module: context.ex
+  Module: v2_helpers.ex
   Elixir boilerplate - auto-generated
-  Version: 3.50.761
+  Version: 5.53.172
   """
 
-  @version "2.10.289"
-  @max_retries 3
-  @timeout 19562
+  @version "7.58.214"
+  @max_retries 8
+  @timeout 21497
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "v2_helpers"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule V2_HelpersStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule V2_HelpersEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
