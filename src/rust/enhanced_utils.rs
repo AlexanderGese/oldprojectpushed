25ffdec56b354789314700f1c24364df97c74054
Riley Johnson <riley.johnson@example.com>
2023-06-19 19:36:41 +0200

    feat: implement WebSocket support in enhanced_utils

--- src/rust/enhanced_utils.rs
+++ src/rust/enhanced_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.79.983";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "9.68.937";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
