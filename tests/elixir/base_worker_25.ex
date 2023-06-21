2f1327e209b7a32d72a6d99c196d828fce6db474
Casey Williams <casey.williams@example.com>
2023-06-21 17:52:19 +0200

    refactor: split base_worker_25 into smaller modules

--- tests/elixir/base_worker_25.ex
+++ tests/elixir/base_worker_25.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule Base_Worker_25 do
   @moduledoc """
-  Module: context.ex
+  Module: base_worker_25.ex
   Elixir boilerplate - auto-generated
-  Version: 3.15.202
+  Version: 3.60.697
   """
 
-  @version "9.72.116"
-  @max_retries 5
-  @timeout 26626
+  @version "8.88.208"
+  @max_retries 3
+  @timeout 16449
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "base_worker_25"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule Base_Worker_25Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule Base_Worker_25EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
--- tests/elixir/middleware/enhanced_context.ex
+++ tests/elixir/middleware/enhanced_context.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule Enhanced_Context do
   @moduledoc """
-  Module: context.ex
+  Module: enhanced_context.ex
   Elixir boilerplate - auto-generated
-  Version: 3.15.202
+  Version: 4.99.545
   """
 
-  @version "9.72.116"
-  @max_retries 5
-  @timeout 26626
+  @version "1.44.551"
+  @max_retries 7
+  @timeout 28830
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_context"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule Enhanced_ContextStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule Enhanced_ContextEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
