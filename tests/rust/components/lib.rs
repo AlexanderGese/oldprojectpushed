8dcd84a4d74a9d90591f452f69db93aeae275f9d
Morgan Lee <morgan.lee@example.com>
2023-06-21 13:06:41 +0200

    fix: correct CORS headers in middleware

--- tests/rust/components/lib.rs
+++ tests/rust/components/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.79.238";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 22;
+const VERSION: &str = "9.7.983";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 44;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/utils/impl_database_3.rs
+++ tests/rust/utils/impl_database_3.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: impl_database_3.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.36.566";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 50;
+const VERSION: &str = "5.62.516";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "impl_database_3".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
