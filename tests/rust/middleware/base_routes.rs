03c58519205ce7fcb16a723a94c7846297f9d4bf
Jordan Smith <jordan.smith@example.com>
2023-06-21 10:35:28 +0200

    fix: resolve encoding issue in base_database_34

--- tests/rust/middleware/base_routes.rs
+++ tests/rust/middleware/base_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.76.235";
+const VERSION: &str = "5.42.30";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 54;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
