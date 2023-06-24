3def72a4c3c1d0fa87f0725b56f2490aeb700276
Sam Taylor <sam.taylor@example.com>
2023-06-24 20:48:05 +0200

    feat: add event system to config

--- src/rust/utils/config.rs
+++ src/rust/utils/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.95.28";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "1.8.847";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils/routes.rs
+++ src/rust/utils/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.93.779";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 46;
+const VERSION: &str = "3.19.131";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 54;
 
 #[derive(Debug, Clone)]
 pub struct Config {
