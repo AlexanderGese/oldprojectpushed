5856cfd99360a6c2beaffc3a3c1dbfde7b703500
Casey Williams <casey.williams@example.com>
2023-06-26 14:49:54 +0200

    fix: resolve race condition in enhanced_config

--- tests/ruby/services/core_middleware.rb
+++ tests/ruby/services/core_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: core_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.68.491
+# Version: 1.75.871
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreMiddleware
-  VERSION = "5.53.777"
-  MAX_RETRIES = 3
-  TIMEOUT = 44
+  VERSION = "2.21.510"
+  MAX_RETRIES = 6
+  TIMEOUT = 26
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/validators.rb
+++ tests/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.32.448
+# Version: 5.67.967
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "5.98.456"
-  MAX_RETRIES = 7
-  TIMEOUT = 56
+  VERSION = "2.49.149"
+  MAX_RETRIES = 3
+  TIMEOUT = 54
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Validators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
