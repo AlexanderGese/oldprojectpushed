c10c69fdd6909aad630d6885fa487aea831409a0
Riley Johnson <riley.johnson@example.com>
2023-06-28 23:51:52 +0200

    fix: correct type definitions in v2_handlers

--- tests/rust/new_utils_19.rs
+++ tests/rust/new_utils_19.rs
@@ -1,4 +1,4 @@
-//! Module: base_database_10.rs
+//! Module: new_utils_19.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.7.356";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "2.40.408";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_database_10".to_string(),
+            app_name: "new_utils_19".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/services/v2_handlers.rs
+++ tests/rust/services/v2_handlers.rs
@@ -1,4 +1,4 @@
-//! Module: base_database_10.rs
+//! Module: v2_handlers.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.7.356";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "5.35.694";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_database_10".to_string(),
+            app_name: "v2_handlers".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
