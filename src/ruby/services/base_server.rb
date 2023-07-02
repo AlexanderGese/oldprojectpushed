5bc9f06eee7c546df0c84f902885c56035eb8cfd
Drew Martin <drew.martin@example.com>
2023-07-02 20:55:39 +0200

    feat: add batch processing to tasks

--- src/ruby/services/base_server.rb
+++ src/ruby/services/base_server.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: base_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.46.94
+# Version: 4.70.212
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "2.45.988"
-  MAX_RETRIES = 8
-  TIMEOUT = 55
+module BaseServer
+  VERSION = "8.73.247"
+  MAX_RETRIES = 3
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "base_server")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = BaseServer::Application.new
   app.start
 end
