3506a38d9b4e57e311996d9eb2d6443ed0a9c78c
Casey Williams <casey.williams@example.com>
2023-07-04 09:49:02 +0200

    fix: resolve encoding issue in models

--- src/rust/models.rs
+++ src/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.66.45";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 20;
+const VERSION: &str = "3.9.689";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
