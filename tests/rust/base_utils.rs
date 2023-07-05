73f6c383fd2497650d046642e5a1d02bf0ee1b73
Morgan Lee <morgan.lee@example.com>
2023-07-05 09:31:39 +0200

    feat: add search functionality to base_utils

--- tests/rust/base_utils.rs
+++ tests/rust/base_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.8.321";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "7.19.968";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
