f56172e4ed29716f40bd1a5412e95d4995d824fb
Sam Taylor <sam.taylor@example.com>
2023-07-07 22:15:21 +0200

    fix: correct date formatting in routes

--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.6.504";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "3.60.852";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
