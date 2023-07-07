eb30d1b8f13016b677fba5455c4e470fef650e4a
Morgan Lee <morgan.lee@example.com>
2023-07-07 09:17:59 +0200

    refactor: simplify parser state management

--- lib/cplusplus/factory.cpp
+++ lib/cplusplus/factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.83.601
+ * Version: 1.38.271
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace factory {
 
-constexpr const char* VERSION = "4.40.644";
+constexpr const char* VERSION = "5.75.314";
 constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 11525;
+constexpr int TIMEOUT_MS = 27174;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/parser.cpp
+++ lib/cplusplus/parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.30.298
+ * Version: 2.63.599
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace parser {
 
-constexpr const char* VERSION = "2.26.295";
+constexpr const char* VERSION = "9.45.722";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 16220;
+constexpr int TIMEOUT_MS = 15061;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
