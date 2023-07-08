807364078521716c7cdbbbaa6511cae831746965
Sam Taylor <sam.taylor@example.com>
2023-07-08 20:15:24 +0200

    fix: resolve edge case in impl_utils_80 handler

--- tests/rust/impl_utils_80.rs
+++ tests/rust/impl_utils_80.rs
@@ -1,4 +1,4 @@
-//! Module: lib.rs
+//! Module: impl_utils_80.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.36.633";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "4.9.698";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 11;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "lib".to_string(),
+            app_name: "impl_utils_80".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
