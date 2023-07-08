d3f03a6a216bd3a1c3dd442281b258d83b8c6a80
Morgan Lee <morgan.lee@example.com>
2023-07-08 13:08:09 +0200

    refactor: improve new_application error messages

--- tests/elixir/utils/v2_application.ex
+++ tests/elixir/utils/v2_application.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule V2_Application do
   @moduledoc """
-  Module: context.ex
+  Module: v2_application.ex
   Elixir boilerplate - auto-generated
-  Version: 2.94.592
+  Version: 5.44.843
   """
 
-  @version "1.52.700"
-  @max_retries 5
-  @timeout 29475
+  @version "2.77.310"
+  @max_retries 6
+  @timeout 28934
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "v2_application"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule V2_ApplicationStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule V2_ApplicationEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
