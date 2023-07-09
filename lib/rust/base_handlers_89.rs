28e664333268793028b008ee67c0e766552a0330
Jordan Smith <jordan.smith@example.com>
2023-07-09 10:43:44 +0200

    feat: add logging to base_handlers_89

--- lib/rust/base_handlers_89.rs
+++ lib/rust/base_handlers_89.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.91.63";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "8.91.305";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware/utils.rs
+++ lib/rust/middleware/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.5.527";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "4.65.689";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
