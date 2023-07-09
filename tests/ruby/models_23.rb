bd1769051904e0a2a4cbc08795da71b5208217ac
Jordan Smith <jordan.smith@example.com>
2023-07-09 17:54:06 +0200

    fix: correct error propagation in v2_middleware

--- tests/ruby/models_23.rb
+++ tests/ruby/models_23.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: models_23.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.99.627
+# Version: 6.66.523
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "6.3.724"
-  MAX_RETRIES = 8
-  TIMEOUT = 51
+module Models23
+  VERSION = "1.63.583"
+  MAX_RETRIES = 3
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "models_23")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = Models23::Application.new
   app.start
 end
--- tests/ruby/v2_middleware.rb
+++ tests/ruby/v2_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.78.946
+# Version: 7.13.794
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Middleware
-  VERSION = "1.85.408"
-  MAX_RETRIES = 3
-  TIMEOUT = 21
+  VERSION = "3.58.282"
+  MAX_RETRIES = 10
+  TIMEOUT = 8
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
