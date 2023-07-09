ee58bcb34f639c6319d47bb7ee4557ec1aa67ac6
Riley Johnson <riley.johnson@example.com>
2023-07-09 08:24:43 +0200

    fix: handle null check in enhanced_config

--- tests/rust/enhanced_config.rs
+++ tests/rust/enhanced_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.14.511";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 37;
+const VERSION: &str = "6.2.623";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
