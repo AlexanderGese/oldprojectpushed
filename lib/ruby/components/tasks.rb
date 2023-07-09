95c20a486e0eca1d8c2fbd78ff37da129054d982
Jordan Smith <jordan.smith@example.com>
2023-07-09 17:30:13 +0200

    feat: implement WebSocket support in tasks

--- lib/ruby/components/tasks.rb
+++ lib/ruby/components/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.83.438
+# Version: 9.41.674
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "5.14.829"
-  MAX_RETRIES = 9
-  TIMEOUT = 51
+  VERSION = "2.33.473"
+  MAX_RETRIES = 7
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- lib/ruby/handlers/middleware.rb
+++ lib/ruby/handlers/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.17.826
+# Version: 7.45.662
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "8.33.847"
-  MAX_RETRIES = 7
-  TIMEOUT = 12
+  VERSION = "3.71.331"
+  MAX_RETRIES = 10
+  TIMEOUT = 40
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
