26a13a32e513d3b851e2071b1ef283b47d773058
Alex Chen <alex.chen@example.com>
2023-07-15 20:40:18 +0200

    test: add unit tests for v2_models_41

--- src/rust/utils/routes.rs
+++ src/rust/utils/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.19.131";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "8.95.669";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
