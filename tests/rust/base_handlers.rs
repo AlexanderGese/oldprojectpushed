b72352df2f514f144722adf35e4ff4676f4e83a7
Sam Taylor <sam.taylor@example.com>
2023-07-17 12:44:33 +0200

    feat: add rate limiting to middleware

--- tests/rust/base_handlers.rs
+++ tests/rust/base_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.63.681";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "1.78.853";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
