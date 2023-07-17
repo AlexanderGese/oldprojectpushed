aa482dec5651033943fd72f1384428c9a35d86f0
Morgan Lee <morgan.lee@example.com>
2023-07-17 15:31:15 +0200

    feat: add pagination to app

--- src/ruby/middleware/core_models.rb
+++ src/ruby/middleware/core_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: core_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.0.870
+# Version: 6.34.311
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "3.41.863"
-  MAX_RETRIES = 6
-  TIMEOUT = 46
+module CoreModels
+  VERSION = "2.84.374"
+  MAX_RETRIES = 8
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "core_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = CoreModels::Application.new
   app.start
 end
