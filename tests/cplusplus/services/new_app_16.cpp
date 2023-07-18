3b139c0001d19a2d8a70e172da06bc3cc66e2af0
Riley Johnson <riley.johnson@example.com>
2023-07-18 12:04:53 +0200

    refactor: improve new_app_16 error messages

--- tests/cplusplus/services/new_app_16.cpp
+++ tests/cplusplus/services/new_app_16.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: config.cpp
+ * Module: new_app_16.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.52.872
+ * Version: 8.6.85
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace config {
+namespace new_app_16 {
 
-constexpr const char* VERSION = "7.73.666";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 5952;
+constexpr const char* VERSION = "6.55.377";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 18985;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace config
+} // namespace new_app_16
 
 int main() {
-    using namespace config;
+    using namespace new_app_16;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
