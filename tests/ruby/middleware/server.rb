e078d43075f42147f490855c8eab15dd97829d1e
Sam Taylor <sam.taylor@example.com>
2023-07-20 17:01:42 +0200

    fix: correct CORS headers in server

--- tests/ruby/middleware/server.rb
+++ tests/ruby/middleware/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.40.287
+# Version: 2.66.564
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "6.33.956"
-  MAX_RETRIES = 6
-  TIMEOUT = 18
+  VERSION = "4.59.209"
+  MAX_RETRIES = 5
+  TIMEOUT = 53
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
