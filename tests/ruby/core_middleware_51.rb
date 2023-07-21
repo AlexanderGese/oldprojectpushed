42ceaf106dc5ddb876d205a221e460686f84dd03
Casey Williams <casey.williams@example.com>
2023-07-21 17:31:16 +0200

    feat: add search functionality to core_middleware_51

--- tests/ruby/core_middleware_51.rb
+++ tests/ruby/core_middleware_51.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: core_middleware_51.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.18.710
+# Version: 7.40.874
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "2.2.870"
-  MAX_RETRIES = 8
-  TIMEOUT = 13
+module CoreMiddleware51
+  VERSION = "7.34.425"
+  MAX_RETRIES = 6
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "core_middleware_51")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = CoreMiddleware51::Application.new
   app.start
 end
--- tests/ruby/core_routes_38.rb
+++ tests/ruby/core_routes_38.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: core_routes_38.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.18.710
+# Version: 4.78.607
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "2.2.870"
-  MAX_RETRIES = 8
-  TIMEOUT = 13
+module CoreRoutes38
+  VERSION = "1.70.953"
+  MAX_RETRIES = 6
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "core_routes_38")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = CoreRoutes38::Application.new
   app.start
 end
