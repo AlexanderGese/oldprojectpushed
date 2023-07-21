7092d44a2e3ef9f0745a7f070ca0b5696dd1109a
Jamie Davis <jamie.davis@example.com>
2023-07-21 15:12:46 +0200

    feat: add rate limiting to utils

--- tests/rust/core_routes_1.rs
+++ tests/rust/core_routes_1.rs
@@ -1,4 +1,4 @@
-//! Module: enhanced_lib.rs
+//! Module: core_routes_1.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.76.58";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "8.86.65";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 39;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "enhanced_lib".to_string(),
+            app_name: "core_routes_1".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.6.552";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "1.38.353";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
