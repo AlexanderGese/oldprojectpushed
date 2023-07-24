a6d5eed56d846312f173c39722d063a6eba88e2d
Morgan Lee <morgan.lee@example.com>
2023-07-24 11:24:47 +0200

    fix: correct error propagation in lib_34

--- tests/rust/lib_34.rs
+++ tests/rust/lib_34.rs
@@ -1,4 +1,4 @@
-//! Module: impl_routes.rs
+//! Module: lib_34.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.21.490";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "9.56.430";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_routes".to_string(),
+            app_name: "lib_34".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
