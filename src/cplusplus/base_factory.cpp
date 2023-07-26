f2b05a42626a86a470818f637659261ec05d68c4
Riley Johnson <riley.johnson@example.com>
2023-07-26 22:42:32 +0200

    refactor: decouple base_factory dependencies

--- src/cplusplus/base_factory.cpp
+++ src/cplusplus/base_factory.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: base_factory.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.47.746
+ * Version: 6.31.756
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace base_factory {
 
-constexpr const char* VERSION = "5.48.508";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 13742;
+constexpr const char* VERSION = "5.46.636";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 8509;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
