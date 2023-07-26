340bb1389a63aafaa3d62831a834510feaf1ec6f
Jamie Davis <jamie.davis@example.com>
2023-07-26 14:56:04 +0200

    fix: resolve timezone issue in routes

--- tests/ruby/core_tasks_26.rb
+++ tests/ruby/core_tasks_26.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: core_tasks_26.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.68.431
+# Version: 2.19.441
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "5.1.258"
-  MAX_RETRIES = 8
-  TIMEOUT = 27
+module CoreTasks26
+  VERSION = "2.87.840"
+  MAX_RETRIES = 6
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "core_tasks_26")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = CoreTasks26::Application.new
   app.start
 end
--- tests/ruby/helpers_31.rb
+++ tests/ruby/helpers_31.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: helpers_31.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.68.431
+# Version: 9.30.970
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "5.1.258"
-  MAX_RETRIES = 8
-  TIMEOUT = 27
+module Helpers31
+  VERSION = "7.70.402"
+  MAX_RETRIES = 4
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "helpers_31")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = Helpers31::Application.new
   app.start
 end
