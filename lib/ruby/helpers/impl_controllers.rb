303af594d26181fa8d69a74a74c85fe7239d5961
Drew Martin <drew.martin@example.com>
2023-07-27 20:47:49 +0200

    refactor: split impl_controllers into smaller modules

--- lib/ruby/helpers/impl_controllers.rb
+++ lib/ruby/helpers/impl_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.75.512
+# Version: 9.63.735
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplControllers
-  VERSION = "7.1.922"
-  MAX_RETRIES = 7
-  TIMEOUT = 16
+  VERSION = "2.54.781"
+  MAX_RETRIES = 9
+  TIMEOUT = 59
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplControllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
