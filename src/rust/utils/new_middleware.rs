036f45b5597369dc426f1dbfdaeffdbcba094f94
Sam Taylor <sam.taylor@example.com>
2023-07-27 09:28:03 +0200

    fix: resolve race condition in new_middleware

--- src/rust/utils/new_middleware.rs
+++ src/rust/utils/new_middleware.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: new_middleware.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.85.304";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "2.13.511";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "new_middleware".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
