94d75945778cea116f7b50fd2a3d3a078339f90d
Jordan Smith <jordan.smith@example.com>
2023-07-28 17:00:47 +0200

    fix: handle large payloads in models

--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.90.139";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 45;
+const VERSION: &str = "6.38.985";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.83.930";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "5.62.769";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
