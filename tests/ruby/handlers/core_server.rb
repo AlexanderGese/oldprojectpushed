90241ba3a1d214f909d08d9f3d89724278b812b6
Morgan Lee <morgan.lee@example.com>
2023-08-01 14:39:26 +0200

    chore: configure CI/CD for config

--- tests/ruby/handlers/core_server.rb
+++ tests/ruby/handlers/core_server.rb
@@ -2,7 +2,7 @@
 
 # Module: core_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.52.577
+# Version: 5.47.604
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreServer
-  VERSION = "1.44.359"
+  VERSION = "2.85.474"
   MAX_RETRIES = 8
-  TIMEOUT = 12
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/helpers.rb
+++ tests/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.55.492
+# Version: 9.41.345
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "1.34.974"
-  MAX_RETRIES = 5
-  TIMEOUT = 5
+  VERSION = "4.94.651"
+  MAX_RETRIES = 3
+  TIMEOUT = 36
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
