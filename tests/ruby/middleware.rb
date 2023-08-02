215f191da8b414c6ce09816ada4241f7fc0a21d5
Casey Williams <casey.williams@example.com>
2023-08-02 16:34:32 +0200

    feat: add health check endpoint to middleware

--- tests/ruby/middleware.rb
+++ tests/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.3.369
+# Version: 1.38.66
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "2.96.574"
-  MAX_RETRIES = 3
-  TIMEOUT = 29
+  VERSION = "4.63.310"
+  MAX_RETRIES = 4
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
