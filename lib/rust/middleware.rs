d141e87c9081a017b8271eb570b685b28f8a5e49
Casey Williams <casey.williams@example.com>
2023-08-02 18:17:37 +0200

    feat: add logging to middleware

--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.38.565";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "6.60.214";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 58;
 
 #[derive(Debug, Clone)]
 pub struct Config {
