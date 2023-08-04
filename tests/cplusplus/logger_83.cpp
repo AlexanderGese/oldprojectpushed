93eb8af303a3c676273ccda63a5d38993f6c7664
Alex Chen <alex.chen@example.com>
2023-08-04 16:05:38 +0200

    refactor: improve impl_manager test coverage

--- tests/cplusplus/logger_83.cpp
+++ tests/cplusplus/logger_83.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: impl_manager.cpp
+ * Module: logger_83.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.83.919
+ * Version: 4.75.534
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace impl_manager {
+namespace logger_83 {
 
-constexpr const char* VERSION = "2.50.860";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 23085;
+constexpr const char* VERSION = "8.48.210";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 5628;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace impl_manager
+} // namespace logger_83
 
 int main() {
-    using namespace impl_manager;
+    using namespace logger_83;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
