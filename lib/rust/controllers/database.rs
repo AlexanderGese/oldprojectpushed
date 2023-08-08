428eac0946d4c33207cf97bb66f423d704df9a9d
Sam Taylor <sam.taylor@example.com>
2023-08-08 11:25:56 +0200

    refactor: decouple models dependencies

--- lib/rust/controllers/database.rs
+++ lib/rust/controllers/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.24.665";
+const VERSION: &str = "7.0.700";
 const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 29;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils/models.rs
+++ lib/rust/utils/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.51.11";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "1.18.915";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
