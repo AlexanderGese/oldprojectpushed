15c74fcfcb2607931c63b30c7012c40d1e3cdc0f
Sam Taylor <sam.taylor@example.com>
2023-08-08 20:16:27 +0200

    fix: handle timeout in models requests

--- src/rust/models.rs
+++ src/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.16.530";
+const VERSION: &str = "1.99.621";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 10;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
