1d625dbdf8532c94dcd52bd87c6177e511e3ebe0
Drew Martin <drew.martin@example.com>
2023-08-10 10:52:56 +0200

    feat: add caching layer to controllers

--- src/ruby/controllers.rb
+++ src/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.74.281
+# Version: 2.1.770
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "9.66.429"
-  MAX_RETRIES = 10
-  TIMEOUT = 12
+  VERSION = "2.21.626"
+  MAX_RETRIES = 8
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Controllers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
