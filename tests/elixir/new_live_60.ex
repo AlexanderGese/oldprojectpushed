d65fa2703872e7ca4be055e64456e1ea8aeadf3e
Jamie Davis <jamie.davis@example.com>
2023-08-11 22:54:01 +0200

    refactor: decouple new_live_60 dependencies

--- tests/elixir/new_live_60.ex
+++ tests/elixir/new_live_60.ex
@@ -1,13 +1,13 @@
-defmodule Application do
+defmodule New_Live_60 do
   @moduledoc """
-  Module: application.ex
+  Module: new_live_60.ex
   Elixir boilerplate - auto-generated
-  Version: 3.20.96
+  Version: 6.68.281
   """
 
-  @version "9.92.632"
-  @max_retries 6
-  @timeout 18223
+  @version "7.49.98"
+  @max_retries 5
+  @timeout 12968
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Application do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "application"),
+      app_name: Keyword.get(opts, :app_name, "new_live_60"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Application do
   end
 end
 
-defmodule ApplicationStore do
+defmodule New_Live_60Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ApplicationStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ApplicationEventBus do
+defmodule New_Live_60EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
