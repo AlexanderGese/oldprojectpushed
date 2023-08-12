bb911ced8666ee2587db85c135c6bb7dc196c9ab
Riley Johnson <riley.johnson@example.com>
2023-08-12 22:55:15 +0200

    feat: add export functionality to main

--- tests/rust/main.rs
+++ tests/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.37.716";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 18;
+const VERSION: &str = "5.33.913";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.38.985";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "8.27.957";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 40;
 
 #[derive(Debug, Clone)]
 pub struct Config {
