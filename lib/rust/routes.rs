39ae6580e01db48d4c0934052775812f99535fb4
Drew Martin <drew.martin@example.com>
2023-08-12 12:29:09 +0200

    feat: implement WebSocket support in routes

--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.70.456";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 60;
+const VERSION: &str = "9.83.269";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
