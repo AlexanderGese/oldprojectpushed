6b6313c7fdc2d16cf98ac3c23dcbef0a20591642
Alex Chen <alex.chen@example.com>
2023-08-13 14:04:28 +0200

    fix: patch security vulnerability in middleware

--- lib/rust/handlers/errors.rs
+++ lib/rust/handlers/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.90.549";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "3.29.971";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.51.82";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "7.16.139";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
