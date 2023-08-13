d8f784dd2b928ebbd3135722d11ac93ef397383d
Sam Taylor <sam.taylor@example.com>
2023-08-13 23:35:23 +0200

    refactor: optimize v2_engine_95 performance

---
+++ tests/cplusplus/v2_engine_95.cpp
@@ -0,0 +1,175 @@
+/**
+ * Module: v2_engine_95.cpp
+ * C++ boilerplate - auto-generated
+ * Version: 8.61.613
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <functional>
+#include <algorithm>
+#include <mutex>
+#include <optional>
+#include <chrono>
+#include <sstream>
+#include <stdexcept>
+
+namespace v2_engine_95 {
+
+constexpr const char* VERSION = "8.94.437";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 20949;
+
+enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
+
+class Logger {
+public:
+    static Logger& instance() {
+        static Logger logger;
+        return logger;
+    }
+
+    void log(LogLevel level, const std::string& message) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        auto now = std::chrono::system_clock::now();
+        auto time_t = std::chrono::system_clock::to_time_t(now);
+        std::cout << "[" << std::ctime(&time_t) << "] "
+                  << "[" << levelToString(level) << "] "
+                  << message << std::endl;
+    }
+
+    void debug(const std::string& msg) { log(LogLevel::DEBUG, msg); }
+    void info(const std::string& msg) { log(LogLevel::INFO, msg); }
+    void warn(const std::string& msg) { log(LogLevel::WARNING, msg); }
+    void error(const std::string& msg) { log(LogLevel::ERROR, msg); }
+
+private:
+    Logger() = default;
+    std::mutex mutex_;
+
+    static const char* levelToString(LogLevel level) {
+        switch (level) {
+            case LogLevel::DEBUG: return "DEBUG";
+            case LogLevel::INFO: return "INFO";
+            case LogLevel::WARNING: return "WARN";
+            case LogLevel::ERROR: return "ERROR";
+            case LogLevel::FATAL: return "FATAL";
+            default: return "UNKNOWN";
+        }
+    }
+};
+
+template<typename T>
+class Result {
+public:
+    static Result<T> ok(T value) { return Result(std::move(value), "", true); }
+    static Result<T> fail(const std::string& error) { return Result(T{}, error, false); }
+
+    bool isSuccess() const { return success_; }
+    const T& getData() const { return data_; }
+    const std::string& getError() const { return error_; }
+
+    template<typename U>
+    Result<U> map(std::function<U(const T&)> mapper) const {
+        if (success_) return Result<U>::ok(mapper(data_));
+        return Result<U>::fail(error_);
+    }
+
+private:
+    Result(T data, std::string error, bool success)
+        : data_(std::move(data)), error_(std::move(error)), success_(success) {}
+
+    T data_;
+    std::string error_;
+    bool success_;
+};
+
+template<typename T>
+class Repository {
+public:
+    virtual ~Repository() = default;
+    virtual std::optional<T> findById(const std::string& id) = 0;
+    virtual std::vector<T> findAll() = 0;
+    virtual T create(const std::string& id, T entity) = 0;
+    virtual bool remove(const std::string& id) = 0;
+    virtual size_t count() const = 0;
+};
+
+template<typename T>
+class InMemoryStore : public Repository<T> {
+public:
+    std::optional<T> findById(const std::string& id) override {
+        std::lock_guard<std::mutex> lock(mutex_);
+        auto it = store_.find(id);
+        if (it != store_.end()) return it->second;
+        return std::nullopt;
+    }
+
+    std::vector<T> findAll() override {
+        std::lock_guard<std::mutex> lock(mutex_);
+        std::vector<T> result;
+        result.reserve(store_.size());
+        for (const auto& [_, value] : store_) result.push_back(value);
+        return result;
+    }
+
+    T create(const std::string& id, T entity) override {
+        std::lock_guard<std::mutex> lock(mutex_);
+        store_[id] = entity;
+        return entity;
+    }
+
+    bool remove(const std::string& id) override {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return store_.erase(id) > 0;
+    }
+
+    size_t count() const override { return store_.size(); }
+
+private:
+    std::unordered_map<std::string, T> store_;
+    mutable std::mutex mutex_;
+};
+
+class EventBus {
+public:
+    using Handler = std::function<void(const std::string&)>;
+
+    void subscribe(const std::string& event, Handler handler) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        handlers_[event].push_back(std::move(handler));
+    }
+
+    void publish(const std::string& event, const std::string& data) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (auto it = handlers_.find(event); it != handlers_.end()) {
+            for (const auto& handler : it->second) {
+                handler(data);
+            }
+        }
+    }
+
+private:
+    std::map<std::string, std::vector<Handler>> handlers_;
+    std::mutex mutex_;
+};
+
+} // namespace v2_engine_95
+
+int main() {
+    using namespace v2_engine_95;
+    auto& logger = Logger::instance();
+    logger.info(std::string("Starting ") + VERSION);
+
+    EventBus bus;
+    bus.subscribe("startup", [&logger](const std::string& data) {
+        logger.info("Startup event: " + data);
+    });
+    bus.publish("startup", "Application initialized");
+
+    return 0;
+}
