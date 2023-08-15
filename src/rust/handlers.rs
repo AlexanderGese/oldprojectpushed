94a12eeddd47d3bce92ac2dbe210ca0314c15543
Alex Chen <alex.chen@example.com>
2023-08-15 12:36:07 +0200

    feat: implement retry logic in handlers

--- src/rust/handlers.rs
+++ src/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.8.181";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "3.67.100";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
