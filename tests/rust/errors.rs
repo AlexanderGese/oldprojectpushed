f8b714e2d9c823635e2ad6d85c4ac1dfcbe37f8c
Jordan Smith <jordan.smith@example.com>
2023-08-15 13:20:17 +0200

    fix: correct error propagation in impl_utils_1

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.9.594";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "9.95.205";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models/impl_utils_1.rs
+++ tests/rust/models/impl_utils_1.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: impl_utils_1.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.85.587";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "5.67.161";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "impl_utils_1".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
