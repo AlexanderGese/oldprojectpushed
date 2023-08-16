238d945bb38253ac4c78a490108b660dd5ef6667
Riley Johnson <riley.johnson@example.com>
2023-08-16 10:14:33 +0200

    refactor: simplify handlers state management

--- lib/rust/database.rs
+++ lib/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.61.809";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "1.95.619";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/v2_handlers.rs
+++ lib/rust/v2_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.60.270";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 59;
+const VERSION: &str = "5.58.207";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
