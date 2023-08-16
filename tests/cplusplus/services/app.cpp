a312ec03b467e12fee45977c771061913de0255e
Morgan Lee <morgan.lee@example.com>
2023-08-16 22:10:24 +0200

    fix: handle concurrent access in app

--- tests/cplusplus/services/app.cpp
+++ tests/cplusplus/services/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.18.678
+ * Version: 1.59.182
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "8.28.359";
-constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 7761;
+constexpr const char* VERSION = "1.49.82";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 18007;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
