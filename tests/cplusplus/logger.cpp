722ecdd37ced5730f9b674522cee4837e82440a8
Sam Taylor <sam.taylor@example.com>
2023-08-16 13:37:22 +0200

    feat: add event system to logger

--- tests/cplusplus/logger.cpp
+++ tests/cplusplus/logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.16.198
+ * Version: 6.42.385
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace logger {
 
-constexpr const char* VERSION = "1.33.325";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 22225;
+constexpr const char* VERSION = "6.56.127";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 5041;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/services/impl_app.cpp
+++ tests/cplusplus/services/impl_app.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: impl_app.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.57.672
+ * Version: 5.18.131
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace impl_app {
 
-constexpr const char* VERSION = "7.83.663";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 21877;
+constexpr const char* VERSION = "1.74.780";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 9855;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace impl_app
 
 int main() {
-    using namespace utils;
+    using namespace impl_app;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
