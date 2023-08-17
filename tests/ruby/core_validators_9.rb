8df1d6a98476b41e8ae9e7d835a76c0d5541d4cf
Riley Johnson <riley.johnson@example.com>
2023-08-17 22:17:36 +0200

    refactor: split new_middleware into smaller modules

--- tests/ruby/core_validators_9.rb
+++ tests/ruby/core_validators_9.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_middleware.rb
+# Module: core_validators_9.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.73.741
+# Version: 3.59.742
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewMiddleware
-  VERSION = "5.76.363"
-  MAX_RETRIES = 8
-  TIMEOUT = 6
+module CoreValidators9
+  VERSION = "7.71.181"
+  MAX_RETRIES = 4
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_middleware")
+      @app_name = opts.fetch(:app_name, "core_validators_9")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewMiddleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewMiddleware::Application.new
+  app = CoreValidators9::Application.new
   app.start
 end
