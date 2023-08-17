d07e83fa1abe673095a688e361e9425e7368bf52
Morgan Lee <morgan.lee@example.com>
2023-08-17 18:38:27 +0200

    fix: patch security vulnerability in v2_models_50

--- tests/rust/middleware/v2_models_50.rs
+++ tests/rust/middleware/v2_models_50.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: v2_models_50.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,8 +6,8 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.79.949";
-const MAX_RETRIES: u32 = 10;
+const VERSION: &str = "4.75.257";
+const MAX_RETRIES: u32 = 9;
 const TIMEOUT_SECS: u64 = 5;
 
 #[derive(Debug, Clone)]
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "v2_models_50".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
