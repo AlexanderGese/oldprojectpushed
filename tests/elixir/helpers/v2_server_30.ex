b4e75117ec4d1bbcea8199503e8a04d629d7d878
Riley Johnson <riley.johnson@example.com>
2023-08-17 13:46:47 +0200

    chore: bump v2_server_30 version

--- tests/elixir/helpers/v2_server_30.ex
+++ tests/elixir/helpers/v2_server_30.ex
@@ -1,13 +1,13 @@
-defmodule Context do
+defmodule V2_Server_30 do
   @moduledoc """
-  Module: context.ex
+  Module: v2_server_30.ex
   Elixir boilerplate - auto-generated
-  Version: 1.72.471
+  Version: 8.14.462
   """
 
-  @version "7.60.434"
-  @max_retries 5
-  @timeout 11549
+  @version "5.62.831"
+  @max_retries 7
+  @timeout 5104
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Context do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "context"),
+      app_name: Keyword.get(opts, :app_name, "v2_server_30"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Context do
   end
 end
 
-defmodule ContextStore do
+defmodule V2_Server_30Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule ContextStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule ContextEventBus do
+defmodule V2_Server_30EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
