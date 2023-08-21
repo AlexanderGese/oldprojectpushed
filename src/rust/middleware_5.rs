b4581f7e00bdd2b41d7df03e73007b34f0a568b0
Jamie Davis <jamie.davis@example.com>
2023-08-21 14:10:47 +0200

    fix: handle timeout in middleware_5 requests

--- src/rust/middleware_5.rs
+++ src/rust/middleware_5.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.38.810";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "2.22.377";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
