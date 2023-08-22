1d7ed93c2da38145d8bd48e4d7bca081b528261a
Jordan Smith <jordan.smith@example.com>
2023-08-22 23:04:41 +0200

    fix: patch security vulnerability in core_live

--- src/elixir/components/core_live.ex
+++ src/elixir/components/core_live.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Core_Live do
   @moduledoc """
-  Module: helpers.ex
+  Module: core_live.ex
   Elixir boilerplate - auto-generated
-  Version: 8.84.246
+  Version: 4.61.910
   """
 
-  @version "6.97.356"
+  @version "4.94.119"
   @max_retries 4
-  @timeout 9881
+  @timeout 10822
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "core_live"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule Core_LiveStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule Core_LiveEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
