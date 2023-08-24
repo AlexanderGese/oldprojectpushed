bd216eb279e2a803480f113ecea42293ae543f10
Jamie Davis <jamie.davis@example.com>
2023-08-24 15:34:26 +0200

    fix: resolve memory leak in lib

--- src/rust/services/database.rs
+++ src/rust/services/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.64.516";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "1.71.476";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 59;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/utils/middleware.rs
+++ src/rust/utils/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.25.72";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "4.19.310";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
