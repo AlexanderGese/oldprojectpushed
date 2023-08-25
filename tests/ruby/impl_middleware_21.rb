57e89928b30d209845e1e9efb28b6cd8a96d93b2
Riley Johnson <riley.johnson@example.com>
2023-08-25 17:55:47 +0200

    refactor: split impl_middleware_21 into smaller modules

--- tests/ruby/impl_middleware_21.rb
+++ tests/ruby/impl_middleware_21.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: impl_middleware_21.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.57.224
+# Version: 2.79.761
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "6.85.336"
-  MAX_RETRIES = 8
-  TIMEOUT = 48
+module ImplMiddleware21
+  VERSION = "4.46.426"
+  MAX_RETRIES = 7
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "impl_middleware_21")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = ImplMiddleware21::Application.new
   app.start
 end
--- tests/ruby/impl_routes_12.rb
+++ tests/ruby/impl_routes_12.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: impl_routes_12.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.57.224
+# Version: 7.6.360
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "6.85.336"
-  MAX_RETRIES = 8
-  TIMEOUT = 48
+module ImplRoutes12
+  VERSION = "8.15.266"
+  MAX_RETRIES = 9
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "impl_routes_12")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = ImplRoutes12::Application.new
   app.start
 end
