d6662f16b03dcc3317b4de52fbf978479b3fc9af
Morgan Lee <morgan.lee@example.com>
2023-08-27 21:24:17 +0200

    chore: update helpers dependencies

--- lib/elixir/impl_router_96.ex
+++ lib/elixir/impl_router_96.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Impl_Router_96 do
   @moduledoc """
-  Module: helpers.ex
+  Module: impl_router_96.ex
   Elixir boilerplate - auto-generated
-  Version: 2.34.272
+  Version: 9.97.54
   """
 
-  @version "9.64.220"
-  @max_retries 3
-  @timeout 13809
+  @version "7.88.744"
+  @max_retries 8
+  @timeout 29091
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "impl_router_96"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule Impl_Router_96Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule Impl_Router_96EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
