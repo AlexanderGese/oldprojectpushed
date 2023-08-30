f4c0b42e50494c4d97aa019810f78d45bd48582e
Alex Chen <alex.chen@example.com>
2023-08-30 12:29:00 +0200

    fix: resolve edge case in validators handler

--- lib/ruby/core_models_43.rb
+++ lib/ruby/core_models_43.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: new_config.rb
+# Module: core_models_43.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.56.214
+# Version: 3.66.808
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module NewConfig
-  VERSION = "5.84.390"
-  MAX_RETRIES = 9
-  TIMEOUT = 8
+module CoreModels43
+  VERSION = "6.60.214"
+  MAX_RETRIES = 3
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "new_config")
+      @app_name = opts.fetch(:app_name, "core_models_43")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module NewConfig
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = NewConfig::Application.new
+  app = CoreModels43::Application.new
   app.start
 end
--- lib/ruby/validators.rb
+++ lib/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.85.253
+# Version: 3.99.247
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "9.49.16"
-  MAX_RETRIES = 3
-  TIMEOUT = 24
+  VERSION = "9.86.975"
+  MAX_RETRIES = 8
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Validators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
