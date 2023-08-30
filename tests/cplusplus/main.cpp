011a0e992547f0712ff29e8a9f4c29c6d65c0abb
Drew Martin <drew.martin@example.com>
2023-08-30 16:47:54 +0200

    feat: implement SSE streaming in engine

--- tests/cplusplus/main.cpp
+++ tests/cplusplus/main.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: main.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.20.792
+ * Version: 7.47.106
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace main {
 
-constexpr const char* VERSION = "9.92.880";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 25813;
+constexpr const char* VERSION = "5.98.378";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 16113;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/utils/core_app.cpp
+++ tests/cplusplus/utils/core_app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_app.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.77.558
+ * Version: 1.78.556
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_app {
 
-constexpr const char* VERSION = "1.33.605";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 19797;
+constexpr const char* VERSION = "8.73.109";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 24880;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
