8e40211124927b93285d4f58b527488985af8f0a
Sam Taylor <sam.taylor@example.com>
2023-08-31 12:08:46 +0200

    fix: patch security vulnerability in enhanced_models_19

--- tests/rust/base_errors_8.rs
+++ tests/rust/base_errors_8.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: base_errors_8.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.28.239";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "3.37.252";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "base_errors_8".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/enhanced_models_19.rs
+++ tests/rust/enhanced_models_19.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: enhanced_models_19.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.28.239";
+const VERSION: &str = "2.8.449";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 30;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "enhanced_models_19".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
