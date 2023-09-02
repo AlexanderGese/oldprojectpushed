4adfbd4b1154021c56db84a167d983d439a5edf5
Casey Williams <casey.williams@example.com>
2023-09-02 16:46:21 +0200

    feat: add export functionality to middleware

--- src/rust/middleware.rs
+++ src/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.51.299";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "5.17.566";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/models/handlers.rs
+++ src/rust/models/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.74.297";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 56;
+const VERSION: &str = "7.93.834";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 41;
 
 #[derive(Debug, Clone)]
 pub struct Config {
