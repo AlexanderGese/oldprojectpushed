f2797f88b2b87ca920999adbafffcd51977d2ef9
Jamie Davis <jamie.davis@example.com>
2023-09-03 17:39:26 +0200

    fix: correct error propagation in middleware

--- lib/ruby/helpers/middleware.rb
+++ lib/ruby/helpers/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.77.928
+# Version: 8.34.18
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "4.54.736"
-  MAX_RETRIES = 6
-  TIMEOUT = 50
+  VERSION = "3.53.712"
+  MAX_RETRIES = 4
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
