25226c3f4ec57fc3165fdef2b0818f6b4c3dabb6
Alex Chen <alex.chen@example.com>
2023-09-04 10:45:09 +0200

    chore: update config configuration

--- tests/cplusplus/config.cpp
+++ tests/cplusplus/config.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: config.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.22.124
+ * Version: 4.79.349
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace config {
 
-constexpr const char* VERSION = "6.51.905";
+constexpr const char* VERSION = "6.58.898";
 constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 15957;
+constexpr int TIMEOUT_MS = 16398;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
