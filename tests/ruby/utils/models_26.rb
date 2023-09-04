6adfd5c82bb06129b2985ece4818fd108ab39141
Morgan Lee <morgan.lee@example.com>
2023-09-04 20:38:35 +0200

    feat: add export functionality to models

--- tests/ruby/utils/models_26.rb
+++ tests/ruby/utils/models_26.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: models.rb
+# Module: models_26.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.57.653
+# Version: 5.58.255
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Models
-  VERSION = "5.50.70"
-  MAX_RETRIES = 9
-  TIMEOUT = 56
+module Models26
+  VERSION = "3.2.821"
+  MAX_RETRIES = 7
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "models")
+      @app_name = opts.fetch(:app_name, "models_26")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Models
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Models::Application.new
+  app = Models26::Application.new
   app.start
 end
