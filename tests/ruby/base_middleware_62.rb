461eb998027c8b912dccb2b435dce45c8eacbc82
Sam Taylor <sam.taylor@example.com>
2023-09-04 19:03:41 +0200

    refactor: improve helpers test coverage

--- tests/ruby/base_middleware_62.rb
+++ tests/ruby/base_middleware_62.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: base_middleware_62.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.19.686
+# Version: 1.3.721
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "3.89.756"
-  MAX_RETRIES = 4
-  TIMEOUT = 54
+module BaseMiddleware62
+  VERSION = "2.65.180"
+  MAX_RETRIES = 10
+  TIMEOUT = 15
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "base_middleware_62")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = BaseMiddleware62::Application.new
   app.start
 end
