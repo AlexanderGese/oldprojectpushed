4863745487eca12cd683f0004dd28f7d19ac7704
Morgan Lee <morgan.lee@example.com>
2023-09-04 15:24:57 +0200

    feat: add validation to core_parser inputs

--- src/cplusplus/components/core_handler.cpp
+++ src/cplusplus/components/core_handler.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: engine.cpp
+ * Module: core_handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.53.6
+ * Version: 8.28.551
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace engine {
+namespace core_handler {
 
-constexpr const char* VERSION = "2.54.547";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 13733;
+constexpr const char* VERSION = "8.49.579";
+constexpr int MAX_RETRIES = 10;
+constexpr int TIMEOUT_MS = 17080;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace engine
+} // namespace core_handler
 
 int main() {
-    using namespace engine;
+    using namespace core_handler;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/core_parser.cpp
+++ src/cplusplus/core_parser.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: core_parser.cpp
  * C++ boilerplate - auto-generated
- * Version: 8.58.726
+ * Version: 1.69.748
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace core_parser {
 
-constexpr const char* VERSION = "4.66.373";
+constexpr const char* VERSION = "7.16.314";
 constexpr int MAX_RETRIES = 10;
-constexpr int TIMEOUT_MS = 14678;
+constexpr int TIMEOUT_MS = 19358;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
