47f6f7e96c729abb48837fcf29c4d22bac308895
Riley Johnson <riley.johnson@example.com>
2023-09-06 19:55:44 +0200

    fix: resolve encoding issue in new_main_41

--- src/rust/handlers/core_routes.rs
+++ src/rust/handlers/core_routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.74.297";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 48;
+const VERSION: &str = "3.31.55";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/helpers/core_handlers.rs
+++ src/rust/helpers/core_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.87.250";
+const VERSION: &str = "8.39.125";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 44;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
