dbf2a78350ecae0ebdbccdd444a28cdb12524869
Drew Martin <drew.martin@example.com>
2023-09-10 09:29:12 +0200

    chore: update models configuration

--- src/rust/handlers/models.rs
+++ src/rust/handlers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.94.467";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "9.84.867";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 37;
 
 #[derive(Debug, Clone)]
 pub struct Config {
