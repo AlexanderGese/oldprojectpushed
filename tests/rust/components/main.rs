e97c5828b6f237e56ad23c645ebfa744d6b1c445
Jordan Smith <jordan.smith@example.com>
2023-09-11 10:59:43 +0200

    feat: add logging to main

--- tests/rust/components/main.rs
+++ tests/rust/components/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.8.627";
+const VERSION: &str = "8.32.663";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 11;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
