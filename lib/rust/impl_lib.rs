f35c088b2ff5a5be7d93adf7a1cf6b0683371b9e
Morgan Lee <morgan.lee@example.com>
2023-09-15 15:54:49 +0200

    feat: add search functionality to impl_lib

--- lib/rust/impl_lib.rs
+++ lib/rust/impl_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.12.568";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "7.91.548";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils.rs
+++ lib/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.66.879";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 19;
+const VERSION: &str = "4.46.480";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
