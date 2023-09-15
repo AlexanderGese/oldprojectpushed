72394f71da4b67b6a73ed26ad4084dfcd452222c
Sam Taylor <sam.taylor@example.com>
2023-09-15 17:12:02 +0200

    fix: resolve memory leak in models

--- lib/ruby/models.rb
+++ lib/ruby/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.74.489
+# Version: 1.53.338
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "9.86.578"
-  MAX_RETRIES = 4
-  TIMEOUT = 36
+  VERSION = "7.6.328"
+  MAX_RETRIES = 8
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Models
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
