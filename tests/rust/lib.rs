35462aa546a63bd5f1a3e481031d9f0b69943087
Casey Williams <casey.williams@example.com>
2023-09-15 09:56:19 +0200

    fix: resolve edge case in main handler

--- tests/rust/lib.rs
+++ tests/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.68.885";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 9;
+const VERSION: &str = "2.84.181";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/utils_16.rs
+++ tests/rust/utils_16.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: utils_16.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.84.162";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "6.30.792";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "utils_16".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
