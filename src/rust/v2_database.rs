f81ffd30a2dbd759cb81cbff2679f8876adb4989
Alex Chen <alex.chen@example.com>
2023-09-16 20:43:40 +0200

    feat: add error handling to v2_database

--- src/rust/v2_database.rs
+++ src/rust/v2_database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.73.842";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "8.87.943";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 15;
 
 #[derive(Debug, Clone)]
 pub struct Config {
