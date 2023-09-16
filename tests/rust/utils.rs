dc8c14dea326156ce535854679734e5ebf8660c7
Alex Chen <alex.chen@example.com>
2023-09-16 13:47:03 +0200

    fix: resolve race condition in utils

--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.79.949";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 5;
+const VERSION: &str = "1.3.848";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 42;
 
 #[derive(Debug, Clone)]
 pub struct Config {
