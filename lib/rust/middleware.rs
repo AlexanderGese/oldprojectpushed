3fa99e3844946fbe4b4fa67f329a8273d4b316c0
Riley Johnson <riley.johnson@example.com>
2023-09-16 10:48:05 +0200

    chore: update config configuration

--- lib/rust/middleware.rs
+++ lib/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.16.139";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 16;
+const VERSION: &str = "9.7.181";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/new_errors.rs
+++ lib/rust/new_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.12.947";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 49;
+const VERSION: &str = "7.6.887";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 36;
 
 #[derive(Debug, Clone)]
 pub struct Config {
