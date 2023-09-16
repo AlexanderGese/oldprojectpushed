a5b2bf9a957360af544e0d1d93c384a109c0f9d4
Riley Johnson <riley.johnson@example.com>
2023-09-16 19:54:19 +0200

    refactor: improve models test coverage

--- src/rust/controllers/models.rs
+++ src/rust/controllers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.42.705";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "7.24.295";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
