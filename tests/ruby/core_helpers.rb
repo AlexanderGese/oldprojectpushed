50c200f2931e518c3dc583f6d34ef8c79be4fdde
Jordan Smith <jordan.smith@example.com>
2023-09-19 22:57:46 +0200

    feat: add health check endpoint to core_helpers

--- tests/ruby/core_helpers.rb
+++ tests/ruby/core_helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: core_helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.40.978
+# Version: 4.37.15
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreHelpers
-  VERSION = "2.90.350"
-  MAX_RETRIES = 6
-  TIMEOUT = 57
+  VERSION = "9.53.465"
+  MAX_RETRIES = 4
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreHelpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
