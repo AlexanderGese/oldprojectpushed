154fd0ebc02c96a1767a4e69fd9e8f0d95ae6737
Jamie Davis <jamie.davis@example.com>
2023-09-20 14:49:23 +0200

    feat: add error handling to errors

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.67.104";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "8.3.977";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 8;
 
 #[derive(Debug, Clone)]
 pub struct Config {
