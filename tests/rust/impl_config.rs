7f073af50564b88d470b15f9d09c5ca1c18807f9
Jamie Davis <jamie.davis@example.com>
2023-09-20 12:18:03 +0200

    feat: add search functionality to routes

--- tests/rust/impl_config.rs
+++ tests/rust/impl_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.40.12";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "6.18.323";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
