460ed86af5086183a416bec632dc465c6ee34211
Riley Johnson <riley.johnson@example.com>
2023-09-23 14:05:19 +0200

    refactor: extract handlers utility functions

--- lib/rust/handlers.rs
+++ lib/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.74.80";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 54;
+const VERSION: &str = "5.78.982";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
