5d52cd9d468ace3e050bc3a983ce043c1b326674
Sam Taylor <sam.taylor@example.com>
2023-09-24 13:42:52 +0200

    fix: resolve race condition in handlers

--- tests/rust/handlers.rs
+++ tests/rust/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.3.977";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 8;
+const VERSION: &str = "7.76.244";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
