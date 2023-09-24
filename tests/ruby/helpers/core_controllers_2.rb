a9509a405b601b00b383c37fc1b58488edb57a1e
Jordan Smith <jordan.smith@example.com>
2023-09-24 09:44:30 +0200

    feat: implement WebSocket support in core_controllers_2

--- tests/ruby/helpers/core_controllers_2.rb
+++ tests/ruby/helpers/core_controllers_2.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: core_controllers_2.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.69.326
+# Version: 9.69.868
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "8.47.249"
-  MAX_RETRIES = 4
-  TIMEOUT = 57
+module CoreControllers2
+  VERSION = "5.37.230"
+  MAX_RETRIES = 10
+  TIMEOUT = 9
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "core_controllers_2")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = CoreControllers2::Application.new
   app.start
 end
--- tests/ruby/models/impl_helpers_23.rb
+++ tests/ruby/models/impl_helpers_23.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: app.rb
+# Module: impl_helpers_23.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.69.326
+# Version: 1.34.308
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module App
-  VERSION = "8.47.249"
-  MAX_RETRIES = 4
-  TIMEOUT = 57
+module ImplHelpers23
+  VERSION = "5.96.694"
+  MAX_RETRIES = 3
+  TIMEOUT = 13
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "app")
+      @app_name = opts.fetch(:app_name, "impl_helpers_23")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module App
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = App::Application.new
+  app = ImplHelpers23::Application.new
   app.start
 end
