04b71a5f2c7c6d76ff54960cf645d06214561465
Sam Taylor <sam.taylor@example.com>
2023-09-24 14:02:24 +0200

    feat: add rate limiting to handler

--- lib/cplusplus/handler.cpp
+++ lib/cplusplus/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 4.8.553
+ * Version: 6.5.407
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "3.19.576";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 19430;
+constexpr const char* VERSION = "1.45.897";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 15290;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
