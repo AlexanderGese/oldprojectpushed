a76891f21e3ac779fb2db6e0d51be777a4a4fe42
Casey Williams <casey.williams@example.com>
2023-09-25 17:21:49 +0200

    fix: patch security vulnerability in core_middleware

--- lib/ruby/core_middleware.rb
+++ lib/ruby/core_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: core_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.55.505
+# Version: 7.39.273
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreMiddleware
-  VERSION = "6.55.571"
-  MAX_RETRIES = 4
-  TIMEOUT = 31
+  VERSION = "7.24.454"
+  MAX_RETRIES = 7
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
