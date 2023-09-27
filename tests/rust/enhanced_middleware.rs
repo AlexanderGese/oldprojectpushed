eea8b141cd0ebd3e0ab7d26d17576f6b4c7c588f
Riley Johnson <riley.johnson@example.com>
2023-09-27 11:31:24 +0200

    fix: resolve encoding issue in routes

--- tests/rust/enhanced_middleware.rs
+++ tests/rust/enhanced_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.22.698";
+const VERSION: &str = "8.68.654";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 30;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.88.22";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "1.34.296";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
