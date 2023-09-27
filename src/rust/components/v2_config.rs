8cdd860e82a260b8c23ef0ab81d9296e80d6fab1
Casey Williams <casey.williams@example.com>
2023-09-27 20:59:12 +0200

    fix: correct date formatting in errors

--- src/rust/components/v2_config.rs
+++ src/rust/components/v2_config.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: v2_config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.17.361";
+const VERSION: &str = "4.68.784";
 const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 24;
+const TIMEOUT_SECS: u64 = 49;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "v2_config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- src/rust/components/v2_models.rs
+++ src/rust/components/v2_models.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: v2_models.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.17.361";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "3.79.110";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 14;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "v2_models".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
