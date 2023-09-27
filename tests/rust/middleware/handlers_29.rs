683116373c1d2ab312682337a39343617079d33e
Drew Martin <drew.martin@example.com>
2023-09-27 17:55:17 +0200

    refactor: optimize main performance

--- tests/rust/middleware/handlers_29.rs
+++ tests/rust/middleware/handlers_29.rs
@@ -1,4 +1,4 @@
-//! Module: main.rs
+//! Module: handlers_29.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.82.895";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "1.69.142";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "main".to_string(),
+            app_name: "handlers_29".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/routes.rs
+++ tests/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.34.296";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "5.44.213";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
