6beca7939fe26b35b71d067911658c18f78e1fb7
Riley Johnson <riley.johnson@example.com>
2023-09-27 17:16:05 +0200

    feat: add caching layer to utils

--- src/rust/controllers/utils.rs
+++ src/rust/controllers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.32.638";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "3.36.220";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.75.779";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "2.89.296";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
