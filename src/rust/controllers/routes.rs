eb8feca80b0e7f310ccd6aba58435759812bef86
Alex Chen <alex.chen@example.com>
2023-09-28 22:53:27 +0200

    fix: resolve encoding issue in routes

--- src/rust/controllers/routes.rs
+++ src/rust/controllers/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.45.313";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 28;
+const VERSION: &str = "7.91.976";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 22;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/middleware/errors.rs
+++ src/rust/middleware/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.3.921";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "4.69.882";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 19;
 
 #[derive(Debug, Clone)]
 pub struct Config {
