6a0380ac37f7f71fc24edeedd416a9ab201f8114
Riley Johnson <riley.johnson@example.com>
2023-09-28 15:03:12 +0200

    fix: resolve memory leak in core_server

--- lib/ruby/core_server.rb
+++ lib/ruby/core_server.rb
@@ -2,7 +2,7 @@
 
 # Module: core_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.56.191
+# Version: 8.16.452
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreServer
-  VERSION = "9.26.233"
-  MAX_RETRIES = 7
-  TIMEOUT = 18
+  VERSION = "3.80.544"
+  MAX_RETRIES = 9
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
