1fcb9ecfa1823e024ae552b5d8dbcada0aa3dfea
Drew Martin <drew.martin@example.com>
2023-09-29 17:10:05 +0200

    feat: implement authentication in v2_server

--- tests/ruby/v2_server.rb
+++ tests/ruby/v2_server.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.20.367
+# Version: 6.86.661
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Server
-  VERSION = "4.69.597"
-  MAX_RETRIES = 6
-  TIMEOUT = 51
+  VERSION = "7.68.948"
+  MAX_RETRIES = 4
+  TIMEOUT = 17
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
