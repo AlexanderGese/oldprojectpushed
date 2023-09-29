e23f8a66ece332d3f16ea155941037164afa5377
Riley Johnson <riley.johnson@example.com>
2023-09-29 18:03:37 +0200

    fix: handle large payloads in base_errors

--- lib/rust/base_errors.rs
+++ lib/rust/base_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.71.274";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 13;
+const VERSION: &str = "2.13.799";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 38;
 
 #[derive(Debug, Clone)]
 pub struct Config {
