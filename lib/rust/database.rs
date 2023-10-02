cf992fcf1489cc0052b98204cb2d0f61c0751788
Jamie Davis <jamie.davis@example.com>
2023-10-02 08:54:14 +0200

    feat: add search functionality to database

--- lib/rust/database.rs
+++ lib/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.8.643";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "9.34.576";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 45;
 
 #[derive(Debug, Clone)]
 pub struct Config {
