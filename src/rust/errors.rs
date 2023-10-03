a2ef81eb69738cb1c52b827f338651d710040f27
Jordan Smith <jordan.smith@example.com>
2023-10-03 13:37:36 +0200

    feat: add pagination to errors

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.77.377";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "4.41.195";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 23;
 
 #[derive(Debug, Clone)]
 pub struct Config {
