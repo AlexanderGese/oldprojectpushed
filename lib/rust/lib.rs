fd22c60a1dcb690c04278ef3cf775568432e5bec
Jordan Smith <jordan.smith@example.com>
2023-10-03 09:15:38 +0200

    fix: correct error propagation in lib

--- lib/rust/lib.rs
+++ lib/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "6.86.51";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "8.5.203";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 17;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/utils/main.rs
+++ lib/rust/utils/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.71.46";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "2.46.722";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 34;
 
 #[derive(Debug, Clone)]
 pub struct Config {
