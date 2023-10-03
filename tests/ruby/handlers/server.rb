1c04325b2400c7a98cf6bb0ed26dd113c53c7cde
Drew Martin <drew.martin@example.com>
2023-10-03 10:24:14 +0200

    refactor: migrate new_tasks to async/await

--- tests/ruby/handlers/server.rb
+++ tests/ruby/handlers/server.rb
@@ -2,7 +2,7 @@
 
 # Module: server.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.23.962
+# Version: 4.24.79
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Server
-  VERSION = "9.85.394"
+  VERSION = "9.96.532"
   MAX_RETRIES = 5
-  TIMEOUT = 50
+  TIMEOUT = 11
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Server
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
