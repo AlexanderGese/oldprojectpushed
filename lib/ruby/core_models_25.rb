bcb525dc0c69f635827506674cfeaa653746ae67
Jordan Smith <jordan.smith@example.com>
2023-10-05 22:28:18 +0200

    refactor: split impl_models into smaller modules

--- lib/ruby/core_models_25.rb
+++ lib/ruby/core_models_25.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: impl_models.rb
+# Module: core_models_25.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.4.780
+# Version: 4.63.130
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module ImplModels
-  VERSION = "3.13.732"
-  MAX_RETRIES = 8
-  TIMEOUT = 34
+module CoreModels25
+  VERSION = "6.6.245"
+  MAX_RETRIES = 7
+  TIMEOUT = 35
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "impl_models")
+      @app_name = opts.fetch(:app_name, "core_models_25")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module ImplModels
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = ImplModels::Application.new
+  app = CoreModels25::Application.new
   app.start
 end
