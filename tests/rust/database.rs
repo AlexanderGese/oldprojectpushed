ee56aca2756849c5d312b31d920f8ae9a3894e96
Jordan Smith <jordan.smith@example.com>
2023-10-08 16:34:49 +0200

    fix: correct date formatting in database

--- tests/rust/database.rs
+++ tests/rust/database.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.36.701";
+const VERSION: &str = "8.13.33";
 const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 10;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
