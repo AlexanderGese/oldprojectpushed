69db9c624de8615c9d4e434638a822d9da03f5bc
Jamie Davis <jamie.davis@example.com>
2023-10-08 15:06:50 +0200

    fix: handle empty response in v2_server

--- lib/ruby/components/middleware.rb
+++ lib/ruby/components/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.18.268
+# Version: 6.86.394
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "5.49.838"
-  MAX_RETRIES = 4
-  TIMEOUT = 57
+  VERSION = "8.19.949"
+  MAX_RETRIES = 3
+  TIMEOUT = 34
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Middleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
--- lib/ruby/v2_server.rb
+++ lib/ruby/v2_server.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.32.733
+# Version: 5.5.540
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Server
-  VERSION = "3.45.486"
-  MAX_RETRIES = 8
-  TIMEOUT = 55
+  VERSION = "5.36.960"
+  MAX_RETRIES = 4
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
