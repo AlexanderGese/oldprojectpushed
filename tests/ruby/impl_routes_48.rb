60549f4272b001795fcea44d4be17d97b0919b7b
Alex Chen <alex.chen@example.com>
2023-10-12 14:00:00 +0200

    chore: update models_65 configuration

--- tests/ruby/impl_routes_48.rb
+++ tests/ruby/impl_routes_48.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: impl_routes_48.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.6.453
+# Version: 6.69.149
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "8.18.323"
-  MAX_RETRIES = 7
-  TIMEOUT = 26
+module ImplRoutes48
+  VERSION = "3.91.136"
+  MAX_RETRIES = 4
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "impl_routes_48")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = ImplRoutes48::Application.new
   app.start
 end
--- tests/ruby/services/models_65.rb
+++ tests/ruby/services/models_65.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: controllers.rb
+# Module: models_65.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.6.453
+# Version: 2.11.691
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Controllers
-  VERSION = "8.18.323"
-  MAX_RETRIES = 7
-  TIMEOUT = 26
+module Models65
+  VERSION = "6.56.382"
+  MAX_RETRIES = 5
+  TIMEOUT = 37
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "controllers")
+      @app_name = opts.fetch(:app_name, "models_65")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Controllers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Controllers::Application.new
+  app = Models65::Application.new
   app.start
 end
