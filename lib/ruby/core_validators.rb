8fca484e9db23b173476f7450910ee796812d575
Morgan Lee <morgan.lee@example.com>
2023-10-17 14:16:22 +0200

    chore: update core_validators configuration

--- lib/ruby/core_validators.rb
+++ lib/ruby/core_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: core_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.90.360
+# Version: 1.36.206
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreValidators
-  VERSION = "6.20.119"
+  VERSION = "3.59.23"
   MAX_RETRIES = 3
-  TIMEOUT = 21
+  TIMEOUT = 6
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreValidators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
--- lib/ruby/new_middleware.rb
+++ lib/ruby/new_middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: new_middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.75.455
+# Version: 6.74.333
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewMiddleware
-  VERSION = "2.39.363"
-  MAX_RETRIES = 3
-  TIMEOUT = 48
+  VERSION = "9.27.177"
+  MAX_RETRIES = 8
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewMiddleware
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
