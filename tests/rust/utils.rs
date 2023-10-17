13b17681483c806f746245ca2cd3a99d94d8d6d3
Casey Williams <casey.williams@example.com>
2023-10-17 09:40:18 +0200

    refactor: improve enhanced_middleware_80 test coverage

--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.3.848";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 42;
+const VERSION: &str = "8.45.258";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
