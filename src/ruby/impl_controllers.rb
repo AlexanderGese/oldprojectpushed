f363eb9c0ecab0a66979e03f05fcd71416798baf
Casey Williams <casey.williams@example.com>
2023-10-17 19:21:19 +0200

    feat: implement authentication in server

--- src/ruby/impl_controllers.rb
+++ src/ruby/impl_controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.18.817
+# Version: 2.49.308
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplControllers
-  VERSION = "9.64.882"
-  MAX_RETRIES = 9
-  TIMEOUT = 47
+  VERSION = "9.38.103"
+  MAX_RETRIES = 7
+  TIMEOUT = 22
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- src/ruby/new_validators.rb
+++ src/ruby/new_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: new_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.44.2
+# Version: 1.97.780
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewValidators
-  VERSION = "4.13.913"
-  MAX_RETRIES = 9
-  TIMEOUT = 60
+  VERSION = "3.21.938"
+  MAX_RETRIES = 8
+  TIMEOUT = 21
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewValidators
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
