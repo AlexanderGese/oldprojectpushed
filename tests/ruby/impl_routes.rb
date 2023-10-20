d1d238df37063a29fa7a924f6421acfb8cb80aea
Jordan Smith <jordan.smith@example.com>
2023-10-20 16:46:11 +0200

    fix: resolve memory leak in impl_routes

--- tests/ruby/impl_routes.rb
+++ tests/ruby/impl_routes.rb
@@ -2,7 +2,7 @@
 
 # Module: impl_routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.92.357
+# Version: 4.68.183
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module ImplRoutes
-  VERSION = "3.36.64"
-  MAX_RETRIES = 7
-  TIMEOUT = 29
+  VERSION = "1.59.576"
+  MAX_RETRIES = 6
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module ImplRoutes
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
