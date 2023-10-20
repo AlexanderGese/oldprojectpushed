8db6ec3a4913d6e8b4f36d5fca6939b1751737fb
Sam Taylor <sam.taylor@example.com>
2023-10-20 14:54:59 +0200

    feat: add error handling to v2_utils

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.41.195";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "7.34.283";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- src/rust/v2_utils.rs
+++ src/rust/v2_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.89.294";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "3.71.387";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
