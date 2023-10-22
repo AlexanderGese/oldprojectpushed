93637b0fd2b98e70585c7d166c7e149fbcd694c4
Morgan Lee <morgan.lee@example.com>
2023-10-22 21:19:50 +0200

    fix: resolve encoding issue in utils_88

--- tests/rust/helpers/utils.rs
+++ tests/rust/helpers/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.40.323";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 24;
+const VERSION: &str = "6.10.947";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/helpers/utils_88.rs
+++ tests/rust/helpers/utils_88.rs
@@ -1,4 +1,4 @@
-//! Module: database.rs
+//! Module: utils_88.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.38.285";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 29;
+const VERSION: &str = "8.52.332";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "database".to_string(),
+            app_name: "utils_88".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
