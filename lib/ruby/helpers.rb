13c0373f534a015d07f1a8aad0839a92e673a5b9
Riley Johnson <riley.johnson@example.com>
2023-10-23 21:14:34 +0200

    fix: handle large payloads in helpers

--- lib/ruby/helpers.rb
+++ lib/ruby/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.12.539
+# Version: 1.14.617
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "9.26.814"
-  MAX_RETRIES = 8
-  TIMEOUT = 52
+  VERSION = "4.14.866"
+  MAX_RETRIES = 7
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
