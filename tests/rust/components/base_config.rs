69db3e46aa0db5a593f60c3d5265dec8b8d06872
Jordan Smith <jordan.smith@example.com>
2023-10-23 11:21:14 +0200

    chore: configure CI/CD for routes

--- tests/rust/components/base_config.rs
+++ tests/rust/components/base_config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.67.591";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "2.70.487";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
