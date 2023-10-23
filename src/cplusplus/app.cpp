0d6036698a58113ebdcd37a7c544d8b404677c98
Drew Martin <drew.martin@example.com>
2023-10-23 18:31:20 +0200

    feat: add middleware support to app

--- src/cplusplus/app.cpp
+++ src/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.32.115
+ * Version: 6.41.586
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "5.29.648";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 16694;
+constexpr const char* VERSION = "4.77.131";
+constexpr int MAX_RETRIES = 7;
+constexpr int TIMEOUT_MS = 28291;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
