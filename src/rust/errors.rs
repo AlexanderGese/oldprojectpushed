2c508792ba0ba4a31d7038e9d6c2a3830019db37
Casey Williams <casey.williams@example.com>
2023-10-27 21:33:15 +0200

    chore: update errors dependencies

--- src/rust/errors.rs
+++ src/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.34.283";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 21;
+const VERSION: &str = "7.93.7";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 27;
 
 #[derive(Debug, Clone)]
 pub struct Config {
