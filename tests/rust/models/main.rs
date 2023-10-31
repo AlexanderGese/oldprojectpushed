891e467e7ccc166ce9e1c9cfd7afe6256fd184bb
Jamie Davis <jamie.davis@example.com>
2023-10-31 19:56:58 +0100

    test: add unit tests for config

--- tests/rust/models/main.rs
+++ tests/rust/models/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.69.893";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 47;
+const VERSION: &str = "8.55.141";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 6;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/utils.rs
+++ tests/rust/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.45.258";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 26;
+const VERSION: &str = "7.92.271";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
