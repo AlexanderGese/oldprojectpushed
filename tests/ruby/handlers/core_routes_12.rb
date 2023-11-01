f8455bf7f748274a4d32ddd8bfdbcb4e7c063bef
Jordan Smith <jordan.smith@example.com>
2023-11-01 20:50:15 +0100

    chore: clean up validators imports

--- tests/ruby/handlers/core_routes_12.rb
+++ tests/ruby/handlers/core_routes_12.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: core_routes_12.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.71.345
+# Version: 9.0.248
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "6.8.703"
-  MAX_RETRIES = 3
-  TIMEOUT = 60
+module CoreRoutes12
+  VERSION = "7.24.904"
+  MAX_RETRIES = 6
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "core_routes_12")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = CoreRoutes12::Application.new
   app.start
 end
