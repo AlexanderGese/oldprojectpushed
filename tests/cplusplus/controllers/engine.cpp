ef23a84d3a955f2b72e36dfb18939a6818d8baf9
Drew Martin <drew.martin@example.com>
2023-11-02 14:49:28 +0100

    chore: bump engine version

--- tests/cplusplus/controllers/engine.cpp
+++ tests/cplusplus/controllers/engine.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: engine.cpp
  * C++ boilerplate - auto-generated
- * Version: 6.84.861
+ * Version: 6.25.657
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace engine {
 
-constexpr const char* VERSION = "3.65.160";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 13315;
+constexpr const char* VERSION = "3.2.518";
+constexpr int MAX_RETRIES = 3;
+constexpr int TIMEOUT_MS = 6742;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- tests/cplusplus/enhanced_logger.cpp
+++ tests/cplusplus/enhanced_logger.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: enhanced_logger.cpp
  * C++ boilerplate - auto-generated
- * Version: 2.9.91
+ * Version: 3.94.331
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace enhanced_logger {
 
-constexpr const char* VERSION = "7.86.67";
-constexpr int MAX_RETRIES = 5;
-constexpr int TIMEOUT_MS = 14289;
+constexpr const char* VERSION = "8.64.152";
+constexpr int MAX_RETRIES = 4;
+constexpr int TIMEOUT_MS = 13665;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
