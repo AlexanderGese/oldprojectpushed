91a2dc9827cd29c97924fb0eff73bf9780c643b2
Riley Johnson <riley.johnson@example.com>
2023-11-02 15:43:45 +0100

    fix: correct type definitions in worker

--- tests/elixir/handlers/base_worker_93.ex
+++ tests/elixir/handlers/base_worker_93.ex
@@ -1,13 +1,13 @@
-defmodule Worker do
+defmodule Base_Worker_93 do
   @moduledoc """
-  Module: worker.ex
+  Module: base_worker_93.ex
   Elixir boilerplate - auto-generated
-  Version: 4.0.554
+  Version: 3.25.869
   """
 
-  @version "4.28.657"
-  @max_retries 6
-  @timeout 26656
+  @version "3.69.215"
+  @max_retries 5
+  @timeout 29206
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Worker do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "worker"),
+      app_name: Keyword.get(opts, :app_name, "base_worker_93"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Worker do
   end
 end
 
-defmodule WorkerStore do
+defmodule Base_Worker_93Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule WorkerStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule WorkerEventBus do
+defmodule Base_Worker_93EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
