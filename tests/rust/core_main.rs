2554c09ec0cf73d47f41e667752a4eacaf929fb9
Jordan Smith <jordan.smith@example.com>
2023-11-02 15:10:01 +0100

    feat: add event system to core_main

--- tests/rust/core_main.rs
+++ tests/rust/core_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.23.319";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 40;
+const VERSION: &str = "5.91.610";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/middleware/config.rs
+++ tests/rust/middleware/config.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.13.974";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "4.2.30";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 46;
 
 #[derive(Debug, Clone)]
 pub struct Config {
