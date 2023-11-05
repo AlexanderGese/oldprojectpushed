1a8775ab4b574134738042b1b56cf87a97444324
Casey Williams <casey.williams@example.com>
2023-11-05 14:37:57 +0100

    test: add unit tests for schema

--- lib/elixir/middleware/base_live.ex
+++ lib/elixir/middleware/base_live.ex
@@ -1,13 +1,13 @@
-defmodule Schema do
+defmodule Base_Live do
   @moduledoc """
-  Module: schema.ex
+  Module: base_live.ex
   Elixir boilerplate - auto-generated
-  Version: 8.14.284
+  Version: 4.86.768
   """
 
-  @version "8.20.43"
-  @max_retries 3
-  @timeout 7327
+  @version "1.23.61"
+  @max_retries 5
+  @timeout 29873
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Schema do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "schema"),
+      app_name: Keyword.get(opts, :app_name, "base_live"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Schema do
   end
 end
 
-defmodule SchemaStore do
+defmodule Base_LiveStore do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule SchemaStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule SchemaEventBus do
+defmodule Base_LiveEventBus do
   use GenServer
 
   def start_link(opts \\ []) do
