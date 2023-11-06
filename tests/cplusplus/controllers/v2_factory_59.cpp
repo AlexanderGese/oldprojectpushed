5c0ff5702789e4e1154edb8416fadd7120fe99fa
Sam Taylor <sam.taylor@example.com>
2023-11-06 16:00:54 +0100

    feat: add search functionality to handler

--- tests/cplusplus/controllers/v2_factory_59.cpp
+++ tests/cplusplus/controllers/v2_factory_59.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: handler.cpp
+ * Module: v2_factory_59.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.96.121
+ * Version: 2.0.971
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace handler {
+namespace v2_factory_59 {
 
-constexpr const char* VERSION = "3.33.476";
-constexpr int MAX_RETRIES = 8;
-constexpr int TIMEOUT_MS = 17679;
+constexpr const char* VERSION = "7.78.163";
+constexpr int MAX_RETRIES = 6;
+constexpr int TIMEOUT_MS = 9596;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace handler
+} // namespace v2_factory_59
 
 int main() {
-    using namespace handler;
+    using namespace v2_factory_59;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
