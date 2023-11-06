460db676e49aece78a00a1592187f37e58f5a1e2
Morgan Lee <morgan.lee@example.com>
2023-11-06 22:55:50 +0100

    fix: correct CORS headers in routes_90

--- tests/rust/controllers/new_middleware_22.rs
+++ tests/rust/controllers/new_middleware_22.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: new_middleware_22.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.45.65";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "7.7.100";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "new_middleware_22".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
