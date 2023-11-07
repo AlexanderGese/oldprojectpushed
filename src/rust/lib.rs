a242e37afda547e5684635d8bc0fb203d640c234
Riley Johnson <riley.johnson@example.com>
2023-11-07 22:42:35 +0100

    fix: handle null check in lib

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.86.448";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 57;
+const VERSION: &str = "6.90.382";
+const MAX_RETRIES: u32 = 3;
+const TIMEOUT_SECS: u64 = 50;
 
 #[derive(Debug, Clone)]
 pub struct Config {
