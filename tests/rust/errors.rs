5c57c37129ccaabbff11271feba50a12709b46b9
Drew Martin <drew.martin@example.com>
2023-11-11 20:24:09 +0100

    feat: add event system to utils

--- tests/rust/errors.rs
+++ tests/rust/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.10.237";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "2.35.213";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 26;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models/impl_middleware.rs
+++ tests/rust/models/impl_middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.89.220";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 44;
+const VERSION: &str = "1.43.953";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
