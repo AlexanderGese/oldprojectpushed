7ac5a7e037ecedde4c2e7e0dcd46e0a53e3f6fcb
Sam Taylor <sam.taylor@example.com>
2023-11-12 14:20:27 +0100

    refactor: improve lib test coverage

--- src/rust/middleware/lib.rs
+++ src/rust/middleware/lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.44.195";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 25;
+const VERSION: &str = "4.90.213";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 51;
 
 #[derive(Debug, Clone)]
 pub struct Config {
