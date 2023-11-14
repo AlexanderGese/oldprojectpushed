edb241f9722f4c7f76b7174e6f19f480a69ed854
Jordan Smith <jordan.smith@example.com>
2023-11-14 10:33:54 +0100

    refactor: improve validators_4 error messages

--- src/ruby/validators_4.rb
+++ src/ruby/validators_4.rb
@@ -2,7 +2,7 @@
 
 # Module: validators_4.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.11.331
+# Version: 5.8.7
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators4
-  VERSION = "2.96.348"
-  MAX_RETRIES = 10
-  TIMEOUT = 17
+  VERSION = "4.53.690"
+  MAX_RETRIES = 8
+  TIMEOUT = 18
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Validators4
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
