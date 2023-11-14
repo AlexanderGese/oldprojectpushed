24c8c4ce0670501e81696361a77c60e25fff4873
Drew Martin <drew.martin@example.com>
2023-11-14 20:09:51 +0100

    test: add unit tests for parser

--- src/cplusplus/impl_utils_55.cpp
+++ src/cplusplus/impl_utils_55.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: impl_utils_55.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.42.391
+ * Version: 1.75.833
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace impl_utils_55 {
 
-constexpr const char* VERSION = "1.93.794";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 14941;
+constexpr const char* VERSION = "5.22.960";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 24339;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace impl_utils_55
 
 int main() {
-    using namespace parser;
+    using namespace impl_utils_55;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
--- src/cplusplus/models/handler.cpp
+++ src/cplusplus/models/handler.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: handler.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.1.424
+ * Version: 9.0.583
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace handler {
 
-constexpr const char* VERSION = "5.44.270";
-constexpr int MAX_RETRIES = 6;
-constexpr int TIMEOUT_MS = 24632;
+constexpr const char* VERSION = "2.43.24";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 5357;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
