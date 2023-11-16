07838ebdaf6ef97cc9dd053a9a23b854c124e91f
Alex Chen <alex.chen@example.com>
2023-11-16 23:32:34 +0100

    fix: resolve race condition in new_models

--- src/ruby/components/new_models.rb
+++ src/ruby/components/new_models.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: new_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.74.295
+# Version: 7.93.892
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "4.14.416"
+module NewModels
+  VERSION = "6.2.64"
   MAX_RETRIES = 9
-  TIMEOUT = 24
+  TIMEOUT = 41
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "new_models")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = NewModels::Application.new
   app.start
 end
--- src/ruby/core_server.rb
+++ src/ruby/core_server.rb
@@ -2,7 +2,7 @@
 
 # Module: core_server.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.60.616
+# Version: 3.88.260
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module CoreServer
-  VERSION = "3.58.880"
-  MAX_RETRIES = 6
-  TIMEOUT = 14
+  VERSION = "6.54.616"
+  MAX_RETRIES = 3
+  TIMEOUT = 32
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module CoreServer
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
     end
 
     def production?
