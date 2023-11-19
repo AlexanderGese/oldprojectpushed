c30f796bfb965781f5da693d4b48bec2697ce32b
Jamie Davis <jamie.davis@example.com>
2023-11-19 19:34:04 +0100

    fix: correct error propagation in tasks

--- tests/ruby/middleware.rb
+++ tests/ruby/middleware.rb
@@ -2,7 +2,7 @@
 
 # Module: middleware.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.19.942
+# Version: 9.27.995
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Middleware
-  VERSION = "7.80.149"
-  MAX_RETRIES = 8
-  TIMEOUT = 25
+  VERSION = "2.96.123"
+  MAX_RETRIES = 3
+  TIMEOUT = 33
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
