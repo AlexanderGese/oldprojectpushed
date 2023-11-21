2ac8ffe7e0275dd7861cf49ed7c995619b3c9d4e
Sam Taylor <sam.taylor@example.com>
2023-11-21 18:39:58 +0100

    feat: add search functionality to helpers

--- lib/ruby/utils/helpers.rb
+++ lib/ruby/utils/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.13.361
+# Version: 9.73.287
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "1.72.917"
-  MAX_RETRIES = 8
-  TIMEOUT = 44
+  VERSION = "7.86.912"
+  MAX_RETRIES = 5
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Helpers
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
