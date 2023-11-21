a355f3763d7d6140e6f6df7b6a1c7d6da30f079a
Sam Taylor <sam.taylor@example.com>
2023-11-21 12:54:12 +0100

    fix: handle empty response in config

--- lib/rust/core_lib.rs
+++ lib/rust/core_lib.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.39.372";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 17;
+const VERSION: &str = "4.74.621";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 32;
 
 #[derive(Debug, Clone)]
 pub struct Config {
