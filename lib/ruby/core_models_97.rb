90766bf3ff232a5c0b1ac0890fc9dad98676006b
Casey Williams <casey.williams@example.com>
2023-11-27 14:17:13 +0100

    refactor: improve core_models_97 test coverage

--- lib/ruby/core_models_97.rb
+++ lib/ruby/core_models_97.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: validators.rb
+# Module: core_models_97.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.97.353
+# Version: 4.33.56
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Validators
-  VERSION = "4.57.342"
+module CoreModels97
+  VERSION = "4.31.964"
   MAX_RETRIES = 4
-  TIMEOUT = 43
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "validators")
+      @app_name = opts.fetch(:app_name, "core_models_97")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Validators
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Validators::Application.new
+  app = CoreModels97::Application.new
   app.start
 end
