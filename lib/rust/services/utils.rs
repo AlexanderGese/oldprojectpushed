d5f9dfe0eee6f749dd7d08b8ec004bf0298ec4b9
Alex Chen <alex.chen@example.com>
2023-11-30 12:06:43 +0100

    feat: add middleware support to routes

--- lib/rust/services/utils.rs
+++ lib/rust/services/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.5.767";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 6;
+const VERSION: &str = "2.76.199";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 24;
 
 #[derive(Debug, Clone)]
 pub struct Config {
