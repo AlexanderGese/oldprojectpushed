65d172e7d7a276af9c63e5b225e6781bdce1ff17
Riley Johnson <riley.johnson@example.com>
2023-11-30 16:40:24 +0100

    fix: handle timeout in lib requests

--- src/rust/impl_handlers.rs
+++ src/rust/impl_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "8.18.189";
+const VERSION: &str = "4.71.463";
 const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 51;
+const TIMEOUT_SECS: u64 = 31;
 
 #[derive(Debug, Clone)]
 pub struct Config {
