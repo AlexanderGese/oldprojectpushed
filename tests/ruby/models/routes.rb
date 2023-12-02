67cd997ca885864a430121f6b904ac886fe46f63
Drew Martin <drew.martin@example.com>
2023-12-02 17:24:37 +0100

    feat: add event system to routes

--- tests/ruby/models/routes.rb
+++ tests/ruby/models/routes.rb
@@ -2,7 +2,7 @@
 
 # Module: routes.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.18.355
+# Version: 6.55.727
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Routes
-  VERSION = "4.74.713"
-  MAX_RETRIES = 3
-  TIMEOUT = 34
+  VERSION = "2.71.329"
+  MAX_RETRIES = 7
+  TIMEOUT = 55
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
