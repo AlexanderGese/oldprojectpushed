92a128c4726be24424949bf7e707fabb0095d809
Drew Martin <drew.martin@example.com>
2023-12-03 17:45:41 +0100

    fix: handle timeout in middleware requests

--- tests/ruby/tasks.rb
+++ tests/ruby/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.92.881
+# Version: 9.2.684
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "3.79.462"
-  MAX_RETRIES = 4
-  TIMEOUT = 56
+  VERSION = "2.75.312"
+  MAX_RETRIES = 7
+  TIMEOUT = 44
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
