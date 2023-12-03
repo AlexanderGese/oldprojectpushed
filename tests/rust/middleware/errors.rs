63a0cfb71e40de7d03b141e2c8178acbfa0bb257
Riley Johnson <riley.johnson@example.com>
2023-12-03 20:54:58 +0100

    feat: add pagination to errors

--- tests/rust/middleware/errors.rs
+++ tests/rust/middleware/errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.26.1";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "4.4.965";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 9;
 
 #[derive(Debug, Clone)]
 pub struct Config {
