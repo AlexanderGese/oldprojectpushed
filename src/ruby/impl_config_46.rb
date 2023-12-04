4019686794b6706034546b7df3fb3a346362f592
Sam Taylor <sam.taylor@example.com>
2023-12-04 21:34:09 +0100

    fix: handle large payloads in core_app

--- src/ruby/impl_config_46.rb
+++ src/ruby/impl_config_46.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: tasks.rb
+# Module: impl_config_46.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.3.627
+# Version: 5.91.437
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Tasks
-  VERSION = "2.62.644"
+module ImplConfig46
+  VERSION = "5.42.309"
   MAX_RETRIES = 4
-  TIMEOUT = 28
+  TIMEOUT = 39
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "tasks")
+      @app_name = opts.fetch(:app_name, "impl_config_46")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Tasks
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Tasks::Application.new
+  app = ImplConfig46::Application.new
   app.start
 end
