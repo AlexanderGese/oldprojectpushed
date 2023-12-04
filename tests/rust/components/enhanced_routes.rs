23f778cfeb4000885eed0915ff802fbab49437fc
Sam Taylor <sam.taylor@example.com>
2023-12-04 15:17:45 +0100

    feat: add middleware support to middleware_49

--- tests/rust/components/enhanced_routes.rs
+++ tests/rust/components/enhanced_routes.rs
@@ -1,4 +1,4 @@
-//! Module: base_handlers.rs
+//! Module: enhanced_routes.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.15.470";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 43;
+const VERSION: &str = "2.20.687";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 25;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_handlers".to_string(),
+            app_name: "enhanced_routes".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/middleware_49.rs
+++ tests/rust/middleware_49.rs
@@ -1,4 +1,4 @@
-//! Module: base_handlers.rs
+//! Module: middleware_49.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.15.470";
+const VERSION: &str = "4.70.481";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 43;
+const TIMEOUT_SECS: u64 = 13;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "base_handlers".to_string(),
+            app_name: "middleware_49".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
