3ed4b0dc07db235b7bfe0ecf8eab20f4f4238492
Drew Martin <drew.martin@example.com>
2023-12-05 19:54:59 +0100

    feat: add export functionality to models

--- lib/ruby/controllers/models.rb
+++ lib/ruby/controllers/models.rb
@@ -2,7 +2,7 @@
 
 # Module: models.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.23.756
+# Version: 2.90.866
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Models
-  VERSION = "2.44.556"
-  MAX_RETRIES = 7
-  TIMEOUT = 55
+  VERSION = "5.21.187"
+  MAX_RETRIES = 6
+  TIMEOUT = 50
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
