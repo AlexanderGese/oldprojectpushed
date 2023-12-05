e98b96617b5acf348e65a52e59d064677cecdeac
Alex Chen <alex.chen@example.com>
2023-12-05 17:35:56 +0100

    refactor: split middleware into smaller modules

--- tests/rust/handlers/middleware.rs
+++ tests/rust/handlers/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.45.65";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 41;
+const VERSION: &str = "7.81.578";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 47;
 
 #[derive(Debug, Clone)]
 pub struct Config {
