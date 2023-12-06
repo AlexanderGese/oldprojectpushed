02d96ab0072742019bb21da88bd4610404dee56e
Casey Williams <casey.williams@example.com>
2023-12-06 23:50:32 +0100

    fix: resolve memory leak in routes

--- src/rust/routes.rs
+++ src/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.63.881";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 30;
+const VERSION: &str = "5.80.84";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
