ea70e8fd86c918d7d0a7e87edae0167f30e18e4e
Riley Johnson <riley.johnson@example.com>
2023-12-09 13:42:46 +0100

    refactor: decouple routes dependencies

--- tests/rust/base_models_80.rs
+++ tests/rust/base_models_80.rs
@@ -1,4 +1,4 @@
-//! Module: routes.rs
+//! Module: base_models_80.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.79.488";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 51;
+const VERSION: &str = "6.29.693";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 53;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "routes".to_string(),
+            app_name: "base_models_80".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
--- tests/rust/utils/handlers.rs
+++ tests/rust/utils/handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.21.862";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 11;
+const VERSION: &str = "5.22.283";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 18;
 
 #[derive(Debug, Clone)]
 pub struct Config {
