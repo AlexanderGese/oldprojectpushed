1c936ff99218bf47ede1fa38b582554cc9c54c5d
Sam Taylor <sam.taylor@example.com>
2023-12-11 20:26:44 +0100

    fix: resolve edge case in impl_middleware handler

--- src/ruby/new_models_82.rb
+++ src/ruby/new_models_82.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: new_models_82.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.10.506
+# Version: 7.65.853
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "5.89.268"
-  MAX_RETRIES = 7
-  TIMEOUT = 36
+module NewModels82
+  VERSION = "1.22.774"
+  MAX_RETRIES = 3
+  TIMEOUT = 34
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "new_models_82")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = NewModels82::Application.new
   app.start
 end
