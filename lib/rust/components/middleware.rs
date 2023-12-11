6fbb3fc2bb27f6c390c85e09b14d32264ec830ef
Casey Williams <casey.williams@example.com>
2023-12-11 15:52:22 +0100

    chore: configure CI/CD for middleware

--- lib/rust/components/middleware.rs
+++ lib/rust/components/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.30.927";
-const MAX_RETRIES: u32 = 7;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "3.66.725";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 48;
 
 #[derive(Debug, Clone)]
 pub struct Config {
