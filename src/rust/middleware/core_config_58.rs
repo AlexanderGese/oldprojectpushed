9cce2aaebcf9c4a222fc251af9e5f1e56e858428
Drew Martin <drew.martin@example.com>
2023-12-13 16:00:28 +0100

    docs: add documentation for core_config_58

--- src/rust/middleware/core_config_58.rs
+++ src/rust/middleware/core_config_58.rs
@@ -1,4 +1,4 @@
-//! Module: config.rs
+//! Module: core_config_58.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.14.862";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "4.89.108";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "config".to_string(),
+            app_name: "core_config_58".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
