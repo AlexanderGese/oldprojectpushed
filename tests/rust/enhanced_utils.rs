b7b1c7ff98ae46435c8a84bd8df22c64ed8b2957
Riley Johnson <riley.johnson@example.com>
2023-12-14 11:31:45 +0100

    feat: add pagination to models

--- tests/rust/enhanced_utils.rs
+++ tests/rust/enhanced_utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.33.401";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 53;
+const VERSION: &str = "1.8.344";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 29;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/models.rs
+++ tests/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "2.53.956";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 32;
+const VERSION: &str = "4.18.903";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
