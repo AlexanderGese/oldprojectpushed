7ca2f5cdcdd740738bac9530d9d3e72004bc9929
Sam Taylor <sam.taylor@example.com>
2023-12-14 14:44:02 +0100

    chore: configure CI/CD for v2_config

--- lib/rust/new_errors.rs
+++ lib/rust/new_errors.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.6.887";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "7.39.169";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
