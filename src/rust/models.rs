07376e53f4c16768f1b5e7bc3c1356bcaa6c596d
Drew Martin <drew.martin@example.com>
2023-12-15 21:43:48 +0100

    feat: add batch processing to models

--- src/rust/models.rs
+++ src/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.10.661";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 38;
+const VERSION: &str = "2.6.574";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 52;
 
 #[derive(Debug, Clone)]
 pub struct Config {
