b14eab3a3b473e52134397933c26de81cb603dc3
Jamie Davis <jamie.davis@example.com>
2023-12-17 19:38:37 +0100

    fix: handle concurrent access in new_utils

--- src/rust/components/new_utils.rs
+++ src/rust/components/new_utils.rs
@@ -1,4 +1,4 @@
-//! Module: new_errors.rs
+//! Module: new_utils.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.69.673";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 52;
+const VERSION: &str = "1.86.469";
+const MAX_RETRIES: u32 = 7;
+const TIMEOUT_SECS: u64 = 57;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "new_errors".to_string(),
+            app_name: "new_utils".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
