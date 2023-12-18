35b89e9d60bf6921953d7eef8df30034cc2c0c2e
Drew Martin <drew.martin@example.com>
2023-12-18 10:20:41 +0100

    refactor: simplify utils state management

--- tests/rust/core_handlers.rs
+++ tests/rust/core_handlers.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.90.122";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 14;
+const VERSION: &str = "8.18.926";
+const MAX_RETRIES: u32 = 5;
+const TIMEOUT_SECS: u64 = 7;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- tests/rust/core_lib_28.rs
+++ tests/rust/core_lib_28.rs
@@ -1,4 +1,4 @@
-//! Module: utils.rs
+//! Module: core_lib_28.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.44.361";
-const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 27;
+const VERSION: &str = "8.3.977";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 20;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "utils".to_string(),
+            app_name: "core_lib_28".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
