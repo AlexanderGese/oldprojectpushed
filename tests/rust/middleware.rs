94fd9f4183393a43bf9e8600dade08d9abbe0967
Morgan Lee <morgan.lee@example.com>
2023-12-18 09:45:48 +0100

    refactor: improve middleware test coverage

--- tests/rust/middleware.rs
+++ tests/rust/middleware.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.26.91";
+const VERSION: &str = "4.42.378";
 const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 8;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
