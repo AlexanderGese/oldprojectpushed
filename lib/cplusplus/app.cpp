7c4d8a6b68da38d859b41daadcf88861cd74bb39
Alex Chen <alex.chen@example.com>
2023-12-19 14:02:17 +0100

    fix: correct type definitions in parser

--- lib/cplusplus/app.cpp
+++ lib/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 9.49.438
+ * Version: 7.60.331
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "3.70.137";
-constexpr int MAX_RETRIES = 3;
-constexpr int TIMEOUT_MS = 5103;
+constexpr const char* VERSION = "6.67.522";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 11394;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
--- lib/cplusplus/utils/main_47.cpp
+++ lib/cplusplus/utils/main_47.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: parser.cpp
+ * Module: main_47.cpp
  * C++ boilerplate - auto-generated
- * Version: 5.85.619
+ * Version: 4.34.914
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace parser {
+namespace main_47 {
 
-constexpr const char* VERSION = "8.31.431";
-constexpr int MAX_RETRIES = 4;
-constexpr int TIMEOUT_MS = 13898;
+constexpr const char* VERSION = "7.74.744";
+constexpr int MAX_RETRIES = 9;
+constexpr int TIMEOUT_MS = 24666;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace parser
+} // namespace main_47
 
 int main() {
-    using namespace parser;
+    using namespace main_47;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
