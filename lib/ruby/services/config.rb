a8a30bc09297ab30dbcc0e73e90123e1b266ce7e
Sam Taylor <sam.taylor@example.com>
2023-12-19 11:16:08 +0100

    feat: add pagination to config

--- lib/ruby/services/config.rb
+++ lib/ruby/services/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 7.7.417
+# Version: 9.34.330
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "8.68.31"
-  MAX_RETRIES = 9
-  TIMEOUT = 15
+  VERSION = "4.70.603"
+  MAX_RETRIES = 6
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
