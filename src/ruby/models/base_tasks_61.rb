d2cb20f18a450e514c126c008cdef7193b0b7286
Alex Chen <alex.chen@example.com>
2023-12-19 17:31:17 +0100

    fix: correct error propagation in new_models

--- src/ruby/models/base_tasks_61.rb
+++ src/ruby/models/base_tasks_61.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_helpers.rb
+# Module: base_tasks_61.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.60.278
+# Version: 1.50.886
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreHelpers
-  VERSION = "5.77.126"
-  MAX_RETRIES = 7
-  TIMEOUT = 33
+module BaseTasks61
+  VERSION = "6.28.529"
+  MAX_RETRIES = 3
+  TIMEOUT = 24
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_helpers")
+      @app_name = opts.fetch(:app_name, "base_tasks_61")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreHelpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreHelpers::Application.new
+  app = BaseTasks61::Application.new
   app.start
 end
--- src/ruby/new_models.rb
+++ src/ruby/new_models.rb
@@ -2,7 +2,7 @@
 
 # Module: new_models.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.32.595
+# Version: 1.55.79
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module NewModels
-  VERSION = "2.77.748"
-  MAX_RETRIES = 3
-  TIMEOUT = 59
+  VERSION = "1.51.541"
+  MAX_RETRIES = 7
+  TIMEOUT = 28
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module NewModels
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
