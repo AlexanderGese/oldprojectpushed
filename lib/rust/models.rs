b8c875151b2736732d8958c8c8e1665056014452
Riley Johnson <riley.johnson@example.com>
2023-12-20 13:16:32 +0100

    fix: resolve race condition in models

--- lib/rust/models.rs
+++ lib/rust/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "4.90.675";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 58;
+const VERSION: &str = "8.68.743";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 30;
 
 #[derive(Debug, Clone)]
 pub struct Config {
