2b4bb1aaf351161001c61b29b1e61fd864646af1
Casey Williams <casey.williams@example.com>
2023-12-21 20:51:28 +0100

    fix: handle concurrent access in enhanced_routes

--- lib/rust/main.rs
+++ lib/rust/main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.74.593";
-const MAX_RETRIES: u32 = 10;
-const TIMEOUT_SECS: u64 = 34;
+const VERSION: &str = "1.97.38";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 43;
 
 #[derive(Debug, Clone)]
 pub struct Config {
