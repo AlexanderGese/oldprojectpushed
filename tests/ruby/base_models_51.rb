b02fc91f4bd4b63253bcc1fe1562da35249217a0
Riley Johnson <riley.johnson@example.com>
2023-12-22 15:08:56 +0100

    test: add unit tests for middleware

--- tests/ruby/base_models_51.rb
+++ tests/ruby/base_models_51.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: middleware.rb
+# Module: base_models_51.rb
 # Ruby boilerplate - auto-generated
-# Version: 8.54.960
+# Version: 5.94.510
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Middleware
-  VERSION = "5.46.259"
-  MAX_RETRIES = 5
-  TIMEOUT = 54
+module BaseModels51
+  VERSION = "2.64.318"
+  MAX_RETRIES = 6
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "middleware")
+      @app_name = opts.fetch(:app_name, "base_models_51")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Middleware
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Middleware::Application.new
+  app = BaseModels51::Application.new
   app.start
 end
