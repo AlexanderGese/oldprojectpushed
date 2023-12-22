6374df2636d63421111b33f0ea05d62327c75e1b
Morgan Lee <morgan.lee@example.com>
2023-12-22 13:24:21 +0100

    feat: add validation to config inputs

--- src/ruby/helpers_87.rb
+++ src/ruby/helpers_87.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: config.rb
+# Module: helpers_87.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.77.27
+# Version: 1.50.914
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module Config
-  VERSION = "9.61.284"
-  MAX_RETRIES = 7
-  TIMEOUT = 32
+module Helpers87
+  VERSION = "8.66.410"
+  MAX_RETRIES = 3
+  TIMEOUT = 12
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "config")
+      @app_name = opts.fetch(:app_name, "helpers_87")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
     end
 
     def production?
@@ -177,6 +177,6 @@ module Config
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Config::Application.new
+  app = Helpers87::Application.new
   app.start
 end
