d7d65b8bb4b702caf220985aab281af2b0bfb8b9
Riley Johnson <riley.johnson@example.com>
2023-12-23 22:04:27 +0100

    chore: add linting rules for handlers

--- tests/rust/errors_13.rs
+++ tests/rust/errors_13.rs
@@ -1,4 +1,4 @@
-//! Module: handlers.rs
+//! Module: errors_13.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "3.40.1";
-const MAX_RETRIES: u32 = 6;
-const TIMEOUT_SECS: u64 = 10;
+const VERSION: &str = "9.1.183";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 35;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "handlers".to_string(),
+            app_name: "errors_13".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
