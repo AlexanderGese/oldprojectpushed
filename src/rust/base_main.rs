047878691e040895c4ebece8db6ebe916e2c778f
Casey Williams <casey.williams@example.com>
2023-12-26 22:30:53 +0100

    feat: add export functionality to models_36

--- src/rust/base_main.rs
+++ src/rust/base_main.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.71.589";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 23;
+const VERSION: &str = "9.30.573";
+const MAX_RETRIES: u32 = 4;
+const TIMEOUT_SECS: u64 = 56;
 
 #[derive(Debug, Clone)]
 pub struct Config {
