e4569f3abc8dd28bac32ce3c63cd3e4a1908f3e8
Jordan Smith <jordan.smith@example.com>
2023-12-27 15:21:01 +0100

    refactor: migrate models to async/await

--- lib/rust/handlers/models.rs
+++ lib/rust/handlers/models.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.60.902";
-const MAX_RETRIES: u32 = 4;
-const TIMEOUT_SECS: u64 = 7;
+const VERSION: &str = "5.11.123";
+const MAX_RETRIES: u32 = 9;
+const TIMEOUT_SECS: u64 = 21;
 
 #[derive(Debug, Clone)]
 pub struct Config {
--- lib/rust/routes.rs
+++ lib/rust/routes.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "9.99.989";
+const VERSION: &str = "2.50.269";
 const MAX_RETRIES: u32 = 9;
-const TIMEOUT_SECS: u64 = 10;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
