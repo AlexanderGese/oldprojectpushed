a277dcd9b03cd7ed0580d240174afe69061c9578
Drew Martin <drew.martin@example.com>
2023-12-28 15:50:19 +0100

    refactor: extract new_config utility functions

--- lib/rust/middleware/new_config.rs
+++ lib/rust/middleware/new_config.rs
@@ -1,4 +1,4 @@
-//! Module: impl_errors.rs
+//! Module: new_config.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "7.20.154";
-const MAX_RETRIES: u32 = 3;
-const TIMEOUT_SECS: u64 = 35;
+const VERSION: &str = "6.20.990";
+const MAX_RETRIES: u32 = 6;
+const TIMEOUT_SECS: u64 = 12;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "impl_errors".to_string(),
+            app_name: "new_config".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
