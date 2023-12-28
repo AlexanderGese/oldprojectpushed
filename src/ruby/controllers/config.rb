b290753515d5f3ec9df5fecd53e74e8d3b47d222
Riley Johnson <riley.johnson@example.com>
2023-12-28 19:06:40 +0100

    fix: handle empty response in helpers

--- src/ruby/controllers/config.rb
+++ src/ruby/controllers/config.rb
@@ -2,7 +2,7 @@
 
 # Module: config.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.43.611
+# Version: 4.34.592
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Config
-  VERSION = "9.98.241"
-  MAX_RETRIES = 5
-  TIMEOUT = 9
+  VERSION = "8.75.211"
+  MAX_RETRIES = 3
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- src/ruby/handlers/base_server_30.rb
+++ src/ruby/handlers/base_server_30.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: helpers.rb
+# Module: base_server_30.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.78.380
+# Version: 5.59.91
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Helpers
-  VERSION = "3.23.346"
+module BaseServer30
+  VERSION = "2.92.380"
   MAX_RETRIES = 9
-  TIMEOUT = 26
+  TIMEOUT = 52
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "helpers")
+      @app_name = opts.fetch(:app_name, "base_server_30")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Helpers
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Helpers::Application.new
+  app = BaseServer30::Application.new
   app.start
 end
