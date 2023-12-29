8886d788adcbbbcb140ba470c6e057c797faaf72
Casey Williams <casey.williams@example.com>
2023-12-29 08:01:00 +0100

    refactor: improve v2_config test coverage

--- lib/ruby/v2_config.rb
+++ lib/ruby/v2_config.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_config.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.20.203
+# Version: 4.56.798
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Config
-  VERSION = "7.9.942"
-  MAX_RETRIES = 9
-  TIMEOUT = 12
+  VERSION = "3.7.986"
+  MAX_RETRIES = 10
+  TIMEOUT = 47
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module V2Config
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
