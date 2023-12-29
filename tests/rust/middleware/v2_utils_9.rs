84f956bdb70447bd3463ab922f8cfdc7d25c1b6e
Sam Taylor <sam.taylor@example.com>
2023-12-29 15:58:53 +0100

    fix: handle empty response in v2_utils_9

--- tests/rust/middleware/v2_utils_9.rs
+++ tests/rust/middleware/v2_utils_9.rs
@@ -1,4 +1,4 @@
-//! Module: middleware.rs
+//! Module: v2_utils_9.rs
 //! Auto-generated Rust boilerplate
 
 use std::collections::HashMap;
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "5.95.749";
-const MAX_RETRIES: u32 = 8;
-const TIMEOUT_SECS: u64 = 36;
+const VERSION: &str = "9.79.795";
+const MAX_RETRIES: u32 = 10;
+const TIMEOUT_SECS: u64 = 16;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -30,7 +30,7 @@ pub enum Environment {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            app_name: "middleware".to_string(),
+            app_name: "v2_utils_9".to_string(),
             version: VERSION.to_string(),
             environment: Environment::Production,
             debug: false,
