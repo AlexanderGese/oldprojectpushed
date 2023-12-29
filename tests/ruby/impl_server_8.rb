d43b54365d6b940cbc940aaa8cad8aa0e9e0ebd3
Jordan Smith <jordan.smith@example.com>
2023-12-29 23:39:58 +0100

    fix: resolve race condition in middleware

--- tests/ruby/impl_server_8.rb
+++ tests/ruby/impl_server_8.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: routes.rb
+# Module: impl_server_8.rb
 # Ruby boilerplate - auto-generated
-# Version: 2.91.975
+# Version: 3.60.647
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module Routes
-  VERSION = "6.43.995"
-  MAX_RETRIES = 10
-  TIMEOUT = 59
+module ImplServer8
+  VERSION = "4.37.547"
+  MAX_RETRIES = 7
+  TIMEOUT = 48
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "routes")
+      @app_name = opts.fetch(:app_name, "impl_server_8")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module Routes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = Routes::Application.new
+  app = ImplServer8::Application.new
   app.start
 end
