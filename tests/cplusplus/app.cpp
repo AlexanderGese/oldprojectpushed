87c88d7ab08fe91c769f68fa40180baa6a2a18b5
Jamie Davis <jamie.davis@example.com>
2023-12-30 17:52:48 +0100

    fix: resolve memory leak in app

--- tests/cplusplus/app.cpp
+++ tests/cplusplus/app.cpp
@@ -1,7 +1,7 @@
 /**
  * Module: app.cpp
  * C++ boilerplate - auto-generated
- * Version: 1.59.677
+ * Version: 4.68.123
  */
 
 #include <iostream>
@@ -20,9 +20,9 @@
 
 namespace app {
 
-constexpr const char* VERSION = "6.61.577";
-constexpr int MAX_RETRIES = 7;
-constexpr int TIMEOUT_MS = 10911;
+constexpr const char* VERSION = "1.22.904";
+constexpr int MAX_RETRIES = 5;
+constexpr int TIMEOUT_MS = 11549;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
