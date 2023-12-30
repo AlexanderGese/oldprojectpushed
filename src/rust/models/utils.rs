cb2871ded4e62943da1e673cf2977eb819e2dcc8
Sam Taylor <sam.taylor@example.com>
2023-12-30 12:42:49 +0100

    docs: add documentation for utils

--- src/rust/models/utils.rs
+++ src/rust/models/utils.rs
@@ -6,9 +6,9 @@ use std::sync::{Arc, RwLock};
 use std::fmt;
 use std::time::{Duration, Instant};
 
-const VERSION: &str = "1.8.542";
-const MAX_RETRIES: u32 = 5;
-const TIMEOUT_SECS: u64 = 33;
+const VERSION: &str = "7.52.942";
+const MAX_RETRIES: u32 = 8;
+const TIMEOUT_SECS: u64 = 55;
 
 #[derive(Debug, Clone)]
 pub struct Config {
