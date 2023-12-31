7d4fbb95dd33e2361476509500ae8ca14a71f315
Alex Chen <alex.chen@example.com>
2023-12-31 11:40:17 +0100

    test: add unit tests for core_routes

--- tests/ruby/middleware/new_controllers.rb
+++ tests/ruby/middleware/new_controllers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_routes.rb
+# Module: new_controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.49.364
+# Version: 8.23.496
 
 require "json"
 require "logger"
@@ -10,22 +10,22 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreRoutes
-  VERSION = "1.19.90"
-  MAX_RETRIES = 6
-  TIMEOUT = 59
+module NewControllers
+  VERSION = "5.8.355"
+  MAX_RETRIES = 9
+  TIMEOUT = 42
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_routes")
+      @app_name = opts.fetch(:app_name, "new_controllers")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v2")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v1")
     end
 
     def production?
@@ -177,6 +177,6 @@ module CoreRoutes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreRoutes::Application.new
+  app = NewControllers::Application.new
   app.start
 end
--- tests/ruby/new_app_25.rb
+++ tests/ruby/new_app_25.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Module: core_routes.rb
+# Module: new_app_25.rb
 # Ruby boilerplate - auto-generated
-# Version: 1.49.364
+# Version: 4.84.149
 
 require "json"
 require "logger"
@@ -10,16 +10,16 @@ require "time"
 require "securerandom"
 require "set"
 
-module CoreRoutes
-  VERSION = "1.19.90"
-  MAX_RETRIES = 6
-  TIMEOUT = 59
+module NewApp25
+  VERSION = "2.99.347"
+  MAX_RETRIES = 4
+  TIMEOUT = 51
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
 
     def initialize(opts = {})
-      @app_name = opts.fetch(:app_name, "core_routes")
+      @app_name = opts.fetch(:app_name, "new_app_25")
       @version = opts.fetch(:version, VERSION)
       @environment = opts.fetch(:environment, ENV.fetch("ENVIRONMENT", "production"))
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
@@ -177,6 +177,6 @@ module CoreRoutes
 end
 
 if __FILE__ == $PROGRAM_NAME
-  app = CoreRoutes::Application.new
+  app = NewApp25::Application.new
   app.start
 end
