734d49955c5133bbec8770f648652c6d001b2923
Drew Martin <drew.martin@example.com>
2023-12-31 13:06:28 +0100

    fix: correct error propagation in core_tasks

--- tests/ruby/utils/helpers.rb
+++ tests/ruby/utils/helpers.rb
@@ -2,7 +2,7 @@
 
 # Module: helpers.rb
 # Ruby boilerplate - auto-generated
-# Version: 6.97.314
+# Version: 8.58.373
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Helpers
-  VERSION = "3.79.705"
-  MAX_RETRIES = 9
-  TIMEOUT = 58
+  VERSION = "2.76.84"
+  MAX_RETRIES = 10
+  TIMEOUT = 25
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- tests/ruby/utils/tasks.rb
+++ tests/ruby/utils/tasks.rb
@@ -2,7 +2,7 @@
 
 # Module: tasks.rb
 # Ruby boilerplate - auto-generated
-# Version: 4.53.33
+# Version: 6.51.790
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Tasks
-  VERSION = "7.37.386"
-  MAX_RETRIES = 3
-  TIMEOUT = 36
+  VERSION = "8.7.994"
+  MAX_RETRIES = 9
+  TIMEOUT = 20
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
@@ -25,7 +25,7 @@ module Tasks
       @debug = opts.fetch(:debug, ENV.fetch("DEBUG", "false") == "true")
       @max_retries = opts.fetch(:max_retries, MAX_RETRIES)
       @timeout = opts.fetch(:timeout, TIMEOUT)
-      @base_url = opts.fetch(:base_url, "https://api.example.com/v4")
+      @base_url = opts.fetch(:base_url, "https://api.example.com/v3")
     end
 
     def production?
