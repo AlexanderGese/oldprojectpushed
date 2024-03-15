1c7df4ab7902d37a5405c9dfcb1a9b33dda2de93
Casey Williams <casey.williams@example.com>
2024-03-15 19:09:38 +0100

    chore: remove deprecated engine_32 code

--- tests/cplusplus/utils/impl_engine_73.cpp
+++ tests/cplusplus/utils/impl_engine_73.cpp
@@ -1,7 +1,7 @@
 /**
- * Module: utils.cpp
+ * Module: impl_engine_73.cpp
  * C++ boilerplate - auto-generated
- * Version: 3.77.645
+ * Version: 1.14.910
  */
 
 #include <iostream>
@@ -18,11 +18,11 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace utils {
+namespace impl_engine_73 {
 
-constexpr const char* VERSION = "2.36.279";
-constexpr int MAX_RETRIES = 9;
-constexpr int TIMEOUT_MS = 13645;
+constexpr const char* VERSION = "6.24.839";
+constexpr int MAX_RETRIES = 8;
+constexpr int TIMEOUT_MS = 27505;
 
 enum class LogLevel { DEBUG, INFO, WARNING, ERROR, FATAL };
 
@@ -158,10 +158,10 @@ private:
     std::mutex mutex_;
 };
 
-} // namespace utils
+} // namespace impl_engine_73
 
 int main() {
-    using namespace utils;
+    using namespace impl_engine_73;
     auto& logger = Logger::instance();
     logger.info(std::string("Starting ") + VERSION);
 
