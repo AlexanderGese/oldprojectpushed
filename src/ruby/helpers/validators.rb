686098d11c149c5685e13ace74243512938e8f1e
Sam Taylor <sam.taylor@example.com>
2024-07-05 10:54:38 +0200

    fix: handle timeout in validators requests

--- src/ruby/helpers/validators.rb
+++ src/ruby/helpers/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.44.266
+# Version: 6.99.981
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "1.68.200"
+  VERSION = "1.63.533"
   MAX_RETRIES = 10
-  TIMEOUT = 38
+  TIMEOUT = 45
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
