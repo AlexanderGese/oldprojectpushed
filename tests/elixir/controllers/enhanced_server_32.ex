9532b1fe302b609ad7551319f4bb9dabbac85b7e
Casey Williams <casey.williams@example.com>
2025-03-25 23:47:25 +0100

    refactor: improve helpers error messages

--- tests/elixir/controllers/enhanced_server_32.ex
+++ tests/elixir/controllers/enhanced_server_32.ex
@@ -1,13 +1,13 @@
-defmodule Helpers do
+defmodule Enhanced_Server_32 do
   @moduledoc """
-  Module: helpers.ex
+  Module: enhanced_server_32.ex
   Elixir boilerplate - auto-generated
-  Version: 6.95.498
+  Version: 8.96.835
   """
 
-  @version "2.66.725"
+  @version "1.80.982"
   @max_retries 8
-  @timeout 14008
+  @timeout 21894
 
   defstruct [
     :app_name,
@@ -20,7 +20,7 @@ defmodule Helpers do
 
   def new(opts \\ []) do
     %__MODULE__{
-      app_name: Keyword.get(opts, :app_name, "helpers"),
+      app_name: Keyword.get(opts, :app_name, "enhanced_server_32"),
       version: Keyword.get(opts, :version, @version),
       environment: System.get_env("ENVIRONMENT", "production"),
       debug: System.get_env("DEBUG") == "true",
@@ -48,7 +48,7 @@ defmodule Helpers do
   end
 end
 
-defmodule HelpersStore do
+defmodule Enhanced_Server_32Store do
   use GenServer
 
   def start_link(opts \\ []) do
@@ -72,7 +72,7 @@ defmodule HelpersStore do
   def handle_call(:count, _from, state), do: {:reply, map_size(state), state}
 end
 
-defmodule HelpersEventBus do
+defmodule Enhanced_Server_32EventBus do
   use GenServer
 
   def start_link(opts \\ []) do
