14f4109bb7a8deff75b17d590b471ebd2db45256
Alex Chen <alex.chen@example.com>
2025-04-14 14:29:57 +0200

    feat: add caching layer to validators

--- src/ruby/validators.rb
+++ src/ruby/validators.rb
@@ -2,7 +2,7 @@
 
 # Module: validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 9.34.32
+# Version: 8.46.243
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Validators
-  VERSION = "7.37.850"
-  MAX_RETRIES = 8
-  TIMEOUT = 40
+  VERSION = "9.74.795"
+  MAX_RETRIES = 7
+  TIMEOUT = 38
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
