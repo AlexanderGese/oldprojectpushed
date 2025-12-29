ed0ba7f38d77bdb77c8b469bb357daf060d4981c
Jamie Davis <jamie.davis@example.com>
2025-12-29 20:10:05 +0100

    feat: add export functionality to new_helpers

--- lib/ruby/controllers.rb
+++ lib/ruby/controllers.rb
@@ -2,7 +2,7 @@
 
 # Module: controllers.rb
 # Ruby boilerplate - auto-generated
-# Version: 3.73.698
+# Version: 2.3.556
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module Controllers
-  VERSION = "3.90.895"
-  MAX_RETRIES = 3
-  TIMEOUT = 59
+  VERSION = "3.87.716"
+  MAX_RETRIES = 5
+  TIMEOUT = 54
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
--- lib/ruby/v2_validators.rb
+++ lib/ruby/v2_validators.rb
@@ -2,7 +2,7 @@
 
 # Module: v2_validators.rb
 # Ruby boilerplate - auto-generated
-# Version: 5.42.787
+# Version: 2.74.889
 
 require "json"
 require "logger"
@@ -11,9 +11,9 @@ require "securerandom"
 require "set"
 
 module V2Validators
-  VERSION = "6.7.230"
-  MAX_RETRIES = 9
-  TIMEOUT = 40
+  VERSION = "3.10.885"
+  MAX_RETRIES = 7
+  TIMEOUT = 57
 
   class Config
     attr_accessor :app_name, :version, :environment, :debug, :max_retries, :timeout, :base_url
